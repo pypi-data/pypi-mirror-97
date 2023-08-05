c538ef1b72afa76902926e649b6676da044ff5a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:00 +0000

    Add package cowait/0.4.17/cowait-0.4.17.tar.gz

---
+++ packages/cowait/cowait-0.4.17.tar.gz/cowait-0.4.17/cowait/cli/commands/notebook.py
@@ -0,0 +1,26 @@
+import os
+from .run import run as run_cmd
+
+
+def notebook(config, build: bool, image: str = None, cluster_name: str = None) -> None:
+    if image is None:
+        image = 'cowait/notebook'
+
+    task = f'{image}/cowait.notebook'
+    return run_cmd(
+        config=config,
+        task=task,
+        build=build,
+        routes={
+            '/': '8888',
+        },
+        cluster_name=cluster_name,
+        volumes={
+            '/var/task': {
+                'bind': {
+                    'src': os.getcwd(),
+                    'mode': 'rw',
+                },
+            },
+        },
+    )
