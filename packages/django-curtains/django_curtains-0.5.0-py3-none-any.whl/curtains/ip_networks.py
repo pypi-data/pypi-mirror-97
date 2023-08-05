0f3701e4942198c842da19319aa41278e3f1b5fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:54 +0000

    Add package django-curtains/0.5.0/django_curtains-0.5.0-py3-none-any.whl

---
+++ packages/django-curtains/django_curtains-0.5.0-py3-none-any.whl/curtains/ip_networks.py
@@ -0,0 +1,40 @@
+import ipaddress
+
+from django.conf import settings
+from django.http import HttpResponse
+
+
+IP_NETWORKS = [
+    ipaddress.ip_network(spec)
+    for spec in getattr(settings, "IP_NETWORKS", ["127.0.0.0/8"])
+]
+
+BLOCKED_PAGE = """
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Unexpected IP address</h1>
+<p>Your IP %s is not in the allowlist, sorry.</p>
+</body>
+</html>
+"""
+
+
+def visitor_ip_address(request):
+    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(",")[0]
+    else:
+        ip = request.META.get("REMOTE_ADDR")
+    return ip
+
+
+def ip_networks_only(get_response):
+    def middleware(request):
+        ip = ipaddress.ip_address(visitor_ip_address(request))
+        if any(ip in network for network in IP_NETWORKS):
+            return get_response(request)
+        return HttpResponse(BLOCKED_PAGE % ip, content_type="text/html", status=403)
+
+    return middleware
---
+++ packages/django-curtains/django_curtains-0.5.0-py3-none-any.whl/curtains/middleware.py
@@ -0,0 +1,6 @@
+from .basic_auth import basic_auth
+from .ip_networks import ip_networks_only
+from .only_staff import only_staff
+
+
+__all__ = ("basic_auth", "ip_networks_only", "only_staff")
