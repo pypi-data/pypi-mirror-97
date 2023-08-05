974ff4c93b1207f72d81d08732029b78234f88c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:04 +0000

    Add package django-rest-sms-auth/0.1.12/django-rest-sms-auth-0.1.12.tar.gz

---
+++ packages/django-rest-sms-auth/django-rest-sms-auth-0.1.12.tar.gz/django-rest-sms-auth-0.1.12/sms_auth/api/exceptions.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+
+from ..conf import conf
+
+
+def get_error(error_message):
+    ERROR = {
+        'message': '',
+        'errors': [
+            {
+                "code": "0000",
+                "field": None,
+                "message": error_message
+            }
+        ]
+    }
+
+    return ERROR
+
+
+class RestApiException(serializers.ValidationError):
+    status_code = 400
+    default_detail = ""
+
+
+class SMSWaitException(RestApiException):
+    default_detail = get_error(conf.SMS_WAIT_TIME)
+
+
+class UserAlreadyExistException(RestApiException):
+    default_detail = get_error(conf.SMS_USER_ALREADY_EXIST)
+
+
+class SMSCodeNotFoundException(RestApiException):
+    default_detail = get_error(conf.SMS_CODE_NOT_FOUND)
