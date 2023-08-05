d5fc8d74967b3b2c0c46563e9ea3ad823e3a02a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:56 +0000

    Add package macop/1.1.2/macop-1.1.2.tar.gz

---
+++ packages/macop/macop-1.1.2.tar.gz/macop-1.1.2/macop/operators/base.py
@@ -0,0 +1,87 @@
+"""Abstract Operator classes
+"""
+# main imports
+from enum import Enum
+from abc import abstractmethod
+
+
+class KindOperator(Enum):
+    """Enum in order to recognize kind of operators
+    """
+    MUTATOR = 1
+    CROSSOVER = 2
+
+
+class Operator():
+    """Abstract Operator class which enables to update solution applying operator (computation)
+    """
+    @abstractmethod
+    def __init__(self):
+        """Abstract Operator initialiser
+        """
+        pass
+
+    @abstractmethod
+    def apply(self, solution):
+        """Apply the current operator transformation
+
+        Args:
+            solution: {:class:`~macop.solutions.base.Solution`} -- Solution instance
+        """
+        pass
+
+    def setAlgo(self, algo):
+        """Keep into operator reference of the whole algorithm
+           The reason is to better manage operator instance
+
+        Args:
+            algo: {:class:`~macop.algorithms.base.Algorithm`} -- the algorithm reference runned
+        """
+        self.algo = algo
+
+
+class Mutation(Operator):
+    """Abstract Mutation extend from Operator
+
+    Attributes:
+        kind: {:class:`~macop.operators.base.KindOperator`} -- specify the kind of operator
+    """
+    def __init__(self):
+        """Mutation initialiser in order to specify kind of Operator
+        """
+        self._kind = KindOperator.MUTATOR
+
+    def apply(self, solution):
+        """Apply mutation over solution in order to obtained new solution
+
+        Args:
+            solution: {:class:`~macop.solutions.base.Solution`} -- solution to use in order to create new solution
+
+        Return:
+            {:class:`~macop.solutions.base.Solution`} -- new generated solution
+        """
+        raise NotImplementedError
+
+
+class Crossover(Operator):
+    """Abstract crossover extend from Operator
+
+    Attributes:
+        kind: {:class:`~macop.operators.base.KindOperator`} -- specify the kind of operator
+    """
+    def __init__(self):
+        """Crossover initialiser in order to specify kind of Operator
+        """
+        self._kind = KindOperator.CROSSOVER
+
+    def apply(self, solution1, solution2=None):
+        """Apply crossover using two solutions in order to obtained new solution
+
+        Args:
+            solution1: {:class:`~macop.solutions.base.Solution`} -- the first solution to use for generating new solution
+            solution2: {:class:`~macop.solutions.base.Solution`} -- the second solution to use for generating new solution
+
+        Return:
+            {:class:`~macop.solutions.base.Solution`} -- new generated solution
+        """
+        raise NotImplementedError
---
+++ packages/macop/macop-1.1.2.tar.gz/macop-1.1.2/macop/policies/reinforcement.py
@@ -0,0 +1,181 @@
+"""Reinforcement learning policy classes implementations for Operator Selection Strategy
+"""
+# main imports
+import logging
+import random
+import math
+import numpy as np
+
+# module imports
+from macop.policies.base import Policy
+from macop.operators.base import KindOperator
+
+
+class UCBPolicy(Policy):
+    """Upper Confidence Bound (UCB) policy class which is used for applying UCB strategy when selecting and applying operator
+
+    Rather than performing exploration by simply selecting an arbitrary action, chosen with a probability that remains constant,
+    the UCB algorithm changes its exploration-exploitation balance as it gathers more knowledge of the environment.
+    It moves from being primarily focused on exploration, when actions that have been tried the least are preferred,
+    to instead concentrate on exploitation, selecting the action with the highest estimated reward.
+
+    - Resource link: https://banditalgs.com/2016/09/18/the-upper-confidence-bound-algorithm/
+
+    Attributes:
+        operators: {[:class:`~macop.operators.base.Operator`]} -- list of selected operators for the algorithm
+        C: {float} -- The second half of the UCB equation adds exploration, with the degree of exploration being controlled by the hyper-parameter ``C``.
+        exp_rate: {float} -- exploration rate (probability to choose randomly next operator)
+        rewards: {[float]} -- list of summed rewards obtained for each operator
+        occurrences: {[int]} -- number of use (selected) of each operator
+
+    The value of attribute ``C`` will allow us to specify whether we wish to exploit or explore further in relation to our earned rewards. 
+    A low value of ``C`` (e.g. 2) will allow more exploitation, while a high value of ``C`` (e.g. 1000) will allow exploration.
+
+    The ``exp_rate`` variable avoids using an operator too much and allows to explore from time to time (especially if the variable ``C`` has a small value). Typical value for ``exp_rate`` can be 0.1.
+
+    Example:
+
+    >>> # operators import
+    >>> from macop.operators.discrete.crossovers import SimpleCrossover
+    >>> from macop.operators.discrete.mutators import SimpleMutation
+    >>>
+    >>> # policy import
+    >>> from macop.policies.reinforcement import UCBPolicy
+    >>>
+    >>> # solution and algorithm
+    >>> from macop.solutions.discrete import BinarySolution
+    >>> from macop.algorithms.mono import IteratedLocalSearch
+    >>> from macop.algorithms.mono import HillClimberFirstImprovment
+    >>>
+    >>> # evaluator import
+    >>> from macop.evaluators.discrete.mono import KnapsackEvaluator
+    >>> # evaluator initialization (worths objects passed into data)
+    >>>
+    >>> worths = [ random.randint(0, 20) for i in range(20) ]
+    >>> evaluator = KnapsackEvaluator(data={'worths': worths})
+    >>>
+    >>> # validator specification (based on weights of each objects)
+    >>> weights = [ random.randint(5, 30) for i in range(20) ]
+    >>> validator = lambda solution: True if sum([weights[i] for i, value in enumerate(solution.data) if value == 1]) < 200 else False
+    >>>
+    >>> # initialiser function with lambda function
+    >>> initialiser = lambda x=20: BinarySolution.random(x, validator)
+    >>>
+    >>> # operators list with crossover and mutation
+    >>> operators = [SimpleCrossover(), SimpleMutation()]
+    >>> policy = UCBPolicy(operators)
+    >>> local_search = HillClimberFirstImprovment(initialiser, evaluator, operators, policy, validator, maximise=True, verbose=False)
+    >>> algo = IteratedLocalSearch(initialiser, evaluator, operators, policy, validator, localSearch=local_search, maximise=True, verbose=False)
+    >>> policy.occurences
+    [0, 0]
+    >>> solution = algo.run(100)
+    >>> type(solution).__name__
+    'BinarySolution'
+    >>> policy.occurences # one more due to first evaluation
+    [50, 52]
+    """
+    def __init__(self, operators, C=100., exp_rate=0.1):
+        """UCB Policy initialiser
+
+        Args:
+            operators: {[:class:`~macop.operators.base.Operator`]} -- list of selected operators for the algorithm
+            C: {float} -- The second half of the UCB equation adds exploration, with the degree of exploration being controlled by the hyper-parameter `C`.
+            exp_rate: {float} -- exploration rate (probability to choose randomly next operator)
+        """
+
+        # private members
+        self.operators = operators
+        self._C = C
+        self._exp_rate = exp_rate
+
+        # public members
+        self.rewards = [0. for o in self.operators]
+        self.occurences = [0 for o in self.operators]
+
+    def select(self):
+        """Select using Upper Confidence Bound the next operator to use (using acquired rewards)
+
+        Returns:
+            {:class:`~macop.operators.base.Operator`}: the selected operator
+        """
+
+        indices = [i for i, o in enumerate(self.occurences) if o == 0]
+
+        # random choice following exploration rate
+        if np.random.uniform(0, 1) <= self._exp_rate:
+
+            index = random.choice(range(len(self.operators)))
+            return self.operators[index]
+
+        elif len(indices) == 0:
+
+            # if operator have at least be used one time
+            ucbValues = []
+            nVisits = sum(self.occurences)
+
+            for i in range(len(self.operators)):
+
+                ucbValue = self.rewards[i] + self._C * math.sqrt(
+                    math.log(nVisits) / (self.occurences[i] + 0.1))
+                ucbValues.append(ucbValue)
+
+            return self.operators[ucbValues.index(max(ucbValues))]
+
+        else:
+            return self.operators[random.choice(indices)]
+
+    def apply(self, solution1, solution2=None):
+        """
+        Apply specific operator chosen to create new solution, computes its fitness and returns solution
+
+        - fitness improvment is saved as rewards
+        - selected operator occurence is also increased
+
+        Args:
+            solution1: {:class:`~macop.solutions.base.Solution`} -- the first solution to use for generating new solution
+            solution2: {:class:`~macop.solutions.base.Solution`} -- the second solution to use for generating new solution (in case of specific crossover, default is best solution from algorithm)
+
+        Returns:
+            {:class:`~macop.solutions.base.Solution`}: new generated solution
+        """
+
+        operator = self.select()
+
+        logging.info("---- Applying %s on %s" %
+                     (type(operator).__name__, solution1))
+
+        # default value of solution2 is current best solution
+        if solution2 is None and self.algo is not None:
+            solution2 = self.algo.result
+
+        # avoid use of crossover if only one solution is passed
+        if solution2 is None and operator._kind == KindOperator.CROSSOVER:
+
+            while operator._kind == KindOperator.CROSSOVER:
+                operator = self.select()
+
+        # apply operator on solution
+        if operator._kind == KindOperator.CROSSOVER:
+            newSolution = operator.apply(solution1, solution2)
+        else:
+            newSolution = operator.apply(solution1)
+
+        # compute fitness of new solution
+        newSolution.evaluate(self.algo.evaluator)
+
+        # compute fitness improvment rate
+        if self.algo._maximise:
+            fir = (newSolution.fitness - solution1.fitness) / solution1.fitness
+        else:
+            fir = (solution1.fitness - newSolution.fitness) / solution1.fitness
+
+        operator_index = self.operators.index(operator)
+
+        if fir > 0:
+            self.rewards[operator_index] += fir
+
+        self.occurences[operator_index] += 1
+
+        logging.info("---- Obtaining %s" % (newSolution))
+
+        return newSolution
