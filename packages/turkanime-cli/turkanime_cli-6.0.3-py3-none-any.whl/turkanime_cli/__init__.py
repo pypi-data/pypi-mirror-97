444238a8c9d410d6a82da6c2a8b0d882fb13859a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:02 +0000

    Add package turkanime-cli/6.0.3/turkanime_cli-6.0.3-py3-none-any.whl

---
+++ packages/turkanime-cli/turkanime_cli-6.0.3-py3-none-any.whl/turkanime_cli/__init__.py
@@ -0,0 +1,5 @@
+""" TurkAnimu API """
+from .anime import AnimeSorgula,Anime
+from .players import elementi_bekle
+from .tools import gereksinim_kontrol,webdriver_hazirla,prompt_tema
+from .config import Config
