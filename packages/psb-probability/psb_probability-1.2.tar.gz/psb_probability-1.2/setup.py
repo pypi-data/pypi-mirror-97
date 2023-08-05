ad428db24ed9dac592581e928bf7d4a443270152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:49 +0000

    Add package psb-probability/1.2/psb_probability-1.2.tar.gz

---
+++ packages/psb-probability/psb_probability-1.2.tar.gz/psb_probability-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='psb_probability',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['psb_probability'],
+      author = 'Prince Singh Bhadauria',
+      author_email = 'princesinghbhadauria@gmail.com',
+      zip_safe=False)
