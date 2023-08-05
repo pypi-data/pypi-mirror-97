30fb073be4f2a298747deb17847ca629e7c7109e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:06 +0000

    Add package itk-cleaver/0.0.2/itk_cleaver-0.0.2-cp36-cp36m-manylinux2014_x86_64.whl

---
+++ packages/itk-cleaver/itk_cleaver-0.0.2-cp36-cp36m-manylinux2014_x86_64.whl/itk/Configuration/CleaverConfig.py
@@ -0,0 +1,18 @@
+depends = ('ITKPyBase', 'ITKImageSources', 'ITKCommon', )
+templates = (
+  ('CleaverImageToMeshFilter', 'itk::CleaverImageToMeshFilter', 'itkCleaverImageToMeshFilterISS2ISS2', True, 'itk::Image< signed short,2 >, itk::Image< signed short,2 >'),
+  ('CleaverImageToMeshFilter', 'itk::CleaverImageToMeshFilter', 'itkCleaverImageToMeshFilterISS3ISS3', True, 'itk::Image< signed short,3 >, itk::Image< signed short,3 >'),
+  ('CleaverImageToMeshFilter', 'itk::CleaverImageToMeshFilter', 'itkCleaverImageToMeshFilterISS4ISS4', True, 'itk::Image< signed short,4 >, itk::Image< signed short,4 >'),
+  ('CleaverImageToMeshFilter', 'itk::CleaverImageToMeshFilter', 'itkCleaverImageToMeshFilterIUC2IUC2', True, 'itk::Image< unsigned char,2 >, itk::Image< unsigned char,2 >'),
+  ('CleaverImageToMeshFilter', 'itk::CleaverImageToMeshFilter', 'itkCleaverImageToMeshFilterIUC3IUC3', True, 'itk::Image< unsigned char,3 >, itk::Image< unsigned char,3 >'),
+  ('CleaverImageToMeshFilter', 'itk::CleaverImageToMeshFilter', 'itkCleaverImageToMeshFilterIUC4IUC4', True, 'itk::Image< unsigned char,4 >, itk::Image< unsigned char,4 >'),
+  ('CleaverImageToMeshFilter', 'itk::CleaverImageToMeshFilter', 'itkCleaverImageToMeshFilterIUS2IUS2', True, 'itk::Image< unsigned short,2 >, itk::Image< unsigned short,2 >'),
+  ('CleaverImageToMeshFilter', 'itk::CleaverImageToMeshFilter', 'itkCleaverImageToMeshFilterIUS3IUS3', True, 'itk::Image< unsigned short,3 >, itk::Image< unsigned short,3 >'),
+  ('CleaverImageToMeshFilter', 'itk::CleaverImageToMeshFilter', 'itkCleaverImageToMeshFilterIUS4IUS4', True, 'itk::Image< unsigned short,4 >, itk::Image< unsigned short,4 >'),
+  ('CleaverImageToMeshFilter', 'itk::CleaverImageToMeshFilter', 'itkCleaverImageToMeshFilterIF2IF2', True, 'itk::Image< float,2 >, itk::Image< float,2 >'),
+  ('CleaverImageToMeshFilter', 'itk::CleaverImageToMeshFilter', 'itkCleaverImageToMeshFilterIF3IF3', True, 'itk::Image< float,3 >, itk::Image< float,3 >'),
+  ('CleaverImageToMeshFilter', 'itk::CleaverImageToMeshFilter', 'itkCleaverImageToMeshFilterIF4IF4', True, 'itk::Image< float,4 >, itk::Image< float,4 >'),
+  ('CleaverImageToMeshFilter', 'itk::CleaverImageToMeshFilter', 'itkCleaverImageToMeshFilterID2ID2', True, 'itk::Image< double,2 >, itk::Image< double,2 >'),
+  ('CleaverImageToMeshFilter', 'itk::CleaverImageToMeshFilter', 'itkCleaverImageToMeshFilterID3ID3', True, 'itk::Image< double,3 >, itk::Image< double,3 >'),
+  ('CleaverImageToMeshFilter', 'itk::CleaverImageToMeshFilter', 'itkCleaverImageToMeshFilterID4ID4', True, 'itk::Image< double,4 >, itk::Image< double,4 >'),
+)
