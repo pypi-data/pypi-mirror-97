209761da2b145d93312ed6516fa92c9a3320356c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:15 +0000

    Add package airflow-db-logger/1.0.2/airflow_db_logger-1.0.2.tar.gz

---
+++ packages/airflow-db-logger/airflow_db_logger-1.0.2.tar.gz/airflow_db_logger-1.0.2/airflow_db_logger/utils.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import inspect
+from typing import Type
+
+global IS_LOADING_CONFIG
+IS_LOADING_CONFIG = False
+
+
+def get_is_no_color():
+    val = os.environ.get("NO_COLOR", "--no-color" in sys.argv)
+    if not isinstance(val, bool):
+        val = val.strip().lower()
+        os.environ["NO_COLOR"] = val
+    return val
+
+
+def colorize(val, color, add_reset=True):
+    if get_is_no_color():
+        return val
+    return color + val + ("\033[0m" if add_reset else "")
+
+
+def get_calling_frame_objects_by_type(otype: Type, offset: int = 1, first_only=False):
+    frames = inspect.stack()
+    frames = frames[offset:]
+    lst = []
+    for f in frames:
+        if "self" not in f.frame.f_locals:
+            continue
+        obj = f.frame.f_locals["self"]
+        if not isinstance(obj, otype):
+            continue
+        if first_only:
+            return obj
+        lst.append(obj)
+
+    if first_only:
+        return None
+
+    return lst
+
+
+class style:
+    GRAY = lambda x: colorize(str(x), "\033[90m")  # noqa: E731
+    LIGHT_GRAY = lambda x: colorize(str(x), "\033[37m")  # noqa: E731
+    BLACK = lambda x: colorize(str(x), "\033[30m")  # noqa: E731
+    RED = lambda x: colorize(str(x), "\033[31m")  # noqa: E731
+    GREEN = lambda x: colorize(str(x), "\033[32m")  # noqa: E731
+    YELLOW = lambda x: colorize(str(x), "\033[33m")  # noqa: E731
+    BLUE = lambda x: colorize(str(x), "\033[34m")  # noqa: E731
+    MAGENTA = lambda x: colorize(str(x), "\033[35m")  # noqa: E731
+    CYAN = lambda x: colorize(str(x), "\033[36m")  # noqa: E731
+    WHITE = lambda x: colorize(str(x), "\033[97m")  # noqa: E731
+    UNDERLINE = lambda x: colorize(str(x), "\033[4m")  # noqa: E731
+    RESET = lambda x: colorize(str(x), "\033[0m")  # noqa: E731
