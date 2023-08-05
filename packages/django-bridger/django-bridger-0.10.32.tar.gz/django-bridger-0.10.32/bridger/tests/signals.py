2b20ce21e9d3d6e5044f71196e838f4b39b265a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:59 +0000

    Add package django-bridger/0.10.32/django-bridger-0.10.32.tar.gz

---
+++ packages/django-bridger/django-bridger-0.10.32.tar.gz/django-bridger-0.10.32/bridger/tests/signals.py
@@ -0,0 +1,10 @@
+from django.dispatch import Signal
+add_factory = Signal()
+add_kwargs =  Signal()
+add_data_factory = Signal()
+create_permission_allowed = Signal()
+delete_permission_allowed = Signal()
+update_permission_allowed = Signal()
+retrieve_permission_allowed = Signal()
+get_retrieve_id_obj = Signal()
+get_specfics_module = Signal()
