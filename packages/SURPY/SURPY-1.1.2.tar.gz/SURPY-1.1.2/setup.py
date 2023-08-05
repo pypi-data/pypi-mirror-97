172a1234a6bdf7c81a3e2e90c2047af6390f6034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:06 +0000

    Add package SURPY/1.1.2/SURPY-1.1.2.tar.gz

---
+++ packages/SURPY/SURPY-1.1.2.tar.gz/SURPY-1.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='SURPY',
+version='1.1.2',
+description='SURPY python for  surgical data analysis',
+url='https://github.com/sasurasa/Surgical-Outcome-Analysis-on-Python/tree/SURG-package',
+author='Surasak Sangkhathat',
+author_email='s.sangkhathat@gmail.com',
+license='Prince of Songkla University',
+packages=['SURPY'],
+zip_safe=False)
