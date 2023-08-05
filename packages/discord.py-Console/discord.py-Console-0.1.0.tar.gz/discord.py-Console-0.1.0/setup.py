58bc373492ebb9b5e126ea81352a2807d5cc85c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:20 +0000

    Add package discord.py-Console/0.1.0/discord.py-Console-0.1.0.tar.gz

---
+++ packages/discord.py-Console/discord.py-Console-0.1.0.tar.gz/discord.py-Console-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="discord.py-Console",
+    version="0.1.0",
+    description="Executes commands from console while your bot is running.",
+    long_description=open("README.md").read(),
+    url="https://github.com/Mihitoko/discord.py-Console",
+    long_description_content_type="text/markdown",
+    author="Mihito",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3.7"
+    ],
+    packages=["dpyConsole"],
+    include_package_data=True,
+    install_requires=[
+        "discord.py"
+    ]
+)
