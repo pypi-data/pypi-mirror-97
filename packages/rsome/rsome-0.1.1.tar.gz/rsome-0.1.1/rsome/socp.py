119c1d7cc085a2f82a3a058b1f475df4d413511d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:41 +0000

    Add package rsome/0.1.1/rsome-0.1.1.tar.gz

---
+++ packages/rsome/rsome-0.1.1.tar.gz/rsome-0.1.1/rsome/socp.py
@@ -0,0 +1,247 @@
+from .lp import Model as LPModel
+from .lp import LinConstr, Bounds, CvxConstr, ConeConstr
+from .lp import LinProg
+import numpy as np
+import pandas as pd
+import scipy.sparse as sp
+from collections import Iterable
+
+
+class Model(LPModel):
+    """
+    The Model class creates an SOCP model object
+    """
+
+    def __init__(self, nobj=False, mtype='R', name=None):
+
+        super().__init__(nobj, mtype, name)
+        self.cvx_constr = []
+        self.cone_constr = []
+        # self.avar_indices = []
+
+    def reset(self):
+
+        self.lin_constr = []
+        self.pws_constr = []
+        self.cone_constr = []
+        self.bounds = []
+        self.aux_constr = []
+        self.aux_bounds = []
+        self.cvx_constr = []
+
+    def st(self, constr):
+
+        if isinstance(constr, Iterable):
+            for item in constr:
+                self.st(item)
+        elif isinstance(constr, (LinConstr, Bounds)):
+            super().st(constr)
+        elif isinstance(constr, CvxConstr):
+            if constr.model is not self:
+                raise ValueError('Constraints are not defined for this model.')
+            if constr.xtype in 'AMI':
+                super().st(constr)
+            elif constr.xtype in 'ESQ':
+                self.cvx_constr.append(constr)
+            else:
+                raise TypeError('Incorrect constraint type.')
+        elif isinstance(constr, ConeConstr):
+            if constr.model is not self:
+                raise ValueError('Constraints are not defined for this model.')
+            self.cone_constr.append(constr)
+        else:
+            raise TypeError('Unknown constraint type.')
+
+        self.pupdate = True
+        self.dupdate = True
+
+    def do_math(self, primal=True, refresh=True, obj=False):
+
+        if primal:
+            if self.primal is not None and not self.pupdate:
+                return self.primal
+
+            if refresh:
+                self.auxs = []
+                self.aux_constr = []
+                self.aux_bounds = []
+                self.last = self.vars[-1].first + self.vars[-1].size
+
+            more_cvx = []
+            if self.obj:
+                obj_constr = (self.vars[0] >= self.sign * self.obj)
+                if isinstance(obj_constr, CvxConstr):
+                    more_cvx.append(obj_constr)
+
+            qmat = []
+            for constr in self.cvx_constr + more_cvx:
+                if constr.xtype == 'E':
+                    aux_left = self.dvar(constr.affine_in.shape, aux=True)
+                    aux_right = self.dvar(constr.affine_out.shape, aux=True)
+                    self.aux_constr.append(constr.affine_in - aux_left == 0)
+                    self.aux_constr.append(constr.affine_out + aux_right == 0)
+                    bounds = (aux_right >= 0)
+                    if isinstance(bounds, Bounds):
+                        self.aux_bounds.append(bounds)
+                    else:
+                        self.aux_constr.append(bounds)
+                    qmat.append([aux_right.first] +
+                                list([aux_left.first + index
+                                      for index in range(aux_left.size)]))
+                elif constr.xtype == 'S':
+                    aux1 = self.dvar(constr.affine_out.shape, aux=True)
+                    aux2 = self.dvar(constr.affine_in.shape, aux=True)
+                    aux3 = self.dvar(constr.affine_out.shape, aux=True)
+                    self.aux_constr.append(aux1 - 0.5*(1+constr.affine_out)
+                                           == 0)
+                    self.aux_constr.append(aux2 - constr.affine_in == 0)
+                    self.aux_constr.append(aux3 - 0.5 * (1-constr.affine_out)
+                                           == 0)
+                    bounds = (aux3 >= 0)
+                    if isinstance(bounds, Bounds):
+                        self.aux_bounds.append(bounds)
+                    else:
+                        self.aux_constr.append(bounds)
+                    for i in range(constr.affine_in.size):
+                        qmat.append([aux3.first + i] +
+                                    [aux1.first + i, aux2.first + i])
+                elif constr.xtype == 'Q':
+                    aux1 = self.dvar(constr.affine_out.shape, aux=True)
+                    aux2 = self.dvar(constr.affine_in.shape, aux=True)
+                    aux3 = self.dvar(constr.affine_out.shape, aux=True)
+                    self.aux_constr.append(aux1 - 0.5 * (1+constr.affine_out)
+                                           == 0)
+                    self.aux_constr.append(aux2 - constr.affine_in == 0)
+                    self.aux_constr.append(aux3 - 0.5 * (1-constr.affine_out)
+                                           == 0)
+                    bounds = (aux3 >= 0)
+                    if isinstance(bounds, Bounds):
+                        self.aux_bounds.append(bounds)
+                    else:
+                        self.aux_constr.append(bounds)
+                    qmat.append([aux3.first] + [aux1.first] +
+                                list(aux2.first + np.arange(aux2.size)))
+
+            for constr in self.cone_constr:
+                qmat.append([constr.right_var.first + constr.right_index] +
+                            [constr.left_var.first + index
+                             for index in constr.left_index])
+
+            formula = super().do_math(primal=True, refresh=False, obj=obj)
+            formula = SOCProg(formula.linear, formula.const, formula.sense,
+                              formula.vtype, formula.ub, formula.lb,
+                              qmat, formula.obj)
+            self.primal = formula
+            self.pupdate = False
+
+            return formula
+        else:
+            if self.dual is not None and not self.dupdate:
+                return self.dual
+
+            primal = self.do_math(obj=obj)
+
+            dual_lp = super().do_math(primal=False, refresh=False, obj=obj)
+            if len(primal.qmat) == 0:
+                formula = SOCProg(dual_lp.linear, dual_lp.const, dual_lp.sense,
+                                  dual_lp.vtype, dual_lp.ub, dual_lp.lb,
+                                  [], dual_lp.obj)
+                self.dual = formula
+                return formula
+
+            eye_indices = [item for inner in primal.qmat for item in inner]
+            lin_indices = [ind for ind in range(primal.linear.shape[1])
+                           if ind not in eye_indices]
+            linear = dual_lp.linear[lin_indices, :]
+            const = dual_lp.const[lin_indices]
+            sense = dual_lp.sense[lin_indices]
+            obj = dual_lp.obj
+            vtype = dual_lp.vtype
+            ub = dual_lp.ub
+            lb = dual_lp.lb
+            qmat = []
+            for qc in primal.qmat:
+                lbz_index = dual_lp.linear[qc[0], :].indices
+                ub[lbz_index] = - lb[lbz_index]
+                lb[lbz_index] = 0
+                linear[:, lbz_index] = - linear[:, lbz_index]
+                obj[lbz_index] = - obj[lbz_index]
+                # ub[dual_lp.linear[qc[0], :].indices] = 0
+                ################## To update in the future
+                qmat.append(list(dual_lp.linear[qc, :].indices))
+
+            formula = SOCProg(linear, const, sense,
+                              vtype, ub, lb, qmat, obj)
+
+            self.dual = formula
+            self.dupdate = False
+
+            return formula
+
+
+class SOCProg(LinProg):
+    """
+    The SOCProg class creates an second-order cone program
+    """
+
+    def __init__(self, linear, const, sense, vtype, ub, lb, qmat, obj=None):
+
+        super().__init__(linear, const, sense, vtype, ub, lb, obj)
+        self.qmat = qmat
+
+    def __repr__(self, header=False):
+
+        qmat = self.qmat
+        string = 'Second order cone program object:\n'
+        string += super().__repr__(header=header)
+        string += '---------------------------------------------\n'
+        string += 'Number of SOC constraints:    {0}\n'.format(len(qmat))
+
+        return string
+
+    def showqc(self):
+
+        n = len(self.qmat)
+
+        if n == 0:
+            return None
+
+        indices = np.concatenate([item for item in self.qmat])
+        values = np.concatenate([[-1.0] + [1.0]*(len(item)-1)
+                                 for item in self.qmat])
+        indptr = [0] * (n + 1)
+        for i in range(n):
+            indptr[i+1] = indptr[i] + len(self.qmat[i])
+
+        var_names = ['x{0}'.format(i)
+                     for i in range(1, self.linear.shape[1] + 1)]
+        constr_names = ['QC{0}'.format(j)
+                        for j in range(1, n + 1)]
+        table = pd.DataFrame(sp.csr_matrix((values, indices, indptr),
+                             (n, self.linear.shape[1])).todense(),
+                             index=constr_names, columns=var_names)
+        table['sense'] = ['<='] * n
+        table['constant'] = [0.0] * n
+
+        return table
+
+    def show(self):
+
+        table = self.showlc()
+        obj_row = pd.DataFrame(self.obj.reshape((1, self.obj.size)),
+                               columns=table.columns[:-2], index=['Obj'])
+        table = pd.concat([obj_row, table], axis=0)
+
+        table_qc = self.showqc()
+        if table_qc is not None:
+            table = pd.concat([table, table_qc], axis=0)
+
+        ub = pd.DataFrame(self.ub.reshape((1, self.ub.size)),
+                          columns=table.columns[:-2], index=['UB'])
+        lb = pd.DataFrame(self.lb.reshape((1, self.lb.size)),
+                          columns=table.columns[:-2], index=['LB'])
+        vtype = pd.DataFrame(self.vtype.reshape((1, self.vtype.size)),
+                             columns=table.columns[:-2], index=['Type'])
+        table = pd.concat([table, ub, lb, vtype], axis=0)
+
+        return table.fillna('-')
---
+++ packages/rsome/rsome-0.1.1.tar.gz/rsome-0.1.1/rsome/subroutines.py
@@ -0,0 +1,230 @@
+import numpy as np
+import scipy.sparse as sp
+from numbers import Real
+from scipy.sparse import csr_matrix
+from collections import Iterable
+
+
+def flat(a_list):
+    flat_list = []
+    for item in a_list:
+        if isinstance(item, Iterable):
+            flat_list.extend(flat(item))
+        else:
+            flat_list.append(item)
+
+    return flat_list
+
+
+def sparse_mul(ndarray, affine):
+
+    array_ones = np.ones(ndarray.shape, dtype='int')
+    affine_ind = np.arange(affine.size, dtype='int').reshape(affine.shape)
+    affine_ones = np.ones(affine.shape, dtype='int')
+
+    index = (array_ones * affine_ind).flatten()
+    values = (ndarray * affine_ones).flatten()
+
+    size = index.size
+
+    return csr_matrix((values, (np.arange(size), index)),
+                      shape=(size, affine.size))
+
+
+def sp_matmul(ndarray, affine, shape):
+
+    size = np.prod(shape)
+
+    left = (ndarray.reshape((ndarray.size//ndarray.shape[-1],
+                             ndarray.shape[-1])) if ndarray.ndim > 1
+            else ndarray.reshape((1, ndarray.size)))
+    right = (affine if len(affine.shape) > 1
+             else affine.reshape((affine.size, 1)))
+
+    row, inner = left.shape
+    column = right.shape[-1]
+
+    index = np.arange(affine.size).reshape(affine.shape)
+    indim = index.ndim
+    index = np.transpose(np.tile(index, (1, row)),
+                         axes=list(range(indim-2)) + [indim-1,
+                                                      indim-2]).flatten()
+    index = np.tile(index, (size*inner//index.size, ))
+
+    data = np.tile(ndarray, (1, column)).flatten()
+    data = np.tile(data, size*inner//data.size)
+
+    indptr = [inner*i for i in range(size+1)]
+
+    return csr_matrix((data, index, indptr), shape=[size, affine.size])
+
+
+def sp_lmatmul(ndarray, affine, shape):
+
+    size = int(np.prod(shape))
+
+    left = (affine.reshape((affine.size // affine.shape[-1],
+                            affine.shape[-1])) if len(affine.shape) > 1
+            else affine.reshape((1, affine.size)))
+    right = (ndarray if ndarray.ndim > 1
+             else ndarray.reshape((ndarray.size, 1)))
+
+    row, inner = left.shape
+    column = right.shape[-1]
+
+    index = np.tile(np.arange(left.size).reshape(left.shape),
+                    (1, column)).flatten()
+    index = np.tile(index, (size * inner // index.size))
+    andim = right.ndim
+    data = np.tile(np.transpose(right,
+                                axes=list(range(andim - 2)) + [andim - 1,
+                                                               andim - 2]),
+                   (size * inner // ndarray.size, 1)).flatten()
+    indptr = [inner * i for i in range(size + 1)]
+
+    return csr_matrix((data, index, indptr), shape=[size, affine.size])
+
+
+def sp_trans(affine):
+
+    index = np.arange(affine.size).reshape(affine.shape).T.flatten()
+    indptr = np.arange(affine.size + 1)
+    data = np.ones(affine.size)
+
+    return csr_matrix((data, index, indptr), shape=[affine.size, affine.size])
+
+
+def index_array(shape):
+
+    if isinstance(shape, tuple):
+        size = np.prod(shape)
+    else:
+        size = int(shape)
+        shape = int(shape),
+
+    return np.arange(size, dtype=int).reshape(shape)
+
+
+def sparse_array(shape):
+
+    shape = shape if isinstance(shape, tuple) else (int(shape), )
+    size = np.prod(shape).item()
+    sparse = csr_matrix(([1.0]*size, np.arange(size, dtype=int),
+                         np.arange(size+1, dtype=int)), (size, size))
+    elements = [sparse[i, :] for i in range(size)]
+
+    return np.array(elements).reshape(shape)
+
+
+def array_to_sparse(array):
+
+    size = array.size
+    all_items = list(array.reshape((size, )))
+
+    indices = np.concatenate(tuple(item.indices for item in all_items))
+    data = np.concatenate(tuple([item.data for item in all_items]))
+    indices = indices[data != 0]
+    data = data[data != 0]
+
+    indptr = np.zeros(size+1)
+    for i in range(size):
+        indptr[1+i] = indptr[i] + all_items[i].count_nonzero()
+
+    return csr_matrix((data, indices, indptr), (size, all_items[0].shape[1]))
+
+
+def sv_to_csr(array):
+
+    size = array.size
+    all_items = list(array.reshape((size, )))
+
+    indices = np.concatenate(tuple(item.index for item in all_items))
+    data = np.concatenate(tuple([item.value for item in all_items]))
+    nonzero = (data != 0)
+    indices = indices[nonzero]
+    data = data[nonzero]
+
+    indptr = np.zeros(size+1)
+    for i in range(size):
+        zero_counts = all_items[i].value.count(0)
+        indptr[1+i] = indptr[i] + len(all_items[i].value) - zero_counts
+
+    return csr_matrix((data, indices, indptr), (size, all_items[0].nvar))
+
+
+def check_numeric(array):
+
+    array = np.array(array.todense()) if sp.issparse(array) else array
+    array = np.array([array]) if not isinstance(array, np.ndarray) else array
+
+    if isinstance(array, np.ndarray):
+        if not isinstance(array.flat[0], np.number):
+            raise TypeError('Incorrect data type of arrays.')
+    elif not isinstance(array, Real):
+        raise TypeError('Incorrect data type of arrays.')
+
+    return array
+
+
+def add_linear(left, right):
+
+    if left.shape[1] > right.shape[1]:
+        right.resize((right.shape[0], left.shape[1]))
+    elif right.shape[1] > left.shape[1]:
+        left.resize((left.shape[0], right.shape[1]))
+
+    return left + right
+
+
+def event_dict(event_set):
+
+    output = {}
+    count = 0
+    for item in event_set:
+        for element in item:
+            output[element] = count
+
+        count += 1
+
+    return output
+
+
+def comb_set(s1, s2):
+
+    d1 = event_dict(s1)
+    d2 = event_dict(s2)
+
+    dc = {item: str(d1[item]) + '-' + str(d2[item])
+          for item in range(len(d1))}
+
+    values = []
+    output = []
+    for key in dc:
+        if dc[key] in values:
+            index = values.index(dc[key])
+            output[index].append(key)
+        else:
+            output.append([key])
+            values.append(dc[key])
+
+    return output
+
+
+def norm(affine, degree=2):
+
+    return affine.norm(degree)
+
+
+def square(affine):
+
+    return affine.to_affine().square()
+
+
+def sumsqr(affine):
+
+    return affine.to_affine().sumsqr()
+
+
+def E(expr):
+
+    return expr.E
