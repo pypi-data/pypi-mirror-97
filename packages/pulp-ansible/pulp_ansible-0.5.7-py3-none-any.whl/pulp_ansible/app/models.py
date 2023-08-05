e9c1fc8cfc03b5290ac7c21098193ffa84221fbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:03 +0000

    Add package pulp-ansible/0.5.7/pulp_ansible-0.5.7-py3-none-any.whl

---
+++ packages/pulp-ansible/pulp_ansible-0.5.7-py3-none-any.whl/pulp_ansible/app/models.py
@@ -0,0 +1,290 @@
+from logging import getLogger
+
+from django.db import models
+from django.contrib.postgres import fields as psql_fields
+from django.contrib.postgres import search as psql_search
+from django.db.models import UniqueConstraint, Q
+
+from pulpcore.plugin.models import (
+    BaseModel,
+    Content,
+    Remote,
+    Repository,
+    RepositoryVersion,
+    RepositoryVersionDistribution,
+    Task,
+)
+from .downloaders import AnsibleDownloaderFactory
+
+log = getLogger(__name__)
+
+
+class Role(Content):
+    """
+    A content type representing a Role.
+    """
+
+    TYPE = "role"
+
+    namespace = models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
+    version = models.CharField(max_length=128)
+
+    @property
+    def relative_path(self):
+        """
+        Return the relative path of the ContentArtifact.
+        """
+        return self.contentartifact_set.get().relative_path
+
+    class Meta:
+        default_related_name = "%(app_label)s_%(model_name)s"
+        unique_together = ("version", "name", "namespace")
+
+
+class Collection(BaseModel):
+    """A model representing a Collection."""
+
+    namespace = models.CharField(max_length=64, editable=False)
+    name = models.CharField(max_length=64, editable=False)
+
+    class Meta:
+        unique_together = ("namespace", "name")
+
+
+class CollectionImport(models.Model):
+    """A model representing a collection import task details."""
+
+    task = models.OneToOneField(
+        Task, on_delete=models.CASCADE, editable=False, related_name="+", primary_key=True
+    )
+    messages = psql_fields.JSONField(default=list, editable=False)
+
+    class Meta:
+        ordering = ["task__pulp_created"]
+
+    def add_log_record(self, log_record):
+        """
+        Records a single log message but does not save the CollectionImport object.
+
+        Args:
+            log_record(logging.LogRecord): The logging record to record on messages.
+
+        """
+        self.messages.append(
+            {"message": log_record.msg, "level": log_record.levelname, "time": log_record.created}
+        )
+
+
+class Tag(BaseModel):
+    """A model representing a Tag.
+
+    Fields:
+
+        name (models.CharField): The Tag's name.
+    """
+
+    name = models.CharField(max_length=32, unique=True, editable=False)
+
+    def __str__(self):
+        """Returns tag name."""
+        return self.name
+
+
+class CollectionVersion(Content):
+    """
+    A content type representing a CollectionVersion.
+
+    This model is primarily designed to adhere to the data format for Collection content. That spec
+    is here: https://docs.ansible.com/ansible/devel/dev_guide/collections_galaxy_meta.html
+
+    Fields:
+
+        authors (psql_fields.ArrayField): A list of the CollectionVersion content's authors.
+        contents (psql_fields.JSONField): A JSON field with data about the contents.
+        dependencies (psql_fields.JSONField): A dict declaring Collections that this collection
+            requires to be installed for it to be usable.
+        description (models.TextField): A short summary description of the collection.
+        docs_blob (psql_fields.JSONField): A JSON field holding the various documentation blobs in
+            the collection.
+        documentation (models.CharField): The URL to any online docs.
+        homepage (models.CharField): The URL to the homepage of the collection/project.
+        issues (models.CharField): The URL to the collection issue tracker.
+        license (psql_fields.ArrayField): A list of licenses for content inside of a collection.
+        name (models.CharField): The name of the collection.
+        namespace (models.CharField): The namespace of the collection.
+        repository (models.CharField): The URL of the originating SCM repository.
+        version (models.CharField): The version of the collection.
+        is_highest (models.BooleanField): Indicates that the version is the highest one
+            in the collection.
+
+    Relations:
+
+        collection (models.ForeignKey): Reference to a collection model.
+        tag (models.ManyToManyField): A symmetric reference to the Tag objects.
+    """
+
+    TYPE = "collection_version"
+
+    # Data Fields
+    authors = psql_fields.ArrayField(models.CharField(max_length=64), default=list, editable=False)
+    contents = psql_fields.JSONField(default=list, editable=False)
+    dependencies = psql_fields.JSONField(default=dict, editable=False)
+    description = models.TextField(default="", blank=True, editable=False)
+    docs_blob = psql_fields.JSONField(default=dict, editable=False)
+    documentation = models.CharField(default="", blank=True, max_length=2000, editable=False)
+    homepage = models.CharField(default="", blank=True, max_length=2000, editable=False)
+    issues = models.CharField(default="", blank=True, max_length=2000, editable=False)
+    license = psql_fields.ArrayField(models.CharField(max_length=32), default=list, editable=False)
+    name = models.CharField(max_length=64, editable=False)
+    namespace = models.CharField(max_length=32, editable=False)
+    repository = models.CharField(default="", blank=True, max_length=2000, editable=False)
+    version = models.CharField(max_length=128, editable=False)
+
+    is_highest = models.BooleanField(editable=False, default=False)
+
+    # Foreign Key Fields
+    collection = models.ForeignKey(
+        Collection, on_delete=models.CASCADE, related_name="versions", editable=False
+    )
+    tags = models.ManyToManyField(Tag, editable=False)
+
+    # Search Fields
+    search_vector = psql_search.SearchVectorField(default="")
+
+    @property
+    def relative_path(self):
+        """
+        Return the relative path for the ContentArtifact.
+        """
+        return "{namespace}-{name}-{version}.tar.gz".format(
+            namespace=self.namespace, name=self.name, version=self.version
+        )
+
+    class Meta:
+        default_related_name = "%(app_label)s_%(model_name)s"
+        unique_together = ("namespace", "name", "version")
+        constraints = [
+            UniqueConstraint(
+                fields=("collection", "is_highest"),
+                name="unique_is_highest",
+                condition=Q(is_highest=True),
+            )
+        ]
+
+
+class RoleRemote(Remote):
+    """
+    A Remote for Ansible content.
+    """
+
+    TYPE = "role"
+
+    class Meta:
+        default_related_name = "%(app_label)s_%(model_name)s"
+
+
+class CollectionRemote(Remote):
+    """
+    A Remote for Collection content.
+    """
+
+    TYPE = "collection"
+
+    requirements_file = models.TextField(null=True)
+    auth_url = models.CharField(null=True, max_length=255)
+    token = models.TextField(null=True, max_length=2000)
+
+    @property
+    def download_factory(self):
+        """
+        Return the DownloaderFactory which can be used to generate asyncio capable downloaders.
+
+        Upon first access, the DownloaderFactory is instantiated and saved internally.
+
+        Plugin writers are expected to override when additional configuration of the
+        DownloaderFactory is needed.
+
+        Returns:
+            DownloadFactory: The instantiated DownloaderFactory to be used by
+                get_downloader()
+
+        """
+        try:
+            return self._download_factory
+        except AttributeError:
+            self._download_factory = AnsibleDownloaderFactory(self)
+            return self._download_factory
+
+    class Meta:
+        default_related_name = "%(app_label)s_%(model_name)s"
+
+
+class AnsibleRepository(Repository):
+    """
+    Repository for "ansible" content.
+    """
+
+    TYPE = "ansible"
+    CONTENT_TYPES = [Role, CollectionVersion]
+    REMOTE_TYPES = [RoleRemote, CollectionRemote]
+
+    class Meta:
+        default_related_name = "%(app_label)s_%(model_name)s"
+
+        permissions = (("modify_ansible_repo_content", "Can modify ansible repository content"),)
+
+    def finalize_new_version(self, new_version):
+        """
+        Finalize the incomplete RepositoryVersion with correct repository-level metadata.
+
+        `AnsibleCollectionDeprecated` objects may need to be created for the new
+        `RepositoryVersion`.
+
+        Args:
+            new_version (pulpcore.app.models.RepositoryVersion): The incomplete RepositoryVersion to
+                finalize.
+
+        """
+        deprecated_in_prev_version_qs = Collection.objects.filter(
+            ansiblecollectiondeprecated__repository_version=new_version.previous()
+        )
+        collection_versions_removed_pks = new_version.removed().values("pk")
+        deprecated_in_this_version = deprecated_in_prev_version_qs.exclude(
+            versions__in=collection_versions_removed_pks
+        )
+        to_deprecate = []
+        for collection_deprecated in deprecated_in_this_version:
+            to_deprecate.append(
+                AnsibleCollectionDeprecated(
+                    repository_version=new_version, collection=collection_deprecated
+                )
+            )
+
+        if to_deprecate:
+            AnsibleCollectionDeprecated.objects.bulk_create(to_deprecate, ignore_conflicts=True)
+
+
+class AnsibleCollectionDeprecated(BaseModel):
+    """
+    A model that represents if a Collection is `deprecated` for a given RepositoryVersion.
+    """
+
+    repository_version = models.ForeignKey(
+        RepositoryVersion, on_delete=models.CASCADE, related_name="collection_memberships"
+    )
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("collection", "repository_version")
+
+
+class AnsibleDistribution(RepositoryVersionDistribution):
+    """
+    A Distribution for Ansible content.
+    """
+
+    TYPE = "ansible"
+
+    class Meta:
+        default_related_name = "%(app_label)s_%(model_name)s"
---
+++ packages/pulp-ansible/pulp_ansible-0.5.7-py3-none-any.whl/pulp_ansible/app/tasks/copy.py
@@ -0,0 +1,77 @@
+from django.db import transaction
+from django.db.models import Q
+from pulpcore.plugin.models import RepositoryVersion
+
+from pulp_ansible.app.models import AnsibleCollectionDeprecated, AnsibleRepository, Collection
+
+
+@transaction.atomic
+def copy_content(config):
+    """
+    Copy content from one repo to another in a single transaction.
+
+    This should be an iterable of dictionaries, with each dictionary containing the following keys:
+
+        * source_repo_version: The RepositoryVersion pk
+        * dest_repo: The pk of the `AnsibleRepository` pk to copy content into. This repository will
+            receive the new RepositoryVersion with the copied content.
+        * `content`: An optional key that if specified should contain a list of pks to copy. These
+            specified keys must be members of the `source_repo_version`. If unspecified all content
+            from the `source_repo_version` will be copied to the `dest_repo`.
+
+    Args:
+        config (iterable): The config that identifies the `source_repo_version`, `dest_repo`, and
+            and `content`.
+
+    """
+
+    def process_entry(entry):
+        source_repo_version = RepositoryVersion.objects.get(pk=entry["source_repo_version"])
+        dest_repo = AnsibleRepository.objects.get(pk=entry["dest_repo"])
+
+        dest_version_provided = bool(entry.get("dest_base_version"))
+        if dest_version_provided:
+            dest_repo_version = RepositoryVersion.objects.get(pk=entry["dest_base_version"])
+        else:
+            dest_repo_version = dest_repo.latest_version()
+
+        if entry.get("content") is not None:
+            content_filter = Q(pk__in=entry.get("content"))
+        else:
+            content_filter = Q()
+
+        return (
+            source_repo_version,
+            dest_repo_version,
+            dest_repo,
+            content_filter,
+            dest_version_provided,
+        )
+
+    for entry in config:
+        (
+            source_repo_version,
+            dest_repo_version,
+            dest_repo,
+            content_filter,
+            dest_version_provided,
+        ) = process_entry(entry)
+
+        content_to_copy = source_repo_version.content.filter(content_filter)
+
+        base_version = dest_repo_version if dest_version_provided else None
+        with dest_repo.new_version(base_version=base_version) as new_version:
+            new_version.add_content(content_to_copy)
+            deprecated_in_source_repo_version_qs = Collection.objects.filter(
+                ansiblecollectiondeprecated__repository_version=source_repo_version
+            )
+            to_deprecate = []
+            for collection_deprecated in deprecated_in_source_repo_version_qs:
+                to_deprecate.append(
+                    AnsibleCollectionDeprecated(
+                        repository_version=new_version, collection=collection_deprecated
+                    )
+                )
+
+            if to_deprecate:
+                AnsibleCollectionDeprecated.objects.bulk_create(to_deprecate, ignore_conflicts=True)
