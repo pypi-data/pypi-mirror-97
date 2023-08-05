5f219567a8b462b8567d279d12aa5fe7a150545f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:44 +0000

    Add package aws-cdk.cloud-assembly-schema/2.0.0a6/aws-cdk.cloud-assembly-schema-2.0.0a6.tar.gz

---
+++ packages/aws-cdk.cloud-assembly-schema/aws-cdk.cloud-assembly-schema-2.0.0a6.tar.gz/aws-cdk.cloud-assembly-schema-2.0.0a6/src/aws_cdk/cloud_assembly_schema/_jsii/__init__.py
@@ -0,0 +1,22 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-cdk/cloud-assembly-schema",
+    "2.0.0-alpha.6",
+    __name__[0:-6],
+    "cloud-assembly-schema@2.0.0-alpha.6.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
