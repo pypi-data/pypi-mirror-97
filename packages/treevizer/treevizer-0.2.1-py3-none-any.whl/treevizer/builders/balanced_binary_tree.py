8af5f0afbc8380c6bbe426806e169ca0f109db81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:52 +0000

    Add package treevizer/0.2.1/treevizer-0.2.1-py3-none-any.whl

---
+++ packages/treevizer/treevizer-0.2.1-py3-none-any.whl/treevizer/builders/balanced_binary_tree.py
@@ -0,0 +1,66 @@
+"""
+Uses invisible nodes to balances trees in graph.
+Based on https://stackoverflow.com/a/48198645 to balance tree.
+"""
+from treevizer.builders.edge import Edge
+from treevizer.builders.base_graph import Graph
+
+class BalancedBinaryTree(Graph):
+    """
+    Builder for Balanced Binary Trees with igraph
+    """
+
+    def __init__(self, node):
+        # used to give unique id to invis nodes
+        self.invis_counter = 0
+        super().__init__(node)
+
+
+
+    def _add_node_to_graph(self, node):
+        """
+        Recutsivly add tree node to the graph.
+        Adds invisible nodes to balance vizualization of the tree.
+        """
+        key = node.key
+        # key = str(node.key)
+        # add node to graph
+        self._add_vertex(name=key, label=key)
+
+        if node.has_parent():
+            self._add_edge(key, node.parent.key, "parent")
+
+        if node.has_left_child():
+            self._add_node_to_graph(node.left)
+            self._add_edge(key, node.left.key, "left")
+        else:
+            self._add_invis_node(key)
+
+        # add middle invis node. Too push the real nodes left and right.
+        self._add_invis_node(key)
+
+        if node.has_right_child():
+            self._add_node_to_graph(node.right)
+            self._add_edge(key, node.right.key, "right")
+        else:
+            self._add_invis_node(key)
+
+
+
+    def _add_invis_node(self, key):
+        """
+        Add invis nodes to balance the tree.
+        Need to set a weight, otherwise it gets weird.
+        https://www.graphviz.org/doc/info/attrs.html#d:weight
+        Don't really understand what it does/how it works.
+        """
+        self.invis_counter += 1
+        inv_key = "i" + str(self.invis_counter)
+        self._add_vertex(inv_key, label=inv_key, style="invis")
+
+        self.edges.append(Edge(
+            self.vertexes[key].id,
+            self.vertexes[inv_key].id,
+            style="invis",
+            weight=5
+        ))
---
+++ packages/treevizer/treevizer-0.2.1-py3-none-any.whl/treevizer/exporters/dot.py
@@ -0,0 +1,54 @@
+"""
+Exports graph to dot file.
+"""
+from treevizer import config
+from treevizer.exporters import utils
+
+def to_dot(graph, filename="tree.dot"):
+    """
+    Use igraph to write dot file and insert global graphviz settings in file.
+    """
+    filename = utils.get_abspath(filename)
+    string = build_string(graph)
+    write_to_file(string, filename)
+
+
+
+def write_to_file(content, filename):
+    """
+    Write string to file
+    """
+    with open(filename, "w") as fd:
+        fd.write(content)
+
+
+
+def create_config_str(graph_type):
+    """
+    Create config string for dot file graph.
+    """
+    dot_config = config.get_config(graph_type)
+
+    config_str = ""
+    for graph_part, values in dot_config.items():
+        options = [f"  {key}={value}" for key, value in values.items()]
+        config_str += graph_part + " [\n" + ",\n".join(options) + "\n];\n"
+    return config_str
+
+
+
+def build_string(graph):
+    """
+    Create string with dot code from graph
+    """
+    graph_settings = create_config_str(graph.__class__.__name__)
+    nodes = "\n".join([repr(vertex) for vertex in graph.vertexes.values()])
+    edges = "\n".join([repr(edge) for edge in graph.edges])
+
+    dot_string = "{graph_type} {{\n{config}\n{nodes}\n{edges}\n}}".format(
+        graph_type=graph.graph_type(),
+        config=graph_settings,
+        nodes=nodes,
+        edges=edges,
+    )
+    return dot_string
