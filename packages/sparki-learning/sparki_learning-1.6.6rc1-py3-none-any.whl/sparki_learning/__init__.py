32d7a03bd17c393c61be388d818f3e3bb08e1b87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:16 +0000

    Add package sparki-learning/1.6.6rc1/sparki_learning-1.6.6rc1-py3-none-any.whl

---
+++ packages/sparki-learning/sparki_learning-1.6.6rc1-py3-none-any.whl/sparki_learning/__init__.py
@@ -0,0 +1,32 @@
+"""
+    This library implements the Python part of a system for using Python with
+    the Sparki robot (<http://arcbotics.com/products/sparki/>).
+
+    For this system to be of any use, you will need to install the other half
+    of the library onto your Sparki. Directions can be found at the homepage
+    at <https://github.com/radarjd/sparki_learning>
+
+    The Python part of the library implements much of the IPRE (Institute for
+    Personal Robotics in Education <http://www.roboteducation.org/>) Myro API
+    (API reference at <http://calicoproject.org/Calico_Myro>). This API was
+    originally chosen because the original author of this project taught (or
+    teaches, depending on when you read this) a class making use of that
+    program. A free textbook exists to teach that program may be found at
+    <http://calicoproject.org/Learning_Computing_With_Robots_Using_Calico_Python>.
+    The Python library here does not implement all of that library -- in
+    particular, anything having to do with the camera cannot be implemented
+    on Sparki because Sparki does not have that hardware.
+    
+    Command documentation on this library may be found at
+    <https://sparki-learning.readthedocs.io/en/latest/>
+"""
+
+from sparki_learning.constants import *
+from sparki_learning.gui import *
+from sparki_learning.sparki_myro import *
+from sparki_learning.speak import speak
+from sparki_learning.sync_lib import get_client_start, start_sync_server, start_sync_client
+from sparki_learning.util import *
+
+import sparki_learning.constants
+import sparki_learning.sparki_myro
