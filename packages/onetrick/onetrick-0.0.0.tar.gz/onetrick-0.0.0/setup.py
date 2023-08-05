e545e00e42c3933eb0e21078b663076d899de2dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:28 +0000

    Add package onetrick/0.0.0/onetrick-0.0.0.tar.gz

---
+++ packages/onetrick/onetrick-0.0.0.tar.gz/onetrick-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='onetrick',
+    version='0.0.0',
+    author='Perzan',
+    packages=["onetrick"],
+    author_email='PerzanDevelopment@gmail.com'
+)
