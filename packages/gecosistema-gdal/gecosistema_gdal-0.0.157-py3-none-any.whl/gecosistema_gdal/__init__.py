ea6a601146ea20719d1298479244c96ea227f19c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:41 +0000

    Add package gecosistema-gdal/0.0.157/gecosistema_gdal-0.0.157-py3-none-any.whl

---
+++ packages/gecosistema-gdal/gecosistema_gdal-0.0.157-py3-none-any.whl/gecosistema_gdal/__init__.py
@@ -0,0 +1,31 @@
+# -------------------------------------------------------------------------------
+# Licence:
+# Copyright (c) 2012-2018 Luzzi Valerio
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+#
+# Name:        __init__.py
+# Purpose:
+#
+# Author:      Luzzi Valerio
+#
+# Created:     31/08/2018
+# -------------------------------------------------------------------------------
+__version__ = '0.0.157'
+
+from .ogr_utils import *
+from .gdal_utils import *
+from .gdal_numpy import *
+from .gdal_sparse import *
+from .taudem import *
