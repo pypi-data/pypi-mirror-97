8a9c7bd3847735d40073f56c5dd3b1abce5b91bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:27 +0000

    Add package matrixdifferential/0.0.10/matrixdifferential-0.0.10.tar.gz

---
+++ packages/matrixdifferential/matrixdifferential-0.0.10.tar.gz/matrixdifferential-0.0.10/setup.py
@@ -0,0 +1,28 @@
+'''
+@File       :   setup.py
+@Author     :   Yiteng Zhang
+@Time       :   2020-10
+@Version    :   1.0
+@Contact    :   zytfdu@icloud.com
+@Dect       :   None
+'''
+ 
+from setuptools import setup, find_packages
+ 
+setup(
+    name = "matrixdifferential",
+    version = "0.0.10",
+    keywords = ("pip", "pyctlib"),
+    description = "This is a mathematic calculation package for computing matrix differential",
+    long_description = "This is a mathematic calculation package for computing matrix differential",
+    license = "MIT Licence",
+ 
+    url = "https://github.com/Bertie97/pyctlib/tree/main/pyctlib",
+    author = "All contributors of PyCTLib",
+    author_email = "zytfdu@icloud.com",
+ 
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["numpy"]
+)
