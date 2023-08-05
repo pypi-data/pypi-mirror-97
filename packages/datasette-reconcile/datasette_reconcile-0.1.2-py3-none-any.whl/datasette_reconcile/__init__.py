00f42fe56e09728666f4c407c3c5dabec09956f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:35 +0000

    Add package datasette-reconcile/0.1.2/datasette_reconcile-0.1.2-py3-none-any.whl

---
+++ packages/datasette-reconcile/datasette_reconcile-0.1.2-py3-none-any.whl/datasette_reconcile/__init__.py
@@ -0,0 +1,58 @@
+import html
+import json
+import sqlite3
+import warnings
+
+from datasette import hookimpl
+from datasette.utils.asgi import Response
+
+from datasette_reconcile.reconcile import reconcile_queries, service_manifest
+from datasette_reconcile.utils import check_config, check_permissions
+
+
+async def reconcile(request, datasette):
+    database = request.url_vars["db_name"]
+    table = request.url_vars["db_table"]
+    db = datasette.get_database(database)
+
+    # get plugin configuration
+    config = datasette.plugin_config(
+        "datasette-reconcile", database=database, table=table
+    )
+    config = await check_config(config, db, table)
+
+    # check user can at least view this table
+    await check_permissions(
+        request,
+        [
+            ("view-table", (database, table)),
+            ("view-database", database),
+            "view-instance",
+        ],
+        datasette,
+    )
+
+    # work out if we are looking for queries
+    post_vars = await request.post_vars()
+    queries = post_vars.get("queries", request.args.get("queries"))
+    if queries:
+        queries = json.loads(queries)
+        return Response.json(
+            {q[0]: q[1] async for q in reconcile_queries(queries, config, db, table)},
+            headers={
+                "Access-Control-Allow-Origin": "*",
+            },
+        )
+
+    # if we're not then just return the service specification
+    return Response.json(
+        service_manifest(config, database, table, datasette),
+        headers={
+            "Access-Control-Allow-Origin": "*",
+        },
+    )
+
+
+@hookimpl
+def register_routes():
+    return [(r"/(?P<db_name>[^/]+)/(?P<db_table>[^/]+?)/-/reconcile$", reconcile)]
