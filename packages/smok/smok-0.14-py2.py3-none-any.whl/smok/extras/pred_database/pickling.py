d0613bbf25274a8b14f481b36e32c8c7f7e48768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:29 +0000

    Add package smok/0.14/smok-0.14-py2.py3-none-any.whl

---
+++ packages/smok/smok-0.14-py2.py3-none-any.whl/smok/extras/pred_database/pickling.py
@@ -0,0 +1,25 @@
+import typing as tp
+import os
+import pickle
+
+from .base import BasePredicateDatabase
+
+
+class PicklingPredicateDatabase(BasePredicateDatabase):
+    def __init__(self, path: str):
+        self.__path = path
+        self.__predicates = []
+        if os.path.exists(self.__path):
+            with open(self.__path, 'rb') as f_in:
+                self.__predicates = pickle.load(f_in)
+
+    def __sync(self):
+        with open(self.__path, 'wb') as f_out:
+            pickle.dump(self.__predicates, f_out, pickle.HIGHEST_PROTOCOL)
+
+    def get_all_predicates(self) -> tp.List[tp.Dict]:
+        return self.__predicates
+
+    def set_new_predicates(self, v: tp.List[tp.Dict]):
+        self.__predicates = v
+        self.__sync()
