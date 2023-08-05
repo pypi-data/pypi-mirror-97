3d61e9f9005f2c2baf489816126d4d27d26a200f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:13 +0000

    Add package docstr-coverage/2.0.1/docstr_coverage-2.0.1.tar.gz

---
+++ packages/docstr-coverage/docstr_coverage-2.0.1.tar.gz/docstr_coverage-2.0.1/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+MAJOR, MINOR, MICRO = 2, 0, 1
+__VERSION__ = "{}.{}.{}".format(MAJOR, MINOR, MICRO)
+
+setup(
+    name="docstr_coverage",
+    version=__VERSION__,
+    description=(
+        "Utility for examining python source files to ensure proper documentation. "
+        "Lists missing docstrings, and calculates overall docstring coverage "
+        "percentage rating."
+    ),
+    long_description_content_type="text/markdown",
+    long_description=readme(),
+    keywords="docstring coverage documentation audit source code statistics report",
+    url="https://github.com/HunterMcGushion/docstr_coverage",
+    author="Hunter McGushion",
+    author_email="hunter@mcgushion.com",
+    license="MIT",
+    packages=["docstr_coverage"],
+    install_requires=["click", "PyYAML"],
+    extras_require={
+        "lint": ["flake8==3.8.2", "black==19.10b0", "isort==5.6.4"],
+        "test": ["pytest==5.4.2", "pytest-mock==3.4.0"],
+    },
+    include_package_data=True,
+    zip_safe=False,
+    entry_points=dict(console_scripts=["docstr-coverage=docstr_coverage.cli:execute"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Documentation",
+        "Topic :: Documentation :: Sphinx",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Utilities",
+    ],
+)
