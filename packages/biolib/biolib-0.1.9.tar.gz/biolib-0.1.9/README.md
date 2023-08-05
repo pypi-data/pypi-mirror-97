34408909d2d2720fa686caa2a856bf1bf8b3c289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:13 +0000

    Add package biolib/0.1.9/biolib-0.1.9.tar.gz

---
+++ packages/biolib/biolib-0.1.9.tar.gz/biolib-0.1.9/README.md
@@ -0,0 +1,28 @@
+# biolib
+
+<b>[This library is in active development and not currently recommended for public use.]</b>
+
+[![version status](https://img.shields.io/pypi/v/biolib.svg)](https://pypi.python.org/pypi/biolib)
+[![Bioconda](https://img.shields.io/conda/vn/bioconda/biolib.svg?color=43b02a)](https://anaconda.org/bioconda/biolib)
+
+Biolib is a python package for common tasks in bioinformatics.
+
+## Announcements
+
+* Ported to Python 3 starting with version 0.1.0
+
+## Install
+
+The simplest way to install this package is through pip:
+
+```shell
+python -m pip install biolib
+```
+
+## Cite
+
+If you find this package useful, please cite this git repository https://github.com/dparks1134/biolib.
+
+## Copyright
+
+Copyright © 2015 Donovan Parks. See LICENSE for further details.
---
+++ packages/biolib/biolib-0.1.9.tar.gz/biolib-0.1.9/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+import os
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+def version():
+    setupDir = os.path.dirname(os.path.realpath(__file__))
+    versionFile = open(os.path.join(setupDir, 'biolib', 'VERSION'))
+    return versionFile.readline().strip()
+
+
+setup(
+    name='biolib',
+    version=version(),
+    author='Donovan Parks',
+    author_email='donovan.parks@gmail.com',
+    packages=['biolib', 'biolib.misc', 'biolib.external', 'biolib.plots'],
+    package_data={'biolib': ['VERSION']},
+    url='http://pypi.python.org/pypi/biolib/',
+    license='GPL3',
+    description='Package for common tasks in bioinformatics.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+    install_requires=['numpy', 'matplotlib', 'scipy'],
+)
