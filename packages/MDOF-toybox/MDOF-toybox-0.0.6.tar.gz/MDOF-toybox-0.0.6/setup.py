6dff42287b15ec88b678ecf969a16ad4c7be5a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:13 +0000

    Add package MDOF-toybox/0.0.6/MDOF-toybox-0.0.6.tar.gz

---
+++ packages/MDOF-toybox/MDOF-toybox-0.0.6.tar.gz/MDOF-toybox-0.0.6/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+# The text of the README file
+with open('README.md') as f:
+    rm = f.read()
+
+# This call to setup() does all the work
+setup(
+    name="MDOF-toybox",
+    version="0.0.6",
+    description="Tooling to generate toy MDOF dynamics data for arbitrary linear and nonlinear systems in python",
+    long_description=rm,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MDCHAMP/Toybox",
+    author="Max Champneys",
+    author_email="max.champneys@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=['toybox'],
+    package_dir={'':'src'},
+    include_package_data=False,
+    install_requires=[
+        "numpy"
+    ],
+)
