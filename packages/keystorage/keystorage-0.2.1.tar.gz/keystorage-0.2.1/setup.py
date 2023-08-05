8bf46d40f1e9f78394a4179136b95a07d6f957ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:36 +0000

    Add package keystorage/0.2.1/keystorage-0.2.1.tar.gz

---
+++ packages/keystorage/keystorage-0.2.1.tar.gz/keystorage-0.2.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="keystorage",
+    version="0.2.1",
+    package=find_packages(),
+    include_packages_data=True,
+    install_requires=[
+        "click", "pycrypto"
+    ],
+    entry_points="""
+        [console_scripts]
+        keys=keystorage.scripts:cli
+    """
+)
