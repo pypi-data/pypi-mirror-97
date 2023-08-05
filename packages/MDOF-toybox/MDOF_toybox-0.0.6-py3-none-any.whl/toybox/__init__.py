0c05f893ff212b4104ce46ac21c51ac26c926799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:13 +0000

    Add package MDOF-toybox/0.0.6/MDOF_toybox-0.0.6-py3-none-any.whl

---
+++ packages/MDOF-toybox/MDOF_toybox-0.0.6-py3-none-any.whl/toybox/__init__.py
@@ -0,0 +1,9 @@
+'''
+Init.py
+'''
+__version__ = '0.0.6'
+
+from toybox.premade import *
+import toybox.integrators as integrators
+import toybox.nonlinearities as nonlinearities
+import toybox.forcings as forcings
---
+++ packages/MDOF-toybox/MDOF_toybox-0.0.6-py3-none-any.whl/toybox/forcings.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+# %% Excitations
+
+class excitation():
+    pass
+
+
+class white_gaussian(excitation):
+
+    def __init__(self, u, sig):
+        self.u = u
+        self.sig = sig
+
+    def generate(self, n):
+        return np.random.normal(self.u, self.sig, size=(n, 1))
+
+
+class sinusoid(excitation):
+    pass
+
+
+class impact(excitation):
+    pass
+
+
+# %% MDOF multi-DOF excitations 
+
+class shaker():
+
+    def __init__(self, excitations):
+        self.excitations = excitations
+    
+    def generate(self, n):
+        out = np.zeros((len(self.excitations), n))
+        for i, e in enumerate(self.excitations):
+            if isinstance(e, excitation):
+                out[i,:] = np.squeeze(e.generate(n))
+            else: 
+                out[i,:] = np.zeros(n)
+        return out.T
+
