f0e3a98c841e089dba1f195fd9fc722b14337892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:22 +0000

    Add package tethys-wrf/0.0.4/tethys_wrf-0.0.4-py2.py3-none-any.whl

---
+++ packages/tethys-wrf/tethys_wrf-0.0.4-py2.py3-none-any.whl/tethys_wrf/__init__.py
@@ -0,0 +1,19 @@
+from tethys_wrf.main import *
+from functools import wraps
+
+# Default proj
+wgs84 = pyproj.Proj(proj='latlong', datum='WGS84')
+
+def lazy_property(fn):
+    """Decorator that makes a property lazy-evaluated."""
+
+    attr_name = '_lazy_' + fn.__name__
+
+    @property
+    @wraps(fn)
+    def _lazy_property(self):
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, fn(self))
+        return getattr(self, attr_name)
+
+    return _lazy_property
