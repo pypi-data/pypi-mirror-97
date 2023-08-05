9524e222008f1357a8241651083e19979672c15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:00 +0000

    Add package opentracing-utils/0.20.1/opentracing-utils-0.20.1.tar.gz

---
+++ packages/opentracing-utils/opentracing-utils-0.20.1.tar.gz/opentracing-utils-0.20.1/opentracing_utils/tracers.py
@@ -0,0 +1,72 @@
+import os
+import logging
+
+import opentracing
+
+OPENTRACING_INSTANA = 'instana'
+OPENTRACING_LIGHTSTEP = 'lightstep'
+OPENTRACING_JAEGER = 'jaeger'
+OPENTRACING_BASIC = 'basic'
+
+
+logger = logging.getLogger(__name__)
+
+
+def init_opentracing_tracer(tracer, **kwargs):
+    if tracer == OPENTRACING_BASIC:
+        from basictracer import BasicTracer  # noqa
+
+        recorder = kwargs.get('recorder')
+        sampler = kwargs.get('sampler')
+
+        opentracing.tracer = BasicTracer(recorder=recorder, sampler=sampler)
+    elif tracer == OPENTRACING_INSTANA:
+        import instana  # noqa
+    elif tracer == OPENTRACING_LIGHTSTEP:
+        import lightstep
+
+        # Get Lightstep specific config vars
+        component_name = kwargs.pop(
+            'component_name',
+            os.environ.get('OPENTRACING_LIGHTSTEP_COMPONENT_NAME'))
+        access_token = kwargs.pop(
+            'access_token',
+            os.environ.get('OPENTRACING_LIGHTSTEP_ACCESS_TOKEN'))
+        collector_host = kwargs.pop(
+            'collector_host',
+            os.environ.get('OPENTRACING_LIGHTSTEP_COLLECTOR_HOST', 'collector.lightstep.com'))
+        collector_port = kwargs.pop(
+            'collector_port',
+            int(os.environ.get('OPENTRACING_LIGHTSTEP_COLLECTOR_PORT', 443)))
+        verbosity = kwargs.pop(
+            'verbosity',
+            int(os.environ.get('OPENTRACING_LIGHTSTEP_VERBOSITY', 0)))
+        scheme = kwargs.pop(
+            'collector_scheme',
+            os.environ.get('OPENTRACING_LIGHTSTEP_COLLECTOR_SCHEME', 'https'))
+
+        if not access_token:
+            logger.warning('Initializing LightStep tracer with no access_token!')
+
+        collector_encryption = "tls"
+        if scheme == "http":
+            collector_encryption = "none"
+        if 'collector_encryption' in kwargs:
+            collector_encryption = kwargs.pop('collector_encryption', collector_encryption)
+
+        opentracing.tracer = lightstep.Tracer(
+            component_name=component_name, access_token=access_token, collector_host=collector_host,
+            collector_port=collector_port, collector_encryption=collector_encryption, verbosity=verbosity,
+            **kwargs)
+    elif tracer == OPENTRACING_JAEGER:
+        from jaeger_client import Config
+
+        service_name = kwargs.pop('service_name', os.environ.get('OPENTRACING_JAEGER_SERVICE_NAME'))
+        config = kwargs.pop('config', {})
+
+        jaeger_config = Config(config=config, service_name=service_name)
+        opentracing.tracer = jaeger_config.initialize_tracer()
+    else:
+        opentracing.tracer = opentracing.Tracer()
+
+    return opentracing.tracer
---
+++ packages/opentracing-utils/opentracing-utils-0.20.1.tar.gz/opentracing-utils-0.20.1/setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Setup file for opentracing-utils.
+"""
+import io
+import sys
+from setuptools import setup, find_packages
+
+PY3 = sys.version_info.major == 3
+
+MAIN_PACKAGE = 'opentracing_utils'
+VERSION = '0.20.1'
+DESCRIPTION = 'OpenTracing utilities library'
+
+
+def load_req(req):
+    try:
+        return [r.strip() for r in open(req).read().splitlines() if r.strip() and not r.strip().startswith('#')]
+    except Exception:
+        return []
+
+
+TEST_REQUIREMENTS = load_req('test_requirements.txt')
+
+
+if __name__ == '__main__':
+    setup(
+        name='opentracing-utils',
+        version=VERSION,
+        description=DESCRIPTION,
+        long_description=io.open('README.rst', encoding='utf-8').read(),
+        license='The MIT License (MIT)',
+        url='https://github.com/zalando-zmon/opentracing-utils',
+        packages=find_packages(exclude=['tests']),
+        install_requires=[
+            'future',
+            'opentracing',
+        ],
+        setup_requires=[
+            'opentracing',
+            'pytest-runner',
+        ],
+        test_suite='tests',
+        tests_require=TEST_REQUIREMENTS,
+        include_package_data=True,
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: Implementation :: CPython',
+            'Environment :: Console',
+            'Operating System :: POSIX :: Linux',
+            'Operating System :: MacOS :: MacOS X',
+            'Topic :: Utilities',
+            'Topic :: System :: Monitoring',
+            'Topic :: System :: Networking :: Monitoring',
+        ]
+    )
