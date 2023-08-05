669a01e5140aaf0220ccc3f4ecf37f8b817b6aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:11 +0000

    Add package longling/1.3.19/longling-1.3.19.tar.gz

---
+++ packages/longling/longling-1.3.19.tar.gz/longling-1.3.19/longling/ML/DL/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+# create by tongshiwei on 2019-9-4
+
+from .ecli import cli as ecli
+from .module import Module
+from .optimizer_cfg import get_optimizer_cfg
+from .service import CliServiceModule, service_wrapper
+from .light_module import train
