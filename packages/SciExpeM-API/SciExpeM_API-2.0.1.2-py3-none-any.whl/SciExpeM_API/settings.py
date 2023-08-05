f6b46793aa703da57506d0469e10eadfae49c0eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:06 +0000

    Add package SciExpeM-API/2.0.1.2/SciExpeM_API-2.0.1.2-py3-none-any.whl

---
+++ packages/SciExpeM-API/SciExpeM_API-2.0.1.2-py3-none-any.whl/SciExpeM_API/settings.py
@@ -0,0 +1,10 @@
+# IP = "127.0.0.1"
+# PORT = 8080
+# TOKEN = "f8fa855a5ff8ac3653528a989afdb6ea1d5096eb"  # LOCAL
+# HTTPS = False
+
+IP = "sciexpem.chem.polimi.it"
+PORT = 443
+TOKEN = "2cc7264bc0a86d2aef9a186171cf9627772af41d"  # OS
+HTTPS = True
+
