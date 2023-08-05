c23b3fc97f17e62806c1c90a08cee08ab59ebd9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:47 +0000

    Add package nestpy/1.4.7/nestpy-1.4.7-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/nestpy/nestpy-1.4.7-cp36-cp36m-manylinux1_x86_64.whl/nestpy/__init__.py
@@ -0,0 +1,10 @@
+__version__ = '1.4.7'
+__nest_version__ = '2.2.1'
+
+from .nestpy import *
+
+from .helpers import Yield, PhotonYield, ElectronYield, GetYieldsVectorized, GetInteractionObject, ListInteractionTypes
+
+# Populate namespace with interaction types to allow e.g. nestpy.NR
+for interaction_type in ListInteractionTypes(): # interaction_type is string
+    vars()[interaction_type] = GetInteractionObject(interaction_type)
