b661f155bd1f916b91154111bd4eceffc12d7689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:13 +0000

    Add package docstr-coverage/2.0.1/docstr_coverage-2.0.1-py3-none-any.whl

---
+++ packages/docstr-coverage/docstr_coverage-2.0.1-py3-none-any.whl/docstr_coverage/cli.py
@@ -0,0 +1,344 @@
+"""This module is the CLI entry point for `docstr_coverage` in which CLI arguments are defined and
+passed on to other modules"""
+import os
+import platform
+import re
+import sys
+import warnings
+from typing import List, Optional
+
+import click
+
+from docstr_coverage.badge import Badge
+from docstr_coverage.config_file import set_config_defaults
+from docstr_coverage.coverage import get_docstring_coverage
+
+
+def do_include_filepath(filepath: str, exclude_re: Optional["re.Pattern"]) -> bool:
+    """Determine whether `filepath` should be included in docstring search.
+    Note on regex matching:
+    On windows we check against unix and windows regex matches (if one of the two matches)
+    Hence we have a one-way compatibility (regex for unix paths work in win as well).
+    Two way compatibility (matching win-path regexes on linux) is not possible as a single backslash
+    is a valid character in a unix path.
+
+    Parameters
+    ----------
+    filepath: String
+        Filepath to match with `exclude_re`. If extension is not ".py", it will be excluded
+    exclude_re: re.Pattern, or None
+        Pattern for files to be excluded. If None, `exclude_re` is ignored
+
+    Returns
+    -------
+    Boolean
+        True if `filepath` should be searched, else False"""
+    if not filepath.endswith(".py"):
+        return False
+    if exclude_re is not None:
+        if exclude_re.match(filepath):
+            return False
+        if platform.system() == "Windows":
+            return not exclude_re.match(filepath.replace("\\", "/"))
+    return True
+
+
+def collect_filepaths(
+    *paths: str, follow_links: bool = False, exclude: Optional[str] = None
+) -> List[str]:
+    """Collect filepaths under given `paths` that are not `exclude`-d
+
+    Parameters
+    ----------
+    *paths: String
+        Path(s) to a directory/file from which filepaths will be collected
+    follow_links: Boolean, default=False
+        Whether to follow symbolic links when traversing directories in `paths`
+    exclude: String (optional)
+        If not None, used as a regex Pattern to exclude filepaths during collection. If a full
+        filepath matches the `exclude` pattern, it is skipped
+
+    Returns
+    -------
+    List
+        List of string filepaths found under `paths` that are not excluded. If `paths` is a single
+        ".py" file, result will be [`paths`]. Otherwise, the contents of `paths` that are not
+        `exclude`-d will comprise the result"""
+    exclude_re = re.compile(r"{}".format(exclude)) if exclude else None
+    filepaths = []
+
+    for path in paths:
+        if path.endswith(".py"):
+            filepaths.append(path)
+        else:
+            for (dirpath, dirnames, filenames) in os.walk(path, followlinks=follow_links):
+                candidates = [os.path.join(dirpath, _) for _ in filenames]
+                filepaths.extend([_ for _ in candidates if do_include_filepath(_, exclude_re)])
+
+    return sorted(filepaths)
+
+
+def parse_ignore_names_file(ignore_names_file: str) -> tuple:
+    """Parse a file containing patterns of names to ignore
+
+    Parameters
+    ----------
+    ignore_names_file: String
+        Path to a file containing name patterns
+
+    Returns
+    -------
+    Tuple
+        Tuple containing one list for each valid line in `ignore_names_file`. Each list contains
+        the space-delimited contents of that line in the file, in which the first value is a file
+        pattern, and all other values are name patterns"""
+    if not os.path.isfile(ignore_names_file):
+        return ()
+
+    with open(ignore_names_file, "r") as f:
+        ignore_names = tuple([line.split() for line in f.readlines() if " " in line])
+
+    return ignore_names
+
+
+def parse_ignore_patterns_from_dict(ignore_patterns_dict) -> tuple:
+    """Parse dictionary containing (file_name_pattern, exclude_patterns) key value pairs
+    to return an output consistent with ignore patterns parsed by `parse_ignore_names_file`
+
+    Parameters
+    ----------
+    ignore_patterns_dict: Dict
+        A dict where each key is a string and each value is a string or a nonempty list of strings.
+
+    Returns
+    -------
+    Tuple
+        Tuple of iterables of string with the same form as the output of `parse_ignore_names_file`
+
+    Notes
+    -----
+    To align the workflow with `parse_ignore_names_file`, we check that the passed values
+    are of type string, but we do not yet check if they are valid regular expressions"""
+
+    def _assert_valid_key_value(k, v):
+        if not isinstance(k, str):
+            raise TypeError("ignore patterns in config contained non-string key {}".format(k))
+        if len(k.strip()) == 0:
+            raise ValueError("ignore pattern in config contained empty (file name) regex")
+        if not all(isinstance(v, str) for v in v) and len(v) > 0:
+            raise TypeError(
+                "ignore patterns for key {} contained non-string values or was empty.".format(k)
+            )
+        if not all(len(v.strip()) > 0 for v in v):
+            raise ValueError("ignore pattern for key {} contained empty regex".format(k))
+
+    if not isinstance(ignore_patterns_dict, dict):
+        raise TypeError(
+            "ignore patterns in config must have type Dict[str, Union[str, List[str]]],"
+            "but was {}".format(type(ignore_patterns_dict))
+        )
+
+    result_list = []
+    for key, value in ignore_patterns_dict.items():
+        res = [key]
+        if not isinstance(value, list):
+            value = [value]
+        _assert_valid_key_value(key, value)
+        res += value
+        result_list.append(res)
+
+    return tuple(result_list)
+
+
+@click.command()
+@click.option(
+    # TODO: Use counting instead: https://click.palletsprojects.com/en/7.x/options/#counting
+    "-v",
+    "--verbose",
+    type=click.Choice(["0", "1", "2", "3"]),
+    default="3",
+    help="Verbosity level",
+    show_default=True,
+    callback=lambda _ctx, _param, value: int(value),
+)
+@click.option(
+    "-e",
+    "--exclude",
+    type=str,
+    default=None,
+    help="Regex identifying filepaths to exclude",
+    show_default=False,
+    # TODO: Add support for multiple `--exclude` regex invocations
+)
+@click.option(
+    "-m",
+    "--skip-magic",
+    is_flag=True,
+    help="Ignore docstrings of magic methods (except `__init__`)",
+)
+@click.option("-f", "--skip-file-doc", is_flag=True, help="Ignore module docstrings")
+@click.option("-i", "--skip-init", is_flag=True, help="Ignore docstrings of `__init__` methods")
+@click.option("-c", "--skip-class-def", is_flag=True, help="Ignore docstrings of class definitions")
+@click.option(
+    "-P",
+    "--skip-private",
+    is_flag=True,
+    help="Ignore docstrings of functions starting with a single underscore",
+)
+@click.option("-l", "--follow-links", is_flag=True, help="Follow symlinks")
+@click.option(
+    "-d",
+    "--docstr-ignore-file",
+    "ignore_names_file",  # TODO: Remove after deprecating in favor of pyproject.toml `blacklist`
+    type=click.Path(exists=False, resolve_path=True),
+    default=".docstr_coverage",
+    help="Filepath containing list of regex (file-pattern, name-pattern) pairs",
+    show_default=True,
+)
+@click.option(
+    "-F",
+    "--fail-under",
+    type=float,
+    default=100.0,
+    help="Fail when coverage % is less than a given amount",
+    show_default=True,
+    metavar="NUMBER",
+)
+@click.option(
+    "-b",
+    "--badge",
+    type=click.Path(exists=False, resolve_path=True),
+    default=None,
+    help="Generate a docstring coverage percent badge as an SVG saved to a given filepath",
+    show_default=False,
+)
+@click.option(
+    "-p",
+    "--percentage-only",
+    is_flag=True,
+    help="Output only the overall coverage percentage as a float, silencing all other logging",
+)
+@click.option(
+    "-a",
+    "--accept-empty",
+    is_flag=True,
+    help="Exit with code 0 if no Python files are found (default: exit code 1)",
+)
+@click.help_option("-h", "--help")
+@click.argument(
+    "paths",
+    type=click.Path(exists=True, file_okay=True, dir_okay=True, readable=True, resolve_path=True),
+    nargs=-1,
+    is_eager=True,  # Eagerly execute before `config` so `set_config_defaults` has `paths`
+)
+@click.option(
+    "-C",
+    "--config",
+    type=click.Path(exists=False, resolve_path=True),
+    default=".docstr.yaml",
+    help="Configuration file containing option defaults",
+    is_eager=True,
+    callback=set_config_defaults,
+)
+@click.option("--skipmagic", is_flag=True, help="Deprecated. Use --skip-magic")
+@click.option("--skipfiledoc", is_flag=True, help="Deprecated. Use --skip-file-doc")
+@click.option("--skipinit", is_flag=True, help="Deprecated. Use --skip-init")
+@click.option("--skipclassdef", is_flag=True, help="Deprecated. Use --skip-class-def")
+@click.option("--followlinks", is_flag=True, help="Deprecated. Use --follow-links")
+@click.option("--failunder", type=float, help="Deprecated. Use --fail-under")
+def execute(paths, **kwargs):
+    """Measure docstring coverage for `PATHS`"""
+    for deprecated_name, name in [
+        ("skipmagic", "skip_magic"),
+        ("skipfiledoc", "skip_file_doc"),
+        ("skipinit", "skip_init"),
+        ("skipclassdef", "skip_class_def"),
+        ("followlinks", "follow_links"),
+    ]:
+        if kwargs.get(deprecated_name):
+            new_flag = name.replace("_", "-")
+            if kwargs.get(name):
+                raise ValueError(
+                    "Should not set deprecated --{} and new --{}".format(deprecated_name, new_flag)
+                )
+            click.secho(
+                "Using deprecated --{}, should use --{}".format(deprecated_name, new_flag), fg="red"
+            )
+            kwargs[name] = kwargs.pop(deprecated_name)
+
+    # handle fail under
+    if kwargs.get("failunder") is not None:
+        if kwargs.get("fail_under") != 100.0:
+            raise ValueError("Should not set deprecated --failunder and --fail-under")
+        click.secho("Using deprecated --failunder, should use --fail-under", fg="red")
+        kwargs["fail_under"] = kwargs.pop("failunder")
+
+    # TODO: Add option to generate pretty coverage reports - Like Python's test `coverage`
+    # TODO: Add option to sort reports by filename, coverage score... (ascending/descending)
+    if kwargs["percentage_only"] is True:
+        # Override verbosity to ensure only the overall percentage is printed
+        kwargs["verbose"] = 0
+
+    all_paths = collect_filepaths(
+        *paths, follow_links=kwargs["follow_links"], exclude=kwargs["exclude"]
+    )
+
+    if len(all_paths) < 1:
+        if kwargs["accept_empty"] is True:
+            warnings.warn("No Python files found in specified paths. Processing aborted")
+            sys.exit(0)
+        else:
+            sys.exit(
+                "No Python files found "
+                "(use `--accept-empty` to exit with code 0 if you expect this case)"
+            )
+
+    # Parse ignore names file
+    has_ignore_patterns_in_config = "ignore_patterns" in kwargs
+    if os.path.isfile(kwargs["ignore_names_file"]) and has_ignore_patterns_in_config:
+        raise ValueError(
+            (
+                "The docstr-coverage configuration file {} contains ignore_patterns,"
+                " and at the same time an ignore file {} was found."
+                " Ignore patterns must be specified in only one location at a time."
+            ).format(kwargs["config_file"], kwargs["ignore_names_file"])
+        )
+    elif os.path.isfile(kwargs["ignore_names_file"]):
+        ignore_names = parse_ignore_names_file(kwargs["ignore_names_file"])
+    elif has_ignore_patterns_in_config:
+        ignore_names = parse_ignore_patterns_from_dict(kwargs["ignore_patterns"])
+    else:
+        ignore_names = []
+
+    # Calculate docstring coverage
+    file_results, total_results = get_docstring_coverage(
+        all_paths,
+        skip_magic=kwargs["skip_magic"],
+        skip_file_docstring=kwargs["skip_file_doc"],
+        skip_init=kwargs["skip_init"],
+        skip_class_def=kwargs["skip_class_def"],
+        skip_private=kwargs["skip_private"],
+        verbose=kwargs["verbose"],
+        ignore_names=ignore_names,
+    )
+
+    # Save badge
+    if kwargs["badge"]:
+        badge = Badge(kwargs["badge"], total_results["coverage"])
+        badge.save()
+
+        if kwargs["verbose"]:
+            print("Docstring coverage badge saved to {!r}".format(badge.path))
+
+    if kwargs["percentage_only"] is True:
+        print(total_results["coverage"])
+
+    # Exit
+    if total_results["coverage"] < kwargs["fail_under"]:
+        raise SystemExit(1)
+
+    raise SystemExit(0)
+
+
+if __name__ == "__main__":
+    execute()
---
+++ packages/docstr-coverage/docstr_coverage-2.0.1-py3-none-any.whl/docstr_coverage/coverage.py
@@ -0,0 +1,351 @@
+"""This repository is based on the work of Alexey "DataGreed" Strelkov,
+and James Harlow (see "THANKS.txt" for details)"""
+
+import logging
+import os
+import re
+
+# TODO: If Python 2, ```from __future__ import print_function```
+from ast import parse
+from typing import List, Tuple
+
+from docstr_coverage.visitor import DocStringCoverageVisitor
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+
+
+GRADES = (
+    ("AMAZING! Your docstrings are truly a wonder to behold!", 100),
+    ("Excellent", 92),
+    ("Great", 85),
+    ("Very good", 70),
+    ("Good", 60),
+    ("Not bad", 40),
+    ("Not good", 25),
+    ("Extremely poor", 10),
+    ("Not documented at all", 2),
+    ("Do you even docstring?", 0),
+)
+
+
+def do_ignore_node(filename: str, base_name: str, node_name: str, ignore_names: tuple) -> bool:
+    """Determine whether a node (identified by its file, base, and own names) should be ignored
+
+    Parameters
+    ----------
+    filename: String
+        Name of the file containing `node_name`
+    base_name: String
+        Name of the node's parent node
+    node_name: String
+        Name of the node within the file. Usually a function name, class name, or a method name. In
+        the case of method names, `node_name` will be only the method's name, while `base_name` will
+        be of the form "<class_name>."
+    ignore_names: Tuple[List[str], ...]
+        Patterns to ignore when checking documentation. Each list in `ignore_names` defines a
+        different pattern to be ignored. The first element in each list is the regular expression
+        for matching filenames. All remaining arguments in each list are regexes for matching names
+        of functions/classes. A node is ignored if it matches the filename regex and at least one
+        of the remaining regexes
+
+    Returns
+    -------
+    Boolean
+        True if the node should be ignored, else False"""
+    filename = os.path.basename(filename).split(".")[0]
+
+    for (file_regex, *name_regexes) in ignore_names:
+        file_match = re.fullmatch(file_regex, filename)
+        file_match = file_match.group() if file_match else None
+
+        if file_match != filename:
+            continue
+
+        for name_regex in name_regexes:
+            # Match on node name only
+            name_match = re.fullmatch(name_regex, node_name)
+            name_match = name_match.group() if name_match else None
+
+            if name_match:
+                return True
+
+            # Match on node's period-delimited path: Its parent nodes (if any), plus the node name.
+            #   This enables targeting i.e. the `__init__` method of a particular class, whereas
+            #   the simple name match above would target `__init__` methods of all classes
+            full_name_match = re.fullmatch(name_regex, "{}{}".format(base_name, node_name))
+            full_name_match = full_name_match.group() if full_name_match else None
+
+            if full_name_match:
+                return True
+    return False
+
+
+def get_docstring_coverage(
+    filenames: list,
+    skip_magic: bool = False,
+    skip_file_docstring: bool = False,
+    skip_init: bool = False,
+    skip_class_def: bool = False,
+    skip_private: bool = False,
+    verbose: int = 0,
+    ignore_names: Tuple[List[str], ...] = (),
+):
+    """Checks contents of `filenames` for missing docstrings, and produces a report
+    detailing docstring status.
+
+    Parameters
+    ----------
+    filenames: List
+        List of filename strings that are absolute or relative paths
+    skip_magic: Boolean, default=False
+        If True, skips all magic methods (double-underscore-prefixed),
+        except '__init__' and does not include them in the report
+    skip_file_docstring: Boolean, default=False
+        If True, skips check for a module-level docstring
+    skip_init: Boolean, default=False
+        If True, skips methods named '__init__' and does not include
+        them in the report
+    skip_class_def: Boolean, default=False
+        If True, skips class definitions and does not include them in the report.
+        If this is True, the class's methods will still be checked
+    skip_private: Boolean, default=False
+        If True, skips function definitions beginning with a single underscore and does
+        not include them in the report.
+    verbose: Int in [0, 1, 2, 3], default=0
+        0) No printing.
+        1) Print total stats only.
+        2) Print stats for all files.
+        3) Print missing docstrings for all files.
+    ignore_names: Tuple[List[str], ...], default=()
+        Patterns to ignore when checking documentation. Each list in `ignore_names` defines a
+        different pattern to be ignored. The first element in each list is the regular expression
+        for matching filenames. All remaining arguments in each list are regexes for matching names
+        of functions/classes. A node is ignored if it matches the filename regex and at least one
+        of the remaining regexes
+
+    Returns
+    -------
+    Dict
+        Links filename keys to a dict of stats for that filename. Example:
+
+        >>> {
+        ...     '<filename>': {
+        ...         'missing': ['<method_or_class_name>', '...'],
+        ...         'module_doc': '<Boolean>',
+        ...         'missing_count': '<missing_count int>',
+        ...         'needed_count': '<needed_docstrings_count int>',
+        ...         'coverage': '<percent_of_coverage float>',
+        ...         'empty': '<Boolean>'
+        ...     }, ...
+        ... }
+    Dict
+        Total summary stats for all files analyzed. Example:
+
+        >>> {
+        ...     'missing_count': '<total_missing_count int>',
+        ...     'needed_count': '<total_needed_docstrings_count int>',
+        ...     'coverage': '<total_percent_of_coverage float>'
+        ... }"""
+    # TODO: Switch to Python's `logging` module, and remove
+    #       below nested `log` function definition
+    def log(text, level=1):
+        """Prints `text` to log depending on `verbose`
+
+        Parameters
+        ----------
+        text: String
+            The text to print
+        level: Int
+            The verbosity level at which it becomes appropriate to print the content.
+            If `level` > `verbose`, nothing happens. Otherwise, `text` is printed."""
+        if verbose >= level:
+            logger.info(text)
+
+    # TODO: Move :func:`print_docstring` to be a normal function
+    #       outside of :func:`get_docstring_coverage`
+    def print_docstring(base, node, filename, ignore_names=()):
+        """Log the existence of a docstring for `node`, and accumulate stats regarding
+        expected and encountered docstrings for `node` and its children (if any).
+
+        Parameters
+        ----------
+        base: String
+            The name of this node's parent node
+        node: Tuple triple of (String, Boolean, List)
+            Information describing a node. `node[0]` is the node's name.
+            `node[1]` is True if the node was properly documented,
+            else False. `node[3]` is a list containing the node's children
+            as triples of the same form (if it had any)
+        filename: String
+            String containing a name of file.
+        ignore_names: tuple of lists ([String, String, [String...]]) where the first
+            element is the regular expression for matching filenames. All remaining
+            arguments are regexes for matching names of functions/classes to be
+            excluded from checking for documentation. It will be excluded if and
+            only if the first and at least one of the remaining regexes hits a match.
+
+        Returns
+        -------
+        docs_needed: Int
+            The number of docstrings expected for `node` and its children to achieve
+            full documentation coverage
+        docs_covered: Int
+            The number of docstrings found for `node` and its children
+        _missing_list: List
+            Nodes that were expected to have docstrings but did not.
+            Values are the concatenation of a node's base and its name"""
+
+        _missing_list = []
+        docs_needed = 1
+        docs_covered = 0
+        name, has_doc, child_nodes = node
+
+        #################### Check Current Node ####################
+        if not has_doc:
+            # TODO: Add option to skip class definition docstrings
+            #      (though one of class or magic methods should used)
+            if skip_init and name == "__init__":
+                docs_needed -= 1
+            elif (
+                skip_magic and name.startswith("__") and name.endswith("__") and name != "__init__"
+            ):
+                docs_needed -= 1
+            elif skip_class_def and "_" not in name and (name[0] == name[0].upper()):
+                docs_needed -= 1
+            elif skip_private and name.startswith("_") and not name.startswith("__"):
+                docs_needed -= 1
+            elif ignore_names and do_ignore_node(filename, base, name, ignore_names):
+                docs_needed -= 1
+            else:
+                log(" - No docstring for `%s%s`" % (base, name), 3)
+                _missing_list.append(base + name)
+        else:
+            docs_covered += 1
+
+        #################### Check Child Nodes ####################
+        for _symbol in child_nodes:
+            _temp_docs_needed, _temp_docs_covered, temp_missing_list = print_docstring(
+                "%s." % name, _symbol, filename, ignore_names
+            )
+            docs_needed += _temp_docs_needed
+            docs_covered += _temp_docs_covered
+            _missing_list += temp_missing_list
+
+        return docs_needed, docs_covered, _missing_list
+
+    total_docs_needed = 0
+    total_docs_covered = 0
+    empty_files = 0
+    file_results = {}
+
+    for filename in filenames:
+        log('\nFile: "%s"' % filename, 2)
+
+        file_docs_needed = 1
+        file_docs_covered = 1
+        file_missing_list = []
+
+        #################### Read and Parse Source ####################
+        with open(filename, "r", encoding="utf-8") as f:
+            source_tree = f.read()
+
+        doc_visitor = DocStringCoverageVisitor(filename=filename)
+        doc_visitor.visit(parse(source_tree))
+        _tree = doc_visitor.tree[0]
+        # pp(_tree)
+
+        #################### Process Results ####################
+        # _tree contains [<module docstring>, <is_empty: bool>, <symbols: classes and funcs>]
+        if (not _tree[0]) and (not _tree[1]) and (not skip_file_docstring):
+            log(" - No module docstring", 3)
+            file_docs_covered -= 1
+        elif _tree[1]:
+            log(" - File is empty", 3)
+            file_docs_needed = 0
+            file_docs_covered = 0
+            empty_files += 1
+
+        # Traverse through functions and classes
+        for symbol in _tree[-1]:
+            temp_docs_needed, temp_docs_covered, missing_list = print_docstring(
+                "", symbol, filename, ignore_names
+            )
+            file_docs_needed += temp_docs_needed
+            file_docs_covered += temp_docs_covered
+            file_missing_list += missing_list
+
+        total_docs_needed += file_docs_needed
+        total_docs_covered += file_docs_covered
+
+        if file_docs_needed:
+            coverage = float(file_docs_covered) * 100 / float(file_docs_needed)
+        else:
+            coverage = 0
+
+        file_results[filename] = {
+            "missing": file_missing_list,
+            "module_doc": bool(_tree[0]),
+            "missing_count": file_docs_needed - file_docs_covered,
+            "needed_count": file_docs_needed,
+            "coverage": coverage,
+            "empty": bool(_tree[1]),
+        }
+
+        log(
+            " Needed: %s; Found: %s; Missing: %s; Coverage: %.1f%%"
+            % (
+                file_docs_needed,
+                file_docs_covered,
+                file_results[filename]["missing_count"],
+                file_results[filename]["coverage"],
+            ),
+            2,
+        )
+
+    total_results = {
+        "missing_count": total_docs_needed - total_docs_covered,
+        "needed_count": total_docs_needed,
+        "coverage": (
+            float(total_docs_covered) * 100 / float(total_docs_needed) if total_docs_needed else 100
+        ),
+    }
+
+    postfix = ""
+    if empty_files:
+        postfix = " (%s files are empty)" % empty_files
+    if skip_magic:
+        postfix += " (skipped all non-init magic methods)"
+    if skip_file_docstring:
+        postfix += " (skipped file-level docstrings)"
+    if skip_init:
+        postfix += " (skipped __init__ methods)"
+    if skip_class_def:
+        postfix += " (skipped class definitions)"
+    if skip_private:
+        postfix += " (skipped private methods)"
+
+    log("\n", 2)
+
+    if len(filenames) > 1:
+        log("Overall statistics for %s files%s:" % (len(filenames), postfix), 1)
+    else:
+        log("Overall statistics%s:" % postfix, 1)
+
+    log(
+        "Needed: {}  -  Found: {}  -  Missing: {}".format(
+            total_docs_needed, total_docs_covered, total_results["missing_count"]
+        ),
+        1,
+    )
+
+    #################### Calculate Total Grade ####################
+    grade = next(
+        message
+        for message, grade_threshold in GRADES
+        if grade_threshold <= total_results["coverage"]
+    )
+
+    log("Total coverage: {:.1f}%  -  Grade: {}".format(total_results["coverage"], grade), 1)
+
+    return file_results, total_results
