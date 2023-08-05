557af8e38941d102eea0094e5081e2769e92836e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:07 +0000

    Add package script-args-parser/0.3.1/script-args-parser-0.3.1.tar.gz

---
+++ packages/script-args-parser/script-args-parser-0.3.1.tar.gz/script-args-parser-0.3.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['script_args_parser']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=5.4.1,<6.0.0', 'toml>=0.10.2,<0.11.0']
+
+setup_kwargs = {
+    'name': 'script-args-parser',
+    'version': '0.3.1',
+    'description': 'Script arguments parsing library.',
+    'long_description': None,
+    'author': 'KRunchPL',
+    'author_email': 'krunchfrompoland@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
