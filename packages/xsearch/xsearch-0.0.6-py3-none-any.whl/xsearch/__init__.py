d75a99c252052293b8a829e8ac95653edcaca5f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:24 +0000

    Add package xsearch/0.0.6/xsearch-0.0.6-py3-none-any.whl

---
+++ packages/xsearch/xsearch-0.0.6-py3-none-any.whl/xsearch/__init__.py
@@ -0,0 +1,5 @@
+# from xsearch.page import SearchPage, IndexPage
+# from xsearch.input import SearchContent
+# from xsearch.output import CrawledContent
+# from xsearch.main import search
+from . import main
