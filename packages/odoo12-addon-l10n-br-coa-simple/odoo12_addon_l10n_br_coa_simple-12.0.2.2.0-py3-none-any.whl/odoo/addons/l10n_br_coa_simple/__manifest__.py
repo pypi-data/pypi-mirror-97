7e09aec1de0c7be3691f24199f330fd4481da0fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:16 +0000

    Add package odoo12-addon-l10n-br-coa-simple/12.0.2.2.0/odoo12_addon_l10n_br_coa_simple-12.0.2.2.0-py3-none-any.whl

---
+++ packages/odoo12-addon-l10n-br-coa-simple/odoo12_addon_l10n_br_coa_simple-12.0.2.2.0-py3-none-any.whl/odoo/addons/l10n_br_coa_simple/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2009  Renato Lima - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Brazilian - Simple Accounting",
+    "summary": "Brazilian Simple Chart of Account",
+    "category": "Localization",
+    "license": "AGPL-3",
+    "author": "Akretion, " "Odoo Community Association (OCA)",
+    "website": "http://github.com/OCA/l10n-brazil",
+    "version": "12.0.2.2.0",
+    "depends": ["l10n_br_coa"],
+    "data": [
+        "data/l10n_br_coa_simple_template.xml",
+        "data/account_group.xml",
+        "data/account.account.template.csv",
+        "data/account_tax_group.xml",
+        "data/l10n_br_coa_simple_template_post.xml",
+    ],
+    "post_init_hook": "post_init_hook",
+}
