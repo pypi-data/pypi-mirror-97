d11a12a299b95126f98d229cedf390201573e161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:39 +0000

    Add package torchvtk/0.4.5/torchvtk-0.4.5-py3-none-any.whl

---
+++ packages/torchvtk/torchvtk-0.4.5-py3-none-any.whl/torchvtk/transforms/__init__.py
@@ -0,0 +1,3 @@
+from .dict_transform import DictTransform, Lambda, RandLambda, Composite, Resize, \
+    GaussianNoise, GaussianBlur, Crop, RandFlip, RandPermute, RandRot90, Noop, \
+    NormalizeMinMax, NormalizeStandardize
