e3892cfb4846093f8a10652fdc1e5664ec97b122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:27 +0000

    Add package pytest-rts/1.2.1/pytest_rts-1.2.1-py3-none-any.whl

---
+++ packages/pytest-rts/pytest_rts-1.2.1-py3-none-any.whl/pytest_rts/pytest/fake_item.py
@@ -0,0 +1,8 @@
+"""This module contains a fake pytest item class"""
+
+
+class FakeItem:  # pylint: disable=too-few-public-methods
+    """Fake class"""
+
+    def __init__(self, config):
+        self.config = config
