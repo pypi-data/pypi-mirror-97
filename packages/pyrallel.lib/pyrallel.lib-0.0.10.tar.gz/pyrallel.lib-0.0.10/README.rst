69e5a9a4201dcc6ed9612c40d3bcd86ffcc04f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:47 +0000

    Add package pyrallel.lib/0.0.10/pyrallel.lib-0.0.10.tar.gz

---
+++ packages/pyrallel.lib/pyrallel.lib-0.0.10.tar.gz/pyrallel.lib-0.0.10/README.rst
@@ -0,0 +1,44 @@
+Pyrallel
+========
+
+.. begin-intro
+
+Yet another easy-to-use python parallel library for humans.
+
+.. image:: https://img.shields.io/badge/license-MIT-blue.svg
+    :target: https://raw.githubusercontent.com/usc-isi-i2/pyrallel/master/LICENSE
+    :alt: License
+
+.. image:: https://github.com/usc-isi-i2/pyrallel/workflows/Tests/badge.svg?branch=master
+    :target: https://github.com/usc-isi-i2/pyrallel/actions
+    :alt: Github actions
+
+.. image:: https://coveralls.io/repos/github/usc-isi-i2/pyrallel/badge.svg?branch=master
+    :target: https://coveralls.io/github/usc-isi-i2/pyrallel?branch=master
+    :alt: Coveralls
+
+.. image:: https://badge.fury.io/py/pyrallel.lib.svg
+    :target: https://badge.fury.io/py/pyrallel.lib
+    :alt: pypi
+
+.. image:: https://readthedocs.org/projects/pyrallel/badge/?version=latest
+    :target: http://pyrallel.readthedocs.io/en/latest
+    :alt: Documents
+
+- ParallelProcessor: Newbie-friendly process-based parallel computing api.
+- MapReduce: Ultimately simple map and reduce computing model.
+- ShmQueue: Extremely fast shared memory driven general purpose multiprocessing queue.
+
+.. end-intro
+
+Installation
+------------
+::
+
+    pip install pyrallel.lib
+
+
+Documentation
+-------------
+
+`Read the Doc <http://pyrallel.readthedocs.io>`_
---
+++ packages/pyrallel.lib/pyrallel.lib-0.0.10.tar.gz/pyrallel.lib-0.0.10/setup.py
@@ -0,0 +1,55 @@
+import os
+from distutils.core import setup
+from setuptools import find_packages
+
+
+with open('README.rst', 'r') as fh:
+    long_description = fh.read()
+
+with open('requirements.txt', 'r') as f:
+    install_requires = list()
+    dependency_links = list()
+    for line in f:
+        re = line.strip()
+        if re:
+            if re.startswith('git+') or re.startswith('svn+') or re.startswith('hg+'):
+                dependency_links.append(re)
+            else:
+                install_requires.append(re)
+
+about = {}
+with open(
+        os.path.join(os.path.abspath(os.path.dirname(__file__)), 'pyrallel', '__version__.py'),
+        'r', encoding='utf-8') as f:
+    exec(f.read(), about)
+
+packages = find_packages()
+
+setup(
+    name='pyrallel.lib',
+    version=about['__version__'],
+    packages=packages,
+    url='https://github.com/usc-isi-i2/pyrallel',
+    project_urls={
+        "Bug Tracker": "https://github.com/usc-isi-i2/pyrallel/issues",
+        "Documentation": "https://pyrallel.readthedocs.io",
+        "Source Code": "https://github.com/usc-isi-i2/pyrallel",
+    },
+    license='MIT',
+    author='USC/ISI',
+    author_email='yixiangy@isi.edu',
+    description='Yet another easy-to-use python3 parallel library for humans.',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    include_package_data=True,
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    )
+)
