71928aa9cc7d85479f7ef5c87a2efdb74f77a29e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:09 +0000

    Add package oso/0.11.1/oso-0.11.1-cp36-cp36m-macosx_10_9_x86_64.whl

---
+++ packages/oso/oso-0.11.1-cp36-cp36m-macosx_10_9_x86_64.whl/oso/__init__.py
@@ -0,0 +1,3 @@
+from polar import polar_class, Variable, Predicate
+from .oso import Oso
+from polar.exceptions import OsoError
---
+++ packages/oso/oso-0.11.1-cp36-cp36m-macosx_10_9_x86_64.whl/polar/__init__.py
@@ -0,0 +1,5 @@
+from .polar import Polar, polar_class
+from .query import Query, QueryResult
+from .variable import Variable
+from .predicate import Predicate
+from .expression import Expression, Pattern
