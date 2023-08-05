a55807b93f44bc4994ebd92c4052e0d7499cd998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:52 +0000

    Add package ppslack/3.0.2/ppslack-3.0.2-py2.py3-none-any.whl

---
+++ packages/ppslack/ppslack-3.0.2-py2.py3-none-any.whl/ppslack/main.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""Tool to send messages via Slack
+
+Copyright (c) 2019-2021 Peter Pakos. All rights reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import argparse
+import os
+import platform
+import sys
+
+from pplogger import get_logger
+
+from . import __version__
+from .slack import Slack
+
+__app_name__ = 'ppslack'
+
+parser = argparse.ArgumentParser(description='Tool to send messages via Slack', add_help=False)
+parser.add_argument('--version', action='version', version='%s %s' % (__app_name__, __version__))
+parser.add_argument('--help', action='help', help='show this help message and exit')
+parser.add_argument('--debug', action='store_true', dest='debug', help='debugging mode')
+parser.add_argument('--verbose', action='store_true', dest='verbose', help='verbose debugging mode')
+parser.add_argument('-f', '--from', dest='sender',
+                    help='sender')
+parser.add_argument('-t', '--to', dest='recipients', nargs='+', required=True, help='recipient', default=[])
+parser.add_argument('-s', '--subject', dest='subject', default='', help='subject')
+parser.add_argument('-S', '--slack', action='store_true', dest='slack',
+                    help='Slack message (keeping for backward compatibility)')
+parser.add_argument('-H', '--code', dest='code', action='store_true', help='send code block')
+args = parser.parse_args()
+
+log = get_logger(name='ppslack.slack', debug=args.debug, verbose=args.verbose)
+
+
+def main():
+    log.debug(args)
+
+    sender = args.sender if args.sender else os.getenv('USER') + '@' + platform.node()
+    log.debug('Sender: %s' % sender)
+
+    message = ''
+    lines = 0
+
+    for line in sys.stdin:
+        message += line
+        if line != '' and line != '\n':
+            lines += 1
+
+    if not lines:
+        log.critical('Nothing to send')
+        sys.exit(1)
+
+    try:
+        slack = Slack()
+        slack.send(
+            sender=sender,
+            recipients=args.recipients,
+            subject=args.subject,
+            message=message,
+            code=args.code
+        )
+    except Exception as e:
+        log.critical(e)
+        sys.exit(1)
---
+++ packages/ppslack/ppslack-3.0.2-py2.py3-none-any.whl/ppslack/slack.py
@@ -0,0 +1,182 @@
+# -*- coding: utf-8 -*-
+"""This module implements sending messages via Slack
+
+Copyright (c) 2019-2021 Peter Pakos. All rights reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import logging
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+from ppconfig import Config
+
+log = logging.getLogger(__name__)
+
+
+class Slack(object):
+    def __init__(self):
+        try:
+            self._config = Config('ppslack')
+            self._slack_key = self._config.get('slack_key')
+            self._email_domain = self._config.get('email_domain')
+        except Exception as e:
+            log.debug(e)
+            try:
+                self._config = Config('ppmail')
+                self._slack_key = self._config.get('slack_key')
+                self._email_domain = self._config.get('email_domain')
+            except Exception as e:
+                log.debug(e)
+                raise Exception('Config file not found')
+            else:
+                log.debug('Config file found: ppmail')
+        else:
+            log.debug('Config file found: ppslack')
+
+        self._client = WebClient(self._slack_key)
+        self._users = {}
+        self._channels = {}
+
+    @property
+    def users(self):
+        if not self._users:
+            try:
+                r = self._client.users_list()
+            except SlackApiError:
+                raise
+
+            for user in r["members"]:
+                if not user['deleted'] and not user['is_bot'] and user['id'] != 'USLACKBOT':
+                    self._users[user['name']] = user
+
+            log.debug('Active members: %s' % len(self._users))
+
+        return self._users
+
+    def find_user_by_email(self, email):
+        for user in self.users.values():
+            if user['profile']['email'] == email:
+                log.debug('Found user with email %s: %s' % (email, user['name']))
+                return user
+
+        return
+
+    @property
+    def channels(self):
+        if not self._channels:
+            try:
+                r = self._client.conversations_list(limit=1000)
+            except SlackApiError:
+                raise
+
+            for channel in r['channels']:
+                if not channel['is_archived'] and not channel['is_private']:
+                    self._channels[channel['name']] = channel
+
+            log.debug('Public channels: %s' % len(self._channels))
+
+        return self._channels
+
+    def send(self, sender, recipients, subject, message, code=False):
+        if type(recipients) is not list:
+            recipientsl = []
+            if recipients:
+                recipientsl.append(recipients)
+            recipients = recipientsl
+
+        if subject:
+            subject = '*%s*\n' % subject.strip()
+
+        if code:
+            message = "```%s```" % message
+
+        text = '%s%s' % (subject, message)
+
+        if code:
+            text = text.splitlines(True)
+
+        failed = 0
+
+        for recipient in recipients:
+            recipient_id = None
+
+            if '@%s' % self._email_domain in recipient:
+                user = self.find_user_by_email(recipient)
+                if user:
+                    recipient_id = user['id']
+            elif str(recipient).startswith('@'):
+                user = self.users[str(recipient).strip('@')]
+                if user:
+                    recipient_id = user['id']
+            else:
+                if recipient in self.channels:
+                    channel = self.channels[recipient]
+                    recipient_id = channel['id']
+                elif recipient in self.users:
+                    user = self.users[recipient]
+                    recipient_id = user['id']
+
+            if not recipient_id:
+                failed += 1
+                log.error('User or channel %s not found' % recipient)
+                continue
+
+            if code:
+                to_print = []
+                length = 0
+
+                for i, line in enumerate(text):
+                    to_print += text[i]
+                    length += len(line)
+
+                    if i == (len(text) - 1) or (length + len(text[i+1])) > 3800:
+                        to_print = ''.join(to_print)
+
+                        if not str(to_print).startswith('```') and not (subject and subject in to_print):
+                            to_print = '```' + to_print
+                        if not str(to_print).endswith('```'):
+                            to_print = to_print + '```'
+
+                        try:
+                            r = self._client.chat_postMessage(
+                                channel=recipient_id,
+                                text=to_print,
+                                username=sender,
+                                link_names=True
+                            )
+                        except SlackApiError:
+                            raise
+
+                        if not r.get('ok'):
+                            failed += 1
+
+                        to_print = []
+                        length = 0
+            else:
+                try:
+                    r = self._client.chat_postMessage(
+                        channel=recipient_id,
+                        text=text,
+                        username=sender,
+                        link_names=True
+                    )
+                except SlackApiError:
+                    raise
+
+                if not r.get('ok'):
+                    failed += 1
+
+        return False if failed else True
