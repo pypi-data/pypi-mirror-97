44660b87c3ee5baeb034d58ed4e67dc2ed43d2ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:50 +0000

    Add package NlpToolkit-SentiNet-Cy/1.0.3/NlpToolkit-SentiNet-Cy-1.0.3.tar.gz

---
+++ packages/NlpToolkit-SentiNet-Cy/NlpToolkit-SentiNet-Cy-1.0.3.tar.gz/NlpToolkit-SentiNet-Cy-1.0.3/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(
+    ext_modules=cythonize(["SentiNet/*.pyx", "SentiNet/*.pxd"],
+                          compiler_directives={'language_level': "3"}),
+    name='NlpToolkit-SentiNet-Cy',
+    version='1.0.3',
+    packages=['SentiNet'],
+    package_data={'SentiNet': ['*.pxd', '*.pyx', '*.c']},
+    url='https://github.com/StarlangSoftware/TurkishSentiNet-Cy',
+    license='',
+    author='olcaytaner',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
+    description='Turkish SentiNet'
+)
