305643e45057b319be8cb50d86d5b83d3b824e66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:21 +0000

    Add package ipa-notify/0.2.3/ipa-notify-0.2.3.tar.gz

---
+++ packages/ipa-notify/ipa-notify-0.2.3.tar.gz/ipa-notify-0.2.3/README.md
@@ -0,0 +1,69 @@
+### FreeIPA Notification
+[![Upload Python Package](https://github.com/cagdasbas/ipa-notify/actions/workflows/python-publish.yml/badge.svg)](https://github.com/cagdasbas/ipa-notify/actions/workflows/python-publish.yml)
+
+Notify IPA Users for password expiration and locked users to admin
+
+Required packages:
+- krb5-devel
+
+1. Create a new role for notifier
+   ```shell
+   ipa role-add --desc "Notification agent role" "Notification Agent"
+   ```
+2. Add privileges to the role
+   ```shell
+   ipa role-add-privilege "Notification Agent" --privileges="User Administrators"
+   ipa role-add-privilege "Notification Agent" --privileges="Group Administrators"
+   ipa role-add-privilege "Notification Agent" --privileges="Password Policy Readers"
+   ```
+3. Create a new service and assign the role to this service
+   ```shell
+   ipa service-add NOTIFY/ipa1.example.com
+   ipa role-add-member  "Notification Agent" --services="NOTIFY/ipa1.example.com@EXAMPLE.COM"
+   ipa service-allow-retrieve-keytab "NOTIFY/ipa1.example.com@EXAMPLE.COM" --hosts=ipa1.example.com
+   ```
+4. Obtain a keytab with fix permissions
+   ```shell
+   ipa-getkeytab -s ipa1.example.com -p "NOTIFY/ipa1.example.com@EXAMPLE.COM" -k ~/.priv/notify.keytab
+   chmod -R 600 ~/.priv
+   ```
+2. Run the command in ```noop``` mode for a successful user listing
+3. Create a script with proper permissions under ```/usr/local/sbin/```
+4. Add a crontab entry. For example ```0 0 *  *  * root ipa_notify.sh > /var/log/ipa_notify.log```
+
+
+#### Parameters:
+```bash
+$ ipa-notify --help
+usage: ipa_notify.py [-h] [--server SERVER] [--verify-ssl] [--no-verify-ssl] [--principal PRINCIPAL] [--keytab KEYTAB] [--groups GROUPS [GROUPS ...]] [--limit LIMIT] [--smtp-host SMTP_HOST] [--smtp-port SMTP_PORT]
+                     [--smtp-user SMTP_USER] [--smtp-pass SMTP_PASS] [--smtp-from SMTP_FROM] [--admin ADMIN] [--noop NOOP] [--loglevel {CRITICAL,ERROR,WARNING,INFO,DEBUG,NOTSET}]
+
+IPA Notifier
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --server SERVER       ipa server fqdn
+  --verify-ssl          verify ipa connection SSL cert (default)
+  --no-verify-ssl       do not verify ipa connection SSL cert
+  --principal PRINCIPAL
+                        user principal for kerberos authentication
+  --keytab KEYTAB       keytab path
+  --groups GROUPS [GROUPS ...]
+                        list of user groups to check
+  --limit LIMIT         number of days before notifying a user
+  --smtp-host SMTP_HOST
+                        smtp host for sending email
+  --smtp-port SMTP_PORT
+                        smtp port for sending email
+  --smtp-user SMTP_USER
+                        smtp user login
+  --smtp-pass SMTP_PASS
+                        smtp user password
+  --smtp-from SMTP_FROM
+                        smtp from email address
+  --admin ADMIN         admin user email to notify about locked users
+  --noop NOOP           no operation mode. Do not send emails
+  --loglevel {CRITICAL,ERROR,WARNING,INFO,DEBUG,NOTSET}
+                        log level
+
+```
---
+++ packages/ipa-notify/ipa-notify-0.2.3.tar.gz/ipa-notify-0.2.3/ipa_notify/__main__.py
@@ -0,0 +1,132 @@
+#  Copyright (c) 2021.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import argparse
+import datetime
+import logging
+import os
+import subprocess
+import sys
+
+import requests
+from python_freeipa.client_meta import ClientMeta
+from python_freeipa.exceptions import NotFound, Unauthorized
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+from ipa_notify import parse_args
+from ipa_notify.notifier import Notifier
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+def init(args: argparse.Namespace) -> tuple:
+	"""
+	Initialize application
+	:param args: command line arguments
+	:return: (ipa_client, email_notifier)
+	"""
+	logging.basicConfig(
+		format='%(asctime)s - %(module)s.%(funcName)s - %(levelname)s - %(message)s',
+		level=getattr(logging, args.log_level, None)
+	)
+
+	if not os.path.exists(args.keytab):
+		logging.error("Cannot find keytab file %s", args.keytab)
+		sys.exit(2)
+
+	try:
+		process = subprocess.Popen(
+			f"/usr/bin/kinit {args.principal} -k -t {args.keytab}".split(), stdout=subprocess.PIPE,
+			stderr=subprocess.PIPE
+		)
+		if process.returncode is not None and process.returncode != 0:
+			logging.error("Cannot obtain kerberos ticket")
+			sys.exit(3)
+	except ValueError:
+		sys.exit(3)
+
+	client = ClientMeta(args.server, verify_ssl=args.verify_ssl)
+	try:
+		client.login_kerberos()
+	except Unauthorized as err:
+		logging.error("login denied: %s", err)
+		sys.exit(1)
+
+	notifier = Notifier(
+		host=args.smtp_host, port=args.smtp_port,
+		user=args.smtp_user, password=args.smtp_pass,
+		from_email=args.smtp_from
+	)
+	return client, notifier
+
+
+# pylint: disable=too-many-locals
+def main():
+	"""
+	Main application process.
+	Checks each user in given groups for password expiration
+	"""
+
+	args = parse_args()
+	ipa_client, email_notifier = init(args)
+
+	admin_mail = args.admin
+	limit_day = args.limit
+
+	locked_users = {}
+	for group in args.groups:
+		try:
+			group_info = ipa_client.group_show(group)
+		except NotFound:
+			logging.error("no group named %s", group)
+			continue
+
+		for username in group_info['result']['member_user']:
+			user = ipa_client.user_show(username, all=True)['result']
+			lock_status = user['nsaccountlock']
+			if lock_status:
+				continue
+
+			try:
+				pw_policy = ipa_client.pwpolicy_show(user=username)
+				user_status = ipa_client.user_status(username, all=True)
+				for result in user_status['result']:
+					server = result['server']
+					is_failed = int(result['krbloginfailedcount'][0]) >= int(pw_policy['result']['krbpwdmaxfailure'][0])
+					if is_failed:
+						if username not in locked_users.keys():
+							locked_users[username] = []
+						locked_users[username].append(server)
+						logging.debug("account locked for %s in server %s", username, server)
+			except NotFound as err:
+				logging.error("password policy find error: %s", err)
+
+			email = user['mail'][0]
+			password_expire_date = user['krbpasswordexpiration'][0]['__datetime__']
+			password_expire_date = datetime.datetime.strptime(password_expire_date, '%Y%m%d%H%M%SZ')
+			left_days = (password_expire_date - datetime.datetime.now()).days
+			if left_days <= limit_day:
+				logging.info("user %s expiration day left %d", user['uid'][0], left_days)
+				if not args.noop:
+					email_notifier.notify_expiration(email, password_expire_date, left_days)
+
+	if len(locked_users.keys()) != 0:
+		logging.info("locked users: %s", locked_users)
+		if not args.noop:
+			email_notifier.notify_locked_users(admin_mail, locked_users)
+
+	subprocess.call(["/bin/kdestroy", "-A"])
+
+
+if __name__ == '__main__':
+	main()
