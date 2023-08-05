3d0a9f04f950b82f7d2fc8fa9af1d2f3daf9f274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:07 +0000

    Add package rubicon-ml/0.1.2/rubicon_ml-0.1.2-py3-none-any.whl

---
+++ packages/rubicon-ml/rubicon_ml-0.1.2-py3-none-any.whl/rubicon/__init__.py
@@ -0,0 +1,23 @@
+from rubicon.client import (
+    Artifact,
+    Dataframe,
+    Experiment,
+    Feature,
+    Metric,
+    Parameter,
+    Project,
+    Rubicon,
+)
+
+__version__ = "0.1.2"
+
+__all__ = [
+    "Artifact",
+    "Dataframe",
+    "Experiment",
+    "Feature",
+    "Metric",
+    "Parameter",
+    "Project",
+    "Rubicon",
+]
