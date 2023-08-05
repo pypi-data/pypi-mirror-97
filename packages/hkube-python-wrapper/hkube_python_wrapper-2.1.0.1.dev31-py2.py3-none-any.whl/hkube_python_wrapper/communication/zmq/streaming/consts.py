e044eeafe9d0960d43d8050d92aba656531dd2a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:09 +0000

    Add package hkube-python-wrapper/2.1.0.1.dev31/hkube_python_wrapper-2.1.0.1.dev31-py2.py3-none-any.whl

---
+++ packages/hkube-python-wrapper/hkube_python_wrapper-2.1.0.1.dev31-py2.py3-none-any.whl/hkube_python_wrapper/communication/zmq/streaming/consts.py
@@ -0,0 +1,17 @@
+
+
+PPP_INIT = b"\x01"  # Signals worker is ready
+PPP_HEARTBEAT = b"\x02"  # Signals worker heartbeat
+PPP_DISCONNECT = b"\x03"  # Disconnect
+PPP_READY = b"\x04"  # Signals worker is not ready
+PPP_NOT_READY = b"\x05"  # Signals worker is not ready
+PPP_DONE = b"\x06"
+PPP_EMPTY = b"\x07"
+PPP_NOOP = b"\x08"
+
+signals = {
+    PPP_INIT: 'INIT',
+    PPP_READY: 'READY',
+    PPP_NOT_READY: 'NOT_READY',
+    PPP_DONE: 'DONE',
+}
