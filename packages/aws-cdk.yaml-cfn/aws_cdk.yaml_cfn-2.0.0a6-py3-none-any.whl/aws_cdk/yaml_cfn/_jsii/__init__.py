b981999aea2dd09c63f25de9b94eac9b87f7e296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:29 +0000

    Add package aws-cdk.yaml-cfn/2.0.0a6/aws_cdk.yaml_cfn-2.0.0a6-py3-none-any.whl

---
+++ packages/aws-cdk.yaml-cfn/aws_cdk.yaml_cfn-2.0.0a6-py3-none-any.whl/aws_cdk/yaml_cfn/_jsii/__init__.py
@@ -0,0 +1,22 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-cdk/yaml-cfn",
+    "2.0.0-alpha.6",
+    __name__[0:-6],
+    "yaml-cfn@2.0.0-alpha.6.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
