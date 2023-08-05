b011889d246ef20ba310aa90f0477120cb7d6318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:44 +0000

    Add package aws-cdk.cx-api/2.0.0a6/aws-cdk.cx-api-2.0.0a6.tar.gz

---
+++ packages/aws-cdk.cx-api/aws-cdk.cx-api-2.0.0a6.tar.gz/aws-cdk.cx-api-2.0.0a6/src/aws_cdk/cx_api/_jsii/__init__.py
@@ -0,0 +1,21 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.cloud_assembly_schema._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-cdk/cx-api", "2.0.0-alpha.6", __name__[0:-6], "cx-api@2.0.0-alpha.6.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
