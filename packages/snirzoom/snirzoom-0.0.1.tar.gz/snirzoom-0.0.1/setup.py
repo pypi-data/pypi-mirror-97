32d3759e21b3c8c22bda739093f80924035c4dae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:27 +0000

    Add package snirzoom/0.0.1/snirzoom-0.0.1.tar.gz

---
+++ packages/snirzoom/snirzoom-0.0.1.tar.gz/snirzoom-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    include_package_data=True,
+    name='snirzoom',
+    version='0.0.1',
+    description='automate zoom meeting',
+    long_description="""enter the link and the time, the link will be opened according to the referenced time""",
+    url='',
+    author='snir dekel',
+    author_email='snirdekel101@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='zoom',
+    packages=find_packages(),
+    install_requires=['selenium', 'ttkthemes']
+)
