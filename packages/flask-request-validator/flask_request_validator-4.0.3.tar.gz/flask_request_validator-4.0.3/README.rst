cdd4dc5f9aef724a042c1901a4134bd11d1d23c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:35 +0000

    Add package flask-request-validator/4.0.3/flask_request_validator-4.0.3.tar.gz

---
+++ packages/flask-request-validator/flask_request_validator-4.0.3.tar.gz/flask_request_validator-4.0.3/README.rst
@@ -0,0 +1,11 @@
+Flask Request Validator
+=======================
+
+Simply request validator for Flask. See `readme`_
+
+Installation
+------------
+
+    $ pip install flask_request_validator
+
+.. _readme: https://github.com/d-ganchar/flask_request_validator
---
+++ packages/flask-request-validator/flask_request_validator-4.0.3.tar.gz/flask_request_validator-4.0.3/setup.py
@@ -0,0 +1,27 @@
+import io
+from setuptools import setup
+
+with io.open('README.rst', 'rt', encoding='utf8') as f:
+    long_description = f.read()
+
+setup(
+    name='flask_request_validator',
+    version='4.0.3',
+    description='Flask request data validation',
+    long_description=long_description,
+    url='https://github.com/d-ganchar/flask_request_validator',
+    author='Danila Ganchar',
+    author_email='danila.ganchar@gmail.com',
+    license='MIT',
+    keywords='flask request validation',
+    packages=['flask_request_validator'],
+    install_requires=['flask'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Flask',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
