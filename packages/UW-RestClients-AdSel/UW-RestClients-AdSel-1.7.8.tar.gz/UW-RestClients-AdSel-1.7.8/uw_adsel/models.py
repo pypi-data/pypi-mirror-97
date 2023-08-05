fc68d2bb41ab057733c3abcdb6ed89af00bd9ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:29 +0000

    Add package UW-RestClients-AdSel/1.7.8/UW-RestClients-AdSel-1.7.8.tar.gz

---
+++ packages/UW-RestClients-AdSel/UW-RestClients-AdSel-1.7.8.tar.gz/UW-RestClients-AdSel-1.7.8/uw_adsel/models.py
@@ -0,0 +1,133 @@
+from restclients_core import models
+
+
+class Major(models.Model):
+    major_abbr = models.CharField(max_length=32)
+    program_code = models.CharField(max_length=128)
+    academic_qtr_key_id = models.IntegerField()
+    major_pathway = models.IntegerField()
+    display_name = models.CharField(max_length=255)
+    college = models.CharField(max_length=255)
+    division = models.CharField(max_length=255)
+    dtx = models.CharField(max_length=255)
+    assigned_count = models.IntegerField()
+    assigned_resident = models.IntegerField(null=True)
+    assigned_nonresident = models.IntegerField(null=True)
+    assigned_international = models.IntegerField(null=True)
+
+
+class Cohort(models.Model):
+    academic_qtr_id = models.IntegerField()
+    cohort_number = models.IntegerField()
+    cohort_description = models.TextField()
+    cohort_residency = models.CharField(max_length=255)
+    admit_decision = models.CharField(max_length=255)
+    protected_group = models.BooleanField()
+    active_cohort = models.BooleanField()
+    assigned_count = models.IntegerField()
+
+
+class Quarter(models.Model):
+    id = models.IntegerField()
+    begin = models.DateTimeField()
+    end = models.DateTimeField()
+    active_ind = models.CharField(max_length=32)
+    appl_yr = models.CharField(max_length=4)
+    appl_qtr = models.CharField(max_length=1)
+    is_current = models.BooleanField()
+
+
+class Activity(models.Model):
+    assignment_date = models.DateTimeField()
+    comment = models.TextField()
+    user = models.CharField(max_length=12)
+    assignment_type = models.TextField()
+    cohort_number = models.IntegerField()
+    major_abbr = models.CharField(max_length=32)
+    major_program_code = models.CharField(max_length=32)
+    total_submitted = models.IntegerField()
+    total_assigned = models.IntegerField()
+
+
+class Application(models.Model):
+    adsel_id = models.IntegerField()
+    application_number = models.IntegerField()
+    system_key = models.IntegerField()
+    campus = models.IntegerField()
+    quarter_id = models.IntegerField()
+    assigned_cohort = models.IntegerField()
+    assigned_major = models.CharField(max_length=32)
+    major_program_code = models.CharField(max_length=255)
+
+    def json_data(self):
+        return {'admissionSelectionId': int(self.adsel_id),
+                'applicationNbr': self.application_number,
+                'systemKey': int(self.system_key)}
+
+
+class PurpleGoldApplication(Application):
+    award_amount = models.IntegerField()
+
+    def json_data(self):
+        return {'admissionSelectionId': int(self.adsel_id),
+                'awardAmount': self.award_amount}
+
+
+class Assignment(models.Model):
+    assignment_type = models.CharField()
+    quarter = models.IntegerField()
+    campus = models.IntegerField()
+    comments = models.TextField()
+    user = models.CharField(max_length=12)
+    applicants = []
+
+
+class CohortAssignment(Assignment):
+    cohort_number = models.IntegerField()
+    override_previous = models.BooleanField()
+    override_protected = models.BooleanField()
+
+    def json_data(self):
+        applicant_json = []
+        for application in self.applicants:
+            applicant_json.append(application.json_data())
+        return {'applicants': applicant_json,
+                'cohortNbr': int(self.cohort_number),
+                'overridePreviousCohort': self.override_previous,
+                'overridePreviousProtectedCohort': self.override_protected,
+                'assignmentDetail': {'assignmentType': self.assignment_type,
+                                     'academicQtrKeyId': self.quarter,
+                                     'campus': int(self.campus),
+                                     'comments': self.comments,
+                                     'decisionImportUser': self.user}
+                }
+
+
+class MajorAssignment(Assignment):
+    major_code = models.CharField()
+
+    def json_data(self):
+        applicant_json = []
+        for application in self.applicants:
+            applicant_json.append(application.json_data())
+        return {'applicants': applicant_json,
+                'majorProgramCode': self.major_code,
+                'assignmentDetail': {'assignmentType': self.assignment_type,
+                                     'academicQtrKeyId': self.quarter,
+                                     'campus': int(self.campus),
+                                     'comments': self.comments,
+                                     'decisionImportUser': self.user}
+                }
+
+
+class PurpleGoldAssignment(Assignment):
+    def json_data(self):
+        applicant_json = []
+        for application in self.applicants:
+            applicant_json.append(application.json_data())
+        return {'applicants': applicant_json,
+                'assignmentDetail': {'assignmentType': self.assignment_type,
+                                     'academicQtrKeyId': self.quarter,
+                                     'comments': self.comments,
+                                     'decisionImportUser': self.user}
+                }
---
+++ packages/UW-RestClients-AdSel/UW-RestClients-AdSel-1.7.8.tar.gz/UW-RestClients-AdSel-1.7.8/uw_adsel/tests/test_adsel.py
@@ -0,0 +1,180 @@
+from unittest import TestCase, mock
+from restclients_core.exceptions import DataFailureException
+from uw_adsel.utilities import fdao_adsel_override
+from uw_adsel import AdSel
+from uw_adsel.models import CohortAssignment, MajorAssignment, Application,\
+    PurpleGoldApplication, PurpleGoldAssignment
+from datetime import datetime
+
+
+def mocked_get_now():
+    dt = datetime(2019, 11, 12, 0, 10, 21)
+    return dt
+
+
+class AdselTest(TestCase):
+    adsel = AdSel()
+
+    def test_request_headers(self):
+        self.assertEqual(self.adsel._headers(), {'Accept': 'application/json'})
+
+    def test_error(self):
+        with self.assertRaises(DataFailureException):
+            self.adsel._get_resource("/foobar/")
+
+    def test_get_majors(self):
+        majors = self.adsel.get_majors_by_qtr(0)
+        self.assertEqual(len(majors), 4)
+        self.assertEqual(majors[1].major_abbr, "CHEM")
+        majors_unpaginated = self.adsel.get_majors_by_qtr(1)
+        self.assertEqual(len(majors_unpaginated), 2)
+
+    @mock.patch('uw_adsel.AdSel.get_now', side_effect=mocked_get_now)
+    def test_get_quarters(self, mock_obj):
+        quarters = self.adsel.get_quarters()
+        self.assertEqual(len(quarters), 2)
+        self.assertEqual(quarters[0].begin, datetime(2019, 11, 5, 0, 0, 0))
+        self.assertFalse(quarters[1].is_current)
+        self.assertTrue(quarters[0].is_current)
+
+    def test_get_cohorts(self):
+        cohorts = self.adsel.get_cohorts_by_qtr(0)
+        self.assertEqual(len(cohorts), 2)
+        self.assertEqual(cohorts[1].cohort_description,
+                         "This is another cohort")
+        cohorts_unpaginated = self.adsel.get_cohorts_by_qtr(1)
+        self.assertEqual(len(cohorts_unpaginated), 2)
+
+    @mock.patch('uw_adsel.AdSel.get_now', side_effect=mocked_get_now)
+    def test_get_now(self, mock_obj):
+        self.assertEqual(self.adsel.get_now(),
+                         datetime(2019, 11, 12, 0, 10, 21))
+
+    def test_get_activities(self):
+        activities = self.adsel.get_activities()
+        self.assertEqual(len(activities), 4)
+        self.assertEqual(activities[0].user, "javerage")
+
+    def test_get_filtered_activities(self):
+        # netid filter
+        activities = self.adsel.get_filtered_activities(netid="javerage")
+        self.assertEqual(len(activities), 2)
+        # No results
+        activities = self.adsel.get_filtered_activities(netid="foo")
+        self.assertEqual(len(activities), 0)
+        # dual filter
+        activities = self.adsel.get_filtered_activities(netid="javerage",
+                                                        system_key=12345)
+        self.assertEqual(len(activities), 4)
+
+    def test_get_application(self):
+        applications = self.adsel.get_applications_by_qtr_syskey(0, 123)
+        self.assertEqual(len(applications), 4)
+        self.assertEqual(applications[0].adsel_id, 1)
+
+    def test_get_applications_by_syskey_list(self):
+        # No Match
+        applications = self.adsel.get_applications_by_qtr_syskey_list(0,
+                                                                      [123])
+        self.assertEqual(len(applications), 0)
+        # Partial Match
+        applications = self.adsel.get_applications_by_qtr_syskey_list(0,
+                                                                      [123,
+                                                                       76711,
+                                                                       656340])
+        self.assertEqual(len(applications), 2)
+        # Full Match
+        applications = self.adsel.get_applications_by_qtr_syskey_list(0,
+                                                                      [456340,
+                                                                       97508,
+                                                                       156340,
+                                                                       76711])
+        self.assertEqual(len(applications), 6)
+
+    def test_post(self):
+        a1 = Application()
+        a1.adsel_id = 123
+        a1.system_key = 41
+        a1.application_number = 1
+        a2 = Application()
+        a2.adsel_id = 734
+        a2.system_key = 34
+        a2.application_number = 5
+
+        cohort_assign = CohortAssignment()
+        cohort_assign.applicants = [a1, a2]
+        cohort_assign.assignment_type = "upload"
+        cohort_assign.cohort_number = 1
+        cohort_assign.override_protected = True
+        cohort_assign.override_previous = False
+        cohort_assign.quarter = 0
+        cohort_assign.campus = 1
+        cohort_assign.comments = "My comment"
+        cohort_assign.user = "javerage"
+
+        cohort_json = cohort_assign.json_data()
+        self.assertEqual(cohort_json['overridePreviousCohort'], False)
+        self.assertEqual(cohort_json['overridePreviousProtectedCohort'], True)
+
+        try:
+            submission = self.adsel.assign_cohorts_bulk(cohort_assign)
+        except Exception:
+            self.fail('assign_cohorts raised an exception')
+
+        major_assign = MajorAssignment()
+        major_assign.applicants = [a1, a2]
+        major_assign.assignment_type = "upload"
+        major_assign.major_code = "CSE"
+        major_assign.quarter = 0
+        major_assign.campus = 1
+        major_assign.comments = "My comment"
+        major_assign.user = "javerage"
+
+        major_json = major_assign.json_data()
+        self.assertEqual(len(major_json['applicants']), 2)
+        self.assertEqual(major_json['applicants'][0]['admissionSelectionId'],
+                         123)
+
+        try:
+            submission = self.adsel.assign_majors(major_assign)
+        except Exception:
+            self.fail('assign_majors raised an exception')
+
+    def test_get_all_app(self):
+        apps = self.adsel.get_all_applications_by_qtr(0)
+        self.assertEqual(len(apps), 4)
+
+    def test_purple_gold_assignment(self):
+        a1 = PurpleGoldApplication()
+        a1.adsel_id = 123
+        a1.system_key = 41
+        a1.application_number = 1
+        a1.award_amount = 1000
+        a2 = PurpleGoldApplication()
+        a2.adsel_id = 734
+        a2.system_key = 34
+        a2.application_number = 5
+        a2.award_amount = 2000
+
+        purple_gold_assign = PurpleGoldAssignment()
+        purple_gold_assign.applicants = [a1, a2]
+        purple_gold_assign.assignment_type = "upload"
+        purple_gold_assign.quarter = 0
+        purple_gold_assign.campus = 1
+        purple_gold_assign.comments = "My comment"
+        purple_gold_assign.user = "javerage"
+
+        json_data = purple_gold_assign.json_data()
+        self.assertEqual(json_data['applicants'][0]['awardAmount'], 1000)
+
+        try:
+            submission = self.adsel.assign_purple_gold(purple_gold_assign)
+        except Exception:
+            self.fail('assign_purple_gold raised an exception')
+
+    def test_get_major_details(self):
+        client = AdSel()
+        major_details = client.get_major_details_by_qtr_major(0, "0_BIOL_1")
+        self.assertEqual(major_details.assigned_nonresident, 6)
+        self.assertEqual(major_details.assigned_international, 0)
+        self.assertEqual(major_details.assigned_resident, 12412)
