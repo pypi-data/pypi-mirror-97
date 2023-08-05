717dc7d1d222aea8fb2a9d887203cc760d05d17a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:03 +0000

    Add package calculation-mapilio/0.0.6/calculation_mapilio-0.0.6-py2.py3-none-any.whl

---
+++ packages/calculation-mapilio/calculation_mapilio-0.0.6-py2.py3-none-any.whl/calculation/area.py
@@ -0,0 +1,11 @@
+from geopy.distance import geodesic
+
+
+class Area:
+
+    # Deprecated
+    @staticmethod
+    def calc(interSection_A_lat, interSection_A_lon,
+             interSection_B_lat, interSection_B_lon,
+             interSection_C_lat, interSection_C_lon):
+        pass
