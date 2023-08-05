1835feb507bc2e8373e983fea2a0ff2626598d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:03 +0000

    Add package django-dynamic-model-validation/1.0.0/django_dynamic_model_validation-1.0.0-py3-none-any.whl

---
+++ packages/django-dynamic-model-validation/django_dynamic_model_validation-1.0.0-py3-none-any.whl/dynamic_validator/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for django-dynamic-model-validation."""
+
+__author__ = """Tonye Jack"""
+__email__ = "jtonye@ymail.com"
+__version__ = "1.0.0"
+
+from .field_validation import ModelFieldRequiredMixin
+
+__all__ = ["ModelFieldRequiredMixin"]
