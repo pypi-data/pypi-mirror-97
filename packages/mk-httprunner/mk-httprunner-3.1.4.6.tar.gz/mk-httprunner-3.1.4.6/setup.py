4e8cfb18389cbd8052906778f5618d755045194a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:40 +0000

    Add package mk-httprunner/3.1.4.6/mk-httprunner-3.1.4.6.tar.gz

---
+++ packages/mk-httprunner/mk-httprunner-3.1.4.6.tar.gz/mk-httprunner-3.1.4.6/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='mk-httprunner',
+    version='3.1.4.6',
+    description='httprunner by mk',
+    url='https://gitlab.mockuai.com/qa/mk_run.git',
+    author='fengqi',
+    author_email='',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    zip_safe=False)
