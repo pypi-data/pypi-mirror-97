34f520122aabcfd14903952c2c38f5c553d23741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:44 +0000

    Add package nestpy/1.4.7/nestpy-1.4.7-cp27-cp27mu-manylinux1_x86_64.whl

---
+++ packages/nestpy/nestpy-1.4.7-cp27-cp27mu-manylinux1_x86_64.whl/nestpy/RandomGen.hh
@@ -0,0 +1,41 @@
+//
+// RandomGen.hh
+//
+// Created by Jacob Cutter, May 10 2018
+
+#ifndef RANDOMGEN_HH
+#define RANDOMGEN_HH 1
+#include "xoroshiro.hh"
+#include <math.h>
+#include <stdlib.h>
+#include <random>
+#include <vector>
+
+using namespace std;
+
+class RandomGen {
+ public:
+  static RandomGen* rndm();
+  void SetSeed(uint64_t s);
+  double rand_uniform();
+  double rand_gauss(double mean, double sigma);
+  double rand_exponential(double half_life);
+  double rand_skewGauss( double xi, double omega, double alpha);
+  int poisson_draw(double mean);
+  int integer_range(int min, int max);
+  vector<double> VonNeumann(double xMin, double xMax, double yMin, double yMax,
+                            double xTest, double yTest, double fValue);
+  int SelectRanXeAtom();
+
+ private:
+  // Random number generator object for this class only
+  //  std::ranlux24 rng;
+  xoroshiro128plus64 rng;
+
+  RandomGen()= default;                // private so that it cannot be manually called
+  RandomGen(RandomGen const&);  // copy constructor is private
+  void operator=(RandomGen const&);  // assignment operator is private
+  static RandomGen* m_pInstance;     // private class pointer instance
+};
+
+#endif
