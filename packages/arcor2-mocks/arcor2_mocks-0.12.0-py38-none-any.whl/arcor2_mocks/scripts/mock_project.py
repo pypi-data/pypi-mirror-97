2ea6f0330c628e5b07789d3e712086ac4ae0bc5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:51 +0000

    Add package arcor2-mocks/0.12.0/arcor2_mocks-0.12.0-py38-none-any.whl

---
+++ packages/arcor2-mocks/arcor2_mocks-0.12.0-py38-none-any.whl/arcor2_mocks/scripts/mock_project.py
@@ -0,0 +1,638 @@
+#!/usr/bin/env python3
+
+import argparse
+from datetime import datetime, timezone
+from io import BytesIO
+from typing import Dict, Tuple
+
+import humps
+from flask import jsonify, request, send_file
+
+from arcor2.data import common, object_type
+from arcor2.flask import RespT, create_app, run_app
+from arcor2_mocks import PROJECT_PORT, PROJECT_SERVICE_NAME, version
+
+app = create_app(__name__)
+
+SCENES: Dict[str, common.Scene] = {}
+PROJECTS: Dict[str, common.Project] = {}
+OBJECT_TYPES: Dict[str, object_type.ObjectType] = {}
+
+BOXES: Dict[str, object_type.Box] = {}
+CYLINDERS: Dict[str, object_type.Cylinder] = {}
+SPHERES: Dict[str, object_type.Sphere] = {}
+
+MESHES: Dict[str, Tuple[BytesIO, str]] = {}
+
+
+@app.route("/models/<string:mesh_id>/mesh/file", methods=["PUT"])
+def put_mesh_file(mesh_id: str) -> RespT:
+    """Puts mesh file.
+    ---
+    put:
+        description: Puts mesh file.
+        tags:
+           - Models
+        parameters:
+            - name: mesh_id
+              in: path
+              description: unique ID
+              required: true
+              schema:
+                type: string
+        requestBody:
+              content:
+                multipart/form-data:
+                  schema:
+                    type: object
+                    required:
+                        - file
+                    properties:
+                      file:
+                        type: string
+                        format: binary
+        responses:
+            200:
+              description: Ok
+    """
+
+    buff = BytesIO()
+    fs = request.files["file"]
+    fs.save(buff)
+    MESHES[mesh_id] = buff, fs.filename
+    return "ok", 200
+
+
+@app.route("/models/<string:mesh_id>/mesh/file", methods=["GET"])
+def get_mesh_file(mesh_id: str) -> RespT:
+    """Gets mesh file by id.
+    ---
+    get:
+        tags:
+            - Models
+        summary: Gets mesh file by id.
+        parameters:
+            - name: mesh_id
+              in: path
+              description: unique ID
+              required: true
+              schema:
+                type: string
+        responses:
+            200:
+              description: Ok
+              content:
+                application/json:
+                    schema:
+                        type: string
+                        format: binary
+    """
+
+    mesh_file, filename = MESHES[mesh_id]
+    mesh_file.seek(0)
+    return send_file(mesh_file, as_attachment=True, cache_timeout=0, attachment_filename=filename)
+
+
+@app.route("/project", methods=["PUT"])
+def put_project() -> RespT:
+    """Add or update project.
+    ---
+    put:
+        tags:
+            - Project
+        description: Add or update project.
+        requestBody:
+              content:
+                application/json:
+                  schema:
+                    $ref: Project
+        responses:
+            200:
+              description: Ok
+    """
+
+    project = common.Project.from_dict(humps.decamelize(request.json))
+    project.modified = datetime.now(tz=timezone.utc)
+    project.int_modified = None
+    PROJECTS[project.id] = project
+    return jsonify(project.modified.isoformat())
+
+
+@app.route("/project/<string:id>", methods=["GET"])
+def get_project(id: str) -> RespT:
+    """Add or update project.
+    ---
+    get:
+        tags:
+            - Project
+        summary: Gets projet by project id.
+        parameters:
+            - name: id
+              in: path
+              description: unique ID
+              required: true
+              schema:
+                type: string
+        responses:
+            200:
+              description: Ok
+              content:
+                application/json:
+                    schema:
+                        $ref: Project
+    """
+
+    try:
+        return jsonify(PROJECTS[id].to_dict())
+    except KeyError:
+        return "Not found", 404
+
+
+@app.route("/project/<string:id>", methods=["DELETE"])
+def delete_project(id: str) -> RespT:
+    """Deletes project.
+    ---
+    delete:
+        tags:
+            - Project
+        summary: Deletes project.
+        parameters:
+            - name: id
+              in: path
+              description: unique ID
+              required: true
+              schema:
+                type: string
+        responses:
+            200:
+              description: Ok
+    """
+
+    try:
+        del PROJECTS[id]
+    except KeyError:
+        return "Not found", 404
+
+    return "ok", 200
+
+
+@app.route("/projects", methods=["GET"])
+def get_projects() -> RespT:
+    """Add or update project.
+    ---
+    get:
+        tags:
+        - Project
+        summary: Gets all projects id and description.
+        responses:
+            '200':
+              description: Success
+              content:
+                application/json:
+                  schema:
+                    $ref: IdDescList
+    """
+
+    ret = common.IdDescList()
+
+    for proj in PROJECTS.values():
+        ret.items.append(common.IdDesc(proj.id, proj.name, proj.desc))
+
+    return jsonify(ret.to_dict())
+
+
+@app.route("/scene", methods=["PUT"])
+def put_scene() -> RespT:
+    """Add or update scene.
+    ---
+    put:
+        tags:
+            - Scene
+        description: Add or update scene.
+        requestBody:
+              content:
+                application/json:
+                  schema:
+                    $ref: Scene
+        responses:
+            200:
+              description: Ok
+    """
+
+    scene = common.Scene.from_dict(humps.decamelize(request.json))
+    scene.modified = datetime.now(tz=timezone.utc)
+    scene.int_modified = None
+    SCENES[scene.id] = scene
+    return jsonify(scene.modified.isoformat())
+
+
+@app.route("/scene/<string:id>", methods=["GET"])
+def get_scene(id: str) -> RespT:
+    """Add or update scene.
+    ---
+    get:
+        tags:
+            - Scene
+        summary: Gets scene by project id.
+        parameters:
+            - name: id
+              in: path
+              description: unique ID
+              required: true
+              schema:
+                type: string
+        responses:
+            200:
+              description: Ok
+              content:
+                application/json:
+                    schema:
+                        $ref: Scene
+    """
+
+    try:
+        return jsonify(SCENES[id].to_dict())
+    except KeyError:
+        return "Not found", 404
+
+
+@app.route("/scene/<string:id>", methods=["DELETE"])
+def delete_scene(id: str) -> RespT:
+    """Deletes scene.
+    ---
+    delete:
+        tags:
+            - Scene
+        summary: Deletes scene.
+        parameters:
+            - name: id
+              in: path
+              description: unique ID
+              required: true
+              schema:
+                type: string
+        responses:
+            200:
+              description: Ok
+    """
+
+    try:
+        del SCENES[id]
+    except KeyError:
+        return "Not found", 404
+
+    return "ok", 200
+
+
+@app.route("/scenes", methods=["GET"])
+def get_scenes() -> RespT:
+    """Add or update scene.
+    ---
+    get:
+        tags:
+        - Scene
+        summary: Gets all scenes id and description.
+        responses:
+            '200':
+              description: Success
+              content:
+                application/json:
+                  schema:
+                    $ref: IdDescList
+    """
+
+    ret = common.IdDescList()
+
+    for scene in SCENES.values():
+        ret.items.append(common.IdDesc(scene.id, scene.name, scene.desc))
+
+    return jsonify(ret.to_dict())
+
+
+@app.route("/object_type", methods=["PUT"])
+def put_object_type() -> RespT:
+    """Add or update object type.
+    ---
+    put:
+        tags:
+            - ObjectType
+        description: Add or update object type.
+        requestBody:
+              content:
+                application/json:
+                  schema:
+                    $ref: ObjectType
+        responses:
+            200:
+              description: Ok
+    """
+
+    obj_type = object_type.ObjectType.from_dict(humps.decamelize(request.json))
+    OBJECT_TYPES[obj_type.id] = obj_type
+    return "ok", 200
+
+
+@app.route("/object_types/<string:id>", methods=["GET"])
+def get_object_type(id: str) -> RespT:
+    """Add or update object_type.
+    ---
+    get:
+        tags:
+            - ObjectType
+        summary: Gets object_type by project id.
+        parameters:
+            - name: id
+              in: path
+              description: unique ID
+              required: true
+              schema:
+                type: string
+        responses:
+            200:
+              description: Ok
+              content:
+                application/json:
+                    schema:
+                        $ref: ObjectType
+    """
+
+    try:
+        return jsonify(OBJECT_TYPES[id].to_dict())
+    except KeyError:
+        return "Not found", 404
+
+
+@app.route("/object_type/<string:id>", methods=["DELETE"])
+def delete_object_type(id: str) -> RespT:
+    """Deletes object type.
+    ---
+    delete:
+        tags:
+            - ObjectType
+        summary: Deletes object type.
+        parameters:
+            - name: id
+              in: path
+              description: unique ID
+              required: true
+              schema:
+                type: string
+        responses:
+            200:
+              description: Ok
+    """
+
+    try:
+        del OBJECT_TYPES[id]
+    except KeyError:
+        return "Not found", 404
+
+    return "ok", 200
+
+
+@app.route("/object_types", methods=["GET"])
+def get_object_types() -> RespT:
+    """Add or update ObjectType.
+    ---
+    get:
+        tags:
+        - ObjectType
+        summary: Gets all object types id and description.
+        responses:
+            '200':
+              description: Success
+              content:
+                application/json:
+                  schema:
+                    $ref: IdDescList
+    """
+
+    ret = common.IdDescList()
+
+    for obj_type in OBJECT_TYPES.values():
+        ret.items.append(common.IdDesc(obj_type.id, "", obj_type.desc))
+
+    return jsonify(ret.to_dict())
+
+
+@app.route("/models/box", methods=["PUT"])
+def put_box() -> RespT:
+    """Add or update box.
+    ---
+    put:
+        tags:
+            - Models
+        description: Add or update service type.
+        requestBody:
+              content:
+                application/json:
+                  schema:
+                    $ref: Box
+        responses:
+            200:
+              description: Ok
+    """
+
+    # box = object_type.Box.from_dict(humps.decamelize(request.json))  # TODO disabled because of bug in pyhumps
+    box = object_type.Box(request.json["id"], request.json["sizeX"], request.json["sizeY"], request.json["sizeZ"])
+    BOXES[box.id] = box
+    return "ok", 200
+
+
+@app.route("/models/<string:id>/box", methods=["GET"])
+def get_box(id: str) -> RespT:
+    """Add or update box.
+    ---
+    get:
+        tags:
+            - Models
+        summary: Gets ServiceType by service id.
+        parameters:
+            - name: id
+              in: path
+              description: unique ID
+              required: true
+              schema:
+                type: string
+        responses:
+            200:
+              description: Ok
+              content:
+                application/json:
+                    schema:
+                        $ref: Box
+    """
+
+    try:
+        return jsonify(BOXES[id].to_dict())
+    except KeyError:
+        return "Not found", 404
+
+
+@app.route("/models/cylinder", methods=["PUT"])
+def put_cylinder() -> RespT:
+    """Add or update cylinder.
+    ---
+    put:
+        tags:
+            - Models
+        description: Add or update service type.
+        requestBody:
+              content:
+                application/json:
+                  schema:
+                    $ref: Cylinder
+        responses:
+            200:
+              description: Ok
+    """
+
+    cylinder = object_type.Cylinder.from_dict(humps.decamelize(request.json))
+    CYLINDERS[cylinder.id] = cylinder
+    return "ok", 200
+
+
+@app.route("/models/<string:id>/cylinder", methods=["GET"])
+def get_cylinder(id: str) -> RespT:
+    """Add or update cylinder.
+    ---
+    get:
+        tags:
+            - Models
+        summary: Gets ServiceType by service id.
+        parameters:
+            - name: id
+              in: path
+              description: unique ID
+              required: true
+              schema:
+                type: string
+        responses:
+            200:
+              description: Ok
+              content:
+                application/json:
+                    schema:
+                        $ref: Cylinder
+    """
+
+    try:
+        return jsonify(CYLINDERS[id].to_dict())
+    except KeyError:
+        return "Not found", 404
+
+
+@app.route("/models/sphere", methods=["PUT"])
+def put_sphere() -> RespT:
+    """Add or update sphere.
+    ---
+    put:
+        tags:
+            - Models
+        description: Add or update sphere.
+        requestBody:
+              content:
+                application/json:
+                  schema:
+                    $ref: Sphere
+        responses:
+            200:
+              description: Ok
+    """
+
+    sphere = object_type.Sphere.from_dict(humps.decamelize(request.json))
+    SPHERES[sphere.id] = sphere
+    return "ok", 200
+
+
+@app.route("/models/<string:id>/sphere", methods=["GET"])
+def get_sphere(id: str) -> RespT:
+    """Add or update sphere.
+    ---
+    get:
+        tags:
+            - Models
+        summary: Gets sphere by id.
+        parameters:
+            - name: id
+              in: path
+              description: unique ID
+              required: true
+              schema:
+                type: string
+        responses:
+            200:
+              description: Ok
+              content:
+                application/json:
+                    schema:
+                        $ref: Sphere
+    """
+
+    try:
+        return jsonify(SPHERES[id].to_dict())
+    except KeyError:
+        return "Not found", 404
+
+
+@app.route("/models/<string:id>", methods=["DELETE"])
+def delete_model(id: str) -> RespT:
+    """Deletes model.
+    ---
+    delete:
+        tags:
+            - Models
+        summary: Deletes model.
+        parameters:
+            - name: id
+              in: path
+              description: unique ID
+              required: true
+              schema:
+                type: string
+        responses:
+            200:
+              description: Ok
+    """
+
+    try:
+        del BOXES[id]
+    except KeyError:
+        try:
+            del CYLINDERS[id]
+        except KeyError:
+            try:
+                del SPHERES[id]
+            except KeyError:
+                return "Not found", 404
+
+    return "ok", 200
+
+
+def main() -> None:
+
+    parser = argparse.ArgumentParser(description=PROJECT_SERVICE_NAME)
+    parser.add_argument("-s", "--swagger", action="store_true", default=False)
+    args = parser.parse_args()
+
+    run_app(
+        app,
+        PROJECT_SERVICE_NAME,
+        version(),
+        "0.4.0",
+        PROJECT_PORT,
+        [
+            common.Project,
+            common.Scene,
+            common.IdDescList,
+            object_type.ObjectType,
+            object_type.Box,
+            object_type.Cylinder,
+            object_type.Sphere,
+        ],
+        args.swagger,
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/arcor2-mocks/arcor2_mocks-0.12.0-py38-none-any.whl/arcor2_mocks/scripts/mock_scene.py
@@ -0,0 +1,315 @@
+#!/usr/bin/env python3
+
+import argparse
+import random
+import time
+from typing import Dict
+
+import humps
+from flask import jsonify, request
+
+from arcor2.data import common, object_type, scene
+from arcor2.flask import RespT, create_app, run_app
+from arcor2_mocks import SCENE_PORT, SCENE_SERVICE_NAME, version
+
+app = create_app(__name__)
+
+collision_objects: Dict[str, object_type.Models] = {}
+started: bool = False
+
+
+@app.route("/collisions/box", methods=["PUT"])
+def put_box() -> RespT:
+    """Add or update collision box.
+    ---
+    put:
+        tags:
+            - Collisions
+        description: Add or update collision box.
+        parameters:
+            - name: boxId
+              in: query
+              schema:
+                type: string
+            - name: sizeX
+              in: query
+              schema:
+                type: number
+                format: float
+            - name: sizeY
+              in: query
+              schema:
+                type: number
+                format: float
+            - name: sizeZ
+              in: query
+              schema:
+                type: number
+                format: float
+        requestBody:
+              content:
+                application/json:
+                  schema:
+                    $ref: Pose
+        responses:
+            200:
+              description: Ok
+    """
+
+    # TODO workarounded because of bug in pyhumps
+    # args = humps.decamelize(request.args.to_dict())
+    # box = object_type.Box(args["box_id"], float(args["size_x"]), float(args["size_y"]), float(args["size_z"]))
+
+    args = request.args.to_dict()
+    box = object_type.Box(args["boxId"], float(args["sizeX"]), float(args["sizeY"]), float(args["sizeZ"]))
+
+    collision_objects[box.id] = box
+    return "ok", 200
+
+
+@app.route("/collisions/sphere", methods=["PUT"])
+def put_sphere() -> RespT:
+    """Add or update collision sphere.
+    ---
+    put:
+        tags:
+            - Collisions
+        description: Add or update collision sphere.
+        parameters:
+            - name: sphereId
+              in: query
+              schema:
+                type: string
+            - name: radius
+              in: query
+              schema:
+                type: number
+                format: float
+        requestBody:
+              content:
+                application/json:
+                  schema:
+                    $ref: Pose
+        responses:
+            200:
+              description: Ok
+    """
+
+    args = humps.decamelize(request.args.to_dict())
+    sphere = object_type.Sphere(args["sphere_id"], float(args["radius"]))
+    collision_objects[sphere.id] = sphere
+    return "ok", 200
+
+
+@app.route("/collisions/cylinder", methods=["PUT"])
+def put_cylinder() -> RespT:
+    """Add or update collision cylinder.
+    ---
+    put:
+        tags:
+            - Collisions
+        description: Add or update collision cylinder.
+        parameters:
+            - name: cylinderId
+              in: query
+              schema:
+                type: string
+            - name: radius
+              in: query
+              schema:
+                type: number
+                format: float
+            - name: height
+              in: query
+              schema:
+                type: number
+                format: float
+        requestBody:
+              content:
+                application/json:
+                  schema:
+                    $ref: Pose
+        responses:
+            200:
+              description: Ok
+    """
+
+    args = humps.decamelize(request.args.to_dict())
+    cylinder = object_type.Cylinder(args["cylinder_id"], float(args["radius"]), float(args["height"]))
+    collision_objects[cylinder.id] = cylinder
+    return "ok", 200
+
+
+@app.route("/collisions/mesh", methods=["PUT"])
+def put_mesh() -> RespT:
+    """Add or update collision mesh.
+    ---
+    put:
+        tags:
+            - Collisions
+        description: Add or update collision mesh.
+        parameters:
+            - name: meshId
+              in: query
+              schema:
+                type: string
+            - name: uri
+              in: query
+              schema:
+                type: string
+            - name: meshScaleX
+              in: query
+              schema:
+                type: number
+                format: float
+                default: 1.0
+            - name: meshScaleY
+              in: query
+              schema:
+                type: number
+                format: float
+                default: 1.0
+            - name: meshScaleZ
+              in: query
+              schema:
+                type: number
+                format: float
+                default: 1.0
+        requestBody:
+              content:
+                application/json:
+                  schema:
+                    $ref: Pose
+        responses:
+            200:
+              description: Ok
+    """
+
+    args = humps.decamelize(request.args.to_dict())
+    mesh = object_type.Mesh(args["mesh_id"], args["uri"])
+    collision_objects[mesh.id] = mesh
+    return "ok", 200
+
+
+@app.route("/collisions/<string:collisionId>", methods=["DELETE"])
+def delete_collision(collisionId: str) -> RespT:
+    """Deletes collision object.
+    ---
+    delete:
+        tags:
+            - Collisions
+        summary: Deletes collision object.
+        parameters:
+            - name: collisionId
+              in: path
+              description: unique ID
+              required: true
+              schema:
+                type: string
+        responses:
+            200:
+              description: Ok
+    """
+
+    try:
+        del collision_objects[collisionId]
+    except KeyError:
+        return "Not found", 404
+
+    return "ok", 200
+
+
+@app.route("/collisions", methods=["GET"])
+def get_collisions() -> RespT:
+    """Gets collision ids.
+    ---
+    get:
+        tags:
+        - Collisions
+        summary: Gets collision ids.
+        responses:
+            '200':
+              description: Success
+              content:
+                application/json:
+                  schema:
+                    type: array
+                    items:
+                        type: string
+    """
+
+    return jsonify(list(collision_objects.keys()))
+
+
+@app.route("/utils/focus", methods=["PUT"])
+def put_focus() -> RespT:
+    """Calculates position of object.
+    ---
+    put:
+        tags:
+            - Utils
+        description: Calculates position of object.
+        requestBody:
+              content:
+                application/json:
+                  schema:
+                    $ref: MeshFocusAction
+        responses:
+            200:
+              description: Ok
+              content:
+                  application/json:
+                      schema:
+                        $ref: Pose
+    """
+
+    return jsonify(common.Pose().to_json())
+
+
+@app.route("/system/start", methods=["PUT"])
+def put_start() -> RespT:
+    global started
+    time.sleep(random.uniform(0.5, 5.0))
+    started = True
+    return "ok", 200
+
+
+@app.route("/system/stop", methods=["PUT"])
+def put_stop() -> RespT:
+    global started
+    time.sleep(random.uniform(0.5, 5.0))
+    started = False
+    return "ok", 200
+
+
+@app.route("/system/running", methods=["GET"])
+def get_started() -> RespT:
+    return jsonify(started)
+
+
+def main() -> None:
+
+    parser = argparse.ArgumentParser(description=SCENE_SERVICE_NAME)
+    parser.add_argument("-s", "--swagger", action="store_true", default=False)
+    args = parser.parse_args()
+
+    run_app(
+        app,
+        SCENE_SERVICE_NAME,
+        version(),
+        "0.3.0",
+        SCENE_PORT,
+        [
+            common.Pose,
+            object_type.Box,
+            object_type.Cylinder,
+            object_type.Sphere,
+            object_type.Mesh,
+            scene.MeshFocusAction,
+        ],
+        args.swagger,
+    )
+
+
+if __name__ == "__main__":
+    main()
