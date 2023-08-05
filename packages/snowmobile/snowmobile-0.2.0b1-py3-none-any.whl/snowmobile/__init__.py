6730f58713c51fb286c3daa5caee27da792e7edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:23 +0000

    Add package snowmobile/0.2.0b1/snowmobile-0.2.0b1-py3-none-any.whl

---
+++ packages/snowmobile/snowmobile-0.2.0b1-py3-none-any.whl/snowmobile/__init__.py
@@ -0,0 +1,43 @@
+"""
+An analytics-focused wrapper around the snowflake.connector for Python.
+"""
+# isort: skip_file
+# fmt: off
+__version__ = "0.2.0b1"
+__author__ = "Grant Murray"
+__application__ = "snowmobile"
+
+__all__ = [
+    # meta
+    "__version__",
+    "__author__",
+    "__application__",
+
+    # API
+    "Snowmobile", "connect",
+    "SQL",
+    "Table",
+    "Configuration",
+    "Script",
+    "Statement",
+    "export_config",
+]
+
+from .core import (
+    SQL,
+    Configuration,
+    Snowmobile, connect,
+    Table,
+    Script,
+    Section,
+    Statement,
+    utils,
+)
+
+from pathlib import Path
+from typing import Optional, Union
+
+
+def export_config(target_dir: Optional[Union[str, Path]] = None):
+    """Exports template `snowmobile.toml` file."""
+    return Configuration(export_dir=(target_dir or Path.cwd()))
