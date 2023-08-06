7eec4225622cf597af379bba7fc5e775a103e65b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:16 +0000

    Add package nbimporter/0.3.4/nbimporter-0.3.4.tar.gz

---
+++ packages/nbimporter/nbimporter-0.3.4.tar.gz/nbimporter-0.3.4/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='nbimporter',
+      version='0.3.4',
+      description='Import IPython notebooks as modules',
+      author='Gregor Sturm',
+      author_email='mail@gregor-sturm.de',
+      py_modules=['nbimporter', 'display_nb'],
+      license="BSD 3-clause",
+      url='https://github.com/grst/nbimporter',
+      setup_requires=["wheel", "setuptools"]
+ )
