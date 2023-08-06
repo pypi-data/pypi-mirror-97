2d7de9db6425155bf36be072ba0e114ae7be1901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:18 +0000

    Add package Distance-Converter/1.0/Distance_Converter-1.0-py3-none-any.whl

---
+++ packages/Distance-Converter/Distance_Converter-1.0-py3-none-any.whl/Distance Converter.py
@@ -0,0 +1,113 @@
+def dst_conv(m):
+    return m * 100
+
+
+print(dst_conv(int(input())))
+
+
+# m to cm
+
+def dst_conv(cm):
+    return cm / 100
+
+
+print(dst_conv(int(input())))
+
+
+# cm to m
+
+
+def dst_conv(km):
+    return km * 1000
+
+
+print(dst_conv(int(input())))
+
+
+# km to m
+
+
+def dst_conv(m):
+    return m / 1000
+
+
+print(dst_conv(int(input())))
+
+
+# m to km
+
+
+def dst_conv(cm):
+    return cm * 10
+
+
+print(dst_conv(int(input())))
+
+
+# cm to mm
+
+
+def dst_conv(mm):
+    return mm / 10
+
+
+print(dst_conv(int(input())))
+
+
+# mm to cm
+
+
+def dst_conv(km):
+    return km * 100000
+
+
+print(dst_conv(int(input())))
+
+
+# km to cm
+
+def dst_conv(cm):
+    return cm / 100000
+
+
+print(dst_conv(int(input())))
+
+
+# cm to km
+
+
+def dst_conv(km):
+    return km * 1000000
+
+
+print(dst_conv(int(input())))
+
+
+# km to mm
+
+
+def dst_conv(mm):
+    return mm / 1000000
+
+
+print(dst_conv(int(input())))
+
+
+# mm to km
+
+
+def dst_conv(m):
+    return m * 1000
+
+
+print(dst_conv(int(input())))
+
+
+# m to mm
+
+def dst_conv(mm):
+    return mm / 1000
+
+
+print(dst_conv(int(input())))
+# mm to m
