07b7bf5a35e7949b25571dd82cf03a81351cd3f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:49 +0000

    Add package djangoldp-contact/2.1.1/djangoldp_contact-2.1.1.tar.gz

---
+++ packages/djangoldp-contact/djangoldp_contact-2.1.1.tar.gz/djangoldp_contact-2.1.1/README.md
@@ -0,0 +1,2 @@
+# DjangoLDP Contact
+
---
+++ packages/djangoldp-contact/djangoldp_contact-2.1.1.tar.gz/djangoldp_contact-2.1.1/djangoldp_contact/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+from djangoldp_contact.models import Contact
+
+class DjangoldpContactConfig(AppConfig):
+    name = 'djangoldp_contact'
+
