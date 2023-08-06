87e9272379b8094cff648ea18226dc7f1bd31535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:01 +0000

    Add package knopfler/0.1.7/knopfler-0.1.7.tar.gz

---
+++ packages/knopfler/knopfler-0.1.7.tar.gz/knopfler-0.1.7/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['knopfler']
+install_requires = \
+['sanic']
+
+extras_require = \
+{'matrix': ['matrix_client'], 'rocketchat': ['RocketChatAPIBot']}
+
+entry_points = \
+{'console_scripts': ['knopfler = knopfler:main']}
+
+setup_kwargs = {
+    'name': 'knopfler',
+    'version': '0.1.7',
+    'description': '',
+    'long_description': None,
+    'author': 'Andreas Nüßlein',
+    'author_email': 'andreas@nuessle.in',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
