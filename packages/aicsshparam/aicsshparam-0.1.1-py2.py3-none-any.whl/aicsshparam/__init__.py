0c127af59c814eb245c243d03dd9b0eece3a93f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:05 +0000

    Add package aicsshparam/0.1.1/aicsshparam-0.1.1-py2.py3-none-any.whl

---
+++ packages/aicsshparam/aicsshparam-0.1.1-py2.py3-none-any.whl/aicsshparam/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for aics-shparam."""
+
+__author__ = "Matheus Viana"
+__email__ = "matheus.viana@alleninstitute.org"
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.1.1"
+
+
+def get_module_version():
+    return __version__
