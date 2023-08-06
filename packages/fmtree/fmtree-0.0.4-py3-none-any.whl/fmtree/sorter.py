817eae208f9c20954991b816d18177c1ee286a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:45 +0000

    Add package fmtree/0.0.4/fmtree-0.0.4-py3-none-any.whl

---
+++ packages/fmtree/fmtree-0.0.4-py3-none-any.whl/fmtree/sorter.py
@@ -0,0 +1,44 @@
+import copy
+from typing import Iterable, List
+from abc import ABC, abstractmethod
+
+from fmtree.node import FileNode
+
+
+class BaseSorter(ABC):
+    """
+    Sorter Base Class for sorting child nodes
+    """
+    def run(self, root_node: FileNode) -> FileNode:
+        """
+        Traverse through the tree and run sorting algorithm implemented by child class
+        :param root_node: root node
+        :return: another tree root node
+        """
+        tree = copy.deepcopy(root_node)
+
+        def traverse(node: FileNode):
+            if node.get_children():
+                node.set_children(self.sorted(node.get_children()))
+            for child in node.get_children():
+                traverse(child)
+
+        traverse(tree)
+        return tree
+
+    @abstractmethod
+    def sorted(self, nodes: List[FileNode]) -> List[FileNode]:
+        """
+        Abstract method for sorting children nodes
+        :param nodes: a list of nodes which is children of another node
+        :return: a list of sorted nodes
+        """
+        raise NotImplementedError
+
+    def __call__(self, node: FileNode) -> FileNode:
+        return self.run(node)
+
+
+class Sorter(BaseSorter):
+    def sorted(self, nodes: List[FileNode]) -> Iterable:
+        return sorted(nodes, key=lambda node: node.get_filename())
