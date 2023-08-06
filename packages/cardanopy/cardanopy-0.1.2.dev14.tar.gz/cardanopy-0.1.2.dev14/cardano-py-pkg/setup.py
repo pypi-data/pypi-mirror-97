efb24174fc20d567d41fec6eeb33c67108fd3c2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:19 +0000

    Add package cardanopy/0.1.2.dev14/cardanopy-0.1.2.dev14.tar.gz

---
+++ packages/cardanopy/cardanopy-0.1.2.dev14.tar.gz/cardanopy-0.1.2.dev14/cardano-py-pkg/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='cardanopy',
+      version='0.1.2-dev14',
+      description='Cardano CLI tools for python3',
+      author='Bourke Floyd',
+      author_email='chbfiv@floydcraft.com',
+      url='https://github.com/floydcraft/cardano-py',
+      packages=find_packages(),
+      keywords='cardano,ada,cli',
+      python_requires='>=3.5.3,<4',
+      install_requires=[
+        'click==7.0'
+      ],
+      entry_points={
+          'console_scripts': [
+              'cardanopy=cardanopy:main',
+          ],
+      },
+      project_urls={
+          'Bug Reports': 'https://github.com/floydcraft/cardano-py/issues',
+          'Chat': 'https://discord.gg/FyDz4Xrt4x',
+          'Source': 'https://github.com/floydcraft/cardano-py',
+      }
+)
