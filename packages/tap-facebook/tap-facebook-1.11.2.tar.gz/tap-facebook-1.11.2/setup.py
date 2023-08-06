908cda75e0cf25e227445205a658c1dbfe8ab63b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:35 +0000

    Add package tap-facebook/1.11.2/tap-facebook-1.11.2.tar.gz

---
+++ packages/tap-facebook/tap-facebook-1.11.2.tar.gz/tap-facebook-1.11.2/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='tap-facebook',
+      version='1.11.2',
+      description='Singer.io tap for extracting data from the Facebook Ads API',
+      author='Stitch',
+      url='https://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_facebook'],
+      install_requires=[
+          'attrs==16.3.0',
+          'backoff==1.8.0',
+          'pendulum==1.2.0',
+          'facebook_business==9.0.0',
+          'requests==2.20.0',
+          'singer-python==5.10.0',
+      ],
+      extras_require={
+          'dev': [
+              'pylint',
+              'ipdb',
+              'nose',
+          ]
+      },
+      entry_points='''
+          [console_scripts]
+          tap-facebook=tap_facebook:main
+      ''',
+      packages=['tap_facebook'],
+      package_data = {
+          'tap_facebook/schemas': [
+              # add schema.json filenames here
+          ],
+          'tap_facebook/schemas/shared': [
+          ]
+      },
+      include_package_data=True,
+)
