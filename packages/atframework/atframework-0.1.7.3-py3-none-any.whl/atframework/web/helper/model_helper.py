7589354713f07a719ee00679a321e37bdab2a88c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:59 +0000

    Add package atframework/0.1.7.3/atframework-0.1.7.3-py3-none-any.whl

---
+++ packages/atframework/atframework-0.1.7.3-py3-none-any.whl/atframework/web/helper/model_helper.py
@@ -0,0 +1,17 @@
+'''
+Created on Mar 03, 2021
+
+@author: Siro
+
+'''
+
+from atframework.web.common.model.closing import Closing
+from atframework.web.common.model.opening import Opening
+from atframework.web.common.model.waiting import Waiting
+from atframework.web.common.model.bo_login import BoLogin
+
+
+class ModelHelper(Closing, Opening, Waiting, BoLogin):
+    '''
+    Integrate all model to this class, Use this class to drive test steps
+    '''
