57832778d723107a0c0694ae9cc2cf4ee84afb84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:34 +0000

    Add package fox-orm/0.2.2/fox-orm-0.2.2.tar.gz

---
+++ packages/fox-orm/fox-orm-0.2.2.tar.gz/fox-orm-0.2.2/fox_orm/model.py
@@ -0,0 +1,235 @@
+from typing import Union, Mapping, TYPE_CHECKING, Dict, Any, TypeVar, List, Type
+
+from pydantic import BaseModel
+from pydantic.main import ModelMetaclass
+from sqlalchemy import select, func, Table, exists
+
+from fox_orm import FoxOrm
+from fox_orm.exceptions import OrmException
+from fox_orm.relations import _GenericIterableRelation
+from fox_orm.utils import validate_model, class_or_instancemethod
+
+if TYPE_CHECKING:
+    # pylint: disable=no-name-in-module,ungrouped-imports
+    from pydantic.typing import MappingIntStrAny, AbstractSetIntStr, TupleGenerator, ReprArgs
+
+EXCLUDE_KEYS = {'__modified__', '__bound__', '__exclude__'}
+
+MODEL = TypeVar('MODEL', bound='OrmModel')
+
+
+class OrmModelMeta(ModelMetaclass):
+    __sqla_table__: Table
+
+    def _ensure_proper_init(cls):
+        if not hasattr(cls, '__sqla_table__'):
+            raise OrmException('__sqla_table__ must be set')
+        if not isinstance(cls.__sqla_table__, Table):
+            raise OrmException('__sqla_table__ type should be sqlalchemy.Table')
+
+    def __new__(mcs, name, bases, namespace, **kwargs):
+        if name == 'OrmModel':
+            return super().__new__(mcs, name, bases, namespace, **kwargs)
+        new_namespace = {}
+        relation_namespace = {}
+        exclude = set()
+        for k, v in namespace.items():
+            if isinstance(v, _GenericIterableRelation):
+                relation_namespace[k] = v
+                exclude.add(k)
+            else:
+                new_namespace[k] = v
+        new_namespace['__annotations__'] = {}
+        for k, v in namespace['__annotations__'].items():
+            if k not in exclude:
+                new_namespace['__annotations__'][k] = v
+        cls = super().__new__(mcs, name, bases, new_namespace, **kwargs)
+        cls._ensure_proper_init()
+        cls.c = cls.__sqla_table__.c
+        cls.__exclude__ = EXCLUDE_KEYS.copy() | exclude | {'id'}
+        cls.__relations__ = relation_namespace
+        return cls
+
+
+class OrmModel(BaseModel, metaclass=OrmModelMeta):
+    class Config:
+        validate_assignment = True
+
+    # cls attrs
+    __private_attributes__: Dict[str, Any]
+    __sqla_table__: Table
+    __exclude__: set
+
+    # instance attrs
+    __modified__: set
+    __bound__: bool
+
+    def __repr_args__(self) -> 'ReprArgs':
+        return [(k, v) for k, v in self.__dict__.items() if k not in self.__exclude__]
+
+    def _init_private_attributes(self):
+        self.__modified__ = set()
+        self.__bound__ = False
+        for k, v in self.__relations__.items():
+            self.__dict__[k] = v._init_copy(self)  # pylint: disable=protected-access
+        super()._init_private_attributes()
+
+    def __init__(self, **data: Any) -> None:  # pylint: disable=super-init-not-called
+        values, fields_set, validation_error = validate_model(self.__class__, data)
+        if validation_error:
+            raise validation_error  # pylint: disable=raising-bad-type
+        object.__setattr__(self, '__dict__', values)
+        object.__setattr__(self, '__fields_set__', fields_set)
+        self._init_private_attributes()
+
+    # pylint: disable=unsubscriptable-object, too-many-arguments
+    def _iter(self, to_dict: bool = False, by_alias: bool = False,
+              include: Union['AbstractSetIntStr', 'MappingIntStrAny'] = None,
+              exclude: Union['AbstractSetIntStr', 'MappingIntStrAny'] = None, exclude_unset: bool = False,
+              exclude_defaults: bool = False, exclude_none: bool = False) -> 'TupleGenerator':
+        exclude_private = self.__exclude__ - {'id'} | EXCLUDE_KEYS
+        if exclude is None:
+            exclude = exclude_private
+        elif isinstance(exclude, Mapping):  # pylint: disable=isinstance-second-argument-not-valid-type
+            exclude = dict(exclude)
+            exclude.update({k: ... for k in exclude_private})
+        else:
+            exclude |= exclude_private
+        return super()._iter(to_dict, by_alias, include, exclude, exclude_unset, exclude_defaults, exclude_none)
+
+    def __setattr__(self, name, value):
+        if name == 'id':
+            if self.__bound__:
+                raise OrmException('Can not modify id')
+            return super().__setattr__(name, value)
+        if name in EXCLUDE_KEYS:
+            return object.__setattr__(self, name, value)
+        if name not in self.__private_attributes__:
+            self.flag_modified(name)
+        return super().__setattr__(name, value)
+
+    @classmethod
+    def construct(cls, values):  # pylint: disable=arguments-differ
+        m = cls.__new__(cls)
+        fields_values = {name: field.get_default() for name, field in cls.__fields__.items() if
+                         not field.required and field.name not in cls.__exclude__}
+        fields_values.update(values)
+        object.__setattr__(m, '__dict__', fields_values)
+        object.__setattr__(m, '__fields_set__', set(values.keys()))
+        m._init_private_attributes()  # pylint: disable=protected-access
+        return m
+
+    def flag_modified(self, attr):
+        self.__modified__.add(attr)
+
+    def ensure_id(self):
+        if not self.__bound__:
+            raise OrmException('Object is not bound to db, execute insert first')
+        if getattr(self, 'id', None) is None:
+            raise OrmException('id must be set')
+
+    async def save(self):
+        if self.__bound__:
+            self.ensure_id()
+            if not self.__modified__:
+                return
+            table = self.__class__.__sqla_table__
+            fields = {k: getattr(self, k) for k in self.__modified__}
+            # pylint: disable=access-member-before-definition
+            await FoxOrm.db.execute(table.update().where(table.c.id == self.id), fields)
+            self.__modified__ = set()
+        else:
+            table = self.__class__.__sqla_table__
+            data = self.dict(exclude={'id'})
+            if len(data) == 0:
+                data['id'] = None
+            self.id = await FoxOrm.db.execute(table.insert(), data)  # pylint: disable=attribute-defined-outside-init
+            self.__bound__ = True
+
+    @classmethod
+    def _generate_query(cls, where, order_by, limit, offset):
+        query = cls.__sqla_table__.select()
+        if where is not None:
+            query = query.where(where)
+        if order_by is not None:
+            if not isinstance(order_by, list):
+                order_by = [order_by]
+            for i in order_by:
+                query = query.order_by(i)
+        if limit is not None:
+            query = query.limit(limit)
+        if offset is not None:
+            query = query.offset(offset)
+        return query
+
+    @classmethod
+    async def select(cls: Type[MODEL], where, *, order_by=None, skip_parsing=False) -> MODEL:
+        construct_func = cls.construct if skip_parsing else cls.parse_obj
+        res = await FoxOrm.db.fetch_one(cls._generate_query(where, order_by, None, None))
+        if not res:
+            return None
+        res = construct_func(res)
+        res.__bound__ = True
+        return res
+
+    @classmethod
+    async def select_all(cls: Type[MODEL], where, *, order_by=None, limit=None, offset=None, skip_parsing=False) -> \
+            List[MODEL]:
+        construct_func = cls.construct if skip_parsing else cls.parse_obj
+        q_res = await FoxOrm.db.fetch_all(cls._generate_query(where, order_by, limit, offset))
+        res = []
+        for x in q_res:
+            res.append(construct_func(x))
+            res[-1].__bound__ = True
+        return res
+
+    @classmethod
+    async def exists(cls: Type[MODEL], where) -> bool:
+        query = cls._generate_query(where, None, None, None)
+        query = exists(query).select()
+        return await FoxOrm.db.fetch_val(query)
+
+    @classmethod
+    async def _delete_cls(cls, where):
+        query = cls.__sqla_table__.delete().where(where)
+        await FoxOrm.db.execute(query)
+
+    async def _delete_inst(self):
+        self.ensure_id()
+        table = self.__class__.__sqla_table__
+        query = table.delete().where(table.c.id == self.id)
+        self.__bound__ = False
+        await FoxOrm.db.execute(query)
+
+    # pylint: disable=bad-classmethod-argument,no-else-return
+    @class_or_instancemethod
+    async def delete(self_or_cls, *args, **kwargs) -> None:
+        if isinstance(self_or_cls, type):
+            return await self_or_cls._delete_cls(*args, **kwargs)
+        else:
+            return await self_or_cls._delete_inst(*args, **kwargs)
+
+    @classmethod
+    async def count(cls: Type[MODEL], where) -> int:
+        query = select([func.count()]).select_from(cls.__sqla_table__)
+        if where is not None:
+            query = query.where(where)
+        return await FoxOrm.db.fetch_val(query)
+
+    @classmethod
+    async def get(cls: Type[MODEL], obj_id: int, skip_parsing=False) -> MODEL:
+        return await cls.select(cls.__sqla_table__.c.id == obj_id, skip_parsing=skip_parsing)
+
+    async def _fetch_related(self, field: str):
+        self.ensure_id()
+        relation: _GenericIterableRelation = getattr(self, field)
+        if not isinstance(relation, _GenericIterableRelation):
+            raise OrmException('fetch_related argument is not a relation')
+        await relation.fetch()
+
+    async def fetch_related(self, *fields: str) -> None:
+        for field in fields:
+            await self._fetch_related(field)
+
+
+__all__ = ['OrmModel']
---
+++ packages/fox-orm/fox-orm-0.2.2.tar.gz/fox-orm-0.2.2/fox_orm/utils.py
@@ -0,0 +1,133 @@
+from typing import Type, Tuple, Optional, TYPE_CHECKING
+
+from pydantic import ValidationError, Extra, ConfigError, ExtraError, MissingError
+from pydantic.error_wrappers import ErrorWrapper
+from pydantic.typing import ForwardRef
+from pydantic.utils import ROOT_KEY, GetterDict
+
+if TYPE_CHECKING:
+    from fox_orm.model import OrmModel
+    from pydantic.types import ModelOrDc  # pylint: disable=no-name-in-module,ungrouped-imports
+    from pydantic.typing import DictStrAny, SetStr  # pylint: disable=no-name-in-module,ungrouped-imports
+
+
+def full_import(name):
+    components = name.split('.')
+    mod = __import__(components[0])
+    for comp in components[1:]:
+        mod = getattr(mod, comp)
+    return mod
+
+
+class OptionalAwaitable:
+    def __init__(self, func, *args, **kwargs):
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+
+    def __await__(self):
+        return self.func(*self.args, **self.kwargs).__await__()
+
+
+_missing = object()
+
+
+def validate_model(
+        model: Type['OrmModel'], input_data: 'DictStrAny', cls: 'ModelOrDc' = None
+) -> Tuple['DictStrAny', 'SetStr', Optional[ValidationError]]:
+    # pylint: disable=R,E
+    """
+    validate data against a model.
+    """
+    values = {}
+    errors = []
+    # input_data names, possibly alias
+    names_used = set()
+    # field names, never aliases
+    fields_set = set()
+    config = model.__config__
+    check_extra = config.extra is not Extra.ignore
+    cls_ = cls or model
+
+    for validator in model.__pre_root_validators__:
+        try:
+            input_data = validator(cls_, input_data)
+        except (ValueError, TypeError, AssertionError) as exc:
+            return {}, set(), ValidationError([ErrorWrapper(exc, loc=ROOT_KEY)], cls_)
+
+    for name, field in model.__fields__.items():
+        if name in model.__exclude__ and name != 'id':
+            values[name] = field.default
+            continue
+        if field.type_.__class__ == ForwardRef:
+            raise ConfigError(
+                f'field "{field.name}" not yet prepared so type is still a ForwardRef, '
+                f'you might need to call {cls_.__name__}.update_forward_refs().'
+            )
+
+        value = input_data.get(field.alias, _missing)
+        using_name = False
+        if value is _missing and config.allow_population_by_field_name and field.alt_alias:
+            value = input_data.get(field.name, _missing)
+            using_name = True
+
+        if value is _missing:
+            if field.required:
+                errors.append(ErrorWrapper(MissingError(), loc=field.alias))
+                continue
+
+            value = field.get_default()
+
+            if not config.validate_all and not field.validate_always:
+                values[name] = value
+                continue
+        else:
+            fields_set.add(name)
+            if check_extra:
+                names_used.add(field.name if using_name else field.alias)
+
+        v_, errors_ = field.validate(value, values, loc=field.alias, cls=cls_)
+        if isinstance(errors_, ErrorWrapper):
+            errors.append(errors_)
+        elif isinstance(errors_, list):
+            errors.extend(errors_)
+        else:
+            values[name] = v_
+
+    if check_extra:
+        if isinstance(input_data, GetterDict):
+            extra = input_data.extra_keys() - names_used
+        else:
+            extra = input_data.keys() - names_used
+        if extra:
+            fields_set |= extra
+            if config.extra is Extra.allow:
+                for f in extra:
+                    values[f] = input_data[f]
+            else:
+                for f in sorted(extra):
+                    errors.append(ErrorWrapper(ExtraError(), loc=f))
+
+    for skip_on_failure, validator in model.__post_root_validators__:
+        if skip_on_failure and errors:
+            continue
+        try:
+            values = validator(cls_, values)
+        except (ValueError, TypeError, AssertionError) as exc:
+            errors.append(ErrorWrapper(exc, loc=ROOT_KEY))
+
+    if errors:
+        return values, fields_set, ValidationError(errors, cls_)
+    else:
+        return values, fields_set, None
+
+
+# noinspection PyMethodOverriding,PyArgumentList,PyUnresolvedReferences,PyPep8Naming
+# pylint: disable=invalid-name,no-member
+class class_or_instancemethod(classmethod):
+    def __get__(self, instance, type_):
+        descr_get = super().__get__ if instance is None else self.__func__.__get__
+        return descr_get(instance, type_)
+
+
+__all__ = ['full_import', 'OptionalAwaitable', 'validate_model', 'class_or_instancemethod']
