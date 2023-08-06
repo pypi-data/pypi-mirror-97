fdf124f153fc1f263600a8d70e380182327aff89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:40 +0000

    Add package troposphere/2.6.4/troposphere-2.6.4.tar.gz

---
+++ packages/troposphere/troposphere-2.6.4.tar.gz/troposphere-2.6.4/troposphere/ecr.py
@@ -0,0 +1,22 @@
+from . import AWSObject, AWSProperty, Tags
+from .compat import policytypes
+
+
+class LifecyclePolicy(AWSProperty):
+    props = {
+        'LifecyclePolicyText': (basestring, False),
+        'RegistryId': (basestring, False),
+    }
+
+
+class Repository(AWSObject):
+    resource_type = "AWS::ECR::Repository"
+
+    props = {
+        'ImageScanningConfiguration': (dict, False),
+        'ImageTagMutability': (basestring, False),
+        'LifecyclePolicy': (LifecyclePolicy, False),
+        'RepositoryName': (basestring, False),
+        'RepositoryPolicyText': (policytypes, False),
+        'Tags': (Tags, False),
+    }
