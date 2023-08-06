7de6b8b47dc641af463a846b6fe1895df20fb5ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:00 +0000

    Add package soapberry/0.0.13/soapberry-0.0.13-py3-none-any.whl

---
+++ packages/soapberry/soapberry-0.0.13-py3-none-any.whl/soapberry/helpers.py
@@ -0,0 +1,11 @@
+import json
+import sys
+
+def print_log(message):
+    print(message.toString(), flush=True)
+
+def print_error(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+def dump_json(results):
+    return json.dumps([ob.__dict__ for ob in results])
