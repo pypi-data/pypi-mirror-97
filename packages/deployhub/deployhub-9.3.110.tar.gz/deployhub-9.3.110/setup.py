65918da7cc6f02e1dd9d58c6d96dae0bdd202c90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:16 +0000

    Add package deployhub/9.3.110/deployhub-9.3.110.tar.gz

---
+++ packages/deployhub/deployhub-9.3.110.tar.gz/deployhub-9.3.110/setup.py
@@ -0,0 +1,26 @@
+"""Setup config for the module."""
+
+from distutils.core import setup
+import setuptools
+
+setup(
+    setup_requires=['wheel'],
+    url='https://www.deployhub.com',
+    author='Steve Taylor',
+    author_email='steve@deployhub.com',
+    name='deployhub',
+    version='9.3.110',
+    packages=['deployhub',],
+    scripts=['bin/dh'],
+    license='GNU AFFERO GENERAL PUBLIC LICENSE',
+    long_description=open('README').read(),
+    python_requires='>=3.6',
+     install_requires=[
+        'click',
+        'qtoml',
+        'PyYAML',
+        'requests',
+        'configobj',
+        'flatten_dict'
+        ],
+)
