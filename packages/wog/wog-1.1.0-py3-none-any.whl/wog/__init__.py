2f2d24e2c0dfd7eb149dc0c4fc75d78ad45a73d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:22 +0000

    Add package wog/1.1.0/wog-1.1.0-py3-none-any.whl

---
+++ packages/wog/wog-1.1.0-py3-none-any.whl/wog/__init__.py
@@ -0,0 +1,11 @@
+"""World Of Games
+
+Play some little games, have fun :)
+
+
+To run the game, just write:
+wog.MainGame.start_game()
+after you imported the package.
+"""
+from . import MainGame
+
