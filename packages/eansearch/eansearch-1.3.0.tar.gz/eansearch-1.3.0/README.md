900eff2c89c1150d8c15ec877ec7128b9f58d47c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:25 +0000

    Add package eansearch/1.3.0/eansearch-1.3.0.tar.gz

---
+++ packages/eansearch/eansearch-1.3.0.tar.gz/eansearch-1.3.0/README.md
@@ -0,0 +1,37 @@
+# EANSearch
+
+A Python class for EAN and ISBN name lookup and validation using the API on https://www.ean-search.org
+
+Compatible with Python 2.x **and** 3.x
+
+```python
+from eansearch import EANSearch
+
+# get a token from https://www.ean-search.org/ean-database-api.html
+apiToken = "secret"
+ean = "5099750442227" # Thriller
+
+eansearch = EANSearch(apiToken)
+
+name = eansearch.barcodeLookup(ean)
+print(ean, " is ", name)
+
+# more detailed result, preferrably in English (1)
+product = eansearch.barcodeSearch(ean, 1)
+print(ean, " is ", product["name"].encode("utf-8"), " from category ", product["categoryName"], " issued in ", product["issuingCountry"])
+
+ok = eansearch.verifyChecksum(ean)
+print(ean, " is ", "OK" if ok else "Not OK")
+
+eanList = eansearch.productSearch('iPod');
+for product in eanList:
+	print(product["ean"], " is ", product["name"].encode("utf-8"))
+
+eanList = eansearch.categorySearch(45, 'thriller');
+for product in eanList:
+	print(product["ean"], " is ", product["name"].encode("utf-8"))
+
+eanList = eansearch.barcodePrefixSearch('4007249146');
+for product in eanList:
+	print(product["ean"], " is ", product["name"].encode("utf-8"))
+
---
+++ packages/eansearch/eansearch-1.3.0.tar.gz/eansearch-1.3.0/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='eansearch',
+      description='A Python class for EAN and ISBN name lookup and validation using the API on ean-search.org',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      version='1.3.0',
+      url='https://github.com/eansearch/python-ean-search',
+      author='Jan Willamowius',
+      author_email='info@relaxedcommunications.com',
+      keywords = 'barcode barcodes ean ean13 ean-13 upc gtin isbn13 isbn-13 search lookup find valid validation',
+      packages=setuptools.find_packages(),
+      py_modules=['eansearch'],
+      classifiers=[
+          'License :: OSI Approved',
+          'License :: OSI Approved :: MIT License',
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Other Audience',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Operating System :: OS Independent',
+          'Operating System :: MacOS',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Operating System :: Unix',
+          'Programming Language :: Python',
+          'Topic :: Office/Business :: Financial :: Point-Of-Sale',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      python_requires='>=2.0',
+)
+
