a2e28184f0bc2a2adff0b45d4dc7d07924d956aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:05 +0000

    Add package tiny-python-toolkit/1.0.0/tiny_python_toolkit-1.0.0.tar.gz

---
+++ packages/tiny-python-toolkit/tiny_python_toolkit-1.0.0.tar.gz/tiny_python_toolkit-1.0.0/tpt/visualize.py
@@ -0,0 +1,33 @@
+r"""A module for data visualization."""
+
+def print_tr(data: dict, offset: Optional[int]=1) -> None:
+    r"""Triverse entire dictionary.
+
+    Parameters
+    ==========
+    data: dict
+        The data wanted to be triversed.
+    offset: Optional[int], default: 1
+        The offset for the indentation when displaying.
+    """
+    if data is None: return None
+
+    if type(data) is dict:
+        for key, val in data.items():
+            if val is None:
+                print(f"{' '*offset*4}{key}: None")
+            elif type(val) is list:
+                print(f"{' '*offset*4}{key}: (List)")
+                triverse_dict(val, offset+1)
+            elif type(val) is dict:
+                print(f"{' '*offset*4}{key}: (Dict)")
+                triverse_dict(val, offset+1)
+            else:
+                print(f"{' '*offset*4}{key}: {val}")
+
+    elif type(data) is list:
+        for ele in data:
+            triverse_dict(ele, offset)
+
+    else:
+        print(f"{'    '*offset}- {data}")
