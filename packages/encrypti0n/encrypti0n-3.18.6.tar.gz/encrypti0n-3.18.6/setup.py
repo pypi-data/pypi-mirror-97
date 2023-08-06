741c9eb90563c8f5ba8f33f790a183f3c2bc57e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:34 +0000

    Add package encrypti0n/3.18.6/encrypti0n-3.18.6.tar.gz

---
+++ packages/encrypti0n/encrypti0n-3.18.6.tar.gz/encrypti0n-3.18.6/setup.py
@@ -0,0 +1,21 @@
+# source: https://python-packaging.readthedocs.io/en/latest/minimal.html
+from setuptools import setup, find_packages
+name='encrypti0n'
+setup(
+	name=name,
+	version='3.18.6',
+	description='Easily encrypt & decrypt files with python / through the CLI.',
+	url=f'http://github.com/vandenberghinc/{name}',
+	author='Daan van den Bergh',
+	author_email='vandenberghinc.contact@gmail.com',
+	license='MIT',
+	packages=find_packages(),
+	zip_safe=False,
+	install_requires=[
+        'certifi',
+        'chardet',
+        'idna',
+        'pycryptodome',
+        'requests',
+        'urllib3',
+    ])
