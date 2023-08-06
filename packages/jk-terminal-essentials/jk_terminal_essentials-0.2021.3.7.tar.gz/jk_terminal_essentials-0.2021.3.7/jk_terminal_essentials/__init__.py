2064077967de6bd250a832d19426cfdf0fb7a91c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:47 +0000

    Add package jk-terminal-essentials/0.2021.3.7/jk_terminal_essentials-0.2021.3.7.tar.gz

---
+++ packages/jk-terminal-essentials/jk_terminal_essentials-0.2021.3.7.tar.gz/jk_terminal_essentials-0.2021.3.7/jk_terminal_essentials/__init__.py
@@ -0,0 +1,35 @@
+﻿
+
+
+__version__ = "0.2021.3.7"
+
+
+
+
+FGCOLOR_BLACK = "\x1b[30m"
+FGCOLOR_RED = "\x1b[31m"
+FGCOLOR_GREEN = "\x1b[32m"
+FGCOLOR_YELLOW = "\x1b[33m"
+FGCOLOR_BLUE = "\x1b[34m"
+FGCOLOR_MAGENTA = "\x1b[35m"
+FGCOLOR_CYAN = "\x1b[36m"
+FGCOLOR_LIGHT_GRAY = "\x1b[37m"
+
+FGCOLOR_DARK_GRAY = "\x1b[90m"
+FGCOLOR_LIGHT_RED = "\x1b[91m"
+FGCOLOR_LIGHT_GREEN = "\x1b[92m"
+FGCOLOR_LIGHT_YELLOW = "\x1b[93m"
+FGCOLOR_LIGHT_BLUE = "\x1b[94m"
+FGCOLOR_LIGHT_MAGENTA = "\x1b[95m"
+FGCOLOR_LIGHT_CYAN = "\x1b[96m"
+FGCOLOR_WHITE = "\x1b[97m"
+
+STYLE_RESET = "\x1b[0m"
+
+
+
+
+from .functions import checkTerminalSupportsColors, getTerminalSize
+
+
+
