a3d796f81fdd182ab921973ff49c6063c0f2d397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:35 +0000

    Add package yoga/0.11.1/yoga-0.11.1-cp37-cp37m-win_amd64.whl

---
+++ packages/yoga/yoga-0.11.1-cp37-cp37m-win_amd64.whl/yoga/model/options.py
@@ -0,0 +1,54 @@
+DEFAULT_OPTIONS = {
+        "output_format": "glb",             # glb|gltf
+        "fallback_texture": None,
+        "no_graph_optimization": False,
+        "no_meshes_optimization": False,
+        "no_textures_optimization": False,
+        }
+
+
+def normalize_options(options=None):
+    if not options:
+        return dict(DEFAULT_OPTIONS)
+
+    result = dict(DEFAULT_OPTIONS)
+
+    # "GLB" -> "glb" (lower case)
+    if "output_format" in options:
+        value = options["output_format"].lower()
+
+        if value not in ("gltf", "glb"):
+            raise ValueError("Invalid value for 'output_format': '%s'" % value) # noqa
+
+        result["output_format"] = value
+
+    # Fallback texture
+    if "fallback_texture" in options:
+        fallback_texture_file = options["fallback_texture"]
+
+        if fallback_texture_file:
+            if not hasattr(fallback_texture_file, "read"):
+                fallback_texture_file = open(fallback_texture_file, "rb")
+
+            result["fallback_texture"] = fallback_texture_file
+
+    # Optimization flags
+    for key in ("no_graph_optimization", "no_meshes_optimization",
+                "no_textures_optimization"):
+        if key in options:
+            result[key] = bool(options[key])
+
+    return result
+
+
+def extract_image_options(options=None):
+    if not options:
+        return None
+
+    result = dict()
+
+    for key in options:
+        if key.startswith("image_"):
+            result[key[6:]] = options[key]
+
+    return result
