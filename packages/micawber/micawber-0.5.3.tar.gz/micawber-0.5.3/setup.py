612ff9414d0dbcf8f63726081b159d1d08dd1706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:58 +0000

    Add package micawber/0.5.3/micawber-0.5.3.tar.gz

---
+++ packages/micawber/micawber-0.5.3.tar.gz/micawber-0.5.3/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+readme = f.read()
+f.close()
+
+setup(
+    name='micawber',
+    version='0.5.3',
+    description='a small library for extracting rich content from urls',
+    long_description=readme,
+    author='Charles Leifer',
+    author_email='coleifer@gmail.com',
+    url='http://github.com/coleifer/micawber/',
+    packages=[p for p in find_packages() if not p.startswith('examples')],
+    package_data = {
+        'micawber': [
+            'contrib/mcdjango/templates/micawber/*.html',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Framework :: Django',
+    ],
+    test_suite='runtests.runtests',
+)
