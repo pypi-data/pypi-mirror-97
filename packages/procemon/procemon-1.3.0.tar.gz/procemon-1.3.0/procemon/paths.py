173c6c392285fa5f5d910597e7a8fb3292c2a5c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:35 +0000

    Add package procemon/1.3.0/procemon-1.3.0.tar.gz

---
+++ packages/procemon/procemon-1.3.0.tar.gz/procemon-1.3.0/procemon/paths.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from pkg_resources import resource_filename
+
+# The path to the data files.
+DATA_DIRECTORY = Path(resource_filename(__name__, "data"))
+# The path to the types files.
+TYPES_DIRECTORY = DATA_DIRECTORY.joinpath("types")
+# The path to the moves files.
+MOVES_DIRECTORY = DATA_DIRECTORY.joinpath("moves")
+# The path to the images files.
+IMAGES_DIRECTORY = DATA_DIRECTORY.joinpath("images")
+# The path to the font files.
+FONTS_DIRECTORY = DATA_DIRECTORY.joinpath("fonts")
+# The path to the flavor text files.
+FLAVOR_TEXT_DIRECTORY = DATA_DIRECTORY.joinpath("flavor_text")
+# The directory of the word vector file.
+WORD_VEC_DIRECTORY = Path.home().joinpath("procemon_wv")
