3601613f5ad41abd3b18363346cf806e1b54622d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:56 +0000

    Add package myFirstPackageDatapro/0.1/myFirstPackageDatapro-0.1.tar.gz

---
+++ packages/myFirstPackageDatapro/myFirstPackageDatapro-0.1.tar.gz/myFirstPackageDatapro-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='myFirstPackageDatapro',
+      version='0.1',
+      url= 'https://dataproelearn.com/',
+      description='Package created by BVRaju datapro for testing',
+      author='BVRaju',
+      author_email='bvraju.itp@gmail.com',
+      packages=find_packages(),
+      install_requires=[],
+      python_requires='>=3.0'
+      )
+      
+      
