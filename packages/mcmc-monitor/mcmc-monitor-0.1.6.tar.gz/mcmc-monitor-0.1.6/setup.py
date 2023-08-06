112a94e4774783f89cc0056a5054150bfb7c0e03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:14 +0000

    Add package mcmc-monitor/0.1.6/mcmc-monitor-0.1.6.tar.gz

---
+++ packages/mcmc-monitor/mcmc-monitor-0.1.6.tar.gz/mcmc-monitor-0.1.6/setup.py
@@ -0,0 +1,32 @@
+import codecs
+import os.path
+
+import setuptools
+
+setuptools.setup(
+    name='mcmc-monitor',
+    version='0.1.6',
+    author="Jeremy Magland",
+    author_email="jmagland@flatironinstitute.org",
+    description="",
+    url="https://github.com/magland/mcmc-monitor",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    scripts=[
+        "bin/mcmc-monitor",
+        "bin/mcmc-monitor-services"
+    ],
+    install_requires=[
+        'cmdstanpy',
+        'numpy',
+        'scipy',
+        "aiohttp",
+        "aiohttp_cors",
+        'labbox==0.1.21'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ]
+)
