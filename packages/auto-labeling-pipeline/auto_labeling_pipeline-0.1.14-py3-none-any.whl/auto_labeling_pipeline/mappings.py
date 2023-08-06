4cb0f27f4131a53028645e785430ce0053db3f39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:08 +0000

    Add package auto-labeling-pipeline/0.1.14/auto_labeling_pipeline-0.1.14-py3-none-any.whl

---
+++ packages/auto-labeling-pipeline/auto_labeling_pipeline-0.1.14-py3-none-any.whl/auto_labeling_pipeline/mappings.py
@@ -0,0 +1,54 @@
+import json
+import pathlib
+from typing import Any, Dict, Optional, Type
+
+from jinja2 import Template
+
+from auto_labeling_pipeline.labels import ClassificationLabels, Labels, SequenceLabels
+
+TEMPLATE_DIR = pathlib.Path(__file__).parent / 'templates'
+
+
+class MappingTemplate:
+    label_collection: Type[Labels]
+    template_file: str = ''
+
+    def __init__(self, label_collection: Type[Labels] = Labels, template: Optional[str] = ''):
+        if self.template_file:
+            template = self.load()
+        if label_collection is not Labels:
+            self.label_collection = label_collection
+        self.template = template
+
+    def render(self, response: Dict) -> Labels:
+        template = Template(self.template)
+        rendered_json = template.render(input=response)
+        labels = json.loads(rendered_json)
+        labels = self.label_collection(labels)
+        return labels
+
+    def load(self) -> str:
+        filepath = TEMPLATE_DIR / self.template_file
+        with open(filepath) as f:
+            return f.read()
+
+    def dict(self) -> Dict[str, Any]:
+        return {
+            'template': self.template,
+            'collection': self.label_collection
+        }
+
+
+class AmazonComprehendSentimentTemplate(MappingTemplate):
+    label_collection = ClassificationLabels
+    template_file = 'amazon_comprehend_sentiment.jinja2'
+
+
+class AmazonComprehendEntityTemplate(MappingTemplate):
+    label_collection = SequenceLabels
+    template_file = 'amazon_comprehend_entity.jinja2'
+
+
+class GCPEntitiesTemplate(MappingTemplate):
+    label_collection = SequenceLabels
+    template_file = 'gcp_entities.jinja2'
