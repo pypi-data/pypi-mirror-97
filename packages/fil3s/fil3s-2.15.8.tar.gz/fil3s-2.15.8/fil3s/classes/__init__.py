e5a97a33660f00b187619ab9ff98457eb5499f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:27 +0000

    Add package fil3s/2.15.8/fil3s-2.15.8.tar.gz

---
+++ packages/fil3s/fil3s-2.15.8.tar.gz/fil3s-2.15.8/fil3s/classes/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# defaults.
+from fil3s.classes.files import Files,Formats,gfp,gdate
+from fil3s.classes.code import Code
+from fil3s.classes import exceptions
