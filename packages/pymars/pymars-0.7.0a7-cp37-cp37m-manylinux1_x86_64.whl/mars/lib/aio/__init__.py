88fa8b6cbec65ec0e2b457db065de27806540e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:22 +0000

    Add package pymars/0.7.0a7/pymars-0.7.0a7-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/pymars/pymars-0.7.0a7-cp37-cp37m-manylinux1_x86_64.whl/mars/lib/aio/__init__.py
@@ -0,0 +1,28 @@
+# Copyright 1999-2020 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import asyncio
+import sys
+
+from .file import AioFileObject, AioFilesystem
+from .parallelism import AioEvent
+
+
+if sys.version_info[:2] < (3, 7):
+    # patch run and get_running_loop etc for python 3.6
+    from ._runners import get_running_loop, run
+
+    asyncio.run = run
+    asyncio.get_running_loop = get_running_loop
+    asyncio.create_task = asyncio.ensure_future
