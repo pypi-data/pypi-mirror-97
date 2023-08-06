318011b30a51109b575ca5b36d01ea83e6aa2083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:32 +0000

    Add package bitermplus/0.5.0/bitermplus-0.5.0.tar.gz

---
+++ packages/bitermplus/bitermplus-0.5.0.tar.gz/bitermplus-0.5.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+ext_modules = [
+    Extension(
+        "bitermplus.btm",
+        sources=["src/bitermplus/btm.pyx"],
+        extra_compile_args=['-fopenmp'],
+        extra_link_args=['-fopenmp']),
+    Extension(
+        "bitermplus.btm_depr",
+        sources=["src/bitermplus/btm_depr.pyx"],
+        extra_compile_args=['-fopenmp'],
+        extra_link_args=['-fopenmp']),
+    Extension(
+        "bitermplus.metrics",
+        sources=["src/bitermplus/metrics.pyx"],
+        extra_compile_args=['-fopenmp'],
+        extra_link_args=['-fopenmp']),
+]
+
+setup(
+    ext_modules=cythonize(
+        ext_modules,
+        compiler_directives={
+            'embedsignature': True,
+            'language_level': 3})
+)
---
+++ packages/bitermplus/bitermplus-0.5.0.tar.gz/bitermplus-0.5.0/src/bitermplus/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ['BTM', 'BTMv1', 'util', 'metrics']
+
+from bitermplus.btm import BTM
+from bitermplus.btm_depr import BTM as BTMv1
+from bitermplus.util import *
+from bitermplus.metrics import *
+from bitermplus.plot import *
