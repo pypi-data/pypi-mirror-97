313ea157ea23026f2ca635256fedffd2fda0f5c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:59 +0000

    Add package pymatgen/2021.3.4/pymatgen-2021.3.4.tar.gz

---
+++ packages/pymatgen/pymatgen-2021.3.4.tar.gz/pymatgen-2021.3.4/pymatgen/__init__.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+# Copyright (c) Pymatgen Development Team.
+# Distributed under the terms of the MIT License.
+
+"""
+Pymatgen (Python Materials Genomics) is a robust, open-source Python library
+for materials analysis. This is the root package.
+"""
+
+__author__ = "Pymatgen Development Team"
+__email__ = "pymatgen@googlegroups.com"
+__maintainer__ = "Shyue Ping Ong"
+__maintainer_email__ = "shyuep@gmail.com"
+__version__ = "2021.3.4"
