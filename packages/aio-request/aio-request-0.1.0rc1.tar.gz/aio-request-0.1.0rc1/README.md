df15d2d8888cd1fad9997d6d77c6b2da4c4679cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:10 +0000

    Add package aio-request/0.1.0rc1/aio-request-0.1.0rc1.tar.gz

---
+++ packages/aio-request/aio-request-0.1.0rc1.tar.gz/aio-request-0.1.0rc1/README.md
@@ -0,0 +1,24 @@
+# aio-request
+
+```python
+import aiohttp
+
+from aio_request import MethodBasedStrategy, RequestStrategiesFactory, get, post, Deadline, AioHttpTransport
+
+client_session = aiohttp.ClientSession()
+async with client_session:
+    request_strategies_factory = RequestStrategiesFactory(
+        AioHttpRequestSender("http://endpoint:8080", client_session)
+    )
+    request_strategy = MethodBasedStrategy(
+        {
+            "GET": request_strategies_factory.parallel(),
+            "POST": request_strategies_factory.sequential()
+        }
+    )
+    async with request_strategy.request(get("/thing"), deadline=Deadline.from_timeout(5)) as response:
+        pass  # process response here
+
+    async with request_strategy.request(post("/thing"), deadline=Deadline.from_timeout(5)) as response:
+        pass  # process response here
+```
---
+++ packages/aio-request/aio-request-0.1.0rc1.tar.gz/aio-request-0.1.0rc1/setup.py
@@ -0,0 +1,41 @@
+import re
+from pathlib import Path
+
+from setuptools import setup
+
+install_requires = [
+    "multidict>=4.5,<7.0",
+    "yarl>=1.0,<2.0",
+]
+
+
+def read(*parts):
+    return Path(__file__).resolve().parent.joinpath(*parts).read_text().strip()
+
+
+def read_version():
+    regexp = re.compile(r"^__version__\W*=\W*\"([\d.abrc]+)\"")
+    for line in read("aio_request", "__init__.py").splitlines():
+        match = regexp.match(line)
+        if match is not None:
+            return match.group(1)
+    else:
+        raise RuntimeError("Cannot find version in aio_request/__init__.py")
+
+
+setup(
+    name="aio-request",
+    version=read_version(),
+    description="Various strategies for sending requests",
+    platforms=["macOS", "POSIX", "Windows"],
+    author="Yury Pliner",
+    python_requires=">=3.9",
+    project_urls={},
+    author_email="yury.pliner@gmail.com",
+    license="MIT",
+    packages=["aio_request"],
+    package_dir={"aio_request": "./aio_request"},
+    package_data={"aio_request": ["py.typed"]},
+    install_requires=install_requires,
+    include_package_data=True,
+)
