9ccf2735fbc5661dfc98fa8babc1619712156405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:11 +0000

    Add package hkube-python-wrapper/2.1.0.2.dev40/hkube_python_wrapper-2.1.0.2.dev40-py2.py3-none-any.whl

---
+++ packages/hkube-python-wrapper/hkube_python_wrapper-2.1.0.2.dev40-py2.py3-none-any.whl/hkube_python_wrapper/communication/streaming/MessageListener.py
@@ -0,0 +1,47 @@
+from hkube_python_wrapper.communication.zmq.streaming.ZMQListener import ZMQListener
+from hkube_python_wrapper.util.encoding import Encoding
+from hkube_python_wrapper.util.DaemonThread import DaemonThread
+from hkube_python_wrapper.util.logger import log
+import time
+
+
+class MessageListener(DaemonThread):
+
+    def __init__(self, options, receiverNode, errorHandler=None):
+        self.errorHandler = errorHandler
+        remoteAddress = options['remoteAddress']
+        encodingType = options['encoding']
+        self._encoding = Encoding(encodingType)
+        self.adapater = ZMQListener(remoteAddress, self.onMessage, self._encoding, receiverNode)
+        self.messageOriginNodeName = options['messageOriginNodeName']
+
+        self.messageListeners = []
+        DaemonThread.__init__(self, "MessageListener-" + str(self.messageOriginNodeName))
+
+    def registerMessageListener(self, listener):
+        self.messageListeners.append(listener)
+
+    def onMessage(self, messageFlowPattern, header, msg):
+        start = time.time()
+        decodedMsg = self._encoding.decode(header=header, value=msg)
+        for listener in self.messageListeners:
+            try:
+                listener(messageFlowPattern, decodedMsg, self.messageOriginNodeName)
+            except Exception as e:
+                log.error('Error during MessageListener onMessage {e}', e=str(e))
+
+        end = time.time()
+        duration = float((end - start) * 1000)
+        return self._encoding.encode({'duration': round(duration, 4)}, plainEncode=True)
+
+    def run(self):
+        log.info("Start receiving from {node}", node=self.messageOriginNodeName)
+        try:
+            self.adapater.start()
+        except Exception as e:
+            if (self.errorHandler):
+                self.errorHandler.sendError(e)
+
+    def close(self, force=True):
+        self.adapater.close(force)
+        self.messageListeners = []
---
+++ packages/hkube-python-wrapper/hkube_python_wrapper-2.1.0.2.dev40-py2.py3-none-any.whl/hkube_python_wrapper/communication/zmq/streaming/producer/ZMQProducer.py
@@ -0,0 +1,138 @@
+#
+# Paranoid Pirate queue
+#
+#   Author: Daniel Lundin <dln(at)eintr(dot)org>
+#
+import time
+from .Flow import Flow
+from .Worker import Worker
+from .WorkerQueue import WorkerQueue
+from .MessageQueue import MessageQueue
+from hkube_python_wrapper.util.logger import log
+import zmq
+
+HEARTBEAT_LIVENESS = 5  # 3..5 is reasonable
+HEARTBEAT_INTERVAL = 5  # Seconds
+CYCLE_LENGTH_MS = 1
+
+#  Paranoid Pirate Protocol constants
+PPP_READY = b"\x01"  # Signals worker is ready
+PPP_HEARTBEAT = b"\x02"  # Signals worker heartbeat
+PPP_DISCONNECT = b"\x03"  # Disconnect
+
+
+class ZMQProducer(object):
+    def __init__(self, port, maxMemorySize, responseAcumulator, consumerTypes, encoding, me):
+        self.me = me
+        self.encoding = encoding
+        self.responseAcumulator = responseAcumulator
+        self.maxMemorySize = maxMemorySize
+        self.port = port
+        self.consumerTypes = consumerTypes
+        self.messageQueue = MessageQueue(consumerTypes, self.me)
+        context = zmq.Context()
+        self._backend = context.socket(zmq.ROUTER)  # ROUTER
+        self._backend.bind("tcp://*:" + str(port))  # For workers
+        log.info("Producer listening on tcp://*:{port}", port=port)
+        self.active = True
+        self.watingForResponse = {}
+
+    def produce(self, header, message, messageFlowPattern=[]):
+        while (self.messageQueue.sizeSum > self.maxMemorySize):
+            self.messageQueue.loseMessage()
+        self.messageQueue.append(messageFlowPattern, header, message)
+
+    def start(self):  # pylint: disable=too-many-branches
+        poll_workers = zmq.Poller()
+        poll_workers.register(self._backend, zmq.POLLIN)
+        workers = WorkerQueue(self.consumerTypes)
+
+        heartbeat_at = time.time() + HEARTBEAT_INTERVAL
+        while self.active:
+            try:
+                socks = dict(poll_workers.poll(CYCLE_LENGTH_MS))
+            except Exception as e:
+                if (self.active):
+                    log.error(e)
+                else:
+                    break
+            # Handle worker activity on self._backend
+            if socks.get(self._backend) == zmq.POLLIN:
+                # Use worker address for LRU routing
+                try:
+                    frames = self._backend.recv_multipart()
+                except Exception as e:
+                    if (self.active):
+                        log.error(e)
+                    else:
+                        break
+                if not frames:
+                    if (self.active):
+                        raise Exception("Unexpected router no frames on receive, no address frame")
+                    break
+                address = frames[0]
+                consumerType = self.encoding.decode(value=frames[2], plainEncode=True)
+                if not consumerType in self.consumerTypes:
+                    log.warning("Producer got message from unknown consumer: {consumerType}, dropping the message", consumerType=consumerType)
+                    continue
+
+                if frames[1] not in (PPP_READY, PPP_HEARTBEAT):
+                    sentTime = self.watingForResponse.get(address)
+                    if (sentTime):
+                        now = time.time()
+                        del self.watingForResponse[address]
+                        self.responseAcumulator(frames[1], consumerType, round((now - sentTime) * 1000, 4))
+                    else:
+                        log.error('missing from watingForResponse:' + str(frames[1]))
+                if not address in self.watingForResponse.keys() and (frames[1] != PPP_DISCONNECT):
+                    expiry = time.time() + (HEARTBEAT_INTERVAL * HEARTBEAT_LIVENESS)
+                    workers.ready(Worker(address, expiry), consumerType)
+
+                # Validate control message, or return reply to client
+                if time.time() >= heartbeat_at:
+                    for consumerType, workersOfType in workers.queues.items():
+                        for worker in workersOfType:
+                            msg = [worker, PPP_HEARTBEAT]
+                            try:
+                                self._backend.send_multipart(msg, copy=False)
+                            except Exception as e:
+                                if (self.active):
+                                    log.error(e)
+                                else:
+                                    break
+                    heartbeat_at = time.time() + HEARTBEAT_INTERVAL
+            for consumerType, workerQueue in workers.queues.items():
+                if (workerQueue):
+                    poped = self.messageQueue.pop(consumerType)
+                    if (poped):
+                        messageFlowPattern, header, payload = poped
+                        flow = Flow(messageFlowPattern)
+                        identity = workers.nextWorker(consumerType)
+                        frames = [identity, self.encoding.encode(flow.getRestOfFlow(self.me), plainEncode=True), header, payload]
+                        self.watingForResponse[identity] = time.time()
+                        try:
+                            self._backend.send_multipart(frames, copy=False)
+                        except Exception as e:
+                            if (self.active):
+                                log.error(e)
+                            else:
+                                break
+            workers.purge()
+
+    def queueSize(self, consumerSize):
+        return self.messageQueue.size(consumerSize)
+
+    def sent(self, consumerType):
+        return self.messageQueue.sent[consumerType]
+
+    def close(self, force=True):
+        log.info('queue size during close = ' + str(len(self.messageQueue.queue)))
+        while self.messageQueue.queue and not force:
+            time.sleep(1)
+        log.info('queue empty, closing producer')
+        self.active = False
+        time.sleep(HEARTBEAT_LIVENESS + 1)
+        if not force:
+            self._backend.close(10)
+        else:
+            self._backend.close(0)
