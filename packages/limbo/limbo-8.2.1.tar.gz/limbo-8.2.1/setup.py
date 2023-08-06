347791aa2b3f3dd2924685af9fa2c1e986681bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:49 +0000

    Add package limbo/8.2.1/limbo-8.2.1.tar.gz

---
+++ packages/limbo/limbo-8.2.1.tar.gz/limbo-8.2.1/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+appdir = os.path.dirname(os.path.realpath(__file__))
+requirements = f"{appdir}/requirements.txt"
+# should I bother to remove testing requirements?
+required = [l.strip() for l in open(requirements) if not l.startswith("#")]
+
+packages = ["limbo", "limbo.plugins"]
+
+try:
+    longdesc = open("README.rst").read()
+except:
+    longdesc = ""
+
+setup(
+    name="limbo",
+    version="8.2.1",
+    description="Simple and Clean Slack Chatbot",
+    long_description=longdesc,
+    author="Bill Mill",
+    author_email="bill@billmill.org",
+    url="https://github.com/llimllib/limbo",
+    packages=packages,
+    scripts=["bin/limbo"],
+    package_data={"": ["LICENSE", "limbo/plugins/*.py"]},
+    include_package_data=True,
+    install_requires=required,
+    license="MIT",
+    python_requires=">=3.4",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    keywords="slack chatbot chat limbo",
+)
