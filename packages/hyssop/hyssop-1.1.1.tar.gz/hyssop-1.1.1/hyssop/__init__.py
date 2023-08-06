dc74bec623160e26700b7a1c5c47a87d0f49b636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:09 +0000

    Add package hyssop/1.1.1/hyssop-1.1.1.tar.gz

---
+++ packages/hyssop/hyssop-1.1.1.tar.gz/hyssop-1.1.1/hyssop/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2020-Present the hyssop authors and contributors.
+#
+# This module is part of hyssop and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+
+'''
+File created: August 21st 2020
+
+Modified By: hsky77
+Last Updated: March 6th 2021 17:37:26 pm
+'''
+
+
+Module_Path = __path__[0]
+Version = '1.1.1'
