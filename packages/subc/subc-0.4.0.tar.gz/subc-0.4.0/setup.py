24db78fa5e7e6b7f6e2608f4c1829659c3f2f8e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:14 +0000

    Add package subc/0.4.0/subc-0.4.0.tar.gz

---
+++ packages/subc/subc-0.4.0.tar.gz/subc-0.4.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    name='subc',
+    version='0.4.0',
+    description='CLI sub-command library',
+    long_description=long_description,
+    url='https://git.sr.ht/~brenns10/subc',
+    author='Stephen Brennan',
+    author_email='stephen@brennan.io',
+    license='Revised BSD',
+    py_modules=['subc'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords='arguments sub-command command',
+)
