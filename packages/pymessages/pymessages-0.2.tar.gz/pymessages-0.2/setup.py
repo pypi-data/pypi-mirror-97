53520e2e807f6b93065ab70f3fafb8d87d201b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:39 +0000

    Add package pymessages/0.2/pymessages-0.2.tar.gz

---
+++ packages/pymessages/pymessages-0.2.tar.gz/pymessages-0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='pymessages',
+      version='0.2',
+      description='📱 SMS gateway using your own phone!',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: ISC License (ISCL)',
+          'Programming Language :: Python :: 3.8',
+          'Topic :: Software Development :: Libraries',
+      ],
+      keywords='python sms-api sms-gateway google-messages',
+      url='http://github.com/shivamsn97/pymessages',
+      author='Shivam Saini',
+      author_email='shivamsn97@gmail.com',
+      license='ISC',
+      packages=find_packages(),
+      install_requires=[
+          'pyppeteer',
+          'pyee',
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      )
