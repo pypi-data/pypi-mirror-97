1384d19bdb6ecce9fb756b2882e9f2afa4ecf5bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:16 +0000

    Add package elektra/0.0.9/elektra-0.0.9.tar.gz

---
+++ packages/elektra/elektra-0.0.9.tar.gz/elektra-0.0.9/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    README = fh.read()
+
+setuptools.setup(
+    name="elektra",
+    version="0.0.9",
+    author="Molecule",
+    author_email="devs@molecule.io",
+    description="Power block price creation and conversion",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/wearemolecule/elektra',
+    packages=setuptools.find_packages(),
+    license='MIT',
+    python_requires='>=3.8',
+    install_requires=[
+        'pandas>=1.2.3',
+        'numpy>=1.20.1'
+    ]
+)
