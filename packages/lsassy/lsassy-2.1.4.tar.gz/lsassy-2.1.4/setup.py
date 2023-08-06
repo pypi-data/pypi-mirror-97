9b34fbfe4014a41dafb65482dd10bea4d903c212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:00 +0000

    Add package lsassy/2.1.4/lsassy-2.1.4.tar.gz

---
+++ packages/lsassy/lsassy-2.1.4.tar.gz/lsassy-2.1.4/setup.py
@@ -0,0 +1,47 @@
+# Author:
+#  Romain Bentz (pixis - @hackanddo)
+# Website:
+#  https://beta.hackndo.com [FR]
+#  https://en.hackndo.com [EN]
+
+import pathlib
+
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="lsassy",
+    version="2.1.4",
+    author="Pixis",
+    author_email="hackndo@gmail.com",
+    description="Python library to parse remote lsass dumps",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=["assets", "cme"]),
+    include_package_data=True,
+    url="https://github.com/Hackndo/lsassy/",
+    zip_safe = True,
+    license="MIT",
+    install_requires=[
+        'impacket',
+        'netaddr',
+        'pypykatz>=0.3.0'
+    ],
+    python_requires='>=3.6',
+    classifiers=(
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    entry_points={
+        'console_scripts': [
+            'lsassy = lsassy.core:run',
+        ],
+    },
+    test_suite='tests.tests'
+)
