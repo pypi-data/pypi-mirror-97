fcfe10885af17f03ef58a8805c01edb1755e1e1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:30 +0000

    Add package sagemath-standard/9.3b8/sagemath-standard-9.3b8.tar.gz

---
+++ packages/sagemath-standard/sagemath-standard-9.3b8.tar.gz/sagemath-standard-9.3b8/sage/sandpiles/all.py
@@ -0,0 +1,12 @@
+from sage.misc.lazy_import import lazy_import
+
+from .sandpile import (Sandpile,
+                       SandpileDivisor,
+                       SandpileConfig,
+                       random_DAG,
+                       firing_graph,
+                       parallel_firing_graph,
+                       wilmes_algorithm,
+                       triangle_sandpile)
+
+lazy_import('sage.sandpiles.examples', 'sandpiles')
