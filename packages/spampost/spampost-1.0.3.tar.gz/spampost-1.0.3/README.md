e637abcd3a1d2d0f785866259e7d29bddff28857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:09 +0000

    Add package spampost/1.0.3/spampost-1.0.3.tar.gz

---
+++ packages/spampost/spampost-1.0.3.tar.gz/spampost-1.0.3/README.md
@@ -0,0 +1,8 @@
+# spampost
+
+`spampost` is checker of spam-post of blog and etc.
+This library for Japanese content only.
+
+	import spampost
+	if spampost.is_spam_post('hello, world'):
+		print('is spam')
---
+++ packages/spampost/spampost-1.0.3.tar.gz/spampost-1.0.3/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="spampost",
+    version="1.0.3",
+    description="Check blog's spam post",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/narupo/spampost",
+    author="narupo",
+    author_email="narupo.glab@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["spampost"],
+    include_package_data=True,
+    install_requires=[
+        "BeautifulSoup4",
+        "soupsieve",
+        "janome",
+    ],
+    entry_points={
+        "console_scripts": [
+            "spampost=spampost.__main__:main",
+        ]
+    },
+)
