dd2f61ed732c92c757777180335608327d68759c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:11 +0000

    Add package foliantcontrib.test-framework/0.1.2/foliantcontrib.test_framework-0.1.2.tar.gz

---
+++ packages/foliantcontrib.test-framework/foliantcontrib.test_framework-0.1.2.tar.gz/foliantcontrib.test_framework-0.1.2/foliant_test/config_extension.py
@@ -0,0 +1,70 @@
+import os
+from logging import getLogger
+from importlib import import_module
+from pathlib import Path
+
+
+class ConfigExtensionTestFramework:
+    _defaults = {
+        'src_dir': Path('./src').expanduser(),
+        'tmp_dir': Path('./__folianttmp__').expanduser()
+    }
+
+    def __init__(self, extension_name: str):
+        try:
+            parser_module = import_module(f'foliant.config.{extension_name}')
+            self.parser = parser_module.Parser
+
+        except ModuleNotFoundError:
+            raise ModuleNotFoundError(f'Config extension {extension_name} is not installed')
+        self.extension_name = extension_name
+        self.project_path = Path('.')
+        self.config_file_name = '_foliant.yml'
+
+        self.config_path = self.project_path / self.config_file_name
+
+        self.logger = getLogger('ConfigExtensionTestFramework')
+        self.quiet = True
+
+    @property
+    def results(self):
+        return self._results_dict
+
+    def test_extension(self,
+                       input_config: str,
+                       expected_config: dict,
+                       keep: bool = False):
+
+        self._cleanup()
+        self._generate_config(input_config)
+        parser = self.parser(project_path=self.project_path,
+                             config_file_name=self.config_file_name,
+                             logger=self.logger,
+                             quiet=self.quiet)
+
+        result = parser.parse()
+
+        if result:
+            self.compare_results(result, expected_config)
+
+    def _cleanup(self):
+        if os.path.isfile(self.config_path):
+            os.remove(self.config_path)
+
+    def _generate_config(self, input_: str):
+        with open(self.config_path, 'w', encoding='utf8') as f:
+            f.write(input_)
+
+    def compare_results(self, result: dict, expected_config: dict):
+        expected = {**self._defaults, **expected_config}
+
+        if result != expected:
+            raise ResultsDifferError(
+                'Parsed config differs from expected!\n'
+                f'Result: {result}\n\n'
+                f'Expected: {expected}'
+            )
+
+
+class ResultsDifferError(Exception):
+    pass
---
+++ packages/foliantcontrib.test-framework/foliantcontrib.test_framework-0.1.2.tar.gz/foliantcontrib.test_framework-0.1.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+
+SHORT_DESCRIPTION = 'Testing framework for Foliant'
+
+try:
+    with open('README.md', encoding='utf8') as readme:
+        LONG_DESCRIPTION = readme.read()
+
+except FileNotFoundError:
+    LONG_DESCRIPTION = SHORT_DESCRIPTION
+
+
+setup(
+    name='foliantcontrib.test_framework',
+    description=SHORT_DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    version='0.1.2',
+    author='Daniil Minukhin',
+    author_email='ddddsa@gmail.com',
+    url='https://github.com/foliant-docs/foliantcontrib.test_framework',
+    packages=['foliant_test'],
+    license='MIT',
+    platforms='any',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Documentation",
+        "Topic :: Utilities",
+    ]
+)
