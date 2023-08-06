7074f88ec8ac98791d751cbc7f7794a93175f62d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:58 +0000

    Add package clubhouse-api/1.0.2/clubhouse_api-1.0.2.tar.gz

---
+++ packages/clubhouse-api/clubhouse_api-1.0.2.tar.gz/clubhouse_api-1.0.2/clubhouse_api/__init__.py
@@ -0,0 +1,15 @@
+"""
+:authors: Peopl3s
+:license: Apache License, Version 2.0, see LICENSE file
+
+:copyright: (c) 2021 Peopl3s
+"""
+
+from .clubhouse import (ClubHouse, ClubHouseApiMethod, )
+from .exceptions import (ClubHouseError, HttpApiMethodError, ApiError, )
+from .enums import (HttpRequestTypes, ResponseCodes, )
+from .utils import *
+
+__author__ = 'Peopl3s'
+__version__ = '1.0.0'
+__email__ = 'peoplesdreamer@gmail.com'
