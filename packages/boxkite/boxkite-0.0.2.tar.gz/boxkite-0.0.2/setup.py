77c895607bf52427eaae051e57dc6a2509578c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:04 +0000

    Add package boxkite/0.0.2/boxkite-0.0.2.tar.gz

---
+++ packages/boxkite/boxkite-0.0.2.tar.gz/boxkite-0.0.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="boxkite",
+    version="0.0.2",
+    author="Luke Marsden",
+    author_email="me@lukemarsden.net",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/basisai/boxkite",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    # TODO: can we relax these requirements at all?
+    install_requires=[
+        "fluent-logger==0.9.6",
+        "prometheus_client==0.9",
+        "numpy==1.19.4",
+    ],
+)
