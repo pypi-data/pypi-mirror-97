a4997519e3270a5e947cd66c9e6c4841ce22f94f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:41 +0000

    Add package fmtree/0.0.3/fmtree-0.0.3.tar.gz

---
+++ packages/fmtree/fmtree-0.0.3.tar.gz/fmtree-0.0.3/fmtree/utils.py
@@ -0,0 +1,31 @@
+import pathlib2
+
+from fmtree.node import FileNode
+
+
+def reproduce_fs_tree(target_dir: pathlib2.Path, root: FileNode) -> None:
+    """
+    Given a tree, recreate the tree structure in the file system
+    :param target_dir: directory the structure is going to be reproduced in
+    :param root: tree root node
+    :return: None
+    """
+    target_dir.mkdir(parents=True, exist_ok=True)
+    if root and root.get_filename():
+        target_dir /= root.get_filename()
+
+    def traverse(node: FileNode):
+        if node.get_root() is None or node.get_relative_path() is None:
+            raise ValueError("To reproduce given tree in file system, every node much have relative path,"
+                             "since relative path is calculated with root path, FileNode has to be initialized"
+                             "with root path. both root and relative path must be available")
+        path = target_dir / node.get_relative_path()
+        if node.is_dir():
+            path.mkdir(parents=True, exist_ok=True)
+        elif node.is_file():
+            path.touch(exist_ok=True)
+        if node.get_children():
+            for child_node in node.get_children():
+                traverse(child_node)
+
+    traverse(root)
---
+++ packages/fmtree/fmtree-0.0.3.tar.gz/fmtree-0.0.3/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fmtree",
+    version="0.0.3",
+    author="Huakun Shen",
+    author_email="huakun.shen@huakunshen.com",
+    description="Scrape File System and output in different formats",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HuakunShen/fmtree",
+    project_urls={
+        "Bug Tracker": "https://github.com/HuakunShen/fmtree/issues",
+        "Documentation": "https://huakunshen.github.io/fmtree/"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pathlib2'
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
+
+# python -m build
+# python -m twine upload dist/* --verbose --repository testpypi
