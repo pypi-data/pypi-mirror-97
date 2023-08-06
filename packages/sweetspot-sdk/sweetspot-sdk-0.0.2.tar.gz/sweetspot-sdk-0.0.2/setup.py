ec7e5e86a213447a1fdbd043f6424f3bfb63f9bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:18 +0000

    Add package sweetspot-sdk/0.0.2/sweetspot-sdk-0.0.2.tar.gz

---
+++ packages/sweetspot-sdk/sweetspot-sdk-0.0.2.tar.gz/sweetspot-sdk-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="sweetspot-sdk",
+    version="0.0.2",
+    description="SDK for Sweetspot Team to interact with REST APIs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Philip Kung",
+    author_email="philip@sweetspot.so",
+    url="https://www.sweetspot.so",
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.9.1, <3.0',
+        'jsonpickle>=0.7.1, <1.0',
+        'python-dateutil>=2.5.3, <3.0',
+        'deprecation>=2.0.6'
+    ]
+)
