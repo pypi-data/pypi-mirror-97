e42bd5131670e8cf2d3989c2f1e3771065a13d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:57 +0000

    Add package kak-spell/0.3.0/kak-spell-0.3.0.tar.gz

---
+++ packages/kak-spell/kak-spell-0.3.0.tar.gz/kak-spell-0.3.0/kak_spell/cli.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+from typing import List, Optional
+import argparse
+import sys
+
+from pathlib import Path
+
+from .checker import Checker
+from . import kak
+from . import log
+
+
+def add_word(word: str, *, verbose: bool = True, lang: str) -> None:
+    checker = Checker(lang=lang)
+    checker.add(word)
+    if verbose:
+        print("Word added to", checker.pwl_path)
+
+
+def remove_word(word: str, *, verbose: bool = True, lang: str) -> None:
+    checker = Checker(lang=lang)
+    checker.remove(word)
+    if verbose:
+        print("Word removed from", checker.pwl_path)
+
+
+def check(
+    path: Path,
+    *,
+    lang: str,
+    filetype: Optional[str] = None,
+    kakoune: bool = False,
+    kak_timestamp: int = 0,
+) -> bool:
+    checker = Checker(lang=lang)
+    errors = checker.check(path, filetype=filetype)
+    if kakoune:
+        # We need a real list because we'll iterate on it twice
+        kak.handle_spelling_errors(list(errors), timestamp=kak_timestamp, lang=lang)
+        return True
+    else:
+        ok = True
+        for error in errors:
+            ok = False
+            print(f"{path}:{error.lineno}:{error.offset + 1}: error: {error.word}")
+    return ok
+
+
+def replace(word: str, *, lang: str, kak_output: bool) -> None:
+    checker = Checker(lang=lang)
+    replacements = checker.replace(word)
+    if kak_output:
+        kak.menu_from_replacements(replacements)
+    else:
+        print(" ".join(replacements))
+
+
+def main(argv: Optional[List[str]] = None) -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--lang", default="en_US")
+    parser.add_argument("--quiet", action="store_false", dest="verbose")
+    parser.set_defaults(verbose=True)
+
+    subparsers = parser.add_subparsers(title="commands", dest="command")
+
+    check_parser = subparsers.add_parser("check")
+    check_parser.add_argument("path", type=Path)
+    check_parser.add_argument(
+        "--filetype", help="file type, as set by kakoune in $kak_opt_filetype"
+    )
+    check_parser.add_argument(
+        "--kakoune", help="output kakoune commands", action="store_true"
+    )
+    check_parser.add_argument("--kak-timestamp", help="kakoune timestamp", type=int)
+
+    list_parser = subparsers.add_parser("list")
+    list_parser.add_argument("path", type=Path)
+    list_parser.add_argument(
+        "--filetype", help="file type, as set by kakoune in $kak_opt_filetype"
+    )
+
+    add_parser = subparsers.add_parser("add")
+    add_parser.add_argument("word")
+
+    remove_parser = subparsers.add_parser("remove")
+    remove_parser.add_argument("word")
+
+    replace_parser = subparsers.add_parser("replace")
+    replace_parser.add_argument(
+        "--kakoune", action="store_true", help="Output kak-script compatible output"
+    )
+    replace_parser.add_argument("word")
+
+    next_parser = subparsers.add_parser("next")
+    next_parser.add_argument("--ranges", help="kakoune ranges", required=True)
+    next_parser.add_argument("--pos", help="kakoune cursor pos", required=True)
+
+    previous_parser = subparsers.add_parser("previous")
+    previous_parser.add_argument("--ranges", help="kakoune ranges", required=True)
+    previous_parser.add_argument("--pos", help="kakoune cursor pos", required=True)
+
+    args = parser.parse_args(args=argv)
+    log(args)
+    lang = args.lang
+    verbose = args.verbose
+
+    if args.command == "add":
+        word = args.word
+        add_word(word, verbose=verbose, lang=lang)
+    elif args.command == "remove":
+        word = args.word
+        remove_word(word, verbose=verbose, lang=lang)
+    elif args.command == "check":
+        ok = check(
+            args.path,
+            lang=lang,
+            filetype=args.filetype,
+            kakoune=args.kakoune,
+            kak_timestamp=args.kak_timestamp,
+        )
+        if not ok:
+            sys.exit(1)
+    elif args.command == "list":
+        kak.show_spelling_buffer()
+    elif args.command == "replace":
+        word = args.word
+        kakoune = args.kakoune
+        replace(word, lang=lang, kak_output=kakoune)
+    elif args.command == "next":
+        kak.goto_next(args.pos, args.ranges)
+    elif args.command == "previous":
+        kak.goto_previous(args.pos, args.ranges)
+    else:
+        parser.print_help()
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/kak-spell/kak-spell-0.3.0.tar.gz/kak-spell-0.3.0/kak_spell/kak.py
@@ -0,0 +1,155 @@
+from typing import List, Tuple
+from . import log
+from .checker import Error
+
+
+def handle_spelling_errors(errors: List[Error], *, timestamp: int, lang: str) -> None:
+    insert_errors_in_scratch_buffer(errors, lang=lang)
+    set_spell_errors(errors, timestamp=timestamp)
+
+
+def show_spelling_buffer() -> None:
+    print("edit -existing *spelling*")
+
+
+def insert_errors_in_scratch_buffer(errors: List[Error], lang: str) -> None:
+    # TODO: -fifo ?
+    print("edit -scratch *spelling*")
+    cmd = "execute-keys \\% <ret> d i %{"
+    for error in errors:
+        line, col, length = error_to_range(error)
+        cmd += f"{line}.{col},{line}.{col+length-1} {error.word}<ret>"
+    cmd += "}"
+    cmd += "<esc> gg"
+    cmd += "\n" "map buffer normal '<ret>' ':<space>kak-spell-jump<ret>'"
+    cmd += (
+        "\n"
+        f"map buffer normal 'a' ':<space>kak-spell-add-from-spelling-buffer {lang}<ret>'"
+    )
+    cmd += "\n" "execute-keys <esc> ga"
+    log(cmd)
+    print(cmd)
+
+
+def set_spell_errors(errors: List[Error], *, timestamp: int) -> None:
+    n = 0
+    cmd = f"set-option buffer spell_errors {timestamp}"
+    for error in errors:
+        (line, col, length) = error_to_range(error)
+        cmd += f" {line}.{col}+{length}|Error"
+        n += 1
+    log(cmd)
+    print(cmd)
+    if n:
+        print("echo -markup {red}", n, "spelling error(s)")
+    else:
+        show_no_errors()
+
+
+def menu_from_replacements(replacements: List[str]) -> None:
+    if not replacements:
+        print("fail no suggestions")
+        return
+    menu = ""
+    for entry in replacements:
+        # Note: %{...} is kakoune way of grouping stuff that may - or not
+        # contains quotes, which prevents us from using `%`, `.format()` or
+        # f-strings :P
+        # also note that we call 'write' and 'kak-spell' *in* the menu action, otherwise
+        # if you call `write` *after* the menu command, the buffer is saved without the new
+        # contents written !
+        menu_entry = "%{ENTRY} %{execute-keys -itersel %{cENTRY<esc>be} "
+        menu_entry += ":write <ret> :kak-spell <ret>}"
+        menu_entry = menu_entry.replace("ENTRY", entry)
+        menu += " " + menu_entry
+    cmd = "menu " + menu
+    print(cmd)
+
+
+def show_no_errors() -> None:
+    print("echo -markup {green} no spelling errors")
+
+
+def goto_next(pos_option: str, ranges_option: str) -> None:
+    cursor = convert_to_pos(pos_option)
+    ranges = convert_ranges(ranges_option)
+    if not ranges:
+        show_no_errors()
+        return
+    next_selection = get_next_selection(cursor, ranges)
+    goto_and_select(next_selection)
+
+
+def goto_previous(pos_option: str, ranges_option: str) -> None:
+    cursor = convert_to_pos(pos_option)
+    ranges = convert_ranges(ranges_option)
+    if not ranges:
+        show_no_errors()
+        return
+    previous_selection = get_previous_selection(cursor, ranges)
+    goto_and_select(previous_selection)
+
+
+# (line, col)
+Pos = Tuple[int, int]
+
+# (line, col, length)
+Range = Tuple[int, int, int]
+
+
+def error_to_range(error: Error) -> Range:
+    bytes_offset = len((error.line[: error.offset]).encode())
+    return (error.lineno, bytes_offset + 1, len(error.word.encode()))
+
+
+def convert_to_pos(pos_option: str) -> Pos:
+    start, end = pos_option.split(".")
+    return int(start), int(end)
+
+
+def convert_ranges(ranges_option: str) -> List[Range]:
+    if ranges_option == "0":
+        return []
+
+    res = []
+    ranges_list = ranges_option.split()[1:]
+    for range_option in ranges_list:
+        range_str = range_option.split("|")[0]  # strip face
+        start, end = range_str.split(",")
+        start_line, start_col = convert_to_pos(start)
+        end_line, end_col = convert_to_pos(end)
+        res.append((start_line, start_col, end_col))
+    return res
+
+
+def goto_and_select(range: Range) -> None:
+    line, col, end = range
+    length = end - col
+    cmd = f"select {line}.{col},{line}.{col+length}"
+    print(cmd)
+
+
+def get_next_selection(cursor: Pos, ranges: List[Range]) -> Range:
+    cursor_line, cursor_col = cursor
+    for r in ranges:
+        start_line, start_col, end_col = r
+        if start_line < cursor_line:
+            continue
+        if start_line == cursor_line and end_col <= cursor_col:
+            continue
+        return r
+    # if we reach there, return the first error (auto-wrap)
+    return ranges[0]
+
+
+def get_previous_selection(cursor: Pos, ranges: List[Range]) -> Range:
+    cursor_line, cursor_col = cursor
+    for r in reversed(ranges):
+        start_line, start_col, end_col = r
+        if start_line > cursor_line:
+            continue
+        if start_line == cursor_line and end_col > cursor_col:
+            continue
+        return r
+    # if we reach there, return the last error (auto-wrap)
+    return ranges[-1]
