26703239331bb79972e2e6913179bad25964515e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:37 +0000

    Add package exergi/0.5.1/exergi-0.5.1.tar.gz

---
+++ packages/exergi/exergi-0.5.1.tar.gz/exergi-0.5.1/exergi/snowflake.py
@@ -0,0 +1,34 @@
+def execute_query(
+    sql_query: str,
+):
+    """Execute query on Snowflake.
+
+    Please note that the following environment variables must have been set:
+        - SNOWFLAKE_USER
+        - SNOWFLAKE_PASSWORD
+        - SNOWFLAKE_ACCOUNT
+        - SNOWFLAKE_WH 
+        - SNOWFLAKE_DB
+        - SNOWFLAKE_SCHEMA
+
+    Arguments:
+        sql_query (str) - Query to execute
+    Returns:
+        None
+    """
+    logger = logging.getLogger()
+
+    # Setup connection
+    ctx = snowflake.connector.connect(
+        user=os.environ["SNOWFLAKE_USER"],
+        password=os.environ["SNOWFLAKE_PASSWORD"],
+        account=os.environ["SNOWFLAKE_ACCOUNT"],
+        warehouse=os.environ["SNOWFLAKE_WH"],
+        database=os.environ["SNOWFLAKE_DB"],
+        schema=os.environ["SNOWFLAKE_SCHEMA"],
+    )
+
+    # Execute query
+    cs = ctx.cursor()
+    cs.execute(rm_string)
+    ctx.close()
