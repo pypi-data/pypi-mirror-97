853c6e498665f3089ebd5b69eb7891f44eb13a19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:01 +0000

    Add package simpledemotivators/1.5.1/simpledemotivators-1.5.1.tar.gz

---
+++ packages/simpledemotivators/simpledemotivators-1.5.1.tar.gz/simpledemotivators-1.5.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="simpledemotivators",
+    version="1.5.1",
+    author="infqq",
+    description="Tool for easy demotivators",
+    url="https://github.com/Infqq/simpledemotivators",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+)
