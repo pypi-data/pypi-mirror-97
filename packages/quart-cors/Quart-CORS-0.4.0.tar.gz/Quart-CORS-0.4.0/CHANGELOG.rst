f042179494c14aa3f703f05a0844917cf7b6890b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:47 +0000

    Add package quart-cors/0.4.0/Quart-CORS-0.4.0.tar.gz

---
+++ packages/quart-cors/Quart-CORS-0.4.0.tar.gz/Quart-CORS-0.4.0/CHANGELOG.rst
@@ -0,0 +1,45 @@
+0.4.0 2021-03-09
+----------------
+
+* Support Python 3.9.
+* Allow the allowed origin to be a regex pattern (or iterable
+  thereof).
+* Bugfix crash when sending OPTIONS with missing
+  Access-Control-Allow-Origin header.
+
+0.3.0 2020-02-09
+----------------
+
+* Support Python 3.8.
+* Support Quart >= 0.11.1 - with this only a single origin (or
+  wildcard) can be returned as the Access-Control-Allow-Origin header,
+  as per the specification.
+
+0.2.0 2019-08-02
+----------------
+
+* Move files to within a quart_cors folder to ensure the py.typed file
+  is picked up.
+* Drop support for Python 3.6.
+* Add a websocket_cors function that checks the origin and will
+  respond with 400 if not an allowed origin.
+
+0.1.3 2019-04-22
+----------------
+
+* Add py.typed for PEP 561 compliance.
+
+0.1.2 2019-01-29
+----------------
+
+* Bugfix allow all request_headers when allow_headers is set to "*".
+
+0.1.1 2018-12-09
+----------------
+
+* Bumped minimum Quart version to 0.6.11 due to a bug in Quart.
+
+0.1.0 2018-06-11
+----------------
+
+* Released initial alpha version.
---
+++ packages/quart-cors/Quart-CORS-0.4.0.tar.gz/Quart-CORS-0.4.0/tests/test_basic.py
@@ -0,0 +1,113 @@
+import re
+from datetime import timedelta
+from typing import Pattern, Union
+
+import pytest
+from quart import Blueprint, Quart
+from werkzeug.datastructures import HeaderSet
+
+from quart_cors import cors, route_cors
+
+
+@pytest.fixture(name="route_cors_app")
+def _route_cors_app() -> Quart:
+    app = Quart(__name__)
+
+    @app.route("/")
+    @route_cors(max_age=timedelta(seconds=5))
+    async def index() -> str:
+        return "Hello"
+
+    return app
+
+
+@pytest.mark.asyncio
+async def test_simple_cross_origin_request(route_cors_app: Quart) -> None:
+    test_client = route_cors_app.test_client()
+    response = await test_client.get("/", headers={"Origin": "https://quart.com"})
+    assert response.access_control_allow_origin == "*"
+
+
+@pytest.mark.asyncio
+async def test_preflight_request(route_cors_app: Quart) -> None:
+    test_client = route_cors_app.test_client()
+    response = await test_client.options(
+        "/", headers={"Origin": "https://quart.com", "Access-Control-Request-Method": "DELETE"}
+    )
+    assert response.access_control_allow_origin == "*"
+    assert response.access_control_allow_methods == HeaderSet(
+        ["GET", "HEAD", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"]
+    )
+    assert response.access_control_max_age == 5
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("kind", ("no-header", "empty-header"))
+async def test_bad_preflight_request(route_cors_app: Quart, kind: str) -> None:
+    test_client = route_cors_app.test_client()
+    # Missing Access-Control-Request-Method header
+    headers = {"Origin": "https://quart.com"}
+    if kind == "empty-header":
+        headers["Access-Control-Request-Method"] = ""
+    response = await test_client.options("/", headers=headers)
+    assert response.access_control_allow_origin == "*"
+    assert response.access_control_allow_methods is None
+    assert response.access_control_max_age is None
+
+
+@pytest.mark.asyncio
+async def test_app_cors() -> None:
+    app = Quart(__name__)
+
+    @app.route("/")
+    async def index() -> str:
+        return "Hello"
+
+    app = cors(app)
+
+    test_client = app.test_client()
+    response = await test_client.get("/", headers={"Origin": "https://quart.com"})
+    assert response.access_control_allow_origin == "*"
+
+
+@pytest.mark.asyncio
+async def test_blueprint_cors() -> None:
+    app = Quart(__name__)
+
+    blueprint = Blueprint("name", __name__)
+
+    blueprint = cors(blueprint)
+
+    @blueprint.route("/")
+    async def index() -> str:
+        return "Hello"
+
+    app.register_blueprint(blueprint)
+
+    test_client = app.test_client()
+    response = await test_client.get("/", headers={"Origin": "https://quart.com"})
+    assert response.access_control_allow_origin == "*"
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "allowed_origin, expected",
+    [
+        ("*", "*"),
+        ("https://quart.com", "https://quart.com"),
+        (re.compile(r"https:\/\/.*\.?quart\.com"), "https://quart.com"),
+    ],
+)
+async def test_regex_matching(allowed_origin: Union[Pattern, str], expected: str) -> None:
+    app = Quart(__name__)
+    app.config["QUART_CORS_ALLOW_ORIGIN"] = [allowed_origin]
+
+    @app.route("/")
+    async def index() -> str:
+        return "Hello"
+
+    app = cors(app)
+
+    test_client = app.test_client()
+    response = await test_client.get("/", headers={"Origin": "https://quart.com"})
+    assert response.access_control_allow_origin == expected
