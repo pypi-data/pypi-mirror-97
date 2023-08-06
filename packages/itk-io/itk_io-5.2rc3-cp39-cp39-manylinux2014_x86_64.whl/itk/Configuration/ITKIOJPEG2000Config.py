e88b456d28de461ed631d21f1f263d9bbb0eb906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:29 +0000

    Add package itk-io/5.2rc3/itk_io-5.2rc3-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/itk-io/itk_io-5.2rc3-cp39-cp39-manylinux2014_x86_64.whl/itk/Configuration/ITKIOJPEG2000Config.py
@@ -0,0 +1,6 @@
+depends = ('ITKPyBase', 'ITKIOImageBase', )
+templates = (
+  ('JPEG2000ImageIOFactory', 'itk::JPEG2000ImageIOFactory', 'itkJPEG2000ImageIOFactory', True),
+  ('JPEG2000ImageIOInternalEnums', 'itk::JPEG2000ImageIOInternalEnums', 'itkJPEG2000ImageIOInternalEnums', False),
+  ('JPEG2000ImageIO', 'itk::JPEG2000ImageIO', 'itkJPEG2000ImageIO', True),
+)
