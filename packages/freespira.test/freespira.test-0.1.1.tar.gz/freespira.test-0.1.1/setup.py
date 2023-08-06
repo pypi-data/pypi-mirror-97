1cf7c46a6bbae15accc2f328bcafded0cf08c641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:03 +0000

    Add package freespira.test/0.1.1/freespira.test-0.1.1.tar.gz

---
+++ packages/freespira.test/freespira.test-0.1.1.tar.gz/freespira.test-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='freespira.test',
+    version='0.1.1',
+    description='test library for freespira code',
+    author='Sean Reynolds and Paul Graham',
+    author_email='sean@freespira.com, paul@freespira.com',
+    instal_requires=[
+        "pandas==1.2.1",
+        "selenium==3.141.0",
+        "matplotlib==3.3.3",
+        "numpy==1.19.5"
+    ],
+    setup_requires=[
+        "pandas==1.2.1",
+        "selenium==3.141.0",
+        "matplotlib==3.3.3",
+        "numpy==1.19.5"
+    ],
+    packages=find_packages()
+)
