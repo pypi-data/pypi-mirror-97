5e0098d08d38a3c2ddac122c0cb1e89b144e5c78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:03 +0000

    Add package navconfig/0.2.17/navconfig-0.2.17-py3-none-any.whl

---
+++ packages/navconfig/navconfig-0.2.17-py3-none-any.whl/navconfig/__init__.py
@@ -0,0 +1,49 @@
+import sys
+import os
+from pathlib import Path
+from .config import navigatorConfig
+
+def is_virtualenv():
+    return (hasattr(sys, 'real_prefix') or (hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix))
+
+# PROJECT PATH IS DEFINED?
+SITE_ROOT = os.getenv('SITE_ROOT', None)
+
+if not SITE_ROOT:
+    # get Project PATH
+    if is_virtualenv():
+        SITE_ROOT = Path(sys.prefix).resolve().parent
+    else:
+        SITE_ROOT = Path(os.path.abspath(os.path.dirname(__file__))).resolve().parent.parent
+    if not SITE_ROOT:
+        SITE_ROOT = Path(sys.prefix).resolve().parent
+else:
+    SITE_ROOT = Path(SITE_ROOT).resolve()
+
+# adding BASE_DIR for compatibility with Django
+BASE_DIR = os.getenv('BASE_DIR', None)
+if not BASE_DIR:
+    BASE_DIR = SITE_ROOT
+else:
+    BASE_DIR = Path(BASE_DIR).resolve()
+
+# for running DataIntegrator
+SERVICES_DIR = BASE_DIR.joinpath('services')
+SETTINGS_DIR = BASE_DIR.joinpath('settings')
+EXTENSION_DIR = BASE_DIR.joinpath('extensions')
+
+config = navigatorConfig(SITE_ROOT)
+
+ENV = config.ENV
+DEBUG = bool(os.getenv('DEBUG', False))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+PRODUCTION = config.getboolean('PRODUCTION', fallback=bool(not DEBUG))
+
+# Add Path Navigator to Sys path
+sys.path.append(str(BASE_DIR))
+sys.path.append(str(SERVICES_DIR))
+sys.path.append(str(SETTINGS_DIR))
+sys.path.append(str(EXTENSION_DIR))
+
+from .conf import *
---
+++ packages/navconfig/navconfig-0.2.17-py3-none-any.whl/navconfig/conf.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import logging
+from pathlib import Path
+
+from .config import navigatorConfig
+config = navigatorConfig()
+
+DEBUG = bool(config.getboolean('DEBUG'))
+print('DEBUG: ', DEBUG)
+
+BASE_DIR = Path(os.getenv('BASE_DIR', config.site_root)).resolve()
+SETTINGS_DIR = BASE_DIR.joinpath('settings')
+print('SETTINGS PATH: ', SETTINGS_DIR)
+
+if str(BASE_DIR) not in sys.path:
+    sys.path.append(str(BASE_DIR))
+
+if str(SETTINGS_DIR) not in sys.path:
+    sys.path.append(str(SETTINGS_DIR))
+
+try:
+    from settings.settings import *
+except (ImportError, ModuleNotFoundError) as err:
+    print(err)
+    # running inside django project:
+    print('Its recommended to use a settings/settings module to customize Navigator Configuration')
+
+"""
+User Local Settings
+"""
+try:
+    from settings.local_settings import *
+except (ImportError, ModuleNotFoundError):
+    pass
