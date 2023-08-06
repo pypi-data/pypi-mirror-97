6993a34dc1228bff81bd39293c8bb4e8f231dc54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:28 +0000

    Add package udacity-test-project-jk8/0.1/udacity_test_project_jk8-0.1.tar.gz

---
+++ packages/udacity-test-project-jk8/udacity_test_project_jk8-0.1.tar.gz/udacity_test_project_jk8-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='udacity_test_project_jk8',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['udacity_test_project_jk8'],
+      author='Jonas Kircher',
+      zip_safe=False)
