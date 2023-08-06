559a017ce34ab1e7cabdb7c436bcdb6adefbea72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:12 +0000

    Add package LsBook/1.0.18/LsBook-1.0.18.tar.gz

---
+++ packages/LsBook/LsBook-1.0.18.tar.gz/LsBook-1.0.18/LsBook/constants/code_extensions.py
@@ -0,0 +1,14 @@
+Extensions = {
+    'csv': 'vim',
+    'vi': 'vim',
+    'js': 'javascript',
+    'py': 'python',
+    'rb': 'ruby',
+    'ps1': 'powershell',
+    'psm1': 'powershell',
+    'sh': 'bash',
+    'bat': 'batch',
+    'cmd': 'batch',
+    'h': 'c',
+    'tex': 'latex'
+}
