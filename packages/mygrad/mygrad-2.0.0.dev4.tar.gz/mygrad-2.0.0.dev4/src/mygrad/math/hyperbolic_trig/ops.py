4d6e6ed2e6260bdc425babc4394530e81743bd0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:42 +0000

    Add package mygrad/2.0.0.dev4/mygrad-2.0.0.dev4.tar.gz

---
+++ packages/mygrad/mygrad-2.0.0.dev4.tar.gz/mygrad-2.0.0.dev4/src/mygrad/math/hyperbolic_trig/ops.py
@@ -0,0 +1,125 @@
+import numpy as np
+
+from mygrad.operation_base import Operation, UnaryUfunc
+
+__all__ = [
+    "Sinh",
+    "Cosh",
+    "Tanh",
+    "Csch",
+    "Sech",
+    "Coth",
+    "" "Arcsinh",
+    "Arccosh",
+    "Arctanh",
+    "Arccsch",
+    "Arccoth",
+]
+
+
+class Sinh(UnaryUfunc):
+    numpy_ufunc = np.sinh
+
+    def backward_var(self, grad, index, **kwargs):
+        (a,) = self.variables
+        return grad * np.cosh(a.data)
+
+
+class Cosh(UnaryUfunc):
+    numpy_ufunc = np.cosh
+
+    def backward_var(self, grad, index, **kwargs):
+        (a,) = self.variables
+        return grad * np.sinh(a.data)
+
+
+class Tanh(UnaryUfunc):
+    numpy_ufunc = np.tanh
+
+    def backward_var(self, grad, index, **kwargs):
+        (a,) = self.variables
+        return grad * (1 - np.tanh(a.data) ** 2)
+
+
+class Csch(Operation):
+    """ f(a) -> csch(a) """
+
+    def __call__(self, a):
+        self.variables = (a,)
+        return 1 / np.sinh(a.data)
+
+    def backward_var(self, grad, index, **kwargs):
+        (a,) = self.variables
+        return grad * -np.cosh(a.data) / np.sinh(a.data) ** 2
+
+
+class Sech(Operation):
+    """ f(a) -> sech(a) """
+
+    def __call__(self, a):
+        self.variables = (a,)
+        return 1 / np.cosh(a.data)
+
+    def backward_var(self, grad, index, **kwargs):
+        (a,) = self.variables
+        return grad * -np.sinh(a.data) / np.cosh(a.data) ** 2
+
+
+class Coth(Operation):
+    """ f(a) -> coth(a) """
+
+    def __call__(self, a):
+        self.variables = (a,)
+        return 1 / np.tanh(a.data)
+
+    def backward_var(self, grad, index, **kwargs):
+        (a,) = self.variables
+        return grad * -1 / np.sinh(a.data) ** 2
+
+
+class Arcsinh(UnaryUfunc):
+    numpy_ufunc = np.arcsinh
+
+    def backward_var(self, grad, index, **kwargs):
+        (a,) = self.variables
+        return grad / np.sqrt(1 + a.data ** 2)
+
+
+class Arccosh(UnaryUfunc):
+    numpy_ufunc = np.arccosh
+
+    def backward_var(self, grad, index, **kwargs):
+        (a,) = self.variables
+        return grad / np.sqrt(a.data ** 2 - 1)
+
+
+class Arctanh(UnaryUfunc):
+    numpy_ufunc = np.arctanh
+
+    def backward_var(self, grad, index, **kwargs):
+        (a,) = self.variables
+        return grad / (1 - a.data ** 2)
+
+
+class Arccsch(Operation):
+    """ f(a) -> arccsch(a) """
+
+    def __call__(self, a):
+        self.variables = (a,)
+        return np.arcsinh(1 / a.data)
+
+    def backward_var(self, grad, index, **kwargs):
+        (a,) = self.variables
+        return -grad / (np.abs(a.data) * np.sqrt(1 + a.data ** 2))
+
+
+class Arccoth(Operation):
+    """ f(a) -> arccoth(a) """
+
+    def __call__(self, a):
+        self.variables = (a,)
+        return np.arctanh(1 / a.data)
+
+    def backward_var(self, grad, index, **kwargs):
+        (a,) = self.variables
+        return grad / (1 - a.data ** 2)
---
+++ packages/mygrad/mygrad-2.0.0.dev4.tar.gz/mygrad-2.0.0.dev4/src/mygrad/math/misc/ops.py
@@ -0,0 +1,81 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+import numpy as np
+
+from mygrad.operation_base import BinaryUfunc, UnaryUfunc
+
+__all__ = ["Abs", "Sqrt", "Cbrt", "Maximum", "Minimum"]
+
+
+class Abs(UnaryUfunc):
+    numpy_ufunc = np.absolute
+
+    def backward_var(self, grad, index, **kwargs):
+        (a,) = self.variables
+        return grad * np.piecewise(
+            a.data, [a.data < 0, a.data == 0, a.data > 0], [-1, np.nan, 1]
+        )
+
+
+class Sqrt(UnaryUfunc):
+    numpy_ufunc = np.sqrt
+
+    def backward_var(self, grad, index, **kwargs):
+        (a,) = self.variables
+        return grad / (2 * np.sqrt(a.data))
+
+
+class Cbrt(UnaryUfunc):
+    numpy_ufunc = np.cbrt
+
+    def backward_var(self, grad, index, **kwargs):
+        (a,) = self.variables
+        return grad / (3 * np.cbrt(a.data ** 2))
+
+
+class _MaxMin(BinaryUfunc, ABC):
+    @staticmethod
+    @abstractmethod
+    def _comparison(
+        self, x1: np.ndarray, x2: np.ndarray
+    ) -> np.ndarray:  # pragma: no cover
+        """Returns `True` where elements of the output were derived from `x1`.
+
+        I.e. max => x1 > x2  ;  min => x1 < x2"""
+        raise NotImplementedError()
+
+    def __init__(self):
+        super().__init__()
+        self._where_tensor_a_was_selected: Optional[np.ndarray] = None
+
+    def backward_var(self, grad, index, **kwargs):
+        a, b = self.variables
+
+        if self._where_tensor_a_was_selected is None:
+            self._where_tensor_a_was_selected = self._comparison(a.data, b.data)
+
+        if index == 0:
+            mask = self._where_tensor_a_was_selected
+        elif index == 1:
+            equal_mask = a.data == b.data
+            mask = np.logical_not(self._where_tensor_a_was_selected)
+
+            if mask.ndim:
+                np.logical_not(mask, out=mask, where=equal_mask)
+            elif equal_mask:
+                mask = np.logical_not(mask)
+        else:  # pragma: no cover
+            raise IndexError(f"Back-propagation through tensor-{index}")
+
+        return mask * grad
+
+
+class Maximum(_MaxMin):
+    numpy_ufunc = np.maximum
+    _comparison = staticmethod(np.greater)
+
+
+class Minimum(_MaxMin):
+    numpy_ufunc = np.minimum
+    _comparison = staticmethod(np.less)
