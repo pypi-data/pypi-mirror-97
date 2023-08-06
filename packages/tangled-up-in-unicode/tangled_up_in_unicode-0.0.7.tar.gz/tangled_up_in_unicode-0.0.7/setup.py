6e40116a475cf84e9c9ba550e766c3851da9a17a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:28 +0000

    Add package tangled-up-in-unicode/0.0.7/tangled_up_in_unicode-0.0.7.tar.gz

---
+++ packages/tangled-up-in-unicode/tangled_up_in_unicode-0.0.7.tar.gz/tangled_up_in_unicode-0.0.7/setup.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+extras_requires = {
+    "dev_docs": ["recommonmark", "sphinx_rtd_theme", "sphinx-autodoc-typehints"],
+    "dev_compile": ["Cython", "pandas"],
+}
+
+tests_require = ["pytest"]
+
+source_root = Path(".")
+with (source_root / "README.md").open(encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="tangled_up_in_unicode",
+    version="0.0.7",
+    description="Access to the Unicode Character Database (UCD)",
+    url="https://github.com/dylan-profiler/tangled-up-in-unicode",
+    license="BSD 4-Clause",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=[],
+    include_package_data=True,
+    python_requires=">=3.5",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+        "Topic :: Database",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Text Processing",
+        "Topic :: Text Processing :: General",
+        "Topic :: Utilities",
+    ],
+)
