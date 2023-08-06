ae9b851fba595f529474bd6e06f285c496734658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:37 +0000

    Add package cl1/1.11.6/cl1-1.11.6.tar.gz

---
+++ packages/cl1/cl1-1.11.6.tar.gz/cl1-1.11.6/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Note!
+# ' are required, do not use any '.
+
+# setup.
+from setuptools import setup, find_packages
+setup(
+	name='cl1',
+	version='1.11.6',
+	description='Some description.',
+	url='http://github.com/vandenberghinc/cl1',
+	author='Daan van den Bergh',
+	author_email='vandenberghinc.contact@gmail.com',
+	license='MIT',
+	packages=find_packages(),
+	zip_safe=False,
+    install_requires=[
+            'fil3s>=2.12.6',
+            'r3sponse>=2.8.4',
+        ])
