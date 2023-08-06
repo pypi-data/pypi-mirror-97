aee0c402dea318c7e550998f40b0807b1be4574f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:35 +0000

    Add package invenio-records-rest/1.6.6/invenio_records_rest-1.6.6-py2.py3-none-any.whl

---
+++ packages/invenio-records-rest/invenio_records_rest-1.6.6-py2.py3-none-any.whl/invenio_records_rest/schemas/fields/sanitizedhtml.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2016-2018 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""HTML sanitized string field."""
+
+from __future__ import absolute_import, print_function
+
+import bleach
+from flask import current_app
+
+from .sanitizedunicode import SanitizedUnicode
+
+
+class SanitizedHTML(SanitizedUnicode):
+    """String field which strips sanitizes HTML using the bleach library."""
+
+    def __init__(self, tags=None, attrs=None, *args, **kwargs):
+        """Initialize field."""
+        super(SanitizedHTML, self).__init__(*args, **kwargs)
+        self.tags = tags
+
+        self.attrs = attrs
+
+    def _deserialize(self, value, attr, data, **kwargs):
+        """Deserialize string by sanitizing HTML."""
+        value = super(SanitizedHTML, self)._deserialize(
+            value, attr, data, **kwargs)
+        return bleach.clean(
+            value,
+            tags=self.tags or current_app.config.get('ALLOWED_HTML_TAGS', []),
+            attributes=self.attrs or current_app.config.get(
+                'ALLOWED_HTML_ATTRS', {}),
+            strip=True,
+        ).strip()
---
+++ packages/invenio-records-rest/invenio_records_rest-1.6.6-py2.py3-none-any.whl/invenio_records_rest/serializers/json.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2016-2018 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Marshmallow based JSON serializer for records."""
+
+from __future__ import absolute_import, print_function
+
+from elasticsearch import VERSION as ES_VERSION
+from flask import json, request
+
+from .base import PreprocessorMixin, SerializerMixinInterface
+from .marshmallow import MarshmallowMixin
+
+lt_es7 = ES_VERSION[0] < 7
+
+
+class JSONSerializerMixin(SerializerMixinInterface):
+    """Mixin serializing records as JSON."""
+
+    @staticmethod
+    def _format_args():
+        """Get JSON dump indentation and separates."""
+        if request and request.args.get('prettyprint'):
+            return dict(
+                indent=2,
+                separators=(', ', ': '),
+            )
+        else:
+            return dict(
+                indent=None,
+                separators=(',', ':'),
+            )
+
+    def serialize(self, pid, record, links_factory=None, **kwargs):
+        """Serialize a single record and persistent identifier.
+
+        :param pid: Persistent identifier instance.
+        :param record: Record instance.
+        :param links_factory: Factory function for record links.
+        """
+        return json.dumps(
+            self.transform_record(pid, record, links_factory, **kwargs),
+            **self._format_args())
+
+    def serialize_search(self, pid_fetcher, search_result, links=None,
+                         item_links_factory=None, **kwargs):
+        """Serialize a search result.
+
+        :param pid_fetcher: Persistent identifier fetcher.
+        :param search_result: Elasticsearch search result.
+        :param links: Dictionary of links to add to response.
+        """
+        total = search_result['hits']['total'] if lt_es7 else \
+            search_result['hits']['total']['value']
+        return json.dumps(dict(
+            hits=dict(
+                hits=[self.transform_search_hit(
+                    pid_fetcher(hit['_id'], hit['_source']),
+                    hit,
+                    links_factory=item_links_factory,
+                    **kwargs
+                ) for hit in search_result['hits']['hits']],
+                total=total,
+            ),
+            links=links or {},
+            aggregations=search_result.get('aggregations', dict()),
+        ), **self._format_args())
+
+
+class JSONSerializer(JSONSerializerMixin, MarshmallowMixin, PreprocessorMixin):
+    """Marshmallow based JSON serializer for records."""
