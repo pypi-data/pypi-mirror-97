54421f58f42b3a3841a77d52322d1114d40a4d51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:39 +0000

    Add package dothttp-req/0.0.6/dothttp_req-0.0.6-py2-none-any.whl

---
+++ packages/dothttp-req/dothttp_req-0.0.6-py2-none-any.whl/dothttp_req/parse_models.py
@@ -0,0 +1,83 @@
+from dataclasses import dataclass
+from typing import List, Optional, Dict
+
+
+@dataclass
+class Allhttp:
+    pass
+
+
+@dataclass
+class NameWrap:
+    name: str
+
+
+@dataclass
+class UrlWrap:
+    method: str
+    url: str
+
+
+@dataclass
+class BasicAuth:
+    username: str
+    password: str
+
+@dataclass
+class Query:
+    key: str
+    value: str
+
+
+@dataclass
+class Header:
+    key: str
+    value: str
+
+
+@dataclass
+class Line:
+    header: Optional[Header]
+    query: Optional[Query]
+
+
+@dataclass
+class MultiPartFile:
+    name: str
+    path: str
+    type: Optional[str]
+
+
+@dataclass
+class FilesWrap:
+    files: List[MultiPartFile]
+
+
+@dataclass
+class Payload:
+    data: Optional[str]
+    datajson: Optional[Dict]
+    file: Optional[str]
+    json: Optional
+    fileswrap: FilesWrap
+    type: Optional[str]
+
+
+@dataclass
+class ToFile:
+    output: str
+
+
+@dataclass
+class Http:
+    namewrap: Optional[NameWrap]
+    urlwrap: UrlWrap
+    basic_auth_wrap: BasicAuth
+    lines: List[Line]
+    payload: Payload
+    output: Optional[ToFile]
+
+
+@dataclass
+class Allhttp:
+    allhttps: List[Http]
---
+++ packages/dothttp-req/dothttp_req-0.0.6-py2-none-any.whl/test/core/test_substition.py
@@ -0,0 +1,83 @@
+import json
+
+from requests import PreparedRequest
+
+from test import TestBase
+from test.core.test_request import dir_path
+
+base_dir = f"{dir_path}/substitution"
+
+
+class SubstitutionTest(TestBase):
+    def test_substitution(self):
+        req = self.get_request(f"{base_dir}/host.http")
+        self.assertEqual("https://dothttp.azurewebsites.net/ram", req.url, "incorrect url")
+
+    def test_substitution_json_query_multiple(self):
+        req = self.get_request(f"{base_dir}/multipleprop.http",
+                               prop=f"{base_dir}/multipleprop.json", )
+        self.assertEqual("https://httpbing.org/ram?haha=value1&key2=ahah", req.url, "incorrect url")
+        self.assertEqual({"qu2": "va2"}, json.loads(req.body), )
+
+    def test_substitution_multiple_env(self, filename=f"{base_dir}/prop1.json"):
+        req = self.get_request(f"{base_dir}/multipleenv.http",
+                               prop=filename,
+                               env=["env1", "env2"])
+        self.assertEqual({"qu2": "va2"}, json.loads(req.body))
+        self.assertEqual('https://httpbing.org/ram?haha=value1&key2=ahah',
+                         req.url)
+
+    def test_substitution_property_file_comments(self):
+        self.test_substitution_multiple_env(f"{base_dir}/prop2.json")
+
+    def test_substitution_with_default_prop(self):
+        req = self.get_request(f"{base_dir}/httpfileprop.http")
+        self.assertEqual("https://google.com/", req.url)
+        self.assertEqual("GET", req.method)
+
+    def test_substitution_commandline(self):
+        req = self.get_request(f"{base_dir}/httpfileprop.http", properties=["dontsubstitute=dothttp.azurewebsites.net"])
+        self.assertEqual("https://dothttp.azurewebsites.net/", req.url)
+        self.assertEqual("GET", req.method)
+
+    def test_substitution_infile_with_quotes(self):
+        req: PreparedRequest = self.get_request(f"{base_dir}/infilepropwithquotes.http")
+        self.assertEqual("https://google.com/", req.url)
+        self.assertEqual("POST", req.method)
+        self.assertEqual(b'{"key": " space in between quotes"}', req.body)
+
+    def test_substitution_infile_with_multiple_suages(self):
+        req: PreparedRequest = self.get_request(f"{base_dir}/infilesinglewithmultipleusages.http")
+        self.assertEquals("https://google.com/", req.url)
+        self.assertEquals(b'{"google.com": "google.com"}', req.body)
+
+    def test_define_on_second_occurence(self):
+        req: PreparedRequest = self.get_request(f"{base_dir}/definevariableonsecond.http")
+        self.assertEquals("https://dothttp.dev/", req.url)
+        self.assertEquals('dothttp.dev', req.body)
+
+    def test_substitution_preference(self):
+        ## command line > env (last env > first env) > infile
+        req: PreparedRequest = self.get_request(f"{base_dir}/simpleinfile.http", prop=f"{base_dir}/simepleinfile.json")
+        self.assertEquals("https://google.com/", req.url)
+        req: PreparedRequest = self.get_request(f"{base_dir}/simpleinfile.http",
+                                                prop=f"{base_dir}/simepleinfile.json",
+                                                env=["env1"]
+                                                )
+        self.assertEquals("https://yahoo.com/", req.url)
+        req: PreparedRequest = self.get_request(f"{base_dir}/simpleinfile.http",
+                                                prop=f"{base_dir}/simepleinfile.json",
+                                                env=["env1", "env2"]
+                                                )
+        self.assertEquals("https://ins.com/", req.url)
+        req: PreparedRequest = self.get_request(f"{base_dir}/simpleinfile.http",
+                                                prop=f"{base_dir}/simepleinfile.json",
+                                                env=["env1", "env2", "env3"]
+                                                )
+        self.assertEquals("https://hub.com/", req.url)
+        req: PreparedRequest = self.get_request(f"{base_dir}/simpleinfile.http",
+                                                prop=f"{base_dir}/simepleinfile.json",
+                                                env=["env1", "env2", "env3"],
+                                                properties=["host=ramba.com"]
+                                                )
+        self.assertEquals("https://ramba.com/", req.url)
