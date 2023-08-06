74288ad0e918029b6984d796f3398ac9c7dc79f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:04 +0000

    Add package discord-github-bot/0.0.0/discord-github-bot-0.0.0.tar.gz

---
+++ packages/discord-github-bot/discord-github-bot-0.0.0.tar.gz/discord-github-bot-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='discord-github-bot',
+    version='0.0.0',
+    description='A Discord Bot to give you some github info',
+    long_description='',
+    url='https://github.com/csci-arch/DiscordGithubBot',
+    author='csci-arch',
+    author_email='csciarch@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
