7f1c3c37af6b0a95e14c7e1e79b08180e11894e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:12 +0000

    Add package django-tcb-blog/0.10.1/django_tcb_blog-0.10.1-py3-none-any.whl

---
+++ packages/django-tcb-blog/django_tcb_blog-0.10.1-py3-none-any.whl/blog/permissions.py
@@ -0,0 +1,69 @@
+from rest_framework import permissions
+
+
+class CanPostButNotRead(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in ('POST'):
+            return True
+        return obj.entry_envelope.author == request.user
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the snippet.
+        return obj.author == request.user
+
+class ReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            return False
+
+
+class IsUserOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user == request.user
+
+
+class CanApprove(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.entry_envelope.author == request.user
+
+
+class InOrganization(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        if obj.organization in user.memberships.all():
+            return True
+        else:
+            return False
+
+
+class InOrganizationForObjectWithRoom(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+
+        if obj.room.organization in user.memberships.all():
+            return True
+        else:
+            return False
---
+++ packages/django-tcb-blog/django_tcb_blog-0.10.1-py3-none-any.whl/blog/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from blog.views import ( EntryViewSet, ListUser, CommentViewSet, SyncConfig, TagViewSet, ViewViewSet,
+                        InteractionViewSet, VisitorProfileViewSet, EntriesFeed, AdminEntryViewSet, AdminCommentViewSet )
+
+router = DefaultRouter()
+
+router.register(r'entries', EntryViewSet, basename='entries')
+router.register(r'admin/entries', AdminEntryViewSet, basename='admin_entries')
+
+router.register(r'comments', CommentViewSet, basename='comments')
+router.register(r'admin/comments', AdminCommentViewSet, basename='admin_comments')
+
+router.register(r'tags', TagViewSet, basename='tags')
+router.register(r'views', ViewViewSet, basename='views')
+router.register(r'interactions', InteractionViewSet, basename='interactions')
+router.register(r'visitor_profiles', VisitorProfileViewSet, basename='visitor_profiles')
+
+urlpatterns = [
+    path('me/', ListUser.as_view()),
+    path('sync_config/', SyncConfig.as_view()),
+    path('rss/', EntriesFeed()),
+] + router.urls
