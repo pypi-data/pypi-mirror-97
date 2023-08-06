8a206505e1464189f73b0e8bd1615c82f9f2aae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:55 +0000

    Add package h1d/0.1.0/h1d-0.1.0.tar.gz

---
+++ packages/h1d/h1d-0.1.0.tar.gz/h1d-0.1.0/README.md
@@ -0,0 +1,25 @@
+# HiC1Dmetrics
+This repository contain code and basic tutorial for "HiC1Dmetrics"
+
+![figure1](https://github.com/wangjk321/HiC1Dmetrics/blob/master/IMG/Figure1.png)
+
+# Installation
+
+HiC1Dmetrics was based on `python3` and released on [PyPI](https://pypi.org/project/h1d/), it could be installed by
+
+```python
+pip install h1d
+```
+After installation, try:
+```
+$ h1d -V
+h1d version 0.1.0
+```
+
+# Documents
+
+For detailed usage, please refer to [h1d Documents](https://h1d.readthedocs.io/en/latest/)
+
+# Web application
+
+Please click [hic1d webapp](http://hic1d.herokuapp.com)
---
+++ packages/h1d/h1d-0.1.0.tar.gz/h1d-0.1.0/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    readme = fh.read()
+
+setup(
+    name='h1d',
+    version='0.1.0',
+    author='wangjiankng',
+    author_email='wangjk321@gmail.com',
+    url='https://github.com/wangjk321/HiC1Dmetrics',
+    description='HiC1Dmetrics pip version',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=['h1d'],
+    include_package_data=True,
+    install_requires=["pandas","numpy","scikit-learn","scipy",
+		"matplotlib","seaborn","multiprocess",
+		"fithic==2.0.7","statsmodels"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+	"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'h1d=h1d.__main__:CLI',
+        ]}
+)
