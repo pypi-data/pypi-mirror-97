51165e03ec5853b67a991be219366b6688b64fe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:17 +0000

    Add package utilitys/0.2.5/utilitys-0.2.5.tar.gz

---
+++ packages/utilitys/utilitys-0.2.5.tar.gz/utilitys-0.2.5/utilitys/typeoverloads.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from os import PathLike
+from pathlib import Path
+from typing import Union
+
+# -----
+# DESCRIPTOR OVERLOADS
+# -----
+
+FilePath = Union[PathLike, str, Path]
