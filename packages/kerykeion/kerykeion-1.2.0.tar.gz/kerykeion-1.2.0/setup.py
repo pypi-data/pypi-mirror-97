2e90428855b3202dcca522425696b6fcde5007f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:33 +0000

    Add package kerykeion/1.2.0/kerykeion-1.2.0.tar.gz

---
+++ packages/kerykeion/kerykeion-1.2.0.tar.gz/kerykeion-1.2.0/setup.py
@@ -0,0 +1,28 @@
+"""
+    This is part of Kerykeion (C) 2020 Giacomo Battaglia
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kerykeion", # Replace with your own username
+    version="1.2.0",
+    author="Giacomo Battaglia",
+    author_email="battaglia.giacomo@yahoo.it",
+    description="A astrology library.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/g-battaglia/kerykeion",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    python_requires='>=3.6',
+    install_requires = ['pyswisseph', 'pytz', 'jsonpickle'],
+)
