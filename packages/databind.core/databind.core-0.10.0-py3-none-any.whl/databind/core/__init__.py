f90b41a60ad5cc02fb13fa6f7f0b176ca532a5b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:17 +0000

    Add package databind.core/0.10.0/databind.core-0.10.0-py3-none-any.whl

---
+++ packages/databind.core/databind.core-0.10.0-py3-none-any.whl/databind/core/__init__.py
@@ -0,0 +1,14 @@
+
+#![feature(dataclasses)]
+#![feature(dict_ordered)]
+#![feature(fstrings)]
+#![feature(member_annotations)]
+
+__author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
+__version__ = '0.10.0'
+
+from ._converter import *
+from ._datamodel import *
+from ._locator import Locator
+from ._union import *
+from .utils import type_repr
