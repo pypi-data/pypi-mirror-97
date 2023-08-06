bdc46b5eab8c3a62d45ab9327ff827dc9984b624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:12 +0000

    Add package pennprov/2.2.12.dev23/pennprov-2.2.12.dev23-py2.py3-none-any.whl

---
+++ packages/pennprov/pennprov-2.2.12.dev23-py2.py3-none-any.whl/pennprov/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from pennprov.api.authentication_api import AuthenticationApi
+from pennprov.api.permission_api import PermissionApi
+from pennprov.api.prov_dm_api import ProvDmApi
+from pennprov.api.provenance_api import ProvenanceApi
