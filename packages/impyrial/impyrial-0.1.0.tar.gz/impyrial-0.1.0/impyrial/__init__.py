2796d30d7a1b43ee22bca4366bf1267a3bdb78ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:26 +0000

    Add package impyrial/0.1.0/impyrial-0.1.0.tar.gz

---
+++ packages/impyrial/impyrial-0.1.0.tar.gz/impyrial-0.1.0/impyrial/__init__.py
@@ -0,0 +1,10 @@
+"""
+impyrial
+========
+A package for converting between imperial 
+measurements of length and weight.
+"""
+
+# Import the length subpackage
+from . import length
+from . import weight
