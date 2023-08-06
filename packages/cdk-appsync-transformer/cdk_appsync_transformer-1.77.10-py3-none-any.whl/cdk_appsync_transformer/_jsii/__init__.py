85e4c57fc7c3c09cea90fb744dec26a69529eb02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:41 +0000

    Add package cdk-appsync-transformer/1.77.10/cdk_appsync_transformer-1.77.10-py3-none-any.whl

---
+++ packages/cdk-appsync-transformer/cdk_appsync_transformer-1.77.10-py3-none-any.whl/cdk_appsync_transformer/_jsii/__init__.py
@@ -0,0 +1,30 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_appsync._jsii
+import aws_cdk.aws_cognito._jsii
+import aws_cdk.aws_dynamodb._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-appsync-transformer",
+    "1.77.10",
+    __name__[0:-6],
+    "cdk-appsync-transformer@1.77.10.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
