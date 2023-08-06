928037db2315c232f61d1fa20cea7aec56de8c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:59 +0000

    Add package kyokusui/0.1.0/kyokusui-0.1.0.tar.gz

---
+++ packages/kyokusui/kyokusui-0.1.0.tar.gz/kyokusui-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['kyokusui']
+
+package_data = \
+{'': ['*'],
+ 'kyokusui': ['static/*',
+              'static/highlight/*',
+              'templates/*',
+              'templates/components/*']}
+
+install_requires = \
+['mitama>=4.5.4,<5.0.0']
+
+setup_kwargs = {
+    'name': 'kyokusui',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'boke0',
+    'author_email': 'speken00.tt@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
