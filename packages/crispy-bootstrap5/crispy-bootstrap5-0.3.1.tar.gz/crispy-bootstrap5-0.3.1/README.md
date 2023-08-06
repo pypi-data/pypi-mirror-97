770c6061cc982310d2aa7120d08938601f8e8777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:51 +0000

    Add package crispy-bootstrap5/0.3.1/crispy-bootstrap5-0.3.1.tar.gz

---
+++ packages/crispy-bootstrap5/crispy-bootstrap5-0.3.1.tar.gz/crispy-bootstrap5-0.3.1/README.md
@@ -0,0 +1,49 @@
+# crispy-bootstrap5
+
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/smithdc1/crispy-bootstrap5/blob/main/LICENSE)
+
+Bootstrap5 template pack for django-crispy-forms
+
+## Installation
+
+Install this plugin using `pip`:
+
+    $ pip install crispy-bootstrap5
+
+## Usage
+
+You will need to update your project's settings file to add ``crispy_forms``
+and ``crispy_bootstrap5`` to your projects ``INSTALLED_APPS``. Also set
+``bootstrap5`` as and allowed template pack and as the default template pack
+for your project::
+
+    INSTALLED_APPS = (
+        ...
+        "crispy_forms",
+        "crispy_bootstrap5",
+        ...
+    )
+
+    CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+    CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+## Development
+
+To contribute to this library, first checkout the code. Then create a new virtual environment:
+
+    cd crispy-bootstrap5
+    python -mvenv venv
+    source venv/bin/activate
+
+Or if you are using `pipenv`:
+
+    pipenv shell
+
+Now install the dependencies and tests:
+
+    pip install -e '.[test]'
+
+To run the tests:
+
+    pytest
---
+++ packages/crispy-bootstrap5/crispy-bootstrap5-0.3.1.tar.gz/crispy-bootstrap5-0.3.1/setup.py
@@ -0,0 +1,38 @@
+import os
+
+from setuptools import setup
+
+VERSION = "0.3.1"
+
+
+def get_long_description():
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
+
+setup(
+    name="crispy-bootstrap5",
+    description="Bootstrap5 template pack for django-crispy-forms",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="David Smith",
+    url="https://github.com/django-crispy-forms/crispy-bootstrap5",
+    project_urls={
+        "Issues": "https://github.com/django-crispy-forms/crispy-bootstrap5/issues",
+        "CI": "https://github.com/django-crispy-forms/crispy-bootstrap5/actions",
+        "Changelog": (
+            "https://github.com/django-crispy-forms/crispy-bootstrap5/releases"
+        ),
+    },
+    license="MIT",
+    version=VERSION,
+    packages=["crispy_bootstrap5"],
+    install_requires=["django-crispy-forms>=1.9.2", "django>=2.2"],
+    extras_require={"test": ["pytest", "pytest-django"]},
+    tests_require=["crispy-bootstrap5[test]"],
+    python_requires=">=3.6",
+    include_package_data=True,
+)
