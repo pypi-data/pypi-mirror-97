2f114f7f5cf9cb15585dccb54275f42e0b314a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:00 +0000

    Add package tctl/0.0.32/tctl-0.0.32.tar.gz

---
+++ packages/tctl/tctl-0.0.32.tar.gz/tctl-0.0.32/tctl/__init__.py
@@ -0,0 +1,63 @@
+import click
+import sys
+
+from pathlib import Path
+__env_path__ = Path.home() / ".tradologics"
+
+try:
+    from .envdev import ENVIRONMENT
+except ImportError:
+    from .env import ENVIRONMENT
+
+if ENVIRONMENT == "dev":
+    __env_path__ = Path.home() / ".tradologics-dev"
+
+# internals
+from . import version
+from . import config
+from . import accounts
+from . import pricing
+from . import universe
+from . import strategies
+from . import tradehooks
+from . import monitors
+from . import orders
+from . import trading
+from . import tokens
+from . import upgrade
+from . import me
+from . import remote
+
+
+if "--version" in sys.argv or "-V" in sys.argv:
+    data, errors = remote.api.get("/version", halt_on_error=False)
+    if data:
+        click.echo(f"\nTctl {version.version} / System {data['version']}")
+    else:
+        click.echo(f"\nTctl {version.version}")
+
+    click.echo("Copyrights (c) Tradologics, Inc.")
+    click.echo("https://tradologics.com")
+    sys.exit()
+
+
+cli = click.CommandCollection(sources=[
+    config.cli,
+    accounts.cli,
+    pricing.cli,
+    universe.cli,
+    strategies.cli,
+    tradehooks.cli,
+    monitors.cli,
+    orders.cli,
+    trading.cli,
+    tokens.cli,
+    upgrade.cli,
+    me.cli,
+])
+
+
+__version__ = version.version
+__author__ = "Tradologics, Inc"
+
+__all__ = ['cli']
---
+++ packages/tctl/tctl-0.0.32.tar.gz/tctl-0.0.32/tctl/trading/__init__.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import click
+import sys
+from .. import utils
+from . import crud
+
+
+commands = {
+    "ls": "  Retreive " + sys.argv[1] + " history: [--account|-a {ACCOUNT_ID}] [--strategy|-s {STRATEGY_ID}] [--start {DATETIME}] [--end {DATETIME}]",
+}
+
+rules = utils.args_actions()
+commands = rules.add_symlinks(commands)
+rules.add_optional("list", {
+    "account": ["-a", "--account"],
+    "strategy": ["-s", "--strategy"],
+    "start": ["--start"],
+    "end": ["--end"],
+})
+
+
+def options_validator(ctx, param, args):
+    return utils.options_validator(args, commands, rules)
+
+
+@click.group()
+def cli():
+    pass
+
+
+# router function
+@cli.command(context_settings={"ignore_unknown_options": True})
+@click.argument('options', nargs=-1, type=click.UNPROCESSED,
+                callback=options_validator)
+def positions(options):
+    """Retreive position history with filtering options"""
+    command, options = options
+
+    if command in ["ls", "list"]:
+        crud.positions_list(options)
+
+
+# router function
+@cli.command(context_settings={"ignore_unknown_options": True})
+@click.argument('options', nargs=-1, type=click.UNPROCESSED,
+                callback=options_validator)
+def trades(options):
+    """Retreive trade history with filtering options"""
+    command, options = options
+
+    if command in ["ls", "list"]:
+        crud.trades_list(options)
