238bfdf5e1d9775ab967601be52da16cea8007f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:58 +0000

    Add package cl1/1.12.5/cl1-1.12.5.tar.gz

---
+++ packages/cl1/cl1-1.12.5.tar.gz/cl1-1.12.5/cl1/classes/exceptions/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# exceptions.
+class InvalidFormatError(Exception):
+	pass
+class ArgumentFormatError(Exception):
+	pass
+class EmptyArgumentError(Exception):
+	pass
+class ArgumentError(Exception):
+	pass
+class UnkownArgumentError(Exception):
+	pass
