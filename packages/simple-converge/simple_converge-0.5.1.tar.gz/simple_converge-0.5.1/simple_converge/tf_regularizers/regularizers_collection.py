94320750dcb4817f81cd84521e50dff0226d809e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:51 +0000

    Add package simple-converge/0.5.1/simple_converge-0.5.1.tar.gz

---
+++ packages/simple-converge/simple_converge-0.5.1.tar.gz/simple_converge-0.5.1/simple_converge/tf_regularizers/regularizers_collection.py
@@ -0,0 +1,8 @@
+from simple_converge.tf_regularizers import L1Regularizer, L1L2Regularizer, L2Regularizer
+
+regularizers_collection = {
+
+    "l1_regularizer": L1Regularizer,
+    "l2_regularizer": L2Regularizer,
+    "l1_l2_regularizer": L1L2Regularizer
+}
