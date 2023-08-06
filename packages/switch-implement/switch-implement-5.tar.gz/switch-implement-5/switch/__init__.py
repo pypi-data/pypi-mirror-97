474cf235e9764f989d8fe48ecdd6c8e9cee04659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:15 +0000

    Add package switch-implement/5/switch-implement-5.tar.gz

---
+++ packages/switch-implement/switch-implement-5.tar.gz/switch-implement-5/switch/__init__.py
@@ -0,0 +1,14 @@
+
+"""
+    upgrades:
+    1.- with pure if elif else you no can use break!
+    2.- is more easy of use
+    3.- contain regex match implementation
+    4.- auto break then no more repetitive (<value> == <case>)
+"""
+
+from switch.switch import switch, bswitch, __version__
+
+__all__ = ["switch", "bswitch"]
+
+
