5c31285cba57f7294e6c772bd7af282246d3710b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:22:46 +0000

    Add package labbox-ephys-widgets-jp/0.1.3a0/labbox_ephys_widgets_jp-0.1.3a0.tar.gz

---
+++ packages/labbox-ephys-widgets-jp/labbox_ephys_widgets_jp-0.1.3a0.tar.gz/labbox_ephys_widgets_jp-0.1.3a0/js/lib/extensionContext.js
@@ -0,0 +1,6 @@
+import { createExtensionContext } from 'labbox';
+import registerExtensions from './registerExtensions';
+const extensionContext = createExtensionContext();
+registerExtensions(extensionContext);
+export default extensionContext;
+//# sourceMappingURL=extensionContext.js.map
