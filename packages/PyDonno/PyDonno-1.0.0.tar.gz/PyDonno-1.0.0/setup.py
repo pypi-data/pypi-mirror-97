aa3c6e863a69c6ea214e8ebd67cfd4a3db5a60dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:33 +0000

    Add package PyDonno/1.0.0/PyDonno-1.0.0.tar.gz

---
+++ packages/PyDonno/PyDonno-1.0.0.tar.gz/PyDonno-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+from distutils.command.install import install
+from subprocess import call
+class Install(install):
+    def run(self):
+        install.run(self)
+        for line in open('requirements.txt').readlines():
+            if len(line) > 1: call(['pip', 'install', line.replace('\n', '')])
+setup(
+    name = 'PyDonno',
+    version = '1.0.0',
+    description = 'All my packages',
+    long_description = open('README.md').read(),
+    url = 'https://github.com/donno2048/PyDonno',
+    license = 'MIT',
+    author = 'Elisha Hollander',
+    author_email = 'just4now666666@gmail.com',
+    cmdclass = {'install': Install}
+)
