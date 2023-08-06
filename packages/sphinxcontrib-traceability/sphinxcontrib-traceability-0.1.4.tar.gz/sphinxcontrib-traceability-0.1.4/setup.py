f4e1edcff3266d72f3867d9cfd2b6aa4fda1633b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:14 +0000

    Add package sphinxcontrib-traceability/0.1.4/sphinxcontrib-traceability-0.1.4.tar.gz

---
+++ packages/sphinxcontrib-traceability/sphinxcontrib-traceability-0.1.4.tar.gz/sphinxcontrib-traceability-0.1.4/setup.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+project_url = 'https://github.com/ociu/sphinx-traceability-extension'
+
+requires = ['Sphinx>=0.6']
+
+setup(
+    name='sphinxcontrib-traceability',
+    version='0.1.4',
+    url=project_url,
+    download_url=project_url + '/archive/v0.1.4.tar.gz',
+    license='GNU General Public License v3 (GPLv3)',
+    author='Oscar Ciudad',
+    author_email='oscar@jacho.net',
+    description='Sphinx traceability extension',
+    long_description=open("README.rst").read(),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Sphinx :: Extension',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Documentation',
+        'Topic :: Documentation :: Sphinx',
+        'Topic :: Utilities',
+    ],
+    platforms='any',
+    packages=find_packages(exclude=['tests', 'example']),
+    include_package_data=True,
+    install_requires=requires,
+    namespace_packages=['sphinxcontrib'],
+    keywords = ['traceability',
+                'requirements engineering',
+                'requirements management',
+                'software engineering',
+                'systems engineering',
+                'sphinx',
+                'requirements'
+            ]
+)
