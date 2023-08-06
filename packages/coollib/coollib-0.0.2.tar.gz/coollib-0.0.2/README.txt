103a69b5578e1b75f0dc023f7e24f764ed65b75a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:14 +0000

    Add package coollib/0.0.2/coollib-0.0.2.tar.gz

---
+++ packages/coollib/coollib-0.0.2.tar.gz/coollib-0.0.2/README.txt
@@ -0,0 +1,1 @@
+The nothing & Everything library!
---
+++ packages/coollib/coollib-0.0.2.tar.gz/coollib-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='coollib',
+  version='0.0.2',
+  description='Nothing & Everything Library, coming soon.',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='aTq_',
+  author_email='',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='cool', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
