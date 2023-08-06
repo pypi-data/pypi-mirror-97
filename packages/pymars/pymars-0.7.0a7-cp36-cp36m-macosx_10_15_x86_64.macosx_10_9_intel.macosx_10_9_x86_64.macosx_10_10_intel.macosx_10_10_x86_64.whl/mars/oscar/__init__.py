2a6109a66c74103a574d7a4ceca0c10ae0944059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:50 +0000

    Add package pymars/0.7.0a7/pymars-0.7.0a7-cp36-cp36m-macosx_10_15_x86_64.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl

---
+++ packages/pymars/pymars-0.7.0a7-cp36-cp36m-macosx_10_15_x86_64.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl/mars/oscar/__init__.py
@@ -0,0 +1,26 @@
+# Copyright 1999-2020 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# import aio to ensure patch enabled for Python 3.6
+from ..lib import aio
+del aio
+
+from .api import actor_ref, create_actor, has_actor, destroy_actor, \
+    kill_actor, Actor, create_actor_pool
+from .errors import ActorNotExist, ActorAlreadyExist
+from .utils import create_actor_ref
+
+# make sure methods are registered
+from .backends import mars, ray
+del mars, ray
