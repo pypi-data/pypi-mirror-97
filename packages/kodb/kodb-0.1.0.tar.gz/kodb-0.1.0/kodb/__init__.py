b3f8c3fa81800e3eb6e237ab30f76dbc10d414c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:53 +0000

    Add package kodb/0.1.0/kodb-0.1.0.tar.gz

---
+++ packages/kodb/kodb-0.1.0.tar.gz/kodb-0.1.0/kodb/__init__.py
@@ -0,0 +1,121 @@
+import sys
+import os
+
+from .log import Message
+MSG = Message("INFO")
+from .download import check_program_availability
+from .make_project import make_project, make_dir_wrapper
+from .utils import style
+
+
+def build(_):
+    from kodb.build import build_document
+    check_program_availability()
+    build_document()
+
+
+def init(_):
+    make_project(".")
+
+
+def new(args):
+    if not args:
+        MSG.error(f"A directory name is required as an argument. Run this command like {style('kodb new <name>', 'bold')}.")
+        sys.exit()
+    make_dir_wrapper(args[0])
+    make_project(args[0])
+
+
+def add(args):
+    from kodb.add_and_remove import add_section
+    if not args:
+        print("Add the name of the section you want to add. Run this command like 'kodb add <section name>'")
+        sys.exit()
+    elif len(args) == 1:
+        add_section(args[0])
+    else:
+        add_section(args[0], args[1])
+
+
+def switch(args):
+    if not len(args) == 2:
+        print("To switch the position of two sections, please include the index or name of the two sections you want to switch place.")
+        sys.exit()
+    from kodb.switch_and_move import switch_sections
+    switch_sections(args[0], args[1])
+
+
+def move(args):
+    pass
+
+
+def remove(args):
+    from kodb.add_and_remove import remove_section
+    for section in args:
+        remove_section(section)
+
+
+def edit(args):
+    from kodb.edit_and_list import edit_project
+    if args:
+        edit_project(args[0])
+    else:
+        edit_project(args)
+
+
+def list(args):
+    from kodb.edit_and_list import list_sections
+    list_sections()
+
+
+def ref(args):
+    from kodb.references import change_style
+    change_style(args[0])
+
+
+def default_doc_structure(_):
+    from kodb.add_and_remove import add_section
+    for section in ["abstract", "introduction", "theory", "results", "discussion", "conclusion", "references", "appendix"]:
+        add_section(section)
+
+
+def help(_):
+    print(f"""Welcome to kodb, a tool which will help you build documents quickly and easily!
+
+To start, create a document in the current directory with {style('kodb init', 'bold')} or create a project directory with {style('kodb new <project name>.', 'bold')}""")
+
+
+command_lookup = {
+    "build": build,
+    "init": init,
+    "new": new,
+    "add": add,
+    "switch": switch,
+    "move": move,
+    "remove": remove,
+    "edit": edit,
+    "list": list,
+    "ref": ref,
+    "help": help,
+    "-h": help,
+    "--help": help,
+    "--default-doc-structure": default_doc_structure
+}
+
+
+def main():
+    args = sys.argv
+    if len(args) == 1:
+        help(args)
+        sys.exit()
+    elif len(args) == 2:
+        command = args[1]
+        command_args = None
+    else:
+        command = args[1]
+        command_args = args[2:]
+
+    try:
+        command_lookup[command](command_args)
+    except KeyError:
+        MSG.error(f"Unknown command \"{command}\"")
---
+++ packages/kodb/kodb-0.1.0.tar.gz/kodb-0.1.0/kodb/add_and_remove.py
@@ -0,0 +1,71 @@
+import os
+import sys
+from kodb.utils import find_root, find_section, style
+from kodb import MSG
+
+ADD_USAGE = f"""Usage:
+    {style('kodb add <section name> <section position (optional)>', 'bold')}"""
+
+REMOVE_USAGE = f"""Usage:
+    {style('kodb remove <section name or index>', 'bold')}"""
+
+
+def add_section(name, index=None):
+    src_path = os.path.join(find_root(), "src")
+
+    src_files = []
+    for file in os.listdir(src_path):
+        sec = file.split("_")
+        src_files.append({"index": int(sec[0]), "name": "_".join(sec[1:]), "path": os.path.join(src_path, file)})
+
+    src_files.sort(key=lambda x: x["index"])
+
+    if index:
+        try:
+            index = int(index)
+        except ValueError:
+            MSG.error(f"Cannot parse the supplied section position {style(index, 'bold')}. The section position must be a parsable integer.")
+            sys.exit()
+
+        for file in src_files:
+            if file["index"] >= index:
+                new_filename = os.path.join(src_path, str(file["index"] + 1).zfill(2) + "_" + file["name"])
+                os.rename(file["path"], new_filename)
+                file["path"] = new_filename
+
+        new_i = index
+    else:
+        new_i = 0
+        for i, file in enumerate(src_files):
+            if i != file["index"]:
+                new_i = i
+                break
+            new_i = i + 1
+
+    new_path = os.path.join(src_path, str(new_i).zfill(2) + "_" + name + ".md")
+
+    if not os.path.isfile(new_path):
+        with open(new_path, "w") as f:
+            if name.lower() == "abstract":
+                f.write("\\begin{abstract}\n\n\\end{abstract}")
+            elif name.lower() == "appendix":
+                f.write("\\clearpage\n\\appendix\n\n# Appendix\n\n")
+            else:
+                f.write(f"# {name.capitalize()}\n\n")
+
+
+def remove_section(sec):
+    src_path = os.path.join(find_root(), "src")
+    remove_path = find_section(sec)
+    renumber = False
+    MSG.info(f"Removing {style(remove_path, 'bold')}...")
+    for file in sorted(os.listdir(src_path)):
+        if renumber:
+            sec = file.split("_")
+            new_filename = os.path.join(src_path, str(int(sec[0]) - 1).zfill(2) + "_" + "_".join(sec[1:]))
+            os.rename(os.path.join(src_path, file), new_filename)
+            continue
+        if os.path.join(src_path, file) == remove_path:
+            os.remove(remove_path)
+            renumber = True
+            MSG.info("Renumbering sections...")
