a91c8a0369810b9c69859043dc28638d528be930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:50 +0000

    Add package dnplab/1.0.3/dnplab-1.0.3-py3-none-any.whl

---
+++ packages/dnplab/dnplab-1.0.3-py3-none-any.whl/unittests/testing.py
@@ -0,0 +1,22 @@
+import numpy as np
+from dnplab.dnpData import dnpdata
+
+
+def get_gauss_3d(std_noise=0.0):
+    x = np.r_[0:100]
+    y = np.r_[0:100]
+    z = np.r_[0:100]
+
+    noise = std_noise * np.random.randn(len(x), len(y), len(z))
+    gauss = np.exp(-1.0 * (x - 50) ** 2.0 / (10.0 ** 2))
+    gauss_3d = (
+        gauss.reshape(-1, 1, 1) * gauss.reshape(1, -1, 1) * gauss.reshape(1, 1, -1)
+    )
+    gauss_3d += noise
+    return x, y, z, gauss_3d
+
+
+def test3d(std_noise=0.0):
+    x, y, z, gauss_3d = get_gauss_3d(std_noise)
+    test_data = dnpdata(gauss_3d, [x, y, z], ["x", "y", "z"])
+    return test_data
