7046e7d208657521875828a4ae305e3bdc2a43c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:23 +0000

    Add package zwembad/1.2.2.dev1/zwembad-1.2.2.dev1-py3-none-any.whl

---
+++ packages/zwembad/zwembad-1.2.2.dev1-py3-none-any.whl/zwembad/__init__.py
@@ -0,0 +1,7 @@
+__author__ = "Robin De Schepper"
+__email__ = "robingilbert.deschepper@unipv.it"
+
+from .pool import MPIPoolExecutor
+from .pool import MPIPoolExecutor as Pool
+
+__version__ = "1.2.2.dev1"
