4c75182db608bdbfb9c4051975c84b63d1e6eb84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:06 +0000

    Add package distributions-hs/0.1/distributions_hs-0.1.tar.gz

---
+++ packages/distributions-hs/distributions_hs-0.1.tar.gz/distributions_hs-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_hs',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['distributions_hs'],
+      author='Hamed Salooti',
+      author_email='hamed.salooti@gmail.com',
+      zip_safe=False)
