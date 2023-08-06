4659be73da5924cfe87cdd8ff505cb2ba94f3fa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:51 +0000

    Add package SATISH-VIRTUAL-ASSISTANT/0.0.2/SATISH%20VIRTUAL%20ASSISTANT-0.0.2.tar.gz

---
+++ packages/SATISH-VIRTUAL-ASSISTANT/SATISH%20VIRTUAL%20ASSISTANT-0.0.2.tar.gz/SATISH VIRTUAL ASSISTANT-0.0.2/README.txt
@@ -0,0 +1,7 @@
+This is a very basic python based voice virtual assistant that performs following tasks
+
+1. It Tells Jokes.
+2. It shows our photo.
+3. It provide you with daily news.
+4. It tells us about weather
+5. It launches various desktop application and many more.
---
+++ packages/SATISH-VIRTUAL-ASSISTANT/SATISH%20VIRTUAL%20ASSISTANT-0.0.2.tar.gz/SATISH VIRTUAL ASSISTANT-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='SATISH VIRTUAL ASSISTANT',
+  version='0.0.2',
+  description="This is a very basic python based voice virtual assistant that performs multiple tasks based on voice commands",
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='SATISH KUMAR TIGGA',
+  author_email='satish.tigga05@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='virtual assistant', 
+  packages=find_packages(),
+  install_requires=["JarvisAI","speech_recognition","pyttsx3","pywhatkit","datetime","wikipedia","pyjokes","re","pprint","random"] 
+)
