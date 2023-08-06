152758adb98988073addd3f6559af54642732828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:29 +0000

    Add package scattertext/0.1.1/scattertext-0.1.1.tar.gz

---
+++ packages/scattertext/scattertext-0.1.1.tar.gz/scattertext-0.1.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+setup(name='scattertext',
+      version='0.1.1',
+      description='An NLP package to visualize interesting terms in text.',
+      url='https://github.com/JasonKessler/scattertext',
+      author='Jason Kessler',
+      author_email='jason.kessler@gmail.com',
+      license='Apache 2.0',
+      packages=find_packages(),
+      install_requires=[
+	      'numpy',
+	      'scipy',
+	      'scikit-learn',
+	      'pandas',
+	      'six',
+          'mock',
+          'statsmodels',
+          'flashtext',
+          'pytextrank==2.1.0'
+	      #'spacy',
+	      #'jieba',
+	      #'tinysegmenter',
+	      #'empath',
+	      #'umap',
+	      #'gensim'
+	      # 'matplotlib',
+	      # 'seaborn',
+	      # 'jupyter',
+      ],
+      package_data={
+	      'scattertext': ['data/*', 'data/viz/*', 'data/viz/*/*']
+      },
+      test_suite="nose.collector",
+      tests_require=['nose'],
+      #setup_requires=['nose>=1.0'],
+      entry_points={
+	      'console_scripts': [
+		      'scattertext = scattertext.CLI:main',
+	      ],
+      },
+      zip_safe=False)
