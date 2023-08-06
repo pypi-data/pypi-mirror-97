012877ba3b45c9e33c7a18f68152525117f2482c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:46 +0000

    Add package pds.api-client/0.1.0/pds.api_client-0.1.0-py3.7.egg

---
+++ packages/pds.api-client/pds.api_client-0.1.0-py3.7.egg/pds/api_client/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from pds.api_client.api.bundles_api import BundlesApi
+from pds.api_client.api.bundles_collections_api import BundlesCollectionsApi
+from pds.api_client.api.capabilities_api import CapabilitiesApi
+from pds.api_client.api.collections_api import CollectionsApi
+from pds.api_client.api.collections_products_api import CollectionsProductsApi
+from pds.api_client.api.products_api import ProductsApi
+from pds.api_client.api.routes_api import RoutesApi
---
+++ packages/pds.api-client/pds.api_client-0.1.0-py3.7.egg/pds_api_client/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from pds_api_client.api.bundles_api import BundlesApi
+from pds_api_client.api.bundles_collections_api import BundlesCollectionsApi
+from pds_api_client.api.capabilities_api import CapabilitiesApi
+from pds_api_client.api.collections_api import CollectionsApi
+from pds_api_client.api.collections_products_api import CollectionsProductsApi
+from pds_api_client.api.products_api import ProductsApi
+from pds_api_client.api.routes_api import RoutesApi
