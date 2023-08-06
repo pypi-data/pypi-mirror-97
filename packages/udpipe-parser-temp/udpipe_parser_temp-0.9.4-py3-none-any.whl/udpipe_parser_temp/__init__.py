e12f8eb19610a8c3705e14909b7c245f3a577e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:32 +0000

    Add package udpipe-parser-temp/0.9.4/udpipe_parser_temp-0.9.4-py3-none-any.whl

---
+++ packages/udpipe-parser-temp/udpipe_parser_temp-0.9.4-py3-none-any.whl/udpipe_parser_temp/__init__.py
@@ -0,0 +1,9 @@
+# encoding=utf8  
+from .udpipe_parser_temp import *
+from .text_preprocessor import TextPreProcessor
+from .dicts import *
+from .utilities import *
+
+__all__ = [
+    'TextPreProcessor','Expression','UDPipeParser'
+]
