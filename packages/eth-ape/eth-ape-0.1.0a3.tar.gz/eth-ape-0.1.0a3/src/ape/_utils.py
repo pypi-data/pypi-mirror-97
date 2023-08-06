74a5ea3510bf9b0f8e4193bb822e24398f17992d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:10 +0000

    Add package eth-ape/0.1.0a3/eth-ape-0.1.0a3.tar.gz

---
+++ packages/eth-ape/eth-ape-0.1.0a3.tar.gz/eth-ape-0.1.0a3/src/ape/_utils.py
@@ -0,0 +1,48 @@
+import collections
+import json
+import os
+from copy import deepcopy
+from pathlib import Path as Path
+from typing import Dict
+
+import yaml
+
+
+def deep_merge(dict1, dict2):
+    """ Return a new dictionary by merging two dictionaries recursively. """
+
+    result = deepcopy(dict1)
+
+    for key, value in dict2.items():
+        if isinstance(value, collections.Mapping):
+            result[key] = deep_merge(result.get(key, {}), value)
+        else:
+            result[key] = deepcopy(dict2[key])
+
+    return result
+
+
+def expand_environment_variables(contents: str) -> str:
+    return os.path.expandvars(contents)
+
+
+def load_config(path: Path, expand_envars=True, must_exist=False) -> Dict:
+    if path.exists():
+        contents = path.read_text()
+        if expand_envars:
+            contents = expand_environment_variables(contents)
+
+        if path.suffix in (".json",):
+            config = json.loads(contents)
+        elif path.suffix in (".yml", ".yaml"):
+            config = yaml.safe_load(contents)
+        else:
+            raise TypeError(f"Cannot parse '{path.suffix}' files!")
+
+        return config
+
+    elif must_exist:
+        raise IOError(f"{path} does not exist!")
+
+    else:
+        return {}
