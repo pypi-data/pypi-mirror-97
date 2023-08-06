2928e4e127bdad67044be9ea2893be912f1ddbfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:20 +0000

    Add package invenio-app-ils/1.0.0a29/invenio_app_ils-1.0.0a29.tar.gz

---
+++ packages/invenio-app-ils/invenio_app_ils-1.0.0a29.tar.gz/invenio_app_ils-1.0.0a29/invenio_app_ils/acquisition/loaders/jsonschemas/vendor.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019-2020 CERN.
+#
+# invenio-app-ils is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Vendor schema for marshmallow loader."""
+
+from invenio_records_rest.schemas import RecordMetadataSchemaJSONV1
+from marshmallow import EXCLUDE, fields
+
+
+class VendorSchemaV1(RecordMetadataSchemaJSONV1):
+    """Vendor schema."""
+
+    class Meta:
+        """Meta attributes for the schema."""
+
+        unknown = EXCLUDE
+
+    address = fields.Str()
+    email = fields.Email()
+    legacy_ids = fields.List(fields.Str())
+    name = fields.Str(required=True)
+    notes = fields.Str()
+    phone = fields.Str()
