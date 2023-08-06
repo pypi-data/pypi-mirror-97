b4f8d91c7bc372deec684eb31ec7a4ce5c80a981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:41 +0000

    Add package duelpy/0.0.1/duelpy-0.0.1-py3-none-any.whl

---
+++ packages/duelpy/duelpy-0.0.1-py3-none-any.whl/duelpy/experiments/environments/__init__.py
@@ -0,0 +1,16 @@
+"""Various dueling bandit environments for use in experiments."""
+
+from duelpy.experiments.environments.hard_condorcet_matrix import HardCondorcetMatrix
+from duelpy.experiments.environments.mallows_model import MallowsModel
+from duelpy.experiments.environments.plackett_luce_model import PlackettLuceModel
+
+# Pylint insists that environment_list is a constant and should be named in
+# UPPER_CASE. See duelpy/algorithms/__init__.py for an explanation of why we
+# disable the check here.
+# pylint: disable=invalid-name
+
+# Make the environment classes available for easy enumeration in experiments
+# and tests.
+environment_list = [HardCondorcetMatrix, PlackettLuceModel, MallowsModel]
+
+__all__ = [environment.__name__ for environment in environment_list]
---
+++ packages/duelpy/duelpy-0.0.1-py3-none-any.whl/duelpy/stats/__init__.py
@@ -0,0 +1,6 @@
+"""Utilities for implementation of PB-MABA algorithms."""
+
+from duelpy.stats import confidence_radius
+from duelpy.stats.preference_estimate import PreferenceEstimate
+
+__all__ = ["PreferenceEstimate", "confidence_radius"]
