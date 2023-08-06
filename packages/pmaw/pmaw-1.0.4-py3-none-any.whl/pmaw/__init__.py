5275c85955e3bfee863dec0040c0509ee3b4faae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:24 +0000

    Add package pmaw/1.0.4/pmaw-1.0.4-py3-none-any.whl

---
+++ packages/pmaw/pmaw-1.0.4-py3-none-any.whl/pmaw/__init__.py
@@ -0,0 +1,17 @@
+# PMAW
+# Copyright 2021 Matthew Podolak
+# See LICENSE for details.
+
+"""
+PMAW: Pushshift Multithread API Wrapper
+"""
+__version__ = '1.0.4'
+__author__ = 'Matthew Podolak'
+__license__ = 'MIT'
+
+from .RateLimit import RateLimit
+from .Request import Request
+from .Response import Response
+from .Cache import Cache
+from .PushshiftAPIBase import PushshiftAPIBase
+from .PushshiftAPI import PushshiftAPI
