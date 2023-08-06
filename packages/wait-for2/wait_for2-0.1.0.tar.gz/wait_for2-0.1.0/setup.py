e754e211c12b022816cd4b06380126d43241f391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:31 +0000

    Add package wait-for2/0.1.0/wait_for2-0.1.0.tar.gz

---
+++ packages/wait-for2/wait_for2-0.1.0.tar.gz/wait_for2-0.1.0/setup.py
@@ -0,0 +1,42 @@
+import ast
+
+from setuptools import setup
+
+author = author_email = version = None
+with open("wait_for2/__init__.py", encoding="utf-8") as f:
+    for line in f:
+        if line.startswith("__author__ = "):
+            author = ast.literal_eval(line[len("__author__ = ") :])
+        elif line.startswith("__author_email__ = "):
+            author_email = ast.literal_eval(line[len("__author_email__ = ") :])
+        elif line.startswith("__version__ = "):
+            version = ast.literal_eval(line[len("__version__ = ") :])
+
+
+description = "Asyncio wait_for that can handle simultaneous cancellation and future completion."
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="wait_for2",
+    version=version,
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Traktormaster/wait-for2",
+    author=author,
+    author_email=author_email,
+    license="Apache 2.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    packages=["wait_for2"],
+)
