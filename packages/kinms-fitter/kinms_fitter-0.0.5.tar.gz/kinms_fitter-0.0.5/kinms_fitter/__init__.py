8016b88ee5f17370e4be1e716083cbda2b2e044c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:30 +0000

    Add package kinms-fitter/0.0.5/kinms_fitter-0.0.5.tar.gz

---
+++ packages/kinms-fitter/kinms_fitter-0.0.5.tar.gz/kinms_fitter-0.0.5/kinms_fitter/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# coding: utf-8
+from kinms_fitter.kinms_fitter import kinms_fitter
+from kinms_fitter.sb_profs import sb_profs
+from kinms_fitter.velocity_profs import velocity_profs
---
+++ packages/kinms-fitter/kinms_fitter-0.0.5.tar.gz/kinms_fitter-0.0.5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+ 
+setup(name='kinms_fitter',
+       version='0.0.5',
+       description='Wrapper for KinMSpy that automates most common galaxy fitting tasks',
+       url='https://github.com/TimothyADavis/KinMS_fitter',
+       author='Timothy A. Davis',
+       author_email='DavisT@cardiff.ac.uk',
+       long_description=long_description,
+       long_description_content_type="text/markdown",
+       license='GNU GPLv3',
+       packages=['kinms_fitter','kinms_fitter.docs'],
+       install_requires=[
+           'numpy',
+           'gastimator',
+           'astropy',
+           'matplotlib',
+           'jampy',
+           'kinms',
+       ],
+       classifiers=[
+         'Development Status :: 3 - Alpha',
+         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+         'Programming Language :: Python :: 3',
+         'Operating System :: OS Independent',
+       ],
+       zip_safe=True)
+       
