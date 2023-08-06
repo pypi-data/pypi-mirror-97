72a01fdcfbdb84c9321ec9b3c288a1249c7c8b11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:25 +0000

    Add package pythondata-misc-opentitan/0.0.post5260/pythondata_misc_opentitan-0.0.post5260-py3-none-any.whl

---
+++ packages/pythondata-misc-opentitan/pythondata_misc_opentitan-0.0.post5260-py3-none-any.whl/pythondata_misc_opentitan/__init__.py
@@ -0,0 +1,62 @@
+import os.path
+__dir__ = os.path.split(os.path.abspath(os.path.realpath(__file__)))[0]
+data_location = os.path.join(__dir__, "resources")
+src = "https://github.com/lowRISC/opentitan"
+
+# Module version
+version_str = "0.0.post5260"
+version_tuple = (0, 0, 5260)
+try:
+    from packaging.version import Version as V
+    pversion = V("0.0.post5260")
+except ImportError:
+    pass
+
+# Data version info
+data_version_str = "0.0.post5168"
+data_version_tuple = (0, 0, 5168)
+try:
+    from packaging.version import Version as V
+    pdata_version = V("0.0.post5168")
+except ImportError:
+    pass
+data_git_hash = "95cea45d4f2912324a0fba32e97e2f87a14bea03"
+data_git_describe = "v0.0-5168-g95cea45d4"
+data_git_msg = """\
+commit 95cea45d4f2912324a0fba32e97e2f87a14bea03
+Author: Pirmin Vogel <vogelpi@lowrisc.org>
+Date:   Tue Mar 2 08:54:01 2021 +0100
+
+    [aes] Interface CSRNG through EDN
+    
+    This commit interfaces the AES unit with CSRNG/EDN to reseed the internal
+    PRNGs for clearing and masking. Since on the SCA platform, we don't have
+    enough resources to implement CSRNG and EDN, a new parameter
+    SecSkipPRNGReseeding is added to skip reseeding requests on the SCA
+    platform.
+    
+    If the reseeding was done with deterministic input instead of entropy
+    provided by CSRNG, this would result in quickly repeating, deterministic
+    PRNG output which is not suitable for evaluating SCA resistance.
+    
+    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
+
+"""
+
+# Tool version info
+tool_version_str = "0.0.post92"
+tool_version_tuple = (0, 0, 92)
+try:
+    from packaging.version import Version as V
+    ptool_version = V("0.0.post92")
+except ImportError:
+    pass
+
+
+def data_file(f):
+    """Get absolute path for file inside pythondata_misc_opentitan."""
+    fn = os.path.join(data_location, f)
+    fn = os.path.abspath(fn)
+    if not os.path.exists(fn):
+        raise IOError("File {f} doesn't exist in pythondata_misc_opentitan".format(f))
+    return fn
