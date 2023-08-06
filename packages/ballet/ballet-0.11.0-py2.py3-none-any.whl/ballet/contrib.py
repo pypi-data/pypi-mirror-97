c3d486cbf3755489248a23b1912c752df7ede574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:01 +0000

    Add package ballet/0.11.0/ballet-0.11.0-py2.py3-none-any.whl

---
+++ packages/ballet/ballet-0.11.0-py2.py3-none-any.whl/ballet/contrib.py
@@ -0,0 +1,129 @@
+import pkgutil
+import types
+from importlib.abc import PathEntryFinder
+from types import ModuleType
+from typing import Iterator, List, Optional, cast
+
+from funcy import collecting, notnone
+
+import ballet
+from ballet.feature import Feature
+from ballet.util import dfilter
+from ballet.util.log import logger
+
+__all__ = (
+    'collect_contrib_features',
+)
+
+
+def collect_contrib_features(
+    project: 'ballet.project.Project'
+) -> List[Feature]:
+    """Collect contributed features for a project at project_root
+
+    For a project ``foo``, walks modules within the ``foo.features.contrib``
+    subpackage. A single object that is an instance of ``ballet.Feature`` is
+    imported if present in each module. The resulting ``Feature`` objects are
+    collected.
+
+    Args:
+        project: project object
+
+    Returns:
+        collected features
+    """
+    contrib = project.resolve('features.contrib')
+    return _collect_contrib_features(contrib)
+
+
+@dfilter(notnone)
+@collecting
+def _collect_contrib_features(
+    module: ModuleType
+) -> Iterator[Optional[Feature]]:
+    """Collect contributed features from within given module
+
+    Be very careful with untrusted code. The module/package will be
+    walked, every submodule will be imported, and all the code therein will be
+    executed. But why would you be trying to import from an untrusted package
+    anyway?
+
+    Args:
+        module: module (standalone or package) that contains feature
+            definitions
+    """
+
+    if isinstance(module, types.ModuleType):
+        # any module that has a __path__ attribute is also a package
+        if hasattr(module, '__path__'):
+            yield from _collect_contrib_features_from_package(module)
+        else:
+            yield _collect_contrib_feature_from_module(module)
+    else:
+        raise ValueError('Input is not a module')
+
+
+@collecting
+def _collect_contrib_features_from_package(
+    package: ModuleType
+) -> Iterator[Optional[Feature]]:
+    logger.debug(
+        'Walking package path {path} to detect modules...'
+        .format(path=package.__path__))  # type: ignore  # mypy issue #1422
+
+    for importer, modname, _ in pkgutil.walk_packages(
+            path=package.__path__,  # type: ignore  # mypy issue #1422
+            prefix=package.__name__ + '.',
+            onerror=logger.error):
+
+        # mistakenly typed as MetaPathFinder
+        importer = cast(PathEntryFinder, importer)
+
+        try:
+            if importer is None:
+                raise ImportError
+            # TODO use find_spec
+            # https://docs.python.org/3/library/importlib.html#importlib.abc.PathEntryFinder.find_spec)
+            finder = importer.find_module(modname)
+            if finder is None:
+                raise ImportError
+            mod = finder.load_module(modname)
+        except ImportError:
+            logger.exception(
+                'Failed to import module {modname}'
+                .format(modname=modname))
+            continue
+
+        yield _collect_contrib_feature_from_module(mod)
+
+
+def _collect_contrib_feature_from_module(mod: ModuleType) -> Optional[Feature]:
+    logger.debug(
+        'Trying to import contributed feature from module {modname}...'
+        .format(modname=mod.__name__))
+
+    candidates = []
+    for attr in dir(mod):
+        obj = getattr(mod, attr)
+        if isinstance(obj, Feature):
+            candidates.append(obj)
+
+    if len(candidates) == 1:
+        feature = candidates[0]
+        feature.source = mod.__name__
+        logger.debug(
+            'Imported 1 feature from {modname} from {Feature.__name__} object'
+            .format(modname=mod.__name__, Feature=Feature))
+        return feature
+    elif len(candidates) > 1:
+        logger.debug(
+            'Found too many {Feature.__name__} objects in module {modname}, '
+            'skipping; candidates were {candidates!r}'
+            .format(Feature=Feature, modname=mod.__name__,
+                    candidates=candidates))
+        return None
+    else:
+        logger.debug(
+            'Failed to import anything useful from module {modname}'
+            .format(modname=mod.__name__))
+        return None
---
+++ packages/ballet/ballet-0.11.0-py2.py3-none-any.whl/ballet/templating.py
@@ -0,0 +1,160 @@
+import pathlib
+import tempfile
+from typing import List, Optional, Tuple
+
+import funcy as fy
+from cookiecutter.main import cookiecutter as _cookiecutter
+
+from ballet.compat import PathLike
+from ballet.exc import ConfigurationError
+from ballet.project import Project, detect_github_username
+from ballet.util.fs import pwalk, synctree
+from ballet.util.git import switch_to_new_branch
+from ballet.util.log import logger
+from ballet.util.typing import Pathy
+from ballet.validation.project_structure.checks import (
+    FEATURE_MODULE_NAME_REGEX, SUBPACKAGE_NAME_REGEX,)
+
+TEMPLATES_PATH = pathlib.Path(__file__).resolve().parent.joinpath('templates')
+FEATURE_TEMPLATE_PATH = TEMPLATES_PATH.joinpath('feature_template')
+PROJECT_TEMPLATE_PATH = TEMPLATES_PATH.joinpath('project_template')
+
+
+def _stringify_path(obj) -> str:
+    return str(obj) if isinstance(obj, PathLike) else obj
+
+
+@fy.wraps(_cookiecutter)
+def cookiecutter(*args, **kwargs) -> str:
+    """Call cookiecutter.main.cookiecutter after stringifying paths
+
+    Return:
+        project directory path
+    """
+    args = fy.walk(_stringify_path, args)
+    kwargs = fy.walk_values(_stringify_path, kwargs)
+    return _cookiecutter(*args, **kwargs)
+
+
+def render_project_template(
+    project_template_path: Optional[Pathy] = None, **cc_kwargs
+) -> str:
+    """Generate a ballet project according to the project template
+
+    Args:
+        project_template_path: path to specific project template
+        **cc_kwargs: options for the cookiecutter template
+    """
+    if project_template_path is None:
+        project_template_path = PROJECT_TEMPLATE_PATH
+    return cookiecutter(project_template_path, **cc_kwargs)
+
+
+def render_feature_template(**cc_kwargs) -> str:
+    """Create a stub for a new feature
+
+    Args:
+        **cc_kwargs: options for the cookiecutter template
+    """
+    feature_template_path = FEATURE_TEMPLATE_PATH
+    return cookiecutter(feature_template_path, **cc_kwargs)
+
+
+def _fail_if_feature_exists(dst: pathlib.Path) -> None:
+    subpackage_name, feature_name = str(dst.parent), str(dst.name)
+    if (
+        dst.is_file()
+        and fy.re_test(SUBPACKAGE_NAME_REGEX, subpackage_name)
+        and fy.re_test(FEATURE_MODULE_NAME_REGEX, feature_name)
+    ):
+        raise FileExistsError(
+            'The feature already exists here: {dst}'
+            .format(dst=dst))
+
+
+def start_new_feature(
+    contrib_dir: Pathy = None,
+    branching: bool = True,
+    **cc_kwargs
+) -> List[Tuple[pathlib.Path, str]]:
+    """Start a new feature within a ballet project
+
+    If run from default branch, by default will attempt to switch to a new
+    branch for this feature, given by `<username>/feature-<featurename>`. By
+    default, will prompt the user for input using cookiecutter's input
+    interface.
+
+    Renders the feature template into a temporary directory, then copies the
+    feature files into the proper path within the contrib directory.
+
+    Args:
+        contrib_dir: directory under which to place contributed features
+        branching: whether to attempt to manage branching
+        **cc_kwargs: options for the cookiecutter template
+
+    Raises:
+        ballet.exc.BalletError: the new feature has the same name as an
+            existing one
+    """
+    if contrib_dir is not None:
+        try:
+            project = Project.from_path(contrib_dir, ascend=True)
+            default_username = detect_github_username(project)
+        except ConfigurationError:
+            default_username = 'username'
+    else:
+        project = Project.from_cwd()
+        contrib_dir = project.config.get('contrib.module_path')
+        default_username = detect_github_username(project)
+
+    # inject default username into context
+    cc_kwargs.setdefault('extra_context', {})
+    cc_kwargs['extra_context'].update({'_default_username': default_username})
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        # render feature template
+        output_dir = tempdir
+        cc_kwargs['output_dir'] = output_dir
+        rendered_dir = render_feature_template(**cc_kwargs)
+
+        # clean pyc files from rendered dir
+        for path in pwalk(rendered_dir, topdown=False):
+            if path.suffix == '.pyc':
+                path.unlink()
+            if path.name == '__pycache__':
+                with fy.suppress(OSError):
+                    path.rmdir()
+
+        # copy into contrib dir
+        src = rendered_dir
+        dst = contrib_dir
+        result = synctree(src, dst, onexist=_fail_if_feature_exists)
+
+    if branching and project.on_master:
+        # what is the target branch?
+        target_branch = None
+        paths = [path for path, kind in result if kind == 'file']
+        for path in paths:
+            parts = pathlib.Path(path).parts
+            subpackage, module = parts[-2], parts[-1]
+            user_match = fy.re_find(SUBPACKAGE_NAME_REGEX, subpackage)
+            feature_match = fy.re_find(FEATURE_MODULE_NAME_REGEX, module)
+            if feature_match:
+                username = user_match['username']
+                featurename = feature_match['featurename'].replace('_', '-')
+                target_branch = f'{username}/feature-{featurename}'
+
+        if target_branch is not None:
+            switch_to_new_branch(project.repo, target_branch)
+
+    _log_start_new_feature_success(result)
+
+    return result
+
+
+def _log_start_new_feature_success(result: List[Tuple[pathlib.Path, str]]):
+    logger.info('Start new feature successful.')
+    for (name, kind) in result:
+        if kind == 'file' and '__init__' not in str(name):
+            relname = pathlib.Path(name).relative_to(pathlib.Path.cwd())
+            logger.info('Created {}'.format(relname))
