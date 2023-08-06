69d86b6148dc7b14823fe1713e05ba3ecfc82e2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:38 +0000

    Add package drf-tweaks/0.9.6/drf_tweaks-0.9.6.tar.gz

---
+++ packages/drf-tweaks/drf_tweaks-0.9.6.tar.gz/drf_tweaks-0.9.6/drf_tweaks/__init__.py
@@ -0,0 +1,8 @@
+__title__ = "DRF Tweaks"
+__version__ = "0.9.6"
+__author__ = "Pawel Krzyzaniak"
+__license__ = "MIT"
+__copyright__ = "Copyright 2019 Ro"
+
+# Version synonym
+VERSION = __version__
---
+++ packages/drf-tweaks/drf_tweaks-0.9.6.tar.gz/drf_tweaks-0.9.6/setup.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import io
+import os
+import re
+
+from setuptools import setup
+
+
+def local_open(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname))
+
+
+def read_md(f):
+    return io.open(f, 'r', encoding='utf-8').read()
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+version = get_version('drf_tweaks')
+
+
+requirements = local_open('requirements/requirements-base.txt')
+required_to_install = [dist.strip() for dist in requirements.readlines()]
+
+setup(
+    name='drf_tweaks',
+    version=version,
+    url='https://github.com/HealthByRo/drf_tweaks',
+    license='MIT',
+    description='Set of tweaks for Django Rest Framework',
+    long_description=read_md('README.rst'),
+    author='Pawel Krzyzaniak',
+    author_email='pawelk@ro.co',
+    packages=get_packages('drf_tweaks'),
+    package_data=get_package_data('drf_tweaks'),
+    install_requires=required_to_install,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
