a55d2b0821b31201bbbe9213b953c3ad1fd60aa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:52 +0000

    Add package mdcrawler/0.0.6/mdcrawler-0.0.6.tar.gz

---
+++ packages/mdcrawler/mdcrawler-0.0.6.tar.gz/mdcrawler-0.0.6/setup.py
@@ -0,0 +1,25 @@
+from glob import glob
+from os.path import basename
+from os.path import splitext
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+
+setup(
+    name="mdcrawler",
+    version="0.0.6",
+    description="Mildom(https://www.mildom.com/) crawler written in Python",
+    author="kthimuo",
+    url="https://github.com/kthimuo/mildom-crawler",
+    packages=find_packages("mdcrawler"),
+    package_dir={"": "mdcrawler"},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+)
+
