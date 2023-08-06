124c10d8f3d86a1745e8d70801a02289f4c33ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:11 +0000

    Add package gym-jiminy-zoo/1.6.1/gym_jiminy_zoo-1.6.1-py3-none-any.whl

---
+++ packages/gym-jiminy-zoo/gym_jiminy_zoo-1.6.1-py3-none-any.whl/gym_jiminy/envs/__init__.py
@@ -0,0 +1,60 @@
+from gym.envs.registration import register
+
+from .cartpole import CartPoleJiminyEnv
+from .acrobot import AcrobotJiminyEnv, AcrobotJiminyGoalEnv
+from .ant import AntEnv
+from .anymal import ANYmalJiminyEnv, ANYmalPDControlJiminyEnv
+from .atlas import AtlasJiminyEnv, AtlasPDControlJiminyEnv
+from .spotmicro import SpotmicroJiminyEnv
+
+
+__all__ = [
+    'CartPoleJiminyEnv',
+    'AcrobotJiminyEnv',
+    'AcrobotJiminyGoalEnv',
+    'AntEnv',
+    'ANYmalJiminyEnv',
+    'ANYmalPDControlJiminyEnv',
+    'AtlasJiminyEnv',
+    'AtlasPDControlJiminyEnv',
+    'SpotmicroJiminyEnv'
+]
+
+register(
+    id='jiminy-cartpole-v0',
+    entry_point='gym_jiminy.envs:CartPoleJiminyEnv',
+    reward_threshold=475.0,
+    max_episode_steps=500
+)
+register(
+    id='jiminy-acrobot-v0',
+    entry_point='gym_jiminy.envs:AcrobotJiminyEnv',
+    reward_threshold=-100.0,
+    max_episode_steps=500
+)
+register(
+    id='jiminy-ant-v0',
+    entry_point='gym_jiminy.envs:AntEnv',
+    max_episode_steps=1000,
+    reward_threshold=6000.0
+)
+register(
+    id='jiminy-anymal-v0',
+    entry_point='gym_jiminy.envs:ANYmalJiminyEnv'
+)
+register(
+    id='jiminy-anymal-pid-v0',
+    entry_point='gym_jiminy.envs:ANYmalPDControlJiminyEnv'
+)
+register(
+    id='jiminy-atlas-v0',
+    entry_point='gym_jiminy.envs:AtlasJiminyEnv'
+)
+register(
+    id='jiminy-atlas-pid-v0',
+    entry_point='gym_jiminy.envs:AtlasPDControlJiminyEnv'
+)
+register(
+    id='jiminy-spotmicro-v0',
+    entry_point='gym_jiminy.envs:SpotmicroJiminyEnv'
+)
