ee07a0f0e56635b8fca6456bcefb3d6fd60cb320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:48 +0000

    Add package wog/1.0.2/wog-1.0.2.tar.gz

---
+++ packages/wog/wog-1.0.2.tar.gz/wog-1.0.2/wog/MainGame.py
@@ -0,0 +1,23 @@
+from wog.Live import load_game, welcome
+
+def main():
+    play_again = True
+    valid_input = False
+    player_name = input("Hi!\nWhat is your name?\n")
+    print(welcome(player_name))
+    while play_again:
+        play_again = False
+        valid_input = False
+        load_game()
+        while not valid_input:
+            user_choice = input("\nDo you want to try again?\nPlease enter \'Yes\' or \'No\'.\n")
+            if user_choice == 'Yes':
+                valid_input = True
+                play_again = True
+            elif user_choice == 'No':
+                valid_input = True
+                play_again = False
+            else:
+                print("Invalid input.\n")
+
+main()
