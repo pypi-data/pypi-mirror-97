fc71f753b362aedaa85542a136dde4fab6b2ed36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:15 +0000

    Add package r3sponse/2.10.4/r3sponse-2.10.4.tar.gz

---
+++ packages/r3sponse/r3sponse-2.10.4.tar.gz/r3sponse-2.10.4/r3sponse/classes/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+from Response.classes.r3sponse import r3sponse, ResponseObject
+from Response.classes.utils import symbol, color
+
---
+++ packages/r3sponse/r3sponse-2.10.4.tar.gz/r3sponse-2.10.4/r3sponse/classes/config/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+import os, sys, requests, ast, json, glob, platform, subprocess, random
+import traceback as _traceback_
+
+# inc imports.
+from fil3s import *
+
+# DEPRICATED.
+Response.log("&RED&DEPRICATED&ORANGE&: Package r3sponse has moved to package fil3s.&END& Import as 'from fil3s import *' & replace 'Response' to 'Response'.")
+
+# functions.
+def __get_operating_system__():
+	os = platform.system().lower()
+	if os in ["darwin"]: return "macos"
+	elif os in ["linux"]: return "linux"
+	else: raise ValueError(f"Unsupported operating system: [{os}].")
+
+# source.
+ALIAS = SOURCE_NAME = "r3sponse"
+SOURCE = Directory(gfp.base(__file__, back=3))
+OS = __get_operating_system__()
+
+# options.
+ERROR_TRACEBACK = os.environ.get("R3SPONSE_ERROR_TRACEBACK")
+if str(ERROR_TRACEBACK) in ["True", "true", True]: ERROR_TRACEBACK = True
+else: ERROR_TRACEBACK = False
