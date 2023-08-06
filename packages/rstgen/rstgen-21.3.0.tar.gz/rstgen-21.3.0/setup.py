6bc71e801bbb9d45b9cc17d145350d8df7b4f7b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:39 +0000

    Add package rstgen/21.3.0/rstgen-21.3.0.tar.gz

---
+++ packages/rstgen/rstgen-21.3.0.tar.gz/rstgen-21.3.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+SETUP_INFO = dict(
+    name='rstgen',
+    version='21.3.0',
+    packages=['rstgen'],
+    install_requires=[],
+    tests_require=['atelier'],
+    test_suite='tests',
+    description="Generate rst chunks from Python objects.",
+    license='BSD-2-Clause',
+    author='Luc Saffre',
+    author_email='luc.saffre@gmail.com')
+
+SETUP_INFO.update(classifiers="""\
+Programming Language :: Python
+Programming Language :: Python :: 3
+Development Status :: 5 - Production/Stable
+Intended Audience :: Developers
+License :: OSI Approved :: BSD License
+Natural Language :: English
+Operating System :: OS Independent""".splitlines())
+
+SETUP_INFO.update(long_description=open("README.rst").read())
+
+if __name__ == '__main__':
+    setup(**SETUP_INFO)
