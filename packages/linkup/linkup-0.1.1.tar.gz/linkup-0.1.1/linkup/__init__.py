4e2dd4963482351847c9de07d547970e4d6255bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:59 +0000

    Add package linkup/0.1.1/linkup-0.1.1.tar.gz

---
+++ packages/linkup/linkup-0.1.1.tar.gz/linkup-0.1.1/linkup/__init__.py
@@ -0,0 +1,10 @@
+"""
+Operating on values of mappings (by default, dictionaries) while keeping keys aligned
+"""
+
+from linkup.base import (
+    map_op_val,
+    key_aligned_val_op_with_forced_defaults,
+    key_aligned_val_op,
+    OperableMapping,
+)
