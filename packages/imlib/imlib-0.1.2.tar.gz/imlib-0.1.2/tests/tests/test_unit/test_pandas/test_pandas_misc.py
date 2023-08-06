2b9727d90e527035ca9be18513da9ab137fced98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:07 +0000

    Add package imlib/0.1.2/imlib-0.1.2.tar.gz

---
+++ packages/imlib/imlib-0.1.2.tar.gz/imlib-0.1.2/tests/tests/test_unit/test_pandas/test_pandas_misc.py
@@ -0,0 +1,34 @@
+import pytest
+import pandas as pd
+import numpy as np
+
+from imlib.pandas import misc as pandas_misc
+
+columns = ["name", "number"]
+data_with_nan = [["one", np.nan], ["two", 15], ["three", np.nan]]
+df_with_nan = pd.DataFrame(data_with_nan, columns=columns)
+data_with_inf = [["one", np.inf], ["two", 15], ["three", np.inf]]
+df_with_inf = pd.DataFrame(data_with_inf, columns=columns)
+
+
+def test_initialise_df():
+    test_df = pandas_misc.initialise_df("one", "two", "3")
+    df = pd.DataFrame(columns=["one", "two", "3"])
+    assert df.equals(test_df)
+
+
+def test_sanitise_df():
+    sanitised_df = pandas_misc.sanitise_df(df_with_inf)
+    assert sanitised_df.equals(df_with_nan)
+
+
+def test_move_column_first():
+    column_first = pandas_misc.move_column_first(df_with_nan, "number")
+    assert column_first.columns[0] == columns[1]
+    assert column_first[columns[1]][1] == 15
+
+    with pytest.raises(ValueError):
+        pandas_misc.move_column_first(data_with_nan, "number")
+
+    with pytest.raises(ValueError):
+        pandas_misc.move_column_first(df_with_nan, columns)
