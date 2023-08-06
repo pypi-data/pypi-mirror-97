bbffc91d3ebf39bb133a11c2c7dcc1ac64b3f5e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:13 +0000

    Add package py3dep/0.10.0/py3dep-0.10.0-py2.py3-none-any.whl

---
+++ packages/py3dep/py3dep-0.10.0-py2.py3-none-any.whl/py3dep/__init__.py
@@ -0,0 +1,12 @@
+"""Top-level package for Py3DEP."""
+from pkg_resources import DistributionNotFound, get_distribution
+
+from .exceptions import InvalidInputType
+from .print_versions import show_versions
+from .py3dep import deg2mpm, elevation_bycoords, elevation_bygrid, get_map
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = "999"
