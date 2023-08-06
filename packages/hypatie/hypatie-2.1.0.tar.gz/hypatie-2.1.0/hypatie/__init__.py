9c9007b6c15f10d55e37dd1ae3f0177cbd8323aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:30 +0000

    Add package hypatie/2.1.0/hypatie-2.1.0.tar.gz

---
+++ packages/hypatie/hypatie-2.1.0.tar.gz/hypatie-2.1.0/hypatie/__init__.py
@@ -0,0 +1,7 @@
+from .core import *
+from .animation import *
+from .plots import *
+from .transform import *
+from .simbad import explore_region, search_region, sky, search_sky, explore_sky
+
+__version__ = "2.1.0"
