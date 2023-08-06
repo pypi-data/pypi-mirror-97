ada221aa7671226cd5559c06c72fd282044e5366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:12 +0000

    Add package Listing/1.2.0/Listing-1.2.0.tar.gz

---
+++ packages/Listing/Listing-1.2.0.tar.gz/Listing-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+	name ='Listing',
+	version = "1.2.0",
+	py_modules = ['Listing'],
+	author = "Santanu",
+	author_email = "ghosalsantanuit@gmail.com",
+	url = "http://wastelandersdiary.blogspot.com/",
+	description = "A Simple Python List Reader using Nested Function"
+	)
