78127c1a12d819f4ad939443225d725428c19441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:47 +0000

    Add package ukboards/0.5.1/ukboards-0.5.1-py2.py3-none-any.whl

---
+++ packages/ukboards/ukboards-0.5.1-py2.py3-none-any.whl/ukboards/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for UK Boards."""
+
+__author__ = """Griffith Rees"""
+__email__ = "griff.rees@gmail.com"
+# fmt: off
+__version__ = '0.5.1'
+# fmt: on
