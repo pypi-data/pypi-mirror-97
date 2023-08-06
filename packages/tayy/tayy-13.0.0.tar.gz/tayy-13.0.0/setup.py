8db192231391733607d3024544a568c7fc4de863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:02 +0000

    Add package tayy/13.0.0/tayy-13.0.0.tar.gz

---
+++ packages/tayy/tayy-13.0.0.tar.gz/tayy-13.0.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+import setuptools
+import tayy
+setuptools.setup(
+    name="tayy", # Replace with your own username
+    version="13.0.0",
+    author="wing",
+    author_email="100102888@qq.com",
+    description="A small example package",
+    long_description_content_type="text/markdown",
+    url="https://github.com/ladjrong/tayy2",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2',
+)
