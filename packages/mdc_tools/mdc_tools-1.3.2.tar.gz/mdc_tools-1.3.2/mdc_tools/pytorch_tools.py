dd5d48b8b1697feccf64e49be849d7d8c57fadff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:06 +0000

    Add package mdc_tools/1.3.2/mdc_tools-1.3.2.tar.gz

---
+++ packages/mdc_tools/mdc_tools-1.3.2.tar.gz/mdc_tools-1.3.2/mdc_tools/pytorch_tools.py
@@ -0,0 +1,72 @@
+# encoding: utf-8
+"""
+@author:  liaoxingyu
+@contact: sherlockliao01@gmail.com
+"""
+from bisect import bisect_right
+import torch
+import libtmux
+
+
+# FIXME ideally this would be achieved with a CombinedLRScheduler,
+# separating MultiStepLR with WarmupLR
+# but the current LRScheduler design doesn't allow it
+
+class WarmupMultiStepLR(torch.optim.lr_scheduler._LRScheduler):
+    def __init__(
+            self,
+            optimizer,
+            milestones,
+            gamma=0.1,
+            warmup_factor=1.0 / 3,
+            warmup_iters=500,
+            warmup_method="linear",
+            last_epoch=-1,
+    ):
+        if not list(milestones) == sorted(milestones):
+            raise ValueError(
+                "Milestones should be a list of" " increasing integers. Got {}",
+                milestones,
+            )
+
+        if warmup_method not in ("constant", "linear"):
+            raise ValueError(
+                "Only 'constant' or 'linear' warmup_method accepted"
+                "got {}".format(warmup_method)
+            )
+        self.milestones = milestones
+        self.gamma = gamma
+        self.warmup_factor = warmup_factor
+        self.warmup_iters = warmup_iters
+        self.warmup_method = warmup_method
+        super(WarmupMultiStepLR, self).__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        warmup_factor = 1
+        if self.last_epoch < self.warmup_iters:
+            if self.warmup_method == "constant":
+                warmup_factor = self.warmup_factor
+            elif self.warmup_method == "linear":
+                alpha = self.last_epoch / self.warmup_iters
+                warmup_factor = self.warmup_factor * (1 - alpha) + alpha
+                # warmup_factor = self.warmup_factor * self.last_epoch
+        return [
+            base_lr
+            * warmup_factor
+            * self.gamma ** bisect_right(self.milestones, self.last_epoch)
+            for base_lr in self.base_lrs
+        ]
+
+
+def grid_search(command_dict, tmux_name='search'):
+    server = libtmux.Server()
+    sess = server.new_session(tmux_name)
+
+    for i, (name, command_) in enumerate(command_dict.items()):
+        if i == 0:
+            window = sess.windows[-1]
+        else:
+            window = sess.new_window()
+        window.rename_window(name)
+        pane = window.panes[0]
+        pane.send_keys(command_)
