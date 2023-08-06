4b90c69ba08c59649ee1b70cd46d431a846ef236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:35 +0000

    Add package govuk-frontend-jinja/1.2.1/govuk-frontend-jinja-1.2.1.tar.gz

---
+++ packages/govuk-frontend-jinja/govuk-frontend-jinja-1.2.1.tar.gz/govuk-frontend-jinja-1.2.1/setup.py
@@ -0,0 +1,38 @@
+import glob
+import os
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+components = []
+directories = glob.glob("govuk_frontend_jinja/**/**/*.html", recursive=True)
+for directory in directories:
+    components.append(os.path.relpath(os.path.dirname(directory), "govuk_frontend_jinja") + "/*.html")
+
+setuptools.setup(
+    name="govuk-frontend-jinja",
+    version="1.2.1",
+    author="Matt Shaw",
+    author_email="matthew.shaw@landregistry.gov.uk",
+    description="GOV.UK Frontend Jinja Macros",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LandRegistry/govuk-frontend-jinja",
+    packages=setuptools.find_packages(),
+    package_data={"govuk_frontend_jinja": components},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Web Environment",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Software Development :: User Interfaces",
+        "Topic :: Text Processing :: Markup :: HTML",
+    ],
+    python_requires=">=3.6",
+    install_requires=["jinja2"],
+)
