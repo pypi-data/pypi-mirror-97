6297fa386e354aa39217bedefcd732182bdfa9cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:54 +0000

    Add package cuenca/0.6.3.dev10/cuenca-0.6.3.dev10.tar.gz

---
+++ packages/cuenca/cuenca-0.6.3.dev10.tar.gz/cuenca-0.6.3.dev10/cuenca/resources/user_logins.py
@@ -0,0 +1,33 @@
+import datetime as dt
+from dataclasses import dataclass
+from typing import ClassVar, cast
+
+from cuenca_validations.types.requests import UserCredentialRequest
+
+from ..http import Session, session as global_session
+from .base import Creatable
+
+
+@dataclass
+class UserLogin(Creatable):
+    _resource: ClassVar = 'user_logins'
+
+    last_login_at: dt.datetime
+    success: bool
+
+    @classmethod
+    def create(
+        cls, password: str, *, session: Session = global_session
+    ) -> 'UserLogin':
+        req = UserCredentialRequest(password=password)
+        login = cast('UserLogin', cls._create(session=session, **req.dict()))
+        if login.success:
+            session.session.headers['X-Cuenca-LoginId'] = login.id
+        return login
+
+    @classmethod
+    def logout(
+        cls, user_id: str = 'me', *, session: Session = global_session
+    ) -> None:
+        session.delete(f'{cls._resource}/{user_id}', dict())
+        session.session.headers.pop('X-Cuenca-LoginId', None)
