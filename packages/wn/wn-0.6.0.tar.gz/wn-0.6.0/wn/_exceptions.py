189b1bb04b035e2b72e0affdfae733e12d71f889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:08 +0000

    Add package wn/0.6.0/wn-0.6.0.tar.gz

---
+++ packages/wn/wn-0.6.0.tar.gz/wn-0.6.0/wn/_exceptions.py
@@ -0,0 +1,13 @@
+
+class Error(Exception):
+    """Generic error class for invalid wordnet operations."""
+
+    # reset the module so the user sees the public name
+    __module__ = 'wn'
+
+
+class WnWarning(Warning):
+    """Generic warning class for dubious worndet operations."""
+
+    # reset the module so the user sees the public name
+    __module__ = 'wn'
