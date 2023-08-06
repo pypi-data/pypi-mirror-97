1b76c11878fe4dbfd0c0cf66bab79b866a2957c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:42 +0000

    Add package sonos-extras/0.1.17/sonos_extras-0.1.17.tar.gz

---
+++ packages/sonos-extras/sonos_extras-0.1.17.tar.gz/sonos_extras-0.1.17/sonos_extras/__init__.py
@@ -0,0 +1,6 @@
+"""sonos_extras - Some extra useful commands for Sonos systems, based on SoCo."""
+
+__version__ = '0.1.17'
+__author__ = 'MaziarA <maziara2@gmail.com>'
+
+from .module import SonosExtras, SonosExtrasCliHelper
