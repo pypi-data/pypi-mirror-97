a2a69cf53791ad2a6e0418dcfd9d4af75f04e988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:16 +0000

    Add package lokii/0.1.0/lokii-0.1.0.tar.gz

---
+++ packages/lokii/lokii-0.1.0.tar.gz/lokii-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lokii',
+    packages=find_packages(where='.'),
+    version='0.1.0',
+    description='CSV dataset generator',
+    author='Package Author',
+    author_email='you@youremail.com',
+    url='http://path-to-my-packagename',
+    py_modules=['lokii'],
+    install_requires=[
+        'pathos',
+        'pandas'
+    ],
+    license='MIT License',
+    zip_safe=False,
+    keywords='boilerplate package',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+    ],)
