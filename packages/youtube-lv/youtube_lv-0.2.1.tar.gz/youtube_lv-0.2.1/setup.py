ebae55e161e3ba1e6eec1d9bded97cc105717139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:54 +0000

    Add package youtube-lv/0.2.1/youtube_lv-0.2.1.tar.gz

---
+++ packages/youtube-lv/youtube_lv-0.2.1.tar.gz/youtube_lv-0.2.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+with open("lol", "r") as lo:
+    long_description = lo.read()
+
+setuptools.setup(
+     name='youtube_lv',  
+     version='0.2.1',
+     author="phillychi3",
+     author_email="phillychi3@gmail.com",
+     description="check youtuber live status",
+     long_description=long_description,
+     url="https://github.com/phillychi3/youtube_lv",
+     py_modules=["youtube","youtuber","status","live"],
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: Apache Software License",
+         "Operating System :: OS Independent",
+     ]
+    
+ )
