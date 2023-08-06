787c88c045e9c9f96d5bed277803f76831eb6802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:08 +0000

    Add package tion/1.22/tion-1.22.tar.gz

---
+++ packages/tion/tion-1.22.tar.gz/tion-1.22/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from os.path import join, dirname
+
+setup(name='tion',
+      version='1.22',
+      description='Tion Magic Air API',
+      url='http://github.com/airens/tion',
+      author='Valeriy Chistyakov',
+      author_email='airens@mail.ru',
+      license='MIT',
+      packages=['tion'],
+      install_requires=['requests'],
+      zip_safe=False,
+      long_description=open(join(dirname(__file__), 'README.md')).read(),
+      long_description_content_type="text/markdown"
+      )
