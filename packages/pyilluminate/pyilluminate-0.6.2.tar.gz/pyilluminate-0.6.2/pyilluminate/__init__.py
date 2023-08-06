2a351ba236a93abe283e19583b580be2e1eb0b2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:25 +0000

    Add package pyilluminate/0.6.2/pyilluminate-0.6.2.tar.gz

---
+++ packages/pyilluminate/pyilluminate-0.6.2.tar.gz/pyilluminate-0.6.2/pyilluminate/__init__.py
@@ -0,0 +1,19 @@
+"""Control the SciMicroscopy Illuminate with ease.
+
+This module defines functions to control the SciMicroscopy module in a modern
+(Python 3) fashinon.
+
+Raise exceptions on failure, don't worry about "set" and "get" methods.
+
+Copyright (C) 2018, Ramona Optics, Inc.
+"""
+
+from .illuminate import Illuminate
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
+__all__ = [
+    'Illuminate',
+]
