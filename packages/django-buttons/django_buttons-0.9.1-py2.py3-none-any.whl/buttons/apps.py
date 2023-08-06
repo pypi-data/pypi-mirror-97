8485f25157cb31d4955034f5a7f8437fd58ece5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:26 +0000

    Add package django-buttons/0.9.1/django_buttons-0.9.1-py2.py3-none-any.whl

---
+++ packages/django-buttons/django_buttons-0.9.1-py2.py3-none-any.whl/buttons/apps.py
@@ -0,0 +1,21 @@
+"""
+Django application config
+
+:creationdate: 09/02/17 15:47
+:moduleauthor: François GUÉRIN <fguerin@ville-tourcoing.fr>
+:modulename: buttons.apps
+
+"""
+import logging
+
+from django.apps import AppConfig
+
+__author__ = "fguerin"
+logger = logging.getLogger("buttons.apps")
+
+
+class ButtonsAppConfig(AppConfig):
+    name = "buttons"
+
+    def ready(self):
+        from buttons.conf import ButtonsAppConf  # noqa
