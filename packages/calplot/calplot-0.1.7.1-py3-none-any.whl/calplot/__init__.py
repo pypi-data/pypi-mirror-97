741f94db2abb001e2df9c4b7efe89d4aa76e4a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:33 +0000

    Add package calplot/0.1.7.1/calplot-0.1.7.1-py3-none-any.whl

---
+++ packages/calplot/calplot-0.1.7.1-py3-none-any.whl/calplot/__init__.py
@@ -0,0 +1,9 @@
+__version_info__ = ('0', '1', '7', '1')
+__date__ = '4 Mar 2021'
+
+__version__ = '.'.join(__version_info__)
+__author__ = 'Tom Kwok'
+__contact__ = 'tom@tomkwok.com'
+__homepage__ = 'https://github.com/tomkwok/calplot'
+
+from .calplot import yearplot, calplot
