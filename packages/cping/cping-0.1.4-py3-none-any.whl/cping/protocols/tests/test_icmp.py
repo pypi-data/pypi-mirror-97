670fecb306ae5fea473ab07c7030b0d4445d1475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:48 +0000

    Add package cping/0.1.4/cping-0.1.4-py3-none-any.whl

---
+++ packages/cping/cping-0.1.4-py3-none-any.whl/cping/protocols/tests/test_icmp.py
@@ -0,0 +1,72 @@
+"""cping.protocols.icmp tests"""
+import unittest
+import unittest.mock
+
+import cping.protocols.icmp
+import cping.protocols.tests
+
+
+class TestPing(unittest.TestCase):
+    """cping.protocols.icmp.Ping tests."""
+    def test_change_interval(self):
+        """Change the interval in the middle of the test."""
+        protocol = cping.protocols.icmp.Ping()
+        cping.protocols.tests.ping_change_interval(self, protocol)
+
+    def test_failed_resolution(self):
+        """Failed resolution."""
+        host = cping.protocols.icmp.Ping()('there.exampe.org')
+
+        # ping_loop is blocking but will exit when the resolution fails
+        host.protocol.ping_loop(host)
+        self.assertEqual(host.status, 'Host resolution failed')
+
+    def test_host_not_responding(self):
+        """Nothing is sent back."""
+        host = cping.protocols.icmp.Ping(0.2)('1.2.3.4')
+        cping.protocols.tests.ping_loop_once(host)
+
+        self.assertEqual(len(host.results), 1)
+        self.assertEqual(host.results[0]['latency'], -1)
+
+    def test_host_responding_ipv4(self):
+        """Host replying on IPv4."""
+        host = cping.protocols.icmp.Ping()('127.0.0.1')
+        cping.protocols.tests.ping_loop_once(host)
+
+        self.assertEqual(len(host.results), 1)
+        self.assertNotEqual(host.results[0]['latency'], -1)
+
+    def test_host_responding_ipv6(self):
+        """Host replying on IPv6."""
+        host = cping.protocols.icmp.Ping()('::1')
+        cping.protocols.tests.ping_loop_once(host)
+
+        self.assertEqual(len(host.results), 1)
+        self.assertNotEqual(host.results[0]['latency'], -1)
+
+    def test_os_error(self):
+        """Test OSError handling on `socket.sendto`."""
+        def patch(*_):
+            raise OSError('Some message')
+
+        host = cping.protocols.icmp.Ping()('127.0.0.1')
+
+        with unittest.mock.patch('threading.Event.wait', patch):
+            # ping_loop is blocking but will exit when the exception is raised
+            host.protocol.ping_loop(host)
+            self.assertEqual(host.status, 'Some message')
+
+
+class TestSession(unittest.TestCase):
+    """cping.protocols.icmp.Session tests."""
+    def test_get_checksum_odd_sized(self):
+        """Ensure that odd-lengthed data is padded accordingly."""
+        even = cping.protocols.icmp.Session.get_checksum(b'\x01\x02\x03\x00')
+        odd = cping.protocols.icmp.Session.get_checksum(b'\x01\x02\x03')
+        self.assertEqual(even, odd)
+
+    def test__generate_data(self):
+        """Loop data until length."""
+        data = cping.protocols.icmp.Session.generate_data(5, '123')
+        self.assertEqual(data, '12312')
---
+++ packages/cping/cping-0.1.4-py3-none-any.whl/cping/tests/test_utils.py
@@ -0,0 +1,96 @@
+"""cping.utils tests"""
+import time
+import threading
+import unittest
+
+import cping.protocols
+import cping.utils
+
+# pylint: disable=protected-access
+
+
+class TestCreateSharedEvent(unittest.TestCase):
+    """cping.utils.create_stared_event tests."""
+    def test_set(self):
+        """Shared event should be set when any of the child events are set, and
+        cleared when all child events are cleared."""
+        events = [threading.Event() for _ in range(3)]
+        shared = cping.utils.create_shared_event(*events)
+
+        # No events set; all are cleared at the start
+        self.assertFalse(all((e.is_set() for e in events + [shared])))
+
+        # Setting an event; shared event set
+        events[0].set()
+        self.assertTrue(events[0].is_set())
+        self.assertFalse(events[1].is_set())
+        self.assertFalse(events[2].is_set())
+        self.assertTrue(shared.is_set())
+
+        # Setting another event; shared event set
+        events[1].set()
+        self.assertTrue(events[0].is_set())
+        self.assertTrue(events[1].is_set())
+        self.assertFalse(events[2].is_set())
+        self.assertTrue(shared.is_set())
+
+        # Clearing one event; shared event set
+        events[0].clear()
+        self.assertFalse(events[0].is_set())
+        self.assertTrue(events[1].is_set())
+        self.assertFalse(events[2].is_set())
+        self.assertTrue(shared.is_set())
+
+        # Clearing the other event; shared event cleared
+        events[1].clear()
+        self.assertFalse(all((e.is_set() for e in events + [shared])))
+
+
+class TestSparklinePoint(unittest.TestCase):
+    """cping.utils.sparkline_point tests."""
+    base = 0x2581
+
+    def test_divide_by_zero(self):
+        """Return the lowest sparkline point when minimum and maximum are equal."""
+        point = cping.utils.sparkline_point(1, 1, 1)
+        self.assertEqual(point, chr(self.base))
+
+    def test_maximum(self):
+        """A value equal to maximum should return the highest sparkline point."""
+        point = cping.utils.sparkline_point(2, 1, 2)
+        self.assertEqual(point, chr(self.base + 6))
+
+    def test_minimum(self):
+        """A value equal to minimum should return the lowest sparkline point."""
+        point = cping.utils.sparkline_point(1, 1, 2)
+        self.assertEqual(point, chr(self.base))
+
+    def test_standard_deviation(self):
+        """If the standard deviation is less than the threshold, the lowest
+        sparkline point is raised."""
+        point = cping.utils.sparkline_point(1, 1, 2, 0.1)
+        self.assertGreater(ord(point), self.base)
+
+
+class TestStaggerStart(unittest.TestCase):
+    """cping.utils.stagger_start tests."""
+    def test_timing(self):
+        """Confirm three hosts are staggered over 0.6 seconds, with the first
+        starting immediately, and then 0.2 before each of the two that follow."""
+        ping = cping.protocols.Ping()
+        ping.ping_loop = lambda _: startup_times.append(time.time())
+
+        # Record the time at which the hosts start
+        startup_fuzz = 0.1
+        startup_times = []
+
+        checkpoint = time.time()
+        hosts = [ping('1'), ping('2'), ping('3')]
+        cping.utils.stagger_start(hosts, 0.6)
+
+        # Wait for the last host to finish
+        hosts[2]._test_thread.join()
+
+        self.assertLess(startup_times[0] - checkpoint, startup_fuzz)
+        self.assertLess(startup_times[1] - checkpoint, 0.2 + startup_fuzz)
+        self.assertLess(startup_times[2] - checkpoint, 0.4 + startup_fuzz)
