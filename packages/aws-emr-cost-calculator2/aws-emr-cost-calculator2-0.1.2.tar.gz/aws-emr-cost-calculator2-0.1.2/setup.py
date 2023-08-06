d6c8ee30ab87d9a84c48a410578da9bd2a8b9660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:34 +0000

    Add package aws-emr-cost-calculator2/0.1.2/aws-emr-cost-calculator2-0.1.2.tar.gz

---
+++ packages/aws-emr-cost-calculator2/aws-emr-cost-calculator2-0.1.2.tar.gz/aws-emr-cost-calculator2-0.1.2/setup.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+import setuptools
+
+setuptools.setup(
+    name='aws-emr-cost-calculator2',
+    version='0.1.2',
+    scripts=['aws-emr-cost-calculator2'],
+    author="Mauro Pelucchi from an idea of Marko Baštovanović",
+    author_email="mauro.pelucchi@gmail.com",
+    description="Utility package to calculate cost of an AWS EMR cluster",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mauropelucchi/aws-emr-cost-calculator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.7',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['requests[security]>=2.18.3',
+                      'retrying',
+                      'boto3>=1.9',
+                      'docopt']
+)
