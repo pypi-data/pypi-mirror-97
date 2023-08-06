1355e049b9d2c2eb16ee628b84b9ac2356d1474c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:50 +0000

    Add package troveharvester/0.4.1/troveharvester-0.4.1.tar.gz

---
+++ packages/troveharvester/troveharvester-0.4.1.tar.gz/troveharvester-0.4.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='troveharvester',
+      version='0.4.1',
+      packages=['troveharvester'],
+      description='Tool for harvesting Trove digitised newspaper articles.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Tim Sherratt',
+      author_email='tim@discontents.com.au',
+      licence='CC0',
+      url='https://github.com/wragge/troveharvester',
+      install_requires=['unicodecsv', 'requests', 'arrow', 'tqdm', 'pillow', 'bs4', 'lxml', 'html2text'],
+      entry_points={
+          'console_scripts': [
+              'troveharvester = troveharvester.__main__:main'
+          ]
+      },
+      )
