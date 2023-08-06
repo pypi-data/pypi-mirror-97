5ec56c55cdd2cda534b61ca789b5998eebe75188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:49 +0000

    Add package nnlocallinear/0.0.4/nnlocallinear-0.0.4.tar.gz

---
+++ packages/nnlocallinear/nnlocallinear-0.0.4.tar.gz/nnlocallinear-0.0.4/nnlocallinear/__init__.py
@@ -0,0 +1,7 @@
+from .lmethods import NLS
+from .lmethods import LLS
+from .predict import NNPredict
+
+__version__ = "0.0.4"
+
+__all__ = ["NLS", "LLS", "NNPredict"]
---
+++ packages/nnlocallinear/nnlocallinear-0.0.4.tar.gz/nnlocallinear-0.0.4/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+#----------------------------------------------------------------------
+# Copyright 2018-2019
+# Marco Inacio <pythonpackages@marcoinacio.com>,
+# Victor Coscrato <vcoscrato@gmail.com>
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, version 3 of the License.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#----------------------------------------------------------------------
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='nnlocallinear',
+      version='0.0.4',
+      description='Conformal prediction using '
+                  'neural networks',
+      author='Marco Inacio, Victor Coscrato',
+      author_email='pythonpackages@marcoinacio.com',
+      packages=['nnlocallinear'],
+      keywords = ['neural networks', 'local linear regression'],
+      license='GPL3',
+      install_requires=['numpy', 'scikit-learn', 'torch']
+     )
