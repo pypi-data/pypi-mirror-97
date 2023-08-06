8019d43bd8240e883fb0d202cde0fabc1590604d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:22 +0000

    Add package conbench/0.0.1/conbench-0.0.1-py3-none-any.whl

---
+++ packages/conbench/conbench-0.0.1-py3-none-any.whl/conbench/db.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+
+
+engine = None
+session_maker = sessionmaker(future=True)
+Session = scoped_session(session_maker)
+
+
+def configure_engine(url):
+    global engine, session_maker, Session
+
+    engine = create_engine(
+        url,
+        future=True,
+        echo=False,
+        pool_pre_ping=True,
+        connect_args={"options": "-c timezone=utc"},
+    )
+    session_maker.configure(bind=engine)
+
+
+def create_all():
+    from .entities._entity import Base
+
+    Base.metadata.create_all(engine)
+    engine.dispose()
+
+
+def drop_all():
+    from .entities._entity import Base
+
+    Base.metadata.drop_all(engine)
