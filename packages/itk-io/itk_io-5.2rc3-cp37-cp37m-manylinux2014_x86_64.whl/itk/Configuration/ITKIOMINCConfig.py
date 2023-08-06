fa83deff14616b29c34e874cde1281233ebb8d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:21 +0000

    Add package itk-io/5.2rc3/itk_io-5.2rc3-cp37-cp37m-manylinux2014_x86_64.whl

---
+++ packages/itk-io/itk_io-5.2rc3-cp37-cp37m-manylinux2014_x86_64.whl/itk/Configuration/ITKIOMINCConfig.py
@@ -0,0 +1,5 @@
+depends = ('ITKPyBase', 'ITKIOImageBase', )
+templates = (
+  ('MINCImageIO', 'itk::MINCImageIO', 'itkMINCImageIO', True),
+  ('MINCImageIOFactory', 'itk::MINCImageIOFactory', 'itkMINCImageIOFactory', True),
+)
---
+++ packages/itk-io/itk_io-5.2rc3-cp37-cp37m-manylinux2014_x86_64.whl/itk/Configuration/ITKIOSpatialObjectsConfig.py
@@ -0,0 +1,9 @@
+depends = ('ITKPyBase', 'ITKSpatialObjects', 'ITKMesh', )
+templates = (
+  ('SpatialObjectReader', 'itk::SpatialObjectReader', 'itkSpatialObjectReader2', True, '2'),
+  ('SpatialObjectReader', 'itk::SpatialObjectReader', 'itkSpatialObjectReader3', True, '3'),
+  ('SpatialObjectReader', 'itk::SpatialObjectReader', 'itkSpatialObjectReader4', True, '4'),
+  ('SpatialObjectWriter', 'itk::SpatialObjectWriter', 'itkSpatialObjectWriter2', True, '2'),
+  ('SpatialObjectWriter', 'itk::SpatialObjectWriter', 'itkSpatialObjectWriter3', True, '3'),
+  ('SpatialObjectWriter', 'itk::SpatialObjectWriter', 'itkSpatialObjectWriter4', True, '4'),
+)
