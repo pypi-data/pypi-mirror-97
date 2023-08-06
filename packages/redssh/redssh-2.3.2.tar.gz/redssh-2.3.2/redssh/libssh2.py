6246d14cc61784c31add12a822e6ff57fe493db6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:27 +0000

    Add package redssh/2.3.2/redssh-2.3.2.tar.gz

---
+++ packages/redssh/redssh-2.3.2.tar.gz/redssh-2.3.2/redssh/libssh2.py
@@ -0,0 +1,40 @@
+# RedSSH
+# Copyright (C) 2018 - 2020  Red_M ( http://bitbucket.com/Red_M )
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+'''
+Just rebinding ssh2-python into a slightly nicer format.
+'''
+from ssh2.session import Session
+from ssh2.session import LIBSSH2_HOSTKEY_HASH_SHA1,LIBSSH2_HOSTKEY_TYPE_RSA
+from ssh2.knownhost import LIBSSH2_KNOWNHOST_TYPE_PLAIN,LIBSSH2_KNOWNHOST_KEYENC_RAW,LIBSSH2_KNOWNHOST_KEY_SSHRSA,LIBSSH2_KNOWNHOST_KEY_SSHDSS
+from ssh2.session import LIBSSH2_SESSION_BLOCK_INBOUND,LIBSSH2_SESSION_BLOCK_OUTBOUND
+from ssh2.error_codes import LIBSSH2_ERROR_EAGAIN
+from ssh2.sftp import LIBSSH2_SFTP_S_IFMT,LIBSSH2_SFTP_S_IFIFO,LIBSSH2_SFTP_S_IFCHR,LIBSSH2_SFTP_S_IFDIR,LIBSSH2_SFTP_S_IFBLK,LIBSSH2_SFTP_S_IFREG,LIBSSH2_SFTP_S_IFLNK,LIBSSH2_SFTP_S_IFSOCK
+from ssh2.sftp import LIBSSH2_FXF_READ,LIBSSH2_FXF_WRITE,LIBSSH2_FXF_APPEND,LIBSSH2_FXF_CREAT,LIBSSH2_FXF_TRUNC,LIBSSH2_FXF_EXCL
+from ssh2.sftp import LIBSSH2_SFTP_S_IRWXU,LIBSSH2_SFTP_S_IRUSR,LIBSSH2_SFTP_S_IWUSR,LIBSSH2_SFTP_S_IXUSR,LIBSSH2_SFTP_S_IRWXG,LIBSSH2_SFTP_S_IRGRP,LIBSSH2_SFTP_S_IWGRP,LIBSSH2_SFTP_S_IXGRP,LIBSSH2_SFTP_S_IRWXO,LIBSSH2_SFTP_S_IROTH,LIBSSH2_SFTP_S_IWOTH,LIBSSH2_SFTP_S_IXOTH,LIBSSH2_SFTP_ST_RDONLY,LIBSSH2_SFTP_ST_NOSUID
+from ssh2.sftp_handle import SFTPAttributes
+import ssh2.exceptions as exceptions
+try:
+    # This is awful, fix it.
+    # The fix for this is in my fork of ssh2-python
+    from ssh2.session import LIBSSH2_METHOD_KEX, LIBSSH2_METHOD_HOSTKEY, \
+    LIBSSH2_METHOD_CRYPT_CS, LIBSSH2_METHOD_CRYPT_SC, LIBSSH2_METHOD_MAC_CS, \
+    LIBSSH2_METHOD_MAC_SC, LIBSSH2_METHOD_COMP_CS, LIBSSH2_METHOD_COMP_SC, \
+    LIBSSH2_METHOD_LANG_CS, LIBSSH2_METHOD_LANG_SC, LIBSSH2_FLAG_SIGPIPE, \
+    LIBSSH2_FLAG_COMPRESS
+    from ssh2.session import LIBSSH2_CALLBACK_X11
+except:
+    pass
---
+++ packages/redssh/redssh-2.3.2.tar.gz/redssh-2.3.2/setup.py
@@ -0,0 +1,92 @@
+# RedSSH
+# Copyright (C) 2018 - 2020  Red_M ( http://bitbucket.com/Red_M )
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+import re
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+with open('redssh/__init__.py', 'r') as fh:
+    reg = re.compile(r"^VERSION.+?\'(.*)\'$",re.DOTALL|re.MULTILINE)
+    redssh_version = reg.findall(fh.read())[0]
+
+deps = [
+    # 'ssh2-python'
+    'redlibssh2'
+]
+
+doc_deps = [
+    'sphinx',
+    'sphinx_rtd_theme'
+]
+
+test_deps = [
+    'jinja2',
+    'requests[socks]',
+    'coveralls',
+    'pytest-cov',
+    'pylint',
+    'bandit',
+    'safety'
+]
+
+package_excludes = [
+    'tests',
+    'tests.*',
+    '*.tests',
+    '*.tests.*'
+]
+
+setuptools.setup(
+    name='redssh',
+    version=redssh_version,
+    url='https://bitbucket.org/Red_M/RedSSH',
+    license='GPLv2',
+    author='Red_M',
+    author_email='redssh_pypi@red-m.net',
+    description='An SSH automation library.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(exclude=package_excludes),
+    platforms='Posix',
+    install_requires=deps,
+    extras_require={
+        'tests':list(set(deps+test_deps)),
+        'docs':list(set(deps+doc_deps))
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Shells',
+        'Topic :: System :: Networking',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: POSIX :: BSD',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: OS Independent'
+    ],
+)
