ad8e33079a5b3534877633bfaaad9c22ad408ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:16 +0000

    Add package ElMD/0.2.2/ElMD-0.2.2.tar.gz

---
+++ packages/ElMD/ElMD-0.2.2.tar.gz/ElMD-0.2.2/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+setup(
+  name = 'ElMD',        
+  packages = ['ElMD'],  
+  version = '0.2.2',      
+  license='GPL3',       
+  description = 'An implementation of the Element movers distance for chemical similarity of ionic compositions',  
+  author = 'Cameron Hagreaves',              
+  author_email = 'cameron.h@rgreaves.me.uk', 
+  url = 'https://github.com/lrcfmd/ElMD/',   
+  download_url = 'https://github.com/lrcfmd/ElMD/archive/v0.2.2.tar.gz',    
+  keywords = ['ChemInformatics', 'Materials Science', 'Machine Learning', 'Materials Representation'],  
+  package_data={"": ["ElementDict.json"]}, 
+  install_requires=[ 
+          'numba',
+          'numpy',
+          'scipy',
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',  
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3) ',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
