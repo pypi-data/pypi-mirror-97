22089868649325edc2386bf89c13183a38b1c468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:25 +0000

    Add package cwmungo/0.1.1/cwmungo-0.1.1.tar.gz

---
+++ packages/cwmungo/cwmungo-0.1.1.tar.gz/cwmungo-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cwmungo']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['opencv-python>=4.5.1,<5.0.0']
+
+setup_kwargs = {
+    'name': 'cwmungo',
+    'version': '0.1.1',
+    'description': 'Extract crossword grids from images',
+    'long_description': None,
+    'author': 'Harry Slatyer',
+    'author_email': 'harry.slatyer@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
