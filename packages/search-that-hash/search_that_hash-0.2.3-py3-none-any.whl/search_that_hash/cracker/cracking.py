007c315111e302279f217b185890b47a3596617a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:08 +0000

    Add package search-that-hash/0.2.3/search_that_hash-0.2.3-py3-none-any.whl

---
+++ packages/search-that-hash/search_that_hash-0.2.3-py3-none-any.whl/search_that_hash/cracker/cracking.py
@@ -0,0 +1,107 @@
+from concurrent.futures import ThreadPoolExecutor
+
+from search_that_hash.cracker.offline_mod import hashcat
+from search_that_hash.cracker.online_mod import online
+
+import logging
+import coloredlogs
+
+
+class Searcher:
+    """
+
+    This class is the one which multi threads and calls all
+    the websites / API to search from
+
+    """
+
+    def __init__(self, config):
+        logging.debug("Initing searcher")
+        self.config = config
+        self.searchers_offline = [hashcat.Hashcat()]
+        self.searchers_online = [
+            online.hashtoolkit(),
+            online.md5crypt(),
+            online.hashdecryption(),
+            online.nitrxgen(),
+            online.cmd5(),
+            online.LmRainbowTables(),
+            online.md5_addr(),
+            online.md5_grom(),
+            online.sha1_grom(),
+        ]
+
+    def main(self, chash, types) -> dict:
+
+        logging.info("Called main cracking function")
+
+        return self.perform_search(self.config, chash, types)
+
+    def perform_search(self, config, chash, types):
+
+        config["chash"] = chash
+        keys = [type["name"].lower() for type in types]
+        self.config["hashcat_types"] = [type["hashcat"] for type in types]
+        config["supported_searchers"] = []
+        config["supported_types"] = []
+
+        if not config["offline"]:
+            for search in self.searchers_online:
+                for hashtype in keys:
+                    if "all" in search.supports:
+                        config["supported_searchers"].append(search)
+                        config["supported_types"].append(hashtype)
+                        continue
+
+                    if hashtype.lower() in search.supports:
+                        if not search in config["supported_searchers"]:
+                            config["supported_searchers"].append(search)
+                        if not hashtype in types:
+                            config["supported_types"].append(hashtype)
+
+        config["supported_searchers"].append(hashcat.Hashcat())
+
+        return self.threaded_search(config)
+
+    def threaded_search(self, config):
+
+        processes = []
+        success = {}
+        fails = {}
+
+        with ThreadPoolExecutor(max_workers=6) as executor:
+            for search in config["supported_searchers"]:
+                processes.append(executor.submit(self.call_searcher, search, config))
+                for (
+                    possible_done
+                ) in processes:  # Checks the progress of everything during cracking
+                    if possible_done in success or possible_done in fails:
+                        continue
+
+                    if (
+                        list(possible_done.result().values())[0] == "Failed"
+                        or list(possible_done.result().values())[0] == "Not connected"
+                    ):
+                        fails.update(possible_done.result())
+                    else:
+                        success.update(possible_done.result())
+
+                        if not self.config["greppable"]:
+
+                            return {
+                                config["chash"]: str(
+                                    list(possible_done.result().values())[0]
+                                )
+                            }
+
+        if success == {} and not self.config["greppable"]:
+            return {config["chash"]: None}
+
+        return {config["chash"]: [success, fails]}
+
+    def call_searcher(self, search, future):
+        try:
+            return {type(search).__name__: search.crack(future)}
+        except:
+            logging.warning(f"{type(search).__name__} Failed")
+            return {type(search).__name__: "Failed"}
---
+++ packages/search-that-hash/search_that_hash-0.2.3-py3-none-any.whl/search_that_hash/printing.py
@@ -0,0 +1,60 @@
+import json
+from rich.console import Console
+
+console = Console(highlighter=False)
+
+
+class Prettifier:
+    """
+    This prints our output
+    """
+
+    @staticmethod
+    def banner():
+        banner = r"""
+  _____                     _        _______ _           _          _    _           _
+ / ____|                   | |      |__   __| |         | |        | |  | |         | |
+| (___   ___  __ _ _ __ ___| |__ ______| |  | |__   __ _| |_ ______| |__| | __ _ ___| |__
+ \___ \ / _ \/ _` | '__/ __| '_ \______| |  | '_ \ / _` | __|______|  __  |/ _` / __| '_ \
+ ____) |  __/ (_| | | | (__| | | |     | |  | | | | (_| | |_       | |  | | (_| \__ \ | | |
+|_____/ \___|\__,_|_|  \___|_| |_|     |_|  |_| |_|\__,_|\__|      |_|  |_|\__,_|___/_| |_|
+        """
+
+        links = [
+            "https://twitter.com/bee_sec_san",
+            "https://github.com/HashPals/Search-That-Hash",
+            "https://twitter.com/Jayy_2004",
+        ]
+
+        console.print(f"[bold blue]{banner}[/bold blue]")
+
+        [
+            console.print(f"[bold underline blue]{link}[/bold underline blue]")
+            for link in links
+        ]
+
+    @staticmethod
+    def error_print(msg, chash):
+        console.print(f"\n\n[bold #011627 on #ff9f1c]{chash}[/bold #011627 on #ff9f1c]")
+        console.print(
+            f"\n[bold underline #E71D36]Error[/bold underline #E71D36]: [bold #E71D36]{msg}[/bold #E71D36]"
+        )
+
+    @staticmethod
+    def sth_print(chash, result, type, verified):
+        console.print(f"\n\n[bold #011627 on #ff9f1c]{chash}[/bold #011627 on #ff9f1c]")
+        texts = (
+            f"\n[bold underline #EC7F5B]Text[/bold underline #EC7F5B] : [bold #AFEADC on #005F5F]{result}[/bold #AFEADC on #005F5F]"
+            + f"\n[bold underline #EC7F5B]Type[/bold underline #EC7F5B] : [bold #AFEADC on #005F5F]{type}[/bold #AFEADC on #005F5F]"
+        )
+        if not verified:
+            texts += "\n[bold underline #EC7F5B]Warning[/bold underline #EC7F5B]: This result is unverified. That means either our workers haven't verified it yet or it's very new. We cannot guarantee this chash is this plaintext."  # pragma: no cover
+
+        console.print(texts)
+
+    @staticmethod
+    def one_print(chash, result):
+        console.print(f"\n\n[bold #011627 on #ff9f1c]{chash}[/bold #011627 on #ff9f1c]")
+        console.print(
+            f"\n[bold underline #EC7F5B]Text[/bold underline #EC7F5B] : [bold #AFEADC on #005F5F]{result}[/bold #AFEADC on #005F5F]"
+        )
