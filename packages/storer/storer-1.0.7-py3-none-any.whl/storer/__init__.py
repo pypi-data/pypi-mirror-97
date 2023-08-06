657b60d0ee069621e2794a44ea7fdcfe212e6d2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:45 +0000

    Add package storer/1.0.7/storer-1.0.7-py3-none-any.whl

---
+++ packages/storer/storer-1.0.7-py3-none-any.whl/storer/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from .storer import Storer
+
+__all__ = (
+    '__version__',
+    'Storer'
+)
+
+__version__           = Storer.__version__
+__short_description__ = "Minimalist storage class for any purpose."
+__license__           = "MIT"
+__author__            = "Alexander D. Kazakov"
+__author_email__      = "alexander.d.kazakov@gmail.com"
+__github_username__   = "AlexanderDKazakov"
---
+++ packages/storer/storer-1.0.7-py3-none-any.whl/storer/compressor/__init__.py
@@ -0,0 +1,8 @@
+from .compressor import Compressor
+
+__version__           = Compressor.__version__
+__short_description__ = "Storer Tool"
+__license__           = "MIT"
+__author__            = "Alexander D. Kazakov"
+__author_email__      = "alexander.d.kazakov@gmail.com"
+__github_username__   = "AlexanderDKazakov"
