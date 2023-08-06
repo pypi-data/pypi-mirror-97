2d37431c3c505bd0228af597dba85b144bbfee55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:52 +0000

    Add package zbookmarks/1.1/zbookmarks-1.1.tar.gz

---
+++ packages/zbookmarks/zbookmarks-1.1.tar.gz/zbookmarks-1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['zbookmarks']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['beautifulsoup4>=4.9.3,<5.0.0']
+
+setup_kwargs = {
+    'name': 'zbookmarks',
+    'version': '1.1',
+    'description': 'Load / dump Chrome bookmark files',
+    'long_description': None,
+    'author': 'Yannis Zarkadas',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6',
+}
+
+
+setup(**setup_kwargs)
