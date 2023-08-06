5b74d733e2e2f9567adb43bfd6d2d90fe7ff8ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:23 +0000

    Add package monty/2021.3.3/monty-2021.3.3.tar.gz

---
+++ packages/monty/monty-2021.3.3.tar.gz/monty-2021.3.3/setup.py
@@ -0,0 +1,40 @@
+import os
+from setuptools import setup, find_packages
+import io
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+with io.open(os.path.join(current_dir, "README.rst"), "rt") as f:
+    long_desc = f.read()
+
+setup(
+    name="monty",
+    packages=find_packages(),
+    version="2021.3.3",
+    extras_require={
+        "yaml": ["ruamel.yaml"],
+    },
+    package_data={},
+    author="Shyue Ping Ong",
+    author_email="ongsp@ucsd.edu",
+    maintainer="Shyue Ping Ong",
+    url="https://github.com/materialsvirtuallab/monty",
+    license="MIT",
+    description="Monty is the missing complement to Python.",
+    long_description=long_desc,
+    keywords=["monty"],
+    python_requires=">=3.5",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
