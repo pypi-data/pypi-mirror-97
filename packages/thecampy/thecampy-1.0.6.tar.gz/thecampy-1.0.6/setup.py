c082fdee9d2cb3f910ce12f101117853d507bbd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:48 +0000

    Add package thecampy/1.0.6/thecampy-1.0.6.tar.gz

---
+++ packages/thecampy/thecampy-1.0.6.tar.gz/thecampy-1.0.6/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="thecampy", # Replace with your own username
+    version="1.0.6",
+    author="lewisleedev",
+    author_email="lewislee@lewislee.net",
+    description="더 캠프 인터넷편지 비공식 라이브러리",
+    url="https://github.com/lewisleedev/thecampy",
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "requests"
+    ],
+    python_requires='>=3.6',
+)
