f7f01002b362b24a3a683e82688c5fc309ff9232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:13 +0000

    Add package example-pkg-coolcat/0.0.1/example-pkg-coolcat-0.0.1.tar.gz

---
+++ packages/example-pkg-coolcat/example-pkg-coolcat-0.0.1.tar.gz/example-pkg-coolcat-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-coolcat", # Replace with your own username
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/superdeen/sampleproject",
+    download_url = 'https://github.com/superdeen/sampleproject/archive/0.0.1.tar.gz',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
