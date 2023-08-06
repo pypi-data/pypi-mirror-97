34f1f81315f7ff8b64a16439ab40de086bc804b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:32 +0000

    Add package ytmdl/2021.3.4/ytmdl-2021.3.4-py3-none-any.whl

---
+++ packages/ytmdl/ytmdl-2021.3.4-py3-none-any.whl/ytmdl/__init__.py
@@ -0,0 +1,21 @@
+name = "ytmdl"
+
+__all__ = ['ytmdl',
+           'defaults',
+           'dir',
+           'prepend',
+           'setupConfig',
+           'song',
+           'yt',
+           'cache',
+           'stringutils',
+           'utility',
+           'gaana',
+           'metadata',
+           'manual',
+           'logger',
+           'trim',
+           'saavn',
+           'exceptions',
+           'core'
+           ]
