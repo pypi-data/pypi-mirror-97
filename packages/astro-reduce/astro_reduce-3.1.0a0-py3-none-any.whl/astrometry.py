f1ab9a9076c30762b28c89838b1d8a0ce027bd19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:37 +0000

    Add package astro-reduce/3.1.0a0/astro_reduce-3.1.0a0-py3-none-any.whl

---
+++ packages/astro-reduce/astro_reduce-3.1.0a0-py3-none-any.whl/astrometry.py
@@ -0,0 +1,5 @@
+'''astrometry module for astro_reduce -- functions for astromatic interface.'''
+
+def run_sex(filename, sex_conf):
+    '''Trigger the `sex` command from astromatic on file {filename}.fits.'''
+    pass
