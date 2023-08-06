caf7af17286be71885038f218a2cdeb8a39d9afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:11 +0000

    Add package guandu/1.1.0.2/guandu-1.1.0.2.tar.gz

---
+++ packages/guandu/guandu-1.1.0.2.tar.gz/guandu-1.1.0.2/test/test_decorator.py
@@ -0,0 +1,23 @@
+# encoding: utf-8
+"""
+    @Time   : 2021-03-09 10:36
+    @Author : feikong
+"""
+
+
+import unittest
+
+from decorator.thd_async import Async
+from decorator.retry import Retry
+
+
+class TestDecorator(unittest.TestCase):
+
+    def test_retry(self):
+        @Retry(tries=3, delay=3)
+        def test(*args, **kwargs):
+            print("test")
+            a = 1 / 0
+
+        with self.assertRaises(ZeroDivisionError):
+            test()
