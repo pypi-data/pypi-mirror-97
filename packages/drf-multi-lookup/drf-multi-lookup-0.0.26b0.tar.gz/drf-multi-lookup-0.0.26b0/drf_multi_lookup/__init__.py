6cd8e3e949a6c54e972516a3e158d4d48533a148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:37 +0000

    Add package drf-multi-lookup/0.0.26b0/drf-multi-lookup-0.0.26b0.tar.gz

---
+++ packages/drf-multi-lookup/drf-multi-lookup-0.0.26b0.tar.gz/drf-multi-lookup-0.0.26b0/drf_multi_lookup/__init__.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+"""
+DRF multi lookup
+"""
+from .mixins import MultiLookUpMixin
+
+__title__ = 'DRF Multi Lookup'
+__version__ = '0.0.26-beta'
+__author__ = 'Spoonshot'
+__license__ = 'MIT Licence'
+__copyright__ = 'Copyright 2019 Spoonshot'
+
+# Version synonym
+VERSION = __version__
