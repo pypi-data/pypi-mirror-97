a608b460591a52e1fbede923c4c0ee38ee5408cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:06 +0000

    Add package tradingfeatures/0.4.1/tradingfeatures-0.4.1-py3-none-any.whl

---
+++ packages/tradingfeatures/tradingfeatures-0.4.1-py3-none-any.whl/tradingfeatures/__init__.py
@@ -0,0 +1,12 @@
+from tradingfeatures.api_base import apiBase
+
+from tradingfeatures.bitfinex import bitfinex
+from tradingfeatures.bitstamp import bitstamp
+from tradingfeatures.bitmex import bitmex
+from tradingfeatures.google_trends import google_trends
+
+# from tradingfeatures.legacy import bitfinexLegacy
+# from tradingfeatures.legacy import bitstampLegacy
+
+from tradingfeatures.main import base
+
