76f9c4685eba95e39207f073fd54fa6525e34193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:54 +0000

    Add package yt-dlp/2021.3.7/yt_dlp-2021.3.7-py2.py3-none-any.whl

---
+++ packages/yt-dlp/yt_dlp-2021.3.7-py2.py3-none-any.whl/yt_dlp/postprocessor/__init__.py
@@ -0,0 +1,48 @@
+from __future__ import unicode_literals
+
+from .embedthumbnail import EmbedThumbnailPP
+from .ffmpeg import (
+    FFmpegPostProcessor,
+    FFmpegEmbedSubtitlePP,
+    FFmpegExtractAudioPP,
+    FFmpegFixupStretchedPP,
+    FFmpegFixupM3u8PP,
+    FFmpegFixupM4aPP,
+    FFmpegMergerPP,
+    FFmpegMetadataPP,
+    FFmpegVideoConvertorPP,
+    FFmpegVideoRemuxerPP,
+    FFmpegSubtitlesConvertorPP,
+)
+from .xattrpp import XAttrMetadataPP
+from .execafterdownload import ExecAfterDownloadPP
+from .metadatafromfield import MetadataFromFieldPP
+from .metadatafromfield import MetadataFromTitlePP
+from .movefilesafterdownload import MoveFilesAfterDownloadPP
+from .sponskrub import SponSkrubPP
+
+
+def get_postprocessor(key):
+    return globals()[key + 'PP']
+
+
+__all__ = [
+    'EmbedThumbnailPP',
+    'ExecAfterDownloadPP',
+    'FFmpegEmbedSubtitlePP',
+    'FFmpegExtractAudioPP',
+    'FFmpegFixupM3u8PP',
+    'FFmpegFixupM4aPP',
+    'FFmpegFixupStretchedPP',
+    'FFmpegMergerPP',
+    'FFmpegMetadataPP',
+    'FFmpegPostProcessor',
+    'FFmpegSubtitlesConvertorPP',
+    'FFmpegVideoConvertorPP',
+    'FFmpegVideoRemuxerPP',
+    'MetadataFromFieldPP',
+    'MetadataFromTitlePP',
+    'MoveFilesAfterDownloadPP',
+    'SponSkrubPP',
+    'XAttrMetadataPP',
+]
