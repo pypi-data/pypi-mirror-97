781f9e20178ddebe5b70c5918ee7e6448b985ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:33 +0000

    Add package flaskapitools/0.1.62/flaskapitools-0.1.62.tar.gz

---
+++ packages/flaskapitools/flaskapitools-0.1.62.tar.gz/flaskapitools-0.1.62/flaskapitools/manage.py
@@ -0,0 +1,24 @@
+import os
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+application = None
+migrate = None
+
+class ApiManager():
+    def __init__(self, app):
+        application = app
+        application.app_context().push()
+        manager = Manager(application)
+
+        migrate = Migrate(application, db, directory="./database/migrations")
+        manager.add_command('db', MigrateCommand)
+
+        with application.app_context():
+            from .models import imports
+
+        @manager.command
+        def start():
+            application.run(host="0.0.0.0")
+        
+        manager.run()
