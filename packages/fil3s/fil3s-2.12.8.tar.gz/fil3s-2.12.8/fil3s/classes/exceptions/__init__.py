ec1b2d92473c3bec1b5fae2f5264914da7639017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:00 +0000

    Add package fil3s/2.12.8/fil3s-2.12.8.tar.gz

---
+++ packages/fil3s/fil3s-2.12.8.tar.gz/fil3s-2.12.8/fil3s/classes/exceptions/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# exceptionns.
+class JSONDecodeError(Exception):
+	pass
+class DuplicateError(Exception):
+	pass
+class InstanceError(Exception):
+	pass
+class FormatError(Exception):
+	pass
+class UnpackError(Exception):
+	pass
+class InvalidUsage(Exception):
+	pass
