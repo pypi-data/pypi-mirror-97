3190c6faf0895a28e2b371125ac2bff0b71f1779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:21 +0000

    Add package pyraster/0.1.0/pyraster-0.1.0.tar.gz

---
+++ packages/pyraster/pyraster-0.1.0.tar.gz/pyraster-0.1.0/setup.py
@@ -0,0 +1,31 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.1.0'
+PACKAGE_NAME = 'pyraster'
+AUTHOR = 'Bashir Kazimi'
+AUTHOR_EMAIL = 'kazimibashir907@gmail.com'
+URL = 'https://github.com/bashirkazimi/pyraster'
+
+LICENSE = 'Apache License 2.0'
+DESCRIPTION = 'Python tools for processing raster and vector data for deep learning applications. GDAL needs to be installed already.'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = ""
+
+INSTALL_REQUIRES = [
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
