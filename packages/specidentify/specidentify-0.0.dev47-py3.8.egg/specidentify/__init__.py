f30b7fcf6bebb7e5e32743f55bf5592d1a17d9eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:14 +0000

    Add package specidentify/0.0.dev47/specidentify-0.0.dev47-py3.8.egg

---
+++ packages/specidentify/specidentify-0.0.dev47-py3.8.egg/specidentify/__init__.py
@@ -0,0 +1,32 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+This is software for the reduction of data from
+optical spectrographs
+
+"""
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+#import sip
+#for cl in ('QString', 'QVariant'):
+#    sip.setapi(cl, 2)
+
+
+class SpecError(Exception):
+
+    """Errors involving this package should cause this exception to be raised.
+    """
+    pass
+
+
+
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+    from .spectools import *
+    from .guitools import * 
+    from .iterfit import *
