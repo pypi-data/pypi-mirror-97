39896882722a6dc6101e81a2aa716dc787668470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:43 +0000

    Add package bashparse/0.0.0/bashparse-0.0.0.tar.gz

---
+++ packages/bashparse/bashparse-0.0.0.tar.gz/bashparse-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='bashparse',
+    packages=["bashparse"],
+    version='0.0.0',
+    author='Perzan',
+    author_email='PerzanDevelopment@gmail.com'
+)
