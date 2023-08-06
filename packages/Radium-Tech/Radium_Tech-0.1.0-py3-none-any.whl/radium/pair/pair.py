01ec4ce49ddcd6984aff2c8089e46287d56b4888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:19 +0000

    Add package Radium-Tech/0.1.0/Radium_Tech-0.1.0-py3-none-any.whl

---
+++ packages/Radium-Tech/Radium_Tech-0.1.0-py3-none-any.whl/radium/pair/pair.py
@@ -0,0 +1,320 @@
+from datetime import datetime, date
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import statsmodels.formula.api as sm
+
+from radium import Equity
+from radium.helpers import _truncate, _convert_date
+
+
+class Pair:
+    """
+    Class for a pair of equities.
+
+    Attributes
+    ----------
+    price_spread : float np.ndarray[]
+        Price spread of equities for self.hedge_ratios
+    equity1 : radium.Equity
+    equity2 : radium.Equity
+    start_date : datetime.date
+    end_date : datetime.date
+    hedge_ratios : float np.ndarray[][2]
+        Day-wise pairs of hedge ratios
+
+    """
+
+    def __init__(self, equity1, equity2):
+        """
+        Initialise Pair class
+
+        Parameters
+        ----------
+        equity1 : radium.Equity 
+        equity2 : radium.Equity 
+
+        Raises
+        ------
+        TypeError
+            If equity1 or equity2 is not of type radium.Equity.
+        ValueError
+            If equity1 and equity2 do not share any date ranges
+        """
+        if not isinstance(equity1, Equity):
+            raise TypeError('equity1 must of type radium.Equity')
+        elif not isinstance(equity2, Equity):
+            raise TypeError('equity2 must of type radium.Equity')
+
+        self.equity1 = equity1
+        self.equity2 = equity2
+
+        # Sets start date as latest start date of equities
+        if equity1.start_date >= equity2.start_date:
+            self.start_date = equity1.start_date
+        else:
+            self.start_date = equity2.start_date
+
+        # Sets end date as earliest end date of equities
+        if equity1.end_date <= equity2.end_date:
+            self.end_date = equity1.end_date
+        else:
+            self.end_date = equity2.end_date
+
+        # If end date is before start date then date ranges of equities is
+        # incompatible
+        if self.end_date <= self.start_date:
+            raise ValueError("There is no shared date ranges between equity1"
+                             "and equity2")
+
+    def hedge(self, method, lookback):
+        """
+        Calculates the hedge_ratios given a method and lookback and stores it
+        in self.hedge_ratios
+
+        Parameters
+        ----------
+        method : str
+            Method for calculating hedge ratios ('ols')
+        lookback: int
+            Number of signals to lookback on when calculating hedge ratios
+
+        Raises
+        ------
+        TypeError
+            If method isn't a string.
+            If lookback isn't an integer.
+        ValueError
+            If lookback <= 0.
+            If method isn't available.
+
+        Notes
+        -----
+        Available methods: 'OLS'
+        """
+
+        # Exception handling of method
+        if not isinstance(method, str):
+            raise TypeError('method must be a string')
+        
+        # Exception handling of lookback
+        if not isinstance(lookback, int):
+            raise TypeError('lookback must be an integer.')
+        elif lookback <= 0:
+            raise ValueError('lookback must be > 0')
+
+        # Calculate hedge ratios based on the method provided
+        if method == 'OLS':
+            self.hedge_ratios = self._hedge_ols(lookback)
+        else:
+            raise ValueError('Available method strings: "OLS"')
+
+    @property
+    def price_spread(self):
+        """
+        float np.ndarray[] : Price spread of equities for self.hedge_ratios
+
+        Raises
+        ------
+        TypeError
+            If self.hedge_ratios isn't defined.
+
+        Notes
+        -----
+        Spread calculated using y = h1*y1 + h2*y2.
+        """
+
+        if hasattr(self, 'hedge_ratios') == False:
+            raise Exception('Pair.hedge_ratios is not defined.')
+
+        # Calculate price_spread if undefined
+        if hasattr(self, '_price_spread') == False:
+            # Construct dataframe of closed prices
+            prices = pd.concat([self.equity1.closed, self.equity2.closed],
+                                axis=1)
+            prices.columns = [self.equity1.symbol, self.equity2.symbol]
+
+            # Multiply and add for each date
+            self._price_spread = np.sum(self.hedge_ratios * prices, axis=1)
+
+        return self._price_spread
+
+    def budget(self, hedge_ratio, dec):
+        """
+        Calculates budget needed to buy integer number of equities.
+
+        Parameters
+        ----------
+        hedge_ratio : int np.ndarray[2]
+            Hedge ratios of pair
+        dec : int
+            Number of decimals to truncate to 
+
+        Returns
+        -------
+        budget : float
+            Budget needed rounded to 2 d.p.
+
+        Raises
+        -----
+        TypeError
+            If hedge_ratio isnt a list of floats, or dec isnt an integer.
+        ValueError
+            If hedge_ratio isn't length 2 or dec < 0.
+        """
+
+        if not isinstance(hedge_ratio, list):
+            raise TypeError('hedge_ratio must be a list')
+        elif not len(hedge_ratio) == 2:
+            raise ValueError('hedge_ratio must have length 2')
+        elif not isinstance(hedge_ratio[0], float):
+            raise TypeError('hedge_ratio must be a list of floats')
+        elif not isinstance(hedge_ratio[1], float):
+            raise TypeError('hedge_ratio must be a list of floats')
+
+        if not isinstance(dec, int):
+            raise TypeError('Decimal places must be an integer.')
+        elif dec < 0:
+            raise ValueError('Decimal places has to be >= 0.')
+
+        # Get the prices at end_date
+        equity1_price = self.equity1.closed.iloc[-1]
+        equity2_price = self.equity2.closed.iloc[-1]
+
+        # Calculate truncated ratios to given number of decimals 
+        truncated_ratios = np.array([_truncate(n, dec) for n in hedge_ratio])
+
+        # Calculate budget to buy integer number of equites
+        budget = equity1_price * abs(truncated_ratios[0]) * 10 ** dec \
+                 + equity2_price * abs(truncated_ratios[1]) * 10 ** dec
+
+        # Truncated budget to 2 decimals places
+        budget = _truncate(budget, 2)
+
+        return budget
+
+    def plot_closed(self, start_date=None, end_date=None):
+        """
+        Plots closed prices of both equities between two dates as a line graph
+
+        Parameters
+        ----------
+        start_date : (optional) str or datetime or datetime.date
+            First date to plot in YYYY-MM-DD form, defaults to equity start date
+        end_date : (optional) str of datetime or datetime.date
+            Last date to plot in YYYY-MM-DD form, defaults to equity end date
+
+        Raises
+        ------
+        ValueError
+            End date is same as or before start date
+        """
+
+        # If no start/end date specified use default
+        if start_date is None:
+            start_date = self.start_date
+        else:
+            start_date = _convert_date(start_date)
+
+        if end_date is None:
+            end_date = self.end_date
+        else:
+            end_date = _convert_date(end_date)
+
+        # Raises error if date range invalid
+        if end_date <= start_date:
+            raise ValueError('end_date is the same as or before start_date')
+        elif start_date < self.start_date:
+            raise ValueError('start_date can\'t be before pair.start_date')
+        elif end_date > self.end_date:
+            raise ValueError('end_date can\'t be after pair.end_date')
+
+        # Gets required range only for both equities
+        equity1_closed = self.equity1.closed
+        mask = (equity1_closed.index >= start_date) \
+               & (equity1_closed.index <= end_date)
+        equity1_closed = equity1_closed.loc[mask]
+
+        equity2_closed = self.equity2.closed
+        mask = (equity2_closed.index >= start_date) \
+               & (equity2_closed.index <= end_date)
+        equity2_closed = equity2_closed.loc[mask]
+
+        fig, ax = plt.subplots()
+        plt.plot(equity1_closed, label=self.equity1.symbol)
+        plt.plot(equity2_closed, label=self.equity2.symbol)
+
+        title = (f'{self.equity1.symbol} and {self.equity2.symbol} '
+                 f'from {start_date} to {end_date}')
+        plt.title(title)
+        plt.xlabel("Date")
+        plt.ylabel("Adjusted closed prices ($)")
+
+        # Put dollar marks infront of y axis
+        formatter = ticker.FormatStrFormatter('$%1.2f')
+        ax.yaxis.set_major_formatter(formatter)
+
+        plt.legend()
+        plt.grid()
+        plt.show()
+
+    def plot_price_spread(self):
+        """
+        Plots price spread of the pair given hedge_ratios
+
+        Raises
+        ------
+        Exception
+            If self.hedge_ratios is not defined.
+        """
+
+        if hasattr(self, '_hedge_ratios') == False:
+            raise Exception('Pair.hedge_ratios is not defined.')
+
+        dates = self.equity1.closed.index.values
+        price_spread = self.price_spread
+
+        plt.plot(dates, price_spread)
+
+        title = f'{self.equity1.symbol} and {self.equity2.symbol} price spread'
+        plt.title(title)
+        plt.xlabel('Date')
+        plt.ylabel('Price Spread ($)')
+
+        plt.grid()
+        plt.show()
+
+    def _hedge_ols(self, lookback):
+        """
+        Calculate pair hedge ratios by OLS regression.
+
+        self.equity1 will be used as response variable when regressing.
+
+        Parameters
+        ----------
+        lookback : int
+            Number of signals to lookback on when regressing.
+
+        Returns 
+        -------
+        hedge_ratios : float np.ndarray[][2]
+            Hedge ratios as [[1, -1*(OLS gradient)],...].
+        """
+
+        # Construct dataframe of closed prices
+        df = pd.concat([self.equity1.closed, self.equity2.closed], axis=1)
+        df.columns = [self.equity1.symbol, self.equity2.symbol]
+
+        # Get ols regression result for each date
+        hedge_ratios = np.zeros(df.shape)
+        for i in range(lookback, hedge_ratios.shape[0]):
+            formula = f"{self.equity1.symbol} ~ {self.equity2.symbol}"
+            df_lookback = df[(i - lookback):i]
+            ols = sm.ols(formula, df_lookback).fit()
+
+            # Hedge ratio for equity2 is -1*(OLS gradient)
+            hedge_ratios[i - 1][0] = 1
+            hedge_ratios[i - 1][1] = -1*ols.params[1]
+
+        return hedge_ratios
---
+++ packages/Radium-Tech/Radium_Tech-0.1.0-py3-none-any.whl/radium/strategy/strategy.py
@@ -0,0 +1,142 @@
+import numpy as np
+from radium.helpers import _truncate
+import pandas as pd
+
+
+class Strategy:
+    def __init__(self, pair):
+        """
+        Base class for trading strategies upon pairs.
+
+        Args:
+            pair: Pair of equities to backtest strategy on
+
+        """
+
+        self.pair = pair
+
+    def returns(self):
+        """
+        Calculates returns accounting for budget and commission
+
+        Returns: Returns as 2D array
+        """
+
+        # Get the optimal positions determined by the strategy
+        optimal_positions = self.positions()
+
+        # Calculate integer positions determined by rounding optimal positions to 3 d.p.
+        rounded_positions = np.zeros(optimal_positions.shape)
+        for i in range(self.lookback, optimal_positions.shape[0] - 1):
+            rounded_positions[i, 0] = _truncate(optimal_positions[i, 0], 3) * 10 ** 3
+            rounded_positions[i, 1] = _truncate(optimal_positions[i, 1], 3) * 10 ** 3
+
+        # Get closed prices
+        prices = pd.concat([self.pair.equity1.closed, self.pair.equity2.closed], axis=1)
+
+        # Calculate orders
+        equity1_orders = np.diff(rounded_positions[:, 0])
+        equity1_orders = np.append(0, equity1_orders)
+        equity2_orders = np.diff(rounded_positions[:, 1])
+        equity2_orders = np.append(0, equity2_orders)
+
+        # Calculate commissions per daily order (minimum price of 0.35)
+        equity1_comm = np.array([max(abs(x) * 0.0035, 0.35) if x != 0 else 0 for x in equity1_orders])
+        equity2_comm = np.array([max(abs(x) * 0.0035, 0.35) if x != 0 else 0 for x in equity2_orders])
+
+        # Calculate Initial Budget, equal to buy 1000 units of each equity
+        init_budget = 1000 * (self.pair.equity1.closed.iloc[0] + self.pair.equity2.closed.iloc[0])
+        # Truncate to 2 d.p.
+        budget = _truncate(init_budget, 2)
+
+        # Calculate returns from Equity 1 and Equity 2
+        for i in range(0, len(equity1_orders) - 1):
+            budget += -1 * equity1_orders[i] * self.pair.equity1.closed.iloc[i] - equity1_comm[i]
+            budget += -1 * equity2_orders[i] * self.pair.equity2.closed.iloc[i] - equity2_comm[i]
+
+        return budget / init_budget - 1
+
+    def th_returns(self):
+        """
+        Calculates theoretical returns without accounting for budget and commission
+
+        Returns: Theoretical returns as 2D array
+        """
+
+        # Get the optimal positions determined by the strategy
+        optimal_positions = self.positions()
+
+        # Get closed prices
+        df = pd.concat([self.pair.equity1.closed, self.pair.equity2.closed], axis=1)
+
+        # Calculate capital allocation to each position
+        positions = optimal_positions * df.values
+
+        # Convert to df
+        positions = pd.DataFrame(positions, index=self.pair.equity1.data.index)
+        positions.columns = [self.pair.equity1.symbol, self.pair.equity2.symbol]
+
+        # Calculate profit and loss with % change of close price and positions
+        close_pct_change = df.pct_change().values
+        pnl = np.sum(positions.shift().values * close_pct_change, axis=1)
+
+        # Calculate return
+        total_position = np.sum(np.abs(positions.shift()), axis=1)
+        ret = pnl / total_position
+
+        return ret
+
+    def cum_returns(self):
+        """
+        Returns: Cumulative returns as array
+
+        """
+        ret = self.th_returns()
+        cum_ret = pd.DataFrame((np.cumprod(1 + ret) - 1))
+        cum_ret.fillna(method='ffill', inplace=True)
+
+        return cum_ret.values.flatten()
+
+    def ann_returns(self):
+        """
+        Returns: Annualised returns
+
+        """
+        start_date = self.pair.start_date
+        end_date = self.pair.end_date
+
+        days = (end_date - start_date).days
+        days = int(days)
+
+        cum_returns = self.cum_returns()
+        final_cum = cum_returns[-1]
+
+        ann_return = (1 + final_cum) ** (365 / days) - 1
+        return ann_return
+
+    def sharpe(self):
+        """
+        Returns: Sharpe ratio
+
+        """
+        ret = self.th_returns()
+        sharpe_ratio = np.sqrt(252) * np.mean(ret) / np.std(ret)
+        return sharpe_ratio
+
+    def MDD(self):
+        """
+        Returns: Maximum drawdown
+
+        """
+        ret = self.th_returns()
+        max_drawdown = (np.min(ret) - np.max(ret)) / np.max(ret)
+        return max_drawdown
+
+    def MDD_duration(self):
+        """
+        Returns: Maximum drawdown duration in days
+
+        """
+        ret = self.th_returns()
+        max_drawdown_days = np.abs(np.argmax(ret) - np.argmax(min))
+        return max_drawdown_days
