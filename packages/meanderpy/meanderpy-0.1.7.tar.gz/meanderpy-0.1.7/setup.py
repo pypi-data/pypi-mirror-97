69b79f545655b8c393d8d885962a1b8cb2f74f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:33 +0000

    Add package meanderpy/0.1.7/meanderpy-0.1.7.tar.gz

---
+++ packages/meanderpy/meanderpy-0.1.7.tar.gz/meanderpy-0.1.7/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="meanderpy",
+    version="0.1.7",
+    author="Zoltan Sylvester",
+    author_email="zoltan.sylvester@beg.utexas.edu",
+    description="A simple model of meander migration",
+    keywords = 'rivers, meandering, geomorphology, stratigraphy',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zsylvester/meanderpy",
+    packages=['meanderpy'],
+    scripts=['/Users/zoltan/Dropbox/Channels/meanderpy/meanderpy/meanderpy.py'],
+    install_requires=['numpy','matplotlib','seaborn',
+        'scipy','numba','pillow','scikit-image'],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        'Intended Audience :: Science/Research',
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
