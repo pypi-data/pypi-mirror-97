dad0db8c4009035522de9232bc001b328f4a3a49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:31 +0000

    Add package syst3m/2.14.8/syst3m-2.14.8.tar.gz

---
+++ packages/syst3m/syst3m-2.14.8.tar.gz/syst3m-2.14.8/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Note!
+# ' are required, do not use any '.
+
+# setup.
+from setuptools import setup, find_packages
+setup(
+	name='syst3m',
+	version='2.14.8',
+	description='Some description.',
+	url='http://github.com/vandenberghinc/syst3m',
+	author='Daan van den Bergh',
+	author_email='vandenberghinc.contact@gmail.com',
+	license='MIT',
+	packages=find_packages(),
+	zip_safe=False,
+	install_requires=[
+            'requests',
+            'flask',
+            'pathlib',
+            'selenium',
+            'cl1>=1.12.2',
+            'fil3s>=2.13.3',
+            'r3sponse>=2.9.0',
+            'netw0rk>=1.7.9',
+        ])
