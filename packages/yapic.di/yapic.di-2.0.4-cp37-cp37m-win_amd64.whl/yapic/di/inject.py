615210efa1ef7a9d31815daad25e4bc15a83ed1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:14 +0000

    Add package yapic.di/2.0.4/yapic.di-2.0.4-cp37-cp37m-win_amd64.whl

---
+++ packages/yapic.di/yapic.di-2.0.4-cp37-cp37m-win_amd64.whl/yapic/di/inject.py
@@ -0,0 +1,35 @@
+from typing import Generic, TypeVar, overload, NoReturn
+
+Inj = TypeVar("Inj")
+Inst = TypeVar("Inst")
+
+
+class Inject(Generic[Inj]):
+    """Inject token in class level
+
+    Example::
+        from yapic.di import Injector, Inject
+
+        class Database:
+            pass
+
+        class A:
+            db: Inject[Database]
+
+        injector = Injector()
+        injector.provide(Database)
+        injector.provide(A)
+
+        a_inst = injector.get(A)
+        assert(isinstance(a_inst.db, Database))
+    """
+    @overload
+    def __get__(self, instance: None, owner: type) -> NoReturn:
+        pass
+
+    @overload
+    def __get__(self, instance: Inst, owner: type) -> Inj:  # noqa
+        pass
+
+    def __get__(self, instance, owner):  # noqa
+        return None
