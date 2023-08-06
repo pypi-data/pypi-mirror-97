9947bd80570b7ce3d021045d5b5eac51b047f50e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:40 +0000

    Add package alas-ce0-client/0.137/alas_ce0_client-0.137-py2.py3-none-any.whl

---
+++ packages/alas-ce0-client/alas_ce0_client-0.137-py2.py3-none-any.whl/alas_ce0/management/bulk.py
@@ -0,0 +1,13 @@
+from alas_ce0.common.client_base import EntityClientBase
+
+
+class BulkClient(EntityClientBase):
+    entity_endpoint_base_url = '/management/bulks/'
+
+    def __init__(self, country_code='cl', **kwargs):
+        super(BulkClient, self).__init__(**kwargs)
+        self.entity_endpoint_base_url += country_code + '/'
+        self.headers['Authorization'] = self.headers['Authorization'].replace("\n", "")
+
+    def ser_travelling_receive(self, code):
+        return self.http_post_json(self.entity_endpoint_base_url + "_ser-travelling-receive", code)
