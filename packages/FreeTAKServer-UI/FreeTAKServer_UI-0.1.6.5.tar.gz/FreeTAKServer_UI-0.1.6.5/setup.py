6a834e424bcae3a6bf17cdf59f4e9ab8ae9b5899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:11 +0000

    Add package FreeTAKServer-UI/0.1.6.5/FreeTAKServer_UI-0.1.6.5.tar.gz

---
+++ packages/FreeTAKServer-UI/FreeTAKServer_UI-0.1.6.5.tar.gz/FreeTAKServer_UI-0.1.6.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='FreeTAKServer_UI',
+    version='0.1.6.5',
+    packages=['FreeTAKServer-UI', 'FreeTAKServer-UI.app', 'FreeTAKServer-UI.app.base', 'FreeTAKServer-UI.app.home', 'FreeTAKServer-UI.tests', 'FreeTAKServer-UI.docs'],
+    url='https://github.com/FreeTAKTeam/FreeTakServer',
+    license='Eclipse License',
+    author='Ghost, C0rv0',
+    author_email='your.email@domain.com',
+    description='an optional UI for FreeTAKServer',
+    install_requires = [
+        "flask",
+        "flask_login",
+        "flask_migrate",
+        "flask_wtf",
+        "flask_sqlalchemy",
+        "email_validator",
+        "gunicorn",
+        "python-decouple",
+        "sqlalchemy-utils"
+    ],
+    include_package_data=True
+)
