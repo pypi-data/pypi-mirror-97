17720ddb2ed4ad07e10f6b81b7fcfc57a386376e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:20 +0000

    Add package pytask-parallel/0.0.8/pytask-parallel-0.0.8.tar.gz

---
+++ packages/pytask-parallel/pytask-parallel-0.0.8.tar.gz/pytask-parallel-0.0.8/src/pytask_parallel/callbacks.py
@@ -0,0 +1,47 @@
+"""Validate command line inputs and configuration values."""
+from pytask_parallel.backends import PARALLEL_BACKENDS
+
+
+def n_workers_callback(value):
+    """Validate the n-workers option."""
+    if value == "auto":
+        pass
+    elif value in [None, "None", "none"]:
+        value = None
+    elif isinstance(value, int) and 1 <= value:
+        pass
+    elif isinstance(value, str) and value.isdigit():
+        value = int(value)
+    else:
+        raise ValueError("n_workers can either be an integer >= 1, 'auto' or None.")
+
+    return value
+
+
+def parallel_backend_callback(value):
+    """Validate the input for the parallel backend."""
+    if value in [None, "None", "none"]:
+        value = None
+    elif value in PARALLEL_BACKENDS:
+        pass
+    else:
+        raise ValueError(
+            f"parallel_backend has to be one of {list(PARALLEL_BACKENDS)}."
+        )
+    return value
+
+
+def delay_callback(value):
+    """Validate the delay option."""
+    if value in [None, "None", "none"]:
+        value = None
+    else:
+        try:
+            value = float(value)
+        except ValueError:
+            pass
+
+        if not (isinstance(value, float) and value > 0):
+            raise ValueError("delay has to be a number greater than 0.")
+
+    return value
---
+++ packages/pytask-parallel/pytask-parallel-0.0.8.tar.gz/pytask-parallel-0.0.8/src/pytask_parallel/config.py
@@ -0,0 +1,46 @@
+"""Configure pytask."""
+import os
+
+from _pytask.config import hookimpl
+from _pytask.shared import get_first_non_none_value
+from pytask_parallel.backends import PARALLEL_BACKENDS_DEFAULT
+from pytask_parallel.callbacks import delay_callback
+from pytask_parallel.callbacks import n_workers_callback
+from pytask_parallel.callbacks import parallel_backend_callback
+
+
+@hookimpl
+def pytask_parse_config(config, config_from_cli, config_from_file):
+    """Parse the configuration."""
+    config["n_workers"] = get_first_non_none_value(
+        config_from_cli,
+        config_from_file,
+        key="n_workers",
+        default=1,
+        callback=n_workers_callback,
+    )
+    if config["n_workers"] == "auto":
+        config["n_workers"] = max(os.cpu_count() - 1, 1)
+
+    config["delay"] = get_first_non_none_value(
+        config_from_cli,
+        config_from_file,
+        key="delay",
+        default=0.1,
+        callback=delay_callback,
+    )
+
+    config["parallel_backend"] = get_first_non_none_value(
+        config_from_cli,
+        config_from_file,
+        key="parallel_backend",
+        default=PARALLEL_BACKENDS_DEFAULT,
+        callback=parallel_backend_callback,
+    )
+
+
+@hookimpl
+def pytask_post_parse(config):
+    """Disable parallelization if debugging is enabled."""
+    if config["pdb"] or config["trace"]:
+        config["n_workers"] = 1
