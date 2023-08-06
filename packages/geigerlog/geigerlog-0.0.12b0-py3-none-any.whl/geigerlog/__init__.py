8f0171059c654333763fba763a7b42fd9ce96bfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:41 +0000

    Add package geigerlog/0.0.12b0/geigerlog-0.0.12b0-py3-none-any.whl

---
+++ packages/geigerlog/geigerlog-0.0.12b0-py3-none-any.whl/geigerlog/__init__.py
@@ -0,0 +1,11 @@
+def main():
+    """Entry point for the application script"""
+    print("INIT INIT INIT INIT INIT INIT INIT INIT Call your main application code here")
+    import geigerlog
+    print("INIT Do what you want")
+    geigerlog
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ packages/geigerlog/geigerlog-0.0.12b0-py3-none-any.whl/geigerlog/__main__.py
@@ -0,0 +1,12 @@
+def main():
+    """MAIN Entry point for the application script"""
+    print("MAIN what do you wnt")
+    print("MAIN Call your main application code here")
+    import geigerlog
+    print("MAIN Do what you want")
+    geigerlog.main()
+
+
+if __name__ == "__main__":
+    main()
+
