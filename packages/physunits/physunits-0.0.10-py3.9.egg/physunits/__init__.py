b29758537fdd850c836e5e181e27897cf38916b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:47 +0000

    Add package physunits/0.0.10/physunits-0.0.10-py3.9.egg

---
+++ packages/physunits/physunits-0.0.10-py3.9.egg/physunits/__init__.py
@@ -0,0 +1,12 @@
+# All modules
+from .length import *
+from .angle import *
+from .mass import *
+from .frequency import *
+from .time_units import *
+from .temperature import *
+from .electromagnetic import*
+from .pressure import *
+from .energy import *
+from .power import *
+from .other import *
