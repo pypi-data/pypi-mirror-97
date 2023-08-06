84ea76c8febdfed29edfed645e87d512628a7d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:35 +0000

    Add package django-transactional-mail/0.2/django-transactional-mail-0.2.tar.gz

---
+++ packages/django-transactional-mail/django-transactional-mail-0.2.tar.gz/django-transactional-mail-0.2/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='django-transactional-mail',
+    version='0.2',
+    author='Vinay Pai',
+    author_email='vinay@vinaypai.com',
+    description='Transactional emails in Django projects made easy',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/vinaypai/django-transactional-mail/',
+    packages=['transactional_mail'],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Communications :: Email',
+    ],
+    install_requires=[
+        'django >= 2.2',
+        'django-render-block >= 0.5',
+        'html2text >= 2017',
+        'cssutils >= 1.0.2'
+    ]
+)
