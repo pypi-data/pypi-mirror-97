a2f8212486f61d0a44e6ae3b986a3b3d94a2e0a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:59 +0000

    Add package qwc-services-core/1.2.1/qwc-services-core-1.2.1.tar.gz

---
+++ packages/qwc-services-core/qwc-services-core-1.2.1.tar.gz/qwc-services-core-1.2.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+desc = """\
+=================
+QWC Services Core
+=================
+Shared modules for QWC services.
+"""
+
+setuptools.setup(
+    name="qwc-services-core",
+    version="1.2.1",
+    author="Sourcepole AG",
+    author_email="info@sourcepole.ch",
+    description="Shared modules for QWC services",
+    long_description=desc,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/qwc-services/qwc-services-core",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
