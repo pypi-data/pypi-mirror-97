f6e96768b7ffba78f0bc80b095af29ca9558c24d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:37 +0000

    Add package phhelper/0.9.16/phhelper-0.9.16.tar.gz

---
+++ packages/phhelper/phhelper-0.9.16.tar.gz/phhelper-0.9.16/setup.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='phhelper',
+    version='0.9.16',
+    author='Luiz Henrique Gomes',
+    author_email="lhgnet@gmail.com",
+    description='Python Handler Helper for Lambda',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    url="https://github.com/lhgomes/phhelper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    license='MIT',
+)
