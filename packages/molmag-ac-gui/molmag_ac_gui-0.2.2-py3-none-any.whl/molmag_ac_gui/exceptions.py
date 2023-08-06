f6df19973a49597a70507346bd2693c334e57bc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:47 +0000

    Add package molmag-ac-gui/0.2.2/molmag_ac_gui-0.2.2-py3-none-any.whl

---
+++ packages/molmag-ac-gui/molmag_ac_gui-0.2.2-py3-none-any.whl/molmag_ac_gui/exceptions.py
@@ -0,0 +1,15 @@
+# https://www.programiz.com/python-programming/user-defined-exception
+
+class FileFormatError(Exception):
+    """Exception raised for errors in the reading of files
+
+    Attributes:
+        filename -- name of file that did not conform to the implied standard
+        message -- explanation of the error
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.message = '{} is not formatted to be read according to the implied standard'.format(self.filename)
+        super().__init__(self.message)
+        
