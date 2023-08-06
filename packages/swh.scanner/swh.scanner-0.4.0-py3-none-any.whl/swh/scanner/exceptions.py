889e163374cc5d3b8d0c2197abc2e960b9a23b91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:07 +0000

    Add package swh.scanner/0.4.0/swh.scanner-0.4.0-py3-none-any.whl

---
+++ packages/swh.scanner/swh.scanner-0.4.0-py3-none-any.whl/swh/scanner/exceptions.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2020  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+
+class InvalidObjectType(TypeError):
+    pass
+
+
+class InvalidDirectoryPath(Exception):
+    pass
+
+
+class LargePayloadExc(Exception):
+    pass
+
+
+class DBError(Exception):
+    pass
+
+
+class APIError(Exception):
+    def __str__(self):
+        return '"%s"' % self.args
+
+
+def error_response(reason: str, status_code: int, api_url: str):
+    error_msg = f"{status_code} {reason}: '{api_url}'"
+    raise APIError(error_msg)
---
+++ packages/swh.scanner/swh.scanner-0.4.0-py3-none-any.whl/swh/scanner/tests/flask_api.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2020  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+from flask import Flask, request
+
+from swh.web.common.exc import LargePayloadExc
+
+from .data import present_swhids
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def index():
+        return "SWH scanner API"
+
+    @app.route("/known/", methods=["POST"])
+    def known():
+        swhids = request.get_json()
+
+        if len(swhids) > 900:
+            raise LargePayloadExc(
+                "The maximum number of SWHIDs this endpoint can receive is 900"
+            )
+
+        res = {swhid: {"known": False} for swhid in swhids}
+        for swhid in swhids:
+            if swhid in present_swhids:
+                res[swhid]["known"] = True
+
+        return res
+
+    return app
