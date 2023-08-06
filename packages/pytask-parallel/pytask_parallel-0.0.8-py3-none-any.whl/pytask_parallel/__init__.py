15289f83bd76b3c5fa100e49d274b3c7d22563c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:20 +0000

    Add package pytask-parallel/0.0.8/pytask_parallel-0.0.8-py3-none-any.whl

---
+++ packages/pytask-parallel/pytask_parallel-0.0.8-py3-none-any.whl/pytask_parallel/__init__.py
@@ -0,0 +1,5 @@
+"""The entry-point for pytask-parallel."""
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
---
+++ packages/pytask-parallel/pytask_parallel-0.0.8-py3-none-any.whl/pytask_parallel/build.py
@@ -0,0 +1,40 @@
+"""Extend the build command."""
+import click
+from _pytask.config import hookimpl
+from pytask_parallel.backends import PARALLEL_BACKENDS
+from pytask_parallel.backends import PARALLEL_BACKENDS_DEFAULT
+
+
+@hookimpl
+def pytask_extend_command_line_interface(cli):
+    """Extend the command line interface."""
+    additional_parameters = [
+        click.Option(
+            ["-n", "--n-workers"],
+            help=(
+                "Max. number of pytask_parallel tasks. Integer >= 1 or 'auto' which is "
+                "os.cpu_count() - 1.  [default: 1 (no parallelization)]"
+            ),
+            metavar="[INTEGER|auto]",
+            default=None,
+        ),
+        click.Option(
+            ["--parallel-backend"],
+            type=click.Choice(PARALLEL_BACKENDS),
+            help=(
+                "Backend for the parallelization.  "
+                f"[default: {PARALLEL_BACKENDS_DEFAULT}]"
+            ),
+            default=None,
+        ),
+        click.Option(
+            ["--delay"],
+            help=(
+                "Delay between checking whether tasks have finished.  [default: 0.1 "
+                "(seconds)]"
+            ),
+            metavar="NUMBER > 0",
+            default=None,
+        ),
+    ]
+    cli.commands["build"].params.extend(additional_parameters)
