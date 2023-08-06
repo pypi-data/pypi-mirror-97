48c5095ca70d9306d9dbd9bdfc769f0dc4b74f93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:24 +0000

    Add package lhf/1.0.1/lhf-1.0.1.tar.gz

---
+++ packages/lhf/lhf-1.0.1.tar.gz/lhf-1.0.1/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+import os
+import re
+import sys
+import sysconfig
+import platform
+import subprocess
+
+from distutils.version import LooseVersion
+from setuptools import setup, find_packages, Extension
+from setuptools.command.build_ext import build_ext
+
+# class build_ext(_build_ext):
+#     def finalize_options(self):
+#         _build_ext.finalize_options(self)
+#         # Prevent numpy from thinking it is still in its setup process:
+#         __builtins__.__NUMPY_SETUP__ = False
+#         import numpy
+#         self.include_dirs.append(numpy.get_include())
+
+
+setup(
+    name='lhf',
+    version='1.0.1',    
+    description='Light Weight Homology Framework',
+    project_urls={
+        "LHF": "https://github.com/wilseypa/lhf",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    package_data={'libLHF': ['libLHFlib.so',
+                          'libLHFlib.so.1',
+                          'libLHFlib.so.1.0.0']},
+    include_package_data=True,
+)
