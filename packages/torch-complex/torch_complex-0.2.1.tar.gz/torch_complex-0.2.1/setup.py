c4e3df866d587f106b08feb87427fc411bf04c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:45 +0000

    Add package torch-complex/0.2.1/torch_complex-0.2.1.tar.gz

---
+++ packages/torch-complex/torch_complex-0.2.1.tar.gz/torch_complex-0.2.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import os
+from setuptools import find_packages
+from setuptools import setup
+
+dirname = os.path.dirname(__file__)
+setup(name='torch_complex',
+      version='0.2.1',
+      description='A fugacious python class for PyTorch-ComplexTensor',
+      long_description=open(os.path.join(dirname, 'README.md'),
+                            encoding='utf-8').read(),
+      long_description_content_type="text/markdown",
+      author='Naoyuki Kamo',
+      author_email='naoyuki.kamo829@gmail.com',
+      url='https://github.com/kamo-naoyuki/torch_complex',
+      packages=find_packages(include=['torch_complex']),
+      install_requires=['numpy'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest', 'pytest-cov'],
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Science/Research',
+          'Operating System :: POSIX :: Linux',
+          'License :: OSI Approved :: Apache Software License',
+          'Topic :: Software Development :: Libraries :: Python Modules'],
+      )
