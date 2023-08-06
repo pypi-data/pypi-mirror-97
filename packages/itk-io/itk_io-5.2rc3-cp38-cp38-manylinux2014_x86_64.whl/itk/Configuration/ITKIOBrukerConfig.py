696c26b9e1d32391494a7d4dfea5f8f95c08937e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:25 +0000

    Add package itk-io/5.2rc3/itk_io-5.2rc3-cp38-cp38-manylinux2014_x86_64.whl

---
+++ packages/itk-io/itk_io-5.2rc3-cp38-cp38-manylinux2014_x86_64.whl/itk/Configuration/ITKIOBrukerConfig.py
@@ -0,0 +1,5 @@
+depends = ('ITKPyBase', 'ITKIOImageBase', )
+templates = (
+  ('Bruker2dseqImageIO', 'itk::Bruker2dseqImageIO', 'itkBruker2dseqImageIO', True),
+  ('Bruker2dseqImageIOFactory', 'itk::Bruker2dseqImageIOFactory', 'itkBruker2dseqImageIOFactory', True),
+)
