13954d3eda6a59475115aad9580fad7c06dd1afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:40 +0000

    Add package fxy/0.1.6/fxy-0.1.6.tar.gz

---
+++ packages/fxy/fxy-0.1.6.tar.gz/fxy-0.1.6/fxy/a.py
@@ -0,0 +1,14 @@
+# Actuarial
+
+import numpy
+import numpy as np
+import pandas
+import pandas as pd
+import scipy
+import scipy.stats as st
+import statsmodels
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+# And plotting
+from fxy.p import *
---
+++ packages/fxy/fxy-0.1.6.tar.gz/fxy-0.1.6/fxy/l.py
@@ -0,0 +1,6 @@
+# Machine Learning
+
+from . import __sklearn__ as sklearn
+
+# And plotting
+from fxy.p import *
