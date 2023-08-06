f2b4c0c76333835906eed1417dab25b4ab742b8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:53 +0000

    Add package cqlsh/6.0.0b4/cqlsh-6.0.0b4.tar.gz

---
+++ packages/cqlsh/cqlsh-6.0.0b4.tar.gz/cqlsh-6.0.0b4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    name="cqlsh",
+    install_requires=[
+        "cql",
+        "cassandra-driver",
+        "six",
+    ],
+)
