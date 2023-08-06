b5f2e053d27bf3c4da1445b2c1e6f4d389fd1815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:08 +0000

    Add package randomquotes/1.0/randomquotes-1.0-py3-none-any.whl

---
+++ packages/randomquotes/randomquotes-1.0-py3-none-any.whl/randomquotes.py
@@ -0,0 +1,15 @@
+import random
+
+def get_quotes():
+    quotes = ["The purpose of our lives to be happy", "Get busy living or get busy dying", "In order to write about life first you must live it",
+              "The unexamined life is not worth living", "Turn your wounds into wisdom ", "Live for each second without hesitation" ,
+              "The healthiest response to life is joy", "Keep calm and carry on", "Every moment is a fresh beginning",
+              "Make each day ur masterpiece", "There is no mistakes,only opportunities", "Peace begins with smile", "I love those who can smile in trouble", "A smile is the best makeup any girl can wear", "Teeth aren't pearly ,until you smile",
+              "Everyone smile in the same language", "Look back ,and smile on perils past", "I can sing and dance .I can smile-a lot","Smile at the obstacle,for it is a bridge",  "A smile is a friend maker"
+
+    ]
+
+    ran_quotes = random.choice(quotes)
+    return ran_quotes
+
+
