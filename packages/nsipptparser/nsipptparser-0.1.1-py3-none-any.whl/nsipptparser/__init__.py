f8917c8d1842ac2c170e3ac7e992164b9a966545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:09 +0000

    Add package nsipptparser/0.1.1/nsipptparser-0.1.1-py3-none-any.whl

---
+++ packages/nsipptparser/nsipptparser-0.1.1-py3-none-any.whl/nsipptparser/__init__.py
@@ -0,0 +1,6 @@
+"""nsipptparser - Very simple package for parsing .nsippt files to json"""
+from nsipptparser.parse import parse
+
+__version__ = '0.1.1'
+__author__ = 'Russell Tran <tranrl@stanford.edu>'
+__all__ = []
