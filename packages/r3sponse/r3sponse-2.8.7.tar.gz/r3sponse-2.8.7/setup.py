548c1e64e1304b04bea7128da01273fa4db29bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:37 +0000

    Add package r3sponse/2.8.7/r3sponse-2.8.7.tar.gz

---
+++ packages/r3sponse/r3sponse-2.8.7.tar.gz/r3sponse-2.8.7/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Note!
+# ' are required, do not use any '.
+
+# setup.
+from setuptools import setup, find_packages
+setup(
+	name='r3sponse',
+	version='2.8.7',
+	description='Some description.',
+	url='http://github.com/vandenberghinc/r3sponse',
+	author='Daan van den Bergh',
+	author_email='vandenberghinc.contact@gmail.com',
+	license='MIT',
+	packages=find_packages(),
+	zip_safe=False,
+	install_requires=[
+            'requests',
+            'pathlib',
+            'django',
+            'fil3s>=2.13.0',
+        ])
