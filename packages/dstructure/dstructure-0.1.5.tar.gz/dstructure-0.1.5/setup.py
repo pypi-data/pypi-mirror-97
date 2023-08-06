02b2a8a924f27e6436b04103f5691325ac3c8fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:11 +0000

    Add package dstructure/0.1.5/dstructure-0.1.5.tar.gz

---
+++ packages/dstructure/dstructure-0.1.5.tar.gz/dstructure-0.1.5/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+   long_description = fh.read()
+
+setuptools.setup(
+    name="dstructure", # Replace with your own username
+    version="0.1.5",
+    author="Vivek Pisal",
+    author_email="vivekpisal12345@gmail.com",
+    description="Data Structure",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
+
+
+#python3 setup.py sdist bdist_wheel
+#twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
