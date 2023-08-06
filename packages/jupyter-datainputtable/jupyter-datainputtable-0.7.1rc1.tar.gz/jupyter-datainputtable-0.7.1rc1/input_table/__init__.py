0288546bb398227c33eb2c43538f6b817fc1fcbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:34 +0000

    Add package jupyter-datainputtable/0.7.1rc1/jupyter-datainputtable-0.7.1rc1.tar.gz

---
+++ packages/jupyter-datainputtable/jupyter-datainputtable-0.7.1rc1.tar.gz/jupyter-datainputtable-0.7.1rc1/input_table/__init__.py
@@ -0,0 +1,30 @@
+name = "input_table"
+#Python to generate a table that the user can input data into in a Jupyter
+# notebook. Requires input_table.js.
+# Jonathan Gutow <gutow@uwosh.edu> Feb. 3, 2020
+# license GPL V3 or greater
+
+from IPython.display import HTML
+from IPython.display import Javascript as JS 
+
+import os
+
+#Locate input_table package directory
+mydir=os.path.dirname(__file__) #absolute path to directory containing this file.
+
+#load the supporting javascript
+tempJSfile=open(os.path.join(mydir,'javascript','input_table.js'))
+tempscript='<script type="text/javascript">'
+tempscript+=tempJSfile.read()+'</script>'
+tempJSfile.close()
+display(HTML(tempscript))
+
+def create_input_table():
+    """
+    This function calls the javascript function `get_table_dim()` to start building a data input table
+    that will be created in the output area of a Jupyter cell. Because the tools are
+    meant to be used with direct calls to javascript from a html/javascript menu, when you call this python
+    function it will add an extra blank cell before the cell in which the table is created. This does
+    not occur when doing things in javascript.
+    """
+    display(JS('get_table_dim();'))
---
+++ packages/jupyter-datainputtable/jupyter-datainputtable-0.7.1rc1.tar.gz/jupyter-datainputtable-0.7.1rc1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="jupyter-datainputtable",
+    version="0.7.1.RC1",
+    description="Predefined data input tables for Jupyter notebooks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JupyterPhysSciLab/jupyter-datainputtable",
+    author="Jonathan Gutow",
+    author_email="jgutow@new.rr.com",
+    license="GPL-3.0+",
+    packages=setuptools.find_packages(),
+    package_data={'input_table': ['javascript/*.js']},
+    include_package_data=True,
+    install_requires=[
+        # 'python>=3.6',
+        'jupyter>=1.0.0',
+        # 'pandas>=0.22.0'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: JavaScript',
+        'Operating System :: OS Independent'
+    ]
+)
