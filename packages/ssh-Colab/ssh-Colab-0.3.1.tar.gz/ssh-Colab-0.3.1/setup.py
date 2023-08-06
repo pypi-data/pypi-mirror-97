91f33abb2f4b37face94a86d87c39e5525bc0acc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:04 +0000

    Add package ssh-Colab/0.3.1/ssh-Colab-0.3.1.tar.gz

---
+++ packages/ssh-Colab/ssh-Colab-0.3.1.tar.gz/ssh-Colab-0.3.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ssh-Colab",
+    version="0.3.1",
+    author="Li-Pin Juan",
+    author_email="lipin.juan02@gmail.com",
+    description="Google Colab secure shell connection helper that automates ngrok tunnels creation (for SSH, TPU, and TensorBoard) and facilitates the use of Google Cloud Storage, Google Drive, and Kaggle Data API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/libinruan/sshColab",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    keywords=['tpu', 'colab', 'ssh', 'tensorboard', 'kaggle', 'google drive', 'gcs', 'google cloud storage', 'secure shell']
+)
