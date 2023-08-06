0a9426f49690d1ebbc0fa3303bc614d509a228de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:51 +0000

    Add package pysilicon/0.0.6/pysilicon-0.0.6-py3-none-any.whl

---
+++ packages/pysilicon/pysilicon-0.0.6-py3-none-any.whl/pysilicon/config.py
@@ -0,0 +1,23 @@
+import os
+
+class Config:
+    running_pynq = 'xilinx' in os.getcwd()
+    running_cocotb = not running_pynq
+    inv_outs = running_pynq
+    _ns = 1000
+    T_default = int(10 * _ns)
+
+    def aslist(generator):
+        "Function decorator to transform a generator into a list"
+        def wrapper(*args, **kwargs):
+            try:
+                next(generator(*args, **kwargs))
+            except StopIteration as ex:
+                return ex.value
+        return wrapper
+
+    if running_cocotb:
+        import cocotb
+        default_decorator = cocotb.coroutine
+    else:
+        default_decorator = aslist
---
+++ packages/pysilicon/pysilicon-0.0.6-py3-none-any.whl/pysilicon/testing/config.py
@@ -0,0 +1,21 @@
+class Config:
+    running_pynq = False
+    running_cocotb = not running_pynq
+    inv_outs = running_pynq
+    _ns = 1000
+    T_default = int(10 * _ns)
+
+    def aslist(generator):
+        "Function decorator to transform a generator into a list"
+        def wrapper(*args, **kwargs):
+            try:
+                next(generator(*args, **kwargs))
+            except StopIteration as ex:
+                return ex.value
+        return wrapper
+
+    if running_cocotb:
+        import cocotb
+        default_decorator = cocotb.coroutine
+    else:
+        default_decorator = aslist
