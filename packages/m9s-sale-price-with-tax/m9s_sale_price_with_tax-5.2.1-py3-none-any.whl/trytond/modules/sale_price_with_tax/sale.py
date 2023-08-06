540d5e476d7ec49f1ca7a1bb1df6719f423bf88f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:14 +0000

    Add package m9s-sale-price-with-tax/5.2.1/m9s_sale_price_with_tax-5.2.1-py3-none-any.whl

---
+++ packages/m9s-sale-price-with-tax/m9s_sale_price_with_tax-5.2.1-py3-none-any.whl/trytond/modules/sale_price_with_tax/sale.py
@@ -0,0 +1,130 @@
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from decimal import Decimal
+from trytond.model import fields
+from trytond.pool import PoolMeta, Pool
+from trytond.transaction import Transaction
+from trytond.pyson import Eval
+
+_ZERO = Decimal('0.0')
+
+
+class SaleLine(metaclass=PoolMeta):
+    __name__ = 'sale.line'
+    unit_price_w_tax = fields.Function(fields.Numeric('Unit Price with Tax',
+            digits=(16, Eval('_parent_sale', {}).get('currency_digits',
+                    Eval('currency_digits', 2))),
+            states={
+                'invisible': Eval('type') != 'line',
+                },
+            depends=['type', 'currency_digits', 'unit_price']),
+        'get_price_with_tax')
+    amount_w_tax = fields.Function(fields.Numeric('Amount with Tax',
+            digits=(16, Eval('_parent_sale', {}).get('currency_digits',
+                    Eval('currency_digits', 2))),
+            states={
+                'invisible': ~Eval('type').in_(['line', 'subtotal']),
+                },
+            depends=['type', 'currency_digits', 'unit_price']),
+        'get_price_with_tax')
+    currency_digits = fields.Function(fields.Integer('Currency Digits'),
+        'on_change_with_currency_digits')
+    currency = fields.Many2One('currency.currency', 'Currency',
+        states={
+            'required': ~Eval('sale'),
+            },
+        depends=['sale'])
+
+    @staticmethod
+    def default_currency_digits():
+        Company = Pool().get('company.company')
+        if Transaction().context.get('company'):
+            company = Company(Transaction().context['company'])
+            return company.currency.digits
+        return 2
+
+    @staticmethod
+    def default_currency():
+        Company = Pool().get('company.company')
+        if Transaction().context.get('company'):
+            company = Company(Transaction().context['company'])
+            return company.currency.id
+
+    @fields.depends('currency')
+    def on_change_with_currency_digits(self, name=None):
+        if self.currency:
+            return self.currency.digits
+        return 2
+
+    @classmethod
+    def get_price_with_tax(cls, lines, names):
+        pool = Pool()
+        Tax = pool.get('account.tax')
+        amount_w_tax = {}
+        unit_price_w_tax = {}
+
+        def compute_amount_with_tax(line):
+            tax_amount = _ZERO
+            if line.taxes:
+                tax_list = Tax.compute(line.taxes,
+                    line.unit_price or _ZERO,
+                    line.quantity or 0.0)
+
+                tax_amount = sum(
+                    [t['amount'] for t in tax_list], _ZERO)
+            return line.amount + tax_amount
+
+        for line in lines:
+            amount = _ZERO
+            unit_price = _ZERO
+            currency = (line.sale.currency if line.sale else line.currency)
+
+            if line.type == 'line':
+                if line.quantity:
+                    amount = compute_amount_with_tax(line)
+                    unit_price = amount / Decimal(str(line.quantity))
+
+            # Only compute subtotals if the two fields are provided to speed up
+            elif line.type == 'subtotal' and len(names) == 2:
+                for line2 in line.sale.lines:
+                    if line2.type == 'line':
+                        amount2 = compute_amount_with_tax(line2)
+                        if currency:
+                            amount2 = currency.round(amount2)
+                        amount += amount2
+                    elif line2.type == 'subtotal':
+                        if line == line2:
+                            break
+                        amount = _ZERO
+
+            if currency:
+                amount = currency.round(amount)
+            amount_w_tax[line.id] = amount
+            unit_price_w_tax[line.id] = unit_price
+
+        result = {
+            'amount_w_tax': amount_w_tax,
+            'unit_price_w_tax': unit_price_w_tax,
+            }
+        for key in list(result.keys()):
+            if key not in names:
+                del result[key]
+        return result
+
+    @fields.depends('type', 'gross_unit_price', 'unit_price', 'quantity',
+        'taxes', 'sale', '_parent_sale.currency', 'currency', 'product',
+        'amount', 'discount')
+    def on_change_with_unit_price_w_tax(self, name=None):
+        if not self.sale:
+            self.sale = Transaction().context.get('sale')
+        return SaleLine.get_price_with_tax([self],
+            ['unit_price_w_tax'])['unit_price_w_tax'][self.id]
+
+    @fields.depends('type', 'gross_unit_price', 'unit_price', 'quantity',
+        'taxes', 'sale', '_parent_sale.currency', 'currency', 'product',
+        'amount', 'discount')
+    def on_change_with_amount_w_tax(self, name=None):
+        if not self.sale:
+            self.sale = Transaction().context.get('sale')
+        return SaleLine.get_price_with_tax([self],
+            ['amount_w_tax'])['amount_w_tax'][self.id]
