6fc3f101c77eed85e569abc2bdea3f03b9ff765c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:30 +0000

    Add package bicm/2.0.1/bicm-2.0.1-py3-none-any.whl

---
+++ packages/bicm/bicm-2.0.1-py3-none-any.whl/bicm/__init__.py
@@ -0,0 +1,20 @@
+"""
+BiCM
+========
+
+BiCM is a Python package for computing the Bipartite Configuration Model
+of a bipartite network and its statistical projection on one layer.
+The paper of reference is at https://www.nature.com/articles/srep10595?origin=ppub
+
+Github repository::
+
+    https://github.com/mat701/BiCM
+
+Author: Matteo Bruno
+"""
+
+from .network_functions import *
+from .graph_classes import *
+
+__version__ = "2.0.1"
+__author__ = """Matteo Bruno (matteo.bruno@imtlucca.it)"""
