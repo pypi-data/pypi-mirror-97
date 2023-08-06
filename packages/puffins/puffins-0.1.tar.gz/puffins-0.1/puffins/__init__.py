a15731b0aa4d25d6f45cdcf44877e979fcabc0e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:01 +0000

    Add package puffins/0.1/puffins-0.1.tar.gz

---
+++ packages/puffins/puffins-0.1.tar.gz/puffins-0.1/puffins/__init__.py
@@ -0,0 +1,24 @@
+"""puffins: tools for large-scale atmospheric and climate dynamics research"""
+from . import names
+from . import constants
+from . import dates
+from . import num_solver
+from . import calculus
+from . import nb_utils
+from . import interp
+from . import stats
+
+from . import dynamics
+from . import grad_bal
+from . import eofs
+from . import eq_area
+from . import fixed_temp_tropo
+from . import had_cell
+from . import hides
+from . import kuo_el
+from . import thermodynamics
+from . import tropopause
+
+from . import held_hou_1980
+from . import lindzen_hou_1988
+from . import plumb_hou_1992
