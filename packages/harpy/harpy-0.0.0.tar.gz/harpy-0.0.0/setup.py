84ca2e6625e748c1b19b1e013485b6c4d53a41f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:40 +0000

    Add package harpy/0.0.0/harpy-0.0.0.tar.gz

---
+++ packages/harpy/harpy-0.0.0.tar.gz/harpy-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='harpy',
+    version='0.0.0',
+    description='Python 3 frequency analysis codes aimed to accelerator physics.',
+    long_description='',
+    url='https://github.com/lmalina/harpy',
+    author='lmalina',
+    author_email='lukas.malina@cern.ch',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
