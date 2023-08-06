87a5382e6ad9394acaa3351c2bee34fda7a3f844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:11 +0000

    Add package jax/0.2.10/jax-0.2.10.tar.gz

---
+++ packages/jax/jax-0.2.10.tar.gz/jax-0.2.10/setup.py
@@ -0,0 +1,43 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup, find_packages
+
+__version__ = None
+
+with open('jax/version.py') as f:
+  exec(f.read(), globals())
+
+setup(
+    name='jax',
+    version=__version__,
+    description='Differentiate, compile, and transform Numpy code.',
+    author='JAX team',
+    author_email='jax-dev@google.com',
+    packages=find_packages(exclude=["examples"]),
+    package_data={'jax': ['py.typed']},
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy >=1.12',
+        'absl-py',
+        'opt_einsum',
+    ],
+    url='https://github.com/google/jax',
+    license='Apache-2.0',
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ]
+)
