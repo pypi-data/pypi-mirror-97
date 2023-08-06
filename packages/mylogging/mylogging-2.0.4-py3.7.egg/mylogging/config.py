906f2a071b0e4ba823d22e46ac5c18a2522b8aa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:14 +0000

    Add package mylogging/2.0.4/mylogging-2.0.4-py3.7.egg

---
+++ packages/mylogging/mylogging-2.0.4-py3.7.egg/mylogging/config.py
@@ -0,0 +1,7 @@
+TO_FILE = False  # Whether log to file. Setup str path (or pathlib.Path) of file with .log suffix (create if not exist). Print to console then doesn't work
+AROUND = True  # If TO_FILE mode separate logs with ===== and line breaks.
+COLOR = 'auto'  # Whether colorize results - mostly python syntax in tracebacks. If _TO_FILE is configured, colorize is ignored.
+
+
+# Do not edit, internal variables
+__DEBUG = 1
