53f34c8b61464d93b1d971b6aa1fc78d96655c5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:23 +0000

    Add package chop-pytorch/0.0.2/chop_pytorch-0.0.2-py3-none-any.whl

---
+++ packages/chop-pytorch/chop_pytorch-0.0.2-py3-none-any.whl/chop/__init__.py
@@ -0,0 +1,10 @@
+"""chop: constrained optimization for PyTorch"""
+__version__ = "0.0.2"
+
+from chop import utils
+from chop import stochastic
+from chop import optim
+from chop import constraints
+from chop import penalties
+from chop import adversary
+from chop.adversary import Adversary
