ec6d90c6689aa07f8de95effbf5ba61c1683a631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:51 +0000

    Add package pysilicon/0.0.6/pysilicon-0.0.6.tar.gz

---
+++ packages/pysilicon/pysilicon-0.0.6.tar.gz/pysilicon-0.0.6/README.md
@@ -0,0 +1,3 @@
+# pysilicon
+
+A Python library that offers useful utilities for digital IC designers. Contains scripts to help with Verilog simulation using Cocotb and FPGA testing using Pynq.
---
+++ packages/pysilicon/pysilicon-0.0.6.tar.gz/pysilicon-0.0.6/pysilicon/clkgen_driver.py
@@ -0,0 +1,77 @@
+import random, copy, math
+from .binary import *
+from .scan_driver import Scanner
+from .config import Config
+from .essentials import *
+
+if Config.running_cocotb:
+    import cocotb
+
+efpga_reset
+efpga_jtag_clk
+efpga_jtag_load
+efpga_jtag_in
+efpga_jtag_out
+
+efpga_scan_clk
+efpga_scan_en
+efpga_scan_in
+efpga_scan_out
+
+conf_sel[1:0]
+
+conf_out[3:0]
+
+gnd:
+conf_sel[3:2]
+
+
+class Clkgen:
+
+    def __init__(self, chain):
+        self.interface = interface
+        self.chain = chain
+
+    def set_fast_clk(division = 7, oscillator = 1, block = None):
+        if block is None:
+            blk = '111111'
+        else:
+            blk = bin(2 ** block)[2:].zfill(6)
+        div = bin(2 ** division)[2:].zfill(14)
+        osc = bin(2 ** oscillator)[2:].zfill(12)
+        bits = blk + div + osc
+
+        write_signal(interface['sel_clk_extern'], 1)
+        write_signal(interface['global_en'], 0)
+        write_signal(interface['en_common'], 0)
+
+        if Config.running_cocotb:
+            yield self.chain.scan_in(bits)
+            out = yield self.chain.scan_out(bits)
+        elif Config.running_pynq:
+            self.chain.scan_in(bits)
+            out = self.chain.scan_out(bits)
+        assert out == bits, out
+
+        write_signal(interface['global_en'], 1)
+        write_signal(interface['en_common'], 1)
+        write_signal(interface['sel_clk_extern'], 0)
+
+
+    def set_slow_clk():
+        write_signal(interface['sel_clk_extern'], 1)
+        write_signal(interface['global_en'], 0)
+        write_signal(interface['en_common'], 0)
+
+        bits = ''.zfill(6+14+12)
+
+        if Config.running_cocotb:
+            yield self.chain.scan_in(bits)
+            out = yield self.chain.scan_out(bits)
+        elif Config.running_pynq:
+            self.chain.scan_in(bits)
+            out = self.chain.scan_out(bits)
+        assert out == bits, out
+
+    set_fast_clk = Config.default_decorator(set_fast_clk)
+    set_slow_clk = Config.default_decorator(set_slow_clk)
