d4c9d1c9b749902b602e591f05dd9a834fb81879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:06 +0000

    Add package hotstepper/1.0.7/hotstepper-1.0.7.tar.gz

---
+++ packages/hotstepper/hotstepper-1.0.7.tar.gz/hotstepper-1.0.7/hotstepper/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '1.0.7'
+__packagename__ = 'HotStepper'
+__author__ = 'Jackson Storm'
+
+from hotstepper.basis.Bases import Bases
+from hotstepper.basis.Basis import Basis
+import hotstepper.utilities.helpers as utilities
+from hotstepper.core.Step import Step
+from hotstepper.core.Steps import Steps
+
+import warnings
+warnings.filterwarnings("ignore")
+
