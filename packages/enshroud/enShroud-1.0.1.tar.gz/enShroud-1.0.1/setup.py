77c1a3daf268562339438003e2c760d368568d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:11 +0000

    Add package enshroud/1.0.1/enShroud-1.0.1.tar.gz

---
+++ packages/enshroud/enShroud-1.0.1.tar.gz/enShroud-1.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['enshroud']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['enShroud = enshroud.enshroud:main']}
+
+setup_kwargs = {
+    'name': 'enshroud',
+    'version': '1.0.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Akarsh Malik',
+    'author_email': 'malikakarsh@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
