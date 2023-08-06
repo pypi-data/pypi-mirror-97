aac867d27c52d368d3e9975bccd53ae179d815d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:31 +0000

    Add package python-sonarqube-api/1.2.2/python_sonarqube_api-1.2.2-py3-none-any.whl

---
+++ packages/python-sonarqube-api/python_sonarqube_api-1.2.2-py3-none-any.whl/sonarqube/enterprise/__init__.py
@@ -0,0 +1,62 @@
+from sonarqube.community import SonarQubeClient
+from sonarqube.enterprise.applications import SonarQubeApplications
+from sonarqube.enterprise.alm_integrations import SonarQubeAlmIntegrations
+from sonarqube.enterprise.alm_settings import SonarQubeAlmSettings
+from sonarqube.enterprise.editions import SonarQubeEditions
+from sonarqube.enterprise.views import SonarQubeViews
+from sonarqube.enterprise.project_dump import SonarQubeProjectdump
+
+
+class SonarEnterpriseClient(SonarQubeClient):
+    """
+    A Python Client for SonarQube Enterprise Server APIs.
+    """
+    @property
+    def applications(self):
+        """
+        SonarQube applications Operations
+
+        :return:
+        """
+        return SonarQubeApplications(api=self)
+
+    @property
+    def alm_integrations(self):
+        """
+        ALM Integrations
+
+        """
+        return SonarQubeAlmIntegrations(api=self)
+
+    @property
+    def alm_settings(self):
+        """
+        ALM settings
+
+        """
+        return SonarQubeAlmSettings(api=self)
+
+    @property
+    def editions(self):
+        """
+        Manage SonarSource commercial editions
+
+        """
+        return SonarQubeEditions(api=self)
+
+    @property
+    def views(self):
+        """
+        Manage Portfolios
+
+        """
+        return SonarQubeViews(api=self)
+
+    @property
+    def project_dump(self):
+        """
+        Project export/import
+
+        :return:
+        """
+        return SonarQubeProjectdump(api=self)
