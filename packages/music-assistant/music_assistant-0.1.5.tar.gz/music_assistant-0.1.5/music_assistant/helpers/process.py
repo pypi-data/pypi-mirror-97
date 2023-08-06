0383f590f416cee1578d6d29e43568a0dbfab4aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:18 +0000

    Add package music-assistant/0.1.5/music_assistant-0.1.5.tar.gz

---
+++ packages/music-assistant/music_assistant-0.1.5.tar.gz/music_assistant-0.1.5/music_assistant/helpers/process.py
@@ -0,0 +1,90 @@
+"""
+Implementation of a (truly) non blocking subprocess.
+
+The subprocess implementation in asyncio can (still) sometimes cause deadlocks,
+even when properly handling reading/writes from different tasks.
+"""
+
+import asyncio
+import logging
+from typing import AsyncGenerator, List, Optional
+
+from async_timeout import timeout
+
+LOGGER = logging.getLogger("AsyncProcess")
+
+DEFAULT_CHUNKSIZE = 512000
+DEFAULT_TIMEOUT = 5
+
+
+class AsyncProcess:
+    """Implementation of a (truly) non blocking subprocess."""
+
+    def __init__(self, process_args: List, enable_write: bool = False):
+        """Initialize."""
+        self._proc = None
+        self._process_args = process_args
+        self._enable_write = enable_write
+
+    async def __aenter__(self) -> "AsyncProcess":
+        """Enter context manager."""
+        self._proc = await asyncio.create_subprocess_exec(
+            *self._process_args,
+            stdin=asyncio.subprocess.PIPE if self._enable_write else None,
+            stdout=asyncio.subprocess.PIPE
+        )
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback) -> bool:
+        """Exit context manager."""
+        if self._proc.returncode is None:
+            # prevent subprocess deadlocking, send terminate and read remaining bytes
+            await self.write_eof()
+            try:
+                self._proc.terminate()
+                await self._proc.stdout.read()
+                self._proc.kill()
+            except (ProcessLookupError, BrokenPipeError):
+                pass
+        del self._proc
+
+    async def iterate_chunks(
+        self, chunk_size: int = DEFAULT_CHUNKSIZE
+    ) -> AsyncGenerator[bytes, None]:
+        """Yield chunks from the process stdout. Generator."""
+        while True:
+            chunk = await self.read(chunk_size)
+            yield chunk
+            if len(chunk) < chunk_size:
+                break
+
+    async def read(
+        self, chunk_size: int = DEFAULT_CHUNKSIZE, time_out: int = DEFAULT_TIMEOUT
+    ) -> bytes:
+        """Read x bytes from the process stdout."""
+        try:
+            async with timeout(time_out):
+                return await self._proc.stdout.readexactly(chunk_size)
+        except asyncio.IncompleteReadError as err:
+            return err.partial
+
+    async def write(self, data: bytes) -> None:
+        """Write data to process stdin."""
+        try:
+            self._proc.stdin.write(data)
+            await self._proc.stdin.drain()
+        except BrokenPipeError:
+            pass
+
+    async def write_eof(self) -> None:
+        """Write eof to process."""
+        if not (self._enable_write and self._proc.stdin.can_write_eof()):
+            return
+        try:
+            self._proc.stdin.write_eof()
+        except BrokenPipeError:
+            pass
+
+    async def communicate(self, input_data: Optional[bytes] = None) -> bytes:
+        """Write bytes to process and read back results."""
+        return await self._proc.communicate(input_data)
---
+++ packages/music-assistant/music_assistant-0.1.5.tar.gz/music_assistant-0.1.5/music_assistant/providers/chromecast/__init__.py
@@ -0,0 +1,111 @@
+"""ChromeCast playerprovider."""
+
+import logging
+from typing import List
+
+import pychromecast
+from music_assistant.models.config_entry import ConfigEntry
+from music_assistant.models.provider import PlayerProvider
+from pychromecast.controllers.multizone import MultizoneManager
+
+from .const import PROV_ID, PROV_NAME, PROVIDER_CONFIG_ENTRIES
+from .models import ChromecastInfo
+from .player import ChromecastPlayer
+
+LOGGER = logging.getLogger(PROV_ID)
+
+
+async def setup(mass):
+    """Perform async setup of this Plugin/Provider."""
+    logging.getLogger("pychromecast").setLevel(logging.WARNING)
+    prov = ChromecastProvider()
+    await mass.register_provider(prov)
+
+
+class ChromecastProvider(PlayerProvider):
+    """Support for ChromeCast Audio PlayerProvider."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize."""
+        self.mz_mgr = MultizoneManager()
+        self._listener = None
+        self._browser = None
+        super().__init__(*args, **kwargs)
+
+    @property
+    def id(self) -> str:
+        """Return provider ID for this provider."""
+        return PROV_ID
+
+    @property
+    def name(self) -> str:
+        """Return provider Name for this provider."""
+        return PROV_NAME
+
+    @property
+    def config_entries(self) -> List[ConfigEntry]:
+        """Return Config Entries for this provider."""
+        return PROVIDER_CONFIG_ENTRIES
+
+    async def on_start(self) -> bool:
+        """Handle initialization of the provider based on config."""
+        self._listener = pychromecast.CastListener(
+            self.__chromecast_add_update_callback,
+            self.__chromecast_remove_callback,
+            self.__chromecast_add_update_callback,
+        )
+
+        def start_discovery():
+            self._browser = pychromecast.discovery.start_discovery(
+                self._listener, self.mass.zeroconf
+            )
+
+        self.mass.add_job(start_discovery)
+        return True
+
+    async def on_stop(self):
+        """Handle correct close/cleanup of the provider on exit."""
+        if not self._browser:
+            return
+        # stop discovery
+        self.mass.add_job(pychromecast.stop_discovery, self._browser)
+
+    def __chromecast_add_update_callback(self, cast_uuid, cast_service_name):
+        """Handle zeroconf discovery of a new or updated chromecast."""
+        # pylint: disable=unused-argument
+        service = self._listener.services[cast_uuid]
+        cast_info = ChromecastInfo(
+            services=service[0],
+            uuid=service[1],
+            model_name=service[2],
+            friendly_name=service[3],
+            host=service[4],
+            port=service[5],
+        )
+        cast_info.fill_out_missing_chromecast_info(self.mass.zeroconf)
+        if cast_info.uuid is None:
+            return  # Discovered chromecast without uuid?
+        player_id = cast_info.uuid
+        LOGGER.debug(
+            "Chromecast discovered: %s (%s)", cast_info.friendly_name, player_id
+        )
+        player = self.mass.players.get_player(player_id)
+        if not player and cast_info.is_audio_group:
+            # audio groups may reappear with new uuid, try to handle that
+            player = self.mass.players.get_player_by_name(
+                cast_info.friendly_name, PROV_ID
+            )
+        if not player:
+            player = ChromecastPlayer(self.mass, cast_info)
+        # if player was already added, the player will take care of reconnects itself.
+        player.set_cast_info(cast_info)
+        self.mass.add_job(self.mass.players.add_player(player))
+
+    @staticmethod
+    def __chromecast_remove_callback(cast_uuid, cast_service_name, cast_service):
+        """Handle a Chromecast removed event."""
+        # pylint: disable=unused-argument
+        player_id = str(cast_service[1])
+        friendly_name = cast_service[3]
+        LOGGER.debug("Chromecast removed: %s - %s", friendly_name, player_id)
+        # we ignore this event completely as the Chromecast socket client handles this itself
