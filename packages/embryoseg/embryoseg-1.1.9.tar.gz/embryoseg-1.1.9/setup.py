cb0e0fb06434e0d7709dae5a451046bc68a938c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:28 +0000

    Add package embryoseg/1.1.9/embryoseg-1.1.9.tar.gz

---
+++ packages/embryoseg/embryoseg-1.1.9.tar.gz/embryoseg-1.1.9/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+
+setup(name="embryoseg",
+      version='1.1.9',
+      author='Varun Kapoor',
+      author_email='randomaccessiblekapoor@gmail.com',
+      url='https://github.com/kapoorlab/EmbryoSeg/',
+      description='SmartSeed Segmentation for animal embryonic cells.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      install_requires=[ "numpy", "pandas", "napari","pyqt5", "natsort", "scikit-image", "scipy", "opencv-python-headless", "tifffile", "matplotlib", "csbdeep"],
+      packages=['embryoseg','embryoseg/utils','embryoseg/stardist_kapoor'],
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Natural Language :: English',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 3.9',
+                   ])
