f7c818c5d6089c46a39d9f2cec2079a49e1d92cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:40 +0000

    Add package ts-idl/0.1.dev164080000050100/ts_idl-0.1.dev164080000050100-py3-none-any.whl

---
+++ packages/ts-idl/ts_idl-0.1.dev164080000050100-py3-none-any.whl/lsst/ts/idl/enums/Electrometer.py
@@ -0,0 +1,19 @@
+import enum
+
+
+class DetailedState(enum.IntEnum):
+    DISABLEDSTATE = 1
+    ENABLEDSTATE = 2
+    FAULTSTATE = 3
+    OFFLINESTATE = 4
+    STANDBYSTATE = 5
+    NOTREADINGSTATE = 6
+    CONFIGURINGSTATE = 7
+    MANUALREADINGSTATE = 8
+    READINGBUFFERSTATE = 9
+    SETDURATIONREADINGSTATE = 10
+
+
+class Units(enum.IntEnum):
+    CURRENT = 1
+    CHARGE = 2
---
+++ packages/ts-idl/ts_idl-0.1.dev164080000050100-py3-none-any.whl/lsst/ts/idl/enums/LinearStage.py
@@ -0,0 +1,6 @@
+import enum
+
+
+class DetailedState(enum.IntEnum):
+    NOTMOVINGSTATE = 1
+    MOVINGSTATE = 2
