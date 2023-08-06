1a2daad5e6d7da781a275057c3f9a1f2a4914243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:14 +0000

    Add package pynamodb-factoryboy/0.1.0/pynamodb-factoryboy-0.1.0.tar.gz

---
+++ packages/pynamodb-factoryboy/pynamodb-factoryboy-0.1.0.tar.gz/pynamodb-factoryboy-0.1.0/pynamodb_factoryboy/factory.py
@@ -0,0 +1,16 @@
+from factory.base import Factory
+
+
+class PynamoDBFactory(Factory):
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def _build(cls, model_class, *args, **kwargs):
+        return model_class(*args, **kwargs)
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        instance = model_class(*args, **kwargs)
+        instance.save()
+        return instance
