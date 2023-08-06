0ad929a4a82477de2644b2cb25ec6e93a3402ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:59 +0000

    Add package consign/0.0.2/consign-0.0.2.tar.gz

---
+++ packages/consign/consign-0.0.2.tar.gz/consign-0.0.2/consign/sessions.py
@@ -0,0 +1,50 @@
+import csv
+import json
+
+from .models import Consignment, PreparedConsignment
+
+
+class Session():
+    """
+    """
+
+    def __init__(self):
+    
+
+    def prepare_consignment(self, consign):
+        """
+        """
+        p = PreparedConsignment()
+        p.prepare(
+            method=consign.method.upper(),
+            data=consign.data,
+            url=consign.url,
+            delimiter=consign.delimiter
+        )
+        return p
+
+    def consign(self, method, data, url,
+            delimiter=None):
+        """Constructs a :class:`Consign <Consign>`, prepares it and stores it.
+        """
+
+        # Creates the Consignment.
+        csgn = Consignment(
+            method=method.upper(),
+            data=data,
+            url=url,
+            delimiter=delimiter
+        )
+
+        # Prepares the Consignment.
+        luggage = self.prepare_consignment(csgn)
+
+        # Stores the Luggage.
+        resp = self.store(luggage)
+
+        return resp
+
+
+    def store(self, luggage):
+        """Send a given PreparedConsignment.
+        """
