93915c580a79deeaf5c4affff3ba8f4fd1355f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:41 +0000

    Add package friendly/0.2.91/friendly-0.2.91.tar.gz

---
+++ packages/friendly/friendly-0.2.91.tar.gz/friendly-0.2.91/friendly/mu.py
@@ -0,0 +1,28 @@
+import sys
+
+exc_hook_name = repr(sys.excepthook)
+
+if "InteractiveShell" in exc_hook_name:
+    from .ipython import *  # noqa  Will automatically install
+    from friendly.theme import init_rich_console
+    import colorama
+
+    colorama.deinit()
+    colorama.init(convert=False, strip=False)
+    console = init_rich_console(
+        style="light", color_system="truecolor", force_jupyter=False
+    )
+
+
+else:
+    from friendly.console_helpers import *  # noqa
+    from friendly.console_helpers import helpers  # noqa
+    from friendly import install, run  # noqa
+
+    __all__ = list(helpers.keys())
+    __all__.append("install")
+    __all__.append("run")
+
+
+del exc_hook_name
+del sys
