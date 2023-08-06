b3ee06a511d6038934ebcef906dc89a24db00e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:08 +0000

    Add package pop-create/1.1/pop_create-1.1.tar.gz

---
+++ packages/pop-create/pop_create-1.1.tar.gz/pop_create-1.1/pop_create/pop_create/seed/_templates/setup.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Import python libs
+import os
+import shutil
+from setuptools import setup, Command
+
+NAME = "R__CLEAN_NAME__"
+DESC = ""
+
+# Version info -- read without importing
+_locals = {}
+with open("R__CLEAN_NAME__/version.py") as fp:
+    exec(fp.read(), None, _locals)
+VERSION = _locals["version"]
+SETUP_DIRNAME = os.path.dirname(__file__)
+if not SETUP_DIRNAME:
+    SETUP_DIRNAME = os.getcwd()
+
+with open("README.rst", encoding="utf-8") as f:
+    LONG_DESC = f.read()
+
+with open("requirements/base.txt") as f:
+    REQUIREMENTS = f.read().splitlines()
+
+
+class Clean(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        for subdir in (NAME, "tests"):
+            for root, dirs, files in os.walk(
+                os.path.join(os.path.dirname(__file__), subdir)
+            ):
+                for dir_ in dirs:
+                    if dir_ == "__pycache__":
+                        shutil.rmtree(os.path.join(root, dir_))
+
+
+def discover_packages():
+    modules = []
+    for package in (NAME,):
+        for root, _, files in os.walk(os.path.join(SETUP_DIRNAME, package)):
+            pdir = os.path.relpath(root, SETUP_DIRNAME)
+            modname = pdir.replace(os.sep, ".")
+            modules.append(modname)
+    return modules
+
+
+setup(
+    name="R__NAME__",
+    author="R__AUTHOR__",
+    author_email="R__AUTHOR_EMAIL__",
+    url="",
+    version=VERSION,
+    install_requires=REQUIREMENTS,
+    description=DESC,
+    long_description=LONG_DESC,
+    long_description_content_type="text/x-rst",
+    python_requires=">=3.6",
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Development Status :: 5 - Production/Stable",
+    ],
+    packages=discover_packages(),
+    entry_points={"console_scripts": ["R__ENTRY__"]},
+    cmdclass={"clean": Clean},
+)
