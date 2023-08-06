4a231175c673c02660a3e910a7fd81cfc4ec84ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:45 +0000

    Add package pyotgw/1.1b1/pyotgw-1.1b1.tar.gz

---
+++ packages/pyotgw/pyotgw-1.1b1.tar.gz/pyotgw-1.1b1/setup.py
@@ -0,0 +1,36 @@
+import os
+
+from setuptools import setup
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="pyotgw",
+    version="1.1b1",
+    author="Milan van Nugteren",
+    author_email="milan@network23.nl",
+    description=(
+        "A library to interface with the opentherm gateway through "
+        "serial or network connection."
+    ),
+    license="GPLv3+",
+    keywords="opentherm gateway otgw",
+    url="https://github.com/mvn23/pyotgw",
+    packages=["pyotgw"],
+    python_requires=">=3.7",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    install_requires=["pyserial-asyncio"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    ],
+)
