12a5c012b16a6e2a5cd73ce701fc25ccd8c5a650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:22:43 +0000

    Add package factory-djoy/2.2.0/factory_djoy-2.2.0-py2.py3-none-any.whl

---
+++ packages/factory-djoy/factory_djoy-2.2.0-py2.py3-none-any.whl/factory_djoy/factories.py
@@ -0,0 +1,117 @@
+from typing import Generator
+
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from factory import LazyFunction, PostGenerationMethodCall, lazy_attribute, post_generation
+from factory.django import DjangoModelFactory
+from faker.factory import Factory as FakerFactory
+
+faker = FakerFactory.create(providers=[])
+max_retries = 200
+
+
+class CleanModelFactory(DjangoModelFactory):
+    """
+    Ensures that created instances pass Django's `full_clean` checks.
+    """
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        """
+        Call `full_clean` on any created instance before saving
+
+        Returns:
+            model_class
+
+        Raises:
+            RuntimeError: Raised when validation fails on built model instance.
+        """
+        obj = model_class(*args, **kwargs)
+        try:
+            obj.full_clean()
+        except ValidationError as ve:
+            message = 'Error building {} with {}.\nBad values:\n'.format(model_class, cls.__name__)
+            for field in ve.error_dict.keys():
+                if field == '__all__':
+                    message += '  __all__: obj.clean() failed\n'
+                else:
+                    message += '  {}: "{}"\n'.format(field, getattr(obj, field))
+            raise RuntimeError(message) from ve
+        obj.save()
+        return obj
+
+
+class UserFactory(DjangoModelFactory):
+    """
+    Factory for default Django User model. Calls full_clean before saving to
+    ensure that generated User instance is valid.
+    """
+    class Meta:
+        model = get_user_model()
+
+    email = LazyFunction(faker.safe_email)
+    first_name = LazyFunction(faker.first_name)
+    last_name = LazyFunction(faker.last_name)
+    password = PostGenerationMethodCall('set_password', 'password')
+
+    @lazy_attribute
+    def username(self):
+        """
+        Create a username from faker's profile generator, but ensure that it's
+        not in the database before using it. This is a pre-full_clean check.
+
+        Uses a simple single uniqueness check, cached against a set of already
+        tried names. 200 max tries is on the username generation, not the round
+        trip to the database - although that could be changed if required.
+
+        Raises:
+            RuntimeError: If a unique username can not be found after 200
+                retries.
+        """
+        unique_usernames = unique_username()
+        non_uniques = set()
+        for _ in range(max_retries):
+            username = next(unique_usernames)
+            username_unique = (
+                username not in non_uniques and
+                get_user_model().objects.filter(username=username).count() == 0
+            )
+            if username_unique:
+                return username
+            non_uniques.add(username)
+        else:
+            non_uniques_str = ', '.join(['"{}"'.format(name) for name in non_uniques])
+            message = (
+                'Unique username not found after 200 tries. Unique values tried: {}'
+            ).format(non_uniques_str)
+            raise RuntimeError(message)
+
+    @post_generation
+    def z_full_clean(self, create, extracted, **kwargs):
+        """
+        Assert that created User is "clean" in Django's opinion if build
+        strategy is happening.
+
+        NOTE: function name is prefixed with 'z_' so that it runs after the
+        'password' post generation function.
+
+        Raises:
+            ValidationError: If there are any invalid fields in the final User
+                object.
+        """
+        if create:
+            self.full_clean()
+
+
+def unique_username() -> Generator[None, str, None]:
+    """
+    Generate a unique username. Keeps a set of previously generated usernames
+    """
+    used = set()
+    while(True):
+        username = faker.user_name()
+        if username not in used:
+            used.add(username)
+            yield username
