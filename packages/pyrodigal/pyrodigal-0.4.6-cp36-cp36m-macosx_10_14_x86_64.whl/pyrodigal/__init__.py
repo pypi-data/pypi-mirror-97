f8294910cfc982f21faa9704391d46bda69b1386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:09 +0000

    Add package pyrodigal/0.4.6/pyrodigal-0.4.6-cp36-cp36m-macosx_10_14_x86_64.whl

---
+++ packages/pyrodigal/pyrodigal-0.4.6-cp36-cp36m-macosx_10_14_x86_64.whl/pyrodigal/__init__.py
@@ -0,0 +1,13 @@
+from collections.abc import Sequence as _Sequence
+
+from . import _pyrodigal
+from ._pyrodigal import Gene, Genes, Pyrodigal
+
+__all__ = ["Gene", "Genes", "Pyrodigal"]
+__doc__ = _pyrodigal.__doc__
+
+__author__ = "Martin Larralde <martin.larralde@embl.de>"
+__license__ = "GPLv3"
+__version__ = "0.4.6"
+
+_Sequence.register(Genes)
