3443082d26810b972a1a079648390791abe63e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:02 +0000

    Add package pulp-ansible-client/0.5.8/pulp_ansible-client-0.5.8.tar.gz

---
+++ packages/pulp-ansible-client/pulp_ansible-client-0.5.8.tar.gz/pulp_ansible-client-0.5.8/pulpcore/client/pulp_ansible/api/__init__.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from pulpcore.client.pulp_ansible.api.ansible_collections_api import AnsibleCollectionsApi
+from pulpcore.client.pulp_ansible.api.ansible_copy_api import AnsibleCopyApi
+from pulpcore.client.pulp_ansible.api.api_collections_api import ApiCollectionsApi
+from pulpcore.client.pulp_ansible.api.api_roles_api import ApiRolesApi
+from pulpcore.client.pulp_ansible.api.collection_import_api import CollectionImportApi
+from pulpcore.client.pulp_ansible.api.content_collection_versions_api import ContentCollectionVersionsApi
+from pulpcore.client.pulp_ansible.api.content_roles_api import ContentRolesApi
+from pulpcore.client.pulp_ansible.api.distributions_ansible_api import DistributionsAnsibleApi
+from pulpcore.client.pulp_ansible.api.galaxy_detail_api import GalaxyDetailApi
+from pulpcore.client.pulp_ansible.api.pulp_ansible_api_api import PulpAnsibleApiApi
+from pulpcore.client.pulp_ansible.api.pulp_ansible_galaxy_api_collections_api import PulpAnsibleGalaxyApiCollectionsApi
+from pulpcore.client.pulp_ansible.api.pulp_ansible_galaxy_api_v2_versions_api import PulpAnsibleGalaxyApiV2VersionsApi
+from pulpcore.client.pulp_ansible.api.pulp_ansible_galaxy_api_v3_collections_api import PulpAnsibleGalaxyApiV3CollectionsApi
+from pulpcore.client.pulp_ansible.api.pulp_ansible_galaxy_api_v3_collections_docs_blob_api import PulpAnsibleGalaxyApiV3CollectionsDocsBlobApi
+from pulpcore.client.pulp_ansible.api.pulp_ansible_galaxy_api_v3_versions_api import PulpAnsibleGalaxyApiV3VersionsApi
+from pulpcore.client.pulp_ansible.api.pulp_ansible_tags_api import PulpAnsibleTagsApi
+from pulpcore.client.pulp_ansible.api.remotes_collection_api import RemotesCollectionApi
+from pulpcore.client.pulp_ansible.api.remotes_role_api import RemotesRoleApi
+from pulpcore.client.pulp_ansible.api.repositories_ansible_api import RepositoriesAnsibleApi
+from pulpcore.client.pulp_ansible.api.repositories_ansible_versions_api import RepositoriesAnsibleVersionsApi
+from pulpcore.client.pulp_ansible.api.versions_api import VersionsApi
