a021580551086468b3f700d0a9be5ddb659fd26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:35 +0000

    Add package ansible-mdgen/0.0.18/ansible-mdgen-0.0.18.tar.gz

---
+++ packages/ansible-mdgen/ansible-mdgen-0.0.18.tar.gz/ansible-mdgen-0.0.18/src/ansiblemdgen/AutoDocumenterTasks.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python3
+
+from ansiblemdgen.Config import SingleConfig
+import sys
+import yaml
+import os
+from os import walk
+from ansiblemdgen.Utils import SingleLog,FileUtils
+from mdutils.mdutils import MdUtils
+
+from ansiblemdgen.AutoDocumenterBase import WriterBase
+
+class TasksWriter(WriterBase):
+
+    tasks_dir = None
+    handlers_dir = None
+    flow = {}
+
+    def render(self):
+
+        self.tasks_dir = self.config.get_base_dir()+"/tasks"
+        self.log.info("Tasks directory: "+self.tasks_dir)
+
+        self.handlers_dir = self.config.get_base_dir()+"/handlers"
+        self.log.info("Tasks directory: "+self.handlers_dir)
+
+        self.makeDocsDir(self.config.get_output_tasks_dir())
+
+        self.createMDFlowFile(self.tasks_dir, self.config.get_output_tasks_dir())
+
+        if (self.config.tasks != None and self.config.tasks['combinations'] != None):
+            self.iterateOnCombinations(self.tasks_dir, self.config.tasks['combinations'], self.config.get_output_tasks_dir())
+        else:
+            self.iterateOnFilesAndDirectories(self.tasks_dir, self.config.get_output_tasks_dir())
+
+        self.makeDocsDir(self.config.get_output_handlers_dir())
+
+        if (self.config.handlers != None and self.config.handlers['combinations'] != None):
+            self.iterateOnCombinations(self.handlers_dir, self.config.handlers['combinations'], self.config.get_output_handlers_dir())
+        else:
+            self.iterateOnFilesAndDirectories(self.handlers_dir,self.config.get_output_handlers_dir())
+
+
+    def createMDFile(self, dirpath, filename, output_directory):
+
+        self.log.info("(createMDFile) Create MD File")
+        self.log.debug("(createMDFile) dirpath: "+dirpath)
+        self.log.debug("(createMDFile) filename: "+filename)
+        self.log.debug("(createMDFile) output_directory: "+output_directory)
+        
+        if output_directory.find(self.config.get_output_tasks_dir()) != -1:
+            docspath = dirpath.replace(self.tasks_dir,self.config.get_output_tasks_dir())
+        else:
+            docspath = dirpath.replace(self.handlers_dir,self.config.get_output_handlers_dir())
+        self.log.debug("(createMDFile) docspath: "+docspath)
+
+        if not os.path.exists(docspath):
+            os.makedirs(docspath)
+
+        mdFile = MdUtils(file_name=docspath+"/"+filename.replace('.yml',''))
+        mdFile.new_header(level=1, title=filename) 
+        self.addTasks(dirpath+"/"+filename, mdFile)
+
+        mdFile.create_md_file()
+        self.log.info("(createMDFile) Create MD File Complete")
+
+    
+    def addTasks(self, filename, mdFile):
+        self.log.debug("(addTasks) Filename: "+filename)
+        with open(filename, 'r') as stream:
+            try:
+                tasks = yaml.safe_load(stream)
+                if tasks != None:
+                    for task in tasks:
+                        try:
+                            if 'block' in task.keys():
+                                if 'name' in task.keys():
+                                    mdFile.new_paragraph('* Block: '+task["name"])
+                                else:
+                                    mdFile.new_paragraph('* Block: ')
+
+                                for btask in task["block"]:
+                                    if 'name' in btask.keys():
+                                        mdFile.new_paragraph('    * '+btask["name"])
+                                    if 'tags' in btask.keys():
+                                        mdFile.write('  \n')
+                                        mdFile.write('Tags: ', bold_italics_code='b', color='green')
+                                        mdFile.write(btask["tags"])
+
+                            elif 'name' in task.keys():
+                                mdFile.new_paragraph('* '+task["name"])
+                            else:
+                                mdFile.new_paragraph('* No description available for this task - here is the definition:')
+                                mdFile.new_line("```")
+                                mdFile.new_paragraph(yaml.safe_dump(task,  default_flow_style=False, allow_unicode=True))
+                                mdFile.new_line("```")
+                            
+                            if 'tags' in task.keys():
+                                mdFile.write('  \n')
+                                mdFile.write('Tags: ', bold_italics_code='b', color='green')
+                                mdFile.write(task["tags"])
+                        except Exception:
+                            print(task)
+                            pass
+
+            except yaml.YAMLError as exc:
+                print(exc)
+
+    def createMDCombinationFile(self, comboFilename, directory, output_directory, filenamesToCombine):
+
+        self.log.info("(createMDCombinationFile) Create MD Combination File")
+        self.log.debug("(createMDCombinationFile) comboFilename: "+comboFilename)
+        self.log.debug("(createMDCombinationFile) directory: "+directory)
+        self.log.debug("(createMDCombinationFile) output_directory: "+output_directory)
+
+        comboFilenameAbs = output_directory+"/"+comboFilename      
+        comboFileDirectory = comboFilenameAbs[0:int(comboFilenameAbs.rfind('/'))]
+
+        if not os.path.exists(comboFileDirectory):
+            os.makedirs(comboFileDirectory)
+
+        mdFile = MdUtils(file_name=comboFilenameAbs)
+
+        mdFile.new_header(level=1, title='Tasks: '+comboFilename[comboFilename.rfind('/')+1:])
+        mdFile.new_line("---")
+        for filename in filenamesToCombine:
+            mdFile.new_line("")
+            mdFile.new_header(level=2, title=filename['name']) 
+
+            self.addTasks(directory+"/"+filename['name'], mdFile)
+
+        mdFile.create_md_file()
+    
+    def createMDFlowFile(self, directory, output_directory):
+
+        mdFile = MdUtils(file_name=output_directory+"/flow")
+        mdFile.new_header(level=1, title='Flow') 
+
+        self.getFlowData(directory)
+
+        mdFile.new_line("```mermaid")
+        mdFile.new_line("graph LR")
+
+        for connection in self.flow:
+            if self.flow[connection] != []:
+                for connectTo in self.flow[connection]:
+                    to = connectTo['include']
+                    mdFile.new_line(connection+"("+connection+") --> "+to+"("+to+")")
+
+        mdFile.new_line("```")
+
+        mdFile.create_md_file()
+
+    def getFlowData(self, directory):
+        self.getFlowDataForFile(directory, 'main.yml')
+        self.getOrphanedFlowData(directory)
+
+    def getFlowDataForFile(self, directory, filename):
+        with open(directory+"/"+filename, 'r') as stream:
+            try:
+                tasks = yaml.safe_load(stream)
+                if tasks != None:
+                    for task in tasks:
+                        if 'block' in task.keys():
+                            for btask in task["block"]:
+                                try:
+                                    if 'include_tasks' in btask.keys():
+                                        self.getTaskReuseFile(btask, "include_tasks", filename)
+                                    elif 'import_tasks' in btask.keys():
+                                        self.getTaskReuseFile(btask, "import_tasks", filename)
+                                except Exception:
+                                    pass
+                        try:
+                            if 'include_tasks' in task.keys():
+                                self.getTaskReuseFile(task, "include_tasks", filename)
+                            elif 'import_tasks' in task.keys():
+                                self.getTaskReuseFile(task, "import_tasks", filename)
+                        except Exception:
+                            pass
+            except yaml.YAMLError as exc:
+                print(exc)
+
+    def getTaskReuseFile(self, task, reuse_type, filename):
+        if task[reuse_type].startswith('{{') is False:
+            if filename not in self.flow.keys():
+                self.flow[filename] = []
+            self.flow[filename].append({"include": task[reuse_type]})
+            self.getFlowDataForFile(directory, task[reuse_type])
+
+
+    def getOrphanedFlowData(self, directory):
+        for (dirpath, dirnames, filenames) in walk(directory):
+            for filename in filenames:
+                relativeFilename = (os.path.relpath(dirpath, directory)+"/"+filename).replace("./","")
+                if relativeFilename not in self.flow.keys():
+                    self.getFlowDataForFile(directory, relativeFilename)
---
+++ packages/ansible-mdgen/ansible-mdgen-0.0.18.tar.gz/ansible-mdgen-0.0.18/src/ansiblemdgen/AutoDocumenterVariables.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python3
+
+from ansiblemdgen.Config import SingleConfig
+import sys
+import yaml
+import os
+from os import walk
+from ansiblemdgen.Utils import SingleLog,FileUtils
+from mdutils.mdutils import MdUtils
+import re
+
+from ansiblemdgen.AutoDocumenterBase import WriterBase
+
+class VariablesWriter(WriterBase):
+
+    defaults_dir = None
+    vars_dir = None
+
+    _all_descriptions = {}
+
+    where_referenced = ""
+
+    def render(self):
+
+        self.defaults_dir = self.config.get_base_dir()+"/defaults"
+        self.log.info("Defaults directory: "+self.defaults_dir)
+
+        self.vars_dir = self.config.get_base_dir()+"/vars"
+        self.log.info("Vars directory: "+self.vars_dir)
+
+        self.makeDocsDir(self.config.get_output_defaults_dir())
+
+        if (self.config.defaults != None and self.config.defaults['combinations'] != None):
+            self.iterateOnCombinations(self.defaults_dir, self.config.defaults['combinations'], self.config.get_output_defaults_dir())
+        else:
+            self.iterateOnFilesAndDirectories(self.defaults_dir, self.config.get_output_defaults_dir())
+
+        self.makeDocsDir(self.config.get_output_variables_dir())
+
+        if (self.config.variables != None and self.config.variables['combinations'] != None):
+            self.iterateOnCombinations(self.vars_dir, self.config.variables['combinations'], self.config.get_output_variables_dir())
+        else:
+            self.iterateOnFilesAndDirectories(self.vars_dir, self.config.get_output_variables_dir())
+
+
+    def createMDFile(self, dirpath, filename, output_directory):
+
+        self.log.info("(createMDFile) Create MD File")
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+
+        title_prefix = output_directory[output_directory.rfind('/')+1:].title()
+
+        mdFile = MdUtils(file_name=output_directory+"/"+filename.replace('.yml',''))
+
+        mdFile.new_header(level=1, title=filename) 
+
+        mdFile.new_line("---")
+
+        self.addVariables(dirpath+"/"+filename, mdFile)
+
+        mdFile.create_md_file()
+        self.log.info("(createMDFile) Create MD File Complete")
+
+    
+    def addVariables(self, filename, mdFile):
+        self.log.debug("(addVariables) Filename: "+filename)
+
+        self.getVarDescriptions(filename)
+
+        with open(filename, 'r') as stream:
+            try:
+                # vault encrypted variables throw errors on loading so replacing the ! with ____ to workaround that and reverting it when outputting to the value.
+                stream = stream.read().replace('!vault','____vault')
+                variables = yaml.safe_load(stream)
+
+                if variables != None:
+                    for variable in variables:
+
+                        mdFile.new_header(level=2, title=variable)
+
+                        if(variable in self._all_descriptions):
+                            mdFile.new_paragraph(yaml.safe_dump(self._all_descriptions[variable][0]["value"],  default_flow_style=False, allow_unicode=True))
+
+                        mdFile.new_line("```")
+                        mdFile.new_paragraph(yaml.safe_dump(variables[variable],  default_flow_style=False, allow_unicode=True).replace('____vault','!vault'))
+                        mdFile.new_line("```")
+
+                        mdFile.new_line('Where Referenced', bold_italics_code='b', color='green')
+
+                        self.where_referenced = ""
+                        self.addVariableReferences(variable, filename, self.config.get_base_dir()+"/tasks", mdFile)
+                        self.addVariableReferences(variable, filename, self.config.get_base_dir()+"/defaults", mdFile)
+                        self.addVariableReferences(variable, filename, self.config.get_base_dir()+"/vars", mdFile)
+                        self.addVariableReferences(variable, filename, self.config.get_base_dir()+"/templates", mdFile)
+                        
+                        if self.where_referenced == "":
+                            mdFile.new_line("No references found")
+                        
+
+            except yaml.YAMLError as exc:
+                print(exc)
+
+    def addVariableReferences(self, variable, filename, directory, mdFile):
+        for (dirpath, dirnames, refFilenames) in walk(directory):
+            for refFilename in refFilenames:
+                if dirpath+"/"+refFilename != filename:
+                    with open(dirpath+"/"+refFilename) as f:
+                        contents = f.read()
+                        if variable in contents:
+                            self.where_referenced = os.path.relpath(dirpath+"/"+refFilename, self.config.get_base_dir())
+                            mdFile.new_line(self.where_referenced)
+
+    def getVarDescriptions(self, filename):
+        file = open(filename, 'r')
+        while True:
+            line = file.readline()
+            
+            if not line:
+                break
+        
+            regex = "(\#\ *\@var:\ +.*)"
+            if re.match(regex, line):
+                # step1 remove the annotation
+                reg1 = "(\#\ *\@var:\ *)"
+                line1 = re.sub(reg1, '', line).strip()
+
+                # step3 take the main key value from the annotation
+                parts = line1.split(":",1)
+                key = str(parts[0].strip())
+                value = str(parts[1].strip())
+
+                item = AnnotationItem()
+
+                if key.strip() == "":
+                    key = "_unset_"
+                item.key = key
+
+                item.value = value
+
+                if item.key not in self._all_descriptions.keys():
+                    self._all_descriptions[item.key] = []
+
+                self._all_descriptions[item.key].append(item.get_obj())
+
+    def createMDCombinationFile(self, comboFilename, directory, output_directory, filenamesToCombine):
+
+        comboFilenameAbs = output_directory+"/"+comboFilename      
+        comboFileDirectory = comboFilenameAbs[0:int(comboFilenameAbs.rfind('/'))]
+
+        if not os.path.exists(comboFileDirectory):
+            os.makedirs(comboFileDirectory)
+
+        mdFile = MdUtils(file_name=comboFilenameAbs)
+
+        mdFile.new_header(level=1, title=comboFilename[comboFilename.rfind('/')+1:]) 
+        for filename in filenamesToCombine:
+            mdFile.new_line("")
+            mdFile.new_header(level=2, title=filename['name']) 
+
+            self.addVariables(directory+"/"+filename['name'], mdFile)
+
+        mdFile.create_md_file()
+
+class AnnotationItem:
+
+    key = ""  # annotation identifying key
+    value = ""  # annotation data
+
+    def __str__(self):
+        s = "{"
+        s += "key: "+self.key+", "
+        s += "value: "+self.value+", "
+        s += "}"
+        return s
+
+    def get_obj(self):
+        return {
+            "key": self.key,
+            "value": self.value,
+        }
