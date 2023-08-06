d193557cf6a4bfd1619f4694869efffd993ad207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:12 +0000

    Add package sr.comp/1.4.4/sr.comp-1.4.4.tar.gz

---
+++ packages/sr.comp/sr.comp-1.4.4.tar.gz/sr.comp-1.4.4/sr/comp/knockout_scheduler/automatic_scheduler.py
@@ -0,0 +1,203 @@
+"""An automatic seeded knockout schedule."""
+
+import math
+from datetime import timedelta
+from typing import Iterable, List, Mapping, Optional, Sized, TYPE_CHECKING
+
+from ..match_period import Match, MatchSlot, MatchType
+from ..match_period_clock import MatchPeriodClock
+from ..scores import Scores
+from ..teams import Team
+from ..types import ArenaName, MatchNumber, TLA, YAMLData
+from . import seeding, stable_random
+from .base_scheduler import BaseKnockoutScheduler, UNKNOWABLE_TEAM
+
+if TYPE_CHECKING:
+    # Circular
+    from ..matches import MatchSchedule
+
+
+class KnockoutScheduler(BaseKnockoutScheduler):
+    """
+    A class that can be used to generate a knockout schedule based on seeding.
+
+    Due to the way the seeding logic works, this class is suitable only when
+    games feature four slots for competitors, with the top two progressing to
+    the next round.
+
+    :param schedule: The league schedule.
+    :param scores: The scores.
+    :param dict arenas: The arenas.
+    :param int num_teams_per_arena: The usual number of teams per arena.
+    :param dict teams: The teams.
+    :param config: Custom configuration for the knockout scheduler.
+    """
+
+    num_teams_per_arena = 4
+    """
+    Constant value due to the way the automatic seeding works.
+    """
+
+    def __init__(
+        self,
+        schedule: 'MatchSchedule',
+        scores: Scores,
+        arenas: Iterable[ArenaName],
+        num_teams_per_arena: int,
+        teams: Mapping[TLA, Team],
+        config: YAMLData,
+    ) -> None:
+        if num_teams_per_arena != self.num_teams_per_arena:
+            raise ValueError(
+                "The automatic knockout scheduler can only be used for {0} teams"
+                " per arena (and not {1})".format(
+                    self.num_teams_per_arena,
+                    num_teams_per_arena,
+                ),
+            )
+
+        super().__init__(schedule, scores, arenas, num_teams_per_arena, teams, config)
+
+        self.R = stable_random.Random()
+
+        self.clock = MatchPeriodClock(self.period, self.schedule.delays)
+
+    def _add_round_of_matches(
+        self,
+        matches: List[List[TLA]],
+        arenas: Iterable[ArenaName],
+        rounds_remaining: int,
+    ) -> None:
+        """
+        Add a whole round of matches.
+
+        :param list matches: A list of lists of teams for each match.
+        """
+
+        self.knockout_rounds += [[]]
+
+        round_num = 0
+        while len(matches):
+            # Deliberately not using iterslots since we need to ensure
+            # that the time advances even after we've run out of matches
+            start_time = self.clock.current_time
+            end_time = start_time + self.schedule.match_duration
+
+            new_matches = {}
+            for arena in arenas:
+                teams = list(matches.pop(0))  # type: List[Optional[TLA]]
+
+                if len(teams) < self.num_teams_per_arena:
+                    # Fill empty zones with None
+                    teams += [None] * (self.num_teams_per_arena - len(teams))
+
+                # Randomise the zones
+                self.R.shuffle(teams)
+
+                num = MatchNumber(len(self.schedule.matches))
+                display_name = self.get_match_display_name(
+                    rounds_remaining,
+                    round_num,
+                    num,
+                )
+
+                match = Match(
+                    num,
+                    display_name,
+                    arena,
+                    teams,
+                    start_time,
+                    end_time,
+                    MatchType.knockout,
+                    # Just the finals don't use the resolved ranking
+                    use_resolved_ranking=rounds_remaining != 0,
+                )
+
+                self.knockout_rounds[-1].append(match)
+                new_matches[arena] = match
+
+                if len(matches) == 0:
+                    break
+
+            self.clock.advance_time(self.schedule.match_duration)
+            self.schedule.matches.append(MatchSlot(new_matches))
+            self.period.matches.append(MatchSlot(new_matches))
+
+            round_num += 1
+
+    def get_winners(self, game: Match) -> List[TLA]:
+        """
+        Find the parent match's winners.
+
+        :param game: A game.
+        """
+
+        ranking = self.get_ranking(game)
+        return ranking[:2]
+
+    def _add_round(self, arenas: Iterable[ArenaName], rounds_remaining: int) -> None:
+        prev_round = self.knockout_rounds[-1]
+        matches = []
+
+        for i in range(0, len(prev_round), 2):
+            winners = []
+            for parent in prev_round[i:i + 2]:
+                winners += self.get_winners(parent)
+
+            matches.append(winners)
+
+        self._add_round_of_matches(matches, arenas, rounds_remaining)
+
+    def _add_first_round(self, conf_arity: Optional[int] = None) -> None:
+        next_match_num = len(self.schedule.matches)
+        teams = self._get_non_dropped_out_teams(MatchNumber(next_match_num))
+        if not self._played_all_league_matches():
+            teams = [UNKNOWABLE_TEAM] * len(teams)
+
+        arity = len(teams)
+        if conf_arity is not None and conf_arity < arity:
+            arity = conf_arity
+
+        # Seed the random generator with the seeded team list
+        # This makes it unpredictable which teams will be in which zones
+        # until the league scores have been established
+        self.R.seed(''.join(teams).encode('utf-8'))
+
+        matches = []
+
+        for seeds in seeding.first_round_seeding(arity):
+            match_teams = [teams[seed] for seed in seeds]
+            matches.append(match_teams)
+
+        rounds_remaining = self.get_rounds_remaining(matches)
+        self._add_round_of_matches(matches, self.arenas, rounds_remaining)
+
+    @staticmethod
+    def get_rounds_remaining(prev_matches: Sized) -> int:
+        return int(math.log(len(prev_matches), 2))
+
+    def add_knockouts(self) -> None:
+        knockout_conf = self.config['knockout']
+        round_spacing = timedelta(seconds=knockout_conf['round_spacing'])
+
+        self._add_first_round(conf_arity=knockout_conf.get('arity'))
+
+        while len(self.knockout_rounds[-1]) > 1:
+
+            # Add the delay between rounds
+            self.clock.advance_time(round_spacing)
+
+            # Number of rounds remaining to be added
+            rounds_remaining = self.get_rounds_remaining(self.knockout_rounds[-1])
+
+            if rounds_remaining <= knockout_conf['single_arena']['rounds']:
+                arenas = knockout_conf['single_arena']['arenas']
+            else:
+                arenas = self.arenas
+
+            if len(self.knockout_rounds[-1]) == 2:
+                # Extra delay before the final match
+                final_delay = timedelta(seconds=knockout_conf['final_delay'])
+                self.clock.advance_time(final_delay)
+
+            self._add_round(arenas, rounds_remaining - 1)
---
+++ packages/sr.comp/sr.comp-1.4.4.tar.gz/sr.comp-1.4.4/sr/comp/match_period_clock.py
@@ -0,0 +1,148 @@
+"""A clock to manage match periods."""
+
+import datetime
+from typing import Iterable, Iterator, List, Optional
+
+from .match_period import Delay, MatchPeriod
+
+
+class OutOfTimeException(Exception):
+    """
+    An exception representing no more time available at the competition to run
+    matches.
+    """
+
+
+class MatchPeriodClock:
+    """
+    A clock for use in scheduling matches within a ``MatchPeriod``.
+
+    It is generally expected that the time information here will be in the form
+    of ``datetime`` and ``timedelta`` instances, though any data which can be
+    compared and added appropriately should work.
+
+    Delay rules:
+
+    - Only delays which are scheduled after the start of the given period will
+      be considered.
+    - Delays are cumilative.
+    - Delays take effect as soon as their ``time`` is reached.
+    """
+
+    @staticmethod
+    def delays_for_period(period: MatchPeriod, delays: Iterable[Delay]) -> List[Delay]:
+        """
+        Filter and sort a list of all possible delays to include only those
+        which occur after the start of the given `period`.
+
+        :param `.MatchPeriod` period: The period to get the delays for.
+        :param list delays: The list of :class:`.Delay` s to consider.
+        :return: A sorted list of delays which occur after the start of the period.
+        """
+
+        # Only consider delays which are at the start of the period or later
+        valid_delays = [d for d in delays if d.time >= period.start_time]
+
+        # Sort by when the delays occur
+        valid_delays.sort(key=lambda d: d.time)
+
+        return valid_delays
+
+    def __init__(self, period: MatchPeriod, delays: Iterable[Delay]):
+        """Create a new clock for the given period and collection of delays."""
+        self._period = period
+
+        self._delays = self.delays_for_period(period, delays)
+
+        # The current time, including any delays
+        self._current_time = period.start_time
+
+        # The total applied delay
+        self._total_delay = None  # type: Optional[datetime.timedelta]
+
+        # Apply any delays which occur at the start
+        self._apply_delays()
+
+    @property
+    def current_time(self) -> datetime.datetime:
+        """
+        Get the apparent current time. This is a combination of the time which
+        has passed (through calls to ``advance_time``) and the delays which
+        have occurred.
+
+        Will raise an :class:`.OutOfTimeException` if either:
+
+        - the end of the period has been reached (i.e: the sum of durations
+          passed to ``advance_time`` has exceeded the planned duration of the
+          period), or
+        - the maximum end of the period has been reached (i.e: the current time
+          would be after the period's ``max_end_time``).
+        """
+
+        ct = self._current_time
+
+        # Ensure we haven't exceeded the maximum time limit
+        # (if we have then matches will get pushed into the next period)
+        if ct > self._period.max_end_time:
+            # we've filled this up to the maximum end time
+            raise OutOfTimeException()
+
+        # Ensure we haven't attempted to pack in more time than will
+        # fit in this period
+        if self._time_without_delays() > self._period.end_time:
+            # we've filled up this period
+            raise OutOfTimeException()
+
+        return ct
+
+    def advance_time(self, duration: datetime.timedelta) -> None:
+        """
+        Make a given amount of time pass. This is expected to be called after
+        scheduling some matches in order to move to the next timeslot.
+
+        .. note::
+           It is assumed that the duration value will always be 'positive',
+           i.e. that time will not go backwards. The results of the duration
+           value being 'negative' are undefined.
+        """
+
+        self._current_time += duration
+        self._apply_delays()
+
+    def _apply_delays(self) -> None:
+        delays = self._delays
+        while len(delays) and delays[0].time <= self._current_time:
+            self._apply_delay(delays.pop(0).delay)
+
+    def _apply_delay(self, delay: datetime.timedelta) -> None:
+        self._current_time += delay
+
+        if self._total_delay is None:
+            self._total_delay = delay
+        else:
+            self._total_delay += delay
+
+    def _time_without_delays(self) -> datetime.datetime:
+        if self._total_delay is None:
+            return self._current_time
+        else:
+            return self._current_time - self._total_delay
+
+    def iterslots(self, slot_duration: datetime.timedelta) -> Iterator[datetime.datetime]:
+        """
+        Iterate through all the available timeslots of the given size within
+        the ``MatchPeriod``, taking into account delays.
+
+        This is equivalent to checking the current_time and repeatedly calling
+        ``advance_time`` with the given duration. As a result, it is safe to
+        call ``advance_time`` between iterations if additional gaps between
+        slots are needed.
+        """
+
+        try:
+            while True:
+                yield self.current_time
+                self.advance_time(slot_duration)
+        except OutOfTimeException:
+            # Reached the end of the period
+            pass
