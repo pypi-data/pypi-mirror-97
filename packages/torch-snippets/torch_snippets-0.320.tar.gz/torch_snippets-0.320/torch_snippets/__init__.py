6196d3baad2de794f89adc2757a1362f467f9e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:14 +0000

    Add package torch-snippets/0.320/torch_snippets-0.320.tar.gz

---
+++ packages/torch-snippets/torch_snippets-0.320.tar.gz/torch_snippets-0.320/torch_snippets/__init__.py
@@ -0,0 +1,7 @@
+from .loader import *
+from .fastcores import *
+from .charts import *
+try:
+    from .torch_loader import *
+except:
+    logger.warning('torch is not found. Skipping torch imports and loading only utilities')
