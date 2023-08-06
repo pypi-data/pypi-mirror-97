6cae0988c0ea899f45ca0b29f8148845bb697fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:42 +0000

    Add package quantifin/0.0.3a1/quantifin-0.0.3a1-py3-none-any.whl

---
+++ packages/quantifin/quantifin-0.0.3a1-py3-none-any.whl/quantifin/equity/Stock.py
@@ -0,0 +1,143 @@
+import yahoofinancials as yf
+import datetime
+class Stock(yf.YahooFinancials):
+    def __init__(self, stock_code):
+        self.stock_code = stock_code
+        super().__init__(stock_code)
+        self.__beta = None 
+        self.__cash_flow = None
+        self.__income = None
+        self.__balance_sheets = None
+        self.__key_stats = None
+
+    @staticmethod
+    def get_date_key(input_dict: dict):
+        return (list(input_dict.keys())[0])
+
+    @property
+    def beta(self):
+        if self.__beta == None:
+            self.__beta = self.get_beta()
+        return self.__beta
+    
+    @property
+    def trailing_eps(self):
+        try:
+            return self.key_stats['trailingEps']
+        except KeyError:
+            return None 
+    
+    @property
+    def forward_eps(self):
+        try:
+            return self.key_stats['forwardEps']
+        except KeyError:
+            return None
+
+    @property
+    def key_stats(self):
+        if self.__key_stats is None:
+            self.__key_stats = self.get_key_statistics_data()[self.stock_code]
+        return self.__key_stats
+
+    @property
+    def cash_flow_stmts(self):
+        if self.__cash_flow == None:
+            self.__cash_flow =  self.get_financial_stmts("annual", "cash")['cashflowStatementHistory'][self.stock_code]
+        return self.__cash_flow
+    
+    @property
+    def income_stmts(self):
+        if self.__income == None:
+            self.__income = self.get_financial_stmts("annual", "income")['incomeStatementHistory'][self.stock_code]
+        return self.__income
+    
+    @property
+    def balance_sheet_stmts(self):
+        if self.__balance_sheets == None:
+            self.__balance_sheets = self.get_financial_stmts("annual", "balance")['balanceSheetHistory'][self.stock_code]
+        return self.__balance_sheets
+
+
+    def full_year_dividend(self):
+        now_date = datetime.datetime.now()
+        start_of_year, end_of_year = self._get_FY_dividend_period(now_date)
+        dividend_resp = self.get_daily_dividend_data(start_of_year.strftime("%Y-%m-%d"), end_of_year.strftime("%Y-%m-%d"))
+        return self._calculate_full_dividend(dividend_resp)
+    
+    def _get_FY_dividend_period(self, now: datetime.datetime):
+        def current_year(now: datetime.datetime):
+            this_year = now.year
+            return [datetime.datetime(this_year, 1, 1), now]
+        
+        def previous_year(now: datetime.datetime):
+            year = now.year - 1
+            return [datetime.datetime(year, 1, 1), datetime.datetime(year, 12, 30)]
+
+        if (now.month == 12):
+            return current_year(now) 
+        return previous_year(now)
+
+    def _calculate_full_dividend(self, dividend_resp: dict):
+        if dividend_resp[self.stock_code] is None:
+            return 0
+        dividends = [payout['amount'] for payout in dividend_resp[self.stock_code]]
+        return round(sum(dividends), 3)
+
+    def get_dividend_payout_ratio_history(self):
+        ###Helper functions
+        
+        def calculate_payout(cash_flow):
+            try:
+                div_payout = abs(int(cash_flow['dividendsPaid']))
+            except KeyError:
+                return 0
+            net_income = int(cash_flow['netIncome'])
+            if net_income < 0:
+                return 0
+            return round(div_payout/net_income, 3)
+
+        def get_payout_ratios(cf_stmts_list):
+            result = {}
+            for cash_flow in self.cash_flow_stmts:
+                date_key = Stock.get_date_key(cash_flow)
+                result[date_key[:4]] = calculate_payout(cash_flow[date_key])
+            return result
+        ## End of helper functions
+        return get_payout_ratios(self.cash_flow_stmts)
+    
+    def average_dividend_payout_ratio(self, payout_ratio_history: dict):
+        payout_list = [payout for payout in payout_ratio_history.values()]
+        return round(sum(payout_list) / len(payout_list), 3)
+    
+    def get_roe_history(self):
+        result = {}
+        for i in range(len(self.balance_sheet_stmts)):
+            date_key = Stock.get_date_key(self.balance_sheet_stmts[i])
+            net_equity = self.balance_sheet_stmts[i][date_key]['totalAssets'] - self.balance_sheet_stmts[i][date_key]['totalLiab']
+            net_income = self.income_stmts[i][date_key]['netIncome']
+            result[date_key[:4]] = self.calculate_roe(net_income, net_equity)
+        return result
+
+    def calculate_roe(self, income: float, equity: float):
+        if equity == 0:
+            raise ValueError("Equity should be a non-zero value. 0 was passed as equity")
+        if equity < 0:
+            raise ValueError(f"Equity is negative: {equity}. A postive value should be passed in")
+        return round(income/equity, 3)
+
+    def average_roe(self, roe_history: dict):
+        sum = 0
+        for roe in roe_history.values():
+            sum += roe
+        return round(sum / len(roe_history), 3)
+    
+    def growth_rate(self):
+        payout = self.average_dividend_payout_ratio(self.get_dividend_payout_ratio_history())
+        roe = self.average_roe(self.get_roe_history())
+        if payout > 1:
+            raise ValueError("Payout rate exceeds 1. Invalid value for growth rate")
+        if roe < 0:
+            raise ValueError("ROE is negative. Invalid value for growth rate")
+        return round((1 - payout) * roe, 3)
+    
---
+++ packages/quantifin/quantifin-0.0.3a1-py3-none-any.whl/quantifin/equity/test/test_stock.py
@@ -0,0 +1,113 @@
+import unittest
+from unittest.mock import patch, MagicMock
+from pathlib import Path
+from json import load
+import yahoofinancials as yf
+from datetime import datetime
+
+from quantifin.equity import Stock
+from quantifin.equity.valuation import *
+from quantifin.util import markets
+
+@patch.object(yf.YahooFinancials, 'get_beta', return_value = 1.2)
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.base_path = Path(__file__).parent
+        self.index_json = self.read_json(str(self.base_path) + "/resource/market_price.json")
+    
+    def read_json(self, file_path):
+        with open (file_path) as f:
+            json_dict = load(f)
+        return json_dict
+
+    def test_CAPM(self, MockGetBeta):
+        req_rate_CAPM = CAPM(0.02, 0.1, 1.2)
+        self.assertEqual(req_rate_CAPM, 0.116)
+    
+    #################
+    #Dividend testing
+    #################
+    def test_get_FY_dividend_period(self, MockGetBeta):
+        stock = Stock("AAPL")
+        dec = datetime(2019, 12, 10)
+        self.assertEqual(stock._get_FY_dividend_period(dec), [datetime(2019, 1, 1), dec])
+
+        not_dec = datetime(2019, 11, 29)
+        self.assertEqual(stock._get_FY_dividend_period(not_dec), [datetime(2018, 1, 1), datetime(2018, 12, 30)])
+    
+
+    def test_calculate_dividend(self, MockGetBeta):
+        stock = Stock("D05.SI")
+
+        dividend_resp = self.read_json(str(self.base_path) + "/resource/dividend/2020_dbs_dividend.json")
+        
+        self.assertEqual(stock._calculate_full_dividend(dividend_resp), 1.35)
+        self.assertEqual(stock._calculate_full_dividend({"D05.SI": None}), 0)
+        amzn = Stock("AMZN")
+        dividend_resp = {'AMZN': None}
+        self.assertEqual(amzn._calculate_full_dividend(dividend_resp), 0)
+
+    @patch.object(yf.YahooFinancials, 'get_daily_dividend_data')
+    def test_get_current_dividend(self, MockDividend, MockGetBeta):
+            
+        MockDividend.side_effect = self.get_dividend_side_effect
+        stock = Stock("D05.SI")
+        self.assertEqual(stock.full_year_dividend(), 1.35)
+    
+    #Payout Ratio tests
+    @patch.object(yf.YahooFinancials, 'get_financial_stmts')
+    def test_average_payout_ratio(self, MockGetFnStmts, MockGetBeta):
+        MockGetFnStmts.side_effect = self.get_financial_stmts_side_effect
+        stock = Stock("D05.SI")
+        cf = stock.get_financial_stmts('annual', 'cash')
+        correct = {'2020': 0.511, '2019': 0.615, '2018': 0.565, '2017': 0.315}
+        self.assertEqual(stock.get_dividend_payout_ratio_history(), correct)
+        self.assertEqual(stock.average_dividend_payout_ratio(stock.get_dividend_payout_ratio_history()), 0.501)
+    
+    @patch.object(Stock, 'get_financial_stmts')
+    def test_payout_history_no_payout(self, MockCashFlow, MockGetBeta):
+        MockCashFlow.side_effect = lambda p, s: self.read_json(str(self.base_path) + '/resource/financial_statements/AMZN_cash_flow_stmts.json')
+        amzn = Stock("AMZN")
+        self.assertEqual(amzn.get_dividend_payout_ratio_history(), {'2020': 0, "2019": 0, "2018": 0, "2017": 0})
+    
+    @patch.object(yf.YahooFinancials, "get_financial_stmts")
+    def test_get_roe_history(self, MockGetFnStmts, MockGetBeta):
+        MockGetFnStmts.side_effect = self.get_financial_stmts_side_effect
+
+        stock = Stock("D05.SI")
+        correct_result = {"2020": 0.086, "2019": 0.123, "2018": 0.112, "2017": 0.088}
+        self.assertEqual(stock.get_roe_history(), correct_result)
+    
+    def test_calulate_roe(self, MockGetBeta):
+        stock = Stock("D05.SI")
+        self.assertRaises(ValueError, stock.calculate_roe, 100, 0)
+        self.assertRaises(ValueError, stock.calculate_roe, 100, -100)
+        self.assertEqual(stock.calculate_roe(50, 300), 0.167)
+    
+    @patch.object(yf.YahooFinancials, "get_financial_stmts")
+    def test_get_average_roe(self, MockGetFnStmts, MockGetBeta):
+        MockGetFnStmts.side_effect = self.get_financial_stmts_side_effect
+        stock = Stock("D05.SI")
+        roe = stock.get_roe_history()
+        self.assertEqual(stock.average_roe(roe), 0.102)
+
+    @patch.object(yf.YahooFinancials, "get_financial_stmts")
+    def test_growth_rate(self, MockGetFnStmts, MockGetBeta):
+        MockGetFnStmts.side_effect = self.get_financial_stmts_side_effect
+        stock = Stock("D05.SI")
+        self.assertEqual(stock.growth_rate(), 0.051)
+         
+
+    
+    def get_dividend_side_effect(self, start_date, end_date):
+        dividend_path = str(self.base_path) + "/resource/dividend/2020_dbs_dividend.json"
+        return self.read_json(dividend_path)
+
+    
+    def get_financial_stmts_side_effect(self, period, stmt_type):
+        if( stmt_type == "cash"):
+            return(self.read_json(str(self.base_path) + '/resource/financial_statements/cash_flow_stmts.json'))
+        if(stmt_type == "income"):
+            return(self.read_json(str(self.base_path) + '/resource/financial_statements/income_stmts.json'))
+        if(stmt_type == "balance"):
+            return(self.read_json(str(self.base_path) + '/resource/financial_statements/balance_sheet_stmts.json'))
