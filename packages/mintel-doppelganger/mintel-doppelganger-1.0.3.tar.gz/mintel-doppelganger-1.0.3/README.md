380f3cbe12b97f5a9a9a48828fbe379271b1be6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:34 +0000

    Add package mintel-doppelganger/1.0.3/mintel-doppelganger-1.0.3.tar.gz

---
+++ packages/mintel-doppelganger/mintel-doppelganger-1.0.3.tar.gz/mintel-doppelganger-1.0.3/README.md
@@ -0,0 +1,3 @@
+# Mintel Doppelganger - python edition
+
+Package for testing [dependency confusion](https://medium.com/@alex.birsan/dependency-confusion-4a5d60fec610) attack.
---
+++ packages/mintel-doppelganger/mintel-doppelganger-1.0.3.tar.gz/mintel-doppelganger-1.0.3/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="mintel-doppelganger",
+    version="1.0.3",
+    author="Kevin Soules",
+    author_email="inceax64@gmail.com",
+    description="Doppelganger?",
+    url="https://github.com/pypa/sampleproject",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
+
+open("/tmp/compromised", "w").write("yep")
