41dadf0f8950e29ed52af7d2d993aa9a929795c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:15 +0000

    Add package calclopts/0.0.1/calclopts-0.0.1.tar.gz

---
+++ packages/calclopts/calclopts-0.0.1.tar.gz/calclopts-0.0.1/setup.py
@@ -0,0 +1,56 @@
+"""
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="calclopt", # Replace with your own username
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
+"""
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="calclopts",
+    version=VERSION,
+    author="Jason Dsouza",
+    author_email="<youremail@email.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+
+    keywords=['python', 'first package'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
