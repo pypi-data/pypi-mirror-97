ac37c7114e5e4be2c76952081d05d1248e9a8cbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:15 +0000

    Add package django-model-tracker/1.4.2/django-model-tracker-1.4.2.tar.gz

---
+++ packages/django-model-tracker/django-model-tracker-1.4.2.tar.gz/django-model-tracker-1.4.2/ModelTracker/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from jsonfield import JSONField
+class History(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=255,default="")
+    table=models.CharField(max_length=255)
+    primary_key=models.CharField(max_length=255)
+    old_state=JSONField(default={})
+    new_state=JSONField(default={})
+    done_by=models.CharField(max_length=255)
+    done_on=models.DateTimeField(auto_now_add=True)
+    def __unicode__(self):
+        return self.id
+
