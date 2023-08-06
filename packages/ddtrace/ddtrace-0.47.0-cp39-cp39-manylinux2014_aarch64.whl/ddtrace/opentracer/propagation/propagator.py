095f8f44ff7adf66814ae5a85cf178037a75118f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:31 +0000

    Add package ddtrace/0.47.0/ddtrace-0.47.0-cp39-cp39-manylinux2014_aarch64.whl

---
+++ packages/ddtrace/ddtrace-0.47.0-cp39-cp39-manylinux2014_aarch64.whl/ddtrace/opentracer/propagation/propagator.py
@@ -0,0 +1,16 @@
+from abc import ABCMeta
+from abc import abstractmethod
+
+
+# ref: https://stackoverflow.com/a/38668373
+ABC = ABCMeta("ABC", (object,), {"__slots__": ()})
+
+
+class Propagator(ABC):
+    @abstractmethod
+    def inject(self, span_context, carrier):
+        pass
+
+    @abstractmethod
+    def extract(self, carrier):
+        pass
