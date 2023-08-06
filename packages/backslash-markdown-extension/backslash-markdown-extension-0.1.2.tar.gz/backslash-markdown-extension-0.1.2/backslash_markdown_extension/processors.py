f719079946e68e330a6a057183feef6590f395c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:52 +0000

    Add package backslash-markdown-extension/0.1.2/backslash-markdown-extension-0.1.2.tar.gz

---
+++ packages/backslash-markdown-extension/backslash-markdown-extension-0.1.2.tar.gz/backslash-markdown-extension-0.1.2/backslash_markdown_extension/processors.py
@@ -0,0 +1,70 @@
+import copy
+import xml.etree.ElementTree as etree
+
+from markdown.treeprocessors import Treeprocessor
+
+
+class BackslashTreeProcessor(Treeprocessor):
+
+	def run(self, root):
+		# print('ROOT', root, root.tag, root.attrib, len(root), etree.tostring(root))
+		elements = etree.Element('div')
+
+		for element in root:
+			print('ELEMENT', element, element.tag, element.attrib, len(element), etree.tostring(element))
+
+			# Paragraphs
+			if element.tag == 'p':
+				element.tag = 'li'
+				elements.append(element)
+				line = etree.Element('li')
+				line.tail = '\n'
+				elements.append(line)
+
+			# Headers
+			elif element.tag in ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']:
+				item = etree.Element('li')
+				header = etree.SubElement(item, element.tag, attrib=element.attrib)
+				header.text = element.text
+				header.tail = ''
+				item.tail = '\n'
+				elements.append(item)
+				line = etree.Element('li')
+				line.tail = '\n'
+				elements.append(line)
+
+			# Blockquotes
+			# elif element.tag == 'blockquote':
+			# 	pass
+
+			# Lists
+			elif element.tag in ['ul', 'ol']:
+
+				for sub_element in element:
+					item = etree.Element('li')
+					span = etree.SubElement(item, 'span', attrib={'class': 'indent'})
+					span.tail = sub_element.text
+					elements.append(item)
+					item.tail = '\n'
+					line = etree.Element('li')
+					line.tail = '\n'
+					elements.append(line)
+
+			# Code blocks
+			# elif element.tag == 'pre':
+			# 	pass
+
+			# Horiztonal rules
+			elif element.tag == 'hr':
+				item = etree.Element('li')
+				etree.SubElement(item, element.tag, attrib=element.attrib)
+				item.tail = '\n'
+				elements.append(item)
+				line = etree.Element('li')
+				line.tail = '\n'
+				elements.append(line)
+
+			else:
+				elements.append(element)
+
+		return elements
