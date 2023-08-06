1ab4a202ba877a1c7e665b32a2362f2756ab2a10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:39 +0000

    Add package yaasr/0.4.5/yaasr-0.4.5.tar.gz

---
+++ packages/yaasr/yaasr-0.4.5.tar.gz/yaasr-0.4.5/yaasr/__init__.py
@@ -0,0 +1,17 @@
+import os
+
+
+__VERSION__ = '0.4.5'
+BASE_FOLDER = os.path.dirname(__file__)
+STREAMS_FOLDER = os.path.join(BASE_FOLDER, "streams")
+
+
+def get_all_streams():
+    streams = []
+    for subdir, dirs, files in os.walk(STREAMS_FOLDER):
+        for sdir in dirs:
+            data_file = os.path.join(STREAMS_FOLDER, sdir, 'data.json')
+            if os.path.isfile(data_file):
+                streams.append(sdir)
+
+    return streams
