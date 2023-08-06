4f15b686300f450577ba6857ba148135ede194e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:35 +0000

    Add package ssht00ls/3.18.1/ssht00ls-3.18.1.tar.gz

---
+++ packages/ssht00ls/ssht00ls-3.18.1.tar.gz/ssht00ls-3.18.1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Note!
+# ' are required, do not use any '.
+
+# setup.
+from setuptools import setup, find_packages
+setup(
+	name='ssht00ls',
+	version='3.18.1',
+	description='Some description.',
+	url='http://github.com/vandenberghinc/ssht00ls',
+	author='Daan van den Bergh',
+	author_email='vandenberghinc.contact@gmail.com',
+	license='MIT',
+	packages=find_packages(),
+	zip_safe=False,
+	install_requires=[
+        'asgiref',
+        'certifi',
+        'chardet',
+        'Django',
+        'idna',
+        'pexpect',
+        'ptyprocess',
+        'pytz',
+        'requests',
+        'sqlparse',
+        'urllib3',
+    ],)
