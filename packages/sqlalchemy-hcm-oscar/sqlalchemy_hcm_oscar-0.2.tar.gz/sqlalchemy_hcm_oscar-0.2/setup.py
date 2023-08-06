7fafcf7d4b0e64a6f4278ecc9ea6b4f91e705bf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:28 +0000

    Add package sqlalchemy-hcm-oscar/0.2/sqlalchemy_hcm_oscar-0.2.tar.gz

---
+++ packages/sqlalchemy-hcm-oscar/sqlalchemy_hcm_oscar-0.2.tar.gz/sqlalchemy_hcm_oscar-0.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""
+Setup for SQLAlchemy backend for DM
+"""
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup_params = dict(
+    name="sqlalchemy_hcm_oscar",
+    version='0.2',
+    description="SQLAlchemy dialect for oscar special for hcm",
+    author="tangrj",
+    author_email="tangrj@inspur.com",
+    keywords='oscar hcm SQLAlchemy',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        "sqlalchemy.dialects":
+            ["oscar = sqlalchemy_oscar.stpython:OracleDialect_stpython", "oscar.stpython = sqlalchemy_oscar.stpython:OracleDialect_stpython"]
+    },
+    install_requires=['sqlalchemy'],
+)
+
+if __name__ == '__main__':
+    setup(**setup_params)
