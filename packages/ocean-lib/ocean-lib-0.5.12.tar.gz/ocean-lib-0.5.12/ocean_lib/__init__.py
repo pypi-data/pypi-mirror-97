159b0c0a695e54f99643ff43ecf0dcc3d58e8d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:37 +0000

    Add package ocean-lib/0.5.12/ocean-lib-0.5.12.tar.gz

---
+++ packages/ocean-lib/ocean-lib-0.5.12.tar.gz/ocean-lib-0.5.12/ocean_lib/__init__.py
@@ -0,0 +1,9 @@
+#
+# Copyright 2021 Ocean Protocol Foundation
+# SPDX-License-Identifier: Apache-2.0
+#
+
+"""Initialises ocean lib package."""
+
+__author__ = """OceanProtocol"""
+__version__ = "__version__ = '0.5.12'"
---
+++ packages/ocean-lib/ocean-lib-0.5.12.tar.gz/ocean-lib-0.5.12/ocean_lib/assets/asset.py
@@ -0,0 +1,19 @@
+#
+# Copyright 2021 Ocean Protocol Foundation
+# SPDX-License-Identifier: Apache-2.0
+#
+from ocean_utils.ddo.ddo import DDO
+
+
+class Asset(DDO):
+    @property
+    def data_token_address(self):
+        return self._other_values["dataToken"]
+
+    @data_token_address.setter
+    def data_token_address(self, token_address):
+        self._other_values["dataToken"] = token_address
+
+    @property
+    def values(self):
+        return self._other_values.copy()
