f756d50148563fb6e5da99073f81ea98782f779e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:01 +0000

    Add package pyclimacell/0.17.0/pyclimacell-0.17.0.tar.gz

---
+++ packages/pyclimacell/pyclimacell-0.17.0.tar.gz/pyclimacell-0.17.0/tests/test_pyclimacell.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""Tests for `pyclimacell` package."""
+
+
+import unittest
+
+
+class TestPyclimacell(unittest.TestCase):
+    """Tests for `pyclimacell` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
