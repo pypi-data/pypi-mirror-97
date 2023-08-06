57ac3009c1d39e2a1b4a42078e7e8fd4e0362414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:12 +0000

    Add package kyokusui/0.1.2/kyokusui-0.1.2-py3-none-any.whl

---
+++ packages/kyokusui/kyokusui-0.1.2-py3-none-any.whl/kyokusui/forms.py
@@ -0,0 +1,12 @@
+from mitama.app.forms import Form, Field, DictField
+
+
+class CreateBoardForm(Form):
+    name = Field(label="タイトル", required=True)
+    owner = Field(label="板主", required=True)
+
+class CreateThreadForm(Form):
+    title = Field(label="タイトル", required=True)
+
+class SettingForm(Form):
+    permissions = DictField(label="権限", listed=True)
