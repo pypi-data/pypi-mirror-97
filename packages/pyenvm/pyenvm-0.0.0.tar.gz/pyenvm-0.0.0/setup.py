6048f854268a625e3138ef0ae463e279a44f5c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:47 +0000

    Add package pyenvm/0.0.0/pyenvm-0.0.0.tar.gz

---
+++ packages/pyenvm/pyenvm-0.0.0.tar.gz/pyenvm-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pyenvm',
+    version='0.0.0',
+    description='Python environment manager',
+    long_description='',
+    url='https://github.com/csci-arch/pyenvm',
+    author='csci-arch',
+    author_email='csciarch@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
