849121d66cf34b77d950545e7b7df6a18ff54740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:00 +0000

    Add package automl-alex/1.3.5/automl_alex-1.3.5-py3-none-any.whl

---
+++ packages/automl-alex/automl_alex-1.3.5-py3-none-any.whl/automl_alex/encoders.py
@@ -0,0 +1,33 @@
+
+from category_encoders import HashingEncoder, SumEncoder, PolynomialEncoder, BackwardDifferenceEncoder 
+from category_encoders import OneHotEncoder, HelmertEncoder, OrdinalEncoder, BaseNEncoder
+from category_encoders import TargetEncoder, CatBoostEncoder, WOEEncoder, JamesSteinEncoder
+from category_encoders.count import CountEncoder
+
+################################################################
+            #               Simple Encoders 
+            #      (do not use information about target)
+################################################################
+
+cat_encoders_names = {
+                'HashingEncoder': HashingEncoder,
+                'SumEncoder': SumEncoder,
+                'BackwardDifferenceEncoder': BackwardDifferenceEncoder,
+                'OneHotEncoder': OneHotEncoder,
+                'HelmertEncoder': HelmertEncoder,
+                'BaseNEncoder': BaseNEncoder,
+                'CountEncoder': CountEncoder,
+                }
+
+
+
+################################################################
+            #                Target Encoders
+################################################################
+
+target_encoders_names = {
+                'TargetEncoder': TargetEncoder,
+                'CatBoostEncoder': CatBoostEncoder,
+                'WOEEncoder': WOEEncoder,
+                'JamesSteinEncoder': JamesSteinEncoder,
+                }
