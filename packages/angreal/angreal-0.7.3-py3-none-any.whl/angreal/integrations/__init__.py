4384c3212da914911cdf1b3d32b9c38c5593825b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:51 +0000

    Add package angreal/0.7.3/angreal-0.7.3-py3-none-any.whl

---
+++ packages/angreal/angreal-0.7.3-py3-none-any.whl/angreal/integrations/__init__.py
@@ -0,0 +1,13 @@
+"""
+    angreal.integrations
+    ~~~~~~~~~~~~~~~~~~~~
+
+"""
+
+from angreal.integrations.gitremote import GitRemote
+from angreal.integrations.gh import GitHub
+from angreal.integrations.gl import GitLab
+from angreal.integrations.git import Git
+from angreal.integrations.virtual_env import VirtualEnv, venv_required
+
+
