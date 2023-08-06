39b7a450a4b9f70082d240e0f55bd22d6042b89e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:39 +0000

    Add package veracode-api-py/0.9.10/veracode_api_py-0.9.10.tar.gz

---
+++ packages/veracode-api-py/veracode_api_py-0.9.10.tar.gz/veracode_api_py-0.9.10/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+  name = 'veracode_api_py',         
+  packages = ['veracode_api_py'],   
+  version = '0.9.10',      
+  license='MIT',        
+  description = 'Python helper library for working with the Veracode APIs. Handles retries, pagination, and other features of the modern Veracode REST APIs.',   
+  author = 'Tim Jarrett',                  
+  author_email = 'tjarrett@veracode.com',      
+  url = 'https://github.com/tjarrettveracode',   
+  download_url = 'https://github.com/tjarrettveracode/veracode-api-py/archive/v_094.tar.gz',    
+  keywords = ['veracode', 'veracode-api'],   
+  install_requires=[            
+          'veracode-api-signing'
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3'
+  ],
+)
