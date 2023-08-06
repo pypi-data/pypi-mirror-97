a611c15eb8de6f8bd972d8c901fb3a94361825d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:38 +0000

    Add package camphr/0.8.6/camphr-0.8.6-py3-none-any.whl

---
+++ packages/camphr/camphr-0.8.6-py3-none-any.whl/camphr/__init__.py
@@ -0,0 +1,4 @@
+from .models import load
+from .VERSION import __version__
+
+__all__ = ["__version__", "load"]
---
+++ packages/camphr/camphr-0.8.6-py3-none-any.whl/camphr/consts.py
@@ -0,0 +1,4 @@
+"""Constants used accross sub packages"""
+KEY_FSTRING = "fstring"
+KEY_KNP_FSTRING = "knp_fstring"
+JUMAN_LINES = "juman_lines"
