4c05bfd8f07d8bc0420c2b7f16daa7be44bd393d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:07 +0000

    Add package connect-openapi-client/22.0.1/connect-openapi-client-22.0.1.tar.gz

---
+++ packages/connect-openapi-client/connect-openapi-client-22.0.1.tar.gz/connect-openapi-client-22.0.1/cnct/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of the Ingram Micro Cloud Blue Connect connect-fluent-client.
+# Copyright (c) 2019-2020 Ingram Micro. All Rights Reserved.
+
+from cnct.client.exceptions import ClientError  # noqa
+from cnct.client.fluent import ConnectClient  # noqa
+from cnct.client.version import get_version  # noqa
+from cnct.rql import R  # noqa
