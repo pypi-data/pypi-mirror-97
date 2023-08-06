e5d91c2c1bc3ae2b740b696a328e93e8cce5445b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:11 +0000

    Add package kpireport-plot/0.2.0/kpireport-plot-0.2.0.tar.gz

---
+++ packages/kpireport-plot/kpireport-plot-0.2.0.tar.gz/kpireport-plot-0.2.0/kpireport_plot/templates/plot.md
@@ -0,0 +1,1 @@
+{% if link_url %}[{% endif %}{{ figure | blob }}{% if link_url %}]({{ link_url }}){% endif %}
---
+++ packages/kpireport-plot/kpireport-plot-0.2.0.tar.gz/kpireport-plot-0.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="kpireport-plot",
+    version="0.2.0",
+    author="KPI Reporter LLC",
+    author_email="dev@kpireporter.com",
+    url="https://kpireporter.com",
+    license="Prosperity Public License",
+    packages=["kpireport_plot"],
+    install_requires=["kpireport", "matplotlib"],
+    package_data={"kpireport_plot": ["templates/*"]},
+    entry_points={
+        "kpireport.view": [
+            "plot = kpireport_plot:Plot",
+            "single_stat = kpireport_plot:SingleStat",
+        ],
+    },
+)
