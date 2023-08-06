b48077faf86a1044ed15eb7d1bd4a452ce65caed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:22:46 +0000

    Add package syst3m/2.16.5/syst3m-2.16.5.tar.gz

---
+++ packages/syst3m/syst3m-2.16.5.tar.gz/syst3m-2.16.5/syst3m/classes/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+from syst3m.classes import utils, console, objects, browser, cache, restapi
+from syst3m.classes.system import User, Group, check_user, check_group
+from syst3m.classes import service
+from syst3m.classes import defaults
+from syst3m.classes.env import env
+from syst3m.classes.disks import disks
+from syst3m.classes.restapi import restapi, RestAPI
+#from syst3m.classes.screen import screen
+#from syst3m.classes.keyboard import keyboard
+#from syst3m.classes.mouse import mouse
