488c2ce5f25a20a603a106be5f8856033b6c6307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:35 +0000

    Add package ZMCintegral/5.3/ZMCintegral-5.3.tar.gz

---
+++ packages/ZMCintegral/ZMCintegral-5.3.tar.gz/ZMCintegral-5.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ZMCintegral",
+    version="5.3",
+    author="Cao Xiao-Yan, ZHANG Jun-Jie",
+    author_email="zjacob@mail.ustc.edu.cn",
+    description="An easy way to use multi-GPUs to calculate multi-dimensional integration",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Letianwu/ZMCintegral",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.6',
+)
