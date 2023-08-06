30d42b92dd4ca809b22aecdedf18f28b6f98b3ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:14 +0000

    Add package pysondb/0.5.0/pysondb-0.5.0.tar.gz

---
+++ packages/pysondb/pysondb-0.5.0.tar.gz/pysondb-0.5.0/CHANGELOG.md
@@ -0,0 +1,8 @@
+# pysonDB
+
+### _Changelog_
+
+* `dev`
+  * New exceptions: `DataNotFoundError`, `IdNotFoundError`, `SchemaError`
+  * YAML support
+  * New database classes: `Database`, `UuidDatabase`, `YamlDatabase`, `YamlUuidDatabase`
---
+++ packages/pysondb/pysondb-0.5.0.tar.gz/pysondb-0.5.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    author="Fredy Somy",
+    author_email="fredysomy@gmail.com",
+    python_requires=">=3",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+    ],
+    description="A Python JSON and/or YAML based lightweight Database.",
+    install_requires=["fire", "beautifultable", "filelock", "pyaml"],
+    license="Apache Software License 2.0",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    version="0.5.0",
+    keywords="pysondb,database,json,yaml",
+    name="pysondb",
+    packages=["pysondb"],
+    entry_points={"console_scripts": ["pysondb=pysondb.cli:main"]},
+    setup_requires=[],
+    url="https://github.com/fredysomy/pysonDB",
+    zip_safe=False,
+)
