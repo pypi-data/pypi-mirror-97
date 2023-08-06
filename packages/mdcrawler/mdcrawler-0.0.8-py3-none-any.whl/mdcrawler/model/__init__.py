b767a7dfffb9ed507740b3cb0d825eb20f6caa82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:52 +0000

    Add package mdcrawler/0.0.8/mdcrawler-0.0.8-py3-none-any.whl

---
+++ packages/mdcrawler/mdcrawler-0.0.8-py3-none-any.whl/mdcrawler/model/__init__.py
@@ -0,0 +1,6 @@
+from mdcrawler.model.user import User
+from mdcrawler.model.playback import Playback
+from mdcrawler.model.chat import Chat
+from mdcrawler.model.initializer_model import InitializerModel
+
+__all__ = ["User","PlayBack","Chat","InitializerModel"]
