00fcb269759957b728ed69956c13ed1989e67e2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:06 +0000

    Add package m9s-customs/5.2.4/m9s_customs-5.2.4.tar.gz

---
+++ packages/m9s-customs/m9s_customs-5.2.4.tar.gz/m9s_customs-5.2.4/tests/__init__.py
@@ -0,0 +1,9 @@
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+
+try:
+    from trytond.modules.customs.tests.test_customs import suite
+except ImportError:
+    from .test_customs import suite
+
+__all__ = ['suite']
