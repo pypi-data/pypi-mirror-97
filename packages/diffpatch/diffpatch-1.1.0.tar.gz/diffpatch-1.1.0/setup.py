4193b81e9ebd5cda51af658bad8a98b5967e82ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:49 +0000

    Add package diffpatch/1.1.0/diffpatch-1.1.0.tar.gz

---
+++ packages/diffpatch/diffpatch-1.1.0.tar.gz/diffpatch-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	name="diffpatch",
+	version="1.1.0",
+	author="Isaac Turner",
+	url="https://gist.github.com/noporpoise/16e731849eb1231e86d78f9dfeca3abc",
+	description="A library to make and apply patches",
+	py_modules=['diffpatch']
+)
