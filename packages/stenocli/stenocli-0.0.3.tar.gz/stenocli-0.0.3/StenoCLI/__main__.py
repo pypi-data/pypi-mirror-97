c161240464b811d490040222a7cc94e31c8b5fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:00 +0000

    Add package stenocli/0.0.3/stenocli-0.0.3.tar.gz

---
+++ packages/stenocli/stenocli-0.0.3.tar.gz/stenocli-0.0.3/StenoCLI/__main__.py
@@ -0,0 +1,45 @@
+from __future__ import print_function, unicode_literals
+from PyInquirer import prompt
+from pprint import pprint
+
+from StenoCLI.Image import photoSteganography
+from StenoCLI.Text import textSteganography
+
+questions = [
+    {
+        'type': 'checkbox',
+        'qmark': '🤔',
+        'message': 'What do you wanna do ?',
+        'name': 'command',
+        'choices': [
+            {
+                'name': 'Photo-steganography'
+            },
+            {
+                'name': 'Text-steganography'
+            },
+            {
+                'name': 'Exit'
+            },
+        ],
+    }
+]
+
+def main():
+    while (True):
+        answers = prompt(questions)
+        if(len(answers['command']) != 0):
+            print('Ready to perform ', answers['command'][0])
+            break
+        print("Please select at least one option!")
+
+    command = answers['command'][0]
+
+    if command == 'Photo-steganography':
+        photoSteganography()
+    elif command == 'Text-steganography':
+        textSteganography()
+
+if __name__ == "__main__":
+    main()
+        
