4b3bdc4d73d16300459a1ffe8c43fcf3c958bfef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:17 +0000

    Add package hanlp-downloader/0.0.2/hanlp_downloader-0.0.2.tar.gz

---
+++ packages/hanlp-downloader/hanlp_downloader-0.0.2.tar.gz/hanlp_downloader-0.0.2/tests/download_file.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+# Author: hankcs
+# Date: 2021-03-04 19:04
+import os
+
+from hanlp_downloader import Downloader
+from hanlp_downloader.log import DownloadCallback
+
+
+def sha1sum(file_name):
+    import hashlib
+    BUF_SIZE = 65536
+
+    sha1 = hashlib.sha1()
+
+    with open(file_name, 'rb') as f:
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                break
+            sha1.update(data)
+
+    return sha1.hexdigest()
+
+
+def main():
+    file_path = '1.zip'
+    downloader = Downloader(
+        'https://file.hankcs.com/model/hanlp-wiki-vec-zh.zip', file_path, 2,
+        high_speed=False)
+    downloader.subscribe(DownloadCallback())
+    downloader.start_sync()
+    assert sha1sum(file_path) == '79eaddb99a04763f1cce33cb2b1b81c0bbeeb77b'
+    os.remove(file_path)
+
+
+if __name__ == '__main__':
+    main()
