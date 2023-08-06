0fd6f4e8a0182828e3cfa78f603af6a0d229b147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:50 +0000

    Add package nettlesome/0.2.1/nettlesome-0.2.1.tar.gz

---
+++ packages/nettlesome/nettlesome-0.2.1.tar.gz/nettlesome-0.2.1/tests/test_explanations.py
@@ -0,0 +1,36 @@
+from nettlesome.terms import ContextRegister, means
+
+from nettlesome.explanations import Explanation
+from nettlesome.predicates import Predicate
+from nettlesome.statements import Statement
+from nettlesome.entities import Entity
+
+
+class TestContext:
+    sale = Predicate("$seller sold $product to $buyer")
+    fact_al = Statement(sale, terms=[Entity("Al"), Entity("the bull"), Entity("Betty")])
+    fact_alice = Statement(
+        sale, terms=[Entity("Alice"), Entity("the cow"), Entity("Bob")]
+    )
+
+    def test_impossible_register(self):
+        context = ContextRegister()
+        context.insert_pair(Entity("Al"), Entity("Bob"))
+        answers = self.fact_al.update_context_register(self.fact_alice, context, means)
+        assert not any(answers)
+
+    def test_possible_register(self):
+        register = ContextRegister()
+        register.insert_pair(Entity("Al"), Entity("Alice"))
+        answers = self.fact_al.update_context_register(self.fact_alice, register, means)
+        assert Entity("the bull").short_string in next(answers).keys()
+
+    def test_explain_consistency(self):
+        register = ContextRegister()
+        register.insert_pair(Entity("Al"), Entity("Alice"))
+        answers = self.fact_al.explain_consistent_with(self.fact_alice, register)
+        explanation = Explanation(
+            factor_matches=[(self.fact_al, self.fact_alice)], context=answers
+        )
+        assert "<the bull> is like <the cow>" in explanation.context.reason
+        assert "terms=(Entity(name='Al'" in repr(explanation)
---
+++ packages/nettlesome/nettlesome-0.2.1.tar.gz/nettlesome-0.2.1/tests/test_quantities.py
@@ -0,0 +1,22 @@
+from datetime import date
+
+from nettlesome import predicates
+from nettlesome.quantities import UnitRange, NumberRange, DateRange, Q_
+
+
+class TestQuantities:
+    def test_unitregistry_imports_do_not_conflict(self, make_comparison):
+        left = UnitRange(quantity=Q_("20 meters"), sign=">")
+        right = make_comparison["meters"].quantity_range
+        assert left.implies(right)
+
+    def test_no_contradiction_between_classes(self):
+        left = UnitRange(quantity=Q_("2000 days"), sign="<")
+        right = NumberRange(quantity=2000, sign=">")
+        assert left.magnitude == right.magnitude
+        assert not left.contradicts(right)
+
+    def test_contradiction_between_date_ranges(self):
+        left = DateRange(quantity=date(2000, 1, 1), sign="<")
+        right = DateRange(quantity=date(2020, 12, 12), sign=">")
+        assert left.contradicts(right)
