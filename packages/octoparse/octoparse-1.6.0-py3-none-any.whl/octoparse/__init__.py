e71eb91560d576d72609dbb2d2222762c2e5be50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:56 +0000

    Add package octoparse/1.6.0/octoparse-1.6.0-py3-none-any.whl

---
+++ packages/octoparse/octoparse-1.6.0-py3-none-any.whl/octoparse/__init__.py
@@ -0,0 +1,6 @@
+from .__version__ import __title__, __description__, __url__
+from .__version__ import __version__, __build__, __author_email__
+from .__version__ import __author__, __license__, __copyright__
+
+from .octoparse import Octoparse
+from .octoparse import _get_request, _post_request
