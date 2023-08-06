d19daf2e8030790cb9cfeaa7e72f43534f8c7eeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:51 +0000

    Add package dataiter/0.19/dataiter-0.19.tar.gz

---
+++ packages/dataiter/dataiter-0.19.tar.gz/dataiter-0.19/dataiter/__init__.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2020 Osmo Salomaa
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from dataiter.vector import Vector # noqa
+from dataiter.data_frame import DataFrame # noqa
+from dataiter.data_frame import DataFrameColumn # noqa
+from dataiter.geojson import GeoJSON # noqa
+from dataiter.list_of_dicts import ListOfDicts # noqa
+
+__version__ = "0.19"
+
+DEFAULT_PEEK_ELEMENTS = 10
+DEFAULT_PEEK_ITEMS = 3
+DEFAULT_PEEK_ROWS = 10
+PRINT_FLOAT_PRECISION = 6
+PRINT_MAX_ELEMENTS = 100
+PRINT_MAX_ITEMS = 10
+PRINT_MAX_ROWS = 100
+
+# Only used as a fallback, see util.get_print_width.
+PRINT_MAX_WIDTH = 80
+
+def ncol(data):
+    """
+    Return the amount of columns in `data`.
+    """
+    return data.ncol
+
+def nrow(data):
+    """
+    Return the amount of rows in `data`.
+
+    This is a useful shorthand for `data.nrow` in contexts where you don't have
+    direct access to the data frame in question, e.g. in group-by-aggregate
+
+    >>> data = di.DataFrame.read_csv("data/listings.csv")
+    >>> data.group_by("hood").aggregate(n=di.nrow)
+    """
+    return data.nrow
