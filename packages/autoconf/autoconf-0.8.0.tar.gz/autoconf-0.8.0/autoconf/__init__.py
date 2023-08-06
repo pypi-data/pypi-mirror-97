717822682642f7a72fd9e8d4e5197987d943e00c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:58 +0000

    Add package autoconf/0.8.0/autoconf-0.8.0.tar.gz

---
+++ packages/autoconf/autoconf-0.8.0.tar.gz/autoconf-0.8.0/autoconf/__init__.py
@@ -0,0 +1,9 @@
+from . import exc
+from .conf import Config
+from .conf import instance
+from .json_prior.config import default_prior
+from .json_prior.config import make_config_for_class
+from .json_prior.config import path_for_class
+from .json_prior.config import JSONPriorConfig
+
+__version__ = '0.8.0'
---
+++ packages/autoconf/autoconf-0.8.0.tar.gz/autoconf-0.8.0/test_autoconf/test_config.py
@@ -0,0 +1,39 @@
+from os import path
+
+import pytest
+
+from autoconf.directory_config import NamedConfig
+from autoconf.mock.mock_real import EllipticalProfile, EllipticalGaussian
+
+directory = path.dirname(path.realpath(__file__))
+
+
+class MockClass:
+    pass
+
+
+@pytest.fixture(name="label_config")
+def make_label_config():
+    return NamedConfig(
+        "{}/files/config/label.ini".format(directory)
+    )
+
+
+class TestLabel:
+    def test_basic(self, label_config):
+        assert label_config["label"]["centre_0"] == "x"
+        assert label_config["label"]["redshift"] == "z"
+
+    def test_escaped(self, label_config):
+        assert label_config["label"]["gamma"] == r"\gamma"
+        assert label_config["label"]["contribution_factor"] == r"\omega0"
+
+    def test_subscript(self, label_config):
+        assert label_config["subscript"].family(EllipticalProfile) == "l"
+
+    def test_inheritance(self, label_config):
+        assert label_config["subscript"].family(EllipticalGaussian) == "l"
+
+    def test_exception(self, label_config):
+        with pytest.raises(KeyError):
+            label_config["subscript"].family(MockClass)
