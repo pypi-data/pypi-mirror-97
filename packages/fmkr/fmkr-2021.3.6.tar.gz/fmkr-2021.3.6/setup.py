bd833c818f9a80f8069fcb7b5fb913cf6adb93c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:08 +0000

    Add package fmkr/2021.3.6/fmkr-2021.3.6.tar.gz

---
+++ packages/fmkr/fmkr-2021.3.6.tar.gz/fmkr-2021.3.6/setup.py
@@ -0,0 +1,72 @@
+# fmkr/setup.py
+
+"""Fmkr package setuptools script."""
+
+import sys
+import re
+
+from setuptools import setup
+
+with open('fmkr/fmkr.py') as fh:
+    code = fh.read()
+
+version = re.search(r"__version__ = '(.*?)'", code).groups()[0]
+
+description = re.search(r'"""(.*)\.(?:\r\n|\r|\n)', code).groups()[0]
+
+readme = re.search(
+    r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}__version__',
+    code,
+    re.MULTILINE | re.DOTALL,
+).groups()[0]
+
+readme = '\n'.join(
+    [description, '=' * len(description)] + readme.splitlines()[1:]
+)
+
+license = re.search(
+    r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""',
+    code,
+    re.MULTILINE | re.DOTALL,
+).groups()[0]
+
+license = license.replace('# ', '').replace('#', '')
+
+if 'sdist' in sys.argv:
+    with open('LICENSE', 'w') as fh:
+        fh.write('BSD 3-Clause License\n\n')
+        fh.write(license)
+    with open('README.rst', 'w') as fh:
+        fh.write(readme)
+
+setup(
+    name='fmkr',
+    version=version,
+    description=description,
+    long_description=readme,
+    author='Christoph Gohlke',
+    author_email='cgohlke@uci.edu',
+    url='https://www.lfd.uci.edu/~gohlke/',
+    project_urls={
+        'Bug Tracker': 'https://github.com/cgohlke/fmkr/issues',
+        'Source Code': 'https://github.com/cgohlke/fmkr',
+        # 'Documentation': 'https://',
+    },
+    packages=['fmkr'],
+    install_requires=['lxml>=4.2'],
+    python_requires='>=3.6',
+    license='BSD',
+    platforms=['any'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Database',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
