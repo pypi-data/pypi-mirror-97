51e396b1c046dd79dd4cf9ce67d64d1112700758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:34 +0000

    Add package vaex-jupyter/0.6.0/vaex-jupyter-0.6.0.tar.gz

---
+++ packages/vaex-jupyter/vaex-jupyter-0.6.0.tar.gz/vaex-jupyter-0.6.0/setup.py
@@ -0,0 +1,32 @@
+import os
+import imp
+from setuptools import setup
+from setuptools import Extension
+
+dirname = os.path.dirname(__file__)
+path_version = os.path.join(dirname, 'vaex/jupyter/_version.py')
+version = imp.load_source('version', path_version)
+
+
+name = 'vaex'
+author = 'Maarten A. Breddels'
+author_email = 'maartenbreddels@gmail.com'
+license = 'MIT'
+version = version.__version__
+url = 'https://www.github.com/maartenbreddels/vaex'
+install_requires_jupyter = ['vaex-core>=4.0.0,<5', 'vaex-viz', 'bqplot>=0.10.1', 'ipyvolume>=0.4', 'ipyleaflet', 'ipympl', 'ipyvuetify>=1.2.2,<2', 'xarray']
+
+setup(name=name + '-jupyter',
+      version=version,
+      description='Jupyter notebook and Jupyter lab support for vaex',
+      url=url,
+      author=author,
+      author_email=author_email,
+      install_requires=install_requires_jupyter,
+      license=license,
+      packages=['vaex.jupyter', 'vaex.jupyter.vendor'],
+      zip_safe=False,
+      entry_points={'vaex.dataframe.accessor': ['widget = vaex.jupyter:DataFrameAccessorWidget']},
+      include_package_data=True,  # include files listed in manifest.in
+      package_data={'vaex.jupyter.vue': ['*.vue']},
+)
