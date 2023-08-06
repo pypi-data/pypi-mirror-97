23a0886db6787d423085e995b92d920125a77f39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:30 +0000

    Add package cator/0.0.2/cator-0.0.2-py3-none-any.whl

---
+++ packages/cator/cator-0.0.2-py3-none-any.whl/cator/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from .base import Database
+from .db_url import parse_url_to_dict
+from .mysql import MysqlDatabase
+from .sqlite import SqliteDatabase
+
+schemes = {
+    'mysql': MysqlDatabase,
+    'sqlite': SqliteDatabase
+}
+
+
+def connect(db_url) -> Database:
+    config = parse_url_to_dict(db_url=db_url)
+    scheme = config.pop('scheme')
+
+    return schemes[scheme](**config)
+
+
+Connect = connect
