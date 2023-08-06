109f373a152d97dfc53a4f8f92b67b9eb65baf1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:06 +0000

    Add package pynhd/0.10.0/pynhd-0.10.0.tar.gz

---
+++ packages/pynhd/pynhd-0.10.0.tar.gz/pynhd-0.10.0/HISTORY.rst
@@ -0,0 +1,97 @@
+=======
+History
+=======
+
+0.10.0 (2021-03-06)
+-------------------
+
+- The first release after renaming hydrodata to pygeohydro.
+- Make ``mypy`` checks more strict and fix all the errors and prevent possible
+  bugs.
+- Speed up CI testing by using ``mamba`` and caching.
+
+0.9.0 (2021-02-14)
+------------------
+
+- Bump version to the same version as pygeohydro.
+
+Breaking Changes
+~~~~~~~~~~~~~~~~
+- Add a new function for getting basins geometries for a list of USGS station IDs.
+  The function is a method of ``NLDI`` class called ``get_basins``. So, now
+  ``NLDI.getfeature_byid`` function does not have a basin flag. This change
+  makes getting geometries easier and faster.
+- Remove ``characteristics_dataframe`` method from ``NLDI`` and made a standalone function
+  called ``nhdplus_attrs`` for accessing NHDPlus attributes directly from ScienceBase.
+- Add support for using `hydro <https://hydro.nationalmap.gov/arcgis/rest/services/NHDPlus_HR/MapServer>`_
+  or `edits <https://edits.nationalmap.gov/arcgis/rest/services/NHDPlus_HR/NHDPlus_HR/MapServer>`_
+  webs services for getting NHDPlus High-Resolution using ``NHDPlusHR`` function. The new arguments
+  are ``service`` which accepts ``hydro`` or ``edits``, and ``autos_switch`` flag for automatically
+  switching to the other service if the ones passed by ``service`` fails.
+
+New Features
+~~~~~~~~~~~~
+- Add a new argument to ``topoogical_sort`` called ``edge_attr`` that allows to
+  add attribute(s) to the returned Networkx Graph. By default it is ``None``.
+- A new base class, ``AGRBase`` for connecting to ArcGISRESTful-based services such as National Map
+  and EPA's WaterGEOS.
+- Add support for setting the buffer distance for the input geometries to ``AGRBase.bygeom``.
+- Add ``comid_byloc`` to ``NLDI`` class for getting ComIDs of the closest flowlines from a list of
+  lon/lat coordinates.
+- Add ``bydistance`` to ``WaterData`` for getting features within a given radius of a point.
+
+0.2.0 (2020-12-06)
+------------------
+
+Breaking Changes
+~~~~~~~~~~~~~~~~
+- Re-wrote the ``NLDI`` function to use API v3 of the NLDI service.
+- The ``crs`` argument of ``WaterData`` now is the target CRS of the output dataframe.
+  The service CRS is now EPSG:4269 for all the layers.
+- Remove the ``url_only`` argument of ``NLDI`` since it's not applicable anymore.
+
+New Features
+~~~~~~~~~~~~
+- Added support for NHDPlus High Resolution for getting features by geometry, IDs, or
+  SQL where clause.
+- The following functions are added to ``NLDI``:
+
+* ``getcharacteristic_byid``: For getting characteristics of NHDPlus catchments.
+* ``navigate_byloc``: For getting the nearest ComID to a coordinate and perform a navigation.
+* ``characteristics_dataframe``: For getting all the available catchment-scale characteristics
+  as a dataframe.
+* ``get_validchars``: For getting a list of available characteristic IDs for a specified
+  characteristic type,.
+
+- The following function is added to ``WaterData``:
+
+* ``byfilter``: For getting data based on any valid CQL filter.
+* ``bygeom``: For getting data within a geometry (polygon and multipolygon).
+
+- Add support for Python 3.9 and tests for Windows.
+
+Bug Fixes
+~~~~~~~~~
+- Refactored ``WaterData`` to fix the CRS inconsistencies (#1).
+
+0.1.3 (2020-08-18)
+------------------
+
+- Replaced ``simplejson`` with ``orjson`` to speed-up JSON operations.
+
+0.1.2 (2020-08-11)
+------------------
+
+- Add ``show_versions`` function for showing versions of the installed deps.
+- Improve documentations
+
+0.1.1 (2020-08-03)
+------------------
+
+- Improved documentation
+- Refactored ``WaterData`` to improve readability.
+
+0.1.0 (2020-07-23)
+------------------
+
+- First release on PyPI.
---
+++ packages/pynhd/pynhd-0.10.0.tar.gz/pynhd-0.10.0/tests/test_pynhd.py
@@ -0,0 +1,172 @@
+"""Tests for PyNHD package."""
+import io
+from pathlib import Path
+
+import pytest
+from shapely.geometry import box
+
+import pynhd as nhd
+from pynhd import NLDI, AGRBase, WaterData
+
+STA_ID = "01031500"
+station_id = f"USGS-{STA_ID}"
+site = "nwissite"
+UM = "upstreamMain"
+UT = "upstreamTributaries"
+nldi = NLDI()
+
+
+class NHDPlusEPA(AGRBase):
+    def __init__(self, layer):
+        super().__init__(layer, "*", "epsg:4326")
+        self.service = self._init_service(
+            "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus/NHDPlus/MapServer"
+        )
+
+
+@pytest.mark.flaky(max_runs=3)
+def test_agr():
+    layer = "network flowline"
+    sql_clause = "FTYPE NOT IN (420,428,566)"
+    geom = [
+        (-97.06138, 32.837),
+        (-97.06133, 32.836),
+        (-97.06124, 32.834),
+        (-97.06127, 32.832),
+    ]
+    geo_crs = "epsg:4269"
+    distance = 1500
+    epa = NHDPlusEPA(layer=layer)
+    df = epa.bygeom(geom, geo_crs=geo_crs, sql_clause=sql_clause, distance=distance)
+    assert abs(df.LENGTHKM.sum() - 8.917) < 1e-3
+
+
+@pytest.mark.flaky(max_runs=3)
+def test_nldi_navigate():
+    stm = nldi.navigate_byid(site, station_id, UM, site)
+    st100 = nldi.navigate_byid(site, station_id, UM, site, distance=100)
+    pp = nldi.navigate_byid(site, station_id, UT, "huc12pp")
+    wqp = nldi.navigate_byloc((-70, 44), UT, "wqp")
+    assert (
+        st100.shape[0] == 3
+        and stm.shape[0] == 3
+        and pp.shape[0] == 12
+        and wqp.comid.iloc[0] == "6710923"
+    )
+
+
+@pytest.mark.flaky(max_runs=3)
+def test_nldi_feature():
+    station = nldi.getfeature_byid(site, station_id)
+    lon = round(station.geometry[0].centroid.x, 1)
+    lat = round(station.geometry[0].centroid.y, 1)
+    comid, missing = nldi.comid_byloc([(lon, lat), (lat, lon)])
+    assert (
+        station.comid.values[0] == "1722317"
+        and comid.comid.values[0] == "1722211"
+        and len(missing) == 1
+    )
+
+
+@pytest.mark.flaky(max_runs=3)
+def test_nldi_basin():
+    eck4 = "+proj=eck4 +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=km"
+    basin, missing = nldi.get_basins([STA_ID, "00000000"])
+    basin = basin.to_crs(eck4)
+    assert abs(basin.area.values[0] - 774.170) < 1e-3 and len(missing) == 1
+
+
+@pytest.mark.flaky(max_runs=3)
+def test_nldi_char():
+    tot, prc = nldi.getcharacteristic_byid(
+        "6710923", "local", char_ids="CAT_BFI", values_only=False
+    )
+    assert abs(tot.CAT_BFI.values[0] - 57) < 1e-3 and prc.CAT_BFI.values[0] == 0
+
+
+@pytest.mark.flaky(max_runs=3)
+def test_nhd_attrs():
+    meta = nhd.nhdplus_attrs(save_dir=".")
+    cat = nhd.nhdplus_attrs("RECHG", ".")
+    Path("nhdplus_attrs.feather").unlink()
+    assert abs(cat[cat.COMID > 0].CAT_RECHG.sum() - 143215331.64) < 1e-3 and len(meta) == 609
+
+
+@pytest.mark.flaky(max_runs=3)
+def test_waterdata_byid():
+    comids = nldi.navigate_byid(site, station_id, UT, "flowlines")
+    comid_list = comids.nhdplus_comid.tolist()
+
+    wd = WaterData("nhdflowline_network")
+    trib = wd.byid("comid", comid_list)
+
+    wd = WaterData("catchmentsp")
+    ct = wd.byid("featureid", comid_list)
+
+    assert (
+        trib.shape[0] == 432
+        and abs(trib.lengthkm.sum() - 565.755) < 1e-3
+        and abs(ct.areasqkm.sum() - 773.954) < 1e-3
+    )
+
+
+@pytest.mark.flaky(max_runs=3)
+def test_waterdata_bybox():
+    wd = WaterData("nhdwaterbody")
+    print(wd)
+    wb_g = wd.bygeom(box(-69.7718, 45.0742, -69.3141, 45.4534), predicate="INTERSECTS", xy=True)
+    wb_b = wd.bybox((-69.7718, 45.0742, -69.3141, 45.4534))
+    assert abs(wb_b.areasqkm.sum() - wb_g.areasqkm.sum()) < 1e-3
+
+
+@pytest.mark.flaky(max_runs=3)
+def test_waterdata_byfilter():
+    crs = "epsg:3857"
+    wd = WaterData("huc12", crs)
+    wb = wd.byfilter(f"{wd.layer} LIKE '17030001%'")
+    coords = (wb.iloc[0].geometry.centroid.x, wb.iloc[0].geometry.centroid.y)
+    hucs = wd.bydistance(coords, 100, crs)
+    assert wb.shape[0] == 52 and hucs.huc12.values[0] == "170300010602"
+
+
+@pytest.mark.flaky(max_runs=3)
+def test_nhdphr():
+    hr = nhd.NHDPlusHR("networknhdflowline", service="edits", auto_switch=True)
+    flwb = hr.bygeom((-69.77, 45.07, -69.31, 45.45))
+    flwi = hr.byids("NHDFlowline.PERMANENT_IDENTIFIER", ["103455178", "103454362", "103453218"])
+    flwf = hr.bysql("NHDFlowline.PERMANENT_IDENTIFIER IN ('103455178', '103454362', '103453218')")
+    assert (
+        flwb.shape[0] == 3892
+        and flwi["NHDFlowline.OBJECTID"].tolist() == flwf["NHDFlowline.OBJECTID"].tolist()
+    )
+
+
+@pytest.mark.flaky(max_runs=3)
+def test_acc():
+    wd = WaterData("nhdflowline_network")
+    comids = nldi.navigate_byid("nwissite", "USGS-11092450", UT, "flowlines")
+    comid_list = comids.nhdplus_comid.tolist()
+    trib = wd.byid("comid", comid_list)
+
+    nhd.prepare_nhdplus(trib, 0, 0, 0, False, False)
+    flw = nhd.prepare_nhdplus(trib, 1, 1, 1, True, True)
+
+    def routing(qin, q):
+        return qin + q
+
+    qsim = nhd.vector_accumulation(
+        flw[["comid", "tocomid", "lengthkm"]],
+        routing,
+        "lengthkm",
+        ["lengthkm"],
+    )
+    flw = flw.merge(qsim, on="comid")
+    diff = flw.arbolatesu - flw.acc_lengthkm
+
+    assert diff.abs().sum() < 1e-5
+
+
+def test_show_versions():
+    f = io.StringIO()
+    nhd.show_versions(file=f)
+    assert "INSTALLED VERSIONS" in f.getvalue()
