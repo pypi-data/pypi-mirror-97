94203f9eba359ff5c5ea03cfa69be37bf8909dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:49 +0000

    Add package salt-tower/1.6.0/salt_tower-1.6.0-py3-none-any.whl

---
+++ packages/salt-tower/salt_tower-1.6.0-py3-none-any.whl/salt_tower/pillar/tower.py
@@ -0,0 +1,408 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring
+"""
+
+"""
+from __future__ import absolute_import
+
+import copy
+import errno
+import logging
+import os
+import string
+
+from glob import glob
+
+import salt.loader
+import salt.minion
+import salt.template
+
+try:
+    from salt.utils.data import traverse_dict_and_list
+except ImportError:
+    from salt.utils import traverse_dict_and_list
+
+LOGGER = logging.getLogger(__name__)
+
+
+if hasattr(salt.loader, "matchers"):
+    # Available since salt 2019.2
+    def _match_minion_impl(tgt, opts):
+        matchers = salt.loader.matchers(
+            dict(__opts__, **opts)
+        )  # pylint: disable=no-member
+        return matchers["compound_match.match"](tgt)
+
+
+else:
+
+    def _match_minion_impl(tgt, opts):
+        return salt.minion.Matcher(opts, __salt__).compound_match(tgt)
+
+
+def ext_pillar(minion_id, pillar, *args, **_kwargs):
+    env = __opts__.get("environment", None)
+
+    if env is None:
+        env = "base"
+
+    tower = Tower(minion_id, env, pillar)
+
+    for top in list(args):
+        if not os.path.exists(top):
+            LOGGER.warning("Tower top file `%s' does not exists", top)
+            continue
+
+        tower.run(top)
+
+    return dict(tower)
+
+
+class Tower(dict):
+    def __init__(self, minion_id, env, pillar):
+        super(Tower, self).__init__(pillar)
+
+        self.env = env
+        self.minion_id = minion_id
+
+        opts = copy.copy(__opts__)
+        opts["pillar"] = self
+
+        self._renderers = salt.loader.render(opts, __salt__)
+        self._formatter = Formatter(self)
+        self._included = []
+
+        if "yamlet" in self._renderers:
+            self._default_renderers = "jinja|yamlet"
+        else:
+            LOGGER.warning(
+                "Yamlet renderer not available. Tower functionality will be limited."
+            )
+            self._default_renderers = "jinja|yaml"
+
+    def get(self, key, default=None, require=False, **kwargs):
+        if require:
+            default = KeyError(f"Pillar key missing: {key}")
+        value = traverse_dict_and_list(self, key, default, **kwargs)
+        if isinstance(value, KeyError):
+            raise value
+        return value
+
+    def update(self, obj, merge=True, **kwargs):
+        if merge:
+            return self.merge(self, obj, **kwargs)
+        return super(Tower, self).update(obj)
+
+    def merge(self, *args, **kwargs):
+        return _merge(*args, **kwargs)
+
+    def format(self, obj, *args, **kwargs):
+        if isinstance(obj, dict):
+            return {k: self.format(v, *args, **kwargs) for k, v in obj.items()}
+
+        if isinstance(obj, list):
+            return [self.format(i, *args, **kwargs) for i in obj]
+
+        if isinstance(obj, bytes):
+            return obj
+
+        if isinstance(obj, str):
+            try:
+                return self._formatter.format(obj, *args, **kwargs)
+            except ValueError:
+                return obj
+
+        return obj
+
+    def run(self, top):
+        LOGGER.debug("Process tower top file `%s'", top)
+
+        base = os.path.dirname(top)
+
+        for item in self._load_top(top, base):
+            if isinstance(item, str):
+                self._load_item(base, item)
+
+            elif isinstance(item, dict):
+                for tgt, items in item.items():
+                    if not self._match_minion(tgt):
+                        continue
+
+                    for itm in items:
+                        self._load_item(base, itm)
+
+    def _match_minion(self, tgt):
+        try:
+            return _match_minion_impl(
+                tgt, {"grains": __grains__, "pillar": self, "id": self.minion_id}
+            )
+        except Exception as err:
+            LOGGER.exception(err)
+            return False
+
+    def _load_top(self, top, base):
+        data = self._compile(top, context={"basedir": base})
+
+        if not isinstance(data, dict):
+            LOGGER.critical("Tower top must be a dict, but is %s.", type(data))
+            return []
+
+        if self.env not in data:
+            LOGGER.warning(
+                "Tower top `%s' does not include env %s, skipping.", top, self.env
+            )
+            return []
+
+        if not isinstance(data[self.env], list):
+            LOGGER.critical(
+                "Tower top `%s' env %s must be a list, but is %s.",
+                top,
+                self.env,
+                type(data[self.env]),
+            )
+            return []
+
+        return data[self.env]
+
+    def _load_item(self, base, item):
+        if isinstance(item, dict):
+            self.update(item, merge=True)
+
+        elif isinstance(item, str):
+            self.load(item, base)
+
+    def lookup(self, item, base=None, cwd=None):
+        if cwd and (item.startswith("./") or item.startswith("../")):
+            path = os.path.join(cwd, self.format(item))
+        elif base:
+            path = os.path.join(base, self.format(item))
+        else:
+            path = self.format(item)
+
+        match = glob(path)
+
+        if match:
+            match = [i for i in match if os.path.isfile(i)]
+
+        if match:
+            LOGGER.debug("Found glob match: %s", match)
+            return sorted(match)
+
+        for match in [path, f"{path}.sls", f"{path}/init.sls"]:
+            if os.path.isfile(match):
+                LOGGER.debug("Found file match: %s", match)
+                return [match]
+
+        return []
+
+    def load(self, item, base=None, cwd=None):
+        for file in self.lookup(item, base, cwd):
+            self._load_file(file, base)
+
+    def _load_file(self, file, base=None):
+        """
+        Try to load fully qualified file path as a tower data file.
+
+        It will first check if the file has already been loaded and, if not,
+        if it exist in the filesystem.
+
+        The file will be compiled and run as a salt template and is expected
+        to return a dict.
+
+        If the dict includes an `include` item or list, these includes will
+        loaded before the loaded data is merged into the current pillar.
+        """
+        if file in self._included:
+            LOGGER.warning("Skipping already included file: %s", file)
+            return
+
+        if not os.path.isfile(file):
+            LOGGER.warning("Skipping non-existing file: %s", file)
+            return
+
+        self._included.append(file)
+
+        data = self._compile(file, context={"basedir": base})
+
+        if not isinstance(data, dict):
+            LOGGER.warning("Loading %s did not return dict, but %s", file, type(data))
+            return
+
+        if "include" in data:
+            includes = data.pop("include")
+
+            if not isinstance(includes, list):
+                includes = [includes]
+
+            for include in includes:
+                self.load(include, base, cwd=os.path.dirname(file))
+
+        self.update(data, merge=True)
+
+    def _compile(
+        self,
+        template,
+        default=None,
+        blacklist=None,
+        whitelist=None,
+        context=None,
+        **kwargs,
+    ):
+        if default is None:
+            default = self._default_renderers
+
+        ctx = {}
+
+        if context:
+            ctx.update(context)
+
+        ctx["tmplpath"] = template
+        ctx["tmpldir"] = os.path.dirname(template)
+        ctx["minion_id"] = self.minion_id
+        ctx["pillar"] = self
+        ctx["tower"] = self
+
+        def render(path, context=None, renderer="text"):
+            if isinstance(context, dict):
+                context = {**ctx, **context}
+            else:
+                context = ctx
+
+            if "basedir" in context:
+                path = os.path.join(context["basedir"], path)
+
+            file = os.path.abspath(path)
+
+            if not os.path.isfile(file):
+                raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), file)
+
+            return self._compile(file, renderer, None, None, context)
+
+        ctx["render"] = render
+
+        kwargs["tower"] = ctx["tower"]
+        kwargs["minion_id"] = ctx["minion_id"]
+
+        try:
+            return salt.template.compile_template(
+                template=template,
+                renderers=self._renderers,
+                default=default,
+                blacklist=blacklist,
+                whitelist=whitelist,
+                context=ctx,
+                **kwargs,
+            )
+        except Exception as err:
+            LOGGER.critical("Unable to render template `%s'", template)
+            LOGGER.exception(err)
+            raise err
+
+
+class Formatter(string.Formatter):
+    def __init__(self, tower):
+        self._tower = tower
+
+    def get_field(self, key, args, kwargs):
+        if key in kwargs:
+            return (kwargs[key], None)
+
+        value = self._tower.get(key, delimiter=".")
+
+        if value is None:
+            return ("{" + key + "}", None)
+
+        return (value, None)
+
+
+def _merge(tgt, *objects, strategy="merge-last"):
+    for obj in objects:
+        if isinstance(tgt, dict):
+            tgt = _merge_dict(tgt, obj, strategy)
+        elif isinstance(tgt, list):
+            tgt = _merge_list(tgt, obj, strategy)
+        else:
+            raise TypeError(f"Cannot merge {type(tgt)}")
+
+    return tgt
+
+
+def _merge_clean(obj):
+    if isinstance(obj, dict):
+        return {k: _merge_clean(v) for k, v in obj.items() if k != "__"}
+
+    if isinstance(obj, list):
+        if obj and isinstance(obj[0], dict) and len(obj[0]) == 1 and "__" in obj[0]:
+            obj = obj[1:]
+        return [_merge_clean(v) for v in obj]
+
+    return obj
+
+
+def _merge_dict(tgt, obj, strategy="merge-last"):
+    if not isinstance(obj, dict):
+        raise TypeError(f"Cannot merge non-dict type, but is {type(obj)}")
+
+    if "__" in obj:
+        strategy = obj.pop("__")
+
+    if strategy == "remove":
+        for k in obj:
+            if k in tgt:
+                tgt.pop(k)
+
+    elif strategy == "merge-last":
+        for key, val in obj.items():
+            if key in tgt and isinstance(tgt[key], dict) and isinstance(val, dict):
+                _merge_dict(tgt[key], val, strategy)
+            elif key in tgt and isinstance(tgt[key], list) and isinstance(val, list):
+                _merge_list(tgt[key], val, strategy)
+            else:
+                tgt[key] = _merge_clean(val)
+
+    elif strategy == "merge-first":
+        for key, val in obj.items():
+            if key in tgt and isinstance(tgt[key], dict) and isinstance(val, dict):
+                _merge_dict(tgt[key], val, strategy)
+            elif key in tgt and isinstance(tgt[key], list) and isinstance(val, list):
+                _merge_list(tgt[key], val, strategy)
+            elif key not in tgt:
+                tgt[key] = _merge_clean(val)
+
+    elif strategy == "overwrite":
+        tgt.clear()
+        tgt.update(_merge_clean(obj))
+
+    else:
+        raise ValueError(f"Unknown strategy: {strategy}")
+
+    return tgt
+
+
+def _merge_list(tgt, lst, strategy="merge-last"):
+    if not isinstance(lst, list):
+        raise TypeError(f"Cannot merge non-list type, but is {type(lst)}")
+
+    if lst and isinstance(lst[0], dict) and len(lst[0]) == 1 and "__" in lst[0]:
+        strategy = lst.pop(0)["__"]
+
+    if strategy == "remove":
+        for val in lst:
+            if val in tgt:
+                tgt.remove(val)
+
+    elif strategy == "merge-last":
+        tgt.extend(_merge_clean(lst))
+
+    elif strategy == "merge-first":
+        for val in _merge_clean(lst):
+            tgt.insert(0, val)
+
+    elif strategy == "overwrite":
+        del tgt[:]
+        tgt.extend(_merge_clean(lst))
+
+    else:
+        raise ValueError(f"Unknown strategy: {strategy}")
+
+    return tgt
---
+++ packages/salt-tower/salt_tower-1.6.0-py3-none-any.whl/salt_tower/renderers/yamlet.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring
+"""
+Extended YAML renderer for salt.
+
+For YAML usage information see :ref:`Understanding YAML <yaml>`.
+
+Special extensions are added to
+
+* include other render files at arbitrary positions
+* include plain or binary files at arbitrary positions
+
+"""
+from __future__ import absolute_import
+
+import copy
+import io
+import os
+
+from yaml.constructor import ConstructorError
+from yaml.nodes import ScalarNode, MappingNode
+
+import salt.loader
+import salt.template
+
+from salt.utils.yamlloader import SaltYamlSafeLoader, load
+from salt.utils.odict import OrderedDict
+
+try:
+    from salt.utils.files import fopen
+except ImportError:
+    from salt.utils import fopen
+
+
+class YamletLoader(SaltYamlSafeLoader):  # pylint: disable=too-many-ancestors
+    def __init__(self, stream, renderers, context=None, tmplpath=None, **_kwargs):
+        SaltYamlSafeLoader.__init__(self, stream, dictclass=OrderedDict)
+
+        if context is None:
+            self.context = {}
+        else:
+            self.context = context
+
+        self.tmplpath = tmplpath
+        self.renderers = renderers
+        self.add_constructor("!read", type(self)._yamlet_read)
+        self.add_constructor("!include", type(self)._yamlet_include)
+
+    def _yamlet_read(self, node):
+        if isinstance(node, ScalarNode):
+            return self._read(node.value)
+
+        return self._invalid_node(node, "a scalar node")
+
+    def _yamlet_include(self, node):
+        if isinstance(node, ScalarNode):
+            return self._compile(node.value)
+
+        if isinstance(node, MappingNode):
+            return self._compile(**self.construct_mapping(node, True))
+
+        return self._invalid_node(node, "a scalar or mapping node")
+
+    def _read(self, source):
+        source = self._resolve(source)
+
+        with fopen(source, "rb") as file:
+            return file.read()
+
+    def _compile(self, source, default="jinja|yamlet", context=None):
+        source = self._resolve(source)
+
+        ctx = copy.copy(self.context)
+
+        if context:
+            ctx.update(context)
+
+        ctx["tmplpath"] = source
+        ctx["tmpldir"] = os.path.dirname(source)
+
+        ret = salt.template.compile_template(
+            template=source,
+            renderers=self.renderers,
+            default=default,
+            blacklist=None,
+            whitelist=None,
+            context=ctx,
+        )
+
+        if isinstance(ret, io.IOBase):
+            ret = ret.read()
+
+        return ret
+
+    def _resolve(self, path):
+        if self.tmplpath:
+            base = os.path.dirname(self.tmplpath)
+
+            return os.path.realpath(os.path.join(base, path))
+
+        return path
+
+    def _invalid_node(self, node, expected):
+        raise ConstructorError(
+            None, None, f"expected {expected}, but found {node.id}", node.start_mark
+        )
+
+
+def get_yaml_loader(**kwargs):
+    def yaml_loader(stream):
+        return YamletLoader(stream, **kwargs)
+
+    return yaml_loader
+
+
+def render(source, _saltenv, _sls, **kwargs):
+    """
+    Processes YAML data in a string or file objects.
+
+    :rtype: A Python data structure
+    """
+    if not isinstance(source, str):
+        source = source.read()
+
+    return load(source, Loader=get_yaml_loader(**kwargs))
