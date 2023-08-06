7ebe015ef7359d01661f9669540dce9f3ce414ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:09 +0000

    Add package LiteSQL/2.0.1/LiteSQL-2.0.1.tar.gz

---
+++ packages/LiteSQL/LiteSQL-2.0.1.tar.gz/LiteSQL-2.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+long_description1 = '''
+# LiteSQL 
+База данных? Без проблем!
+
+# ЧИТАТЬ НА GITHUB
+https://github.com/Ma-Mush/LiteSQL
+
+'''
+
+setup(
+name='LiteSQL', 
+version='2.0.1',
+description='Библиотека для легкого использования БД!', 
+packages=['LiteSQL'], 
+author_email='ma_mush@mail.ru', 
+zip_safe=False,
+python_requires='>=3.6',
+long_description=long_description1,
+long_description_content_type="text/markdown"
+)
