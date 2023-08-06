e951494af15aa1e44457173627dc55a52565f6d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:25 +0000

    Add package starid/1.4.7/starid-1.4.7.tar.gz

---
+++ packages/starid/starid-1.4.7.tar.gz/starid-1.4.7/starid/__main__.py
@@ -0,0 +1,13 @@
+import argparse
+
+def demo():
+    from starid import Starid
+    starid = Starid()
+    starid.sky()
+    starid.plot(starndx=3)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('starid')
+    parser.add_argument('-d', '--demo', dest='demo', action='store_true', default=False)
+    args = parser.parse_args()
+    if args.demo: demo()
