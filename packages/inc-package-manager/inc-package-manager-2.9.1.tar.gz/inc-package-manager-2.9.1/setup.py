b1391e08128751afbb41f8152b3377203dad78b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:46 +0000

    Add package inc-package-manager/2.9.1/inc-package-manager-2.9.1.tar.gz

---
+++ packages/inc-package-manager/inc-package-manager-2.9.1.tar.gz/inc-package-manager-2.9.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Note!
+# ' are required, do not use any '.
+
+# setup.
+from setuptools import setup, find_packages
+setup(
+	name='inc-package-manager',
+	version='2.9.1',
+	description='Some description.',
+	url='http://github.com/vandenberghinc/vandenberghinc-package-manager',
+	author='Daan van den Bergh',
+	author_email='vandenberghinc.contact@gmail.com',
+	license='MIT',
+	packages=find_packages(),
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=[
+            'cl1>=1.12.0',
+            'netw0rk>=1.7.7',
+            'r3sponse>=2.8.8',
+            'syst3m>=2.14.5',
+            'fil3s>=2.13.1',
+        ],)
