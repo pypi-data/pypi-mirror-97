76cc5fbc75f68d964bc060af914db4bfcbaa08e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:57 +0000

    Add package qcs-api-client/0.6.2.dev1074480209/qcs-api-client-0.6.2.dev1074480209.tar.gz

---
+++ packages/qcs-api-client/qcs-api-client-0.6.2.dev1074480209.tar.gz/qcs-api-client-0.6.2.dev1074480209/qcs_api_client/operations/sync_from_dict/__init__.py
@@ -0,0 +1,32 @@
+from qcs_api_client.api.authentication.auth_email_password_reset_token import (
+    sync_from_dict as auth_email_password_reset_token,
+)
+from qcs_api_client.api.authentication.auth_get_user import sync_from_dict as auth_get_user
+from qcs_api_client.api.authentication.auth_reset_password import sync_from_dict as auth_reset_password
+from qcs_api_client.api.authentication.auth_reset_password_with_token import (
+    sync_from_dict as auth_reset_password_with_token,
+)
+from qcs_api_client.api.client_applications.check_client_application import sync_from_dict as check_client_application
+from qcs_api_client.api.client_applications.get_client_application import sync_from_dict as get_client_application
+from qcs_api_client.api.client_applications.list_client_applications import sync_from_dict as list_client_applications
+from qcs_api_client.api.default.get_health import sync_from_dict as get_health
+from qcs_api_client.api.default.health_check import sync_from_dict as health_check
+from qcs_api_client.api.endpoints.create_endpoint import sync_from_dict as create_endpoint
+from qcs_api_client.api.endpoints.delete_endpoint import sync_from_dict as delete_endpoint
+from qcs_api_client.api.endpoints.get_default_endpoint import sync_from_dict as get_default_endpoint
+from qcs_api_client.api.endpoints.get_endpoint import sync_from_dict as get_endpoint
+from qcs_api_client.api.endpoints.list_endpoints import sync_from_dict as list_endpoints
+from qcs_api_client.api.engagements.create_engagement import sync_from_dict as create_engagement
+from qcs_api_client.api.quantum_processors.get_instruction_set_architecture import (
+    sync_from_dict as get_instruction_set_architecture,
+)
+from qcs_api_client.api.quantum_processors.get_quantum_processor import sync_from_dict as get_quantum_processor
+from qcs_api_client.api.quantum_processors.list_quantum_processors import sync_from_dict as list_quantum_processors
+from qcs_api_client.api.reservations.create_reservation import sync_from_dict as create_reservation
+from qcs_api_client.api.reservations.delete_reservation import sync_from_dict as delete_reservation
+from qcs_api_client.api.reservations.find_available_reservations import sync_from_dict as find_available_reservations
+from qcs_api_client.api.reservations.list_reservations import sync_from_dict as list_reservations
+from qcs_api_client.api.translation.get_quilt_calibrations import sync_from_dict as get_quilt_calibrations
+from qcs_api_client.api.translation.translate_native_quil_to_encrypted_binary import (
+    sync_from_dict as translate_native_quil_to_encrypted_binary,
+)
