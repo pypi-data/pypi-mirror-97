6411eff1895d699e7db0016277693f1eb7fffe0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:50 +0000

    Add package mapapi/0.2.2/mapapi-0.2.2.tar.gz

---
+++ packages/mapapi/mapapi-0.2.2.tar.gz/mapapi-0.2.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import codecs
+
+def readme():
+    with codecs.open('README.rst', 'r', encoding='utf-8') as f:
+        return f.read()
+
+setup(name='mapapi',
+      version='0.2.2',
+      description='map web api, current support baidu',
+      long_description=readme(),
+      keywords='map baidu',
+      url='https://github.com/starplanet/mapapi',
+      author='zhangjinjie',
+      author_email='zhangjinjie@yimian.com.cn',
+      license='MIT',
+      packages=['mapapi', 'mapapi/baidu'],
+      install_requires=[
+          'requests',
+      ],
+      include_package_data=True,
+      zip_safe=False)
