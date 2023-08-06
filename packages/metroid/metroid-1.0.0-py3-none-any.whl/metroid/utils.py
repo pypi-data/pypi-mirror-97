bb4205d7761d65429dbb93b24b0a9984d3bb5d02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:09 +0000

    Add package metroid/1.0.0/metroid-1.0.0-py3-none-any.whl

---
+++ packages/metroid/metroid-1.0.0-py3-none-any.whl/metroid/utils.py
@@ -0,0 +1,25 @@
+import re
+import logging
+from django.core.exceptions import ImproperlyConfigured
+
+logger = logging.getLogger('metroid')
+
+
+def match_handler_subject(
+    subject: str,
+    message_subject: str,
+    is_regex: bool,
+) -> bool:
+    """
+    Checks if the provided message subject matches the handler's subject. Performs a match by using the regular
+    expression, or compares strings based on  handler settings defined in settings.py.
+
+    """
+    if is_regex:
+        try:
+            pattern = re.compile(subject)
+            return bool(re.match(pattern=pattern, string=message_subject))
+        except re.error:
+            raise ImproperlyConfigured(f'Provided regex pattern: {subject} is invalid.')
+    else:
+        return subject == message_subject
