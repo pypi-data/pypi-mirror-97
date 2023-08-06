6f91a30a3b0b8572a1ae8db84f6afaed1adc3d02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:33 +0000

    Add package needlepy/1.4/needlepy-1.4.tar.gz

---
+++ packages/needlepy/needlepy-1.4.tar.gz/needlepy-1.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='needlepy',
+    version='1.4',
+    packages=find_packages(),
+    url='https://github.com/BitTheByte/Needle',
+    author='BitTheByte',
+)
