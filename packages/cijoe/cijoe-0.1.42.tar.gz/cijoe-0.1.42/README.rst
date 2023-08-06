d564e5d895e5c3abe1669fd512727f03a0160b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:50 +0000

    Add package cijoe/0.1.42/cijoe-0.1.42.tar.gz

---
+++ packages/cijoe/cijoe-0.1.42.tar.gz/cijoe-0.1.42/README.rst
@@ -0,0 +1,33 @@
+cijoe: tools for systems development and testing
+================================================
+
+.. image:: https://travis-ci.com/refenv/cijoe.svg?branch=master
+   :target: https://travis-ci.com/refenv/cijoe
+   :alt: Build Status
+
+.. image:: https://readthedocs.org/projects/cijoe/badge/?version=latest
+   :target: https://cijoe.readthedocs.io/en/latest/?badge=latest
+   :alt: Documentation Status
+
+.. image:: https://img.shields.io/pypi/v/cijoe.svg
+   :target: https://pypi.org/project/cijoe
+   :alt: PyPI
+
+Tools for systems development and testing.
+
+Please take a look at the documentation for how to install and use ``cijoe``:
+
+* `Quickstart Guide`_
+* `Installation`_
+* `Usage`_
+
+If you find bugs or need help then feel free to submit an `Issue`_. If you want
+to get involved head over to the `GitHub page`_ to get the source code and
+submit a `Pull request`_ with your changes.
+
+.. _Quickstart Guide: https://cijoe.readthedocs.io/en/latest/quickstart.html
+.. _Installation: https://cijoe.readthedocs.io/
+.. _Usage: https://cijoe.readthedocs.io/
+.. _GitHub page: https://github.com/refenv/cijoe
+.. _Pull request: https://github.com/refenv/cijoe/pulls
+.. _Issue: https://github.com/refenv/cijoe/issues
---
+++ packages/cijoe/cijoe-0.1.42.tar.gz/cijoe-0.1.42/setup.py
@@ -0,0 +1,63 @@
+"""
+    Definition of CIJOE distribution package
+"""
+import codecs
+import glob
+import os
+from setuptools import setup
+
+
+def read(*parts):
+    """Read parts to use a e.g. long_description"""
+
+    here = os.path.abspath(os.path.dirname(__file__))
+
+    # intentionally *not* adding an encoding option to open, See:
+    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    with codecs.open(os.path.join(here, *parts), 'r') as pfp:
+        return pfp.read()
+
+
+setup(
+    name="cijoe",
+    version="0.1.42",
+    description="Tools for systems development and testing",
+    long_description=read('README.rst'),
+    author="Simon A. F. Lund",
+    author_email="os@safl.dk",
+    url="https://github.com/refenv/cijoe",
+    license="Apache License 2.0",
+    install_requires=[
+        "pyyaml (>=3.10)", "jinja2 (>=2.0)", "kmdo"
+    ],
+    zip_safe=False,
+    packages=["cij", "cij.unittests", "cij.extractors"],
+    package_dir={"": "modules"},
+    data_files=[
+        ("bin", glob.glob("bin/*")),
+
+        ("share/cijoe/hooks", glob.glob("hooks/*")),
+        ("share/cijoe/modules", glob.glob("modules/*.sh")),
+        ("share/cijoe/templates", glob.glob("templates/*html")),
+
+        ("share/cijoe/envs", glob.glob("envs/*")),
+
+        ("share/cijoe/testfiles", glob.glob("testfiles/*")),
+        ("share/cijoe/testcases", glob.glob("testcases/*")),
+        ("share/cijoe/testsuites", glob.glob("testsuites/*")),
+        ("share/cijoe/testplans", glob.glob("testplans/*"))
+    ],
+    options={'bdist_wheel': {'universal': True}},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Topic :: Text Processing",
+        "Topic :: Utilities",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Testing"
+    ],
+)
