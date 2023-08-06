af312c4ea4e535bbe7123631a14629d155f4e20e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:32 +0000

    Add package django-candy/0.7.2/django-candy-0.7.2.tar.gz

---
+++ packages/django-candy/django-candy-0.7.2.tar.gz/django-candy-0.7.2/django_candy/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include, re_path
+from rest_framework import routers
+
+from django_candy import views
+
+app_name = 'django_candy'
+
+
+router = routers.SimpleRouter()
+router.register(r'api/v1/(?P<app_label>[\w]+)/(?P<model_name>[\w]+)', views.CommonViewSet, basename='candy')
+
+urlpatterns = router.urls
+
+
+urlpatterns += [
+    path('api/v1/init/', views.InitView.as_view(), name='init'),
+    path('api/v1/login/', views.login_view, name='login'),
+    path('api/v1/logout/', views.logout_view, name='logout'),
+    path('api/v1/<app_label>/<model_name>/', views.api_view, name='model-endpoint'),
+    re_path('.*', views.index, name='index'),
+]
