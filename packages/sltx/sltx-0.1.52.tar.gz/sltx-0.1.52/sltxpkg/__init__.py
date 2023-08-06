18292a2082b5f7bab22e5813bc0d55cb4d2061ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:46 +0000

    Add package sltx/0.1.52/sltx-0.1.52.tar.gz

---
+++ packages/sltx/sltx-0.1.52.tar.gz/sltx-0.1.52/sltxpkg/__init__.py
@@ -0,0 +1,8 @@
+import sltxpkg.globals
+import sltxpkg.config
+import sltxpkg.dep
+import sltxpkg.generate
+import sltxpkg.util
+import sltxpkg.commands
+import sltxpkg.prompt
+import sltxpkg.command_config
