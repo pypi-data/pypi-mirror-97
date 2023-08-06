a281d8b66ec0597104d331a773cc72369a83241d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:40 +0000

    Add package django-wildewidgets/0.3.3/django_wildewidgets-0.3.3-py2.py3-none-any.whl

---
+++ packages/django-wildewidgets/django_wildewidgets-0.3.3-py2.py3-none-any.whl/wildewidgets/__init__.py
@@ -0,0 +1,24 @@
+__version__ = "0.3.3"
+
+from .wildewidgets import (
+    AltairChart,
+    BarChart, 
+    BasicMenu,
+    DoughnutChart,
+    Histogram,
+    HorizontalBarChart, 
+    HorizontalHistogram,
+    HorizontalStackedBarChart,
+    LightMenu,
+    PieChart,
+    StackedBarChart, 
+    WildewidgetDispatch,
+)
+
+try:
+    from .wildewidgets import (
+        DataTable,
+        DataTableFilter,
+    )
+except ImportError:
+    pass
