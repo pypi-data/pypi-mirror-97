a40b1fb2e760ab48dfc59d87cca6fd8434cccb29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:30 +0000

    Add package certbot-apache/1.13.0/certbot-apache-1.13.0.tar.gz

---
+++ packages/certbot-apache/certbot-apache-1.13.0.tar.gz/certbot-apache-1.13.0/setup.py
@@ -0,0 +1,61 @@
+from setuptools import find_packages
+from setuptools import setup
+
+version = '1.13.0'
+
+# Remember to update local-oldest-requirements.txt when changing the minimum
+# acme/certbot version.
+install_requires = [
+    'acme>=0.29.0',
+    'certbot>=1.6.0',
+    'python-augeas',
+    'setuptools>=39.0.1',
+    'zope.component',
+    'zope.interface',
+]
+
+dev_extras = [
+    'apacheconfig>=0.3.2',
+]
+
+setup(
+    name='certbot-apache',
+    version=version,
+    description="Apache plugin for Certbot",
+    url='https://github.com/letsencrypt/letsencrypt',
+    author="Certbot Project",
+    author_email='client-dev@letsencrypt.org',
+    license='Apache License 2.0',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Plugins',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Security',
+        'Topic :: System :: Installation/Setup',
+        'Topic :: System :: Networking',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+    ],
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    extras_require={
+        'dev': dev_extras,
+    },
+    entry_points={
+        'certbot.plugins': [
+            'apache = certbot_apache._internal.entrypoint:ENTRYPOINT',
+        ],
+    },
+)
