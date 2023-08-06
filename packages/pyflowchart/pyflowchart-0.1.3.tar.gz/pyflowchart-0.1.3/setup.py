8d82621e64d24430a645285d82dbb296c5e03ed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:53 +0000

    Add package pyflowchart/0.1.3/pyflowchart-0.1.3.tar.gz

---
+++ packages/pyflowchart/pyflowchart-0.1.3.tar.gz/pyflowchart-0.1.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "rb") as fh:
+    long_description = fh.read().decode("utf-8")
+
+setuptools.setup(
+    name='pyflowchart',
+    version='0.1.3',
+    url='https://github.com/cdfmlr/pyflowchart',
+    license='MIT',
+    author='CDFMLR',
+    author_email='cdfmlr@outlook.com',
+    description='Python codes to Flowcharts.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Utilities",
+    ],
+    python_requires='>=3.6',
+    install_requires=['astunparse', 'chardet'],
+)
