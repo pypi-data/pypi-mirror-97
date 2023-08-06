f9451c0dda928d533b083d568c87ceb442457448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:22 +0000

    Add package yahoo-finance-api/v0.2/yahoo-finance-api-v0.2.tar.gz

---
+++ packages/yahoo-finance-api/yahoo-finance-api-v0.2.tar.gz/yahoo-finance-api-v0.2/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name = 'yahoo-finance-api',
+    packages = ['yahoo_finance'],
+    version = 'v0.2', 
+    description = 'Package to source data from Yahoo Finance',
+    author = 'Rakesh Bhat',
+    author_email = 'rakeshbhat9@gmail.com',
+    url = 'https://github.com/rakeshbhat9/yahoo-finance-api',
+    download_url = 'https://github.com/rakeshbhat9/yahoo-finance-api/archive/v0.2.tar.gz',
+    keywords = ['v0.1'],
+    classifiers = [],
+        install_requires=[
+        'pandas==1.1.3',
+        'requests==2.24.0',
+        'bs4==4.9.3'
+        'matplotlib==3.3.2']
+)
