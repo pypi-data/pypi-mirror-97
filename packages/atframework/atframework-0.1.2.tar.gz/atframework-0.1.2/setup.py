0858da936a45acb8d54961eaef5347747ddb78d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:34 +0000

    Add package atframework/0.1.2/atframework-0.1.2.tar.gz

---
+++ packages/atframework/atframework-0.1.2.tar.gz/atframework-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+name = 'atframework',
+version = '0.1.2',
+author = 'Siro',
+author_email = 'siro@foxmail.com',
+description = 'this is one automation framework base on selenium',
+long_description = 'file: README.md',
+long_description_content_type = 'text/markdown',
+url = 'https://github.com/pypa/sampleproject',
+py_modules = ['drivers','tools','web']
+)
