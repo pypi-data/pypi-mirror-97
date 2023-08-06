1932c2aafd5da35285bf31f45b70d2613eac0795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:04 +0000

    Add package grispy/0.1.0/grispy-0.1.0.tar.gz

---
+++ packages/grispy/grispy-0.1.0.tar.gz/grispy-0.1.0/grispy/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of the
+#   GriSPy Project (https://github.com/mchalela/GriSPy).
+# Copyright (c) 2019, Martin Chalela
+# License: MIT
+#   Full Text: https://github.com/mchalela/GriSPy/blob/master/LICENSE
+
+"""Grid Search in Python.
+
+GriSPy is a regular grid search algorithm for quick nearest-neighbor lookup.
+"""
+
+__all__ = ["GriSPy"]
+
+
+__version__ = "0.1.0"
+
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+from .core import GriSPy
