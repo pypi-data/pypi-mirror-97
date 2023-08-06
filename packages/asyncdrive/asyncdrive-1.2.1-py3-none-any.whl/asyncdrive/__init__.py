a64ff26f8577cbf0a6bb6b384ca10dbf93498ecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:11 +0000

    Add package asyncdrive/1.2.1/asyncdrive-1.2.1-py3-none-any.whl

---
+++ packages/asyncdrive/asyncdrive-1.2.1-py3-none-any.whl/asyncdrive/__init__.py
@@ -0,0 +1,11 @@
+"""
+A simple wrapper around the Google Drive REST API v3,
+supports service accounts only, use at your own discretion.
+
+:copyright: (c) 2020 Fliqqr
+:license: MIT
+"""
+
+from .asyncdrive import AsyncDrive
+
+__version__ = "1.2.0"
