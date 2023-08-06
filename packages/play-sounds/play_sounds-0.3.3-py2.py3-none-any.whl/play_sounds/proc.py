d6aeee8b0c892d968683e30db3b4b658a99f64ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:34 +0000

    Add package play-sounds/0.3.3/play_sounds-0.3.3-py2.py3-none-any.whl

---
+++ packages/play-sounds/play_sounds-0.3.3-py2.py3-none-any.whl/play_sounds/proc.py
@@ -0,0 +1,60 @@
+from typing import Callable, Optional, Set
+from multiprocessing import Process
+from platform import platform
+from pathlib import Path
+from sys import exit
+import signal
+
+
+PLATFORM: str = platform().lower()
+_PROCS: Set[Process] = set()
+
+
+def play_process(
+  file: Path,
+  target: Callable,
+  running_procs: Optional[Set[Process]] = _PROCS
+) -> Process:
+  proc = Process(target=target, args=(file,))
+
+  if running_procs is not None:
+    running_procs.add(proc)
+
+  proc.start()
+
+  return proc
+
+
+def kill_process(
+  proc: Process,
+  running_procs: Optional[Set[Process]] = _PROCS
+):
+  proc.kill()
+  proc.join()
+
+  if running_procs is not None and proc in running_procs:
+    running_procs.remove(proc)
+
+
+def kill_child_procs(signum: int, frame):
+  if _PROCS:
+    for proc in _PROCS.copy():
+      try:
+        kill_process(proc)
+
+      except:
+        pass
+
+  exit()
+
+
+signal.signal(signal.SIGTERM, kill_child_procs)
+signal.signal(signal.SIGINT, kill_child_procs)
+signal.signal(signal.SIGSEGV, kill_child_procs)
+signal.signal(signal.SIGABRT, kill_child_procs)
+signal.signal(signal.SIGILL, kill_child_procs)
+
+if 'windows' not in PLATFORM:
+  signal.signal(signal.SIGPIPE, kill_child_procs)
+  signal.signal(signal.SIGQUIT, kill_child_procs)
+  signal.signal(signal.SIGBUS, kill_child_procs)
