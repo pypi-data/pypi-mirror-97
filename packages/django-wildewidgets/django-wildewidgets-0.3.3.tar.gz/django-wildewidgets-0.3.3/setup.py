1450d6d229813b6c73d4bdf3e15561740991f42a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:40 +0000

    Add package django-wildewidgets/0.3.3/django-wildewidgets-0.3.3.tar.gz

---
+++ packages/django-wildewidgets/django-wildewidgets-0.3.3.tar.gz/django-wildewidgets-0.3.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-wildewidgets',
+    version="0.3.3",
+    description='django-wildewidgets is a Django library designed to help you make charts, graphs, tables, and UI widgets quickly and easily with libraries like Chartjs, Altair, and Datatables.',
+    long_description=open('README.md', 'rt').read(),
+    long_description_content_type="text/markdown",
+    author="IMSS ADS",
+    author_email="imss-ads-staff@caltech.edu",
+    url='https://github.com/caltechads/django-wildewidget',
+    packages=find_packages(exclude=['bin']),
+    include_package_data=True,
+)
