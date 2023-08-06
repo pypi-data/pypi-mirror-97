eb5760d3c35c9a644b66ab0ade59b33d76ee9048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:20 +0000

    Add package cfut/1.3.1/cfut-1.3.1.tar.gz

---
+++ packages/cfut/cfut-1.3.1.tar.gz/cfut-1.3.1/cfut/models.py
@@ -0,0 +1,29 @@
+from enum import Enum
+from typing import Dict, Optional, List, Any
+
+from pydantic import BaseModel
+
+
+class Capability(str, Enum):
+    CAPABILITY_NAMED_IAM = "CAPABILITY_NAMED_IAM"
+    CAPABILITY_IAM = "CAPABILITY_IAM"
+    CAPABILITY_AUTO_EXPAND = "CAPABILITY_AUTO_EXPAND"
+
+
+class CfnTemplate(BaseModel):
+    name: str
+    path: str
+    capabilities: Optional[List[Capability]]
+    parameters: Optional[Dict[str, Any]]
+
+
+class EcrConfig(BaseModel):
+    repo: str
+    tag = "dev"
+    src = "."   # path to the directory with Dockerfile, e.g. "."
+
+
+class IniFile(BaseModel):
+    ecr: Optional[EcrConfig]
+    profile: Optional[str]
+    templates: Dict[str, CfnTemplate]
