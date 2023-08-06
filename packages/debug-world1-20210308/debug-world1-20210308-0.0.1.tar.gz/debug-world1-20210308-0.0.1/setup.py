85dc85afa0961916cff80937a706a99fdac7efad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:05 +0000

    Add package debug-world1-20210308/0.0.1/debug-world1-20210308-0.0.1.tar.gz

---
+++ packages/debug-world1-20210308/debug-world1-20210308-0.0.1.tar.gz/debug-world1-20210308-0.0.1/setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup, find_packages
+setup(
+    name="debug-world1-20210308",                                     
+    version="0.0.1",                                        
+    author="JINKUN",                                       
+    author_email="author@example.com",                      
+    description="PyPI Tutorial",                            
+    packages=find_packages(),                    
+    classifiers=[                                           
+        "Programming Language :: Python :: 3",           
+        "Operating System :: OS Independent",               
+    ],
+)
