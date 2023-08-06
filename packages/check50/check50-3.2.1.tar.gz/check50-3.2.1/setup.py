d16dce53aea3206241e3bba37198829ce3ca38a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:11 +0000

    Add package check50/3.2.1/check50-3.2.1.tar.gz

---
+++ packages/check50/check50-3.2.1.tar.gz/check50-3.2.1/setup.py
@@ -0,0 +1,34 @@
+if __import__("os").name == "nt":
+    raise RuntimeError("check50 does not support Windows directly. Instead, you should install the Windows Subsystem for Linux (https://docs.microsoft.com/en-us/windows/wsl/install-win10) and then install check50 within that.")
+
+from setuptools import setup
+
+setup(
+    author="CS50",
+    author_email="sysadmins@cs50.harvard.edu",
+    classifiers=[
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Topic :: Education",
+        "Topic :: Utilities"
+    ],
+    description="This is check50, with which you can check solutions to problems for CS50.",
+    license="GPLv3",
+    message_extractors = {
+        'check50': [('**.py', 'python', None),],
+    },
+    install_requires=["attrs>=18", "beautifulsoup4>=0", "pexpect>=4.6", "lib50>=2,<4", "pyyaml>=3.10", "requests>=2.19", "termcolor>=1.1", "jinja2>=2.10"],
+    extras_require = {
+        "develop": ["sphinx", "sphinx-autobuild", "sphinx_rtd_theme"]
+    },
+    keywords=["check", "check50"],
+    name="check50",
+    packages=["check50", "check50.renderer"],
+    python_requires=">= 3.6",
+    entry_points={
+        "console_scripts": ["check50=check50.__main__:main"]
+    },
+    url="https://github.com/cs50/check50",
+    version="3.2.1",
+    include_package_data=True
+)
