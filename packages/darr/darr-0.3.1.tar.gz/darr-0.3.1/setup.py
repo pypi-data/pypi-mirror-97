5ac0633206cb197b93d86672b9778f6e671adaf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:33 +0000

    Add package darr/0.3.1/darr-0.3.1.tar.gz

---
+++ packages/darr/darr-0.3.1.tar.gz/darr-0.3.1/setup.py
@@ -0,0 +1,104 @@
+import sys
+import versioneer
+import setuptools
+
+if sys.version_info < (3,6):
+    print("Darr requires Python 3.6 or higher please upgrade")
+    sys.exit(1)
+
+long_description = \
+"""
+Darr is a Python science library that enables you to store and access
+disk-based numeric arrays, without depending on tool-specific data formats.
+This makes it easy to access the same data in many different languages and on
+different analysis platforms. No exporting required and, as the data is saved
+in a self-explanatory way, not much explanation required either when sharing
+or archiving your data. Tool-independent and easy access to data is in line
+with good scientific practice as it promotes wide and long-term availability,
+to others but also to yourself. More rationale for this approach is provided
+`here <https://darr.readthedocs.io/en/latest/rationale.html>`__.
+
+Darr supports efficient read/write/append access and is based on universally
+readable flat binary files and automatically generated text files, containing
+human-readable explanation of precisely how your binary data is stored. It
+also provides specific code that reads the data in a variety of current
+scientific data tools such as Python, R, Julia, IDL, Matlab, Maple, and
+Mathematica (see
+`example array <https://github.com/gbeckers/Darr/tree/master/examplearrays/examplearray_uint64.darr>`__).
+
+Darr currently supports numerical N-dimensional arrays, and experimentally
+supports numerical ragged arrays, i.e. a series of arrays in which one
+dimension varies in length.
+
+See this `tutorial <https://darr.readthedocs.io/en/latest/tutorial.html>`__
+for a brief introduction, or the
+`documentation <http://darr.readthedocs.io/>`__ for more info.
+
+Darr is currently pre-1.0, still undergoing significant development. However
+we have been using it in practice in our lab for more than a year on both
+Linux and Windows machines. It is open source and freely available under the
+`New BSD License <https://opensource.org/licenses/BSD-3-Clause>`__ terms.
+
+Features
+--------
+
+Pro's:
+
+-  Data storage purely based on **flat binary** and **text** files, 
+   tool independence.
+-  **Human-readable explanation of how the binary data is stored** is
+   saved in a README text file.
+-  README includes **examples of how to read the particular array** in popular
+   analysis environments such as Python (without Darr), R, Julia,
+   Octave/Matlab, GDL/IDL, and Mathematica.
+-  Supports **very large data arrays**, larger than RAM, through memory-mapping.
+-  Data read/write access is simple and powerful through **NumPy indexing** 
+   (see `here <https://docs.scipy.org/doc/numpy-1.13.0/reference/arrays.indexing.html>`__).
+-  Data is easily **appendable**.
+-  **Many numeric types** are supported: (u)int8-(u)int64, float16-float64,
+   complex64, complex128.
+-  Easy use of **metadata**, stored in a separate
+   `JSON <https://en.wikipedia.org/wiki/JSON>`__ text file.
+-  **Minimal dependencies**, only `NumPy <http://www.numpy.org/>`__.
+-  **Integrates easily** with the
+   `Dask <https://dask.pydata.org/en/latest/>`__ library for
+   **numeric computation on very large arrays**.
+-  Supports **ragged arrays** (still experimental).
+
+See the `documentation <http://darr.readthedocs.io/>`__ for more information.
+
+"""
+
+setuptools.setup(
+    name='darr',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=['darr', 'darr.tests'],
+    url='https://github.com/gbeckers/darr',
+    license='BSD-3',
+    author='Gabriel J.L. Beckers',
+    author_email='gabriel@gbeckers.nl',
+    description='Memory-mapped numeric arrays, based on a '\
+                'format that is self-explanatory and tool-independent',
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    python_requires='>=3.6',
+    install_requires=['numpy'],
+    data_files = [("", ["LICENSE"])],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+    ],
+    project_urls={  # Optional
+        'Source': 'https://github.com/gbeckers/darr',
+    },
+)
