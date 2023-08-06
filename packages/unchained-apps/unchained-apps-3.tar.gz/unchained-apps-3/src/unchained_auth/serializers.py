7e9af9270c5ce7f7af0c39d93b8b7716f8360b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:42 +0000

    Add package unchained-apps/3/unchained-apps-3.tar.gz

---
+++ packages/unchained-apps/unchained-apps-3.tar.gz/unchained-apps-3/src/unchained_auth/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+from unchained_auth.models import User
+
+
+class UserSerializer(ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            "email",
+            "password"
+        ]
+
+
+class GoogleUIDSerializer(serializers.Serializer):
+    # This can be uid when using phone login or google login
+    token = serializers.CharField(
+        max_length=4096, required=True, trim_whitespace=True)
