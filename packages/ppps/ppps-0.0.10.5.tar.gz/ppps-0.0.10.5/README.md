4c94785ec6d69c2e08ca855bc789110c4f890ca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:47 +0000

    Add package ppps/0.0.10.5/ppps-0.0.10.5.tar.gz

---
+++ packages/ppps/ppps-0.0.10.5.tar.gz/ppps-0.0.10.5/README.md
@@ -0,0 +1,1 @@
+# Pre-preprocessing with python scripts
---
+++ packages/ppps/ppps-0.0.10.5.tar.gz/ppps-0.0.10.5/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+__version__ = "0.0.10.5"
+
+
+def read_me():
+    with open("README.md", "r") as file:
+        return file.read()
+
+
+# /tunapro1234/proprepros/archive/v0.0.10-alpha.tar.gz
+
+setup(
+    name="ppps",
+    license="MIT",
+    zip_safe=False,
+    packages=["ppps"],
+    version=__version__,
+    author="TUNAPRO1234",
+    long_description=read_me(),
+    include_package_data=True,
+    author_email="tunagul54@gmail.com",
+    url="https://github.com/tunapro1234/proprepros/",
+    description="pre-preprocessor for C/C++",
+    keywords=["c", "preprocessor", "pre-preprocessor"],
+    download_url=
+    "https://github.com/tunapro1234/proprepros/archive/v0.0.10.5-alpha.tar.gz",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Build Tools',
+    ],
+)
