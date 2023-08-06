4eb8e55f1f9f52eb9119b8e1fe5ad6fb9bf4ab9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:26 +0000

    Add package hostypage/0.3/hostypage-0.3.tar.gz

---
+++ packages/hostypage/hostypage-0.3.tar.gz/hostypage-0.3/setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup
+
+version = os.environ.get('CI_COMMIT_TAG', '0.3')
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='hostypage',
+    version=version,
+    packages=['hosty'],
+    author='Hosty.Page',
+    author_email='christopherdavies553@gmail.com',
+    description='CLI for Hosty.Page',
+    scripts=['bin/hostypage'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['click==7.1.2', 'requests==2.25.1', 'progress==1.5'],
+)
