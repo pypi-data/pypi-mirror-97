46e3127d9c59e11d4f867f69416acea304b1f36c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:47 +0000

    Add package pants-jupyter-plugin/0.0.1/pants_jupyter_plugin-0.0.1-py3-none-any.whl

---
+++ packages/pants-jupyter-plugin/pants_jupyter_plugin-0.0.1-py3-none-any.whl/pants_jupyter_plugin/__init__.py
@@ -0,0 +1,14 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+"""Jupyter support for Pants projects and PEX files."""  # N.B.: Flit uses this as our distribution description.
+
+__version__ = "0.0.1"  # N.B.: Flit uses this as our distribution version.
+
+from IPython import InteractiveShell
+
+from .plugin import _PexEnvironmentBootstrapper
+
+
+def load_ipython_extension(ipython: InteractiveShell) -> None:
+    ipython.register_magics(_PexEnvironmentBootstrapper)
