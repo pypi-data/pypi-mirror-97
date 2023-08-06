7fa35dbbe2b6f459f83f08fee820d35fb9882d2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:41 +0000

    Add package dataset-pipe/0.7.7/dataset_pipe-0.7.7-py3-none-any.whl

---
+++ packages/dataset-pipe/dataset_pipe-0.7.7-py3-none-any.whl/dataset_pipe/encoders/sparse_word2idx_encoder.py
@@ -0,0 +1,38 @@
+import numpy as np
+from dataset_pipe.encoders.math.ops import zeros
+
+
+class SparseWord2IdxEncoder:
+
+    def __init__(self, word2idx, width, no_word_idx=None):
+        self.no_word_idx = no_word_idx
+        self.word2idx = word2idx
+        self.width = width
+        self._dim = width
+        self._shape = (width, )
+        self._type = "float32"
+
+    def encode(self, data):
+        if not isinstance(data, list):
+            raise ValueError("Param data must be list. {} given fo type {}".format(data, type(data)))
+
+        vector = []
+        for word in data[:self.width]:
+            if word in self.word2idx:
+                vector.append(self.word2idx[word])
+            elif self.no_word_idx:
+                vector.append(self.no_word_idx)
+
+        vector = np.array(vector)
+        z = zeros(self.width)
+        z[:vector.shape[0]] = vector
+        return z
+
+    def shape(self):
+        return self._shape
+
+    def type(self):
+        return self._type
+
+    def dim(self):
+        return self._dim
