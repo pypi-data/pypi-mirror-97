559a52fdfd9e59820a5b687173e58d38bdeeb86e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:28 +0000

    Add package DkamSDK/1.4.87/DkamSDK-1.4.87-cp39-cp39-manylinux1_x86_64.whl

---
+++ packages/DkamSDK/DkamSDK-1.4.87-cp39-cp39-manylinux1_x86_64.whl/DkamSDK-1.4.87.data/purelib/DkamSDK/sample/GetTimeStamp.py
@@ -0,0 +1,35 @@
+
+from DkamSDK import *
+import numpy as np
+camera_num = DiscoverCamera()
+print("camera_num=", camera_num)
+if camera_num > 0:
+    camera_sort = CameraSort(0)
+    print("camera_sort=", camera_sort)
+for i in range(camera_num):
+    print("IP:", CameraIP(i))
+    if CameraIP(i) == b'10.0.0.97':
+        camera_ret = i
+CreateCamera()
+connect = CameraConnect(camera_ret)
+print("connect=", connect)
+if connect == 0:  
+
+	# set timestamp
+    setTime=SetTimestamp(camera_ret,1)
+    print("SetTimestamp:", setTime)
+    getTime = GetTimestamp(camera_ret)
+    print("GetTimestamp:", getTime)
+    getTimestatus = GetTimestampStatus(camera_ret)
+    print("GetTimestampStatus:", getTimestatus)
+    settimeLatch=SetTimestampControlLatch(camera_ret)
+    print("SetTimestampControlLatch:", settimeLatch)
+    gettimevalue=GetTimestampValue(camera_ret)
+    print("GetTimestampValue:", gettimevalue)
+    gettimeFrequency=GetTimestampTickFrequency(camera_ret)
+    print("GetTimestampTickFrequency:", gettimeFrequency)
+	
+    disconnect = CameraDisconnect(camera_ret)
+    print("disconnect=", disconnect)
+
+    DestroyCamera()
