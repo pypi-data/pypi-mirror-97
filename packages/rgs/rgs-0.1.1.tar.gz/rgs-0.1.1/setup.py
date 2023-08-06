987fbd993f79440e3352a41263bfb18d005689d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:55 +0000

    Add package rgs/0.1.1/rgs-0.1.1.tar.gz

---
+++ packages/rgs/rgs-0.1.1.tar.gz/rgs-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['rgs', 'rgs.mndrpy', 'rgs.pmaps', 'rgs.ribbons', 'rgs.slimtree']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'rgs',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Vladislav Kargin',
+    'author_email': 'slavakargin@yahoo.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
