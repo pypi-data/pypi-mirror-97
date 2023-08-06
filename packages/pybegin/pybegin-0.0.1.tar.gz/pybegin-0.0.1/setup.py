c03ae571ed897d79ceb9b4f43c35e91b9e6d36a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:06 +0000

    Add package pybegin/0.0.1/pybegin-0.0.1.tar.gz

---
+++ packages/pybegin/pybegin-0.0.1.tar.gz/pybegin-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+  name='pybegin',
+  packages=['pybegin'],
+  description='Lets get started',
+  version='0.0.1',
+  url='http://intel.com/someplace',
+  author='Tsemach Mizrachi',
+  author_email='tsemach.mizachi@intelc.om',  
+  install_requires=[], 
+  keywords=['pip','geospatail','begin'] 
+)
