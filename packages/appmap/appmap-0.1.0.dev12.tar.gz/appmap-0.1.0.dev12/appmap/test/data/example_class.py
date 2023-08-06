265ab9b2bbd170516f960319689999a52481ac76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:41 +0000

    Add package appmap/0.1.0.dev12/appmap-0.1.0.dev12.tar.gz

---
+++ packages/appmap/appmap-0.1.0.dev12.tar.gz/appmap-0.1.0.dev12/appmap/test/data/example_class.py
@@ -0,0 +1,28 @@
+import time
+
+class ClassMethodMixin:
+    @classmethod
+    def class_method(cls):
+        return 'ClassMethodMixin#class_method, cls %s' % (cls.__name__)
+
+
+class Super:
+    def instance_method(self):
+        return 'Super#instance_method'
+
+
+class ExampleClass(Super, ClassMethodMixin):
+    def __repr__(self):
+        return 'ExampleClass and %s' % (self.another_method())
+
+    @staticmethod
+    def static_method():
+        return 'ExampleClass.static_method'
+
+    def another_method(self):
+        return "ExampleClass#another_method"
+
+    def test_exception(self):
+        raise Exception('test exception')
+
+    what_time_is_it = time.gmtime
