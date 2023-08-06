c675fe641f909bbd7dff81918f90958b12a181a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:04 +0000

    Add package tech-inspection-service-client/1.0.2/tech_inspection_service_client-1.0.2.tar.gz

---
+++ packages/tech-inspection-service-client/tech_inspection_service_client-1.0.2.tar.gz/tech_inspection_service_client-1.0.2/tech_inspection_service_client/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from tech_inspection_service_client.api.operators_api import OperatorsApi
+from tech_inspection_service_client.api.points_api import PointsApi
