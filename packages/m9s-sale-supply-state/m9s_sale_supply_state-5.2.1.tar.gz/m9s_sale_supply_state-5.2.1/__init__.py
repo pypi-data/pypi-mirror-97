e326dc6cef6bdd9ed080eab99b9f693aa8cf1731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:14 +0000

    Add package m9s-sale-supply-state/5.2.1/m9s_sale_supply_state-5.2.1.tar.gz

---
+++ packages/m9s-sale-supply-state/m9s_sale_supply_state-5.2.1.tar.gz/m9s_sale_supply_state-5.2.1/__init__.py
@@ -0,0 +1,12 @@
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
+from trytond.pool import Pool
+from . import sale
+
+__all__ = ['register']
+
+
+def register():
+    Pool.register(
+        sale.Sale,
+        module='sale_supply_state', type_='model')
