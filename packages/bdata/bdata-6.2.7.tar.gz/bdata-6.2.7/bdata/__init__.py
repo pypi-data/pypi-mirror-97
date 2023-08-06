4052ca85e99c64cddd49fa4ac46506b658960203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:24 +0000

    Add package bdata/6.2.7/bdata-6.2.7.tar.gz

---
+++ packages/bdata/bdata-6.2.7.tar.gz/bdata-6.2.7/bdata/__init__.py
@@ -0,0 +1,11 @@
+from bdata.bdata import bdata, life
+from bdata.bjoined import bjoined
+from bdata.bmerged import bmerged
+from bdata.exceptions import InputError, MinimizationError
+
+import os
+
+__all__ = ['bdata','bjoined','bmerged']
+__version__ = '6.2.7'
+__author__ = 'Derek Fujimoto'
+_mud_data = os.path.join(os.environ['HOME'],'.bdata')
