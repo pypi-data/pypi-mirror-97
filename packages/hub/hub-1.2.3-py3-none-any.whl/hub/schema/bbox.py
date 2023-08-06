dc6c7434ebfa1b9961b00b5425e080ade6c0855b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:03 +0000

    Add package hub/1.2.3/hub-1.2.3-py3-none-any.whl

---
+++ packages/hub/hub-1.2.3-py3-none-any.whl/hub/schema/bbox.py
@@ -0,0 +1,42 @@
+"""
+License:
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+"""
+
+# from typing import Tuple
+
+from hub.schema.features import Tensor
+
+
+class BBox(Tensor):
+    """| HubSchema` for a normalized bounding box.
+
+    Output: Tensor of type `float32` and shape `[4,]` which contains the
+    normalized coordinates of the bounding box `[xmin, ymin, xmax, ymax]`
+    """
+
+    def __init__(self, dtype="float64", chunks=None, compressor="lz4"):
+        """Construct the connector.
+
+        Parameters
+        ----------
+        dtype : str
+                dtype of bbox coordinates. Default: 'float32'
+        chunks : Tuple[int] | True
+            Describes how to split tensor dimensions into chunks (files) to store them efficiently.
+            It is anticipated that each file should be ~16MB.
+            Sample Count is also in the list of tensor's dimensions (first dimension)
+            If default value is chosen, automatically detects how to split into chunks
+        """
+        super(BBox, self).__init__(
+            shape=(4,), dtype=dtype, chunks=chunks, compressor=compressor
+        )
+
+    def __str__(self):
+        out = super().__str__()
+        out = "BBox" + out[6:]
+        return out
+
+    def __repr__(self):
+        return self.__str__()
