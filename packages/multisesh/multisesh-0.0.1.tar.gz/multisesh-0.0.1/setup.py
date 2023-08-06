17c27135c7466b4462f780fd37795d54e137073c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:52 +0000

    Add package multisesh/0.0.1/multisesh-0.0.1.tar.gz

---
+++ packages/multisesh/multisesh-0.0.1.tar.gz/multisesh-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='multisesh', 
+    version='0.0.1', 
+    author='Tom Wyatt',
+    author_email='womtyatt@gmail.com',
+    url='https://github.com/TomPJWyatt/MultiSesh',
+    license_files = 'MIT',
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    description='Handles imaging data made by multiple sessions.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",    
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.14.5',
+        'matplotlib>=2.2.0,<3.0.0',
+        'tifffile>=2020.11.18',
+        'numpy>=1.19.4',
+        'scipy>=1.5.3',
+        'opencv>=4.5.0',
+        'scikit-image>=0.17.2'
+    ]
+)
