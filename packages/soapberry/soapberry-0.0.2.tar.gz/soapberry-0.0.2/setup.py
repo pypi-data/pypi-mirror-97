039fc997503b4c76e3437616daf381b4a5e7daf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:55 +0000

    Add package soapberry/0.0.2/soapberry-0.0.2.tar.gz

---
+++ packages/soapberry/soapberry-0.0.2.tar.gz/soapberry-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2' 
+DESCRIPTION = 'SoapBerry'
+LONG_DESCRIPTION = 'SoapBerry helper for searching/downloading series'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="soapberry", 
+        version=VERSION,
+        author="Floris Heyvaert",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'soapberry']
+)
+
+
+# python setup.py sdist bdist_wheel
+# twine upload dist/* -u florishey -p "l#vV4BaEWzE2xC2tp0PfY3l5^R8Ajjlb1#4F$#Qk&34hZE5jHhG"
