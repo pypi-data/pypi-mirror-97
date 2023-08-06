6e568443130988a9ef6dd1a707e2616765570df8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:41 +0000

    Add package pyaxmlparser/0.3.25/pyaxmlparser-0.3.25.tar.gz

---
+++ packages/pyaxmlparser/pyaxmlparser-0.3.25.tar.gz/pyaxmlparser-0.3.25/setup.py
@@ -0,0 +1,50 @@
+from setuptools import find_packages, setup
+
+__VERSION__ = '0.3.25'
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pyaxmlparser',
+    version=__VERSION__,
+    url='https://github.com/appknox/pyaxmlparser',
+
+    author='Subho Halder',
+    author_email='sunny@appknox.com',
+    license='Apache License 2.0',
+
+    data_files=[('man/man1', ['apkinfo.1'])],
+    packages=find_packages(exclude=['tests', 'examples']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=['lxml', 'click>=6.7'],
+    entry_points='''
+    [console_scripts]
+    apkinfo = pyaxmlparser.cli:main
+    ''',
+    py_modules=['pyaxmlparser'],
+    description="Python3 Parser for Android XML file and get Application Name without using Androguard",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='appknox axmlparser arscparser android',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
