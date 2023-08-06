dde179f3bea541fe0de93a70f953c8f5a4498e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:32 +0000

    Add package WorldOfGame-pkg-guysaar8/0.0.6/WorldOfGame-pkg-guysaar8-0.0.6.tar.gz

---
+++ packages/WorldOfGame-pkg-guysaar8/WorldOfGame-pkg-guysaar8-0.0.6.tar.gz/WorldOfGame-pkg-guysaar8-0.0.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+setup(
+    name="WorldOfGame-pkg-guysaar8",
+    version="0.0.6",
+    description="Python Games package",
+    py_modules=["MainGame, Live, UserAccount, CurrencyRoulette, GuessGame, MainGame, ManegeGame, MemoryGame"],
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author="Guy Saar",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["worldofgames"],
+    include_package_data=True,
+    install_requires=["requests", "random", "os", "time", "json","sys"],
+)
+
