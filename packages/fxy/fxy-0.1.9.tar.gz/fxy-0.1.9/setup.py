306317df8cf7a4a4eb6144f988415200bd29216d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:29 +0000

    Add package fxy/0.1.9/fxy-0.1.9.tar.gz

---
+++ packages/fxy/fxy-0.1.9.tar.gz/fxy-0.1.9/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='fxy',
+    version='0.1.9',
+    description='Convenience imports and scientific functions.',
+    long_description=long_description,
+    url='https://github.com/mindey/fxy',
+    author='Mindey',
+    author_email='mindey@qq.com',
+    license='UNLICENSE',
+    packages = find_packages(exclude=['docs', 'tests*']),
+    install_requires=['bpython', 'ipython'],
+    extras_require = {
+        'test': ['coverage', 'pytest', 'pytest-cov'],
+        'main': ["sympy", "xarray", "scipy", "statsmodels", "sklearn", "matplotlib", "seaborn"],
+        'all': ["sympy", "xarray", "scipy", "statsmodels", "sklearn", "matplotlib", "seaborn"] + ["xgboost"]
+    },
+    zip_safe=False,
+    entry_points = {
+        'console_scripts': [
+            'fxy=fxy:main'
+        ],
+    }
+)
