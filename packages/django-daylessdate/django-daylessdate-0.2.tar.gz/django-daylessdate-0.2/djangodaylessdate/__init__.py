70bcb3d6ce87636b35f77ce18e0c3b9a9e93387e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:26 +0000

    Add package django-daylessdate/0.2/django-daylessdate-0.2.tar.gz

---
+++ packages/django-daylessdate/django-daylessdate-0.2.tar.gz/django-daylessdate-0.2/djangodaylessdate/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+
+
+__author__ = 'Dmitriy Sokolov'
+__version__ = '0.2'
+
+
+default_app_config = 'djangodaylessdate.apps.DjangoDaylessDateConfig'
+
+
+VERSION = __version__
