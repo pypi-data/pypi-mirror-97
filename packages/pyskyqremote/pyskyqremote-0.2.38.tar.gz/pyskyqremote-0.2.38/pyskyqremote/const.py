8c432ba05a5974f91fb28a20d911bb2c252efc5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:16 +0000

    Add package pyskyqremote/0.2.38/pyskyqremote-0.2.38.tar.gz

---
+++ packages/pyskyqremote/pyskyqremote-0.2.38.tar.gz/pyskyqremote-0.2.38/pyskyqremote/const.py
@@ -0,0 +1,110 @@
+"""Constants for pyskyqremote."""
+# SOAP/UPnP Constants
+SKY_PLAY_URN = "urn:nds-com:serviceId:SkyPlay"
+SKYCONTROL = "SkyControl"
+SOAP_ACTION = '"urn:schemas-nds-com:service:SkyPlay:2#{0}"'
+SOAP_CONTROL_BASE_URL = "http://{0}:49153{1}"
+SOAP_DESCRIPTION_BASE_URL = "http://{0}:49153/description{1}.xml"
+SOAP_PAYLOAD = """<s:Envelope xmlns:s='http://schemas.xmlsoap.org/soap/envelope/' s:encodingStyle='http://schemas.xmlsoap.org/soap/encoding/'>
+    <s:Body>
+        <u:{0} xmlns:u="urn:schemas-nds-com:service:SkyPlay:2">
+            <InstanceID>0</InstanceID>
+        </u:{0}>
+    </s:Body>
+</s:Envelope>"""
+SOAP_RESPONSE = "u:{0}Response"
+SOAP_USER_AGENT = "SKYPLUS_skyplus"
+UPNP_GET_MEDIA_INFO = "GetMediaInfo"
+UPNP_GET_TRANSPORT_INFO = "GetTransportInfo"
+
+# WebSocket Constants
+WS_BASE_URL = "ws://{0}:9006/as/{1}"
+WS_CURRENT_APPS = "apps/status"
+
+# REST Constants
+REST_BASE_URL = "http://{0}:{1}/as/{2}"
+REST_CHANNEL_LIST = "services"
+REST_RECORDING_DETAILS = "pvr/details/{0}"
+REST_RECORDINGS_LIST = "pvr/?limit=1000&offset=0"
+REST_PATH_SYSTEMINFO = "system/information"
+REST_PATH_DEVICEINFO = "system/deviceinformation"
+
+# Sky specific constants
+CURRENT_URI = "CurrentURI"
+CURRENT_TRANSPORT_STATE = "CurrentTransportState"
+APP_STATUS_VISIBLE = "VISIBLE"
+
+PVR = "pvr"
+XSI = "xsi"
+
+SKY_STATE_NOMEDIA = "NO_MEDIA_PRESENT"
+SKY_STATE_OFF = "POWERED OFF"
+SKY_STATE_ON = "ON"
+SKY_STATE_PLAYING = "PLAYING"
+SKY_STATE_PAUSED = "PAUSED_PLAYBACK"
+SKY_STATE_STANDBY = "STANDBY"
+SKY_STATE_STOPPED = "STOPPED"
+SKY_STATE_TRANSITIONING = "TRANSITIONING"
+SKY_STATUS_LIVE = "LIVE"
+
+APP_EPG = "com.bskyb.epgui"
+
+COMMANDS = {
+    "power": 0,
+    "select": 1,
+    "backup": 2,
+    "dismiss": 2,
+    "channelup": 6,
+    "channeldown": 7,
+    "interactive": 8,
+    "sidebar": 8,
+    "help": 9,
+    "services": 10,
+    "search": 10,
+    "tvguide": 11,
+    "home": 11,
+    "i": 14,
+    "text": 15,
+    "up": 16,
+    "down": 17,
+    "left": 18,
+    "right": 19,
+    "red": 32,
+    "green": 33,
+    "yellow": 34,
+    "blue": 35,
+    "0": 48,
+    "1": 49,
+    "2": 50,
+    "3": 51,
+    "4": 52,
+    "5": 53,
+    "6": 54,
+    "7": 55,
+    "8": 56,
+    "9": 57,
+    "play": 64,
+    "pause": 65,
+    "stop": 66,
+    "record": 67,
+    "fastforward": 69,
+    "rewind": 71,
+    "boxoffice": 240,
+    "sky": 241,
+}
+
+KNOWN_COUNTRIES = {
+    "DEU": "DEU",
+    "GBR": "GBR",
+    "IRL": "GBR",
+    "ITA": "ITA",
+}
+
+# Random set of other constants
+EPG_ERROR_PAST_END = "past end of epg"
+EPG_ERROR_NO_DATA = "no epg data found"
+
+RESPONSE_OK = 200
+CONNECT_TIMEOUT = 1000
+HTTP_TIMEOUT = 6
+SOAP_TIMEOUT = 2
---
+++ packages/pyskyqremote/pyskyqremote-0.2.38.tar.gz/pyskyqremote-0.2.38/pyskyqremote/country/remote_gb.py
@@ -0,0 +1,77 @@
+"""UK specific code."""
+import logging
+from datetime import datetime
+
+import requests
+
+from ..classes.programme import Programme
+from ..const import RESPONSE_OK, SKY_STATUS_LIVE
+from .const_gb import CHANNEL_IMAGE_URL, LIVE_IMAGE_URL, PVR_IMAGE_URL, SCHEDULE_URL
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class SkyQCountry:
+    """UK specific SkyQ."""
+
+    def __init__(self):
+        """Initialise UK remote."""
+        self.pvr_image_url = PVR_IMAGE_URL
+
+    def getEpgData(self, sid, channelno, channelName, epgDate):
+        """Get EPG data for UK."""
+        return self._getData(sid, channelno, channelName, epgDate)
+
+    def buildChannelImageUrl(self, sid, channelname):
+        """Build the channel image URL."""
+        return CHANNEL_IMAGE_URL.format(sid)
+
+    def _getData(self, sid, channelno, channelName, epgDate):
+        epgDateStr = epgDate.strftime("%Y%m%d")
+
+        epgUrl = SCHEDULE_URL.format(sid, epgDateStr)
+        epgData = None
+        programmes = set()
+
+        resp = requests.get(epgUrl)
+        if resp.status_code == RESPONSE_OK:
+            epgData = resp.json()["schedule"]
+
+        if epgData is None:
+            return programmes
+
+        if len(epgData) == 0:
+            return programmes
+
+        for p in epgData[0]["events"]:
+            starttime = datetime.utcfromtimestamp(p["st"])
+            endtime = datetime.utcfromtimestamp(p["st"] + p["d"])
+            title = p["t"]
+            season = None
+            if "seasonnumber" in p:
+                if p["seasonnumber"] > 0:
+                    season = p["seasonnumber"]
+            episode = None
+            if "episodenumber" in p:
+                if p["episodenumber"] > 0:
+                    episode = p["episodenumber"]
+            programmeuuid = None
+            imageUrl = None
+            if "programmeuuid" in p:
+                programmeuuid = str(p["programmeuuid"])
+                imageUrl = LIVE_IMAGE_URL.format(programmeuuid)
+
+            programme = Programme(
+                programmeuuid,
+                starttime,
+                endtime,
+                title,
+                season,
+                episode,
+                imageUrl,
+                channelName,
+                SKY_STATUS_LIVE,
+            )
+            programmes.add(programme)
+
+        return programmes
