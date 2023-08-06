35483745471c4cbc65cbecb2dd8c0b0f8952e7e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:39 +0000

    Add package htrc/0.1.55b1/htrc-0.1.55b1.tar.gz

---
+++ packages/htrc/htrc-0.1.55b1.tar.gz/htrc-0.1.55b1/htrc/config.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+"""
+`htrc.volumes`
+
+Contains the configuration parser object.
+"""
+from future import standard_library
+standard_library.install_aliases()
+from builtins import input
+
+from configparser import RawConfigParser as ConfigParser, NoSectionError
+from codecs import open
+from getpass import getpass
+import logging
+import os.path
+import shutil
+import time
+
+from htrc.lib.cli import bool_prompt
+
+DEFAULT_PATH = os.path.expanduser('~')
+DEFAULT_PATH = os.path.join(DEFAULT_PATH, '.htrc')
+if not os.path.exists(DEFAULT_PATH):
+    DEFAULT_FILE = os.path.dirname(__file__)
+    DEFAULT_FILE = os.path.join(DEFAULT_FILE, '.htrc.default')
+    logging.info("Copying default config file to home directory.")
+    shutil.copyfile(DEFAULT_FILE, DEFAULT_PATH)
+
+def _get_value(section, key, path=None):
+    if path is None:
+        path = DEFAULT_PATH
+
+    config = ConfigParser(allow_no_value=True)
+    with open(path, encoding='utf8') as configfile:
+        config.readfp(configfile)
+    try:
+        return config.get(section, key)
+    except NoSectionError:
+        raise EnvironmentError("Config not set for {} {} in {}".format(
+            section, key, path))
+    
+def get_dataapi_port(path=None):
+    port = int(_get_value('data', 'port', path))
+    return (port)
+
+def get_dataapi_host(path=None):
+    host = _get_value('data', 'host', path)
+    return (host)
+
+def get_dataapi_epr(path=None):
+    return _get_value('data', 'url', path)
+
+def get_dataapi_cert(path=None):
+    return _get_value('data', 'cert', path)
+
+def get_dataapi_key(path=None):
+    return _get_value('data', 'key', path)
+
+def get_dataapi_access(path=None):
+    return _get_value('data', 'pd_only', path)
+
+def get_idp_host_port(path=None):
+    host = _get_value('idp', 'host', path)
+    port = _get_value('idp', 'port', path)
+
+    return (host, port)
+
+def get_idp_path(path=None):
+    return _get_value('idp', 'url')
+
+def get_idp_url(path=None):
+    host, port = get_idp_host_port(path)
+    path = get_idp_path(path)
+    if port == 443:
+        # On HTTPS Default Path
+        return "https://{}{}".format(host, path)
+    else:
+        return "https://{}:{}{}".format(host, port, path)
+
+
+# Add jwt credential access methods
+def get_jwt_token(path=None):
+    try:
+        token = _get_value('jwt', 'token', path)
+
+        # check expiration date
+        expiration = int(_get_value('jwt', 'expiration', path))
+        if time.time() > expiration:
+            raise RuntimeError("JWT token expired.") 
+    except:
+        # This should run on either a missing or expired token.
+        import htrc.auth
+        token, expiration = htrc.auth.get_jwt_token()
+        htrc.config.save_jwt_token(token, expiration, path)
+
+
+    return token
+
+def save_jwt_token(token, expiration=None, path=None):
+    """
+    Saves JWT token in the config file.
+    """
+    # Default to ~/.htrc
+    if path is None:
+        path = DEFAULT_PATH
+
+    # Default to expiration of now - force a new token on next request
+    if expiration is None:
+        expiration = time.time()
+
+    # Open and modify existing config file, if it exists.
+    config = ConfigParser(allow_no_value=True)
+    if os.path.exists(path):
+        config.read(path)
+    if not config.has_section('jwt'):
+        config.add_section('jwt')
+
+    # set token and expiration
+    config.set('jwt', 'token', token)
+    config.set('jwt', 'expiration', expiration)
+
+    with open(path, 'w') as credential_file:
+        config.write(credential_file)
+
+    return token
+
+def remove_jwt_token(path=None):
+    """
+    Removes JWT token from the config file.
+    """
+    # Default to ~/.htrc
+    if path is None:
+        path = DEFAULT_PATH
+
+    # Open and modify existing config file, if it exists.
+    config = ConfigParser(allow_no_value=True)
+    if os.path.exists(path):
+        config.read(path)
+    if not config.has_section('jwt'):
+        config.add_section('jwt')
+    # set token and expiration
+    config.set('jwt', 'token', " ")
+    config.set('jwt', 'expiration', " ")
+
+    with open(path, 'w') as credential_file:
+        config.write(credential_file)
+
+
+def get_credentials(path=None):
+    """
+    Retrieves the username and password from a config file for the Data API.
+    Raises an EnvironmentError if not specified.
+    See also: credential_prompt
+    """
+    client_id = _get_value('idp', 'client_id', path)
+    client_secret = _get_value('idp', 'client_secret', path)
+
+    if not client_id and not client_secret:
+        logging.error("Config path: {}".format(path))
+        raise EnvironmentError("No client_id and client_secret stored in config file.")
+
+    return (client_id, client_secret)
+
+def populate_parser(parser):
+    return parser
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser = populate_parser(parser)
+    parser.parse_args()
+
---
+++ packages/htrc/htrc-0.1.55b1.tar.gz/htrc-0.1.55b1/htrc/tools/mallet.py
@@ -0,0 +1,100 @@
+from builtins import str
+import os, os.path
+import subprocess
+import sys
+import tarfile
+import wget
+
+from htrc.volumes import download_volumes
+from htrc.workset import path_to_volumes
+
+MALLET_DIR = os.path.expanduser('~/mallet')
+
+# Mallet is downloaded and intalled in user's home directory
+def install_mallet():
+    if not os.path.exists(MALLET_DIR):
+        os.makedirs(MALLET_DIR)
+        mallet_zip = wget.download('http://mallet.cs.umass.edu/dist/mallet-2.0.8RC3.tar.gz')
+        mallet_dir = tarfile.open(mallet_zip, "r:gz")
+        mallet_dir.extractall(path=MALLET_DIR)
+        mallet_dir.close()
+
+def main(path, topics, iterations, output_dir='/media/secure_volume/workset/'):
+    if not os.path.exists(MALLET_DIR):
+        if not os.path.exists('/media/secure_volume/'):
+            print('Installing Mallet ...')
+            install_mallet()
+            print('\n')
+        else:
+            print('Mallet not installed, but capsule is in secure mode.')
+            print('Switch to maintenance mode and run this command again')
+            print('to install Mallet. Then, switch to secure mode to train')
+            print('topic models.')
+            sys.exit(1)
+
+    if not os.path.isdir(path):
+        try:
+            volumes = path_to_volumes(path)
+        except ValueError as e:
+            print("Could not process workset. {}".format(str(e)))
+            sys.exit(1)
+
+        try:
+            download_volumes(volumes, output_dir)
+        except OSError as e:
+            if not os.path.exists('/media/secure_volume/'):
+                print('Secure volume not mounted. Could not download volumes')
+                sys.exit(1)
+            else:
+                print("Could not download volumes. {} {}".format(e.strerror, e.filename))
+                sys.exit(1)
+        except RuntimeError as e:
+            if not args.debug:
+                print("Could not download volumes. {}".format(str(e)))
+                sys.exit(1)
+            else:
+                raise e
+        path = output_dir
+
+
+    # import the workset to MALLET format.
+    subprocess.check_call([
+        '{}/mallet-2.0.8RC3/bin/mallet'.format(MALLET_DIR),
+        'import-dir',
+        '--input', path,
+        '--output', os.path.join(path, '../corpus.mallet'),
+        '--keep-sequence',
+        '--remove-stopwords'
+        ])
+
+    subprocess.check_call([
+        '{}/mallet-2.0.8RC3/bin/mallet'.format(MALLET_DIR),
+        'train-topics',
+        '--input', os.path.join(path, '../corpus.mallet'),
+        '--num-topics', str(topics),
+        '--output-state', os.path.join(path, '../mallet_state.gz'),
+        '--output-topic-keys', os.path.join(path, '../mallet_topic-keys.txt'),
+        '--output-doc-topics', os.path.join(path, '../mallet_doc-topics.txt'),
+        '--num-iterations', str(iterations)
+        ])
+
+def populate_parser(parser=None):
+    if parser is None:
+        from argparse import ArgumentParser
+        parser = ArgumentParser()
+    parser.add_argument('-k', help="number of topics", required=True)
+    parser.add_argument('--iter', help="number of iterations", default=200)
+    parser.add_argument('--workset-path', help="Location to store workset download.",
+                        default='/media/secure_volume/workset/')
+    parser.add_argument('path', default='/media/secure_volume/workset/',
+        nargs='?')
+    return parser
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description="MALLET tools for the HTRC")
+    populate_parser(parser)
+    args = parser.parse_args()
+
+    main(args.path, args.k, args.iter, args.workset_path)
