45e7adad3c08c1450840801774b8cd5a6b0ccd4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:37 +0000

    Add package plogger-develhoper/0.1.1/plogger_develhoper-0.1.1-py3-none-any.whl

---
+++ packages/plogger-develhoper/plogger_develhoper-0.1.1-py3-none-any.whl/logger/__init__.py
@@ -0,0 +1,3 @@
+from .logger import Logger
+from .logger_interface import LoggerInterface
+from .logger_exception import LoggerException
---
+++ packages/plogger-develhoper/plogger_develhoper-0.1.1-py3-none-any.whl/logger/logger.py
@@ -0,0 +1,33 @@
+from logger_interface import LoggerInterface,DefaultOutput
+from logger_exception import LoggerException
+
+class Logger:
+
+    __instance=None
+    __output=None
+
+    @staticmethod
+    def getInstance():
+        if Logger.__instance is not None:
+            return Singleton.__instance
+        else:
+            Logger()
+            return Logger.__instance
+
+    def __init__(self):
+        if Logger.__instance is not None:
+            raise Exception("This class is a Singleton")
+        else:
+            self.__output=DefaultOutput()
+            Logger.__instance=self
+        
+
+    def log(self,title,message,format="[{title}] {message}",output=None):
+
+        if output is None:
+            output=self.__output
+
+        if not isinstance(output, LoggerInterface):
+            raise LoggerException("output must be instance of LoggerInterface")
+
+        output.write(format.format(title=title,message=message))
