d66f97d2c9cc2cd6006dd0af9898026c603d0023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:29 +0000

    Add package diff-cover/4.2.3/diff_cover-4.2.3-py2.py3-none-any.whl

---
+++ packages/diff-cover/diff_cover-4.2.3-py2.py3-none-any.whl/diff_cover/__init__.py
@@ -0,0 +1,5 @@
+# This file is imported in setup.py and cannot use any external dependencies
+
+VERSION = "4.2.3"
+DESCRIPTION = "Automatically find diff lines that need test coverage."
+QUALITY_DESCRIPTION = "Automatically find diff lines with quality violations."
