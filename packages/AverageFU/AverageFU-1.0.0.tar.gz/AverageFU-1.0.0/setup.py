3b477aac8645b5b491222b1dfc24fb9c67b45882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:09 +0000

    Add package AverageFU/1.0.0/AverageFU-1.0.0.tar.gz

---
+++ packages/AverageFU/AverageFU-1.0.0.tar.gz/AverageFU-1.0.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='AverageFU',
+	version='1.0.0',
+	author='jerrow4',
+	author_email='arsninja2000@gmail.com',
+	description='def Average',
+	packages=['AverageFU'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
