f06e3897717862f742a509c689aff8a82f45374b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:10 +0000

    Add package libiap/0.4.1/libiap-0.4.1.tar.gz

---
+++ packages/libiap/libiap-0.4.1.tar.gz/libiap-0.4.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="libiap",
+    version="0.4.1",
+    packages=find_packages(),
+    url="https://github.com/umccr-illumina/libiap",
+    license="MIT",
+    author="UMCCR and Contributors",
+    author_email="services@umccr.org",
+    description="Please use https://github.com/umccr-illumina/libica instead",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6",
+    install_requires=[
+        "libica==0.5.0",
+    ],
+    extras_require={
+        "dev": [
+            "twine",
+            "setuptools",
+            "wheel",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 7 - Inactive",
+    ],
+)
