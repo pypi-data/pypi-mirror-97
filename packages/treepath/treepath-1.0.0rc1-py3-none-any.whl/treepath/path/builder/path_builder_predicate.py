1a2f66d64e69d47c69dfb9b211fbdeb72e84da30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:23 +0000

    Add package treepath/1.0.0rc1/treepath-1.0.0rc1-py3-none-any.whl

---
+++ packages/treepath/treepath-1.0.0rc1-py3-none-any.whl/treepath/path/builder/path_builder_predicate.py
@@ -0,0 +1,38 @@
+import operator
+
+from treepath.path.builder.path_predicate import PathPredicate
+
+
+def create_partial_operator(operator_, b):
+    def partial_operator_(a):
+        return operator_(a, b)
+
+    return partial_operator_
+
+
+class PathBuilderPredicate:
+    __slots__ = ()
+
+    def __lt__(self, other):
+        operator_ = create_partial_operator(operator.__lt__, other)
+        return PathPredicate(self, operator_)
+
+    def __le__(self, other):
+        operator_ = create_partial_operator(operator.__le__, other)
+        return PathPredicate(self, operator_)
+
+    def __eq__(self, other):
+        operator_ = create_partial_operator(operator.__eq__, other)
+        return PathPredicate(self, operator_)
+
+    def __ne__(self, other):
+        operator_ = create_partial_operator(operator.__ne__, other)
+        return PathPredicate(self, operator_)
+
+    def __gt__(self, other):
+        operator_ = create_partial_operator(operator.__gt__, other)
+        return PathPredicate(self, operator_)
+
+    def __ge__(self, other):
+        operator_ = create_partial_operator(operator.__ge__, other)
+        return PathPredicate(self, operator_)
