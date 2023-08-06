c602489d035861fce336f6a34fadc4ada990b40c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:43 +0000

    Add package django-afip/8.0.1/django_afip-8.0.1-py3-none-any.whl

---
+++ packages/django-afip/django_afip-8.0.1-py3-none-any.whl/django_afip/templatetags/django_afip.py
@@ -0,0 +1,13 @@
+import re
+
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def format_cuit(cuit):
+    numbers = re.sub("[^\\d]", "", str(cuit))
+    if len(numbers) != 11:
+        return cuit
+    return "{}-{}-{}".format(numbers[0:2], numbers[2:10], numbers[10:11])
