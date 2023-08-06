b82ecee4a0fca6b07c30fa55f0953caec14d2f89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:45 +0000

    Add package itallic/0.0.3/itallic-0.0.3-py3.8.egg

---
+++ packages/itallic/itallic-0.0.3-py3.8.egg/itallic/CleaningUtils/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+#######################################################
+#copyright (c) Getiria Onsongo
+#authors: Samantha Fritsche, Thy Nguyen and Getiria Onsongo
+#######################################################
+
+__version__ = '0.0.3'
