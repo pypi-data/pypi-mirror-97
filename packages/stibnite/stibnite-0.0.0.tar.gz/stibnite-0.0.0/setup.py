3554264c1094e20e5570c173ea431d66df679419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:47 +0000

    Add package stibnite/0.0.0/stibnite-0.0.0.tar.gz

---
+++ packages/stibnite/stibnite-0.0.0.tar.gz/stibnite-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='stibnite',
+    version='0.0.0',
+    description='Automated python documentation generator ',
+    long_description='',
+    url='https://github.com/csci-arch/stibnite',
+    author='csci-arch',
+    author_email='csciarch@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
