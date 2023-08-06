da443163ede5daa0a3780d1432126b1df5b9ad6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:11 +0000

    Add package torgrims/0.0.10/torgrims-0.0.10.tar.gz

---
+++ packages/torgrims/torgrims-0.0.10.tar.gz/torgrims-0.0.10/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="torgrims",
+    version="0.0.10",
+    author="Tomas Torgrimsby",
+    author_email="torgrimsatpypi@gmail.com",
+    packages=["torgrims"],
+    description="Package for ML",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://irisml.eu",
+    license="OSI Approved (new BSD)",
+    python_requires=">=3.6",
+    install_requires=[
+        "pandas",
+        "matplotlib",
+        "numpy",
+        "scikit-learn",
+        "scipy",
+        "seaborn",
+        "umap-learn",
+    ]
+)
