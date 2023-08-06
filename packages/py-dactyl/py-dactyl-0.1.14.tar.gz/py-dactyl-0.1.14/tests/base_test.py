c5c93ca96f69ae24eb418d6f46653d6fb10e8545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:17 +0000

    Add package py-dactyl/0.1.14/py-dactyl-0.1.14.tar.gz

---
+++ packages/py-dactyl/py-dactyl-0.1.14.tar.gz/py-dactyl-0.1.14/tests/base_test.py
@@ -0,0 +1,113 @@
+import unittest
+
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+from requests import Session
+from pydactyl.api import base
+
+
+class BaseTests(unittest.TestCase):
+
+    def setUp(self):
+        self.api = base.PterodactylAPI(url='https://dummy.com',
+                                       api_key='dummy_key')
+
+    def test_init(self):
+        self.assertEqual('dummy_key', self.api._api_key)
+        self.assertEqual('https://dummy.com/api', self.api._url)
+
+    def test_get_headers(self):
+        expected = {
+            'Authorization': 'Bearer dummy_key',
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+        self.assertDictEqual(expected, self.api._get_headers())
+
+    def test_url_join(self):
+        self.assertEqual('www.test.com/path', base.url_join(
+            'www.test.com', 'path'))
+        self.assertEqual('test.com/path/to/thing',
+                         base.url_join('test.com', 'path', 'to', 'thing'))
+        self.assertEqual('https://asdf.com/api/other/things',
+                         base.url_join('https://asdf.com', '/api/',
+                                       'other/things'))
+
+    def test_parse_response_with_detail(self):
+        expected = {'object': 'server', 'attributes': {'id': 12}}
+        self.assertEqual(expected, base.parse_response(expected, detail=True))
+
+    def test_parse_response_without_detail(self):
+        expected = {'id': 12}
+        response = {'object': 'server', 'attributes': {'id': 12}}
+        self.assertEqual(expected, base.parse_response(response, detail=False))
+
+    def test_parse_response_list_object_with_detail(self):
+        expected = {'object': 'list',
+                    'data': [{'object': 'server', 'attributes': {'id': 5}},
+                             {'object': 'server', 'attributes': {'id': 6}}]}
+        self.assertEqual(expected, base.parse_response(expected, detail=True))
+
+    def test_parse_response_list_object_without_detail(self):
+        expected = [{'id': 5}, {'id': 6}]
+        response = {'object': 'list',
+                    'data': [{'object': 'server', 'attributes': {'id': 5}},
+                             {'object': 'server', 'attributes': {'id': 6}}]}
+        self.assertEqual(expected, base.parse_response(response, detail=False))
+
+    @mock.patch.object(Session, 'get')
+    def test_valid_api_get_request(self, mock_request):
+        expected = {
+            'params': None,
+            'headers': self.api._get_headers(),
+        }
+        self.api._api_request(endpoint='nomorecoffee')
+        mock_request.assert_called_with('https://dummy.com/api/nomorecoffee',
+                                        **expected)
+
+    @mock.patch.object(Session, 'post')
+    def test_valid_api_post_request(self, mock_request):
+        data = {'test': 'data'}
+        expected = {
+            'params': None,
+            'headers': self.api._get_headers(),
+        }
+        self.api._api_request(endpoint='foo', mode='POST', data=data)
+        mock_request.assert_called_with('https://dummy.com/api/foo', json=data,
+                                        **expected)
+
+    @mock.patch.object(Session, 'patch')
+    def test_valid_api_patch_request(self, mock_request):
+        data = {'patch': 'me'}
+        expected = {
+            'params': None,
+            'headers': self.api._get_headers(),
+            'json': data,
+        }
+        self.api._api_request(endpoint='bar', mode='PATCH', data=data)
+        mock_request.assert_called_with('https://dummy.com/api/bar', **expected)
+
+    @mock.patch.object(Session, 'delete')
+    def test_valid_api_delete_request(self, mock_request):
+        expected = {
+            'params': None,
+            'headers': self.api._get_headers(),
+        }
+        self.api._api_request(endpoint='havecoffee', mode='DELETE')
+        mock_request.assert_called_with('https://dummy.com/api/havecoffee',
+                                        **expected)
+
+    @mock.patch.object(Session, 'put')
+    def test_valid_api_put_request(self, mock_request):
+        data = {'put': 'me'}
+        expected = {
+            'params': None,
+            'headers': self.api._get_headers(),
+            'json': data,
+        }
+        self.api._api_request(endpoint='putstuff', mode='PUT', data=data)
+        mock_request.assert_called_with('https://dummy.com/api/putstuff',
+                                        **expected)
---
+++ packages/py-dactyl/py-dactyl-0.1.14.tar.gz/py-dactyl-0.1.14/tests/client_test.py
@@ -0,0 +1,61 @@
+import unittest
+
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+from pydactyl import PterodactylClient
+from pydactyl.exceptions import BadRequestError
+
+
+class ClientTests(unittest.TestCase):
+
+    def setUp(self):
+        self.client = PterodactylClient(url='dummy', api_key='dummy')
+
+    def test_invalid_power_action_raises_exception(self):
+        with self.assertRaises(BadRequestError):
+            self.client.client.send_power_action(1, 'BADSIGNAL')
+
+    @mock.patch('pydactyl.api.base.PterodactylAPI._api_request')
+    def test_get_server(self, mock_api):
+        expected = {
+            'endpoint': 'client/servers/11',
+        }
+        self.client.client.get_server(11)
+        mock_api.assert_called_with(**expected)
+
+    @mock.patch('pydactyl.api.base.PterodactylAPI._api_request')
+    def test_get_server_utilization(self, mock_api):
+        expected = {
+            'endpoint': 'client/servers/22/utilization',
+        }
+        self.client.client.get_server_utilization(22)
+        mock_api.assert_called_with(**expected)
+
+    @mock.patch('pydactyl.api.base.PterodactylAPI._api_request')
+    def test_send_console_command(self, mock_api):
+        expected = {
+            'endpoint': 'client/servers/33/command',
+            'mode': 'POST',
+            'data': {'command': 'say Test Command'},
+            'json': False,
+        }
+        self.client.client.send_console_command(33, 'say Test Command')
+        mock_api.assert_called_with(**expected)
+
+    @mock.patch('pydactyl.api.base.PterodactylAPI._api_request')
+    def test_valid_power_action_valid_request(self, mock_api):
+        expected = {
+            'endpoint': 'client/servers/1/power',
+            'mode': 'POST',
+            'data': {'signal': 'start'},
+            'json': False,
+        }
+        self.client.client.send_power_action(1, 'start')
+        mock_api.assert_called_with(**expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
