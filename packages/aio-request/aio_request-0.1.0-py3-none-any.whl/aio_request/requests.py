be290dee252ad91045ac3999207757aa90b9fc4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:20 +0000

    Add package aio-request/0.1.0/aio_request-0.1.0-py3-none-any.whl

---
+++ packages/aio-request/aio_request-0.1.0-py3-none-any.whl/aio_request/requests.py
@@ -0,0 +1,132 @@
+import json
+from typing import Any, Callable, Mapping, Optional, Union
+
+import multidict
+import yarl
+
+from .base import Header, Method, Request
+from .utils import get_headers_to_enrich
+
+
+def get(
+    url: Union[str, yarl.URL],
+    *,
+    headers: Optional[multidict.CIMultiDictProxy[str]] = None,
+    path_parameters: Optional[Mapping[str, str]] = None,
+) -> Request:
+    return build_request(Method.GET, url, path_parameters=path_parameters, headers=headers)
+
+
+def post(
+    url: Union[str, yarl.URL],
+    body: Optional[bytes] = None,
+    *,
+    headers: Optional[multidict.CIMultiDictProxy[str]] = None,
+    path_parameters: Optional[Mapping[str, str]] = None,
+) -> Request:
+    return build_request(Method.POST, url, path_parameters=path_parameters, headers=headers, body=body)
+
+
+def put(
+    url: Union[str, yarl.URL],
+    body: Optional[bytes] = None,
+    *,
+    headers: Optional[multidict.CIMultiDictProxy[str]] = None,
+    path_parameters: Optional[Mapping[str, str]] = None,
+) -> Request:
+    return build_request(Method.PUT, url, path_parameters=path_parameters, headers=headers, body=body)
+
+
+def delete(
+    url: Union[str, yarl.URL],
+    *,
+    headers: Optional[multidict.CIMultiDictProxy[str]] = None,
+    path_parameters: Optional[Mapping[str, str]] = None,
+) -> Request:
+    return build_request(Method.DELETE, url, path_parameters=path_parameters, headers=headers)
+
+
+def post_json(
+    url: Union[str, yarl.URL],
+    data: Any,
+    *,
+    path_parameters: Optional[Mapping[str, str]] = None,
+    headers: Optional[multidict.CIMultiDictProxy[str]] = None,
+    encoding: str = "utf-8",
+    dumps: Callable[[str], Any] = json.dumps,
+    content_type: str = "application/json",
+) -> Request:
+    return build_json_request(
+        Method.POST,
+        url,
+        data,
+        path_parameters=path_parameters,
+        headers=headers,
+        encoding=encoding,
+        dumps=dumps,
+        content_type=content_type,
+    )
+
+
+def put_json(
+    url: Union[str, yarl.URL],
+    data: Any,
+    *,
+    path_parameters: Optional[Mapping[str, str]] = None,
+    headers: Optional[multidict.CIMultiDictProxy[str]] = None,
+    encoding: str = "utf-8",
+    dumps: Callable[[str], Any] = json.dumps,
+    content_type: str = "application/json",
+) -> Request:
+    return build_json_request(
+        Method.PUT,
+        url,
+        data,
+        path_parameters=path_parameters,
+        headers=headers,
+        encoding=encoding,
+        dumps=dumps,
+        content_type=content_type,
+    )
+
+
+def build_json_request(
+    method: str,
+    url: Union[str, yarl.URL],
+    data: Any,
+    *,
+    path_parameters: Optional[Mapping[str, str]] = None,
+    headers: Optional[multidict.CIMultiDictProxy[str]] = None,
+    encoding: str = "utf-8",
+    dumps: Callable[[Any], str] = json.dumps,
+    content_type: str = "application/json",
+) -> Request:
+    enriched_headers = get_headers_to_enrich(headers)
+    enriched_headers.add(Header.CONTENT_TYPE, content_type)
+
+    body = dumps(data).encode(encoding)
+
+    return build_request(
+        method=method,
+        url=url,
+        headers=multidict.CIMultiDictProxy[str](enriched_headers),
+        body=body,
+        path_parameters=path_parameters,
+    )
+
+
+def build_request(
+    method: str,
+    url: Union[str, yarl.URL],
+    *,
+    path_parameters: Optional[Mapping[str, str]] = None,
+    headers: Optional[multidict.CIMultiDictProxy[str]] = None,
+    body: Optional[bytes] = None,
+) -> Request:
+    return Request(
+        method=method,
+        url=yarl.URL(url) if isinstance(url, str) else url,
+        headers=headers,
+        body=body,
+        path_parameters=path_parameters,
+    )
---
+++ packages/aio-request/aio_request-0.1.0-py3-none-any.whl/aio_request/utils.py
@@ -0,0 +1,75 @@
+import asyncio
+import contextlib
+from typing import Any, Callable, Collection, Dict, Mapping, Optional, Protocol, TypeVar, Union
+
+import multidict
+import yarl
+
+EMPTY_HEADERS = multidict.CIMultiDictProxy[str](multidict.CIMultiDict[str]())
+
+
+def get_headers_to_enrich(
+    headers: Optional[Union[Mapping[Union[str, multidict.istr], str], multidict.CIMultiDictProxy[str]]]
+) -> multidict.CIMultiDict[str]:
+    return multidict.CIMultiDict[str](headers) if headers is not None else multidict.CIMultiDict[str]()
+
+
+class Closable(Protocol):
+    async def close(self) -> None:
+        ...
+
+
+TClosable = TypeVar("TClosable", bound=Closable)
+
+
+async def _close(item: TClosable) -> None:
+    with contextlib.suppress(Exception):
+        await item.close()
+
+
+T = TypeVar("T")
+
+
+async def close_futures(items: Collection[asyncio.Future[T]], as_close: Callable[[T], TClosable]) -> None:
+    for item in items:
+        if item.cancelled():
+            continue
+        try:
+            await _close(as_close(await item))
+        except asyncio.CancelledError:
+            if not item.cancelled():
+                raise
+
+
+async def close(items: Collection[TClosable]) -> None:
+    for item in items:
+        await _close(item)
+
+
+async def cancel_futures(futures: Collection[asyncio.Future[T]]) -> None:
+    for future in futures:
+        if future.done():
+            continue
+        future.cancel()
+
+
+def substitute_path_parameters(url: yarl.URL, parameters: Optional[Mapping[str, str]] = None) -> yarl.URL:
+    if not parameters:
+        return url
+
+    path = url.path
+    for name, value in parameters.items():
+        path = path.replace(f"{{{name}}}", value)
+
+    build_parameters: Dict[str, Any] = dict(
+        scheme=url.scheme,
+        user=url.user,
+        password=url.password,
+        host=url.host,
+        port=url.port,
+        path=path,
+        query=url.query,
+        fragment=url.fragment,
+    )
+
+    return yarl.URL.build(**{k: v for k, v in build_parameters.items() if v is not None})
