621f05eccef251d9f67e5271d7ec10694aada837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:00 +0000

    Add package gsem/0.2.2/gsem-0.2.2.tar.gz

---
+++ packages/gsem/gsem-0.2.2.tar.gz/gsem-0.2.2/README.md
@@ -0,0 +1,79 @@
+# gsem
+
+[![PyPI version](https://badge.fury.io/py/gsem.svg)](https://pypi.org/project/gsem/)
+
+*gsem* - Command line extension manager for Gnome-Shell
+
+```
+usage: gsem [-h]
+            {ls,enabled,disabled,outdated,info,install,reinstall,uninstall,update,search,enable,disable}
+            ...
+
+Gnome-Shell extension manager
+
+positional arguments:
+  {ls,enabled,disabled,outdated,info,install,reinstall,uninstall,update,search,enable,disable}
+    ls                  list installed extensions
+    enabled             list enabled extensions
+    disabled            list disabled extensions
+    outdated            list outdated extensions
+    info                show extension information
+    install             install extension
+    reinstall           reinstall extension
+    uninstall           uninstall extension
+    update              update extensions
+    search              search extensions
+    enable              enable extension
+    disable             disable extension
+
+optional arguments:
+  -h, --help            show this help message and exit
+```
+
+## Installation
+
+### User installation (recommended)
+Run `pip install --user gsem`
+
+Make sure you have `"$HOME/.local/bin"` in your `$PATH`.
+
+### Global installation
+Run `sudo pip install gsem`
+
+### Updating the package
+
+Run `pip install -U --user gsem` for user installation or `sudo pip install -U gsem` for global installation.
+
+## DONE:
+* list installed
+* list enabled/disabled
+* list outdated
+* extension info
+* search
+* enable/disable
+* install/uninstall/reinstall
+* update
+
+## Contributing
+
+Development on latest python version is preferred, as of now it's 3.9.
+To start you'll need the following setup:
+
+Example uses pyenv to install latest python and manage virtualenv. Run the following commands from the root of the repository.
+
+```sh
+pyenv install 3.9.2           # install latest python version
+pyenv virtualenv 3.9.2 gsem   # create gsem virtual environment
+pyenv activate gsem           # activate the venv
+pyenv local gsem              # set local python version for the repo
+poetry install                # install all dependencies inside the virtual environment
+pre-commit install            # install pre-commit hooks
+```
+
+Run all the linters:
+```sh
+pre-commit run -a
+```
+
+## TODO:
+* pin
---
+++ packages/gsem/gsem-0.2.2.tar.gz/gsem-0.2.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['gsem']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyGObject>=3.38.0,<4.0.0']
+
+entry_points = \
+{'console_scripts': ['gsem = gsem.cli:main']}
+
+setup_kwargs = {
+    'name': 'gsem',
+    'version': '0.2.2',
+    'description': 'Command line extension manager for Gnome-Shell',
+    'long_description': '# gsem\n\n[![PyPI version](https://badge.fury.io/py/gsem.svg)](https://pypi.org/project/gsem/)\n\n*gsem* - Command line extension manager for Gnome-Shell\n\n```\nusage: gsem [-h]\n            {ls,enabled,disabled,outdated,info,install,reinstall,uninstall,update,search,enable,disable}\n            ...\n\nGnome-Shell extension manager\n\npositional arguments:\n  {ls,enabled,disabled,outdated,info,install,reinstall,uninstall,update,search,enable,disable}\n    ls                  list installed extensions\n    enabled             list enabled extensions\n    disabled            list disabled extensions\n    outdated            list outdated extensions\n    info                show extension information\n    install             install extension\n    reinstall           reinstall extension\n    uninstall           uninstall extension\n    update              update extensions\n    search              search extensions\n    enable              enable extension\n    disable             disable extension\n\noptional arguments:\n  -h, --help            show this help message and exit\n```\n\n## Installation\n\n### User installation (recommended)\nRun `pip install --user gsem`\n\nMake sure you have `"$HOME/.local/bin"` in your `$PATH`.\n\n### Global installation\nRun `sudo pip install gsem`\n\n### Updating the package\n\nRun `pip install -U --user gsem` for user installation or `sudo pip install -U gsem` for global installation.\n\n## DONE:\n* list installed\n* list enabled/disabled\n* list outdated\n* extension info\n* search\n* enable/disable\n* install/uninstall/reinstall\n* update\n\n## Contributing\n\nDevelopment on latest python version is preferred, as of now it\'s 3.9.\nTo start you\'ll need the following setup:\n\nExample uses pyenv to install latest python and manage virtualenv. Run the following commands from the root of the repository.\n\n```sh\npyenv install 3.9.2           # install latest python version\npyenv virtualenv 3.9.2 gsem   # create gsem virtual environment\npyenv activate gsem           # activate the venv\npyenv local gsem              # set local python version for the repo\npoetry install                # install all dependencies inside the virtual environment\npre-commit install            # install pre-commit hooks\n```\n\nRun all the linters:\n```sh\npre-commit run -a\n```\n\n## TODO:\n* pin\n',
+    'author': 'Andrii Kohut',
+    'author_email': 'kogut.andriy@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/andriykohut/gsem',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
