2abf2a928fd7ef95bb3abe8505bcc91742418732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:24 +0000

    Add package Geode-SimplexRemesh/3.7.1/Geode_SimplexRemesh-3.7.1-cp36-cp36m-manylinux2014_x86_64.whl

---
+++ packages/Geode-SimplexRemesh/Geode_SimplexRemesh-3.7.1-cp36-cp36m-manylinux2014_x86_64.whl/geode_simplexremesh/solid.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2019 - 2021 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+from geode_common import core, geometry, helpers, modifier_surface, modifier_edged_curve, modifier_model
+
+from .geode_simplexremesh_py_solid import *
+
