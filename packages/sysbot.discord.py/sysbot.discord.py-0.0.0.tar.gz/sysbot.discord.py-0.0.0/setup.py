b29596565a322470a4c38b638c7ecd9118c264eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:53 +0000

    Add package sysbot.discord.py/0.0.0/sysbot.discord.py-0.0.0.tar.gz

---
+++ packages/sysbot.discord.py/sysbot.discord.py-0.0.0.tar.gz/sysbot.discord.py-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='sysbot.discord.py',
+    packages=["sysbot"],
+    version='0.0.0',
+    author='Perzan',
+    author_email='PerzanDevelopment@gmail.com'
+)
