41038d585b54035c4cc8133c5039041e91c0810c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:16 +0000

    Add package databasedict/0.0.0/databasedict-0.0.0.tar.gz

---
+++ packages/databasedict/databasedict-0.0.0.tar.gz/databasedict-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='databasedict',
+    packages=["databasedict"],
+    version='0.0.0',
+    author='Perzan',
+    author_email='PerzanDevelopment@gmail.com',
+    install_requires=["onetrick~=2.1"]
+)
