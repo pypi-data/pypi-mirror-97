844a857271ec8b7a4a5060517e6c30e88bc37a98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:01 +0000

    Add package pyclimacell/0.17.0/pyclimacell-0.17.0-py2.py3-none-any.whl

---
+++ packages/pyclimacell/pyclimacell-0.17.0-py2.py3-none-any.whl/pyclimacell/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for Python ClimaCell API."""
+
+from .pyclimacell import ClimaCellV3, ClimaCellV4, ClimaCellV3Sync, ClimaCellV4Sync
+
+__author__ = """raman325"""
+__email__ = "7243222+raman325@users.noreply.github.com"
+__version__ = "0.17.0"
