f0e2117e2be770c326d628ac3eb53b62123b1dc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:55 +0000

    Add package tg-store-cli/0.2.0/tg_store_cli-0.2.0-py3-none-any.whl

---
+++ packages/tg-store-cli/tg_store_cli-0.2.0-py3-none-any.whl/tg_store_cli/main.py
@@ -0,0 +1,27 @@
+import typer
+
+
+app = typer.Typer()
+
+
+@app.callback()
+def callback():
+    """
+    TG Store CLI
+    """
+
+
+@app.command()
+def shoot():
+    """
+    Стрельба 
+    """
+    typer.echo("Shooting portal gun")
+
+
+@app.command()
+def load():
+    """
+    Зарядка
+    """
+    typer.echo("Loading portal gun")
