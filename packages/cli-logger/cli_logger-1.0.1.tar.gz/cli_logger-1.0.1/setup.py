0cad326554cf03cf82f8e5a414af4e23a24e0465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:09 +0000

    Add package cli-logger/1.0.1/cli_logger-1.0.1.tar.gz

---
+++ packages/cli-logger/cli_logger-1.0.1.tar.gz/cli_logger-1.0.1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+      name='cli_logger',
+      version='1.0.1',
+      description='A module to pretty-print outputs for command-line programs.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/TomMeHo/cli_logger',
+      author='Thomas Meder',
+      author_email='tom@tommho.net',
+      license='BSD',
+      packages=setuptools.find_packages(),
+      zip_safe=False,
+      install_requires=[
+            'click >= 7.1.2',
+            'colorama >= 0.4.4',
+            'datetime >= 4.3'
+      ],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: System :: Logging"
+      ],
+      python_requires='>=3.6'
+)
