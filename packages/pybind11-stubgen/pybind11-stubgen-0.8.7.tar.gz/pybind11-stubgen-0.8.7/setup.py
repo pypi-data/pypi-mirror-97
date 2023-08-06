6306f854470e8d1c039a0c9508d1fa6f08e64e03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:13 +0000

    Add package pybind11-stubgen/0.8.7/pybind11-stubgen-0.8.7.tar.gz

---
+++ packages/pybind11-stubgen/pybind11-stubgen-0.8.7.tar.gz/pybind11-stubgen-0.8.7/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pybind11-stubgen',
+    maintainer="Sergei Izmailov",
+    maintainer_email="sergei.a.izmailov@gmail.com",
+    description="PEP 561 type stubs generator for pybind11 modules",
+    url="https://github.com/sizmailov/pybind11-stubgen",
+    version="0.8.7",
+    long_description=open("README.rst").read(),
+    license="BSD",
+    entry_points={'console_scripts': 'pybind11-stubgen = pybind11_stubgen.__init__:main'},
+    packages=['pybind11_stubgen']
+)
