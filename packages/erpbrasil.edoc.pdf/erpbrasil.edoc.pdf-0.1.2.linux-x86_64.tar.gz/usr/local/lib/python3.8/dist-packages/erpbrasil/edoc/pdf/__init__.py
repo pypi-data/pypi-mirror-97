105d2f95db619806e4af6bee41a8559e0b5a95e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:26 +0000

    Add package erpbrasil.edoc.pdf/0.1.2/erpbrasil.edoc.pdf-0.1.2.linux-x86_64.tar.gz

---
+++ packages/erpbrasil.edoc.pdf/erpbrasil.edoc.pdf-0.1.2.linux-x86_64.tar.gz/usr/local/lib/python3.8/dist-packages/erpbrasil/edoc/pdf/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '0.1.2'
+
+from lxml import objectify, etree
+
+lookup = etree.ElementNamespaceClassLookup(
+    objectify.ObjectifyElementClassLookup())
+parser = etree.XMLParser()
+parser.set_element_class_lookup(lookup)
+
+from erpbrasil.edoc.pdf import nfe
+from erpbrasil.edoc.pdf.base import ImprimirXml
