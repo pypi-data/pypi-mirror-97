84e738e3e56e081ae67bffe167cab3a00ae09a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:10 +0000

    Add package AFU/1.0.0/AFU-1.0.0.tar.gz

---
+++ packages/AFU/AFU-1.0.0.tar.gz/AFU-1.0.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='AFU',
+	version='1.0.0',
+	author='jerrow4',
+	author_email='arsninja2000@gmail.com',
+	description='average for you',
+	packages=['AverageFU'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
