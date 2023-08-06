16c6bb27a1e5557a6950a53534d03d79a6157fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:11 +0000

    Add package pyworkforce/0.1.1/pyworkforce-0.1.1.tar.gz

---
+++ packages/pyworkforce/pyworkforce-0.1.1.tar.gz/pyworkforce-0.1.1/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="pyworkforce",
+    version="0.1.1",
+    description="Common tools for workforce management, schedule and optimization problems",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rodrigo-arenas/pyworkforce",
+    author="Rodrigo Arenas",
+    author_email="rodrigo.arenas456@gmail.com",
+    license="MIT",
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+)
