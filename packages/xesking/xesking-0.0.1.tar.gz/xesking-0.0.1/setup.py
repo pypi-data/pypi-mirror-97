782d6aadee4cc7314f74b05308ca30be86e9e6c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:36 +0000

    Add package xesking/0.0.1/xesking-0.0.1.tar.gz

---
+++ packages/xesking/xesking-0.0.1.tar.gz/xesking-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+setuptools.setup(
+    name='xesking',
+    version='0.0.1',
+    author='Yanming Guo',
+    author_email='gyms1111@163.com',
+    description='这个是C站的封装库，现在已上传到PyPi',
+    long_descripition_content_type="text/markdown",
+    url='https://github.com/',
+    packages=setuptools.find_packages(),
+    classifiers= [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent" ,
+    ],
+)
