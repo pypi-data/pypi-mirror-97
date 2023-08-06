4a774fa13e4aad783b2f966e24fdd247ef6b4c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:25 +0000

    Add package Radium-Tech/0.0.4/Radium-Tech-0.0.4.tar.gz

---
+++ packages/Radium-Tech/Radium-Tech-0.0.4.tar.gz/Radium-Tech-0.0.4/radium/helpers/_convert_date.py
@@ -0,0 +1,37 @@
+from datetime import date as datetime_date
+from datetime import datetime
+
+
+def _convert_date(date):
+    """
+    Converts a date to datetime.date type
+
+    Parameters
+    ----------
+    date : str or datetime or datetime.date
+        First date to plot in YYYY-MM-DD form
+
+    Returns
+    -------
+    date : datetime.date
+        date as datetime.date type
+
+    Raises
+    ------
+    TypeError
+        Date not of types str or datetime or datetime.date
+    """
+
+    # Converts strings
+    if isinstance(date, str):
+        date = datetime.strptime(date, "%Y-%m-%d").date()
+
+    # Converts date object
+    elif isinstance(date, datetime):
+        date = date.date()
+
+    # If not in required form input is incorrect
+    if not isinstance(date, datetime_date):
+        raise TypeError("date should be a string or datetime object")
+
+    return date
---
+++ packages/Radium-Tech/Radium-Tech-0.0.4.tar.gz/Radium-Tech-0.0.4/radium/pair/cadf_test.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import statsmodels.tsa.stattools as ts
+
+from .pair import Pair
+
+
+def cadf_test(pair):
+    """
+    Conducts a Cointegrated Augmented Dickey Fuller Test on a pair of equities.
+
+    Prints t-statistic, p-value and critical values
+
+    Parameters
+    ----------
+    pair : radium.Pair
+
+    Raises
+    ------
+    TypeError
+        If pair is not of type radium.Pair.
+    """
+
+    if not isinstance(pair, Pair):
+        raise TypeError('Pair must be of type radium.Pair')
+
+    # Get closed prices in dataframe
+    equity1_df = pair.equity1.closed
+    equity2_df = pair.equity2.closed
+
+    # Set column name as symbol
+    equity1_df = equity1_df.rename(pair.equity1.symbol)
+    equity2_df = equity2_df.rename(pair.equity2.symbol)
+
+    # Augment dataframes, removing missing data
+    df = pd.concat([equity1_df, equity2_df], axis=1, join="inner")
+
+    # Get CADF result
+    coint_t, pvalue, crit_value = ts.coint(df[pair.equity1.symbol],
+                                           df[pair.equity2.symbol])
+
+    # Round to make more interpretable
+    coint_t = round(coint_t, 3)
+    pvalue = round(pvalue, 3)
+    crit_value = [round(x, 3) for x in crit_value]
+
+    # Print results
+    results = (f'CADF Test for cointegration between {pair.equity1.symbol} '
+               f'and {pair.equity2.symbol} from {pair.start_date} to '
+               f'{pair.end_date}\n\n'
+               f't-statistic = {coint_t}\n'
+               f'p-value = {pvalue}\n\n'
+               f'1% Critical Value: {crit_value[0]}\n'
+               f'5% Critical Value: {crit_value[1]}\n'
+               f'10% Critical Value: {crit_value[2]}\n\n')
+
+    print(results)
