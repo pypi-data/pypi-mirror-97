6e5684b2567149b55f04c9f9be8490779b9adfdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:19 +0000

    Add package vaex-astro/0.8.0/vaex-astro-0.8.0.tar.gz

---
+++ packages/vaex-astro/vaex-astro-0.8.0.tar.gz/vaex-astro-0.8.0/setup.py
@@ -0,0 +1,38 @@
+import os
+import imp
+from setuptools import setup
+
+dirname = os.path.dirname(__file__)
+path_version = os.path.join(dirname, 'vaex/astro/_version.py')
+version = imp.load_source('version', path_version)
+
+name        = 'vaex'
+author      = 'Maarten A. Breddels'
+author_email= 'maartenbreddels@gmail.com'
+license     = 'MIT'
+version     = version.__version__
+url         = 'https://www.github.com/maartenbreddels/vaex'
+install_requires_astro = ['vaex-core>=4.0.0,<5', 'astropy']
+
+setup(
+    name=name + '-astro',
+    version=version,
+    description='Astronomy related transformations and FITS file support',
+    url=url,
+    author=author,
+    author_email=author_email,
+    install_requires=install_requires_astro,
+    license=license,
+    packages=['vaex.astro'],
+    zip_safe=False,
+    entry_points={
+        'vaex.plugin': ['astro = vaex.astro.legacy:add_plugin'],
+        'vaex.dataframe.accessor': ['astro = vaex.astro.transformations:DataFrameAccessorAstro'],
+        'vaex.dataset.opener': [
+                'fits = vaex.astro.fits:FitsBinTable',
+                'gadget = vaex.astro.gadget:MemoryMappedGadget',
+                'votable = vaex.astro.votable:VOTable',
+                'tap = vaex.astro.tap:DatasetTap',
+        ],
+    },
+)
