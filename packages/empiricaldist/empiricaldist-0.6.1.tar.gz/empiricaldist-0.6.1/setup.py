e7a37a693490a154ece2a8f700996739b1bf45a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:46 +0000

    Add package empiricaldist/0.6.1/empiricaldist-0.6.1.tar.gz

---
+++ packages/empiricaldist/empiricaldist-0.6.1.tar.gz/empiricaldist-0.6.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+def readme():
+    try:
+        with open('README.rst') as f:
+            return f.read()
+    except IOError:
+        return ''
+
+
+setup(
+    name='empiricaldist',
+    version='0.6.1',
+    author='Allen B. Downey',
+    author_email='downey@allendowney.com',
+    packages=['empiricaldist'],
+    url='http://github.com/AllenDowney/empiricaldist',
+    license='LICENSE',
+    description='Python library that represents empirical distributions.',
+    long_description=readme(),
+)
