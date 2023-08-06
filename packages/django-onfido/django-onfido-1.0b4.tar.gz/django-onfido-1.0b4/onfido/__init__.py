3874e5f1ef8e8a35cbff155a2cf404ff08742f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:15 +0000

    Add package django-onfido/1.0b4/django-onfido-1.0b4.tar.gz

---
+++ packages/django-onfido/django-onfido-1.0b4.tar.gz/django-onfido-1.0b4/onfido/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = "onfido.apps.OnfidoAppConfig"
---
+++ packages/django-onfido/django-onfido-1.0b4.tar.gz/django-onfido-1.0b4/onfido/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class OnfidoAppConfig(AppConfig):
+    """AppConfig for Django-Onfido."""
+
+    name = "onfido"
+    verbose_name = "Onfido"
