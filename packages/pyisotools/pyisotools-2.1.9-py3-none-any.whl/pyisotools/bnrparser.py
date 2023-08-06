669fdf1f71cd32a94d3a2105f2cc7ec3f8e1d004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:24 +0000

    Add package pyisotools/2.1.9/pyisotools-2.1.9-py3-none-any.whl

---
+++ packages/pyisotools/pyisotools-2.1.9-py3-none-any.whl/pyisotools/bnrparser.py
@@ -0,0 +1,393 @@
+from __future__ import annotations
+
+import json
+
+from argparse import ArgumentParser
+from functools import wraps
+from io import BytesIO
+from pathlib import Path
+from typing import Union
+
+from PIL import Image
+
+
+def _read_string(io, offset: int = 0, maxlen: int = 0, encoding: str = "iso-8859-1") -> str:
+    io.seek(offset)
+    cString = io.read(maxlen)
+    return cString.rstrip(b"\x00").decode(encoding)
+
+
+def io_preserve(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        _loc = self._rawdata.tell()
+        value = func(self, *args, **kwargs)
+        self._rawdata.seek(_loc)
+        return value
+    return wrapper
+
+
+class Jobs:
+    COMPILE = "COMPILE"
+    EXTRACT = "EXTRACT"
+
+
+class RGB5A1(object):
+
+    TileWidth = 4
+    TileHeight = 4
+
+    @staticmethod
+    def encode_pixel(pixel: tuple) -> int:
+        """ RGB888 or RGBA8888 ONLY """
+        r = (pixel[0] >> 3) & 0b11111
+        g = (pixel[1] >> 3) & 0b11111
+        b = (pixel[2] >> 3) & 0b11111
+        a = 1
+
+        return (a << 15) | (r << 10) | (g << 5) | b
+
+    @staticmethod
+    def decode_pixel(pixel: int) -> tuple:
+        """ RGBA8888 ONLY """
+        r = ((pixel >> 10) & 0b11111) << 3
+        g = ((pixel >> 5) & 0b11111) << 3
+        b = (pixel & 0b11111) << 3
+        a = 0xFF
+
+        return (r, g, b, a)
+
+
+class RGB5A3(object):
+
+    TileWidth = 4
+    TileHeight = 4
+
+    @staticmethod
+    def encode_pixel(pixel: tuple) -> int:
+        """ RGB888 or RGBA8888 ONLY """
+        r = (pixel[0] >> 4) & 0b1111
+        g = (pixel[1] >> 4) & 0b1111
+        b = (pixel[2] >> 4) & 0b1111
+        if len(pixel) == 3:
+            a = 0b111
+        else:
+            a = (pixel[3] >> 5) & 0b111
+
+        return (a << 12) | (r << 8) | (g << 4) | b
+
+    @staticmethod
+    def decode_pixel(pixel: int) -> tuple:
+        """ RGBA8888 ONLY """
+        r = ((pixel >> 8) & 0b1111) << 4
+        g = ((pixel >> 4) & 0b1111) << 4
+        b = (pixel & 0b1111) << 4
+        a = ((pixel >> 12) & 0b111) << 5
+
+        return (r, g, b, a)
+
+
+class BNR(RGB5A3):
+    class Regions:
+        AMERICA = 0
+        EUROPE = 1
+        JAPAN = 2
+        KOREA = 0
+
+    ImageWidth = 96
+    ImageHeight = 32
+
+    ImgTileWidth = ImageWidth // RGB5A1.TileWidth
+    ImgTileHeight = ImageHeight // RGB5A1.TileHeight
+
+    def __init__(self, f: Path, region: Regions = Regions.AMERICA, gameName: str = "", gameTitle: str = "", developerName: str = "", developerTitle: str = "", desc: str = "", overwrite=False):
+        self._rawdata = BytesIO(b"\x00" * (0x1960 if region != BNR.Regions.EUROPE else 0x1FA0))
+        self.regionID = region
+        self.index = 0
+        
+        self.load(f, region, gameName, gameTitle,
+                  developerName, developerTitle, desc, overwrite)
+
+    @classmethod
+    def from_data(cls, obj, region: Regions = Regions.AMERICA, size: int = -1):
+        self = cls.__new__(cls)
+        self._rawdata = BytesIO(obj.read(size))
+        self.regionID = region
+        self.index = 0
+        return self
+
+    @property
+    def isBNR2(self) -> bool:
+        return self.magic == "BNR2" and len(self._rawdata.getbuffer()) == 0x1FA0
+
+    @property
+    def region(self) -> str:
+        if self.regionID == BNR.Regions.AMERICA:
+            return "NTSC-U"
+        elif self.regionID == BNR.Regions.EUROPE:
+            return "PAL"
+        elif self.regionID == BNR.Regions.JAPAN:
+            return "NTSC-J"
+        else:
+            return "NTSC-K"
+
+    @property
+    @io_preserve
+    def magic(self) -> str:
+        self._rawdata.seek(0)
+        return self._rawdata.read(4).decode("iso-8859-1")
+
+    @magic.setter
+    @io_preserve
+    def magic(self, region: Regions):
+        self._rawdata.seek(0)
+
+        if region == BNR.Regions.EUROPE and len(self._rawdata.getbuffer()) == 0x1FA0:
+            self._rawdata.write(b"BNR2")
+        else:
+            self._rawdata.write(b"BNR1")
+
+    @property
+    @io_preserve
+    def rawImage(self) -> BytesIO:
+        self._rawdata.seek(0x20)
+        return BytesIO(self._rawdata.read(0x1800))
+
+    @rawImage.setter
+    @io_preserve
+    def rawImage(self, _img: Union[bytes, BytesIO, Image.Image]):
+        self._rawdata.seek(0x20)
+        if isinstance(_img, bytes):
+            self._rawdata.write(_img[:0x1800])
+        elif isinstance(_img, BytesIO):
+            self._rawdata.write(_img.getvalue()[:0x1800])
+        elif isinstance(_img, Image.Image):
+            smallImg = _img.resize((BNR.ImageWidth, BNR.ImageHeight))
+            for yBlock in range(BNR.ImgTileHeight):
+                for xBlock in range(BNR.ImgTileWidth):
+                    for iy in range(BNR.TileHeight):
+                        for ix in range(BNR.TileWidth):
+                            x = xBlock*BNR.TileWidth + ix
+                            y = yBlock*BNR.TileHeight + iy
+
+                            pixel = smallImg.getpixel((x, y))
+                            if x < BNR.ImageWidth and y < BNR.ImageHeight:
+                                self._rawdata.write(self._encode_pixel(
+                                    pixel).to_bytes(2, "big", signed=False))
+
+    @property
+    @io_preserve
+    def gameName(self) -> str:
+        return _read_string(self._rawdata, 0x1820 + (self.index * 0x140), 0x20, encoding="iso-8859-1" if self.region != "NTSC-J" else "shift-jis")
+
+    @gameName.setter
+    @io_preserve
+    def gameName(self, name: str):
+        self._rawdata.seek(0x1820 + (self.index * 0x140))
+        self._rawdata.write(b"\x00" * 0x20)
+        self._rawdata.seek(0x1820 + (self.index * 0x140))
+        self._rawdata.write(bytes(name[:0x1F], "iso-8859-1" if self.region != "NTSC-J" else "shift-jis"))
+
+    @property
+    @io_preserve
+    def developerName(self) -> str:
+        return _read_string(self._rawdata, 0x1840 + (self.index * 0x140), 0x20, encoding="iso-8859-1" if self.region != "NTSC-J" else "shift-jis")
+
+    @developerName.setter
+    @io_preserve
+    def developerName(self, name: str):
+        self._rawdata.seek(0x1840 + (self.index * 0x140))
+        self._rawdata.write(b"\x00" * 0x20)
+        self._rawdata.seek(0x1840 + (self.index * 0x140))
+        self._rawdata.write(bytes(name[:0x1F], "iso-8859-1" if self.region != "NTSC-J" else "shift-jis"))
+
+    @property
+    @io_preserve
+    def gameTitle(self) -> str:
+        return _read_string(self._rawdata, 0x1860 + (self.index * 0x140), 0x40, encoding="iso-8859-1" if self.region != "NTSC-J" else "shift-jis")
+
+    @gameTitle.setter
+    @io_preserve
+    def gameTitle(self, name: str):
+        self._rawdata.seek(0x1860 + (self.index * 0x140))
+        self._rawdata.write(b"\x00" * 0x40)
+        self._rawdata.seek(0x1860 + (self.index * 0x140))
+        self._rawdata.write(bytes(name[:0x3F], "iso-8859-1" if self.region != "NTSC-J" else "shift-jis"))
+
+    @property
+    @io_preserve
+    def developerTitle(self) -> str:
+        return _read_string(self._rawdata, 0x18A0 + (self.index * 0x140), 0x40, encoding="iso-8859-1" if self.region != "NTSC-J" else "shift-jis")
+
+    @developerTitle.setter
+    @io_preserve
+    def developerTitle(self, name: str):
+        self._rawdata.seek(0x18A0 + (self.index * 0x140))
+        self._rawdata.write(b"\x00" * 0x40)
+        self._rawdata.seek(0x18A0 + (self.index * 0x140))
+        self._rawdata.write(bytes(name[:0x3F], "iso-8859-1" if self.region != "NTSC-J" else "shift-jis"))
+
+    @property
+    @io_preserve
+    def gameDescription(self) -> str:
+        return _read_string(self._rawdata, 0x18E0 + (self.index * 0x140), 0x80, encoding="iso-8859-1" if self.region != "NTSC-J" else "shift-jis")
+
+    @gameDescription.setter
+    @io_preserve
+    def gameDescription(self, name: str):
+        self._rawdata.seek(0x18E0 + (self.index * 0x140))
+        self._rawdata.write(b"\x00" * 0x80)
+        self._rawdata.seek(0x18E0 + (self.index * 0x140))
+        self._rawdata.write(bytes(name[:0x7F], "iso-8859-1" if self.region != "NTSC-J" else "shift-jis"))
+
+    def getImage(self) -> Image:
+        _image = self.rawImage
+        img = Image.new("RGBA", (BNR.ImageWidth, BNR.ImageHeight))
+
+        for yBlock in range(BNR.ImgTileHeight):
+            for xBlock in range(BNR.ImgTileWidth):
+                for iy in range(BNR.TileHeight):
+                    for ix in range(BNR.TileWidth):
+                        x = xBlock*BNR.TileWidth + ix
+                        y = yBlock*BNR.TileHeight + iy
+
+                        pixel = int.from_bytes(
+                            _image.read(2), "big", signed=False)
+                        if x < BNR.ImageWidth and y < BNR.ImageHeight:
+                            img.putpixel((x, y), self._decode_pixel(pixel))
+
+        return img
+
+    def save_bnr(self, dest: Path):
+        dest.write_bytes(self._rawdata.getvalue())
+
+    def save_png(self, dest: Path):
+        img = self.getImage()
+        img.save(str(dest))
+
+        f = dest.parent / (dest.stem + ".json")
+        info = {}
+
+        info["gamename"] = self.gameName
+        info["gametitle"] = self.gameTitle
+        info["developername"] = self.developerName
+        info["developertitle"] = self.developerTitle
+        info["description"] = self.gameDescription
+
+        with f.open("w") as jsonFile:
+            json.dump(info, jsonFile, indent=4)
+
+    def load(self, f: Path, region: Regions = Regions.AMERICA, gameName: str = "", gameTitle: str = "", developerName: str = "", developerTitle: str = "", desc: str = "", overwrite: bool = False):
+        self.regionID = region
+        if f.suffix == ".bnr":
+            self._rawdata = BytesIO(f.read_bytes())
+            if overwrite:
+                self.magic = region
+                self.gameName = gameName
+                self.gameTitle = gameTitle
+                self.developerName = developerName
+                self.developerTitle = developerTitle
+                self.gameDescription = desc
+        elif f.suffix in (".png", ".jpeg"):
+            j = f.parent / (f.stem + ".json")
+
+            if j.exists() and not overwrite:
+                with j.open("r") as jsonFile:
+                    info = json.load(jsonFile)
+
+                self.gameName = info["gamename"]
+                self.gameTitle = info["gametitle"]
+                self.developerName = info["developername"]
+                self.developerTitle = info["developertitle"]
+                self.gameDescription = info["description"]
+            else:
+                self.gameName = gameName
+                self.gameTitle = gameTitle
+                self.developerName = developerName
+                self.developerTitle = developerTitle
+                self.gameDescription = desc
+
+            self.magic = region
+
+            self.rawImage = Image.open(f)
+
+    @staticmethod
+    def _encode_pixel(pixel: tuple):
+        if len(pixel) == 3:
+            return RGB5A1.encode_pixel(pixel)
+        else:
+            return RGB5A3.encode_pixel(pixel)
+
+    @staticmethod
+    def _decode_pixel(pixel: int):
+        if (pixel >> 15) & 1:
+            return RGB5A1.decode_pixel(pixel)
+        else:
+            return RGB5A3.decode_pixel(pixel)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        "BNR Parser", description="Tool to extract and compile bnr files")
+
+    parser.add_argument("bnr", help="bnr file")
+    parser.add_argument("img", help="png or jpeg image")
+    parser.add_argument("-j", "--job",
+                        help="Job to execute",
+                        choices=[Jobs.EXTRACT, Jobs.COMPILE],
+                        default=Jobs.COMPILE)
+    parser.add_argument("-r", "--region",
+                        help="Game region",
+                        choices=["E", "P", "J"],
+                        default="E")
+    parser.add_argument("-g", "--gamename",
+                        help="Game short name",
+                        default="",
+                        metavar="STR")
+    parser.add_argument("-G", "--gametitle",
+                        help="Game long name",
+                        default="",
+                        metavar="STR")
+    parser.add_argument("-d", "--devname",
+                        help="Developer short name",
+                        default="",
+                        metavar="STR")
+    parser.add_argument("-D", "--devtitle",
+                        help="Developer long name",
+                        default="",
+                        metavar="STR")
+    parser.add_argument("--desc",
+                        help="Game description",
+                        default="",
+                        metavar="STR")
+    parser.add_argument("-o", "--overwrite",
+                        help="Force arguments over json",
+                        action="store_true")
+
+    args = parser.parse_args()
+
+    if args.region == "E":
+        region = BNR.Regions.AMERICA
+    elif args.region == "P":
+        region = BNR.Regions.EUROPE
+    elif args.region == "J":
+        region = BNR.Regions.JAPAN
+    else:
+        raise NotImplementedError(
+            f"Unknown region type {args.region} provided")
+
+    if args.job == Jobs.COMPILE:
+        bnr = BNR(Path(args.img).resolve(),
+                  region=region,
+                  gameName=args.gamename,
+                  gameTitle=args.gametitle,
+                  developerName=args.devname,
+                  developerTitle=args.devtitle,
+                  desc=args.desc,
+                  overwrite=args.overwrite)
+
+        bnr.save_bnr(Path(args.bnr).resolve())
+    elif args.job == Jobs.EXTRACT:
+        bnr = BNR(Path(args.bnr).resolve(), overwrite=args.overwrite)
+
+        bnr.save_png(Path(args.img).resolve())
---
+++ packages/pyisotools/pyisotools-2.1.9-py3-none-any.whl/pyisotools/fst.py
@@ -0,0 +1,369 @@
+from __future__ import annotations
+
+from fnmatch import fnmatch
+from pathlib import Path
+from typing import Union
+
+
+class FileAccessOnFolderError(Exception):
+    pass
+
+
+class FolderAccessOnFileError(Exception):
+    pass
+
+
+class InvalidEntryError(Exception):
+    pass
+
+
+class InvalidFSTError(Exception):
+    pass
+
+
+class FSTNode(object):
+
+    FILE = 0
+    FOLDER = 1
+
+    def __init__(self, name: str, nodetype: int = None, nodeid: int = 0, parent: FSTNode = None, children: tuple = ()):
+        """
+        Initialize a new FSTNode object, and set the parent and children according to the optional args :parentnode: and :children:
+
+            name:     Name of the node
+            nodetype: Node type (0 = File, 1 = Folder)
+            nodeid:   Node id
+            parent:   Parent node
+            children: Tuple of children nodes
+        """
+
+        self.name = name
+        self.type = nodetype
+
+        # metadata
+        self._alignment = 4
+        self._position = None
+        self._exclude = False
+
+        # file attributes
+        self._filesize = None
+        self._fileoffset = None
+
+        # folder attributes
+        self._dirparent = None
+        self._dirnext = None
+
+        self._parent = None
+        self._children = {}
+        self._id = nodeid
+
+        # setup
+        self.parent = parent
+
+        for child in children:
+            self.add_child(child)
+
+
+    def __repr__(self):
+        if self.is_dir():
+            info = f"Parent: {self._parent}, Children: {self.size}"
+        else:
+            info = f"Offset: {self._fileoffset}, Size: {self.size}, Parent: {self._parent}"
+
+        return f"{self.__class__.__name__}<Type: {self.type}, {info}>"
+
+    @classmethod
+    def file(cls, name: str, parent: FSTNode = None, size: int = None, offset: int = None):
+        node = cls(name, FSTNode.FILE, parent=parent)
+        node._filesize = size
+        node._fileoffset = offset
+        return node
+
+    @classmethod
+    def folder(cls, name: str, parent: FSTNode = None, children: tuple = ()):
+        return cls(name, FSTNode.FOLDER, parent=parent, children=children)
+
+    @classmethod
+    def from_path(cls, path: Path) -> FSTNode:
+        if path.is_file():
+            node = cls.file(path.name, size=path.stat().st_size())
+        elif path.is_dir():
+            node = cls.folder(path.name, children=[cls.from_path(f) for f in path.iterdir()])
+        else:
+            raise NotImplementedError(
+                "Initializing a node using anything other than a file or folder is not allowed")
+        return node
+
+    @classmethod
+    def empty(cls):
+        return cls("")
+
+    @property
+    def path(self) -> str:
+        path = self.name
+        parent = self.parent
+        while parent is not None:
+            if parent.is_root():
+                break
+
+            path = f"{parent.name}/{path}"
+            parent = parent.parent
+
+        return path
+
+    @property
+    def dirs(self) -> FSTNode:
+        for node in self.children:
+            if node.is_dir():
+                yield node
+
+    @property
+    def files(self) -> FSTNode:
+        for node in self.children:
+            if node.is_file():
+                yield node
+
+    def rdirs(self, includedOnly: bool = False) -> FSTNode:
+        for node in self.children:
+            if includedOnly and node._exclude:
+                continue
+            
+            if node.is_dir():
+                yield node
+                yield from node.rdirs(includedOnly=includedOnly)
+
+    def rfiles(self, includedOnly: bool = False) -> FSTNode:
+        for node in self.children:
+            if includedOnly and node._exclude:
+                continue
+
+            if node.is_file():
+                yield node
+            else:
+                yield from node.rfiles(includedOnly=includedOnly)
+
+    def rchildren(self, includedOnly: bool = False) -> FSTNode:
+        for node in self.children:
+            if includedOnly and node._exclude:
+                continue
+            
+            yield node
+            yield from node.rchildren(includedOnly=includedOnly)
+
+    @property
+    def parent(self) -> FSTNode:
+        return self._parent
+
+    @parent.setter
+    def parent(self, node: FSTNode):
+        if self._parent is not None:
+            if self.is_dir():
+                if node is not None:
+                    diff = node._id - self._dirparent
+                    self._dirparent = node._id
+                else:
+                    diff = -self._dirparent
+                    self._dirparent = 0
+                for child in node.children:
+                    child._id += diff
+            self._parent.remove_child(self)
+        else:
+            if node is not None:
+                self._dirparent = node._id
+            else:
+                self._dirparent = 0
+
+        if node is not None:
+            node._children[self.name] = self
+
+        self._parent = node
+
+    @property
+    def children(self):
+        for child in sorted(self._children.values(), key=lambda x: x.name.upper()):
+            yield child
+
+    @property
+    def rootnode(self) -> FSTRoot:
+        prev = self
+        parent = self.parent
+        while parent is not None:
+            prev = parent
+            parent = parent.parent
+        return prev
+
+    @property
+    def size(self) -> int:
+        if self.is_file():
+            return self._filesize
+        else:
+            return self.num_children()
+
+    @size.setter
+    def size(self, size: int):
+        if self.is_file():
+            self._filesize = size
+
+    @property
+    def datasize(self) -> int:
+        if self.is_file():
+            return self._filesize
+        else:
+            size = 0
+            for node in self.rfiles(includedOnly=True):
+                size += node.size
+            return size
+
+    def find_by_path(self, path: Union[Path, str], skipExcluded: bool = True) -> FSTNode:
+        _path = str(path).lower()
+        doGlob = "?" in _path or "*" in path
+
+        for node in self.rfiles(includedOnly=skipExcluded):
+            if doGlob:
+                if fnmatch(node.path, _path):
+                    return node
+            else:
+                if node.path.lower() == _path:
+                    return node
+        for node in self.rdirs(includedOnly=skipExcluded):
+            if doGlob:
+                if fnmatch(node.path, _path):
+                    return node
+            else:
+                if node.path.lower() == _path:
+                    return node
+
+    def add_child(self, node: FSTNode):
+        self._children[node.name] = node
+        node.parent = self
+
+    def remove_child(self, node: FSTNode):
+        self._children.pop(node.name)
+        node.parent = None
+
+    def num_children(self, skipExcluded: bool = True) -> int:
+        counter = 0
+        for _ in self.rchildren(includedOnly=skipExcluded):
+            counter += 1
+        return counter
+
+    def destroy(self):
+        self.parent = None
+        for child in self.children:
+            self.remove_child(child)
+
+    def is_dir(self) -> bool:
+        return self.type == FSTNode.FOLDER
+
+    def is_file(self) -> bool:
+        return self.type == FSTNode.FILE
+
+    def is_root(self) -> bool:
+        return self.type == FSTNode.FOLDER and self.name == "files" and self.parent == None
+
+    def __eq__(self, other: FSTNode) -> bool:
+        return self.name == other.name and self.type == other.type
+
+    def __ne__(self, other: FSTNode) -> bool:
+        return self.name != other.name or self.type != other.type
+
+    def __len__(self) -> int:
+        if self.is_file():
+            return self._filesize
+        else:
+            return self.num_children() + 1
+
+    def __bool__(self) -> bool:
+        return True
+
+    def __contains__(self, other: Union[FSTNode, Path]) -> bool:
+        if isinstance(other, FSTNode):
+            for child in self.children:
+                if child == other:
+                    return True
+            return False
+        else:
+            if self.find_by_path(other):
+                return True
+            else:
+                return False
+
+
+class FSTRoot(FSTNode):
+    def __init__(self):
+        super().__init__("files", FSTNode.FOLDER)
+        self._id = 0
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}<{self.num_children()} entries>"
+
+    def nodes_by_offset(self, reverse: bool = False) -> FSTNode:
+        for node in sorted(self.rfiles(), key=lambda x: x._fileoffset, reverse=reverse):
+            yield node
+
+    def _detect_alignment(self, node: FSTNode, prev: FSTNode = None) -> int:
+        if prev:
+            offset = node._fileoffset - (prev._fileoffset + prev.size)
+        else:
+            offset = node._fileoffset
+
+        if offset == 0:
+            return 4
+
+        alignment = 4
+        mask = 0x7FFF
+        for _ in range(13):
+            if (node._fileoffset & mask) == 0:
+                alignment = mask + 1
+                break
+            mask >>= 1
+
+        mask = 0x7FFF
+        found = False
+        for _ in range(13):
+            if (offset & mask) == 0:
+                if mask + 1 <= alignment:
+                    alignment = mask + 1
+                    found = True
+                    break
+                else:
+                    found = True
+                    break
+            mask >>= 1
+
+        if not found:
+            return 4
+        return alignment
+
+
+class FST(FSTRoot):
+
+    def __init__(self):
+        super().__init__()
+
+    @property
+    def strTableOfs(self) -> int:
+        return len(self) * 0xC
+
+    def print_info(self, fst=None):
+        def print_tree(node: FSTNode, string: str, depth: int) -> str:
+            if node.is_file():
+                string += "  "*depth + node.name + "\n"
+            else:
+                string += "  "*depth + \
+                    f"{node.name} ({node._dirparent}, {node._dirnext})\n" + \
+                    "  "*depth + "{\n"
+                for child in node.children:
+                    string = print_tree(child, string, depth + 1)
+                string += "  "*depth + "}\n"
+
+            return string
+
+        print(self)
+        print("-"*32)
+
+        string = ""
+        for child in self.children:
+            string = print_tree(child, string, 0)
+
+        print(string)
