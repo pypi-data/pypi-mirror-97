229d4fd21e471c3190e1a5d8221d3f899921a561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:32 +0000

    Add package zero-two/0.0.1/zero-two-0.0.1.tar.gz

---
+++ packages/zero-two/zero-two-0.0.1.tar.gz/zero-two-0.0.1/README.md
@@ -0,0 +1,3 @@
+# zero-two
+
+Arbitrary library used for testing publishing a python package using poetry.
---
+++ packages/zero-two/zero-two-0.0.1.tar.gz/zero-two-0.0.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['zero_two']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'zero-two',
+    'version': '0.0.1',
+    'description': 'Zero Two',
+    'long_description': '# zero-two\n\nArbitrary library used for testing publishing a python package using poetry.\n',
+    'author': 'Michael Kim',
+    'author_email': 'kim.michael95@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
