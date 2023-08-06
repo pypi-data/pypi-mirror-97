2cf77a0ff363a2317f2419fc682ae12086e9c09b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:47 +0000

    Add package physunits/1.0.0/physunits-1.0.0-py3-none-any.whl

---
+++ packages/physunits/physunits-1.0.0-py3-none-any.whl/physunits/__init__.py
@@ -0,0 +1,11 @@
+from .length import *
+from .angle import *
+from .mass import *
+from .frequency import *
+from .time_units import *
+from .temperature import *
+from .electromagnetic import*
+from .pressure import *
+from .energy import *
+from .power import *
+from .other import *
