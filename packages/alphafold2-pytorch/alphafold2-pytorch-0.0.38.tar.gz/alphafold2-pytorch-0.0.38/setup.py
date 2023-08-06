4c95e171ac0560c691f1b64c763b866d696c3bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:57 +0000

    Add package alphafold2-pytorch/0.0.38/alphafold2-pytorch-0.0.38.tar.gz

---
+++ packages/alphafold2-pytorch/alphafold2-pytorch-0.0.38.tar.gz/alphafold2-pytorch-0.0.38/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'alphafold2-pytorch',
+  packages = find_packages(),
+  version = '0.0.38',
+  license='MIT',
+  description = 'AlphaFold2 - Pytorch',
+  author = 'Phil Wang, Eric Alcaide',
+  author_email = 'lucidrains@gmail.com, ericalcaide1@gmail.com',
+  url = 'https://github.com/lucidrains/alphafold2',
+  keywords = [
+    'artificial intelligence',
+    'attention mechanism',
+    'protein folding'
+  ],
+  install_requires=[
+    'einops>=0.3',
+    'lie-transformer-pytorch',
+    'numpy',
+    'torch>=1.6',
+    'mdtraj>=1.8',
+    'tqdm',
+    'se3-transformer-pytorch'
+  ],
+  setup_requires=[
+    'pytest-runner',
+  ],
+  tests_require=[
+    'pytest'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
