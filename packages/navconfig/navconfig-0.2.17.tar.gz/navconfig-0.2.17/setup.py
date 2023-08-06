cfe68d85c8c4fce9605268c9909f049102a63d49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:03 +0000

    Add package navconfig/0.2.17/navconfig-0.2.17.tar.gz

---
+++ packages/navconfig/navconfig-0.2.17.tar.gz/navconfig-0.2.17/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""NavConfig.
+
+    Configuration Service for Navigator and DataIntegrator.
+See:
+https://github.com/phenobarbital/NavConfig
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='navconfig',
+    version=open("VERSION").read().strip(),
+    python_requires=">=3.7.0",
+    url='https://github.com/phenobarbital/NavConfig',
+    description='Configuration tool for Navigator Services',
+    long_description='Configuration tool for Navigator Services',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3.7',
+    ],
+    author='Jesus Lara',
+    author_email='jlara@trocglobal.com',
+    packages=find_packages(),
+    install_requires=[
+        'asyncio==3.4.3',
+        'python-dotenv==0.15.0',
+        'configparser==5.0.1',
+        'PyDrive==1.3.1',
+        'pylibmc==1.6.1',
+        'objectpath==0.6.1',
+        'python-logstash-async==2.2.0',
+        'asyncdb'
+    ],
+    dependency_links=[
+        'git+https://github.com/phenobarbital/asyncdb.git@master#egg=asyncdb'
+    ],
+    project_urls={  # Optional
+        'Source': 'https://github.com/phenobarbital/NavConfig',
+        'Funding': 'https://paypal.me/phenobarbital',
+        'Say Thanks!': 'https://saythanks.io/to/phenobarbital',
+    },
+)
