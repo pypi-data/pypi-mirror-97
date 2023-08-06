e97cd958bc2a2b820c1899040b12f401eb4c5337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:42 +0000

    Add package memprofiler/0.1.3/memprofiler-0.1.3.tar.gz

---
+++ packages/memprofiler/memprofiler-0.1.3.tar.gz/memprofiler-0.1.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+CLASSIFIERS = """\
+Development Status :: 5 - Production/Stable
+Environment :: Console
+Intended Audience :: Developers
+License :: OSI Approved :: BSD License
+Natural Language :: English
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Software Development :: Libraries :: Python Modules
+"""
+
+
+setup(
+    name="memprofiler",
+    description="A simple IPython extension for monitoring memory usage of Jupyter notebook cells.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="BSD (3-clause)",
+    version="0.1.3",
+    author="Aleix Alcacer",
+    author_email="aleixalcacer@gmail.com",
+    url="https://github.com/aleixalcacer/memprofiler",
+    packages=["memprofiler"],
+    install_requires=["psutil", "ipython", "plotly"],
+    python_requires=">=3.4",
+    classifiers=list(filter(None, CLASSIFIERS.split("\n"))),
+)
