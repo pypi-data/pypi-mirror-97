b8e8f645653345d3fe75117619c6279468860ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:43 +0000

    Add package nehushtan/0.2.14/nehushtan-0.2.14.tar.gz

---
+++ packages/nehushtan/nehushtan-0.2.14.tar.gz/nehushtan-0.2.14/tests/logger/test-for-file-logger.py
@@ -0,0 +1,17 @@
+import logging
+
+from nehushtan.logger.NehushtanFileLogger import NehushtanFileLogger
+
+if __name__ == '__main__':
+    file_logger = NehushtanFileLogger('test', '/Users/leqee/code/nehushtan/log', log_level=logging.INFO)
+
+    file_logger.critical('C', {"A": "B"})
+    file_logger.error('E', 111)
+    file_logger.warning('W', 'dd')
+    file_logger.info('I', file_logger)
+    file_logger.debug('D')
+
+    try:
+        raise ValueError('A<>B')
+    except Exception as error:
+        file_logger.exception('X', error)
