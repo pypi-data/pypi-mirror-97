832f8cfe498e52bd94e648be2278ad38351ff389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:10 +0000

    Add package atframework/0.1.4/atframework-0.1.4-py3-none-any.whl

---
+++ packages/atframework/atframework-0.1.4-py3-none-any.whl/atframework/web/common/model/BoHelpdesk.py
@@ -0,0 +1,10 @@
+
+from framework.web.common.model.Model import Model
+
+class BoHelpdesk(Model):
+    '''
+    Inherit the basic GUI action, and expand some methods on BO helpdesk
+    '''
+
+    def openHelpdeskPage(self, css_selector):
+        self._clickLinkByCSS(css_selector)
