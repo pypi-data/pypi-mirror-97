4b6405f03c42549d97842f6b4aa99944d3c86d12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:35 +0000

    Add package sretoolbox/0.12.1/sretoolbox-0.12.1-py3-none-any.whl

---
+++ packages/sretoolbox/sretoolbox-0.12.1-py3-none-any.whl/sretoolbox/container/__init__.py
@@ -0,0 +1,8 @@
+"""
+Exposes the container libs for easy access.
+"""
+
+from sretoolbox.container.image import Image
+from sretoolbox.container.skopeo import Skopeo
+
+__all__ = ['Image', 'Skopeo']
