a9029d1fe3b656d15a60871e275397c6cf9993a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:27 +0000

    Add package fastmat/0.2.post0/fastmat-0.2.post0-cp38-cp38-manylinux1_x86_64.whl

---
+++ packages/fastmat/fastmat-0.2.post0-cp38-cp38-manylinux1_x86_64.whl/fastmat/algorithms/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2018 Sebastian Semper, Christoph Wagner
+#     https://www.tu-ilmenau.de/it-ems/
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+r"""
+We provide some algorithms, which make use of the speedups provided by fastmat
+to allow easy production use out of the box.
+"""
+# algorithms for sparse recovery
+
+from .Algorithm import Algorithm
+from .ISTA import ISTA
+from .OMP import OMP
+from .FISTA import FISTA
+from .STELA import STELA
