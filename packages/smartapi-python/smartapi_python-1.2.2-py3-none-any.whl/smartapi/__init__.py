737975c2d368420bef89e7a8423f55a8a69fbad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:06 +0000

    Add package smartapi-python/1.2.2/smartapi_python-1.2.2-py3-none-any.whl

---
+++ packages/smartapi-python/smartapi_python-1.2.2-py3-none-any.whl/smartapi/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals,absolute_import
+
+from smartapi.smartConnect import SmartConnect
+from smartapi.webSocket import WebSocket
+
+__all__ = ["SmartConnect","WebSocket"]
+
+
+
+
