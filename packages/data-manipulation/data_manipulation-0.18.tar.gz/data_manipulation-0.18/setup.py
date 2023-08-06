4a72affe74d5583e595924daa5f220638287aeeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:31 +0000

    Add package data-manipulation/0.18/data_manipulation-0.18.tar.gz

---
+++ packages/data-manipulation/data_manipulation-0.18.tar.gz/data_manipulation-0.18/setup.py
@@ -0,0 +1,45 @@
+from setuptools import find_packages, setup
+
+import versioneer
+
+LONG_DESCRIPTION = """
+Data Manipulation is a Python package providing powerful utility functions. 
+It contains many subpackages with utility functions built for popular packages such as Pandas, PySpark and many more.
+"""
+
+CLASSIFIERS = [
+    "Development Status :: 3 - Alpha",
+    "Environment :: Console",
+    "Operating System :: OS Independent",
+    "Intended Audience :: Science/Research",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Cython",
+    "Topic :: Scientific/Engineering",
+]
+
+DEPENDENCIES = [
+    "beautifulsoup4", "django", "numpy", "pandas", "IPython", "requests", "psycopg2", "pyspark",
+]
+
+setup(
+    author="Shawn Ng",
+    author_email="shawn.coding.acc@gmail.com",
+    classifiers=CLASSIFIERS,
+    cmdclass=versioneer.get_cmdclass(),
+    description="Powerful data manipulation",
+    install_requires=DEPENDENCIES,
+    license="BSD-3",
+    long_description=LONG_DESCRIPTION,
+    name="data_manipulation",
+    packages=find_packages(include=["data_manipulation", "data_manipulation.*"]),
+    platforms="any",
+    test_suite="nose.collector",
+    tests_require=["nose"],
+    url="https://github.com/shawnngtq/data-manipulation",
+    version=versioneer.get_version(),
+)
