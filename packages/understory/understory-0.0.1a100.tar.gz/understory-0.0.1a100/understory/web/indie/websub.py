694cb7c32deb28b4cd0693d43d6ec36eb2a5eb88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:48 +0000

    Add package understory/0.0.1a100/understory-0.0.1a100.tar.gz

---
+++ packages/understory/understory-0.0.1a100.tar.gz/understory-0.0.1a100/understory/web/indie/websub.py
@@ -0,0 +1,41 @@
+"""WebSub hub and subscriber apps."""
+
+from understory import web
+from understory.web import tx
+
+
+hub = web.application("WebSubHub", mount_prefix="hub")
+subs = web.application("WebSubSubs", mount_prefix="subs")
+
+
+def wrap_hub(handler, app):
+    """Ensure server links are in head of root document."""
+    yield
+    if tx.request.uri.path == "" and tx.response.body:
+        doc = web.parse(tx.response.body)
+        try:
+            head = doc.select("head")[0]
+        except IndexError:
+            pass
+        else:
+            head.append("<link rel=self href=/>")
+            head.append("<link rel=hub href=/hub>")
+            tx.response.body = doc.html
+        web.header("Link", '</>; rel="self"', add=True)
+        web.header("Link", '</hub>; rel="hub"', add=True)
+
+
+@hub.route(r"")
+class Hub:
+    """."""
+
+    def _get(self):
+        return "hub.."
+
+
+@subs.route(r"")
+class Subscriptions:
+    """."""
+
+    def _get(self):
+        return "subscriptions.."
