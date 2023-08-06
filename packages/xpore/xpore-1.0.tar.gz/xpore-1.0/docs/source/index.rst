fb07f815c2a364ceac800dc6b4f58ee8e10b2a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:11 +0000

    Add package xpore/1.0/xpore-1.0.tar.gz

---
+++ packages/xpore/xpore-1.0.tar.gz/xpore-1.0/docs/source/index.rst
@@ -0,0 +1,38 @@
+.. xpore documentation master file, created by
+   sphinx-quickstart on Sat Dec 21 06:31:05 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to xPore's documentation!
+=================================
+xPore is a Python package for detection of differentail RNA modifications from Nanopore sequencing data.
+
+To install the latest release, run::
+
+    pip install xpore
+    pyensembl install --release 91 --species homo_sapiens  # Please specify the compatible Ensembl release with your data when you install it.
+
+See our :ref:`Installation page <installation>` for details.
+
+To detect differential modifications, you can follow the instructions in our :ref:`Quickstart page <quickstart>`. 
+
+Contents
+------------------
+.. toctree::
+   :maxdepth: 1
+   
+   installation
+   quickstart
+   outputtable
+   configuration
+   preparation
+   scripts
+
+Contacts
+--------
+xPore is maintained by `Ploy N. Pratanwanich <https://github.com/ploy-rukawa>`_ and `Jonathan Goeke <https://github.com/jonathangoeke>`_ from the Genome Institute of Singapore, A*STAR. 
+
+If you want to contribute, please leave an issue in `our repo <https://github.com/GoekeLab/xpore>`_
+
+Thank you!
+
---
+++ packages/xpore/xpore-1.0.tar.gz/xpore-1.0/xpore/diffmod/configurator.py
@@ -0,0 +1,78 @@
+import yaml
+import os
+from collections import defaultdict
+
+from ..utils import misc
+
+def get_condition_run_name(condition_name,run_name):
+    return '-'.join([condition_name,run_name])
+
+class Configurator(object):
+    def __init__(self, config_filepath):
+        self.filepath = os.path.abspath(config_filepath)
+        self.filename = self.filepath.split('/')[-1]
+        self.yaml = yaml.safe_load(open(self.filepath, 'r'))
+        
+    def get_paths(self):
+        paths = {}
+        
+        if 'prior' in self.yaml:
+            paths['model_kmer'] = os.path.abspath(self.yaml['prior'])
+        else:
+            paths['model_kmer'] = os.path.join(os.path.dirname(__file__),'model_kmer.csv')
+
+        paths['out_dir'] = os.path.join(os.path.abspath(self.yaml['out']))
+        paths.update(misc.makedirs(paths['out_dir'],sub_dirs=['models']))
+        paths['model_filepath'] = os.path.join(paths['out_dir'], 'models', '%s.model')        
+        return paths
+        
+    def get_data_info(self):
+        data = defaultdict(dict)
+        for condition_name, run_names in self.yaml['data'].items():
+            for run_name, dirpath in run_names.items():
+                data[condition_name][get_condition_run_name(condition_name,run_name)] = dirpath
+        return data
+    
+    def get_criteria(self):
+        criteria = {}
+        if 'criteria' in self.yaml.keys():
+            criteria = self.yaml['criteria']
+        else:
+            criteria['readcount_min'] = 15
+            criteria['readcount_max'] = 1000
+
+        return criteria
+        
+    def get_method(self):
+        if 'method' in self.yaml.keys():
+            method = self.yaml['method']
+        else:
+            method = {}
+
+        method.setdefault('name', 'gmm')
+        method.setdefault('max_iters', 500)
+        method.setdefault('stopping_criteria', 0.00001)
+        method.setdefault('compute_elbo', True)
+        method.setdefault('verbose', False)
+        method.setdefault('update', ['z','y','w','mu_tau'])
+        method.setdefault('pooling', False)
+        method.setdefault('prefiltering',False)
+        return method
+    
+    def get_priors(self):
+        prior_params = defaultdict(dict)
+        if 'priors' not in self.yaml.keys():
+            # mu_tau
+            prior_params['mu_tau']['location'] = ['model_kmer_mean','model_kmer_mean']
+            prior_params['mu_tau']['lambda'] = [1,1]
+            prior_params['mu_tau']['alpha'] = [0.5,0.5]
+            prior_params['mu_tau']['beta'] = ['model_kmer_tau','model_kmer_tau']
+            prior_params['mu_tau']['beta_scale'] = [0.5,0.5]
+
+            # w
+            prior_params['w']['concentration'] = [0.001,0.001]        
+        else:
+            pass #todo
+
+        return prior_params
+        
