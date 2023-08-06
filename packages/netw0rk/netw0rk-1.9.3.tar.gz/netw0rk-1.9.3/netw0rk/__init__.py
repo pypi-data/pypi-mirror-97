bbf293d2781d771b5fb0fdd67008b4447f96f615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:41 +0000

    Add package netw0rk/1.9.3/netw0rk-1.9.3.tar.gz

---
+++ packages/netw0rk/netw0rk-1.9.3.tar.gz/netw0rk-1.9.3/netw0rk/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+from netw0rk.classes import *
+
+# source path & version.
+from dev0s import Version, Directory, Files, gfp
+source = Directory(gfp.base(__file__))
+base = Directory(source.fp.base())
+try: version = Version(Files.load(source.join(".version.py")))
+except: version = None
