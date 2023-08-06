0d17eeb71bf9b0f8c801d510feff9b6feb2b202d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:25 +0000

    Add package pettingzoo/1.6.1/PettingZoo-1.6.1.tar.gz

---
+++ packages/pettingzoo/PettingZoo-1.6.1.tar.gz/PettingZoo-1.6.1/pettingzoo/sisl/__init__.py
@@ -0,0 +1,14 @@
+from pettingzoo.utils.deprecated_module import DeprecatedModule
+
+multiwalker_v0 = DeprecatedModule("multiwalker", "v0", "v6")
+multiwalker_v1 = DeprecatedModule("multiwalker", "v1", "v6")
+multiwalker_v2 = DeprecatedModule("multiwalker", "v2", "v6")
+multiwalker_v3 = DeprecatedModule("multiwalker", "v3", "v6")
+multiwalker_v4 = DeprecatedModule("multiwalker", "v4", "v6")
+multiwalker_v5 = DeprecatedModule("multiwalker", "v5", "v6")
+pursuit_v0 = DeprecatedModule("pursuit", "v0", "v3")
+pursuit_v1 = DeprecatedModule("pursuit", "v1", "v3")
+pursuit_v2 = DeprecatedModule("pursuit", "v2", "v3")
+waterworld_v0 = DeprecatedModule("waterworld", "v0", "v3")
+waterworld_v1 = DeprecatedModule("waterworld", "v1", "v3")
+waterworld_v2 = DeprecatedModule("waterworld", "v2", "v3")
