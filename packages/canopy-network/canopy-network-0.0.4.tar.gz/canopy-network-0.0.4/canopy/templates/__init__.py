94a2a57ac195dec029331f489fea3470907401fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:30 +0000

    Add package canopy-network/0.0.4/canopy-network-0.0.4.tar.gz

---
+++ packages/canopy-network/canopy-network-0.0.4.tar.gz/canopy-network-0.0.4/canopy/templates/__init__.py
@@ -0,0 +1,8 @@
+"""Template globals."""
+
+from pprint import pformat
+
+from understory.web import tx
+from understory.web.indie.micropub import discover_post_type
+
+__all__ = ["pformat", "tx", "discover_post_type"]
