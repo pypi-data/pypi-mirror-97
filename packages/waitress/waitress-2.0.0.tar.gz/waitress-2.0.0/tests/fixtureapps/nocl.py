56f81b80269f857a4b9a8745a5b32f178828f055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:48 +0000

    Add package waitress/2.0.0/waitress-2.0.0.tar.gz

---
+++ packages/waitress/waitress-2.0.0.tar.gz/waitress-2.0.0/tests/fixtureapps/nocl.py
@@ -0,0 +1,21 @@
+def chunks(l, n):  # pragma: no cover
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i : i + n]
+
+
+def gen(body):  # pragma: no cover
+    yield from chunks(body, 10)
+
+
+def app(environ, start_response):  # pragma: no cover
+    cl = environ.get("CONTENT_LENGTH", None)
+    if cl is not None:
+        cl = int(cl)
+    body = environ["wsgi.input"].read(cl)
+    start_response("200 OK", [("Content-Type", "text/plain")])
+    if environ["PATH_INFO"] == "/list":
+        return [body]
+    if environ["PATH_INFO"] == "/list_lentwo":
+        return [body[0:1], body[1:]]
+    return gen(body)
