3c8a2db528c3cea52e0edf5990e9ef0aeb76f836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:10 +0000

    Add package kiri/0.5.0.dev1/kiri-0.5.0.dev1.tar.gz

---
+++ packages/kiri/kiri-0.5.0.dev1.tar.gz/kiri-0.5.0.dev1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kiri",
+    version="0.5.0.dev1",
+    author="Kiri OÜ",
+    author_email="hello@kiri.ai",
+    description="Kiri Natural Language Engine",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kiri-ai/kiri",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "transformers>=4.3.2",
+        "sentence_transformers>=0.4.1.2",
+        "torch",
+        "torchvision",
+        "ftfy",
+        "pytorch_lightning==1.1.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
