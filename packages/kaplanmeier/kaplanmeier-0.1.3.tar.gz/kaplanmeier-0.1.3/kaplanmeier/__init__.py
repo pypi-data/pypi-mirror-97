76e91d7da12020d4d95047b3c89f25a7c5ed8bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:48 +0000

    Add package kaplanmeier/0.1.3/kaplanmeier-0.1.3.tar.gz

---
+++ packages/kaplanmeier/kaplanmeier-0.1.3.tar.gz/kaplanmeier-0.1.3/kaplanmeier/__init__.py
@@ -0,0 +1,19 @@
+from kaplanmeier.kaplanmeier import (
+    fit,
+	plot,
+	example_data,
+)
+
+__author__ = 'Erdogan Tasksen'
+__email__ = 'erdogant@gmail.com'
+__version__ = '0.1.3'
+
+# module level doc-string
+__doc__ = """
+kaplanmeier - This packages computes the log-rank P-value and computes survival curves based on kaplan meier
+=====================================================================
+
+**kaplanmeier** 
+See README.md file for more information.
+
+"""
