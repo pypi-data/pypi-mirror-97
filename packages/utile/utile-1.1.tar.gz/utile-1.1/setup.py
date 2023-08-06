51bb767ae104e6082484135ea086f4c2e7edc61d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:58 +0000

    Add package utile/1.1/utile-1.1.tar.gz

---
+++ packages/utile/utile-1.1.tar.gz/utile-1.1/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("readmepy.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="utile",  # Replace with your own username
+    version="v1.1",
+    author="Jofin F Archbald",
+    author_email="jofinfab@gmail.com",
+    description="The python package which eases your <codeflow> using decorators",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/j0fiN/utile.git",
+    packages=setuptools.find_packages(),
+    project_urls={
+        'Documentation': 'https://utile.readthedocs.io/',
+        'Say Thanks!': 'https://jofin-f-archbald.herokuapp.com/',
+        'Source': 'https://github.com/j0fiN/utile/',
+    },
+    include_package_data=True,
+    package_data = {
+        '': ['*.pyd']
+        },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'timsortyer'
+    ]
+)
---
+++ packages/utile/utile-1.1.tar.gz/utile-1.1/utile/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+The python package which eases your <codeflow> using @decorators.
+
+Sorter.sort(List)
+    sorts the list in ascending order.
+
+@timer(store=False, round_off=10)
+    Decorator which print execution time of any function.
+
+@threader(funcs, func_result=False)
+    A Frame-Determined decorator to spring up number of I/O bound tasks.
+
+@processor(funcs, func_result=False, get_result=False)
+    A Frame-Determined decorator to spring up number of CPU bound tasks.
+
+For more info: https://pypi.org/project/utile/
+Author : JOFIN F ARCHBALD
+"""
+from .Processor import processor
+from .Threader import threader
+from .Timer import timer
+from .Sorter import sort
+# from .timsortyer import *
