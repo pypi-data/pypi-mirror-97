511605673bb7eb353c0469e1c0a41f4de066f5c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:59 +0000

    Add package fkit/0.0.9/fkit-0.0.9.tar.gz

---
+++ packages/fkit/fkit-0.0.9.tar.gz/fkit-0.0.9/src/config.py
@@ -0,0 +1,24 @@
+import os
+
+version = '0.0.9'
+
+# 开发
+login_base_url_develop = 'http://develop.tentech.com.cn:8080'
+cloud_base_url_develop = 'http://develop.tentech.com.cn:8081'
+docker_registry_develop = 'develop.tentech.com.cn:5000'
+
+# 测试
+login_base_url_stage = 'http://stage.tentech.com.cn:8080'
+cloud_base_url_stage = 'http://stage.tentech.com.cn:8081'
+docker_registry_stage = 'stage.tentech.com.cn:5000'
+
+# 生产
+login_base_url_master = 'http://www.flowhub.com.cn:8080'
+cloud_base_url_master = 'http://www.flowhub.com.cn:8081'
+docker_registry_master = 'tool.flowhub.com.cn:5000'
+
+cli_dir = '.cli'
+USER_BASE_PATH = os.path.expanduser('~')
+cli_json = '.cli.json'
+
+oss_file_path = 'file/'
