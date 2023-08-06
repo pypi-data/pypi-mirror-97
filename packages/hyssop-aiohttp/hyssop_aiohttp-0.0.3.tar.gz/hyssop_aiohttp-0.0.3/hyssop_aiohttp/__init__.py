cf2e71262dc3812c3319ff6c88d746f51600e16a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:09 +0000

    Add package hyssop-aiohttp/0.0.3/hyssop_aiohttp-0.0.3.tar.gz

---
+++ packages/hyssop-aiohttp/hyssop_aiohttp-0.0.3.tar.gz/hyssop_aiohttp-0.0.3/hyssop_aiohttp/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2020-Present the hyssop authors and contributors.
+#
+# This module is part of hyssop and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+
+'''
+File created: November 21st 2020
+
+Modified By: hsky77
+Last Updated: March 6th 2021 17:42:53 pm
+'''
+
+from .server import AioHttpView, AioHttpRequest, AioHttpApplication, AioHttpServer, routes
+
+Version = '0.0.3'
