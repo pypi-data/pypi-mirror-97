1601d974589d30d5c0fd75dd24d6449bf7fd672c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:49 +0000

    Add package pythondata-cpu-serv/1.0.post190/pythondata_cpu_serv-1.0.post190-py3-none-any.whl

---
+++ packages/pythondata-cpu-serv/pythondata_cpu_serv-1.0.post190-py3-none-any.whl/pythondata_cpu_serv/__init__.py
@@ -0,0 +1,50 @@
+import os.path
+__dir__ = os.path.split(os.path.abspath(os.path.realpath(__file__)))[0]
+data_location = os.path.join(__dir__, "verilog")
+src = "https://github.com/olofk/serv"
+
+# Module version
+version_str = "1.0.post190"
+version_tuple = (1, 0, 190)
+try:
+    from packaging.version import Version as V
+    pversion = V("1.0.post190")
+except ImportError:
+    pass
+
+# Data version info
+data_version_str = "1.0.post96"
+data_version_tuple = (1, 0, 96)
+try:
+    from packaging.version import Version as V
+    pdata_version = V("1.0.post96")
+except ImportError:
+    pass
+data_git_hash = "7624466ddd552dbd8c94127149527638dee3ca37"
+data_git_describe = "v1.0-96-g7624466"
+data_git_msg = """\
+commit 7624466ddd552dbd8c94127149527638dee3ca37
+Author: Olof Kindgren <olof.kindgren@gmail.com>
+Date:   Sun Feb 14 22:13:49 2021 +0100
+
+    Optimize serv_rf_ram_if
+
+"""
+
+# Tool version info
+tool_version_str = "0.0.post94"
+tool_version_tuple = (0, 0, 94)
+try:
+    from packaging.version import Version as V
+    ptool_version = V("0.0.post94")
+except ImportError:
+    pass
+
+
+def data_file(f):
+    """Get absolute path for file inside pythondata_cpu_serv."""
+    fn = os.path.join(data_location, f)
+    fn = os.path.abspath(fn)
+    if not os.path.exists(fn):
+        raise IOError("File {f} doesn't exist in pythondata_cpu_serv".format(f))
+    return fn
