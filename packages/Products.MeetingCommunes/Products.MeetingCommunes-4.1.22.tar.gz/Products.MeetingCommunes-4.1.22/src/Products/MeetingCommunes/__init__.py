6fffe5ed0759a568dc506bcfbbd8b6f6a9619ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:10 +0000

    Add package Products.MeetingCommunes/4.1.22/Products.MeetingCommunes-4.1.22.tar.gz

---
+++ packages/Products.MeetingCommunes/Products.MeetingCommunes-4.1.22.tar.gz/Products.MeetingCommunes-4.1.22/src/Products/MeetingCommunes/__init__.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#
+# File: MeetingCommunes.py
+#
+# Copyright (c) 2016 by Imio.be
+# Generator: ArchGenXML Version 2.7
+#            http://plone.org/products/archgenxml
+#
+# GNU General Public License (GPL)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+
+__author__ = """Gauthier Bastien <g.bastien@imio.be>, Stephan Geulette <s.geulette@imio.be>"""
+__docformat__ = 'plaintext'
+
+
+# There are three ways to inject custom code here:
+#
+#   - To set global configuration variables, create a file AppConfig.py.
+#       This will be imported in config.py, which in turn is imported in
+#       each generated class and in this file.
+#   - To perform custom initialisation after types have been registered,
+#       use the protected code section at the bottom of initialize().
+
+import logging
+logger = logging.getLogger('MeetingCommunes')
+logger.debug('Installing Product')
+
+from Products.CMFCore import DirectoryView
+from config import product_globals
+
+DirectoryView.registerDirectory('skins', product_globals)
+
+import model.pm_updates  # noqa
+import adapters  # noqa
+
+
+def initialize(context):
+    """initialize product (called by zope)"""
