dc053e102e4f36a31bb32fabefba64c687577e8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:40 +0000

    Add package datastand/1.7/datastand-1.7.tar.gz

---
+++ packages/datastand/datastand-1.7.tar.gz/datastand-1.7/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='datastand',
+    packages=['datastand'],
+    version='1.7',
+    license='MIT  ',
+    author="Vincent Njonge",
+    author_email="njongevincent@gmail.com",
+    description="""A python package to help users especially Data Scientists, Machine Learning Engineers and 
+    Analysts to better understand a dataset. Gives quick insights about given data; general dataset statistics, 
+    size and shape of dataset, number of unique data types, number of numerical and non-numerical columns, small 
+    overview of dataset, data description, missing data statistics, missing data heatmap and provides methodology 
+    to impute missing data.""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lyraxvincent/datastand",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+ )
