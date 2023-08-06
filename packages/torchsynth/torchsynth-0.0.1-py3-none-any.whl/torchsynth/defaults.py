3fd64a1ee280dc75c3e2732b2ec8715c49e21a56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:06 +0000

    Add package torchsynth/0.0.1/torchsynth-0.0.1-py3-none-any.whl

---
+++ packages/torchsynth/torchsynth-0.0.1-py3-none-any.whl/torchsynth/defaults.py
@@ -0,0 +1,13 @@
+"""
+Default parameters
+"""
+SAMPLE_RATE = 44100
+
+# Number of samples for fixed length synthesis.
+BUFFER_SIZE = 88200
+
+# Small value to avoid log errors.
+EPSILON = 1e-6
+
+# Equal power coefficient. 1/sqrt(2)
+EQ_POW = 0.70710678118
