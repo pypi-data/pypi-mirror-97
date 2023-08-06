1d1ee1cb419e7bb94cae191ed1cc30d91d2e45ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:26 +0000

    Add package sipak/1.0/sipak-1.0-py3-none-any.whl

---
+++ packages/sipak/sipak-1.0-py3-none-any.whl/sipak/__init__.py
@@ -0,0 +1,11 @@
+#Pacakge For Sipak
+#Nothing much is here
+totalinfo = """My Name:- Sipak
+Age:- Who are You to see my Age
+Class:- Why will I Tell you
+My Fav Language:- MotherToungue, English, Hindi
+My Hobby:- Programming // Gaming"""
+name = "SIPAK"
+hobby = "Programming // Gaming"
+contact = "https://t.me/ItzSipak"
+__version__ = "v1.0"
