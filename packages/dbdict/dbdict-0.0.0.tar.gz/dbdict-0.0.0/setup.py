42ef0df40cb319594c901953849050df24701325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:39 +0000

    Add package dbdict/0.0.0/dbdict-0.0.0.tar.gz

---
+++ packages/dbdict/dbdict-0.0.0.tar.gz/dbdict-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='dbdict',
+    packages=["dbdict"],
+    version='0.0.0',
+    author='Perzan',
+    author_email='PerzanDevelopment@gmail.com',
+    install_requires=["onetrick~=2.1"]
+)
