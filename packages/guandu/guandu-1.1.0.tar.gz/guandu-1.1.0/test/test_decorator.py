e46583d41c4c4b29f1df9cfda1f1b366e945482d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:52 +0000

    Add package guandu/1.1.0/guandu-1.1.0.tar.gz

---
+++ packages/guandu/guandu-1.1.0.tar.gz/guandu-1.1.0/test/test_decorator.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+"""
+    @Time   : 2021-03-09 10:36
+    @Author : feikong
+"""
+
+
+# import unittest
+#
+#
+# class TestDecorator(unittest.TestCase):
+#
+#     def test_retry(self):
+#         from db_cmd.mysql import ExecuteMysql
+#         func = ExecuteMysql.create_new_report
+#         # 判断func是否是方法
+#         self.assertTrue(callable(func))
