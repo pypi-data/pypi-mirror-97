59d4e8be08a2a196d8ecc1a90f2d06f48b4c8036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:10 +0000

    Add package brevitas/0.3.1/brevitas-0.3.1.tar.gz

---
+++ packages/brevitas/brevitas-0.3.1.tar.gz/brevitas-0.3.1/src/brevitas/export/onnx/pyxir/dpuv2/manager.py
@@ -0,0 +1,25 @@
+from brevitas.export.onnx.base import ONNXBaseManager
+from ...transform import move_domain_attributes_into_domain
+from .handler import DPUv2QuantConv2dHandler, DPUv2QuantMaxPool2dHandler
+from ..handler import DPUQuantReLUHandler, DPUQuantEltwiseAddHandler
+from ..handler import DPUQuantAvgPool2dHandler, DPUQuantLinearHandler
+
+
+class DPUv2Manager(ONNXBaseManager):
+
+    handlers = [
+        DPUQuantReLUHandler,
+        DPUQuantEltwiseAddHandler,
+        DPUQuantAvgPool2dHandler,
+        DPUQuantLinearHandler,
+        DPUv2QuantConv2dHandler,
+        DPUv2QuantMaxPool2dHandler]
+
+    model_transforms = [
+        move_domain_attributes_into_domain]
+
+    onnx_passes = [
+        # use initializers instead of Constant nodes for fixed params
+        "extract_constant_to_initializer",
+        # remove unused graph inputs & initializers
+        "eliminate_unused_initializer"]
