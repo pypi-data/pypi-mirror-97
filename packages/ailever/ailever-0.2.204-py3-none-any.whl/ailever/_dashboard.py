6ef2fa37be7bb6fcf5bf7ad3c79e97054bd45b6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:51 +0000

    Add package ailever/0.2.204/ailever-0.2.204-py3-none-any.whl

---
+++ packages/ailever/ailever-0.2.204-py3-none-any.whl/ailever/_dashboard.py
@@ -0,0 +1,28 @@
+import os
+import signal
+from multiprocessing import Queue, Process
+import time
+
+queue = Queue()
+
+def remover(name, sustain, queue=queue):
+    if sustain:
+        time.sleep(sustain)
+        os.remove(f'./{name}.py')
+        while not queue.empty():
+            os.kill(queue.get(), signal.SIGTERM)
+
+def _dashboard(name, host='127.0.0.1', port='8050', queue=queue):
+    if not os.path.isfile(f'{name}.py'):
+        urlretrieve('https://raw.githubusercontent.com/ailever/openapi/master/main.py', f'./main.py')
+        print(f'[AILEVER] The file "{name}.py" is downloaded!')
+    queue.put(os.getpid())
+    os.system(f'python {name}.py --ds {host} --dp {port}')
+
+def dashboard(name, host='127.0.0.1', port='8050', sustain=None, queue=queue):
+    proc1 = Process(target=remover, args=(name, sustain, queue, ))
+    proc2 = Process(target=_dashboard, args=(name, host, port, queue, ))
+    proc1.start()
+    proc2.start()
+    proc1.join()
+    proc2.join()
