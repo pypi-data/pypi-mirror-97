d60e9752a1a949441e73196a57953f4bab90158b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:23 +0000

    Add package gdal2numpy/0.0.27/gdal2numpy-0.0.27.tar.gz

---
+++ packages/gdal2numpy/gdal2numpy-0.0.27.tar.gz/gdal2numpy-0.0.27/gdal2numpy/__init__.py
@@ -0,0 +1,27 @@
+#-------------------------------------------------------------------------------
+# Licence:
+# Copyright (c) 2012-2020 Valerio for Gecosistema S.r.l.
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+#
+# Name:        module.py
+# Purpose:
+#
+# Author:      Luzzi Valerio
+#
+# Created:
+#-------------------------------------------------------------------------------
+__version__ = '0.0.27'
+
+from .module import *
