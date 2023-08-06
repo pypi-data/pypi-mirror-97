7c799a029f876b745576f7e97170eaea33649654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:32 +0000

    Add package django-daylessdate/0.4/django_daylessdate-0.4-py3-none-any.whl

---
+++ packages/django-daylessdate/django_daylessdate-0.4-py3-none-any.whl/djangodaylessdate/filters.py
@@ -0,0 +1,53 @@
+from django.contrib import messages
+from django.contrib.admin import FieldListFilter
+from django.core.exceptions import ValidationError
+from django.contrib.admin.options import IncorrectLookupParameters
+
+from .helpers import DaylessDate
+
+class DaylessDateFilter(FieldListFilter):
+    parameter_name = None
+
+    def __init__(self, field, request, params, model, model_admin, field_path):
+        super().__init__(field, request, params, model, model_admin, field_path)
+        if self.parameter_name is None:
+            self.parameter_name = self.field.name
+
+        if self.parameter_name in params:
+            value = params.pop(self.parameter_name)
+            try:
+                DaylessDate(*value.split('/'))
+                self.used_parameters[self.parameter_name] = value
+            except:
+                messages.warning(request, 'Invalid data')
+
+    def get_template(self):
+        return 'djangodaylessdate/daylessdate_filter.html'
+
+    template = property(get_template)
+
+    def queryset(self, request, queryset):
+        query_params = {k: v for k, v in self.used_parameters.items() if k != self.parameter_name}
+        if self.parameter_name in self.used_parameters:
+            try:
+                query_params[self.parameter_name] = DaylessDate(*self.used_parameters[self.parameter_name].split('/'))
+            except:
+                pass
+        try:
+            return queryset.filter(**query_params)
+        except (ValueError, ValidationError) as e:
+            raise IncorrectLookupParameters(e)
+
+    def has_output(self):
+        return True
+
+    def expected_parameters(self):
+        return [self.parameter_name]
+
+    def choices(self, changelist):
+        yield {
+            'get_query': changelist.params,
+            'current_value': self.used_parameters.get(self.parameter_name),
+            'query_string': changelist.get_query_string({}, [self.parameter_name]),
+            'parameter_name': self.parameter_name
+        }
