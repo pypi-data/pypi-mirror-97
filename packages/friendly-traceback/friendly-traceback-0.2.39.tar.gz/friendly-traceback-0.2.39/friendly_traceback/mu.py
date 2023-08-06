7d9c18e67bc79952f34a91b8caa3b87bbd108d3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:00 +0000

    Add package friendly-traceback/0.2.39/friendly-traceback-0.2.39.tar.gz

---
+++ packages/friendly-traceback/friendly-traceback-0.2.39.tar.gz/friendly-traceback-0.2.39/friendly_traceback/mu.py
@@ -0,0 +1,19 @@
+import sys
+
+exc_hook_name = repr(sys.excepthook)
+
+if "InteractiveShell" in exc_hook_name:
+    from .ipython import *  # noqa
+
+else:
+    from friendly_traceback.console_helpers import *  # noqa
+    from friendly_traceback.console_helpers import helpers  # noqa
+    from friendly_traceback import install, run  # noqa
+
+    __all__ = list(helpers.keys())
+    __all__.append("install")
+    __all__.append("run")
+
+
+del exc_hook_name
+del sys
