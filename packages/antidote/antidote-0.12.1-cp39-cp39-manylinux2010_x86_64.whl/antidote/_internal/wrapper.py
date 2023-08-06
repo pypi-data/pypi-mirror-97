f519f8aa007e1a7dbf09881a2913cd255652d73b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:18 +0000

    Add package antidote/0.12.1/antidote-0.12.1-cp39-cp39-manylinux2010_x86_64.whl

---
+++ packages/antidote/antidote-0.12.1-cp39-cp39-manylinux2010_x86_64.whl/antidote/_internal/wrapper.py
@@ -0,0 +1,209 @@
+import functools
+import inspect
+from typing import Awaitable, Callable, Dict, Hashable, List, Sequence, Union, cast
+
+from . import API
+from .state import current_container
+from .utils import FinalImmutable
+from ..core.container import Container
+from ..core.exceptions import DependencyNotFoundError
+
+compiled = False
+
+
+@API.private
+class Injection(FinalImmutable):
+    """
+    Maps an argument name to its dependency and if the injection is required,
+    which is equivalent to no default argument.
+    """
+    __slots__ = ('arg_name', 'required', 'dependency')
+    arg_name: str
+    required: bool
+    dependency: Hashable
+
+
+@API.private
+class InjectionBlueprint(FinalImmutable):
+    """
+    Stores all the injections for a function.
+    """
+    __slots__ = ('injections',)
+    injections: Sequence[Injection]
+
+    def is_empty(self) -> bool:
+        return all(injection.dependency is None for injection in self.injections)
+
+
+@API.private
+def build_wrapper(blueprint: InjectionBlueprint,
+                  wrapped: Callable[..., object],
+                  skip_self: bool = False) -> Callable[..., object]:
+    if inspect.iscoroutinefunction(wrapped):
+        return AsyncInjectedWrapper(blueprint,
+                                    cast(Callable[..., Awaitable[object]], wrapped),
+                                    skip_self)
+    return SyncInjectedWrapper(blueprint, wrapped, skip_self)
+
+
+@API.private
+def get_wrapper_dependencies(wrapper: Callable[..., object]) -> List[Hashable]:
+    if not isinstance(wrapper, InjectedWrapper):
+        raise TypeError(f"Argument must be an {InjectedWrapper}")
+
+    if isinstance(wrapper, SyncInjectedWrapper):
+        prefix = f"_{SyncInjectedWrapper.__name__}"
+    else:
+        prefix = f"_{AsyncInjectedWrapper.__name__}"
+    blueprint: InjectionBlueprint = getattr(wrapper,
+                                            f"{prefix}__blueprint")
+
+    return [inj.dependency for inj in blueprint.injections if inj.dependency is not None]
+
+
+@API.private
+def is_wrapper(x: object) -> bool:
+    return isinstance(x, InjectedWrapper)
+
+
+@API.private
+def get_wrapped(x: object) -> object:
+    assert isinstance(x, InjectedWrapper)
+    return x.__wrapped__
+
+
+@API.private
+class InjectedWrapper:
+    __wrapped__: object
+
+
+@API.private
+class SyncInjectedWrapper(InjectedWrapper):
+    """
+    Wrapper which injects all the dependencies not supplied in the passed
+    arguments. An InjectionBlueprint is used to store the mapping of the
+    arguments to their dependency if any and if the injection is required.
+    """
+
+    def __init__(self,
+                 blueprint: InjectionBlueprint,
+                 wrapped: Callable[..., object],
+                 skip_self: bool = False):
+        """
+        Args:
+            blueprint: Injection blueprint for the underlying function
+            wrapped:  real function to be called
+            skip_self:  whether the first argument must be skipped. Used internally
+        """
+        self.__blueprint = blueprint
+        self.__wrapped__: Callable[..., object] = wrapped
+        self.__injection_offset = 1 if skip_self else 0
+        functools.wraps(wrapped, updated=())(self)
+
+    def __call__(self, *args: object, **kwargs: object) -> object:
+        kwargs = _inject_kwargs(
+            current_container(),
+            self.__blueprint,
+            self.__injection_offset + len(args),
+            kwargs
+        )
+        return self.__wrapped__(*args, **kwargs)
+
+    def __get__(self, instance: object, owner: type) -> object:
+        return SyncInjectedBoundWrapper(
+            self.__blueprint,
+            self.__wrapped__.__get__(instance, owner),  # type: ignore
+            instance is not None
+        )
+
+    def __getattr__(self, item: str) -> object:
+        return getattr(self.__wrapped__, item)
+
+
+@API.private
+class SyncInjectedBoundWrapper(SyncInjectedWrapper):
+    """
+    Behaves like Python bound methods. Unsure whether this is really necessary
+    or not.
+    """
+
+    def __get__(self, instance: object, owner: type) -> object:
+        return self  # pragma: no cover
+
+
+@API.private
+class AsyncInjectedWrapper(InjectedWrapper):
+    """
+    Wrapper which injects all the dependencies not supplied in the passed
+    arguments. An InjectionBlueprint is used to store the mapping of the
+    arguments to their dependency if any and if the injection is required.
+    """
+
+    def __init__(self,
+                 blueprint: InjectionBlueprint,
+                 wrapped: Callable[..., Awaitable[object]],
+                 skip_self: bool = False):
+        """
+        Args:
+            blueprint: Injection blueprint for the underlying function
+            wrapped:  real function to be called
+            skip_self:  whether the first argument must be skipped. Used internally
+        """
+        self.__blueprint = blueprint
+        self.__wrapped__: Callable[..., Awaitable[object]] = wrapped
+        self.__injection_offset = 1 if skip_self else 0
+        functools.wraps(wrapped, updated=())(self)
+
+    async def __call__(self, *args: object, **kwargs: object) -> object:
+        kwargs = _inject_kwargs(
+            current_container(),
+            self.__blueprint,
+            self.__injection_offset + len(args),
+            kwargs
+        )
+        return await self.__wrapped__(*args, **kwargs)
+
+    def __get__(self, instance: object, owner: type) -> object:
+        return AsyncInjectedBoundWrapper(
+            self.__blueprint,
+            self.__wrapped__.__get__(instance, owner),  # type: ignore
+            instance is not None
+        )
+
+    def __getattr__(self, item: str) -> object:
+        return getattr(self.__wrapped__, item)
+
+
+@API.private
+class AsyncInjectedBoundWrapper(AsyncInjectedWrapper):
+    """
+    Behaves like Python bound methods. Unsure whether this is really necessary
+    or not.
+    """
+
+    def __get__(self, instance: object, owner: type) -> object:
+        return self  # pragma: no cover
+
+
+@API.private
+def _inject_kwargs(container: Container,
+                   blueprint: InjectionBlueprint,
+                   offset: int,
+                   kwargs: Dict[str, object]) -> Dict[str, object]:
+    """
+    Does the actual injection of the dependencies. Used by InjectedCallableWrapper.
+    """
+    dirty_kwargs = False
+    for injection in blueprint.injections[offset:]:
+        if injection.dependency is not None and injection.arg_name not in kwargs:
+            try:
+                arg = container.get(injection.dependency)
+                if not dirty_kwargs:
+                    kwargs = kwargs.copy()
+                    dirty_kwargs = True
+                kwargs[injection.arg_name] = arg
+            except DependencyNotFoundError:
+                if injection.required:
+                    raise
+
+    return kwargs
---
+++ packages/antidote/antidote-0.12.1-cp39-cp39-manylinux2010_x86_64.whl/antidote/_providers/factory.py
@@ -0,0 +1,166 @@
+import inspect
+from typing import Callable, Dict, Hashable, Optional
+
+from .service import Build
+from .._internal import API
+from .._internal.utils import FinalImmutable, SlotRecord, debug_repr
+from ..core import (Container, DependencyDebug, DependencyValue, Provider,
+                    Scope)
+
+
+@API.private
+class FactoryProvider(Provider[Hashable]):
+    def __init__(self) -> None:
+        super().__init__()
+        self.__factories: Dict[FactoryDependency, Factory] = dict()
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}(factories={list(self.__factories.keys())})"
+
+    def clone(self, keep_singletons_cache: bool) -> 'FactoryProvider':
+        p = FactoryProvider()
+        if keep_singletons_cache:
+            factories = {
+                k: (f.copy() if f.dependency is not None else f)
+                for k, f in self.__factories.items()
+            }
+        else:
+            factories = {
+                k: (f.copy(keep_function=False) if f.dependency is not None else f)
+                for k, f in self.__factories.items()
+            }
+        p.__factories = factories
+        return p
+
+    def exists(self, dependency: Hashable) -> bool:
+        # For now we don't support multiple factories for a single dependency. Neither
+        # is sharing the dependency with another provider. Simply because I don't see a
+        # use case where it would make sense.
+        # Open for discussions though, create an issue if you a use case.
+        if isinstance(dependency, Build):
+            dependency = dependency.dependency
+        return (isinstance(dependency, FactoryDependency)
+                and dependency in self.__factories)
+
+    def maybe_debug(self, build: Hashable) -> Optional[DependencyDebug]:
+        dependency_factory = build.dependency if isinstance(build, Build) else build
+        if not isinstance(dependency_factory, FactoryDependency):
+            return None
+
+        try:
+            factory = self.__factories[dependency_factory]
+        except KeyError:
+            return None
+
+        dependencies = []
+        wired = []
+        if factory.dependency is not None:
+            dependencies.append(factory.dependency)
+            if isinstance(factory.dependency, type) \
+                    and inspect.isclass(factory.dependency):
+                wired.append(factory.dependency.__call__)
+        else:
+            wired.append(factory.function)
+
+        return DependencyDebug(debug_repr(build),
+                               scope=factory.scope,
+                               wired=wired,
+                               dependencies=dependencies)
+
+    def maybe_provide(self, build: Hashable, container: Container
+                      ) -> Optional[DependencyValue]:
+        dependency_factory = build.dependency if isinstance(build, Build) else build
+        if not isinstance(dependency_factory, FactoryDependency):
+            return None
+
+        try:
+            factory = self.__factories[dependency_factory]
+        except KeyError:
+            return None
+
+        if factory.function is None:
+            f = container.provide(factory.dependency)
+            assert f.is_singleton(), "factory dependency is expected to be a singleton"
+            factory.function = f.unwrapped
+
+        instance = (factory.function(**build.kwargs)
+                    if isinstance(build, Build) and build.kwargs
+                    else factory.function())
+
+        return DependencyValue(instance, scope=factory.scope)
+
+    def register(self,
+                 output: type,
+                 *,
+                 scope: Optional[Scope],
+                 factory: Callable[..., object] = None,
+                 factory_dependency: Hashable = None
+                 ) -> 'FactoryDependency':
+        assert inspect.isclass(output) \
+               and (factory is None or factory_dependency is None) \
+               and (factory is None or callable(factory)) \
+               and (isinstance(scope, Scope) or scope is None)
+
+        dependency = FactoryDependency(output, factory or factory_dependency)
+        self._assert_not_duplicate(dependency)
+
+        if factory_dependency:
+            self.__factories[dependency] = Factory(scope,
+                                                   dependency=factory_dependency)
+        else:
+            self.__factories[dependency] = Factory(scope,
+                                                   function=factory)
+
+        return dependency
+
+
+@API.private
+class FactoryDependency(FinalImmutable):
+    __slots__ = ('output', 'factory', '__hash')
+    output: Hashable
+    factory: object
+    __hash: int
+
+    def __init__(self, output: Hashable, factory: object):
+        super().__init__(output, factory, hash((output, factory)))
+
+    def __repr__(self) -> str:
+        return f"FactoryDependency({self})"
+
+    def __antidote_debug_repr__(self) -> str:
+        return str(self)
+
+    def __str__(self) -> str:
+        return f"{debug_repr(self.output)} @ {debug_repr(self.factory)}"
+
+    # Custom hash & eq necessary to find duplicates
+    def __hash__(self) -> int:
+        return self.__hash
+
+    def __eq__(self, other: object) -> bool:
+        return (isinstance(other, FactoryDependency)
+                and self.__hash == other.__hash
+                and (self.output is other.output
+                     or self.output == other.output)
+                and (self.factory is other.factory
+                     or self.factory == other.factory))  # noqa
+
+
+@API.private
+class Factory(SlotRecord):
+    __slots__ = ('scope', 'function', 'dependency')
+    scope: Optional[Scope]
+    function: Callable[..., object]
+    dependency: Hashable
+
+    def __init__(self,
+                 scope: Optional[Scope],
+                 function: Callable[..., object] = None,
+                 dependency: Hashable = None):
+        assert function is not None or dependency is not None
+        super().__init__(scope, function, dependency)
+
+    def copy(self, keep_function: bool = True) -> 'Factory':
+        return Factory(self.scope,
+                       self.function if keep_function else None,
+                       self.dependency)
