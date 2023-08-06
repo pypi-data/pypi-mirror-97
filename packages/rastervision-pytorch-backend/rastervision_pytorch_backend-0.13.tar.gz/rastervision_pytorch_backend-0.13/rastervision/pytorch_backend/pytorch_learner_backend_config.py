b1294688d6f7f8ea66880d3feaa3eb3f82b74842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:45 +0000

    Add package rastervision-pytorch-backend/0.13/rastervision_pytorch_backend-0.13.tar.gz

---
+++ packages/rastervision-pytorch-backend/rastervision_pytorch_backend-0.13.tar.gz/rastervision_pytorch_backend-0.13/rastervision/pytorch_backend/pytorch_learner_backend_config.py
@@ -0,0 +1,51 @@
+from typing import Optional, List
+
+from rastervision.pipeline.config import (register_config, Field)
+from rastervision.core.backend import BackendConfig
+from rastervision.core.rv_pipeline import RVPipeline
+from rastervision.pytorch_learner.learner_config import (
+    SolverConfig, ModelConfig, DataConfig, ImageDataConfig, GeoDataConfig)
+
+
+@register_config('pytorch_learner_backend')
+class PyTorchLearnerBackendConfig(BackendConfig):
+    model: ModelConfig
+    solver: SolverConfig
+    data: DataConfig
+    log_tensorboard: bool = Field(
+        True, description='If True, log events to Tensorboard log files.')
+    run_tensorboard: bool = Field(
+        False,
+        description='If True, run Tensorboard server pointing at log files.')
+    test_mode: bool = Field(
+        False,
+        description=
+        ('This field is passed along to the LearnerConfig which is returned by '
+         'get_learner_config(). For more info, see the docs for'
+         'pytorch_learner.learner_config.LearnerConfig.test_mode.'))
+
+    def get_bundle_filenames(self):
+        return ['model-bundle.zip']
+
+    def update(self, pipeline: Optional[RVPipeline] = None):
+        super().update(pipeline=pipeline)
+
+        if isinstance(self.data, ImageDataConfig):
+            if self.data.uri is None and self.data.group_uris is None:
+                self.data.uri = pipeline.chip_uri
+        if not self.data.class_names:
+            self.data.class_names = pipeline.dataset.class_config.names
+        if not self.data.class_colors:
+            self.data.class_colors = pipeline.dataset.class_config.colors
+
+    def get_learner_config(self, pipeline: Optional[RVPipeline]):
+        raise NotImplementedError()
+
+    def build(self, pipeline: Optional[RVPipeline], tmp_dir: str):
+        raise NotImplementedError()
+
+    def filter_commands(self, commands: List[str]) -> List[str]:
+        nochip = isinstance(self.data, GeoDataConfig)
+        if nochip and 'chip' in commands:
+            commands = [c for c in commands if c != 'chip']
+        return commands
---
+++ packages/rastervision-pytorch-backend/rastervision_pytorch_backend-0.13.tar.gz/rastervision_pytorch_backend-0.13/rastervision/pytorch_backend/pytorch_semantic_segmentation_config.py
@@ -0,0 +1,89 @@
+from typing import Optional
+
+from rastervision.core.rv_pipeline import RVPipeline
+from rastervision.pipeline.config import (register_config, ConfigError)
+from rastervision.pytorch_backend.pytorch_learner_backend_config import (
+    PyTorchLearnerBackendConfig)
+from rastervision.pytorch_learner.learner_config import default_augmentors
+from rastervision.pytorch_learner.semantic_segmentation_learner_config import (
+    SemanticSegmentationModelConfig, SemanticSegmentationLearnerConfig,
+    SemanticSegmentationImageDataConfig)
+from rastervision.pytorch_backend.pytorch_semantic_segmentation import (
+    PyTorchSemanticSegmentation)
+
+
+def ss_learner_backend_config_upgrader(cfg_dict, version):
+    if version == 0:
+        fields = {
+            'augmentors': default_augmentors,
+            'group_uris': None,
+            'group_train_sz': None,
+            'group_train_sz_rel': None,
+            'num_workers': 4,
+            'img_sz': None,
+            'base_transform': None,
+            'aug_transform': None,
+            'plot_options': None,
+            'preview_batch_limit': None
+        }
+        data_cfg_dict = {
+            key: cfg_dict.pop(key, default_val)
+            for key, default_val in fields.items() if key in cfg_dict
+        }
+        if data_cfg_dict['img_sz'] is None:
+            data_cfg_dict['img_sz'] = 256
+
+        data_cfg = SemanticSegmentationImageDataConfig(**data_cfg_dict)
+        data_cfg.update()
+        data_cfg.validate_config()
+        cfg_dict['data'] = data_cfg.dict()
+    return cfg_dict
+
+
+@register_config(
+    'pytorch_semantic_segmentation_backend',
+    upgrader=ss_learner_backend_config_upgrader)
+class PyTorchSemanticSegmentationConfig(PyTorchLearnerBackendConfig):
+    model: SemanticSegmentationModelConfig
+
+    def update(self, pipeline: Optional[RVPipeline] = None):
+        super().update(pipeline=pipeline)
+        dcfg = self.data
+        pcfg = pipeline
+
+        dcfg.img_channels = pcfg.dataset.img_channels
+
+        if isinstance(dcfg, SemanticSegmentationImageDataConfig):
+            if (dcfg.img_format is not None) and (dcfg.img_format !=
+                                                  pcfg.img_format):
+                raise ConfigError(
+                    'SemanticSegmentationImageDataConfig.img_format is '
+                    'specified and not equal to '
+                    'SemanticSegmentationConfig.img_format.')
+            if (dcfg.label_format is not None) and (dcfg.label_format !=
+                                                    pcfg.label_format):
+                raise ConfigError(
+                    'SemanticSegmentationImageDataConfig.label_format is '
+                    'specified and not equal to '
+                    'SemanticSegmentationConfig.label_format.')
+            if dcfg.img_format is None:
+                dcfg.img_format = pcfg.img_format
+            if dcfg.label_format is None:
+                dcfg.label_format = pcfg.label_format
+
+    def get_learner_config(self, pipeline):
+        learner = SemanticSegmentationLearnerConfig(
+            data=self.data,
+            model=self.model,
+            solver=self.solver,
+            test_mode=self.test_mode,
+            output_uri=pipeline.train_uri,
+            log_tensorboard=self.log_tensorboard,
+            run_tensorboard=self.run_tensorboard)
+        learner.update()
+        learner.validate_config()
+        return learner
+
+    def build(self, pipeline, tmp_dir):
+        learner = self.get_learner_config(pipeline)
+        return PyTorchSemanticSegmentation(pipeline, learner, tmp_dir)
