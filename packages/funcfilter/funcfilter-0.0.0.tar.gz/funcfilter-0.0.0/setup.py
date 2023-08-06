3d9ca04fc302530b3a6a49ba3846800e58d87a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:25 +0000

    Add package funcfilter/0.0.0/funcfilter-0.0.0.tar.gz

---
+++ packages/funcfilter/funcfilter-0.0.0.tar.gz/funcfilter-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='funcfilter',
+    packages=['funcfilter'],
+    version='0.0.0',
+    author='Perzan',
+    author_email='PerzanDevelopment@gmail.com'
+)
