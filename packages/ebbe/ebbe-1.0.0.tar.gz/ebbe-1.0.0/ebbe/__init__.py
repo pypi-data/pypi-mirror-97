6bb5776ba8f82482b5960a75a62619b46c0c373c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:18 +0000

    Add package ebbe/1.0.0/ebbe-1.0.0.tar.gz

---
+++ packages/ebbe/ebbe-1.0.0.tar.gz/ebbe-1.0.0/ebbe/__init__.py
@@ -0,0 +1,12 @@
+# =============================================================================
+# Ebbe Library Enpoint
+# =============================================================================
+#
+from ebbe.functions import (
+    as_chunks,
+    uniq,
+    with_prev,
+    with_next,
+    with_is_first,
+    with_is_last
+)
