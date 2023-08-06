8fd255e75a319210876b16e520caf2db8afc6a28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:28 +0000

    Add package turnsole/0.0.4/turnsole-0.0.4.tar.gz

---
+++ packages/turnsole/turnsole-0.0.4.tar.gz/turnsole-0.0.4/setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Author        : lk
+# @Email         : 9428.al@gmail.com
+# @Created Date  : 2021-03-04 16:56:27
+# @Last Modified : 2021-03-04 17:16:57
+# @Description   :
+
+import setuptools
+
+setuptools.setup()
