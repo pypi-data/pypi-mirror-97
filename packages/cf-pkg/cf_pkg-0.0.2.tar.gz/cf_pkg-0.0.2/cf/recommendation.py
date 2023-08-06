fe1dff3264f1ceab2900d68fafb827543ff6aa5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:03 +0000

    Add package cf-pkg/0.0.2/cf_pkg-0.0.2.tar.gz

---
+++ packages/cf-pkg/cf_pkg-0.0.2.tar.gz/cf_pkg-0.0.2/cf/recommendation.py
@@ -0,0 +1,8 @@
+class Recommender:
+    def __init__(self, user_skill_matrix, user_task_matrix):
+        # 初始化需要 用户技能矩阵 用户历史数据
+        return
+
+    def recommend(self, user_id, upper=5):
+        # upper:推荐任务数量上限
+        return
