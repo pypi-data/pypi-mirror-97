61794bc639cc0204c14e3b360db047ee948aeb58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:27 +0000

    Add package django-admin-filter/0.6/django_admin_filter-0.6-py3-none-any.whl

---
+++ packages/django-admin-filter/django_admin_filter-0.6-py3-none-any.whl/django_admin_filter/models.py
@@ -0,0 +1,93 @@
+import json
+from urllib.parse import urlencode
+from django.utils import timezone
+from django.utils.translation import gettext as _
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.conf import settings
+from django.core.exceptions import FieldError
+from django.core.exceptions import ValidationError
+from django.core.serializers.json import DjangoJSONEncoder
+from . import settings as app_settings
+
+
+def default_dict():
+    return dict()
+
+
+class JSONField(models.TextField):
+    """
+    A very raw and simple JSONField.
+    (It is only used internally - so we know what we get.)
+    """
+    def from_db_value(self, value, *args):
+        if value is None:
+            return value
+        else:
+            return json.loads(value)
+
+    def to_python(self, value):
+        if type(value) is str:
+            return json.loads(value)
+        else:
+            return value
+
+    def get_prep_value(self, value):
+        return json.dumps(value, cls=DjangoJSONEncoder)
+
+
+class FilterQuery(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    persistent = models.BooleanField(default=False)
+    querydict = JSONField(default=default_dict)
+    created = models.DateTimeField(auto_now_add=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    for_everyone = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created']
+        permissions = [('can_handle_global_filterqueries', 'Can handle global FilterQueries')]
+
+    @staticmethod
+    def has_global_perm(user):
+        perm = 'django_admin_filter.can_handle_global_filterqueries'
+        return user.has_perm(perm)
+
+    def save(self, *args, **kwargs):
+        # create a generic name if missing
+        if not self.name:
+            if settings.USE_TZ:
+                now = timezone.localtime().strftime('%x %X')
+            else:
+                now = timezone.now().strftime('%x %X')
+            self.name = _('Filter from ') + now
+
+        # create a generic description if missing
+        if not self.description:
+            self.description = self.pretty_query
+
+        # save filter-query
+        super().save(*args, **kwargs)
+
+        # truncate history
+        if app_settings.TRUNCATE_HISTORY and not self.persistent:
+            history = FilterQuery.objects.filter(
+                user=self.user,
+                content_type=self.content_type,
+                persistent=False
+            )[app_settings.HISTORY_LIMIT:]
+            FilterQuery.objects.filter(id__in=[f.id for f in history]).delete()
+
+    @property
+    def pretty_query(self):
+        lines = list()
+        for key in sorted(self.querydict.keys()):
+            line = '{} = {}'.format(key, self.querydict[key])
+            lines.append(line)
+        return '\n'.join(lines)
+
+    @property
+    def urlquery(self):
+        return urlencode(self.querydict)
---
+++ packages/django-admin-filter/django_admin_filter-0.6-py3-none-any.whl/django_admin_filter/views.py
@@ -0,0 +1,179 @@
+import html.parser
+from decimal import Decimal
+
+from django import forms
+from django.http import Http404
+from django.http import HttpResponseRedirect
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.utils.text import format_lazy
+from django.utils.translation import gettext as _
+from django.core.exceptions import PermissionDenied
+from django.views.generic.base import TemplateResponseMixin
+from django.views.generic.edit import BaseCreateView, BaseUpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.contenttypes.models import ContentType
+from django.urls import reverse
+from .filterset import AdminFilterSet
+from .models import FilterQuery
+from .forms import FilterForm
+
+
+def can_view_related_model(func):
+    """
+    Decorator for view-methods to check permission to view the filtered items.
+    """
+    def wrapper(self, request, **kwargs):
+        permission = '{app_label}.view_{model}'.format(**kwargs)
+        if not request.user.has_perm(permission):
+            raise PermissionDenied
+        return func(self, request, **kwargs)
+    return wrapper
+
+
+def can_handle_filterquery(func):
+    """
+    Decorator to check the permission to delete or update a specific filterquery.
+    """
+    def wrapper(self, request, **kwargs):
+        obj = self.get_object()
+        is_owner = obj.user == self.request.user
+        can_handle = obj.for_everyone and obj.has_global_perm(self.request.user)
+        if not is_owner and not can_handle:
+            raise PermissionDenied
+
+        return func(self, request, **kwargs)
+    return wrapper
+
+
+def setup_filterclass(func):
+    """
+    Extract the contenttype from url-paramerters and setup the filter-class.
+    """
+    def wrapper(self, request, **kwargs):
+        params = dict(app_label=kwargs['app_label'], model=kwargs['model'])
+        try:
+            self.content_type = ContentType.objects.get(**params)
+        except ContentType.DoesNotExist:
+            msg = format_lazy(_("No model '{model}' in app '{app_label}'"), **params)
+            raise Http404(msg)
+        else:
+            ct_model = self.content_type.model_class()
+            self.filterset_class = AdminFilterSet.by_model(ct_model)
+        return func(self, request, **kwargs)
+    return wrapper
+
+
+class BaseFilterQueryView(LoginRequiredMixin, TemplateResponseMixin):
+    """
+    View to add and delete Filters.
+    """
+    template_name = 'django_admin_filter/filter_query.html'
+    model = FilterQuery
+    form_class = FilterForm
+    prefix = 'fq'
+    object = None
+
+    def post(self, *args, **kwargs):
+        form = self.get_form()
+        query_form = self.get_query_form()
+
+        if form.is_valid() and query_form.is_valid():
+            return self.form_valid(form, query_form)
+        else:
+            return self.form_invalid(form, query_form)
+
+    def get_querydict(self):
+        # TODO: Filter out FILTERS_NULL_CHOICE_VALUE for ChoiceFilter and
+        # 'unknown' for BooleanFields.
+        field_names = self.filterset_class().form.fields.keys()
+        fields = self.filterset_class().form.fields
+        querydict = dict(
+            (k, v) for k, v in self.request.POST.items()
+            if k in field_names and not v is str()
+        )
+        return querydict
+
+    def form_valid(self, form, query_form):
+        self.object = form.save(commit=False)
+        if 'save' not in self.request.POST :
+            self.object.id = None
+        self.object.querydict = self.get_querydict()
+        self.object.content_type = self.content_type
+        self.object.persistent = 'save' in self.request.POST or 'save_new' in self.request.POST
+        self.object.for_everyone = self.object.for_everyone and self.object.persistent
+        self.object.user = self.request.user
+        self.object.save()
+
+        # check extra permission for global filterqueries
+        if self.object.for_everyone and not self.object.has_global_perm(self.request.user):
+            raise PermissionDenied
+
+        return HttpResponseRedirect(self.get_success_url()) 
+
+    def form_invalid(self, form, query_form):
+        context = self.get_context_data(form=form, query_form=query_form)
+        return self.render_to_response(context)
+
+    def get_success_url(self):
+        url_pattern = 'admin:{app_label}_{model}_changelist'
+        query = '?filter_id={}'.format(self.object.id)
+        url = reverse(url_pattern.format(**self.kwargs)) + query
+        return url
+
+    def get_form(self):
+        form = super().get_form()
+        # hide the for_everyone field if user has no permission
+        if not FilterQuery.has_global_perm(self.request.user):
+            form.fields['for_everyone'].widget = forms.HiddenInput()
+        return form
+
+    def get_query_form(self):
+        if self.request.method in ('POST', 'PUT'):
+            data = self.request.POST
+        else:
+            data = self.object.querydict if self.object else dict()
+        filter = self.filterset_class(data)
+        return filter.form
+
+    def get_context_data(self, **kwargs):
+        query_form = self.get_query_form()
+        extra_context = dict(query_form=query_form)
+        extra_context.update(kwargs)
+        return super().get_context_data(**extra_context)
+
+
+class CreateFilterQueryView(BaseFilterQueryView, BaseCreateView):
+    @setup_filterclass
+    @can_view_related_model
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @setup_filterclass
+    @can_view_related_model
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+
+class UpdateFilterQueryView(BaseFilterQueryView, BaseUpdateView):
+    @setup_filterclass
+    @can_view_related_model
+    @can_handle_filterquery
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().get(request, *args, **kwargs)
+
+    @setup_filterclass
+    @can_view_related_model
+    @can_handle_filterquery
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+
+    @can_view_related_model
+    @can_handle_filterquery
+    def delete(self, *args, **kwargs):
+        self.object = self.get_object()
+        response = dict(id=self.object.id)
+        self.object.delete()
+        return JsonResponse(response)
