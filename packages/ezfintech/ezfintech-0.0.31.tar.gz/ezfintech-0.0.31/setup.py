6d32db0f2e4733d8310884a9a0c96bf702e9066e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:36 +0000

    Add package ezfintech/0.0.31/ezfintech-0.0.31.tar.gz

---
+++ packages/ezfintech/ezfintech-0.0.31.tar.gz/ezfintech-0.0.31/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import sys
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ezfintech",
+    version="0.0.31",
+    author="Yizhe Zhang",
+    author_email="ervinzhang319@gmail.com",
+    description="get stock data from different APIs and Analyze Stock Data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/YizheZhang-Ervin/EZFintech",
+    packages=find_packages(),
+    install_requires=["numpy","pandas","matplotlib","requests","mplfinance"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
