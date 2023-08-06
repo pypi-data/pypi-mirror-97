c0a04e5c18ae9b5597cb6dffbbef4e6e4d74ddb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:53 +0000

    Add package hypatie/2.2.0/hypatie-2.2.0-py3-none-any.whl

---
+++ packages/hypatie/hypatie-2.2.0-py3-none-any.whl/hypatie/__init__.py
@@ -0,0 +1,7 @@
+from .core import *
+from .animation import *
+from .plots import *
+from .transform import *
+from .simbad import bright_objects, get_objects, explore_region, search_region, search_sky, explore_sky
+
+__version__ = "2.2.0"
