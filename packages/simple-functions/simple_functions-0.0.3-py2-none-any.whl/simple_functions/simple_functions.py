4fce4b5947c7549f7e0d8b1991f8a2941baa06bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:34 +0000

    Add package simple-functions/0.0.3/simple_functions-0.0.3-py2-none-any.whl

---
+++ packages/simple-functions/simple_functions-0.0.3-py2-none-any.whl/simple_functions/simple_functions.py
@@ -0,0 +1,21 @@
+import datetime
+import string
+import random
+
+def company_id(company_id = 1, length = 6):
+	# company ID
+	return "ESE" + str(company_id).zfill(length)
+
+def restaurant_id(restaurant_id = 1, length = 6):
+	# restaurant ID
+	return "ESR" + str(restaurant_id).zfill(length)
+
+def invoice_id(invoice_id = 1, length = 6):
+	# invoice ID
+	return "ES" + str(datetime.datetime.today().year) + str(invoice_id).zfill(length)
+
+def reservation_code(res_code_id = 1, length = 6):
+	# reservation Code
+	letters = string.ascii_uppercase + string.digits
+	random_string = ''.join(random.choice(letters) for i in range(6))
+	return random_string + str(res_code_id).zfill(length)
