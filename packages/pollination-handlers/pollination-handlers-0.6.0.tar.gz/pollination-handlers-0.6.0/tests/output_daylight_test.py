6db4f9ab6d6c18b99b87407c45273a589f0a1bb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:21 +0000

    Add package pollination-handlers/0.6.0/pollination-handlers-0.6.0.tar.gz

---
+++ packages/pollination-handlers/pollination-handlers-0.6.0.tar.gz/pollination-handlers-0.6.0/tests/output_daylight_test.py
@@ -0,0 +1,16 @@
+from pollination_handlers.outputs.daylight import read_df_from_folder, \
+    sort_ill_from_folder
+
+
+def test_read_df():
+    res = read_df_from_folder('./tests/assets/df_results/')
+    assert res == [[25.1, 0, 10, 100]]
+
+
+def test_read_da():
+    res = sort_ill_from_folder('./tests/assets/annual_dl_results/')
+    assert res == [
+        './tests/assets/annual_dl_results/TestRoom_1.ill',
+        './tests/assets/annual_dl_results/TestRoom_2.ill',
+        './tests/assets/annual_dl_results/sun-up-hours.txt'
+    ]
