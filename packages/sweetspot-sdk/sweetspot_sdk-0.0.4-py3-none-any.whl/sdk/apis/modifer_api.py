d5ac49b38dda269155f26f7956bc708ca334ac6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:44 +0000

    Add package sweetspot-sdk/0.0.4/sweetspot_sdk-0.0.4-py3-none-any.whl

---
+++ packages/sweetspot-sdk/sweetspot_sdk-0.0.4-py3-none-any.whl/sdk/apis/modifer_api.py
@@ -0,0 +1,58 @@
+from .base_api import BaseApi 
+from ..https.api_response import ApiResponse 
+from ..https.auth.o_auth_2 import OAuth2 
+from ..api_helper import ApiHelper 
+
+class ModifierApi(BaseApi):
+
+    def __init__(self, config):
+        super().__init__(config)
+
+    def get_all_modifiers(self):
+        _url_path = f'/modifier'
+        _query_builder = self.config.get_base_uri()
+        _query_builder += _url_path 
+        _query_url = ApiHelper.clean_url(_query_builder)
+
+        _headers = { 
+            'accept': 'application/json'
+        }
+
+        _request = self.config.http_client.get(_query_builder, headers=_headers) 
+        OAuth2.apply(self.config, _request)
+        _response = self.execute_request(_request)
+
+        decoded = ApiHelper.json_deserialize(_response.text)
+        if type(decoded) == dict: 
+            _errors = decoded.get('error')
+        else:
+            _errors = None
+
+        _result = ApiResponse(_response, body=decoded, errors=_errors)
+        return _result
+
+    def add_modifier(self, payload):
+        _url_path = f'/modifier'
+        _query_builder = self.config.get_base_uri()
+        _query_builder += _url_path
+        _query_url = ApiHelper.clean_url(_query_builder)
+        
+        _headers = {
+            'accept': 'application/json',
+            'Content-Type': 'application/json; charset=utf-8'
+        }
+
+        _request = self.config.http_client.post(_query_builder, headers=_headers, parameters=ApiHelper.json_serialize(payload))
+        OAuth2.apply(self.config, _request)
+        _response = self.execute_request(_request)
+
+        decoded = ApiHelper.json_deserialize(_response.text)
+        if type(decoded) == dict: 
+            _errors = decoded.get('error')
+        else: 
+            _errors = None
+        
+        _result = ApiResponse(_response, body=decoded, errors=_errors)
+        return _result 
+
+    
