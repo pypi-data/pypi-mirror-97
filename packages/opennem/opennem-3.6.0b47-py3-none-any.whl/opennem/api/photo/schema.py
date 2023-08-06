1e42127597aefe87c92bfba75a47bd73487987f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:17 +0000

    Add package opennem/3.6.0b47/opennem-3.6.0b47-py3-none-any.whl

---
+++ packages/opennem/opennem-3.6.0b47-py3-none-any.whl/opennem/api/photo/schema.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+from opennem.api.schema import ApiBase
+
+
+class Photo(ApiBase):
+    id: int
+
+    hash_id: Optional[str]
+    width: int
+    height: int
+    photo_url: Optional[str]
+
+    license_type: Optional[str]
+    license_link: Optional[str]
+    author: Optional[str]
+    author_link: Optional[str]
