b9710d051ac29a8727bf67fdbfeacf128b62caaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:06 +0000

    Add package dpack/0.4.0/dpack-0.4.0.tar.gz

---
+++ packages/dpack/dpack-0.4.0.tar.gz/dpack-0.4.0/dpack/cli.py
@@ -0,0 +1,44 @@
+import argparse
+import os
+import sys
+
+import yaml
+
+from .base import DPack
+from .serve import serve
+
+
+def main(*args):
+    settings_module = os.getenv("DJANGO_SETTINGS_MODULE")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", default=None, help="config file to use (dpack.yaml by default)")
+    parser.add_argument("-s", "--settings", default=settings_module, help="Django settings module")
+    parser.add_argument("-o", "--output", default=None, help="override output directory")
+    parser.add_argument("-y", "--yaml", action="store_true", default=False, help="print YAML config")
+    parser.add_argument("command", nargs="*")
+    options = parser.parse_args(args=args or None)
+    overrides = {}
+    if options.output:
+        overrides["output"] = options.output
+    if options.settings:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", options.settings)
+        # If you call dpack -s at the same level as manage.py, you probably expect it to find your settings module.
+        sys.path.insert(0, os.getcwd())
+        import django
+
+        from .finders import DjangoDPack
+
+        django.setup()
+        packer = DjangoDPack(options.config, **overrides)
+    else:
+        packer = DPack(options.config, **overrides)
+    if options.yaml:
+        print(yaml.safe_dump(packer.dump_config()))
+        sys.exit(1)
+    if not options.command:
+        packer.pack()
+    elif options.command[0] == "serve":
+        serve(packer.__class__, options.config, overrides)
+    else:
+        print("Unknown command: {}".format(options.command[0]), file=sys.stderr)
+        sys.exit(1)
---
+++ packages/dpack/dpack-0.4.0.tar.gz/dpack-0.4.0/dpack/finders.py
@@ -0,0 +1,68 @@
+import os
+
+from django.conf import settings
+from django.contrib.staticfiles.finders import BaseFinder, get_finders
+from django.contrib.staticfiles.utils import matches_patterns
+from django.core.checks import Warning
+from django.core.files.storage import FileSystemStorage
+
+from .base import DPack
+
+
+class DjangoDPack(DPack):
+    def __init__(self, config=None, **overrides):
+        dpack_setting = getattr(settings, "DPACK", None)
+        if config is None:
+            config = dpack_setting if isinstance(dpack_setting, str) else None
+        options = dpack_setting if isinstance(dpack_setting, dict) else {}
+        options.update(overrides)
+        if "output" not in options:
+            options["output"] = settings.STATIC_ROOT
+        if "prefix" not in options:
+            options["prefix"] = settings.STATIC_URL
+        base_dir = getattr(settings, "BASE_DIR", None)
+        super().__init__(config, base_dir=base_dir, **options)
+
+    def find_input(self, name):
+        for finder in get_finders():
+            if isinstance(finder, DPackFinder):
+                continue
+            found = finder.find(name)
+            if found:
+                return found
+        # TODO: should this call through to super? maybe only if `search` is specified?
+        return None
+
+
+class DPackFinder(BaseFinder):
+    def __init__(self, *args, **kwargs):
+        self.packer = DjangoDPack()
+        self.storage = FileSystemStorage(self.packer.storage_path)
+
+    def check(self, **kwargs):
+        errors = []
+        if not self.packer.assets:
+            errors.append(
+                Warning(
+                    "You have not specified any assets to pack.",
+                    hint='Make sure you specify an "assets" dict in your dpack.yaml or DPACK setting.',
+                    id="dpack.W001",
+                )
+            )
+        return errors
+
+    def find(self, path, all=False):
+        # Only pack the path being searched for, and only if it's out of date.
+        if path in self.packer.assets:
+            self.packer.pack(path)
+            full_path = os.path.join(self.packer.location, path)
+            return [full_path] if all else full_path
+        return []
+
+    def list(self, ignore_patterns):
+        # This is called from collectstatic, pack everything up front.
+        self.packer.pack(force=True)
+        for asset in self.packer.assets:
+            if matches_patterns(asset, ignore_patterns):
+                continue
+            yield asset, self.storage
