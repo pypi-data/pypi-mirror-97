9c03e89feb18f835b7b605512b25d2280a287646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:13 +0000

    Add package thornfield/1.3.0/thornfield-1.3.0.tar.gz

---
+++ packages/thornfield/thornfield-1.3.0.tar.gz/thornfield-1.3.0/src/thornfield/cache_factories/postgresql_cache_factory.py
@@ -0,0 +1,48 @@
+import re
+from types import MethodType, FunctionType
+from typing import Union, Optional, Callable, Any
+
+from .cache_factory import CacheFactory
+from ..caches.postgresql_cache import PostgresqlCache
+from ..postgresql_key_value_adapter import (
+    PostgresqlKeyValueAdapter,
+    ConnectionPool,
+)
+
+
+class PostgresqlCacheFactory(CacheFactory):
+    def __init__(
+        self,
+        connection_pool: ConnectionPool,
+        serializer: Optional[Callable[[Any], str]] = None,
+        deserializer: Optional[Callable[[Optional[str]], Any]] = None,
+        index_table: str = "_index",
+    ) -> None:
+        super().__init__()
+        self.connection_pool = connection_pool
+        self.serializer = serializer
+        self.deserializer = deserializer
+        self._adapter = PostgresqlKeyValueAdapter(
+            self.connection_pool, index_table, ts_col=None
+        )
+
+    def create(self, func: Union[MethodType, FunctionType]) -> PostgresqlCache:
+        table = self._normalize_table_name(self._func_to_key(func))
+        return PostgresqlCache(
+            connection_pool=self.connection_pool,
+            table=table,
+            serializer=self.serializer,
+            deserializer=self.deserializer,
+        )
+
+    def _normalize_table_name(self, table_name: str) -> str:
+        table_name = table_name.lower()
+        if len(table_name) > 63:
+            n = self._adapter.get(table_name)
+            if not n:
+                used = {int(self._adapter.get(k)) for k in self._adapter.keys()}
+                n = min({i + 1 for i in range(len(used) + 1)}.difference(used))
+                self._adapter.set(table_name, str(n))
+            return f"cache_{n}"
+        else:
+            return re.sub(r"[^\w]", "_", table_name)
---
+++ packages/thornfield/thornfield-1.3.0.tar.gz/thornfield-1.3.0/src/thornfield/caches/postgresql_cache.py
@@ -0,0 +1,40 @@
+from time import time
+from typing import Optional, Any, Callable
+
+from .cache import Cache
+from ..constants import NOT_FOUND
+from ..errors import CachingError
+from ..postgresql_key_value_adapter import (
+    PostgresqlKeyValueAdapter,
+    ConnectionPool,
+)
+
+
+class PostgresqlCache(Cache):
+    def __init__(
+        self,
+        connection_pool: ConnectionPool,
+        table: str,
+        serializer: Optional[Callable[[Any], str]] = None,
+        deserializer: Optional[Callable[[Optional[str]], Any]] = None,
+    ) -> None:
+        super().__init__(serializer=serializer, deserializer=deserializer)
+        self._adapter = PostgresqlKeyValueAdapter(connection_pool, table)
+
+    def get(self, key):
+        t = round(time() * 1000)
+        try:
+            value = self._adapter.get(self._serialize(key), t)
+            if value is None:
+                return NOT_FOUND
+            return self._deserialize(value)
+        except Exception as e:
+            raise CachingError(f"Could not get {key}", exc=e)
+
+    def set(self, key, value, expiration: int):
+        if expiration:
+            expiration = round(time() * 1000) + expiration
+        try:
+            self._adapter.set(self._serialize(key), self._serialize(value), expiration)
+        except Exception as e:
+            raise CachingError(f"Could not set {key} as {value}", exc=e)
