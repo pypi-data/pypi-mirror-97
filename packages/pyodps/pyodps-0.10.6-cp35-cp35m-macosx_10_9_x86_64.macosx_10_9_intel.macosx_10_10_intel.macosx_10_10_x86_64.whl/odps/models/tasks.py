62313896e92b62d7dce27982759e738b518678d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:19 +0000

    Add package pyodps/0.10.6/pyodps-0.10.6-cp35-cp35m-macosx_10_9_x86_64.macosx_10_9_intel.macosx_10_10_intel.macosx_10_10_x86_64.whl

---
+++ packages/pyodps/pyodps-0.10.6-cp35-cp35m-macosx_10_9_x86_64.macosx_10_9_intel.macosx_10_10_intel.macosx_10_10_x86_64.whl/odps/models/tasks.py
@@ -0,0 +1,279 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 1999-2017 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json  # don't remove
+import random
+import time
+import warnings
+
+from .core import AbstractXMLRemoteModel
+from .. import serializers, errors, compat
+from ..compat import six
+from ..config import options
+
+
+class Task(AbstractXMLRemoteModel):
+
+    __slots__ = 'name', 'comment', 'properties'
+
+    _type_indicator = 'type'
+
+    name = serializers.XMLNodeField('Name')
+    type = serializers.XMLTagField('.')
+    comment = serializers.XMLNodeField('Comment')
+    properties = serializers.XMLNodePropertiesField('Config', 'Property',
+                                                    key_tag='Name', value_tag='Value')
+
+    def __new__(cls, *args, **kwargs):
+        typo = kwargs.get('type')
+
+        if typo is not None:
+            task_cls = None
+            for v in six.itervalues(globals()):
+                if not isinstance(v, type) or not issubclass(v, Task):
+                    continue
+                cls_type = getattr(v, '_root', v.__name__)
+                if typo == cls_type:
+                    task_cls = v
+            if task_cls is None:
+                task_cls = cls
+        else:
+            task_cls = cls
+
+        return object.__new__(task_cls)
+
+    def set_property(self, key, value):
+        if self.properties is None:
+            self.properties = compat.OrderedDict()
+        self.properties[key] = value
+
+    def _update_property_json(self, field, value):
+        def update(kv, dest):
+            if not kv:
+                return
+            for k, v in six.iteritems(kv):
+                if isinstance(v, bool):
+                    dest[k] = 'true' if v else 'false'
+                else:
+                    dest[k] = str(v)
+
+        if self.properties is None:
+            self.properties = compat.OrderedDict()
+        if field in self.properties:
+            settings = json.loads(self.properties[field])
+        else:
+            settings = compat.OrderedDict()
+        update(value, settings)
+        self.properties[field] = json.dumps(settings)
+
+    def update_settings(self, value):
+        self._update_property_json('settings', value)
+
+    def serialize(self):
+        if type(self) is Task:
+            raise errors.ODPSError('Unknown task type')
+        return super(Task, self).serialize()
+
+    @property
+    def instance(self):
+        return self.parent.parent
+
+    @property
+    def progress(self):
+        """
+        Get progress of a task.
+        """
+        return self.instance.get_task_progress(self.name)
+
+    @property
+    def stages(self):
+        """
+        Get execution stages of a task.
+        """
+        return self.instance.get_task_progress(self.name).stages
+
+    @property
+    def result(self):
+        """
+        Get execution result of the task.
+        """
+        return self.instance.get_task_result(self.name)
+
+    @property
+    def summary(self):
+        """
+        Get execution summary of the task.
+        """
+        return self.instance.get_task_summary(self.name)
+
+    @property
+    def detail(self):
+        """
+        Get execution details of the task.
+        """
+        return self.instance.get_task_detail(self.name)
+
+    @property
+    def quota(self):
+        """
+        Get quota json of the task.
+        """
+        return self.instance.get_task_quota(self.name)
+
+    @property
+    def workers(self):
+        """
+        Get workers of the task.
+        """
+        return self.instance.get_task_workers(self.name)
+
+    def get_info(self, key):
+        """
+        Get associated information of the task.
+        """
+        return self.instance.get_task_info(self.name, key)
+
+    def put_info(self, key, value):
+        """
+        Put associated information of the task.
+        """
+        return self.instance.put_task_info(self.name, key, value)
+
+
+def format_cdata(query, semicolon=False):
+    stripped_query = query.strip()
+    if semicolon and not stripped_query.endswith(';'):
+        stripped_query += ';'
+    return '<![CDATA[%s]]>' % stripped_query
+
+
+def collect_settings(value, glob):
+    settings = dict()
+    if glob:
+        if options.sql.use_odps2_extension:
+            settings['odps.sql.type.system.odps2'] = True
+        if options.local_timezone is not None:
+            if not options.local_timezone:
+                settings['odps.sql.timezone'] = 'Etc/GMT'
+            elif isinstance(options.local_timezone, bool):
+                from ..lib import tzlocal
+                settings['odps.sql.timezone'] = tzlocal.get_localzone().zone
+            elif isinstance(options.local_timezone, six.string_types):
+                settings['odps.sql.timezone'] = options.local_timezone
+            else:
+                zone_str = getattr(options.local_timezone, 'zone', None)
+                if zone_str is None:
+                    warnings.warn('Failed to get timezone string from options.local_timezone. '
+                                  'You need to deal with timezone in the return data yourself.')
+                else:
+                    settings['odps.sql.timezone'] = zone_str
+        if options.sql.settings:
+            settings.update(options.sql.settings)
+    if value:
+        settings.update(value)
+    return settings
+
+
+class SQLTask(Task):
+    __slots__ = '_anonymous_sql_task_name',
+
+    _root = 'SQL'
+    _anonymous_sql_task_name = 'AnonymousSQLTask'
+
+    query = serializers.XMLNodeField('Query',
+                                     serialize_callback=lambda s: format_cdata(s, True))
+
+    def __init__(self, **kwargs):
+        if 'name' not in kwargs:
+            kwargs['name'] = SQLTask._anonymous_sql_task_name
+        super(SQLTask, self).__init__(**kwargs)
+
+    def serial(self):
+        if self.properties is None:
+            self.properties = compat.OrderedDict()
+
+        key = 'settings'
+        if key not in self.properties:
+            self.properties[key] = '{"odps.sql.udf.strict.mode": "true"}'
+
+        return super(SQLTask, self).serial()
+
+    def update_sql_settings(self, value=None, glob=True):
+        settings = collect_settings(value, glob)
+        self.update_settings(settings)
+
+    def update_aliases(self, value):
+        self._update_property_json('aliases', value)
+
+    @property
+    def warnings(self):
+        return json.loads(self.get_info('warnings')).get('warnings')
+
+
+class MergeTask(Task):
+    _root = 'Merge'
+
+    table = serializers.XMLNodeField('TableName')
+
+    def __init__(self, name=None, **kwargs):
+        if name is None:
+            name = 'merge_task_{0}_{1}'.format(int(time.time()), random.randint(100000, 999999))
+        kwargs['name'] = name
+        super(MergeTask, self).__init__(**kwargs)
+
+
+class CupidTask(Task):
+    _root = 'CUPID'
+
+    plan = serializers.XMLNodeField('Plan', serialize_callback=format_cdata)
+
+    def __init__(self, name=None, plan=None, hints=None, **kwargs):
+        kwargs['name'] = name
+        kwargs['plan'] = plan
+        super(CupidTask, self).__init__(**kwargs)
+        hints = hints or {}
+        self.set_property('type', 'cupid')
+        if hints:
+            self.set_property('settings', json.dumps(hints))
+
+
+class SQLCostTask(Task):
+    __slots__ = '_anonymous_sql_cost_task_name',
+
+    _root = 'SQLCost'
+    _anonymous_sql_cost_task_name = 'AnonymousSQLCostTask'
+
+    query = serializers.XMLNodeField('Query',
+                                     serialize_callback=lambda s: format_cdata(s, True))
+
+    def __init__(self, **kwargs):
+        if 'name' not in kwargs:
+            kwargs['name'] = self._anonymous_sql_cost_task_name
+        super(SQLCostTask, self).__init__(**kwargs)
+
+    def update_sql_cost_settings(self, value=None, glob=True):
+        settings = collect_settings(value, glob)
+        self.update_settings(settings)
+
+
+class SQLRTTask(Task):
+    _root = "SQLRT"
+
+
+try:
+    from ..internal.models.tasks import *
+except ImportError:
+    pass
---
+++ packages/pyodps/pyodps-0.10.6-cp35-cp35m-macosx_10_9_x86_64.macosx_10_9_intel.macosx_10_10_intel.macosx_10_10_x86_64.whl/odps/static/ui/src/common.js
@@ -0,0 +1,223 @@
+/*
+ * Copyright 1999-2017 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ * Common PyODPS Javascript Module
+ */
+
+require(['base/js/utils'], function(jupyter_utils) {
+    jupyter_utils.load_extension('jupyter-js-widgets/extension').then(function () {
+        require(['@jupyter-widgets/base'], function (_) {
+            window.console.log('ipywidgets later than 7.0.0 detected.');
+        }, function () {
+            require.config({
+                map: {
+                    '*': {
+                        '@jupyter-widgets/base': 'nbextensions/jupyter-js-widgets/extension',
+                        '@jupyter-widgets/controls': 'nbextensions/jupyter-js-widgets/extension',
+                        '@jupyter-widgets/output': 'nbextensions/jupyter-js-widgets/extension',
+                    }
+                }
+            });
+            window.console.log('ipywidgets between 4.0.0 and 7.0.0 detected.');
+        });
+    }, function () {
+        require.config({
+            map: {
+                '*': {
+                    '@jupyter-widgets/base': 'nbextensions/widgets/widgets/js/widget',
+                    '@jupyter-widgets/controls': 'nbextensions/widgets/widgets/js/widget',
+                    '@jupyter-widgets/output': 'nbextensions/widgets/widgets/js/widget',
+                }
+            }
+        });
+        window.console.log('ipywidgets prior than 4.0.0 detected.');
+    });
+});
+
+var pyodps_init_time = new Date();
+
+define('pyodps/common', ['jquery', 'base/js/namespace', '@jupyter-widgets/base'], function ($, Jupyter) {
+    "use strict";
+
+    var entityMap = {
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        '"': '&quot;',
+        "'": '&#39;',
+        "/": '&#x2F;'
+    };
+
+    function escape_html(string) {
+        return String(string).replace(/[&<>"'\/]/g, function (s) {
+            return entityMap[s];
+        });
+    }
+
+    var view_prompts = {};
+
+    var register_css = function (url) {
+        var nbext_path = require.toUrl('nbextensions');
+        var user_base_path = nbext_path.substr(0, nbext_path.indexOf('nbextensions'));
+        require.config({
+            paths: {
+                pyodps: user_base_path + 'nbextensions/pyodps'
+            }
+        });
+
+        if ($('style[data-pyodps-styles="' + url + '"]').length > 0) return;
+        var url_parts = require.toUrl(url).split('?', 1);
+        var css_url = url_parts[0] + '.css';
+        if (url_parts.length > 1) css_url += '?' + url_parts[1];
+        $('head').append('<link type="text/css" rel="stylesheet" href="' + css_url + '" data-pyodps-styles="' + url + '">');
+    };
+
+    var _instant_widgets = {};
+
+    var _get_widget_cell = function (_widget) {
+        if (_widget.options.cell)
+            return _widget.options.cell;
+        return $(_widget.options.output.element).closest('.cell').data('cell');
+    };
+
+    var _auto_close_widgets = function () {
+        var cells = Jupyter.notebook.get_cells();
+        for (var i in cells) {
+            var output_area = cells[i].output_area;
+            var new_outputs = [];
+            for (var j in output_area.outputs) {
+                var o = output_area.outputs[j];
+                if (undefined !== o.data) {
+                    var o_data = o.data['application/vnd.jupyter.widget-view+json'];
+                    if (undefined !== o_data) {
+                        if (_instant_widgets[o_data.model_id]) {
+                            delete _instant_widgets[o_data.model_id];
+                            continue;
+                        }
+                    }
+                }
+                new_outputs.push(o);
+            }
+            output_area.outputs = new_outputs;
+        }
+
+        var _hide_empty_widgets = function() {
+            for (var i in cells) {
+                var cell_el = $(cells[i].element);
+                if (!cell_el.hasClass('running')) {
+                    cell_el.find('.output_area:has(.jupyter-widgets-view)').each(function () {
+                        if ($(this).text() === '') $(this).remove();
+                    });
+                }
+            }
+        };
+        window.setTimeout(_hide_empty_widgets, 500);
+    };
+
+    $([Jupyter.events]).on('kernel_idle.Kernel', _auto_close_widgets);
+    $([Jupyter.events]).on('kernel_ready.Kernel', _auto_close_widgets);
+
+    var add_transient_widgets = function (comm_id) {
+        _instant_widgets[comm_id] = true;
+    };
+
+    var close_widget = function (widget) {
+        var widget_output_area = _get_widget_cell(widget).output_area;
+        var widget_area = $(widget.$el).closest('.output_area:has(.jupyter-widgets-view)');
+        if (widget_area.length > 0)
+            widget_area.hide();
+        else {
+            $(widget.$el).closest('.widget-area').children('div').map(function(idx, o) {
+                if ($(o).text() === '') $(o).hide();
+            });
+            widget.remove();
+        }
+
+        var new_outputs = [];
+        for (var i in widget_output_area.outputs) {
+            var o = widget_output_area.outputs[i];
+            if (o.data === undefined) {
+                new_outputs.push(o);
+            } else {
+                var o_data = o.data['application/vnd.jupyter.widget-view+json'];
+                if (undefined === o_data || o_data.model_id !== widget.model.comm.comm_id)
+                    new_outputs.push(o);
+            }
+        }
+        widget_output_area.outputs = new_outputs;
+    };
+
+    /**
+     * Install a hook for a widget on termination of cell execution
+     * @param _widget The widget object
+     * @param func The function to be hooked
+     */
+    var call_on_executed = function (_widget, func) {
+        var view_name = _widget.model.get('_view_name');
+        var cell_obj = _get_widget_cell(_widget);
+        if (undefined === view_prompts[view_name]) {
+            view_prompts[view_name] = {
+                prompt_num: -1,
+                func: func,
+            };
+
+            var notifier = function () {
+                // only display notifications when the cell stops running.
+                if ($(cell_obj.element).hasClass('running'))
+                    window.setTimeout(notifier, 100);
+                else {
+                    // ensure that notifications for a cell appear only once.
+                    var prompt_num = cell_obj.input_prompt_number;
+                    if (prompt_num !== view_prompts[view_name].prompt_num) {
+                        view_prompts[view_name].prompt_num = prompt_num;
+                        view_prompts[view_name].func.apply(_widget);
+                    }
+                    // make sure that notification will reoccur when rerun this cell.
+                    view_prompts[view_name] = undefined;
+                }
+            };
+            window.setTimeout(notifier, 100);
+        } else {
+            view_prompts[view_name].func = func
+        }
+    };
+    $([Jupyter.events]).on('kernel_restarting.Kernel', function () {
+        view_prompts = {};
+    });
+
+    register_css('nbextensions/pyodps/styles');
+
+    return {
+        init_time: pyodps_init_time,
+        register_css: register_css,
+        call_on_executed: call_on_executed,
+        add_transient_widgets: add_transient_widgets,
+        close_widget: close_widget,
+        escape_html: escape_html,
+    }
+});
+
+define('nbextensions/pyodps/main', ['base/js/namespace'], function(Jupyter) {
+    return {
+        load_ipython_extension: function() {
+            if (Jupyter.CodeCell.config_defaults) {
+                Jupyter.CodeCell.config_defaults.highlight_modes['magic_sql'] = {'reg':[/^%%sql/]};
+            } else {
+                Jupyter.CodeCell.options_default.highlight_modes['magic_sql'] = {'reg':[/^%%sql/]};
+            }
+        }
+    }
+});
