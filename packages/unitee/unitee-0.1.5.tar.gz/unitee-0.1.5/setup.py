09b66640ed40eb682de911d98dee648dbef68fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:49 +0000

    Add package unitee/0.1.5/unitee-0.1.5.tar.gz

---
+++ packages/unitee/unitee-0.1.5.tar.gz/unitee-0.1.5/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['unitee']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'unitee',
+    'version': '0.1.5',
+    'description': 'super simple units',
+    'long_description': None,
+    'author': '0Hughman0',
+    'author_email': 'rammers2@hotmail.co.uk',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
