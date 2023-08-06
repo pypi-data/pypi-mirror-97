1b62887ac644586090f667444045cd43f4873f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:16 +0000

    Add package coloredterm/0.1.8/coloredterm-0.1.8.tar.gz

---
+++ packages/coloredterm/coloredterm-0.1.8.tar.gz/coloredterm-0.1.8/coloredterm/__init__.py
@@ -0,0 +1,30 @@
+from coloredterm.coloredterm import (
+    Back,
+    bg,
+    colored,
+    colors,
+    cprint,
+    fg,
+    Fore,
+    names,
+    pattern_input,
+    pattern_print,
+    rand,
+    Style
+)
+
+__version__ = "0.1.8"
+__all__ = [
+    'Back',
+    'bg',
+    'colored',
+    'colors',
+    'cprint',
+    'fg',
+    'Fore',
+    'names',
+    'pattern_input',
+    'pattern_print',
+    'rand',
+    'Style'
+]
