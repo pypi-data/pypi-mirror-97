7d04f4c8231bf0622c917f9324a90a72a715fb27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:48 +0000

    Add package terra-sdk/0.12.0/terra_sdk-0.12.0-py3-none-any.whl

---
+++ packages/terra-sdk/terra_sdk-0.12.0-py3-none-any.whl/terra_sdk/client/lcd/__init__.py
@@ -0,0 +1,4 @@
+from .lcdclient import AsyncLCDClient, LCDClient
+from .wallet import AsyncWallet, Wallet
+
+__all__ = ["AsyncLCDClient", "LCDClient", "AsyncWallet", "Wallet"]
---
+++ packages/terra-sdk/terra_sdk-0.12.0-py3-none-any.whl/terra_sdk/client/lcd/api/auth.py
@@ -0,0 +1,37 @@
+from typing import Union
+
+from terra_sdk.core import AccAddress
+from terra_sdk.core.auth import Account, LazyGradedVestingAccount
+
+from ._base import BaseAsyncAPI, sync_bind
+
+__all__ = ["AsyncAuthAPI", "AuthAPI"]
+
+
+class AsyncAuthAPI(BaseAsyncAPI):
+    async def account_info(
+        self, address: AccAddress
+    ) -> Union[Account, LazyGradedVestingAccount]:
+        """Fetches the account information.
+
+        Args:
+            address (AccAddress): account address
+
+        Returns:
+            Union[Account, LazyGradedVestingAccount]: account information
+        """
+        result = await self._c._get(f"/auth/accounts/{address}")
+        if result["type"] == "core/Account":
+            return Account.from_data(result)
+        else:
+            return LazyGradedVestingAccount.from_data(result)
+
+
+class AuthAPI(AsyncAuthAPI):
+    @sync_bind(AsyncAuthAPI.account_info)
+    def account_info(
+        self, address: AccAddress
+    ) -> Union[Account, LazyGradedVestingAccount]:
+        pass
+
+    account_info.__doc__ = AsyncAuthAPI.account_info.__doc__
