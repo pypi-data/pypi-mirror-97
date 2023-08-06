a64dbcf50e8560e4c03ad9fb2b8a80f5ca628971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:58 +0000

    Add package compai/0.1.2/compai-0.1.2-py3-none-any.whl

---
+++ packages/compai/compai-0.1.2-py3-none-any.whl/compai/__init__.py
@@ -0,0 +1,21 @@
+from importlib.metadata import version
+
+from .compai import (apply, compose, const, dict_map, ffilter, fmap, identity,
+                     length, none_map, swap, tuple_map, tupled)
+
+__version__ = version('compai')
+
+__all__ = [
+    'compose',
+    'fmap',
+    'none_map',
+    'dict_map',
+    'tupled',
+    'const',
+    'swap',
+    'tuple_map',
+    'length',
+    'identity',
+    'apply',
+    'ffilter',
+]
