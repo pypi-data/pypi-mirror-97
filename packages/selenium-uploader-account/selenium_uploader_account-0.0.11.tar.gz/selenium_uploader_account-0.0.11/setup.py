21884b0a8cbe4755bd912cbc33db31285ec5e034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:49 +0000

    Add package selenium-uploader-account/0.0.11/selenium_uploader_account-0.0.11.tar.gz

---
+++ packages/selenium-uploader-account/selenium_uploader_account-0.0.11.tar.gz/selenium_uploader_account-0.0.11/setup.py
@@ -0,0 +1,28 @@
+import setuptools, os
+
+readme_path = os.path.join(os.getcwd(), "README.md")
+if os.path.exists(readme_path):
+    with open(readme_path, "r") as f:
+        long_description = f.read()
+else:
+    long_description = 'selenium_uploader_account'
+
+setuptools.setup(
+    name="selenium_uploader_account",
+    version="0.0.11",
+    author="Kristof",
+    description="selenium_uploader_account",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kkristof200/selenium_uploader_account",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'selenium-account>=0.1.13'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
