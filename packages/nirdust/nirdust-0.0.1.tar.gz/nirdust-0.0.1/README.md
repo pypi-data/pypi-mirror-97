53d5f6daa881f152def42717f366a6fe432010d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:27 +0000

    Add package nirdust/0.0.1/nirdust-0.0.1.tar.gz

---
+++ packages/nirdust/nirdust-0.0.1.tar.gz/nirdust-0.0.1/README.md
@@ -0,0 +1,55 @@
+[![Build Status](https://travis-ci.com/Gaiana/nirdust.svg?branch=main)](https://travis-ci.com/Gaiana/nirdust)
+[![https://github.com/leliel12/diseno_sci_sfw](https://img.shields.io/badge/DiSoftCompCi-FAMAF-ffda00)](https://github.com/leliel12/diseno_sci_sfw)
+[![Documentation Status](https://readthedocs.org/projects/nirdust/badge/?version=latest)](https://nirdust.readthedocs.io/en/latest/?badge=latest)
+
+# Nirdust: Near Infrared Dust Finder
+
+Nirdust is a python package that uses K-band (2.2 micrometers) spectra to 
+measure the temperature of the dust heated by an Active Galactic Nuclei (AGN) 
+accretion disk. 
+
+
+## Motivation:
+
+K-band nuclear spectral continuum of Type 2 AGNs is often composed of two 
+components: the stellar population emission and a hot 800 - 1600 K dust component.
+Via subtraction of the stellar emission dust component fitting can be performed
+to map its presence and estimate its temperature.
+
+
+## Features
+
+The package uses the modeling features of astropy to fit the hot dust component 
+of a AGN K-band spectrum with black body functions. And provide a class with
+methods for spectrum manipulation and normalized-blackbody-fitting. Because 
+Nirdust normalizes the spectra before fitting, is not necessary to 
+flux-calibrate spectra to use it. 
+
+Nirdust needs a minimum of two spectra to run: a nuclear one, where the dust 
+temperature will be determined, and an off-nuclear spectrum, where the emission 
+is considered to be purely stellar. The off-nuclear spectrum will be used by
+Nirdust to subtract the stellar emission from the nuclear spectrum. 
+
+
+
+
+Footnote: the hot dust component may or may not be present in your type 2 
+nuclei, do not get disappointed if Nirdust finds nothing.
+
+
+## Requeriments
+
+You will need Python 3.8 or 3.9 to run Nirdust.
+
+## Installation
+
+Clone this repo and then inside the local directory execute
+
+``` python
+$ pip install -e .
+```
+
+Authors: Gaia Gaspar, José Alacoria.
+
+
+
---
+++ packages/nirdust/nirdust-0.0.1.tar.gz/nirdust-0.0.1/setup.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of the
+#   NirDust Project (https://github.com/Gaiana/nirdust)
+# Copyright (c) 2020, Gaia Gaspar, Jose Alacoria
+# License: MIT
+#   Full Text: https://github.com/Gaiana/nirdust/LICENSE
+
+
+# =============================================================================
+# DOCS
+# =============================================================================
+
+"""This file is for distribute and install NirDust
+"""
+
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+import os
+import pathlib
+
+from ez_setup import use_setuptools
+
+from setuptools import setup
+
+use_setuptools()
+
+# =============================================================================
+# CONSTANTS
+# =============================================================================
+
+REQUIREMENTS = [
+    "numpy",
+    "scipy",
+    "astropy",
+    "specutils",
+    "matplotlib",
+    "attrs",
+]
+
+PATH = pathlib.Path(os.path.abspath(os.path.dirname(__file__)))
+
+with open(PATH / "README.md") as fp:
+    LONG_DESCRIPTION = fp.read()
+
+
+DESCRIPTION = "Measure temperature of hot dust in type 2 AGN"
+
+
+# =============================================================================
+# FUNCTIONS
+# =============================================================================
+
+
+def do_setup():
+    setup(
+        name="nirdust",
+        version="0.0.1",
+        description=DESCRIPTION,
+        long_description=open("README.md").read(),
+        long_description_content_type="text/markdown",
+        author=["Gaia Gaspar", "Jose Alacoria"],
+        author_email="gaiagaspar@gmail.com",
+        url="https://github.com/Gaiana/nirdust",
+        py_modules=["nirdust", "ez_setup"],
+        license="MIT",
+        keywords=["nirdust", "dust", "AGN", "NIR", "temperatures"],
+        classifiers=[
+            "Intended Audience :: Education",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3.8",
+        ],
+        install_requires=REQUIREMENTS,
+    )
+
+
+if __name__ == "__main__":
+    do_setup()
