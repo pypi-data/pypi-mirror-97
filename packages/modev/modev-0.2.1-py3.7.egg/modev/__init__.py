b7d5e5a730b3e38b8d5c871bd6ea91d8154d758c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:05 +0000

    Add package modev/0.2.1/modev-0.2.1-py3.7.egg

---
+++ packages/modev/modev-0.2.1-py3.7.egg/modev/__init__.py
@@ -0,0 +1,13 @@
+import modev.approaches
+import modev.common
+import modev.default_pars
+import modev.etl
+import modev.evaluation
+import modev.execution
+import modev.exploration
+from modev.pipeline import Pipeline
+import modev.plotting
+import modev.selection
+import modev.utils
+import modev.templates
+import modev.validation
