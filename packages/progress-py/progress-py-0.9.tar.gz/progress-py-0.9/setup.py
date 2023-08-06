b91eb81474564c7bf361b6e6efe3c38503f62257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:51 +0000

    Add package progress-py/0.9/progress-py-0.9.tar.gz

---
+++ packages/progress-py/progress-py-0.9.tar.gz/progress-py-0.9/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='progress-py',
+    version='0.9',
+    description='Make customizable progress bars and spinners for cli',
+    author='Priyansh Anand',
+    author_email='pr1y4nsh@protonmail.com',
+    license='MIT',
+    packages=['progress_py']
+)
