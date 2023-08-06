4d292b1cf927067536741fd5f88595bc2e072c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:38 +0000

    Add package Data-HandWrite/1.1.0/Data_HandWrite-1.1.0.tar.gz

---
+++ packages/Data-HandWrite/Data_HandWrite-1.1.0.tar.gz/Data_HandWrite-1.1.0/setup.py
@@ -0,0 +1,33 @@
+# coding:utf-8
+
+from setuptools import setup
+# or
+# from distutils.core import setup  
+foruser = '''# Author:KuoYuan Li
+[![N|Solid](https://images2.imgbox.com/8f/03/gv0QnOdH_o.png)](https://sites.google.com/ms2.ccsh.tn.edu.tw/pclearn0915)
+本程式提供功能如下
+1.輕量手寫數字資料庫(訓練資料640張圖，測試資料50張圖)
+2.建立手寫圖片的windows小程式(paint.exe) 使用 dowloadPaintToolKit() 下載
+ '''
+setup(
+        name='Data_HandWrite',   
+        version='1.1.0',  
+        description='Hand write toolkit and data for windows',
+        long_description=foruser,
+        author='KuoYuan Li',  
+        author_email='funny4875@gmail.com',  
+        url='https://pypi.org/project/Data_HandWrite',      
+        packages=['Data_HandWrite'],   
+        include_package_data=True,
+        keywords = ['machine learning','HandWrite','data'],   # Keywords that define your package best
+          install_requires=[ 
+          'numpy',
+          'matplotlib'
+          ],
+      classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+      ],
+)
