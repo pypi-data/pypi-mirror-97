e34aa7c17bbf199a46db3eb8dcdd1f4fe79154ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:25 +0000

    Add package django-daylessdate/0.2/django_daylessdate-0.2-py3-none-any.whl

---
+++ packages/django-daylessdate/django_daylessdate-0.2-py3-none-any.whl/djangodaylessdate/apps.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class DjangoDaylessDateConfig(AppConfig):
+    name = 'djangodaylessdate'
+    verbose_name = 'djangodaylessdate'
