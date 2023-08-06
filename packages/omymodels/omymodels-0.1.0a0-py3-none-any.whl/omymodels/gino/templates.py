003f52c873905bf3fd080b787ebefc21b4af5903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:06 +0000

    Add package omymodels/0.1.0a0/omymodels-0.1.0a0-py3-none-any.whl

---
+++ packages/omymodels/omymodels-0.1.0a0-py3-none-any.whl/omymodels/gino/templates.py
@@ -0,0 +1,31 @@
+# imports 
+postgresql_dialect_import = "from sqlalchemy.dialects.postgresql import {types}"
+sql_alchemy_func_import = "from sqlalchemy.sql import func"
+uniquer_constraint_import = "from sqlalchemy import UniqueConstraint"
+
+gino_import = "from gino import Gino"
+
+gino_init = 'db = Gino()'
+gino_init_schema = 'db = Gino(schema="{schema}")'
+
+# model defenition 
+model_template = """
+
+class {model_name}(db.Model):\n
+    __tablename__ = \'{table_name}\'\n
+"""
+
+# columns defenition
+column_template = """    {column_name} = db.Column({column_type}"""
+required = ", nullable=False"
+default = ", server_default={default}"
+pk_template = ", primary_key=True"
+autoincrement = ", autoincrement=True"
+index = ", index=True"
+
+# tables properties
+fk_template = """
+    {fk_name} = db.ForeignKeyConstraint(
+        [{fk_columns}], [{fk_references_columns}])
+"""
+
