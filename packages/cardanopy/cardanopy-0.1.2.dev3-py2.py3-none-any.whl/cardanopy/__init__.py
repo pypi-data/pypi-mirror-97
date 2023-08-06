60757fce5b8870adff4d47007dc1cb5b0f2ffe91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:51 +0000

    Add package cardanopy/0.1.2.dev3/cardanopy-0.1.2.dev3-py2.py3-none-any.whl

---
+++ packages/cardanopy/cardanopy-0.1.2.dev3-py2.py3-none-any.whl/cardanopy/__init__.py
@@ -0,0 +1,20 @@
+__version__ = "0.1.2-dev3"
+__license__ = "Apache-2.0 License"
+__title__ = "cardanopy"
+
+import click
+
+from .apply import apply
+from .run import run
+from .cli import cli
+
+@click.group()
+@click.version_option(version=__version__)
+@click.pass_context
+def main(ctx):
+    pass
+
+# export
+main.add_command(apply)
+main.add_command(run)
+main.add_command(cli)
