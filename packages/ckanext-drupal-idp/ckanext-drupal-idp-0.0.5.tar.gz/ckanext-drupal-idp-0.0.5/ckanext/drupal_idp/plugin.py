13fd858fdd57df83e34d97d69785462452c6b1d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:42 +0000

    Add package ckanext-drupal-idp/0.0.5/ckanext-drupal-idp-0.0.5.tar.gz

---
+++ packages/ckanext-drupal-idp/ckanext-drupal-idp-0.0.5.tar.gz/ckanext-drupal-idp-0.0.5/ckanext/drupal_idp/plugin.py
@@ -0,0 +1,54 @@
+import logging
+
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as tk
+
+
+import ckanext.drupal_idp.utils as utils
+import ckanext.drupal_idp.helpers as helpers
+
+
+log = logging.getLogger(__name__)
+
+
+class DrupalIdpPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IAuthenticator, inherit=True)
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
+
+    # ITemplateHelpers
+
+    def get_helpers(self):
+        return helpers.get_helpers()
+
+    # IAuthenticator
+
+    def identify(self):
+        """This does drupal authorization.
+        The drupal session contains the drupal id of the logged in user.
+        We need to convert this to represent the ckan user."""
+        cookie_sid = tk.request.cookies.get(utils.session_cookie_name())
+        if not cookie_sid:
+            return
+        sid = utils.decode_sid(cookie_sid)
+        details = utils.get_user_details(sid)
+        if not details:
+            return
+        try:
+            user = utils.get_or_create_from_details(details)
+            if utils.is_synchronization_enabled():
+                user = utils.synchronize(user, details)
+        except tk.ValidationError as e:
+            log.error(
+                f"Cannot create user {details.name}<{details.email}>:"
+                f" {e.error_summary}"
+            )
+            return
+        tk.c.user = user["name"]
+
+    # IConfigurer
+
+    def update_config(self, config_):
+        # If DB config is missing, the following line will raise
+        # CkaneConfigurationException and won't allow server to start
+        utils.db_url()
---
+++ packages/ckanext-drupal-idp/ckanext-drupal-idp-0.0.5.tar.gz/ckanext-drupal-idp-0.0.5/ckanext/drupal_idp/tests/test_utils.py
@@ -0,0 +1,136 @@
+import pytest
+
+import ckan.model as model
+import ckan.lib.munge as munge
+from ckan.exceptions import CkanConfigurationException
+
+import ckanext.drupal_idp.utils as utils
+
+
+class TestDetails:
+    def test_init(self, details_data):
+        details = utils.Details(details_data)
+        for prop in details_data:
+            assert getattr(details, prop) == details_data[prop]
+
+    def test_into_dict(self, details_data):
+        details = utils.Details(details_data)
+        assert dict(details) == details_data
+
+    def test_into_user(self, details_data):
+        details = utils.Details(details_data)
+        userdict = details.make_userdict()
+
+        assert userdict == {
+            "email": details_data["email"],
+            "name": munge.munge_name(details_data["name"]),
+            "plugin_extras": {"drupal_idp": details_data},
+        }
+
+
+class TestSynchronizationEnabled:
+    def test_default_value(self):
+        assert not utils.is_synchronization_enabled()
+
+    @pytest.mark.ckan_config(utils.CONFIG_SYNCHRONIZATION_ENABLED, "true")
+    def test_updated_value(self):
+        assert utils.is_synchronization_enabled()
+
+
+class TestDbUrl:
+    def test_default_value(self):
+        assert utils.db_url()
+
+    @pytest.mark.ckan_config(utils.CONFIG_DB_URL, "")
+    def test_exception_with_missing_config(self):
+        with pytest.raises(CkanConfigurationException):
+            utils.db_url()
+
+
+class TestSessionCookieName:
+    @pytest.mark.usefixtures("with_request_context")
+    def test_http(self):
+        assert (
+            utils.session_cookie_name()
+            == "SESS49960de5880e8c687434170f6476605b"
+        )
+
+    @pytest.mark.usefixtures("with_request_context")
+    def test_https(self, ckan_config, monkeypatch):
+        monkeypatch.setitem(ckan_config, "ckan.site_url", "https://test.net")
+        assert (
+            utils.session_cookie_name()
+            == "SSESS49960de5880e8c687434170f6476605b"
+        )
+
+    @pytest.mark.ckan_config("ckanext.drupal_idp.host", "my.site.com")
+    def test_static_host(self):
+        assert (
+            utils.session_cookie_name()
+            == "SESSe181b034216cdf301d365b2fc8aa54db"
+        )
+
+
+
+def test_decode_sid():
+    cookie = "x7kHcLGoGrro66NTty7EPBluIVK8HNUOMvVpUT_6g7o"
+    expected = "y7ZGy1KGrckATBB5gixhyKBS2BfhMeLmdMuX4_H-Uak"
+    assert utils.decode_sid(cookie) == expected
+
+
+class TestFixtures:
+    def test_no_session(self, with_no_drupal_session):
+        assert utils.get_user_details("sid") is None
+
+    def test_with_session(self, with_drupal_session):
+        assert utils.get_user_details("sid") is not None
+
+
+@pytest.mark.usefixtures("clean_db")
+class TestGetOrCreation:
+    def test_user_created(self, details_data):
+        assert model.User.get(details_data["name"]) is None
+        details = utils.Details(details_data)
+        utils.get_or_create_from_details(details)
+        user = model.User.get(details_data["name"])
+        assert user.name == details_data["name"]
+        assert user.plugin_extras["drupal_idp"] == details_data
+
+
+@pytest.mark.usefixtures("clean_db")
+class TestGetOrCreation:
+    def test_user_sync(self, details_data, monkeypatch, ckan_config):
+        details = utils.Details(details_data)
+        userdict = utils.get_or_create_from_details(details)
+        user = model.User.get(userdict["id"])
+        user.email = "hello@world"
+        user.name = "hello"
+        model.Session.commit()
+
+        userdict = utils.get_or_create_from_details(details)
+        assert userdict["name"] != details_data["name"]
+        assert userdict["name"] == "hello"
+        user = model.User.get(userdict["id"])
+        assert user.email != details_data["email"]
+        assert user.email == "hello@world"
+
+        utils.synchronize(userdict, details)
+        userdict = utils.get_or_create_from_details(details)
+        assert userdict["name"] == details_data["name"]
+        user = model.User.get(userdict["id"])
+        assert user.email == details_data["email"]
+
+        assert (
+            model.Session.query(model.User).filter_by(name="hello").count()
+            == 0
+        )
+        assert (
+            model.Session.query(model.User)
+            .filter_by(name=details_data["name"])
+            .count()
+            == 1
+        )
+
+    @pytest.mark.xfail
+    def test_plugin_extras_not_erased(self):
+        assert False
