45e19352cc33b8b0372be7a2c8e25e55b012c725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:12 +0000

    Add package akshita-distributions-2306/2.2/akshita_distributions_2306-2.2.tar.gz

---
+++ packages/akshita-distributions-2306/akshita_distributions_2306-2.2.tar.gz/akshita_distributions_2306-2.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='akshita_distributions_2306',
+      version='2.2',
+      description='Gaussian distributions',
+      packages=['akshita_distributions_2306'],
+      zip_safe=False)
