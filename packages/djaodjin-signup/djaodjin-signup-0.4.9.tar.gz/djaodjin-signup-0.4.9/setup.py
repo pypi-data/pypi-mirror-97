5808d21d9fcf2fb696a49348e081fc3691506781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:57 +0000

    Add package djaodjin-signup/0.4.9/djaodjin-signup-0.4.9.tar.gz

---
+++ packages/djaodjin-signup/djaodjin-signup-0.4.9.tar.gz/djaodjin-signup-0.4.9/setup.py
@@ -0,0 +1,62 @@
+# Copyright (c) 2021, Djaodjin Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from distutils.core import setup
+
+import signup
+
+requirements = []
+with open('./requirements.txt') as requirements_txt:
+    for line in requirements_txt:
+        prerequisite = line.split('#')[0].strip()
+        if prerequisite:
+            requirements += [prerequisite]
+
+setup(
+    name='djaodjin-signup',
+    version=signup.__version__,
+    author='The DjaoDjin Team',
+    author_email='support@djaodjin.com',
+    install_requires=requirements,
+    packages=['signup',
+              'signup.api',
+              'signup.backends',
+              'signup.migrations',
+              'signup.views',
+              'signup.urls',
+              'signup.urls.api',
+              'signup.urls.views',
+              ],
+    package_data={'signup': ['templates/accounts/*.html',
+                             'templates/notification/*.eml',
+                             'templates/users/*.html',
+                             'static/js/*.js']},
+    url='https://github.com/djaodjin/djaodjin-signup/',
+    download_url='https://github.com/djaodjin/djaodjin-signup/tarball/%s' \
+        % signup.__version__,
+    license='BSD',
+    description="DjaoDjin's Implementation of Frictionless Sign Up",
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+)
