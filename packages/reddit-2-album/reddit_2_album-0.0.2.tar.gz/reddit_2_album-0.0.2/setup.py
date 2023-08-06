5ace11891b0cfc11ad735bfcb313123ea4709eb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:46 +0000

    Add package reddit-2-album/0.0.2/reddit_2_album-0.0.2.tar.gz

---
+++ packages/reddit-2-album/reddit_2_album-0.0.2.tar.gz/reddit_2_album-0.0.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="reddit_2_album",
+    version="0.0.2",
+    author="Yunzhi Gao",
+    author_email="gaoyunzhi@gmail.com",
+    description="Return photo list and caption (markdown format) from reddit.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gaoyunzhi/reddit_2_album",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'bs4',
+        'telegram_util',
+        'pyyaml',
+        'praw',
+        'cached_url',
+    ],
+    python_requires='>=3.0',
+)
