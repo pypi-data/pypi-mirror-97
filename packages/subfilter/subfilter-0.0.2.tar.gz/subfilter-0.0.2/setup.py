ae2f0ba80059090ff162b30c5dc6bfecd2879c45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:16 +0000

    Add package subfilter/0.0.2/subfilter-0.0.2.tar.gz

---
+++ packages/subfilter/subfilter-0.0.2.tar.gz/subfilter-0.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='subfilter',
+    packages=['subfilter'],
+    version='0.0.2',
+    author='Perzan',
+    author_email='PerzanDevelopment@gmail.com'
+)
