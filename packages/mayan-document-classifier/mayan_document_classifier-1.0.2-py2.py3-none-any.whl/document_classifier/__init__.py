27f6cb6bff3cf6b1a2da4db40888af92fbd30060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:41 +0000

    Add package mayan-document-classifier/1.0.2/mayan_document_classifier-1.0.2-py2.py3-none-any.whl

---
+++ packages/mayan-document-classifier/mayan_document_classifier-1.0.2-py2.py3-none-any.whl/document_classifier/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'document_classifier.apps.DocumentClassifierApp'
---
+++ packages/mayan-document-classifier/mayan_document_classifier-1.0.2-py2.py3-none-any.whl/document_classifier/dependencies.py
@@ -0,0 +1,16 @@
+from django.utils.translation import ugettext_lazy as _
+
+from mayan.apps.dependencies.classes import BinaryDependency, PythonDependency
+
+"""
+Add here all external Python or binaries required for the classifier
+executable to work.
+"""
+
+"""
+BinaryDependency(
+    help_text=_(
+        'Library and program to analyze document images.'
+    ), module=__name__, name='binary_name', path=DEFAULT_BINARY_PATH
+)
+"""
