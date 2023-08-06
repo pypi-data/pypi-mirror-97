cbe81024a8ec2452d35f2cafe8fb65191396c579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:11 +0000

    Add package polukrug/1.0/polukrug-1.0.tar.gz

---
+++ packages/polukrug/polukrug-1.0.tar.gz/polukrug-1.0/polukrug/__init__.py
@@ -0,0 +1,7 @@
+from turtle import*
+speed(10000000000000)
+def risui(step):
+    for i in range(180):
+        fd(step)
+        rt(1)
+        
---
+++ packages/polukrug/polukrug-1.0.tar.gz/polukrug-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='polukrug',
+      version='1.0',
+      description='Нарисуй полукруг!',
+      packages=['polukrug'],
+      author_email='p.dergalin@sch-int.ru',
+      zip_safe=False)
