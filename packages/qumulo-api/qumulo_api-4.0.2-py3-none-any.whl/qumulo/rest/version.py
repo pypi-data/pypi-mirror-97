1d7c44206320fcbe0d89bec3e9abd950f3226523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:11 +0000

    Add package qumulo-api/4.0.2/qumulo_api-4.0.2-py3-none-any.whl

---
+++ packages/qumulo-api/qumulo_api-4.0.2-py3-none-any.whl/qumulo/rest/version.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2012 Qumulo, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+import qumulo.lib.request as request
+
+from qumulo.lib.auth import Credentials
+from qumulo.lib.request import RestResponse
+
+# aliases to stop imports failing outside mypy. keep these sync with the stub file!
+ApiVersionInfo = None
+
+
+@request.request
+def version(
+    conninfo: request.Connection, credentials: Credentials
+) -> request.RestResponse:
+    method = 'GET'
+    uri = '/v1/version'
+    return request.rest_request(conninfo, credentials, method, uri)
+
+
+@request.request
+def numeric_version(
+    conninfo: request.Connection, credentials: Credentials
+) -> request.RestResponse:
+    # e.g., {... revision_id: Qumulo Core 1.2.3, ...} -> "1.2.3"
+    numeric = version(conninfo, credentials)[0]['revision_id'].rpartition(' ')[2]
+    return RestResponse(numeric, None)
