efa359581a8c3fe949f4ae19b229133bb549e92c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:38 +0000

    Add package dash-arcgis/0.0.1/dash-arcgis-0.0.1.tar.gz

---
+++ packages/dash-arcgis/dash-arcgis-0.0.1.tar.gz/dash-arcgis-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name="dash-arcgis",
+    version="0.0.1",
+    author="Alex Johnson",
+    author_email="alex@plotly.com",
+    url="https://plotly.com/dash/",
+    packages=["dash_arcgis"],
+    license="Commercial",
+    description="A Public stub for dash-arcgis by Plotly",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: Dash",
+    ],
+)
