aed60a9d3cd15c2feb42b57697eb0f977fc18715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:16 +0000

    Add package osol/0.0.1/osol-0.0.1.tar.gz

---
+++ packages/osol/osol-0.0.1.tar.gz/osol-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['osol', 'osol.extremum', 'osol.extremum.algorithms', 'osol.extremum.tools']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.20.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'osol',
+    'version': '0.0.1',
+    'description': '',
+    'long_description': None,
+    'author': 'wol4aravio',
+    'author_email': 'panovskiy.v@yandex.ru',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
