b28e4cf901818a351805926d7379a8525d66ace2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:33 +0000

    Add package lukeparser/0.3.24/lukeparser-0.3.24-cp39-cp39-manylinux1_x86_64.whl

---
+++ packages/lukeparser/lukeparser-0.3.24-cp39-cp39-manylinux1_x86_64.whl/luke/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "0.3.24"
+
+__title__         = "lukeparser"
+__description__   = "The Style of Markdown with the Power of LaTeX."
+__url__           = "https://lukeparser.github.io"
+__uri__           = __url__
+__doc__           = __description__ + " <" + __uri__ + ">"
+__documentation__ = 'https://lukeparser.github.io/lukeparser'
+__source__        = 'https://github.com/lukeparser/lukeparser/'
+__tracker__       = 'https://github.com/lukeparser/lukeparser/issues'
+
+__author__ = "David Hartmann"
+
+__license__ = "GPLv2"
+__copyright__ = "Copyright (c) 2019 David Hartmann"
