0449904dad9ce648885545f4ee5ab6a25d710da6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:04 +0000

    Add package pystache-cli/0.0.4/pystache_cli-0.0.4.tar.gz

---
+++ packages/pystache-cli/pystache_cli-0.0.4.tar.gz/pystache_cli-0.0.4/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+import pystache_cli as pkg
+
+if __name__ == "__main__":
+
+    with open("README.md", encoding="utf-8") as file:
+        long_description = file.read()
+
+    setup(
+        packages=[pkg.__name__],
+        install_requires=[
+            "pystache"
+        ],
+        name=pkg.__name__,
+        description="SHORT",  # should be one line
+        long_description=long_description,
+        long_description_content_type="text/markdown",  # text/markdown or text/x-rst or text/plain
+        version=pkg.__version__,
+        author=pkg.__author__,
+        author_email=pkg.__email__,
+        url=pkg.__url__,
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        entry_points={
+            "console_scripts": ["pystache-cli = pystache_cli.pystache_cli:main"]
+        },
+        package_data={
+            # 'package.module: [file_patterns]'  # better to use MANIFEST.in
+        },
+    )
