71bba7d6fca5bb3d7ebca4c3b1d33638cc13e18a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:53 +0000

    Add package aiostream/0.4.2/aiostream-0.4.2.tar.gz

---
+++ packages/aiostream/aiostream-0.4.2.tar.gz/aiostream-0.4.2/setup.py
@@ -0,0 +1,34 @@
+import sys
+from setuptools import setup
+
+TESTING = any(x in sys.argv for x in ["test", "pytest"])
+UPLOAD = "upload_sphinx" in sys.argv
+
+if not UPLOAD and sys.version_info < (3, 6):
+    raise RuntimeError("aiostream requires Python 3.6")
+
+with open("README.rst", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="aiostream",
+    version="0.4.2",
+    packages=["aiostream", "aiostream.stream"],
+    setup_requires=["pytest-runner" if TESTING else ""],
+    install_requires=['async_exit_stack;python_version<"3.7"'],
+    tests_require=["pytest", "pytest-asyncio", "pytest-cov"],
+    description="Generator-based operators for asynchronous iteration",
+    long_description=long_description,
+    url="https://github.com/vxgmichel/aiostream",
+    license="GPLv3",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    author="Vincent Michel",
+    author_email="vxgmichel@gmail.com",
+)
