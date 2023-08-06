d798e171a8e640ef5aad1d95ea217d5ced5de1b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:14 +0000

    Add package xsearch/1.0.0/xsearch-1.0.0.tar.gz

---
+++ packages/xsearch/xsearch-1.0.0.tar.gz/xsearch-1.0.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xsearch",
+    version="1.0.0",
+    author="Yuqing Xue",
+    author_email="xueyuqing98@163.com",
+    description="Search, export and analyse more efficiently",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/YuqingXue/xsearch",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
