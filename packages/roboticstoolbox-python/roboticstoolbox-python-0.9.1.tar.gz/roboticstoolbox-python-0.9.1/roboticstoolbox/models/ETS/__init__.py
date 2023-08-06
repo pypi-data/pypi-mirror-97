15d86052eaf6fb7b01bf98eb1f76362724600c31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:26 +0000

    Add package roboticstoolbox-python/0.9.1/roboticstoolbox-python-0.9.1.tar.gz

---
+++ packages/roboticstoolbox-python/roboticstoolbox-python-0.9.1.tar.gz/roboticstoolbox-python-0.9.1/roboticstoolbox/models/ETS/__init__.py
@@ -0,0 +1,14 @@
+from roboticstoolbox.models.ETS.Panda import Panda
+from roboticstoolbox.models.ETS.Frankie import Frankie
+from roboticstoolbox.models.ETS.Puma560 import Puma560
+from roboticstoolbox.models.ETS.Planar_Y import Planar_Y
+from roboticstoolbox.models.ETS.GenericSeven import GenericSeven
+
+
+__all__ = [
+    'Panda',
+    'Frankie',
+    'Puma560',
+    'Planar_Y',
+    'GenericSeven'
+]
