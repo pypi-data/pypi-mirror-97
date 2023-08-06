b6b3e54b361fce9e7eb61a70776accb496f91450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:30 +0000

    Add package psycopg2-connection/0.1.1.1/psycopg2-connection-0.1.1.1.tar.gz

---
+++ packages/psycopg2-connection/psycopg2-connection-0.1.1.1.tar.gz/psycopg2-connection-0.1.1.1/setup.py
@@ -0,0 +1,26 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / 'README.md').read_text()
+
+setup(
+    name='psycopg2-connection',
+    version='0.1.1.1',
+    description='Abstract away common patterns you might use in psycopg2.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ethancarlsson/psycopg2-mini-abstractions",
+    author="Ethan Carlsson",
+    author_email="ethanmcarlsson@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["psycopg2_connection"],
+    include_package_data=True,
+    install_requires=["psycopg2"]
+)
