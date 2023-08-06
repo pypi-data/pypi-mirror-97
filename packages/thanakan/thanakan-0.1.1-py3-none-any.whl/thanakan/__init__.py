0e2e6471efa80b62738dfdcc7d62982dd45bca05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:49 +0000

    Add package thanakan/0.1.1/thanakan-0.1.1-py3-none-any.whl

---
+++ packages/thanakan/thanakan-0.1.1-py3-none-any.whl/thanakan/__init__.py
@@ -0,0 +1,13 @@
+from .slip.qrcode import QRData
+
+try:
+    from importlib.metadata import PackageNotFoundError, version
+except ImportError:  # pragma: no cover
+    # noinspection PyUnresolvedReferences,PyUnresolvedReferences,PyUnresolvedReferences
+    from importlib_metadata import PackageNotFoundError, version
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
