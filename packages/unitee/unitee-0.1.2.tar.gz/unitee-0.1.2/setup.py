187fb7b5320e0a91dd1fc1b70956717fc2d0db6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:41 +0000

    Add package unitee/0.1.2/unitee-0.1.2.tar.gz

---
+++ packages/unitee/unitee-0.1.2.tar.gz/unitee-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import toml 
+import os
+
+
+with open('pyproject.toml') as fs:
+    pyproject = toml.load(fs)['tool']['poetry']
+
+
+if __name__ == "__main__":
+    setup(name=pyproject['name'],
+          version=pyproject['version'],
+          description=pyproject['description'],
+          author=pyproject['authors'][0],
+          license=pyproject['license'],
+          long_description='README.md',
+          packages=['unitee'],
+          python_requires='>=3.7',
+          extras_require={'dev': ['pytest']},
+          data_files=[[os.path.expanduser('~/.unitee/SISystem'),
+                      pyproject['include']]])
