789d1ff87db1436400463ed093bab428c4e4240f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:46 +0000

    Add package chess.com/1.6.2/chess.com-1.6.2.tar.gz

---
+++ packages/chess.com/chess.com-1.6.2.tar.gz/chess.com-1.6.2/chessdotcom/__init__.py
@@ -0,0 +1,4 @@
+from chessdotcom.errors import ChessDotComError
+from chessdotcom.client import *
+from chessdotcom.response import ChessDotComResponse
+
---
+++ packages/chess.com/chess.com-1.6.2.tar.gz/chess.com-1.6.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chess.com", 
+    version="1.6.2",
+    author="Artur Saradzhyan",
+    author_email="saradzhyanartur@gmail.com",
+    description="Python Wrapper around Chess.com API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sarartur/chess.com",
+    packages=setuptools.find_packages(),
+    install_requires=['requests==2.25.1', ],
+    setup_requires=['requests==2.25.1', ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
