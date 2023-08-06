808c010523c2edd54e3c4a05b030649f186fae12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:34 +0000

    Add package pytask-parallel/0.0.7/pytask-parallel-0.0.7.tar.gz

---
+++ packages/pytask-parallel/pytask-parallel-0.0.7.tar.gz/pytask-parallel-0.0.7/src/pytask_parallel/plugin.py
@@ -0,0 +1,15 @@
+"""Entry-point for the plugin."""
+from _pytask.config import hookimpl
+from pytask_parallel import build
+from pytask_parallel import config
+from pytask_parallel import execute
+from pytask_parallel import logging
+
+
+@hookimpl
+def pytask_add_hooks(pm):
+    """Register plugins."""
+    pm.register(build)
+    pm.register(config)
+    pm.register(execute)
+    pm.register(logging)
