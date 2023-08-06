231131d8414d1bef4503128f76980fcc97710210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:29 +0000

    Add package pdbr/0.4.1/pdbr-0.4.1.tar.gz

---
+++ packages/pdbr/pdbr-0.4.1.tar.gz/pdbr-0.4.1/pdbr/utils.py
@@ -0,0 +1,41 @@
+from rich.layout import Layout
+
+
+def set_history_file(filename):
+    import atexit
+    import os
+    import readline
+
+    histfile = os.path.join(os.path.expanduser("~"), filename)
+    try:
+        readline.read_history_file(histfile)
+        readline.set_history_length(1000)
+    except FileNotFoundError:
+        pass
+
+    atexit.register(readline.write_history_file, histfile)
+
+
+def make_layout():
+    layout = Layout()
+    left_layout = Layout(name="left", ratio=2)
+    right_layout = Layout(name="right")
+    layout.split(left_layout, right_layout, direction="horizontal")
+    return layout
+
+
+def set_traceback(theme):
+    from rich.traceback import install
+
+    install(theme=theme)
+
+
+def debugger_cls():
+    try:
+        from IPython.terminal.debugger import TerminalPdb
+
+        return TerminalPdb
+    except BaseException:
+        from pdb import Pdb
+
+        return Pdb
