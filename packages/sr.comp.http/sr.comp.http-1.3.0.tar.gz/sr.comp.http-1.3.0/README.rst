ae6fdb67921e1bcf10004ac2e59e182e83d1fca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:16 +0000

    Add package sr.comp.http/1.3.0/sr.comp.http-1.3.0.tar.gz

---
+++ packages/sr.comp.http/sr.comp.http-1.3.0.tar.gz/sr.comp.http-1.3.0/README.rst
@@ -0,0 +1,49 @@
+SR Comp HTTP
+============
+
+|Build Status| |Docs Status|
+
+A HTTP interface around `SRComp <https://github.com/PeterJCLaw/srcomp/wiki/SRComp>`__,
+the fifth round of `Student Robotics <http://srobo.org>`__ competition
+software.
+
+This repository provides a JSON API to accessing information about the
+state of the competition. It is a lightweight
+`Flask <http://flask.pocoo.org/>`__ application wrapping the
+`SRComp <https://github.com/PeterJCLaw/srcomp>`__ python
+APIs to the competition state.
+
+Usage
+-----
+
+Run with ``./run $COMPSTATE``.
+
+Test with ``./run-tests``.
+
+Developers may wish to use the `SRComp
+Dev <https://github.com/PeterJCLaw/srcomp-dev>`__ repo
+to setup a dev instance.
+
+State Caching
+~~~~~~~~~~~~~
+
+Since loading a given state repo takes a non-trivial amount of time,
+this is cached within the Flask application. Updates to the state repo
+are not tracked directly, and must be signalled by running the
+``./update`` script provided.
+
+Requirements
+------------
+
+-  `SRComp <https://github.com/PeterJCLaw/srcomp>`__
+-  Flask
+-  python-dateutil
+-  simplejson
+
+Don't forget to also check the requirements for SRComp!
+
+.. |Build Status| image:: https://travis-ci.org/PeterJCLaw/srcomp-http.png?branch=master
+   :target: https://travis-ci.org/PeterJCLaw/srcomp-http
+
+.. |Docs Status| image:: https://readthedocs.org/projects/srcomp-http/badge/?version=latest
+   :target: https://srcomp-http.readthedocs.org/
---
+++ packages/sr.comp.http/sr.comp.http-1.3.0.tar.gz/sr.comp.http-1.3.0/sr/comp/http/manager.py
@@ -0,0 +1,113 @@
+"""Routines for managing a Compstate instance."""
+
+import contextlib
+import errno
+import fcntl
+import logging
+import os
+import time
+from typing import IO, Iterator, Optional
+
+from sr.comp.comp import SRComp
+
+LOCK_FILE = ".update-lock"
+UPDATE_FILE = ".update-pls"
+
+
+def update_lock_path(compstate_path: str) -> str:
+    return os.path.join(compstate_path, LOCK_FILE)
+
+
+def update_pls_path(compstate_path: str) -> str:
+    return os.path.join(compstate_path, UPDATE_FILE)
+
+
+def exclusive_lock(lock_path: str) -> IO[str]:
+    fd = open(lock_path, "w")
+    fcntl.lockf(fd, fcntl.LOCK_EX)
+    return fd
+
+
+def share_lock(lock_path: str) -> IO[str]:
+    try:
+        fd = open(lock_path, "r")
+    except IOError as ioe:
+        if ioe.errno == errno.ENOENT:
+            # Touch the file so it exists
+            fd = open(lock_path, "w+")
+        else:
+            # Some other issue -- fail out
+            raise
+    fcntl.lockf(fd, fcntl.LOCK_SH)
+    return fd
+
+
+def touch_update_file(compstate_path: str) -> None:
+    file_path = update_pls_path(compstate_path)
+    open(file_path, 'w').close()
+
+
+@contextlib.contextmanager
+def update_lock(compstate_path: str) -> Iterator[None]:
+    """
+    Acquire a lock on the given compstate for the purposes of updating it.
+
+    :return: A context manager object which will remove the lock when
+             __exit__ed and, if a clean exit, touch the update file. In turn
+             that triggers the manager to re load the information it has.
+    """
+
+    lock_path = update_lock_path(compstate_path)
+    with exclusive_lock(lock_path):
+        yield
+        touch_update_file(compstate_path)
+
+
+class SRCompManager:
+    """An ``SRComp`` manager."""
+
+    def __init__(self) -> None:
+        self.root_dir = "./"
+
+        self.update_time = None  # type: Optional[float]
+        """The last time we updated our information."""
+
+        self._update_pls_time = None  # type: Optional[float]
+        """The time the update pls file was last modified."""
+
+        self._comp = None  # type: Optional[SRComp]
+        """Cached SRComp instance."""
+
+    def _load(self) -> None:
+        lock_path = update_lock_path(self.root_dir)
+        with share_lock(lock_path):
+            # Grab a lock & reload
+            logging.info("Loading compstate from %s", self.root_dir)
+            self._comp = SRComp(self.root_dir)
+            self.update_time = time.time()
+
+    def _state_changed(self) -> bool:
+        update_path = update_pls_path(self.root_dir)
+        try:
+            new_time = os.path.getmtime(update_path)
+        except OSError:
+            # It doesn't exist. That's fine -- use a value which won't ever
+            # happen so that we load at least the first time through
+            new_time = -1
+
+        if new_time != self._update_pls_time:
+            self._update_pls_time = new_time
+            return True
+
+        return False
+
+    def get_comp(self) -> SRComp:
+        if self.update_time is None:
+            self._load()
+
+        elif time.time() - self.update_time > 5 and self._state_changed():
+            # data is more than 5 seconds old and the state has changed, reload
+            self._load()
+
+        assert self._comp is not None
+        return self._comp
