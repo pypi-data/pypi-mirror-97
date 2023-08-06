4dd067bc742ebac890e9b61d070d89b4069bbd86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:42 +0000

    Add package mindsdb-sdk/0.14.0/mindsdb_sdk-0.14.0.tar.gz

---
+++ packages/mindsdb-sdk/mindsdb_sdk-0.14.0.tar.gz/mindsdb_sdk-0.14.0/mindsdb_sdk/classes/sdk.py
@@ -0,0 +1,20 @@
+from mindsdb_sdk.classes.proxy import Proxy
+from mindsdb_sdk.classes.datasources import DataSources
+from mindsdb_sdk.classes.predictors import Predictors
+from mindsdb_sdk.classes.intergrations import Integrations
+
+
+class SDK():
+    def __init__(self, host, user=None, password=None, token=None, url_token=None):
+        self.proxy = Proxy(host, user, password, token, url_token)
+        if self.proxy.ping():
+            print(f'Connected to mindsdb host: {host} !')
+        else:
+            print(f'Failed to connect to mindsdb host: {host} !')
+
+        self.datasources = DataSources(self.proxy)
+        self.predictors = Predictors(self.proxy)
+        self.integrations = Integrations(self.proxy)
+
+    def ping(self):
+        return self.proxy.ping()
