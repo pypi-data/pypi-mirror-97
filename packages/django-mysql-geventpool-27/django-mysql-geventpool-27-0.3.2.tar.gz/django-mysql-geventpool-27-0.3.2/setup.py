a37c38f5a10e25ce1054c59f8348cdf2f4d09ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:56 +0000

    Add package django-mysql-geventpool-27/0.3.2/django-mysql-geventpool-27-0.3.2.tar.gz

---
+++ packages/django-mysql-geventpool-27/django-mysql-geventpool-27-0.3.2.tar.gz/django-mysql-geventpool-27-0.3.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-mysql-geventpool-27',
+    version='0.3.2',
+    install_requires=['django<2.0', 'gevent'],
+    description='Add a MySQL connection pool for django using gevent',
+    long_description=open("README.rst").read(),
+    packages=find_packages(),
+    include_package_data=True,
+    license='Apache 2.0',
+    lassifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+    author='lollo789',
+    author_email='laurent@labatut.net'
+)
