b2ece802f1fa0ff528fe0ed3cc5845f0fb0aba58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:59 +0000

    Add package hdx-data-freshness-emailer/1.5.1/hdx_data_freshness_emailer-1.5.1-py2.py3-none-any.whl

---
+++ packages/hdx-data-freshness-emailer/hdx_data_freshness_emailer-1.5.1-py2.py3-none-any.whl/hdx/freshness/emailer/__main__.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+REGISTER:
+---------
+
+Caller script. Designed to call all other functions.
+
+"""
+import argparse
+import datetime
+import logging
+from os import getenv
+
+from hdx.database import Database
+from hdx.facades.keyword_arguments import facade
+from hdx.hdx_configuration import Configuration
+from hdx.utilities.dictandlist import args_to_dict
+from hdx.utilities.easy_logging import setup_logging
+from hdx.utilities.encoding import base64_to_str
+from hdx.utilities.path import script_dir_plus_file
+
+from hdx.freshness.emailer.databasequeries import DatabaseQueries
+from hdx.freshness.emailer.datafreshnessstatus import DataFreshnessStatus
+from hdx.freshness.emailer.datasethelper import DatasetHelper
+from hdx.freshness.emailer.freshnessemail import Email
+from hdx.freshness.emailer.sheet import Sheet
+from hdx.freshness.emailer.version import get_freshness_emailer_version
+
+setup_logging()
+logger = logging.getLogger(__name__)
+
+
+def main(db_url, db_params, email_server, gsheet_auth, email_test, spreadsheet_test, **ignore):
+    logger.info('> Data freshness emailer %s' % get_freshness_emailer_version())
+    configuration = Configuration.read()
+    if email_server:
+        email_config = email_server.split(',')
+        email_config_dict = {'connection_type': email_config[0], 'host': email_config[1], 'port': int(email_config[2]),
+                             'username': email_config[3], 'password': email_config[4]}
+        if len(email_config) > 5:
+            email_config_dict['sender'] = email_config[5]
+        configuration.setup_emailer(email_config_dict=email_config_dict)
+        logger.info('> Email host: %s' % email_config[1])
+        send_emails = configuration.emailer().send
+    else:
+        logger.info('> No email host!')
+        send_emails = None
+    if db_params:
+        params = args_to_dict(db_params)
+    elif db_url:
+        params = Database.get_params_from_sqlalchemy_url(db_url)
+    else:
+        params = {'driver': 'sqlite', 'database': 'freshness.db'}
+    logger.info('> Database parameters: %s' % params)
+    with Database(**params) as session:
+        now = datetime.datetime.utcnow()
+        email = Email(now, send_emails=send_emails, configuration=configuration)
+        sheet = Sheet(now)
+
+        failure_list = list()
+        for address in configuration['failure_emails']:
+            failure_list.append(base64_to_str(address))
+        error = sheet.setup_gsheet(configuration, gsheet_auth, spreadsheet_test)
+        if error:
+            email.htmlify_send(failure_list, 'Error opening Google sheets!', error)
+        else:
+            error = sheet.setup_input()
+            if error:
+                email.htmlify_send(failure_list, 'Error reading DP duty roster or data grid curation sheet!', error)
+            else:
+                datasethelper = DatasetHelper(site_url=configuration.get_hdx_site_url())
+                databasequeries = DatabaseQueries(session=session, now=now)
+                freshness = DataFreshnessStatus(datasethelper=datasethelper, databasequeries=databasequeries,
+                                                email=email, sheet=sheet)
+                if not freshness.check_number_datasets(now, send_failures=failure_list):
+                    test_users = [failure_list[0]]
+                    if email_test:  # send just to test users
+                        freshness.process_broken(recipients=test_users)
+                        freshness.process_overdue(recipients=test_users, sysadmins=test_users)
+                        freshness.process_delinquent(recipients=test_users)
+                        freshness.process_maintainer_orgadmins(recipients=test_users)
+                        freshness.process_datasets_noresources(recipients=test_users)
+                        # freshness.process_datasets_dataset_date(recipients=test_users, sysadmins=test_users)
+                        freshness.process_datasets_datagrid(recipients=test_users)
+                    else:
+                        freshness.process_broken()
+                        freshness.process_overdue()
+                        freshness.process_delinquent()
+                        freshness.process_maintainer_orgadmins()
+                        freshness.process_datasets_noresources()
+                        # freshness.process_datasets_dataset_date(sysadmins=test_users)
+                        freshness.process_datasets_datagrid()
+
+    logger.info('Freshness emailer completed!')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Data Freshness Emailer')
+    parser.add_argument('-hk', '--hdx_key', default=None, help='HDX api key')
+    parser.add_argument('-ua', '--user_agent', default=None, help='user agent')
+    parser.add_argument('-pp', '--preprefix', default=None, help='preprefix')
+    parser.add_argument('-hs', '--hdx_site', default=None, help='HDX site to use')
+    parser.add_argument('-db', '--db_url', default=None, help='Database connection string')
+    parser.add_argument('-dp', '--db_params', default=None, help='Database connection parameters. Overrides --db_url.')
+    parser.add_argument('-es', '--email_server', default=None, help='Email server to use')
+    parser.add_argument('-gs', '--gsheet_auth', default=None, help='Credentials for accessing Google Sheets')
+    parser.add_argument('-et', '--email_test', default=False, action='store_true',
+                        help='Email only test users for testing purposes')
+    parser.add_argument('-st', '--spreadsheet_test', default=False, action='store_true',
+                        help='Use test instead of prod issues spreadsheet')
+    args = parser.parse_args()
+    hdx_key = args.hdx_key
+    if hdx_key is None:
+        hdx_key = getenv('HDX_KEY')
+    user_agent = args.user_agent
+    if user_agent is None:
+        user_agent = getenv('USER_AGENT')
+        if user_agent is None:
+            user_agent = 'freshness-emailer'
+    preprefix = args.preprefix
+    if preprefix is None:
+        preprefix = getenv('PREPREFIX')
+    hdx_site = args.hdx_site
+    if hdx_site is None:
+        hdx_site = getenv('HDX_SITE', 'prod')
+    db_url = args.db_url
+    if db_url is None:
+        db_url = getenv('DB_URL')
+    if db_url and '://' not in db_url:
+        db_url = 'postgresql://%s' % db_url
+    email_server = args.email_server
+    if email_server is None:
+        email_server = getenv('EMAIL_SERVER')
+    gsheet_auth = args.gsheet_auth
+    if gsheet_auth is None:
+        gsheet_auth = getenv('GSHEET_AUTH')
+    project_config_yaml = script_dir_plus_file('project_configuration.yml', main)
+    facade(main, hdx_key=hdx_key, user_agent=user_agent, preprefix=preprefix, hdx_site=hdx_site,
+           project_config_yaml=project_config_yaml, db_url=db_url, db_params=args.db_params,
+           email_server=email_server, gsheet_auth=gsheet_auth, email_test=args.email_test,
+           spreadsheet_test=args.spreadsheet_test)
---
+++ packages/hdx-data-freshness-emailer/hdx_data_freshness_emailer-1.5.1-py2.py3-none-any.whl/hdx/freshness/emailer/sheet.py
@@ -0,0 +1,215 @@
+# -*- coding: utf-8 -*-
+"""
+Sheet
+-----
+
+Utilities to handle interaction with Google sheets
+"""
+import json
+import logging
+from datetime import datetime
+
+import pygsheets
+from google.oauth2 import service_account
+
+logger = logging.getLogger(__name__)
+
+
+def get_date(datestr):
+    return datetime.strptime(datestr, '%Y-%m-%d')
+
+
+class Sheet:
+    def __init__(self, now):
+        self.now = now
+        self.dutyofficers_spreadsheet = None
+        self.datagrids_spreadsheet = None
+        self.issues_spreadsheet = None
+        self.dutyofficer = None
+        self.datagrids = dict()
+        self.datagridccs = list()
+
+    @staticmethod
+    def add_query(hxltags, grid, row):
+        category = row[hxltags['#category']].strip()
+        include = row[hxltags['#include']]
+        if include:
+            include = include.strip()
+        exclude = row[hxltags['#exclude']]
+        if exclude:
+            exclude = exclude.strip()
+        query = grid.get(category, '')
+        if query:
+            queryparts = query.split(' ! ')
+            query = queryparts[0]
+            if include:
+                query = '%s OR %s' % (query, include)
+            if len(queryparts) > 1:
+                query = '%s ! %s' % (query, ' ! '.join(queryparts[1:]))
+        elif include:
+            query = include
+        if exclude:
+            query = '%s ! %s' % (query, exclude)
+        grid[category] = query
+
+    def get_datagrid(self, hxltags, dg, datagrids, defaultgrid):
+        datagridname = dg.strip()
+        if datagridname == '' or datagridname == 'cc':
+            return None
+        datagrid = self.datagrids.get(datagridname)
+        if datagrid is None:
+            datagrid = dict()
+            self.datagrids[datagridname] = datagrid
+            for row in datagrids:
+                if row[hxltags['#datagrid']] == datagridname:
+                    self.add_query(hxltags, datagrid, row)
+            for key in defaultgrid:
+                if key not in datagrid:
+                    if key == 'datagrid':
+                        datagrid[key] = defaultgrid[key].replace('$datagrid', datagridname)
+                    else:
+                        datagrid[key] = defaultgrid[key]
+        return datagrid
+
+    def setup_gsheet(self, configuration, gsheet_auth, spreadsheet_test):
+        if not gsheet_auth:
+            return 'No GSheet Credentials!'
+        try:
+            info = json.loads(gsheet_auth)
+            scopes = ['https://www.googleapis.com/auth/spreadsheets']
+            credentials = service_account.Credentials.from_service_account_info(info, scopes=scopes)
+            gc = pygsheets.authorize(custom_credentials=credentials)
+            if spreadsheet_test:  # use test not prod spreadsheet
+                issues_spreadsheet = configuration['test_issues_spreadsheet_url']
+            else:
+                issues_spreadsheet = configuration['prod_issues_spreadsheet_url']
+            logger.info('Opening duty officers gsheet')
+            self.dutyofficers_spreadsheet = gc.open_by_url(configuration['dutyofficers_url'])
+            logger.info('Opening datagrids gsheet')
+            self.datagrids_spreadsheet = gc.open_by_url(configuration['datagrids_url'])
+            logger.info('Opening issues gsheet')
+            self.issues_spreadsheet = gc.open_by_url(issues_spreadsheet)
+        except Exception as ex:
+            return str(ex)
+        return None
+
+    def setup_input(self):
+        logger.info('--------------------------------------------------')
+        try:
+            sheet = self.dutyofficers_spreadsheet.worksheet_by_title('DutyRoster')
+            current_values = sheet.get_all_values(returnas='matrix')
+            hxltags = {tag: i for i, tag in enumerate(current_values[1])}
+            startdate_ind = hxltags['#date+start']
+            contactname_ind = hxltags['#contact+name']
+            contactemail_ind = hxltags['#contact+email']
+            for row in sorted(current_values[2:], key=lambda x: x[startdate_ind], reverse=True):
+                startdate = row[startdate_ind].strip()
+                if datetime.strptime(startdate, '%Y-%m-%d') <= self.now:
+                    dutyofficer_name = row[contactname_ind]
+                    if dutyofficer_name:
+                        dutyofficer_name = dutyofficer_name.strip()
+                        self.dutyofficer = {'name': dutyofficer_name, 'email': row[contactemail_ind].strip()}
+                        logger.info('Duty officer: %s' % dutyofficer_name)
+                        break
+
+            sheet = self.datagrids_spreadsheet.worksheet_by_title('DataGrids')
+            current_values = sheet.get_all_values(returnas='matrix')
+            hxltags = {tag: i for i, tag in enumerate(current_values[1])}
+            datagrids = current_values[2:]
+            defaultgrid = dict()
+            for row in datagrids:
+                if row[hxltags['#datagrid']] == 'default':
+                    self.add_query(hxltags, defaultgrid, row)
+
+            sheet = self.datagrids_spreadsheet.worksheet_by_title('Curators')
+            current_values = sheet.get_all_values(returnas='matrix')
+            curators_hxltags = {tag: i for i, tag in enumerate(current_values[1])}
+            curators = current_values[2:]
+            for row in curators:
+                curatoremail = row[curators_hxltags['#contact+email']].strip()
+                owner = row[curators_hxltags['#datagrid']]
+                for dg in owner.strip().split(','):
+                    if dg.strip() == 'cc':
+                        self.datagridccs.append(curatoremail)
+            for row in curators:
+                curatorname = row[curators_hxltags['#contact+name']].strip()
+                curatoremail = row[curators_hxltags['#contact+email']].strip()
+                owner = row[curators_hxltags['#datagrid']]
+                if owner is not None:
+                    for dg in owner.strip().split(','):
+                        datagrid = self.get_datagrid(hxltags, dg, datagrids, defaultgrid)
+                        if datagrid is None:
+                            continue
+                        if datagrid.get('owner'):
+                            raise ValueError('There is more than one owner of datagrid %s!' % dg)
+                        datagrid['owner'] = {'name': curatorname, 'email': curatoremail}
+            for datagridname in self.datagrids:
+                if 'owner' not in self.datagrids[datagridname]:
+                    raise ValueError('Datagrid %s does not have an owner!' % datagridname)
+        except Exception as ex:
+            return str(ex)
+
+    def update(self, sheetname, datasets, dutyofficer_name=None):
+        # sheet must have been set up!
+        if self.issues_spreadsheet is None or (self.dutyofficer is None and dutyofficer_name is None):
+            logger.warning('Cannot update Google spreadsheet!')
+            return
+        logger.info('Updating Google spreadsheet.')
+        sheet = self.issues_spreadsheet.worksheet_by_title(sheetname)
+        current_values = sheet.get_all_values(returnas='matrix')
+        keys = current_values[0]
+        url_ind = keys.index('URL')
+        dateadded_ind = keys.index('Date Added')
+        no_times_ind = keys.index('No. Times')
+        assigned_ind = keys.index('Assigned')
+        status_ind = keys.index('Status')
+        urls = [x[url_ind] for i, x in enumerate(current_values) if i != 0]
+        updated_notimes = set()
+        for dataset in datasets:
+            url = dataset['URL']
+            new_row = [dataset.get(key, '') for key in keys]
+            try:
+                rowno = urls.index(url) + 1
+                current_row = current_values[rowno]
+                new_row[dateadded_ind] = current_row[dateadded_ind]
+                no_times = current_row[no_times_ind]
+                new_row[no_times_ind] = int(no_times)
+                if url not in updated_notimes:
+                    updated_notimes.add(url)
+                    new_row[no_times_ind] += 1
+                new_row[assigned_ind] = current_row[assigned_ind]
+                new_row[status_ind] = current_row[status_ind]
+                current_values[rowno] = new_row
+            except ValueError:
+                new_row[dateadded_ind] = self.now.isoformat()
+                new_row[no_times_ind] = 1
+                if dutyofficer_name is not None:
+                    new_row[assigned_ind] = dutyofficer_name
+                else:
+                    new_row[assigned_ind] = self.dutyofficer['name']
+                current_values.append(new_row)
+                urls.append(url)
+                updated_notimes.add(url)
+        current_values = sorted(current_values, key=lambda x: x[dateadded_ind], reverse=True)
+        sheet.update_values('A1', current_values)
+
+    @staticmethod
+    def construct_row(datasethelper, dataset, maintainer, orgadmins):
+        url = datasethelper.get_dataset_url(dataset)
+        title = dataset['title']
+        org_title = dataset['organization_title']
+        if maintainer:
+            maintainer_name, maintainer_email = maintainer
+        else:
+            maintainer_name, maintainer_email = '', ''
+        orgadmin_names = ','.join([x[0] for x in orgadmins])
+        orgadmin_emails = ','.join([x[1] for x in orgadmins])
+        update_freq = datasethelper.get_update_frequency(dataset)
+        latest_of_modifieds = dataset['latest_of_modifieds'].isoformat()
+        # URL	Title	Organisation	Maintainer	Maintainer Email	Org Admins	Org Admin Emails
+        # Update Frequency	Latest of Modifieds
+        row = {'URL': url, 'Title': title, 'Organisation': org_title,
+               'Maintainer': maintainer_name, 'Maintainer Email': maintainer_email,
+               'Org Admins': orgadmin_names, 'Org Admin Emails': orgadmin_emails,
+               'Update Frequency': update_freq, 'Latest of Modifieds': latest_of_modifieds}
+        return row
