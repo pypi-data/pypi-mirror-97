33e0d7e4549bde22c36b7977ece297bcbaa26d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:26 +0000

    Add package tiny-python-toolkit/1.0.1/tiny_python_toolkit-1.0.1.tar.gz

---
+++ packages/tiny-python-toolkit/tiny_python_toolkit-1.0.1.tar.gz/tiny_python_toolkit-1.0.1/setup.py
@@ -0,0 +1,14 @@
+r"""The setup file"""
+
+from setuptools import setup
+
+
+setup(
+    name='tiny_python_toolkit',
+    version='1.0.1',
+    description='Collections of some useful tiny tool',
+    atuhor='YTLi',
+    author_email='liyitingtom@gmail.com',
+    packages=['tpt'],
+    requires=['typing']
+)
